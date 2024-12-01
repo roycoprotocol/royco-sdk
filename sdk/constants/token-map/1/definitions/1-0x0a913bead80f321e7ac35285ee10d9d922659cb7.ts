628971c15cf29d94ee97a6a20f02028a2e3f265c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-01 09:17:11 -0800

    feat(token-map): Update Token -- 1-0x0a913bead80f321e7ac35285ee10d9d922659cb7 -- DOS Network (DOS) (0x0a91...9cb7)
    Token Details:
    - Id: 1-0x0a913bead80f321e7ac35285ee10d9d922659cb7
    - Chain Id: 1
    - Contract Address: 0x0a913bead80f321e7ac35285ee10d9d922659cb7
    - Name: DOS Network
    - Symbol: DOS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3809.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3809

---
+++ sdk/constants/token-map/1/definitions/1-0x0a913bead80f321e7ac35285ee10d9d922659cb7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0a913bead80f321e7ac35285ee10d9d922659cb7",
+  chain_id: 1,
+  contract_address: "0x0a913bead80f321e7ac35285ee10d9d922659cb7",
+  name: "DOS Network",
+  symbol: "DOS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3809.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3809",
+  type: "token",
+});
