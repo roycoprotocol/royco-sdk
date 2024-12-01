76802100154c075ecfbf31ca296dbe2f1cbc2392
githubotoro <yupuday.git@gmail.com>
2024-12-01 09:41:23 -0800

    feat(sdk): migration to npm

--- sdk/constants/token-map/1/definitions/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./1-0x0a2d9370cf74da3fd3df5d764e394ca8205c50b6";
---
+++ sdk/constants/token-map/1/index.ts
@@ -0,0 +1,6 @@
+import token_1_0x0a913bead80f321e7ac35285ee10d9d922659cb7 from "./definitions/1-0x0a913bead80f321e7ac35285ee10d9d922659cb7";
+
+export const TokenMap1 = {
+  [token_1_0x0a913bead80f321e7ac35285ee10d9d922659cb7.id]:
+    token_1_0x0a913bead80f321e7ac35285ee10d9d922659cb7,
+};
