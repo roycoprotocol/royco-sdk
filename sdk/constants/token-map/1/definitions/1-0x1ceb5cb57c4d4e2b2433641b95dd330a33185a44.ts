bdcfb533424a61081e7c94d2db275811e0128a95
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:28 -0800

    feat(token-map): Update Token -- 1-0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44 -- Keep3rV1 (KP3R) (0x1ceb...5a44)
    Token Details:
    - Id: 1-0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44
    - Chain Id: 1
    - Contract Address: 0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44
    - Name: Keep3rV1
    - Symbol: KP3R
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7535.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7535

---
+++ sdk/constants/token-map/1/definitions/1-0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44",
+  chain_id: 1,
+  contract_address: "0x1ceb5cb57c4d4e2b2433641b95dd330a33185a44",
+  name: "Keep3rV1",
+  symbol: "KP3R",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7535.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7535",
+  type: "token",
+});
