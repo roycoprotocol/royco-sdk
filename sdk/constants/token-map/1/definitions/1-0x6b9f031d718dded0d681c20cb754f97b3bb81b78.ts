21bfcae0e0cd83ad978e0c2902e7547a1e6da37b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:16:24 -0800

    feat(token-map): Update Token -- 1-0x6b9f031d718dded0d681c20cb754f97b3bb81b78 -- Geeq (GEEQ) (0x6b9f...1b78)
    Token Details:
    - Id: 1-0x6b9f031d718dded0d681c20cb754f97b3bb81b78
    - Chain Id: 1
    - Contract Address: 0x6b9f031d718dded0d681c20cb754f97b3bb81b78
    - Name: Geeq
    - Symbol: GEEQ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6194.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6194

---
+++ sdk/constants/token-map/1/definitions/1-0x6b9f031d718dded0d681c20cb754f97b3bb81b78.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6b9f031d718dded0d681c20cb754f97b3bb81b78",
+  chain_id: 1,
+  contract_address: "0x6b9f031d718dded0d681c20cb754f97b3bb81b78",
+  name: "Geeq",
+  symbol: "GEEQ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6194.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6194",
+  type: "token",
+});
