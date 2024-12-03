24342544e56307b337e169ba4020dd58239eaddf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:36:03 -0800

    feat(token-map): Update Token -- 1-0xe5b826ca2ca02f09c1725e9bd98d9a8874c30532 -- ZEON (ZEON) (0xe5b8...0532)
    Token Details:
    - Id: 1-0xe5b826ca2ca02f09c1725e9bd98d9a8874c30532
    - Chain Id: 1
    - Contract Address: 0xe5b826ca2ca02f09c1725e9bd98d9a8874c30532
    - Name: ZEON
    - Symbol: ZEON
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3795.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3795

---
+++ sdk/constants/token-map/1/definitions/1-0xe5b826ca2ca02f09c1725e9bd98d9a8874c30532.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe5b826ca2ca02f09c1725e9bd98d9a8874c30532",
+  chain_id: 1,
+  contract_address: "0xe5b826ca2ca02f09c1725e9bd98d9a8874c30532",
+  name: "ZEON",
+  symbol: "ZEON",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3795.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3795",
+  type: "token",
+});
