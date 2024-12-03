0db6217add0422ad9b29ca0c74ae293dbc3b4138
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:30 -0800

    feat(token-map): Update Token -- 1-0x7c84e62859d0715eb77d1b1c4154ecd6abb21bec -- SHPING (SHPING) (0x7c84...1bec)
    Token Details:
    - Id: 1-0x7c84e62859d0715eb77d1b1c4154ecd6abb21bec
    - Chain Id: 1
    - Contract Address: 0x7c84e62859d0715eb77d1b1c4154ecd6abb21bec
    - Name: SHPING
    - Symbol: SHPING
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3422.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3422

---
+++ sdk/constants/token-map/1/definitions/1-0x7c84e62859d0715eb77d1b1c4154ecd6abb21bec.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7c84e62859d0715eb77d1b1c4154ecd6abb21bec",
+  chain_id: 1,
+  contract_address: "0x7c84e62859d0715eb77d1b1c4154ecd6abb21bec",
+  name: "SHPING",
+  symbol: "SHPING",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3422.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3422",
+  type: "token",
+});
