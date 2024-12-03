9f76ca885128f836abb33a607255c672a4858cc2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:36 -0800

    feat(token-map): Update Token -- 1-0x1e4ede388cbc9f4b5c79681b7f94d36a11abebc9 -- X2Y2 (X2Y2) (0x1e4e...ebc9)
    Token Details:
    - Id: 1-0x1e4ede388cbc9f4b5c79681b7f94d36a11abebc9
    - Chain Id: 1
    - Contract Address: 0x1e4ede388cbc9f4b5c79681b7f94d36a11abebc9
    - Name: X2Y2
    - Symbol: X2Y2
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/18106.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 18106

---
+++ sdk/constants/token-map/1/definitions/1-0x1e4ede388cbc9f4b5c79681b7f94d36a11abebc9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1e4ede388cbc9f4b5c79681b7f94d36a11abebc9",
+  chain_id: 1,
+  contract_address: "0x1e4ede388cbc9f4b5c79681b7f94d36a11abebc9",
+  name: "X2Y2",
+  symbol: "X2Y2",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/18106.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "18106",
+  type: "token",
+});
