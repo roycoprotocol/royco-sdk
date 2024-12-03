266ed3476f8f29869c24f0c317ead8f525abcb98
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:57:49 -0800

    feat(token-map): Update Token -- 1-0x06450dee7fd2fb8e39061434babcfc05599a6fb8 -- XEN Crypto (XEN) (0x0645...6fb8)
    Token Details:
    - Id: 1-0x06450dee7fd2fb8e39061434babcfc05599a6fb8
    - Chain Id: 1
    - Contract Address: 0x06450dee7fd2fb8e39061434babcfc05599a6fb8
    - Name: XEN Crypto
    - Symbol: XEN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/22118.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 22118

---
+++ sdk/constants/token-map/1/definitions/1-0x06450dee7fd2fb8e39061434babcfc05599a6fb8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x06450dee7fd2fb8e39061434babcfc05599a6fb8",
+  chain_id: 1,
+  contract_address: "0x06450dee7fd2fb8e39061434babcfc05599a6fb8",
+  name: "XEN Crypto",
+  symbol: "XEN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/22118.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "22118",
+  type: "token",
+});
