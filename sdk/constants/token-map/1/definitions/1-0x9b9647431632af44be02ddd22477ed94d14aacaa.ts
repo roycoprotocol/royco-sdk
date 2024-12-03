180048f8f678a911299e34706643c54f52bbf45c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:37 -0800

    feat(token-map): Update Token -- 1-0x9b9647431632af44be02ddd22477ed94d14aacaa -- KOK (KOK) (0x9b96...acaa)
    Token Details:
    - Id: 1-0x9b9647431632af44be02ddd22477ed94d14aacaa
    - Chain Id: 1
    - Contract Address: 0x9b9647431632af44be02ddd22477ed94d14aacaa
    - Name: KOK
    - Symbol: KOK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5185.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5185

---
+++ sdk/constants/token-map/1/definitions/1-0x9b9647431632af44be02ddd22477ed94d14aacaa.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9b9647431632af44be02ddd22477ed94d14aacaa",
+  chain_id: 1,
+  contract_address: "0x9b9647431632af44be02ddd22477ed94d14aacaa",
+  name: "KOK",
+  symbol: "KOK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5185.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5185",
+  type: "token",
+});
