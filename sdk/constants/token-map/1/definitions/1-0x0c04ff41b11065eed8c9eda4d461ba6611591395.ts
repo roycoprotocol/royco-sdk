93f582566c5f26b301f626b40de51c9e0a3b03f9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:19 -0800

    feat(token-map): Update Token -- 1-0x0c04ff41b11065eed8c9eda4d461ba6611591395 -- VIVEK (TRUTH) (0x0c04...1395)
    Token Details:
    - Id: 1-0x0c04ff41b11065eed8c9eda4d461ba6611591395
    - Chain Id: 1
    - Contract Address: 0x0c04ff41b11065eed8c9eda4d461ba6611591395
    - Name: VIVEK
    - Symbol: TRUTH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/32012.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 32012

---
+++ sdk/constants/token-map/1/definitions/1-0x0c04ff41b11065eed8c9eda4d461ba6611591395.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0c04ff41b11065eed8c9eda4d461ba6611591395",
+  chain_id: 1,
+  contract_address: "0x0c04ff41b11065eed8c9eda4d461ba6611591395",
+  name: "VIVEK",
+  symbol: "TRUTH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32012.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "32012",
+  type: "token",
+});
