4f1fe69dc58824afee01b5e347c67bc78fff9acf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:36 -0800

    feat(token-map): Update Token -- 1-0x2f109021afe75b949429fe30523ee7c0d5b27207 -- Occam.Fi (OCC) (0x2f10...7207)
    Token Details:
    - Id: 1-0x2f109021afe75b949429fe30523ee7c0d5b27207
    - Chain Id: 1
    - Contract Address: 0x2f109021afe75b949429fe30523ee7c0d5b27207
    - Name: Occam.Fi
    - Symbol: OCC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9191.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9191

---
+++ sdk/constants/token-map/1/definitions/1-0x2f109021afe75b949429fe30523ee7c0d5b27207.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2f109021afe75b949429fe30523ee7c0d5b27207",
+  chain_id: 1,
+  contract_address: "0x2f109021afe75b949429fe30523ee7c0d5b27207",
+  name: "Occam.Fi",
+  symbol: "OCC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9191.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9191",
+  type: "token",
+});
