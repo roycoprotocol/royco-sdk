fcf0a1f4aaa3b38377be63b19411a378f83757d5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:44 -0800

    feat(token-map): Update Token -- 1-0x8a88f04e0c905054d2f33b26bb3a46d7091a039a -- IGToken (IG) (0x8a88...039a)
    Token Details:
    - Id: 1-0x8a88f04e0c905054d2f33b26bb3a46d7091a039a
    - Chain Id: 1
    - Contract Address: 0x8a88f04e0c905054d2f33b26bb3a46d7091a039a
    - Name: IGToken
    - Symbol: IG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3121.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3121

---
+++ sdk/constants/token-map/1/definitions/1-0x8a88f04e0c905054d2f33b26bb3a46d7091a039a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8a88f04e0c905054d2f33b26bb3a46d7091a039a",
+  chain_id: 1,
+  contract_address: "0x8a88f04e0c905054d2f33b26bb3a46d7091a039a",
+  name: "IGToken",
+  symbol: "IG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3121.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3121",
+  type: "token",
+});
