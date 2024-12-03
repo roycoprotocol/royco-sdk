45fc39916b6995db2ced17d80f9e63751109daaa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:31:50 -0800

    feat(token-map): Update Token -- 1-0xc7bba5b765581efb2cdd2679db5bea9ee79b201f -- Gems (GEM) (0xc7bb...201f)
    Token Details:
    - Id: 1-0xc7bba5b765581efb2cdd2679db5bea9ee79b201f
    - Chain Id: 1
    - Contract Address: 0xc7bba5b765581efb2cdd2679db5bea9ee79b201f
    - Name: Gems
    - Symbol: GEM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2537.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2537

---
+++ sdk/constants/token-map/1/definitions/1-0xc7bba5b765581efb2cdd2679db5bea9ee79b201f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc7bba5b765581efb2cdd2679db5bea9ee79b201f",
+  chain_id: 1,
+  contract_address: "0xc7bba5b765581efb2cdd2679db5bea9ee79b201f",
+  name: "Gems",
+  symbol: "GEM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2537.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2537",
+  type: "token",
+});
