515364c2ba98b6030bb5366209317e565bb99eb9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:50 -0800

    feat(token-map): Update Token -- 1-0x3231cb76718cdef2155fc47b5286d82e6eda273f -- Monerium EUR emoney (EURe) (0x3231...273f)
    Token Details:
    - Id: 1-0x3231cb76718cdef2155fc47b5286d82e6eda273f
    - Chain Id: 1
    - Contract Address: 0x3231cb76718cdef2155fc47b5286d82e6eda273f
    - Name: Monerium EUR emoney
    - Symbol: EURe
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/20920.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 20920

---
+++ sdk/constants/token-map/1/definitions/1-0x3231cb76718cdef2155fc47b5286d82e6eda273f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3231cb76718cdef2155fc47b5286d82e6eda273f",
+  chain_id: 1,
+  contract_address: "0x3231cb76718cdef2155fc47b5286d82e6eda273f",
+  name: "Monerium EUR emoney",
+  symbol: "EURe",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/20920.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "20920",
+  type: "token",
+});
