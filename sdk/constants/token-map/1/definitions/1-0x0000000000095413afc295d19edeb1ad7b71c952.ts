b0b609627bdc0f235f074be8250c48a1c661c2e9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:55:16 -0800

    feat(token-map): Update Token -- 1-0x0000000000095413afc295d19edeb1ad7b71c952 -- Tokenlon Network Token (LON) (0x0000...c952)
    Token Details:
    - Id: 1-0x0000000000095413afc295d19edeb1ad7b71c952
    - Chain Id: 1
    - Contract Address: 0x0000000000095413afc295d19edeb1ad7b71c952
    - Name: Tokenlon Network Token
    - Symbol: LON
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8083.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8083

---
+++ sdk/constants/token-map/1/definitions/1-0x0000000000095413afc295d19edeb1ad7b71c952.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0000000000095413afc295d19edeb1ad7b71c952",
+  chain_id: 1,
+  contract_address: "0x0000000000095413afc295d19edeb1ad7b71c952",
+  name: "Tokenlon Network Token",
+  symbol: "LON",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8083.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8083",
+  type: "token",
+});
