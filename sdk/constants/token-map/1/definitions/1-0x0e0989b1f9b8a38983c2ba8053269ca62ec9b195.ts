e09c058809e198e050e4f3dd13a6275ba93129b2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:42 -0800

    feat(token-map): Update Token -- 1-0x0e0989b1f9b8a38983c2ba8053269ca62ec9b195 -- Po.et (POE) (0x0e09...b195)
    Token Details:
    - Id: 1-0x0e0989b1f9b8a38983c2ba8053269ca62ec9b195
    - Chain Id: 1
    - Contract Address: 0x0e0989b1f9b8a38983c2ba8053269ca62ec9b195
    - Name: Po.et
    - Symbol: POE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1937.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 1937

---
+++ sdk/constants/token-map/1/definitions/1-0x0e0989b1f9b8a38983c2ba8053269ca62ec9b195.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0e0989b1f9b8a38983c2ba8053269ca62ec9b195",
+  chain_id: 1,
+  contract_address: "0x0e0989b1f9b8a38983c2ba8053269ca62ec9b195",
+  name: "Po.et",
+  symbol: "POE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1937.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "1937",
+  type: "token",
+});
