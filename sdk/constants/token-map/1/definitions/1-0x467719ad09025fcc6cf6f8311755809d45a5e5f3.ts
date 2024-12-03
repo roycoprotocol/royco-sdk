84c68b23cdcf9c53fa3ac8d7e0baed2bd972348d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:47 -0800

    feat(token-map): Update Token -- 1-0x467719ad09025fcc6cf6f8311755809d45a5e5f3 -- Axelar (AXL) (0x4677...e5f3)
    Token Details:
    - Id: 1-0x467719ad09025fcc6cf6f8311755809d45a5e5f3
    - Chain Id: 1
    - Contract Address: 0x467719ad09025fcc6cf6f8311755809d45a5e5f3
    - Name: Axelar
    - Symbol: AXL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/17799.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 17799

---
+++ sdk/constants/token-map/1/definitions/1-0x467719ad09025fcc6cf6f8311755809d45a5e5f3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x467719ad09025fcc6cf6f8311755809d45a5e5f3",
+  chain_id: 1,
+  contract_address: "0x467719ad09025fcc6cf6f8311755809d45a5e5f3",
+  name: "Axelar",
+  symbol: "AXL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/17799.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "17799",
+  type: "token",
+});
