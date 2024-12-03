0dd1c4bfd80c7cd9bf109ee9ce2e7ae2e28b3dc8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:05 -0800

    feat(token-map): Update Token -- 1-0x7995ab36bb307afa6a683c24a25d90dc1ea83566 -- HitChain (HIT) (0x7995...3566)
    Token Details:
    - Id: 1-0x7995ab36bb307afa6a683c24a25d90dc1ea83566
    - Chain Id: 1
    - Contract Address: 0x7995ab36bb307afa6a683c24a25d90dc1ea83566
    - Name: HitChain
    - Symbol: HIT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3182.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 3182

---
+++ sdk/constants/token-map/1/definitions/1-0x7995ab36bb307afa6a683c24a25d90dc1ea83566.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7995ab36bb307afa6a683c24a25d90dc1ea83566",
+  chain_id: 1,
+  contract_address: "0x7995ab36bb307afa6a683c24a25d90dc1ea83566",
+  name: "HitChain",
+  symbol: "HIT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3182.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "3182",
+  type: "token",
+});
