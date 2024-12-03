39c59ee834cd5842fbd8fc2190b397ce28b62d2d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:13 -0800

    feat(token-map): Update Token -- 1-0xee059f0ca1507e4e20c689b20cff71b5e924f7bd -- Litecoin SV (LSV) (0xee05...f7bd)
    Token Details:
    - Id: 1-0xee059f0ca1507e4e20c689b20cff71b5e924f7bd
    - Chain Id: 1
    - Contract Address: 0xee059f0ca1507e4e20c689b20cff71b5e924f7bd
    - Name: Litecoin SV
    - Symbol: LSV
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5577.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5577

---
+++ sdk/constants/token-map/1/definitions/1-0xee059f0ca1507e4e20c689b20cff71b5e924f7bd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xee059f0ca1507e4e20c689b20cff71b5e924f7bd",
+  chain_id: 1,
+  contract_address: "0xee059f0ca1507e4e20c689b20cff71b5e924f7bd",
+  name: "Litecoin SV",
+  symbol: "LSV",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5577.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5577",
+  type: "token",
+});
