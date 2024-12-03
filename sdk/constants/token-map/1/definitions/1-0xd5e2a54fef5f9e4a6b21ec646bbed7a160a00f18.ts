b0ca81062851aacc508b2c1b27b61240dd5b6452
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:51 -0800

    feat(token-map): Update Token -- 1-0xd5e2a54fef5f9e4a6b21ec646bbed7a160a00f18 -- BeforeCoinMarketCap (BCMC1) (0xd5e2...0f18)
    Token Details:
    - Id: 1-0xd5e2a54fef5f9e4a6b21ec646bbed7a160a00f18
    - Chain Id: 1
    - Contract Address: 0xd5e2a54fef5f9e4a6b21ec646bbed7a160a00f18
    - Name: BeforeCoinMarketCap
    - Symbol: BCMC1
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9318.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9318

---
+++ sdk/constants/token-map/1/definitions/1-0xd5e2a54fef5f9e4a6b21ec646bbed7a160a00f18.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd5e2a54fef5f9e4a6b21ec646bbed7a160a00f18",
+  chain_id: 1,
+  contract_address: "0xd5e2a54fef5f9e4a6b21ec646bbed7a160a00f18",
+  name: "BeforeCoinMarketCap",
+  symbol: "BCMC1",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9318.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9318",
+  type: "token",
+});
