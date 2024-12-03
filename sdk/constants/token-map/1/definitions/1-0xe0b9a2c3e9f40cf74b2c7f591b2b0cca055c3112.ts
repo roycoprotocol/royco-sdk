0bef3258f6c8826e705c9028d9ea064179830ef1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:22 -0800

    feat(token-map): Update Token -- 1-0xe0b9a2c3e9f40cf74b2c7f591b2b0cca055c3112 -- Genesis Shards (GS) (0xe0b9...3112)
    Token Details:
    - Id: 1-0xe0b9a2c3e9f40cf74b2c7f591b2b0cca055c3112
    - Chain Id: 1
    - Contract Address: 0xe0b9a2c3e9f40cf74b2c7f591b2b0cca055c3112
    - Name: Genesis Shards
    - Symbol: GS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9196.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9196

---
+++ sdk/constants/token-map/1/definitions/1-0xe0b9a2c3e9f40cf74b2c7f591b2b0cca055c3112.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe0b9a2c3e9f40cf74b2c7f591b2b0cca055c3112",
+  chain_id: 1,
+  contract_address: "0xe0b9a2c3e9f40cf74b2c7f591b2b0cca055c3112",
+  name: "Genesis Shards",
+  symbol: "GS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9196.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9196",
+  type: "token",
+});
