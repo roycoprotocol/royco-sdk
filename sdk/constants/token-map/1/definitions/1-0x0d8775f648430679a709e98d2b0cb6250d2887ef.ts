037657bb60fb74664748812bde8b9418a1a6e5e7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:36 -0800

    feat(token-map): Update Token -- 1-0x0d8775f648430679a709e98d2b0cb6250d2887ef -- Basic Attention Token (BAT) (0x0d87...87ef)
    Token Details:
    - Id: 1-0x0d8775f648430679a709e98d2b0cb6250d2887ef
    - Chain Id: 1
    - Contract Address: 0x0d8775f648430679a709e98d2b0cb6250d2887ef
    - Name: Basic Attention Token
    - Symbol: BAT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1697.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1697

---
+++ sdk/constants/token-map/1/definitions/1-0x0d8775f648430679a709e98d2b0cb6250d2887ef.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+  chain_id: 1,
+  contract_address: "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+  name: "Basic Attention Token",
+  symbol: "BAT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1697.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1697",
+  type: "token",
+});
