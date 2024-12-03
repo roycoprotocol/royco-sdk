2460769f1ed4dccf44b5c6af30a195f0f2ab8ffc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:10 -0800

    feat(token-map): Update Token -- 1-0xd9016a907dc0ecfa3ca425ab20b6b785b42f2373 -- GAMEE (GMEE) (0xd901...2373)
    Token Details:
    - Id: 1-0xd9016a907dc0ecfa3ca425ab20b6b785b42f2373
    - Chain Id: 1
    - Contract Address: 0xd9016a907dc0ecfa3ca425ab20b6b785b42f2373
    - Name: GAMEE
    - Symbol: GMEE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9103.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9103

---
+++ sdk/constants/token-map/1/definitions/1-0xd9016a907dc0ecfa3ca425ab20b6b785b42f2373.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd9016a907dc0ecfa3ca425ab20b6b785b42f2373",
+  chain_id: 1,
+  contract_address: "0xd9016a907dc0ecfa3ca425ab20b6b785b42f2373",
+  name: "GAMEE",
+  symbol: "GMEE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9103.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9103",
+  type: "token",
+});
