29eba39dae177b26e24dd0cc32a63fe5e91566a6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:41 -0800

    feat(token-map): Update Token -- 1-0xf59ae934f6fe444afc309586cc60a84a0f89aaea -- Polkadex (PDEX) (0xf59a...aaea)
    Token Details:
    - Id: 1-0xf59ae934f6fe444afc309586cc60a84a0f89aaea
    - Chain Id: 1
    - Contract Address: 0xf59ae934f6fe444afc309586cc60a84a0f89aaea
    - Name: Polkadex
    - Symbol: PDEX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9017.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9017

---
+++ sdk/constants/token-map/1/definitions/1-0xf59ae934f6fe444afc309586cc60a84a0f89aaea.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf59ae934f6fe444afc309586cc60a84a0f89aaea",
+  chain_id: 1,
+  contract_address: "0xf59ae934f6fe444afc309586cc60a84a0f89aaea",
+  name: "Polkadex",
+  symbol: "PDEX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9017.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9017",
+  type: "token",
+});
