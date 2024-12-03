8546cbb2fdccc72064f39b00948b824364ec9e28
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:23 -0800

    feat(token-map): Update Token -- 1-0x67466be17df832165f8c80a5a120ccc652bd7e69 -- Landwolf 0x67 (WOLF) (0x6746...7e69)
    Token Details:
    - Id: 1-0x67466be17df832165f8c80a5a120ccc652bd7e69
    - Chain Id: 1
    - Contract Address: 0x67466be17df832165f8c80a5a120ccc652bd7e69
    - Name: Landwolf 0x67
    - Symbol: WOLF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/30902.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 30902

---
+++ sdk/constants/token-map/1/definitions/1-0x67466be17df832165f8c80a5a120ccc652bd7e69.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x67466be17df832165f8c80a5a120ccc652bd7e69",
+  chain_id: 1,
+  contract_address: "0x67466be17df832165f8c80a5a120ccc652bd7e69",
+  name: "Landwolf 0x67",
+  symbol: "WOLF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/30902.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "30902",
+  type: "token",
+});
