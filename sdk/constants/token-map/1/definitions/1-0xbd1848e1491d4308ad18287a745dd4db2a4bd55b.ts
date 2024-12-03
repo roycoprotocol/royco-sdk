f825b838909d545c17335484283234bc1fb95c8c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:59 -0800

    feat(token-map): Update Token -- 1-0xbd1848e1491d4308ad18287a745dd4db2a4bd55b -- Mochi Market (MOMA) (0xbd18...d55b)
    Token Details:
    - Id: 1-0xbd1848e1491d4308ad18287a745dd4db2a4bd55b
    - Chain Id: 1
    - Contract Address: 0xbd1848e1491d4308ad18287a745dd4db2a4bd55b
    - Name: Mochi Market
    - Symbol: MOMA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9440.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9440

---
+++ sdk/constants/token-map/1/definitions/1-0xbd1848e1491d4308ad18287a745dd4db2a4bd55b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbd1848e1491d4308ad18287a745dd4db2a4bd55b",
+  chain_id: 1,
+  contract_address: "0xbd1848e1491d4308ad18287a745dd4db2a4bd55b",
+  name: "Mochi Market",
+  symbol: "MOMA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9440.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9440",
+  type: "token",
+});
