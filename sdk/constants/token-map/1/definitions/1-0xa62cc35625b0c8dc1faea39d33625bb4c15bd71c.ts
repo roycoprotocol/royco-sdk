612eebc1a2b6d50a137d3cecd74c7651630b3b76
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:23 -0800

    feat(token-map): Update Token -- 1-0xa62cc35625b0c8dc1faea39d33625bb4c15bd71c -- StormX (STMX) (0xa62c...d71c)
    Token Details:
    - Id: 1-0xa62cc35625b0c8dc1faea39d33625bb4c15bd71c
    - Chain Id: 1
    - Contract Address: 0xa62cc35625b0c8dc1faea39d33625bb4c15bd71c
    - Name: StormX
    - Symbol: STMX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2297.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2297

---
+++ sdk/constants/token-map/1/definitions/1-0xa62cc35625b0c8dc1faea39d33625bb4c15bd71c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa62cc35625b0c8dc1faea39d33625bb4c15bd71c",
+  chain_id: 1,
+  contract_address: "0xa62cc35625b0c8dc1faea39d33625bb4c15bd71c",
+  name: "StormX",
+  symbol: "STMX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2297.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2297",
+  type: "token",
+});
