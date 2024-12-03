6f5563aa0a41791ecb34a8da9d866ed3de522e34
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:00:21 -0800

    feat(token-map): Update Token -- 1-0x111111517e4929d3dcbdfa7cce55d30d4b6bc4d6 -- ICHI (ICHI) (0x1111...c4d6)
    Token Details:
    - Id: 1-0x111111517e4929d3dcbdfa7cce55d30d4b6bc4d6
    - Chain Id: 1
    - Contract Address: 0x111111517e4929d3dcbdfa7cce55d30d4b6bc4d6
    - Name: ICHI
    - Symbol: ICHI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7726.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7726

---
+++ sdk/constants/token-map/1/definitions/1-0x111111517e4929d3dcbdfa7cce55d30d4b6bc4d6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x111111517e4929d3dcbdfa7cce55d30d4b6bc4d6",
+  chain_id: 1,
+  contract_address: "0x111111517e4929d3dcbdfa7cce55d30d4b6bc4d6",
+  name: "ICHI",
+  symbol: "ICHI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7726.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7726",
+  type: "token",
+});
