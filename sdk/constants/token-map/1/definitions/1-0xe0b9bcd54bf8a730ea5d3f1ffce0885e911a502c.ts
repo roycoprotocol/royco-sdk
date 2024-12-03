2458913ae5d73e747986f35fd3ffdf72cfbb2987
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:24 -0800

    feat(token-map): Update Token -- 1-0xe0b9bcd54bf8a730ea5d3f1ffce0885e911a502c -- ZUM TOKEN (ZUM) (0xe0b9...502c)
    Token Details:
    - Id: 1-0xe0b9bcd54bf8a730ea5d3f1ffce0885e911a502c
    - Chain Id: 1
    - Contract Address: 0xe0b9bcd54bf8a730ea5d3f1ffce0885e911a502c
    - Name: ZUM TOKEN
    - Symbol: ZUM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4826.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 4826

---
+++ sdk/constants/token-map/1/definitions/1-0xe0b9bcd54bf8a730ea5d3f1ffce0885e911a502c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe0b9bcd54bf8a730ea5d3f1ffce0885e911a502c",
+  chain_id: 1,
+  contract_address: "0xe0b9bcd54bf8a730ea5d3f1ffce0885e911a502c",
+  name: "ZUM TOKEN",
+  symbol: "ZUM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4826.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "4826",
+  type: "token",
+});
