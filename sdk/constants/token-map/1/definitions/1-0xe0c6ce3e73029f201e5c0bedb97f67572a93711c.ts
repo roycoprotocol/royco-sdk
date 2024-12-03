da8007f60f8de7c3dcceff22018b63185e0a9356
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:27 -0800

    feat(token-map): Update Token -- 1-0xe0c6ce3e73029f201e5c0bedb97f67572a93711c -- ETHplode (ETHPLO) (0xe0c6...711c)
    Token Details:
    - Id: 1-0xe0c6ce3e73029f201e5c0bedb97f67572a93711c
    - Chain Id: 1
    - Contract Address: 0xe0c6ce3e73029f201e5c0bedb97f67572a93711c
    - Name: ETHplode
    - Symbol: ETHPLO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4076.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 4076

---
+++ sdk/constants/token-map/1/definitions/1-0xe0c6ce3e73029f201e5c0bedb97f67572a93711c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe0c6ce3e73029f201e5c0bedb97f67572a93711c",
+  chain_id: 1,
+  contract_address: "0xe0c6ce3e73029f201e5c0bedb97f67572a93711c",
+  name: "ETHplode",
+  symbol: "ETHPLO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4076.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "4076",
+  type: "token",
+});
