42c7c46098d54321be65547162072f879a467471
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:24 -0800

    feat(token-map): Update Token -- 1-0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd -- DODO (DODO) (0x43df...4ddd)
    Token Details:
    - Id: 1-0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd
    - Chain Id: 1
    - Contract Address: 0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd
    - Name: DODO
    - Symbol: DODO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7224.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7224

---
+++ sdk/constants/token-map/1/definitions/1-0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd",
+  chain_id: 1,
+  contract_address: "0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd",
+  name: "DODO",
+  symbol: "DODO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7224.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7224",
+  type: "token",
+});
