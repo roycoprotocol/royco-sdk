2fd8f516a40acd91492869c60c787fc117e6b80a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:57 -0800

    feat(token-map): Update Token -- 42161-0x9fb9a33956351cf4fa040f65a13b835a3c8764e3 -- Multichain (MULTI) (0x9fb9...64e3)
    Token Details:
    - Id: 42161-0x9fb9a33956351cf4fa040f65a13b835a3c8764e3
    - Chain Id: 42161
    - Contract Address: 0x9fb9a33956351cf4fa040f65a13b835a3c8764e3
    - Name: Multichain
    - Symbol: MULTI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/17050.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 17050

---
+++ sdk/constants/token-map/42161/definitions/42161-0x9fb9a33956351cf4fa040f65a13b835a3c8764e3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x9fb9a33956351cf4fa040f65a13b835a3c8764e3",
+  chain_id: 42161,
+  contract_address: "0x9fb9a33956351cf4fa040f65a13b835a3c8764e3",
+  name: "Multichain",
+  symbol: "MULTI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/17050.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "17050",
+  type: "token",
+});
