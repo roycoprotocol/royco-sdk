9652533db9f18be48854e02e3d8466f787cfc129
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:02 -0800

    feat(token-map): Update Token -- 1-0x4297394c20800e8a38a619a243e9bbe7681ff24e -- Hot Cross (HOTCROSS) (0x4297...f24e)
    Token Details:
    - Id: 1-0x4297394c20800e8a38a619a243e9bbe7681ff24e
    - Chain Id: 1
    - Contract Address: 0x4297394c20800e8a38a619a243e9bbe7681ff24e
    - Name: Hot Cross
    - Symbol: HOTCROSS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9867.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9867

---
+++ sdk/constants/token-map/1/definitions/1-0x4297394c20800e8a38a619a243e9bbe7681ff24e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4297394c20800e8a38a619a243e9bbe7681ff24e",
+  chain_id: 1,
+  contract_address: "0x4297394c20800e8a38a619a243e9bbe7681ff24e",
+  name: "Hot Cross",
+  symbol: "HOTCROSS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9867.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9867",
+  type: "token",
+});
