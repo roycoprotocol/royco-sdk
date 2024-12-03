da53712fae52a58e552ca75eb250a15da76f0702
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:52 -0800

    feat(token-map): Update Token -- 1-0x853d955acef822db058eb8505911ed77f175b99e -- Frax (FRAX) (0x853d...b99e)
    Token Details:
    - Id: 1-0x853d955acef822db058eb8505911ed77f175b99e
    - Chain Id: 1
    - Contract Address: 0x853d955acef822db058eb8505911ed77f175b99e
    - Name: Frax
    - Symbol: FRAX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6952.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6952

---
+++ sdk/constants/token-map/1/definitions/1-0x853d955acef822db058eb8505911ed77f175b99e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x853d955acef822db058eb8505911ed77f175b99e",
+  chain_id: 1,
+  contract_address: "0x853d955acef822db058eb8505911ed77f175b99e",
+  name: "Frax",
+  symbol: "FRAX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6952.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6952",
+  type: "token",
+});
