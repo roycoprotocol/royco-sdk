72aa3a6c8de14aa252b16423b6e5627f8c4840fb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:50 -0800

    feat(token-map): Update Token -- 1-0xa8c8cfb141a3bb59fea1e2ea6b79b5ecbcd7b6ca -- Synternet (NOIA) (0xa8c8...b6ca)
    Token Details:
    - Id: 1-0xa8c8cfb141a3bb59fea1e2ea6b79b5ecbcd7b6ca
    - Chain Id: 1
    - Contract Address: 0xa8c8cfb141a3bb59fea1e2ea6b79b5ecbcd7b6ca
    - Name: Synternet
    - Symbol: NOIA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4191.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4191

---
+++ sdk/constants/token-map/1/definitions/1-0xa8c8cfb141a3bb59fea1e2ea6b79b5ecbcd7b6ca.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa8c8cfb141a3bb59fea1e2ea6b79b5ecbcd7b6ca",
+  chain_id: 1,
+  contract_address: "0xa8c8cfb141a3bb59fea1e2ea6b79b5ecbcd7b6ca",
+  name: "Synternet",
+  symbol: "NOIA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4191.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4191",
+  type: "token",
+});
