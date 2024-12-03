ea44412259201b2906954f1a37c00c47e76dc233
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:07 -0800

    feat(token-map): Update Token -- 1-0x4da08a1bff50be96bded5c7019227164b49c2bfc -- Mononoke Inu (Mononoke-Inu) (0x4da0...2bfc)
    Token Details:
    - Id: 1-0x4da08a1bff50be96bded5c7019227164b49c2bfc
    - Chain Id: 1
    - Contract Address: 0x4da08a1bff50be96bded5c7019227164b49c2bfc
    - Name: Mononoke Inu
    - Symbol: Mononoke-Inu
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12562.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 12562

---
+++ sdk/constants/token-map/1/definitions/1-0x4da08a1bff50be96bded5c7019227164b49c2bfc.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4da08a1bff50be96bded5c7019227164b49c2bfc",
+  chain_id: 1,
+  contract_address: "0x4da08a1bff50be96bded5c7019227164b49c2bfc",
+  name: "Mononoke Inu",
+  symbol: "Mononoke-Inu",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12562.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "12562",
+  type: "token",
+});
