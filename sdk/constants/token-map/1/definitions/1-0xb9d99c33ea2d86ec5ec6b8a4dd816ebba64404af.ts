a2630ddb7ddfbfcdf77a7489071886f73dea8c99
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:22 -0800

    feat(token-map): Update Token -- 1-0xb9d99c33ea2d86ec5ec6b8a4dd816ebba64404af -- K21 (K21) (0xb9d9...04af)
    Token Details:
    - Id: 1-0xb9d99c33ea2d86ec5ec6b8a4dd816ebba64404af
    - Chain Id: 1
    - Contract Address: 0xb9d99c33ea2d86ec5ec6b8a4dd816ebba64404af
    - Name: K21
    - Symbol: K21
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9205.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9205

---
+++ sdk/constants/token-map/1/definitions/1-0xb9d99c33ea2d86ec5ec6b8a4dd816ebba64404af.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb9d99c33ea2d86ec5ec6b8a4dd816ebba64404af",
+  chain_id: 1,
+  contract_address: "0xb9d99c33ea2d86ec5ec6b8a4dd816ebba64404af",
+  name: "K21",
+  symbol: "K21",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9205.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9205",
+  type: "token",
+});
