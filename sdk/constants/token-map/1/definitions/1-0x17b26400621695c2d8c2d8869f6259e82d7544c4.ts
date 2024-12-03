e7c384ae41ac845ab8b3de689b12f7cb66d55b5e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:30 -0800

    feat(token-map): Update Token -- 1-0x17b26400621695c2d8c2d8869f6259e82d7544c4 -- CustomContractNetwork (CCN) (0x17b2...44c4)
    Token Details:
    - Id: 1-0x17b26400621695c2d8c2d8869f6259e82d7544c4
    - Chain Id: 1
    - Contract Address: 0x17b26400621695c2d8c2d8869f6259e82d7544c4
    - Name: CustomContractNetwork
    - Symbol: CCN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3770.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3770

---
+++ sdk/constants/token-map/1/definitions/1-0x17b26400621695c2d8c2d8869f6259e82d7544c4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x17b26400621695c2d8c2d8869f6259e82d7544c4",
+  chain_id: 1,
+  contract_address: "0x17b26400621695c2d8c2d8869f6259e82d7544c4",
+  name: "CustomContractNetwork",
+  symbol: "CCN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3770.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3770",
+  type: "token",
+});
