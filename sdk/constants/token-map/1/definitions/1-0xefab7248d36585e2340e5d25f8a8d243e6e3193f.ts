42461e1bd340bbbb1ac4a6e5cd444182742b9864
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:39 -0800

    feat(token-map): Update Token -- 1-0xefab7248d36585e2340e5d25f8a8d243e6e3193f -- Dacxi (DACXI) (0xefab...193f)
    Token Details:
    - Id: 1-0xefab7248d36585e2340e5d25f8a8d243e6e3193f
    - Chain Id: 1
    - Contract Address: 0xefab7248d36585e2340e5d25f8a8d243e6e3193f
    - Name: Dacxi
    - Symbol: DACXI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10372.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10372

---
+++ sdk/constants/token-map/1/definitions/1-0xefab7248d36585e2340e5d25f8a8d243e6e3193f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xefab7248d36585e2340e5d25f8a8d243e6e3193f",
+  chain_id: 1,
+  contract_address: "0xefab7248d36585e2340e5d25f8a8d243e6e3193f",
+  name: "Dacxi",
+  symbol: "DACXI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10372.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10372",
+  type: "token",
+});
