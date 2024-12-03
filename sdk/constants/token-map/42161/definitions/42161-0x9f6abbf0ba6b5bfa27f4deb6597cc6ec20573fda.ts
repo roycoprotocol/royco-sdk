f427ff51a4751e59b77bdc3e760c9a5f4647ee26
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:55 -0800

    feat(token-map): Update Token -- 42161-0x9f6abbf0ba6b5bfa27f4deb6597cc6ec20573fda -- Ferrum Network (FRM) (0x9f6a...3fda)
    Token Details:
    - Id: 42161-0x9f6abbf0ba6b5bfa27f4deb6597cc6ec20573fda
    - Chain Id: 42161
    - Contract Address: 0x9f6abbf0ba6b5bfa27f4deb6597cc6ec20573fda
    - Name: Ferrum Network
    - Symbol: FRM
    - Image: https://coin-images.coingecko.com/coins/images/8251/large/FRM.png?1696508455
    - Decimals: 18
    - Source: coingecko
    - Search Id: ferrum-network

---
+++ sdk/constants/token-map/42161/definitions/42161-0x9f6abbf0ba6b5bfa27f4deb6597cc6ec20573fda.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x9f6abbf0ba6b5bfa27f4deb6597cc6ec20573fda",
+  chain_id: 42161,
+  contract_address: "0x9f6abbf0ba6b5bfa27f4deb6597cc6ec20573fda",
+  name: "Ferrum Network",
+  symbol: "FRM",
+  image: "https://coin-images.coingecko.com/coins/images/8251/large/FRM.png?1696508455",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "ferrum-network",
+  type: "token",
+});
