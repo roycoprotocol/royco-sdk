7083a8c8f29326f1f07d9110c189626cc893b44f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:56 -0800

    feat(token-map): Update Token -- 1-0x60715e436c37444e29772c0d26a98ae1e8e1a989 -- Voltz (VOLTZ) (0x6071...a989)
    Token Details:
    - Id: 1-0x60715e436c37444e29772c0d26a98ae1e8e1a989
    - Chain Id: 1
    - Contract Address: 0x60715e436c37444e29772c0d26a98ae1e8e1a989
    - Name: Voltz
    - Symbol: VOLTZ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4508.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4508

---
+++ sdk/constants/token-map/1/definitions/1-0x60715e436c37444e29772c0d26a98ae1e8e1a989.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x60715e436c37444e29772c0d26a98ae1e8e1a989",
+  chain_id: 1,
+  contract_address: "0x60715e436c37444e29772c0d26a98ae1e8e1a989",
+  name: "Voltz",
+  symbol: "VOLTZ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4508.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4508",
+  type: "token",
+});
