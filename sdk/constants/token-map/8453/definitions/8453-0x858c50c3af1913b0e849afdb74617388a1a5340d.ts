f02bbe045d373cbfe5ca009870bd508d8a1d2a20
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:58 -0800

    feat(token-map): Update Token -- 8453-0x858c50c3af1913b0e849afdb74617388a1a5340d -- SubQuery Network (SQT) (0x858c...340d)
    Token Details:
    - Id: 8453-0x858c50c3af1913b0e849afdb74617388a1a5340d
    - Chain Id: 8453
    - Contract Address: 0x858c50c3af1913b0e849afdb74617388a1a5340d
    - Name: SubQuery Network
    - Symbol: SQT
    - Image: https://coin-images.coingecko.com/coins/images/23359/large/LinkedIn_Avatar_Op1.png?1724379155
    - Decimals: 18
    - Source: coingecko
    - Search Id: subquery-network

---
+++ sdk/constants/token-map/8453/definitions/8453-0x858c50c3af1913b0e849afdb74617388a1a5340d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0x858c50c3af1913b0e849afdb74617388a1a5340d",
+  chain_id: 8453,
+  contract_address: "0x858c50c3af1913b0e849afdb74617388a1a5340d",
+  name: "SubQuery Network",
+  symbol: "SQT",
+  image: "https://coin-images.coingecko.com/coins/images/23359/large/LinkedIn_Avatar_Op1.png?1724379155",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "subquery-network",
+  type: "token",
+});
