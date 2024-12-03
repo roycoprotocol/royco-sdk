10a7482e70fad235ebb2d8016aa3b9603198b53f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:24 -0800

    feat(token-map): Update Token -- 1-0xb2617246d0c6c0087f18703d576831899ca94f01 -- ZIGChain (ZIG) (0xb261...4f01)
    Token Details:
    - Id: 1-0xb2617246d0c6c0087f18703d576831899ca94f01
    - Chain Id: 1
    - Contract Address: 0xb2617246d0c6c0087f18703d576831899ca94f01
    - Name: ZIGChain
    - Symbol: ZIG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9260.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9260

---
+++ sdk/constants/token-map/1/definitions/1-0xb2617246d0c6c0087f18703d576831899ca94f01.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb2617246d0c6c0087f18703d576831899ca94f01",
+  chain_id: 1,
+  contract_address: "0xb2617246d0c6c0087f18703d576831899ca94f01",
+  name: "ZIGChain",
+  symbol: "ZIG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9260.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9260",
+  type: "token",
+});
