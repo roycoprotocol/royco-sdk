e07aa091f3a3331c411804ec4d1c882c68a9edbf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:26 -0800

    feat(token-map): Update Token -- 1-0x44197a4c44d6a059297caf6be4f7e172bd56caaf -- ELTCOIN (ELTCOIN) (0x4419...caaf)
    Token Details:
    - Id: 1-0x44197a4c44d6a059297caf6be4f7e172bd56caaf
    - Chain Id: 1
    - Contract Address: 0x44197a4c44d6a059297caf6be4f7e172bd56caaf
    - Name: ELTCOIN
    - Symbol: ELTCOIN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2147.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2147

---
+++ sdk/constants/token-map/1/definitions/1-0x44197a4c44d6a059297caf6be4f7e172bd56caaf.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x44197a4c44d6a059297caf6be4f7e172bd56caaf",
+  chain_id: 1,
+  contract_address: "0x44197a4c44d6a059297caf6be4f7e172bd56caaf",
+  name: "ELTCOIN",
+  symbol: "ELTCOIN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2147.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2147",
+  type: "token",
+});
