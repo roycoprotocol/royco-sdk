dc831f840de6e3d95d1f1e7aee3dac6a60e93f05
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:18 -0800

    feat(token-map): Update Token -- 1-0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0 -- dForce (DF) (0x431a...dbe0)
    Token Details:
    - Id: 1-0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0
    - Chain Id: 1
    - Contract Address: 0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0
    - Name: dForce
    - Symbol: DF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4758.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4758

---
+++ sdk/constants/token-map/1/definitions/1-0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0",
+  chain_id: 1,
+  contract_address: "0x431ad2ff6a9c365805ebad47ee021148d6f7dbe0",
+  name: "dForce",
+  symbol: "DF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4758.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4758",
+  type: "token",
+});
