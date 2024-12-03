fa2437ae37828991d193c8b7bf29b9079423f55b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:23 -0800

    feat(token-map): Update Token -- 1-0xcc4304a31d09258b0029ea7fe63d032f52e44efe -- TrustSwap (SWAP) (0xcc43...4efe)
    Token Details:
    - Id: 1-0xcc4304a31d09258b0029ea7fe63d032f52e44efe
    - Chain Id: 1
    - Contract Address: 0xcc4304a31d09258b0029ea7fe63d032f52e44efe
    - Name: TrustSwap
    - Symbol: SWAP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5829.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5829

---
+++ sdk/constants/token-map/1/definitions/1-0xcc4304a31d09258b0029ea7fe63d032f52e44efe.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xcc4304a31d09258b0029ea7fe63d032f52e44efe",
+  chain_id: 1,
+  contract_address: "0xcc4304a31d09258b0029ea7fe63d032f52e44efe",
+  name: "TrustSwap",
+  symbol: "SWAP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5829.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5829",
+  type: "token",
+});
