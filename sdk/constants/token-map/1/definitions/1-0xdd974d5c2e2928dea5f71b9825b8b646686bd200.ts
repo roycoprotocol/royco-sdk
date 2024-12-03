222d086f9bf9e5ec6ff5f0d5779976a2f9f58ac9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:43 -0800

    feat(token-map): Update Token -- 1-0xdd974d5c2e2928dea5f71b9825b8b646686bd200 -- Kyber Network Crystal Legacy (KNCL) (0xdd97...d200)
    Token Details:
    - Id: 1-0xdd974d5c2e2928dea5f71b9825b8b646686bd200
    - Chain Id: 1
    - Contract Address: 0xdd974d5c2e2928dea5f71b9825b8b646686bd200
    - Name: Kyber Network Crystal Legacy
    - Symbol: KNCL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1982.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1982

---
+++ sdk/constants/token-map/1/definitions/1-0xdd974d5c2e2928dea5f71b9825b8b646686bd200.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdd974d5c2e2928dea5f71b9825b8b646686bd200",
+  chain_id: 1,
+  contract_address: "0xdd974d5c2e2928dea5f71b9825b8b646686bd200",
+  name: "Kyber Network Crystal Legacy",
+  symbol: "KNCL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1982.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1982",
+  type: "token",
+});
