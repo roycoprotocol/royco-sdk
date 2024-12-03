8859bb322b255a2dd30ae1bd05c10c68f92f4fc5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:39 -0800

    feat(token-map): Update Token -- 1-0xba386a4ca26b85fd057ab1ef86e3dc7bdeb5ce70 -- Jesus Coin (JESUS) (0xba38...ce70)
    Token Details:
    - Id: 1-0xba386a4ca26b85fd057ab1ef86e3dc7bdeb5ce70
    - Chain Id: 1
    - Contract Address: 0xba386a4ca26b85fd057ab1ef86e3dc7bdeb5ce70
    - Name: Jesus Coin
    - Symbol: JESUS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/24747.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 24747

---
+++ sdk/constants/token-map/1/definitions/1-0xba386a4ca26b85fd057ab1ef86e3dc7bdeb5ce70.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xba386a4ca26b85fd057ab1ef86e3dc7bdeb5ce70",
+  chain_id: 1,
+  contract_address: "0xba386a4ca26b85fd057ab1ef86e3dc7bdeb5ce70",
+  name: "Jesus Coin",
+  symbol: "JESUS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/24747.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "24747",
+  type: "token",
+});
