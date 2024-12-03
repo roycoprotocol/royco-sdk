296b1cfff16873e9d09d8119c0dab2655c19a293
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:51 -0800

    feat(token-map): Update Token -- 1-0xe453c3409f8ad2b1fe1ed08e189634d359705a5b -- DGI Game (DGI) (0xe453...5a5b)
    Token Details:
    - Id: 1-0xe453c3409f8ad2b1fe1ed08e189634d359705a5b
    - Chain Id: 1
    - Contract Address: 0xe453c3409f8ad2b1fe1ed08e189634d359705a5b
    - Name: DGI Game
    - Symbol: DGI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29112.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29112

---
+++ sdk/constants/token-map/1/definitions/1-0xe453c3409f8ad2b1fe1ed08e189634d359705a5b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe453c3409f8ad2b1fe1ed08e189634d359705a5b",
+  chain_id: 1,
+  contract_address: "0xe453c3409f8ad2b1fe1ed08e189634d359705a5b",
+  name: "DGI Game",
+  symbol: "DGI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29112.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29112",
+  type: "token",
+});
