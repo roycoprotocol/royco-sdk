c6e653b396dfaed0dc97cc561d5853f658842e04
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:46 -0800

    feat(token-map): Update Token -- 1-0x0aacfbec6a24756c20d41914f2caba817c0d8521 -- YAM V3 (YAM) (0x0aac...8521)
    Token Details:
    - Id: 1-0x0aacfbec6a24756c20d41914f2caba817c0d8521
    - Chain Id: 1
    - Contract Address: 0x0aacfbec6a24756c20d41914f2caba817c0d8521
    - Name: YAM V3
    - Symbol: YAM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7131.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7131

---
+++ sdk/constants/token-map/1/definitions/1-0x0aacfbec6a24756c20d41914f2caba817c0d8521.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0aacfbec6a24756c20d41914f2caba817c0d8521",
+  chain_id: 1,
+  contract_address: "0x0aacfbec6a24756c20d41914f2caba817c0d8521",
+  name: "YAM V3",
+  symbol: "YAM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7131.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7131",
+  type: "token",
+});
