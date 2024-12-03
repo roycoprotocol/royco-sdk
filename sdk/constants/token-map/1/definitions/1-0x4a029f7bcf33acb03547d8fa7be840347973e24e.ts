c4a6d71122c0bb3d15e1e75c335ffbc1af07b4c9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:28 -0800

    feat(token-map): Update Token -- 1-0x4a029f7bcf33acb03547d8fa7be840347973e24e -- Mazze (MAZZE) (0x4a02...e24e)
    Token Details:
    - Id: 1-0x4a029f7bcf33acb03547d8fa7be840347973e24e
    - Chain Id: 1
    - Contract Address: 0x4a029f7bcf33acb03547d8fa7be840347973e24e
    - Name: Mazze
    - Symbol: MAZZE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29982.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29982

---
+++ sdk/constants/token-map/1/definitions/1-0x4a029f7bcf33acb03547d8fa7be840347973e24e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4a029f7bcf33acb03547d8fa7be840347973e24e",
+  chain_id: 1,
+  contract_address: "0x4a029f7bcf33acb03547d8fa7be840347973e24e",
+  name: "Mazze",
+  symbol: "MAZZE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29982.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29982",
+  type: "token",
+});
