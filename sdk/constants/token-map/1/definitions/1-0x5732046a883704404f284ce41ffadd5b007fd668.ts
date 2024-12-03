93a8ad9c2565a17a0d0e17318b177de64fb12858
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:26 -0800

    feat(token-map): Update Token -- 1-0x5732046a883704404f284ce41ffadd5b007fd668 -- Bluzelle (BLZ) (0x5732...d668)
    Token Details:
    - Id: 1-0x5732046a883704404f284ce41ffadd5b007fd668
    - Chain Id: 1
    - Contract Address: 0x5732046a883704404f284ce41ffadd5b007fd668
    - Name: Bluzelle
    - Symbol: BLZ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2505.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2505

---
+++ sdk/constants/token-map/1/definitions/1-0x5732046a883704404f284ce41ffadd5b007fd668.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5732046a883704404f284ce41ffadd5b007fd668",
+  chain_id: 1,
+  contract_address: "0x5732046a883704404f284ce41ffadd5b007fd668",
+  name: "Bluzelle",
+  symbol: "BLZ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2505.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2505",
+  type: "token",
+});
