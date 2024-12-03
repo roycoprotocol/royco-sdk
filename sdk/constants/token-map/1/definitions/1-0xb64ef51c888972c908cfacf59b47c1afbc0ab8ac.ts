58d1d042976fd724b947905bfa735fa9ff677a7e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:43 -0800

    feat(token-map): Update Token -- 1-0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac -- Storj (STORJ) (0xb64e...b8ac)
    Token Details:
    - Id: 1-0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac
    - Chain Id: 1
    - Contract Address: 0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac
    - Name: Storj
    - Symbol: STORJ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1772.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 1772

---
+++ sdk/constants/token-map/1/definitions/1-0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac",
+  chain_id: 1,
+  contract_address: "0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac",
+  name: "Storj",
+  symbol: "STORJ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1772.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "1772",
+  type: "token",
+});
