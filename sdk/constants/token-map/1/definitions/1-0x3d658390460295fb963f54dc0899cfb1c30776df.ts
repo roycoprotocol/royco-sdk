3239123da26e3f250e367d9c14de37190d74c009
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:12 -0800

    feat(token-map): Update Token -- 1-0x3d658390460295fb963f54dc0899cfb1c30776df -- Circuits of Value (COVAL) (0x3d65...76df)
    Token Details:
    - Id: 1-0x3d658390460295fb963f54dc0899cfb1c30776df
    - Chain Id: 1
    - Contract Address: 0x3d658390460295fb963f54dc0899cfb1c30776df
    - Name: Circuits of Value
    - Symbol: COVAL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/788.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 788

---
+++ sdk/constants/token-map/1/definitions/1-0x3d658390460295fb963f54dc0899cfb1c30776df.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3d658390460295fb963f54dc0899cfb1c30776df",
+  chain_id: 1,
+  contract_address: "0x3d658390460295fb963f54dc0899cfb1c30776df",
+  name: "Circuits of Value",
+  symbol: "COVAL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/788.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "788",
+  type: "token",
+});
