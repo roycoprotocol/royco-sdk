03b997153c2e34fc1b9c27621f5f3499ec27fbc4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:47 -0800

    feat(token-map): Update Token -- 1-0x57e114b691db790c35207b2e685d4a43181e6061 -- Ethena (ENA) (0x57e1...6061)
    Token Details:
    - Id: 1-0x57e114b691db790c35207b2e685d4a43181e6061
    - Chain Id: 1
    - Contract Address: 0x57e114b691db790c35207b2e685d4a43181e6061
    - Name: Ethena
    - Symbol: ENA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/30171.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 30171

---
+++ sdk/constants/token-map/1/definitions/1-0x57e114b691db790c35207b2e685d4a43181e6061.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x57e114b691db790c35207b2e685d4a43181e6061",
+  chain_id: 1,
+  contract_address: "0x57e114b691db790c35207b2e685d4a43181e6061",
+  name: "Ethena",
+  symbol: "ENA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/30171.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "30171",
+  type: "token",
+});
