643da7af056ecfed41f4543cd2d26fb506a93752
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:43 -0800

    feat(token-map): Update Token -- 1-0x2a3bff78b79a009976eea096a51a948a3dc00e34 -- Wilder World (WILD) (0x2a3b...0e34)
    Token Details:
    - Id: 1-0x2a3bff78b79a009976eea096a51a948a3dc00e34
    - Chain Id: 1
    - Contract Address: 0x2a3bff78b79a009976eea096a51a948a3dc00e34
    - Name: Wilder World
    - Symbol: WILD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9674.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9674

---
+++ sdk/constants/token-map/1/definitions/1-0x2a3bff78b79a009976eea096a51a948a3dc00e34.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2a3bff78b79a009976eea096a51a948a3dc00e34",
+  chain_id: 1,
+  contract_address: "0x2a3bff78b79a009976eea096a51a948a3dc00e34",
+  name: "Wilder World",
+  symbol: "WILD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9674.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9674",
+  type: "token",
+});
