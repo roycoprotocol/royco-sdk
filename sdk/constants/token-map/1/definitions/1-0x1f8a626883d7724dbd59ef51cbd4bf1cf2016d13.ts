33e9328b8566fe1051b2eb8b1772a58453d2620e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:44 -0800

    feat(token-map): Update Token -- 1-0x1f8a626883d7724dbd59ef51cbd4bf1cf2016d13 -- Jigstack (STAK) (0x1f8a...6d13)
    Token Details:
    - Id: 1-0x1f8a626883d7724dbd59ef51cbd4bf1cf2016d13
    - Chain Id: 1
    - Contract Address: 0x1f8a626883d7724dbd59ef51cbd4bf1cf2016d13
    - Name: Jigstack
    - Symbol: STAK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9441.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9441

---
+++ sdk/constants/token-map/1/definitions/1-0x1f8a626883d7724dbd59ef51cbd4bf1cf2016d13.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1f8a626883d7724dbd59ef51cbd4bf1cf2016d13",
+  chain_id: 1,
+  contract_address: "0x1f8a626883d7724dbd59ef51cbd4bf1cf2016d13",
+  name: "Jigstack",
+  symbol: "STAK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9441.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9441",
+  type: "token",
+});
