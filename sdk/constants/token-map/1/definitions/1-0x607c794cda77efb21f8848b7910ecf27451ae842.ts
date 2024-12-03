559b102e7a44865b8f512111d65c9654d907a767
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:58 -0800

    feat(token-map): Update Token -- 1-0x607c794cda77efb21f8848b7910ecf27451ae842 -- DeFiPie (PIE) (0x607c...e842)
    Token Details:
    - Id: 1-0x607c794cda77efb21f8848b7910ecf27451ae842
    - Chain Id: 1
    - Contract Address: 0x607c794cda77efb21f8848b7910ecf27451ae842
    - Name: DeFiPie
    - Symbol: PIE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6243.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6243

---
+++ sdk/constants/token-map/1/definitions/1-0x607c794cda77efb21f8848b7910ecf27451ae842.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x607c794cda77efb21f8848b7910ecf27451ae842",
+  chain_id: 1,
+  contract_address: "0x607c794cda77efb21f8848b7910ecf27451ae842",
+  name: "DeFiPie",
+  symbol: "PIE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6243.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6243",
+  type: "token",
+});
