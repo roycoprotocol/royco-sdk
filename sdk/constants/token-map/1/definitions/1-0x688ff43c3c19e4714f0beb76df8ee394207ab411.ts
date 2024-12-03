e292fee600224fac197ab15e29999bc1e8c5131b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:46 -0800

    feat(token-map): Update Token -- 1-0x688ff43c3c19e4714f0beb76df8ee394207ab411 -- CitiOs (R2R) (0x688f...b411)
    Token Details:
    - Id: 1-0x688ff43c3c19e4714f0beb76df8ee394207ab411
    - Chain Id: 1
    - Contract Address: 0x688ff43c3c19e4714f0beb76df8ee394207ab411
    - Name: CitiOs
    - Symbol: R2R
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5204.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5204

---
+++ sdk/constants/token-map/1/definitions/1-0x688ff43c3c19e4714f0beb76df8ee394207ab411.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x688ff43c3c19e4714f0beb76df8ee394207ab411",
+  chain_id: 1,
+  contract_address: "0x688ff43c3c19e4714f0beb76df8ee394207ab411",
+  name: "CitiOs",
+  symbol: "R2R",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5204.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5204",
+  type: "token",
+});
