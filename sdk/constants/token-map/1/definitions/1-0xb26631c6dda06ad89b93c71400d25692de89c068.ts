2ed3bca99c6e2d653498e551fe0cb69307af0d3b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:26 -0800

    feat(token-map): Update Token -- 1-0xb26631c6dda06ad89b93c71400d25692de89c068 -- Minds (MINDS) (0xb266...c068)
    Token Details:
    - Id: 1-0xb26631c6dda06ad89b93c71400d25692de89c068
    - Chain Id: 1
    - Contract Address: 0xb26631c6dda06ad89b93c71400d25692de89c068
    - Name: Minds
    - Symbol: MINDS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8675.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8675

---
+++ sdk/constants/token-map/1/definitions/1-0xb26631c6dda06ad89b93c71400d25692de89c068.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb26631c6dda06ad89b93c71400d25692de89c068",
+  chain_id: 1,
+  contract_address: "0xb26631c6dda06ad89b93c71400d25692de89c068",
+  name: "Minds",
+  symbol: "MINDS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8675.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8675",
+  type: "token",
+});
