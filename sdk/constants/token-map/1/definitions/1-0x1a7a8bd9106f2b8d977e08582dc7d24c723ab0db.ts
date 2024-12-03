f7916866efc30431a6f0020cfebb780f0af416b6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:57 -0800

    feat(token-map): Update Token -- 1-0x1a7a8bd9106f2b8d977e08582dc7d24c723ab0db -- AppCoins (APPC) (0x1a7a...b0db)
    Token Details:
    - Id: 1-0x1a7a8bd9106f2b8d977e08582dc7d24c723ab0db
    - Chain Id: 1
    - Contract Address: 0x1a7a8bd9106f2b8d977e08582dc7d24c723ab0db
    - Name: AppCoins
    - Symbol: APPC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2344.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2344

---
+++ sdk/constants/token-map/1/definitions/1-0x1a7a8bd9106f2b8d977e08582dc7d24c723ab0db.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1a7a8bd9106f2b8d977e08582dc7d24c723ab0db",
+  chain_id: 1,
+  contract_address: "0x1a7a8bd9106f2b8d977e08582dc7d24c723ab0db",
+  name: "AppCoins",
+  symbol: "APPC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2344.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2344",
+  type: "token",
+});
