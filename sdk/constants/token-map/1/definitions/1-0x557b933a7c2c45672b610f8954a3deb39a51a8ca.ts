e90afc709ce5d2d1f01ea832254e3d22c5be446f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:11 -0800

    feat(token-map): Update Token -- 1-0x557b933a7c2c45672b610f8954a3deb39a51a8ca -- REVV (REVV) (0x557b...a8ca)
    Token Details:
    - Id: 1-0x557b933a7c2c45672b610f8954a3deb39a51a8ca
    - Chain Id: 1
    - Contract Address: 0x557b933a7c2c45672b610f8954a3deb39a51a8ca
    - Name: REVV
    - Symbol: REVV
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6993.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6993

---
+++ sdk/constants/token-map/1/definitions/1-0x557b933a7c2c45672b610f8954a3deb39a51a8ca.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x557b933a7c2c45672b610f8954a3deb39a51a8ca",
+  chain_id: 1,
+  contract_address: "0x557b933a7c2c45672b610f8954a3deb39a51a8ca",
+  name: "REVV",
+  symbol: "REVV",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6993.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6993",
+  type: "token",
+});
