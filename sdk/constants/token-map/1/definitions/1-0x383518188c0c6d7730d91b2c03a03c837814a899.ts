ed1a94721dd9e0a4f8b0f0350d6b2819d2ffca59
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:09 -0800

    feat(token-map): Update Token -- 1-0x383518188c0c6d7730d91b2c03a03c837814a899 -- Olympus v1 (OHM) (0x3835...a899)
    Token Details:
    - Id: 1-0x383518188c0c6d7730d91b2c03a03c837814a899
    - Chain Id: 1
    - Contract Address: 0x383518188c0c6d7730d91b2c03a03c837814a899
    - Name: Olympus v1
    - Symbol: OHM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/16209.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 16209

---
+++ sdk/constants/token-map/1/definitions/1-0x383518188c0c6d7730d91b2c03a03c837814a899.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x383518188c0c6d7730d91b2c03a03c837814a899",
+  chain_id: 1,
+  contract_address: "0x383518188c0c6d7730d91b2c03a03c837814a899",
+  name: "Olympus v1",
+  symbol: "OHM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/16209.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "16209",
+  type: "token",
+});
