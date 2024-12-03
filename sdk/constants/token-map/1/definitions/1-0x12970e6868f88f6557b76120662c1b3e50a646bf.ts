c2cb8bb0a52550658aa4b3937d6c205293554c5e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:00:34 -0800

    feat(token-map): Update Token -- 1-0x12970e6868f88f6557b76120662c1b3e50a646bf -- Milady Meme Coin (LADYS) (0x1297...46bf)
    Token Details:
    - Id: 1-0x12970e6868f88f6557b76120662c1b3e50a646bf
    - Chain Id: 1
    - Contract Address: 0x12970e6868f88f6557b76120662c1b3e50a646bf
    - Name: Milady Meme Coin
    - Symbol: LADYS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/25023.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 25023

---
+++ sdk/constants/token-map/1/definitions/1-0x12970e6868f88f6557b76120662c1b3e50a646bf.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x12970e6868f88f6557b76120662c1b3e50a646bf",
+  chain_id: 1,
+  contract_address: "0x12970e6868f88f6557b76120662c1b3e50a646bf",
+  name: "Milady Meme Coin",
+  symbol: "LADYS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/25023.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "25023",
+  type: "token",
+});
