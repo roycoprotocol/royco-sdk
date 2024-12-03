d718fb7a0c2ce119b36ce91d0d1f082a8b3ebe8d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:55 -0800

    feat(token-map): Update Token -- 1-0x5283d291dbcf85356a21ba090e6db59121208b44 -- Blur (BLUR) (0x5283...8b44)
    Token Details:
    - Id: 1-0x5283d291dbcf85356a21ba090e6db59121208b44
    - Chain Id: 1
    - Contract Address: 0x5283d291dbcf85356a21ba090e6db59121208b44
    - Name: Blur
    - Symbol: BLUR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/23121.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 23121

---
+++ sdk/constants/token-map/1/definitions/1-0x5283d291dbcf85356a21ba090e6db59121208b44.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5283d291dbcf85356a21ba090e6db59121208b44",
+  chain_id: 1,
+  contract_address: "0x5283d291dbcf85356a21ba090e6db59121208b44",
+  name: "Blur",
+  symbol: "BLUR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/23121.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "23121",
+  type: "token",
+});
