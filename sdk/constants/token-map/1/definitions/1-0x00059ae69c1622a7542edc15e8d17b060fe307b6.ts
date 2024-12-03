ce3ae70913f64954f2024fe2ddb860365444d820
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:55:43 -0800

    feat(token-map): Update Token -- 1-0x00059ae69c1622a7542edc15e8d17b060fe307b6 -- AmonD (AMON) (0x0005...07b6)
    Token Details:
    - Id: 1-0x00059ae69c1622a7542edc15e8d17b060fe307b6
    - Chain Id: 1
    - Contract Address: 0x00059ae69c1622a7542edc15e8d17b060fe307b6
    - Name: AmonD
    - Symbol: AMON
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4712.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4712

---
+++ sdk/constants/token-map/1/definitions/1-0x00059ae69c1622a7542edc15e8d17b060fe307b6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x00059ae69c1622a7542edc15e8d17b060fe307b6",
+  chain_id: 1,
+  contract_address: "0x00059ae69c1622a7542edc15e8d17b060fe307b6",
+  name: "AmonD",
+  symbol: "AMON",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4712.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4712",
+  type: "token",
+});
