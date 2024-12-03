96483a62f1068056de6635b0766cb258151bd9c3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:57 -0800

    feat(token-map): Update Token -- 1-0x32353a6c91143bfd6c7d363b546e62a9a2489a20 -- Adventure Gold (AGLD) (0x3235...9a20)
    Token Details:
    - Id: 1-0x32353a6c91143bfd6c7d363b546e62a9a2489a20
    - Chain Id: 1
    - Contract Address: 0x32353a6c91143bfd6c7d363b546e62a9a2489a20
    - Name: Adventure Gold
    - Symbol: AGLD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11568.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11568

---
+++ sdk/constants/token-map/1/definitions/1-0x32353a6c91143bfd6c7d363b546e62a9a2489a20.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x32353a6c91143bfd6c7d363b546e62a9a2489a20",
+  chain_id: 1,
+  contract_address: "0x32353a6c91143bfd6c7d363b546e62a9a2489a20",
+  name: "Adventure Gold",
+  symbol: "AGLD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11568.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11568",
+  type: "token",
+});
