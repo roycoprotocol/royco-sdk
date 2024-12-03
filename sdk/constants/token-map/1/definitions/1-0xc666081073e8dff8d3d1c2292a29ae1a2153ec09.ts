ee186efcd722d1813b3eafea90cc21f56afe362e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:31:33 -0800

    feat(token-map): Update Token -- 1-0xc666081073e8dff8d3d1c2292a29ae1a2153ec09 -- Digitex (DGTX) (0xc666...ec09)
    Token Details:
    - Id: 1-0xc666081073e8dff8d3d1c2292a29ae1a2153ec09
    - Chain Id: 1
    - Contract Address: 0xc666081073e8dff8d3d1c2292a29ae1a2153ec09
    - Name: Digitex
    - Symbol: DGTX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2772.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2772

---
+++ sdk/constants/token-map/1/definitions/1-0xc666081073e8dff8d3d1c2292a29ae1a2153ec09.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc666081073e8dff8d3d1c2292a29ae1a2153ec09",
+  chain_id: 1,
+  contract_address: "0xc666081073e8dff8d3d1c2292a29ae1a2153ec09",
+  name: "Digitex",
+  symbol: "DGTX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2772.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2772",
+  type: "token",
+});
