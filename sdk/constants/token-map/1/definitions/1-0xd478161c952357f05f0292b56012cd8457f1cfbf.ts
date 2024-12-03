b62bdc33f5d9b437cf48fb55f96713fc16d2ec19
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:32 -0800

    feat(token-map): Update Token -- 1-0xd478161c952357f05f0292b56012cd8457f1cfbf -- Polkamarkets (POLK) (0xd478...cfbf)
    Token Details:
    - Id: 1-0xd478161c952357f05f0292b56012cd8457f1cfbf
    - Chain Id: 1
    - Contract Address: 0xd478161c952357f05f0292b56012cd8457f1cfbf
    - Name: Polkamarkets
    - Symbol: POLK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8579.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8579

---
+++ sdk/constants/token-map/1/definitions/1-0xd478161c952357f05f0292b56012cd8457f1cfbf.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd478161c952357f05f0292b56012cd8457f1cfbf",
+  chain_id: 1,
+  contract_address: "0xd478161c952357f05f0292b56012cd8457f1cfbf",
+  name: "Polkamarkets",
+  symbol: "POLK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8579.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8579",
+  type: "token",
+});
