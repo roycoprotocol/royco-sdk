f9d01928be72bf4ce7a4c5bdd431bf0f0fd905b3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:25 -0800

    feat(token-map): Update Token -- 1-0x6243d8cea23066d098a15582d81a598b4e8391f4 -- Reflexer Ungovernance Token (FLX) (0x6243...91f4)
    Token Details:
    - Id: 1-0x6243d8cea23066d098a15582d81a598b4e8391f4
    - Chain Id: 1
    - Contract Address: 0x6243d8cea23066d098a15582d81a598b4e8391f4
    - Name: Reflexer Ungovernance Token
    - Symbol: FLX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9493.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9493

---
+++ sdk/constants/token-map/1/definitions/1-0x6243d8cea23066d098a15582d81a598b4e8391f4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6243d8cea23066d098a15582d81a598b4e8391f4",
+  chain_id: 1,
+  contract_address: "0x6243d8cea23066d098a15582d81a598b4e8391f4",
+  name: "Reflexer Ungovernance Token",
+  symbol: "FLX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9493.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9493",
+  type: "token",
+});
