d9dd2e128701ee97ddf85314e7ebf3f2f0867cf6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:58 -0800

    feat(token-map): Update Token -- 1-0xc221b7e65ffc80de234bbb6667abdd46593d34f0 -- Wrapped Centrifuge (WCFG) (0xc221...34f0)
    Token Details:
    - Id: 1-0xc221b7e65ffc80de234bbb6667abdd46593d34f0
    - Chain Id: 1
    - Contract Address: 0xc221b7e65ffc80de234bbb6667abdd46593d34f0
    - Name: Wrapped Centrifuge
    - Symbol: WCFG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10898.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10898

---
+++ sdk/constants/token-map/1/definitions/1-0xc221b7e65ffc80de234bbb6667abdd46593d34f0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc221b7e65ffc80de234bbb6667abdd46593d34f0",
+  chain_id: 1,
+  contract_address: "0xc221b7e65ffc80de234bbb6667abdd46593d34f0",
+  name: "Wrapped Centrifuge",
+  symbol: "WCFG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10898.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10898",
+  type: "token",
+});
