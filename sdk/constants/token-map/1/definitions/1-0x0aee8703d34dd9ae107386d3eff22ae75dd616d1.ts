d77df52422465a833a9538ab41165a78418bffd0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:52 -0800

    feat(token-map): Update Token -- 1-0x0aee8703d34dd9ae107386d3eff22ae75dd616d1 -- Tranche Finance (SLICE) (0x0aee...16d1)
    Token Details:
    - Id: 1-0x0aee8703d34dd9ae107386d3eff22ae75dd616d1
    - Chain Id: 1
    - Contract Address: 0x0aee8703d34dd9ae107386d3eff22ae75dd616d1
    - Name: Tranche Finance
    - Symbol: SLICE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8637.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8637

---
+++ sdk/constants/token-map/1/definitions/1-0x0aee8703d34dd9ae107386d3eff22ae75dd616d1.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0aee8703d34dd9ae107386d3eff22ae75dd616d1",
+  chain_id: 1,
+  contract_address: "0x0aee8703d34dd9ae107386d3eff22ae75dd616d1",
+  name: "Tranche Finance",
+  symbol: "SLICE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8637.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8637",
+  type: "token",
+});
