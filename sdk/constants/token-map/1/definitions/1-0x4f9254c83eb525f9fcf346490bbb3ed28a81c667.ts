bf0851435540b03bdbbd014cb13d0f138d882d91
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:27 -0800

    feat(token-map): Update Token -- 1-0x4f9254c83eb525f9fcf346490bbb3ed28a81c667 -- Celer Network (CELR) (0x4f92...c667)
    Token Details:
    - Id: 1-0x4f9254c83eb525f9fcf346490bbb3ed28a81c667
    - Chain Id: 1
    - Contract Address: 0x4f9254c83eb525f9fcf346490bbb3ed28a81c667
    - Name: Celer Network
    - Symbol: CELR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3814.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3814

---
+++ sdk/constants/token-map/1/definitions/1-0x4f9254c83eb525f9fcf346490bbb3ed28a81c667.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4f9254c83eb525f9fcf346490bbb3ed28a81c667",
+  chain_id: 1,
+  contract_address: "0x4f9254c83eb525f9fcf346490bbb3ed28a81c667",
+  name: "Celer Network",
+  symbol: "CELR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3814.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3814",
+  type: "token",
+});
