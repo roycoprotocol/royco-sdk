afb70986dabdf9939038a9baa1d6a0e3222dc7b4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:49 -0800

    feat(token-map): Update Token -- 1-0xde30da39c46104798bb5aa3fe8b9e0e1f348163f -- Gitcoin (GTC) (0xde30...163f)
    Token Details:
    - Id: 1-0xde30da39c46104798bb5aa3fe8b9e0e1f348163f
    - Chain Id: 1
    - Contract Address: 0xde30da39c46104798bb5aa3fe8b9e0e1f348163f
    - Name: Gitcoin
    - Symbol: GTC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10052.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10052

---
+++ sdk/constants/token-map/1/definitions/1-0xde30da39c46104798bb5aa3fe8b9e0e1f348163f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
+  chain_id: 1,
+  contract_address: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
+  name: "Gitcoin",
+  symbol: "GTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10052.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10052",
+  type: "token",
+});
