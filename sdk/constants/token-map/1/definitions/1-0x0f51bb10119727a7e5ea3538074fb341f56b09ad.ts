39d171dac759023eb4440157d3d36dbba7a61d50
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:54 -0800

    feat(token-map): Update Token -- 1-0x0f51bb10119727a7e5ea3538074fb341f56b09ad -- DAO Maker (DAO) (0x0f51...09ad)
    Token Details:
    - Id: 1-0x0f51bb10119727a7e5ea3538074fb341f56b09ad
    - Chain Id: 1
    - Contract Address: 0x0f51bb10119727a7e5ea3538074fb341f56b09ad
    - Name: DAO Maker
    - Symbol: DAO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8420.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8420

---
+++ sdk/constants/token-map/1/definitions/1-0x0f51bb10119727a7e5ea3538074fb341f56b09ad.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0f51bb10119727a7e5ea3538074fb341f56b09ad",
+  chain_id: 1,
+  contract_address: "0x0f51bb10119727a7e5ea3538074fb341f56b09ad",
+  name: "DAO Maker",
+  symbol: "DAO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8420.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8420",
+  type: "token",
+});
