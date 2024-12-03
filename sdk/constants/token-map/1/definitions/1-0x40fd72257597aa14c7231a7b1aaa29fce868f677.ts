4cf978dae78fd14987b782a863280deb74a0b7c4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:31 -0800

    feat(token-map): Update Token -- 1-0x40fd72257597aa14c7231a7b1aaa29fce868f677 -- SORA (XOR) (0x40fd...f677)
    Token Details:
    - Id: 1-0x40fd72257597aa14c7231a7b1aaa29fce868f677
    - Chain Id: 1
    - Contract Address: 0x40fd72257597aa14c7231a7b1aaa29fce868f677
    - Name: SORA
    - Symbol: XOR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5802.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5802

---
+++ sdk/constants/token-map/1/definitions/1-0x40fd72257597aa14c7231a7b1aaa29fce868f677.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x40fd72257597aa14c7231a7b1aaa29fce868f677",
+  chain_id: 1,
+  contract_address: "0x40fd72257597aa14c7231a7b1aaa29fce868f677",
+  name: "SORA",
+  symbol: "XOR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5802.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5802",
+  type: "token",
+});
