315d74f03670e2f8b89a4746de39aaecc96215aa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:10 -0800

    feat(token-map): Update Token -- 42161-0x431402e8b9de9aa016c743880e04e517074d8cec -- Hegic (HEGIC) (0x4314...8cec)
    Token Details:
    - Id: 42161-0x431402e8b9de9aa016c743880e04e517074d8cec
    - Chain Id: 42161
    - Contract Address: 0x431402e8b9de9aa016c743880e04e517074d8cec
    - Name: Hegic
    - Symbol: HEGIC
    - Image: https://coin-images.coingecko.com/coins/images/12454/large/new.png?1696512274
    - Decimals: 18
    - Source: coingecko
    - Search Id: hegic

---
+++ sdk/constants/token-map/42161/definitions/42161-0x431402e8b9de9aa016c743880e04e517074d8cec.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x431402e8b9de9aa016c743880e04e517074d8cec",
+  chain_id: 42161,
+  contract_address: "0x431402e8b9de9aa016c743880e04e517074d8cec",
+  name: "Hegic",
+  symbol: "HEGIC",
+  image: "https://coin-images.coingecko.com/coins/images/12454/large/new.png?1696512274",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "hegic",
+  type: "token",
+});
