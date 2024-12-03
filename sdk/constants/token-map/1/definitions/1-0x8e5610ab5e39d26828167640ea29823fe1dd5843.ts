32c28fe19f5010d875500b036ddaa29611038f2d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:26 -0800

    feat(token-map): Update Token -- 1-0x8e5610ab5e39d26828167640ea29823fe1dd5843 -- KanadeCoin (KNDC) (0x8e56...5843)
    Token Details:
    - Id: 1-0x8e5610ab5e39d26828167640ea29823fe1dd5843
    - Chain Id: 1
    - Contract Address: 0x8e5610ab5e39d26828167640ea29823fe1dd5843
    - Name: KanadeCoin
    - Symbol: KNDC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2890.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2890

---
+++ sdk/constants/token-map/1/definitions/1-0x8e5610ab5e39d26828167640ea29823fe1dd5843.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8e5610ab5e39d26828167640ea29823fe1dd5843",
+  chain_id: 1,
+  contract_address: "0x8e5610ab5e39d26828167640ea29823fe1dd5843",
+  name: "KanadeCoin",
+  symbol: "KNDC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2890.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2890",
+  type: "token",
+});
