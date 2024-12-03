6b763bcd1492da0ebade2cf7a16c6dbdecf7b1d5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:50 -0800

    feat(token-map): Update Token -- 1-0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4 -- Enigma (ENG) (0xf0ee...23e4)
    Token Details:
    - Id: 1-0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4
    - Chain Id: 1
    - Contract Address: 0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4
    - Name: Enigma
    - Symbol: ENG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2044.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2044

---
+++ sdk/constants/token-map/1/definitions/1-0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4",
+  chain_id: 1,
+  contract_address: "0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4",
+  name: "Enigma",
+  symbol: "ENG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2044.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2044",
+  type: "token",
+});
