c9847cb0560be54e4b01f1d686c29a030cb672ea
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:40:48 -0800

    feat(token-map): Update Token -- 42161-0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8 -- Pendle (PENDLE) (0x0c88...c9e8)
    Token Details:
    - Id: 42161-0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8
    - Chain Id: 42161
    - Contract Address: 0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8
    - Name: Pendle
    - Symbol: PENDLE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9481.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9481

---
+++ sdk/constants/token-map/42161/definitions/42161-0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8",
+  chain_id: 42161,
+  contract_address: "0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8",
+  name: "Pendle",
+  symbol: "PENDLE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9481.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9481",
+  type: "token",
+});
