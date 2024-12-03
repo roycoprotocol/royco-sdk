ae4ac8d97e946cb6aacbcaf9558eebe71b1dbd8b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:59 -0800

    feat(token-map): Update Token -- 1-0x97cb5cc1b2e10cc56dc16ab9179f06dfedbe41a2 -- MobilinkToken (MOLK) (0x97cb...41a2)
    Token Details:
    - Id: 1-0x97cb5cc1b2e10cc56dc16ab9179f06dfedbe41a2
    - Chain Id: 1
    - Contract Address: 0x97cb5cc1b2e10cc56dc16ab9179f06dfedbe41a2
    - Name: MobilinkToken
    - Symbol: MOLK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3304.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3304

---
+++ sdk/constants/token-map/1/definitions/1-0x97cb5cc1b2e10cc56dc16ab9179f06dfedbe41a2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x97cb5cc1b2e10cc56dc16ab9179f06dfedbe41a2",
+  chain_id: 1,
+  contract_address: "0x97cb5cc1b2e10cc56dc16ab9179f06dfedbe41a2",
+  name: "MobilinkToken",
+  symbol: "MOLK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3304.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3304",
+  type: "token",
+});
