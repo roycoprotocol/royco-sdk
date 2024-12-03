b4edf3b452592d5ba5a7e0c80f60e41bcea54a62
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:56 -0800

    feat(token-map): Update Token -- 1-0xc20464e0c373486d2b3335576e83a218b1618a5e -- Datarius Credit (DTRC) (0xc204...8a5e)
    Token Details:
    - Id: 1-0xc20464e0c373486d2b3335576e83a218b1618a5e
    - Chain Id: 1
    - Contract Address: 0xc20464e0c373486d2b3335576e83a218b1618a5e
    - Name: Datarius Credit
    - Symbol: DTRC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2752.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2752

---
+++ sdk/constants/token-map/1/definitions/1-0xc20464e0c373486d2b3335576e83a218b1618a5e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc20464e0c373486d2b3335576e83a218b1618a5e",
+  chain_id: 1,
+  contract_address: "0xc20464e0c373486d2b3335576e83a218b1618a5e",
+  name: "Datarius Credit",
+  symbol: "DTRC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2752.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2752",
+  type: "token",
+});
