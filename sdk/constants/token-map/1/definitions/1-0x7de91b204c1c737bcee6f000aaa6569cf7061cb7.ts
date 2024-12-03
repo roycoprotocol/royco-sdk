101aba4ddcd6eafbb400978caaf8aa552d3e1fe0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:47 -0800

    feat(token-map): Update Token -- 1-0x7de91b204c1c737bcee6f000aaa6569cf7061cb7 -- Robonomics.network (XRT) (0x7de9...1cb7)
    Token Details:
    - Id: 1-0x7de91b204c1c737bcee6f000aaa6569cf7061cb7
    - Chain Id: 1
    - Contract Address: 0x7de91b204c1c737bcee6f000aaa6569cf7061cb7
    - Name: Robonomics.network
    - Symbol: XRT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4757.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 4757

---
+++ sdk/constants/token-map/1/definitions/1-0x7de91b204c1c737bcee6f000aaa6569cf7061cb7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7de91b204c1c737bcee6f000aaa6569cf7061cb7",
+  chain_id: 1,
+  contract_address: "0x7de91b204c1c737bcee6f000aaa6569cf7061cb7",
+  name: "Robonomics.network",
+  symbol: "XRT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4757.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "4757",
+  type: "token",
+});
