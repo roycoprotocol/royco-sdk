86ba76969ee04c5c446e4ecf79ffcea120a0be40
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:05 -0800

    feat(token-map): Update Token -- 1-0x485d17a6f1b8780392d53d64751824253011a260 -- Chrono.tech (TIME) (0x485d...a260)
    Token Details:
    - Id: 1-0x485d17a6f1b8780392d53d64751824253011a260
    - Chain Id: 1
    - Contract Address: 0x485d17a6f1b8780392d53d64751824253011a260
    - Name: Chrono.tech
    - Symbol: TIME
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1556.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 1556

---
+++ sdk/constants/token-map/1/definitions/1-0x485d17a6f1b8780392d53d64751824253011a260.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x485d17a6f1b8780392d53d64751824253011a260",
+  chain_id: 1,
+  contract_address: "0x485d17a6f1b8780392d53d64751824253011a260",
+  name: "Chrono.tech",
+  symbol: "TIME",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1556.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "1556",
+  type: "token",
+});
