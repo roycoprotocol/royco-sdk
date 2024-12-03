6bfab558625ba9357872954f51ecd2cad08ac816
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:02 -0800

    feat(token-map): Update Token -- 1-0x0b38210ea11411557c13457d4da7dc6ea731b88a -- API3 (API3) (0x0b38...b88a)
    Token Details:
    - Id: 1-0x0b38210ea11411557c13457d4da7dc6ea731b88a
    - Chain Id: 1
    - Contract Address: 0x0b38210ea11411557c13457d4da7dc6ea731b88a
    - Name: API3
    - Symbol: API3
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7737.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7737

---
+++ sdk/constants/token-map/1/definitions/1-0x0b38210ea11411557c13457d4da7dc6ea731b88a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0b38210ea11411557c13457d4da7dc6ea731b88a",
+  chain_id: 1,
+  contract_address: "0x0b38210ea11411557c13457d4da7dc6ea731b88a",
+  name: "API3",
+  symbol: "API3",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7737.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7737",
+  type: "token",
+});
