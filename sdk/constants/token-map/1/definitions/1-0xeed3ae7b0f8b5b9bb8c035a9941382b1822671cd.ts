2ebe115abca2ba53e62d3c96866831a77b487987
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:26 -0800

    feat(token-map): Update Token -- 1-0xeed3ae7b0f8b5b9bb8c035a9941382b1822671cd -- EveryCoin  (EVY) (0xeed3...71cd)
    Token Details:
    - Id: 1-0xeed3ae7b0f8b5b9bb8c035a9941382b1822671cd
    - Chain Id: 1
    - Contract Address: 0xeed3ae7b0f8b5b9bb8c035a9941382b1822671cd
    - Name: EveryCoin 
    - Symbol: EVY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3754.png
    - Decimals: 12
    - Source: coinmarketcap
    - Search Id: 3754

---
+++ sdk/constants/token-map/1/definitions/1-0xeed3ae7b0f8b5b9bb8c035a9941382b1822671cd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xeed3ae7b0f8b5b9bb8c035a9941382b1822671cd",
+  chain_id: 1,
+  contract_address: "0xeed3ae7b0f8b5b9bb8c035a9941382b1822671cd",
+  name: "EveryCoin ",
+  symbol: "EVY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3754.png",
+  decimals: 12,
+  source: "coinmarketcap",
+  search_id: "3754",
+  type: "token",
+});
