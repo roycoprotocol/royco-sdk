22979d35d8c0d0a0b9c58f307e1d143bee84882a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:00:39 -0800

    feat(token-map): Update Token -- 1-0x12b6893ce26ea6341919fe289212ef77e51688c8 -- Tamadoge (TAMA) (0x12b6...88c8)
    Token Details:
    - Id: 1-0x12b6893ce26ea6341919fe289212ef77e51688c8
    - Chain Id: 1
    - Contract Address: 0x12b6893ce26ea6341919fe289212ef77e51688c8
    - Name: Tamadoge
    - Symbol: TAMA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/21968.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 21968

---
+++ sdk/constants/token-map/1/definitions/1-0x12b6893ce26ea6341919fe289212ef77e51688c8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x12b6893ce26ea6341919fe289212ef77e51688c8",
+  chain_id: 1,
+  contract_address: "0x12b6893ce26ea6341919fe289212ef77e51688c8",
+  name: "Tamadoge",
+  symbol: "TAMA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/21968.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "21968",
+  type: "token",
+});
