3e66fa22898e7b2a110ba49ac6f70df18b50fee9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:22 -0800

    feat(token-map): Update Token -- 1-0xeeaa40b28a2d1b0b08f6f97bb1dd4b75316c6107 -- Govi (GOVI) (0xeeaa...6107)
    Token Details:
    - Id: 1-0xeeaa40b28a2d1b0b08f6f97bb1dd4b75316c6107
    - Chain Id: 1
    - Contract Address: 0xeeaa40b28a2d1b0b08f6f97bb1dd4b75316c6107
    - Name: Govi
    - Symbol: GOVI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8408.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8408

---
+++ sdk/constants/token-map/1/definitions/1-0xeeaa40b28a2d1b0b08f6f97bb1dd4b75316c6107.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xeeaa40b28a2d1b0b08f6f97bb1dd4b75316c6107",
+  chain_id: 1,
+  contract_address: "0xeeaa40b28a2d1b0b08f6f97bb1dd4b75316c6107",
+  name: "Govi",
+  symbol: "GOVI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8408.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8408",
+  type: "token",
+});
