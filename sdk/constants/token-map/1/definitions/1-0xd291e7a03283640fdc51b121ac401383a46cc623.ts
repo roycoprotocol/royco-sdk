bd2ce58482840a55b8adab2c3968754ac7dd52be
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:03 -0800

    feat(token-map): Update Token -- 1-0xd291e7a03283640fdc51b121ac401383a46cc623 -- Rari Governance Token (RGT) (0xd291...c623)
    Token Details:
    - Id: 1-0xd291e7a03283640fdc51b121ac401383a46cc623
    - Chain Id: 1
    - Contract Address: 0xd291e7a03283640fdc51b121ac401383a46cc623
    - Name: Rari Governance Token
    - Symbol: RGT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7486.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7486

---
+++ sdk/constants/token-map/1/definitions/1-0xd291e7a03283640fdc51b121ac401383a46cc623.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd291e7a03283640fdc51b121ac401383a46cc623",
+  chain_id: 1,
+  contract_address: "0xd291e7a03283640fdc51b121ac401383a46cc623",
+  name: "Rari Governance Token",
+  symbol: "RGT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7486.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7486",
+  type: "token",
+});
