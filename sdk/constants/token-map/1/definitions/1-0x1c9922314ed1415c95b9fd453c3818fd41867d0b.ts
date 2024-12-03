ff43bd90c63835d16890dc3b83229c57cd762dad
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:26 -0800

    feat(token-map): Update Token -- 1-0x1c9922314ed1415c95b9fd453c3818fd41867d0b -- TOWER (TOWER) (0x1c99...7d0b)
    Token Details:
    - Id: 1-0x1c9922314ed1415c95b9fd453c3818fd41867d0b
    - Chain Id: 1
    - Contract Address: 0x1c9922314ed1415c95b9fd453c3818fd41867d0b
    - Name: TOWER
    - Symbol: TOWER
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8620.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8620

---
+++ sdk/constants/token-map/1/definitions/1-0x1c9922314ed1415c95b9fd453c3818fd41867d0b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1c9922314ed1415c95b9fd453c3818fd41867d0b",
+  chain_id: 1,
+  contract_address: "0x1c9922314ed1415c95b9fd453c3818fd41867d0b",
+  name: "TOWER",
+  symbol: "TOWER",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8620.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8620",
+  type: "token",
+});
