a1d10d73f24ea4b13450245814f8f1dc4b922a9c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:18 -0800

    feat(token-map): Update Token -- 1-0xbf5495efe5db9ce00f80364c8b423567e58d2110 -- Renzo Restaked ETH (EZETH) (0xbf54...2110)
    Token Details:
    - Id: 1-0xbf5495efe5db9ce00f80364c8b423567e58d2110
    - Chain Id: 1
    - Contract Address: 0xbf5495efe5db9ce00f80364c8b423567e58d2110
    - Name: Renzo Restaked ETH
    - Symbol: EZETH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29520.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29520

---
+++ sdk/constants/token-map/1/definitions/1-0xbf5495efe5db9ce00f80364c8b423567e58d2110.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbf5495efe5db9ce00f80364c8b423567e58d2110",
+  chain_id: 1,
+  contract_address: "0xbf5495efe5db9ce00f80364c8b423567e58d2110",
+  name: "Renzo Restaked ETH",
+  symbol: "EZETH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29520.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29520",
+  type: "token",
+});
