bee2ad84970df0c113f10e96322739b18cd0fe01
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:31:29 -0800

    feat(token-map): Update Token -- 1-0xc5fb36dd2fb59d3b98deff88425a3f425ee469ed -- Dejitaru Tsuka (TSUKA) (0xc5fb...69ed)
    Token Details:
    - Id: 1-0xc5fb36dd2fb59d3b98deff88425a3f425ee469ed
    - Chain Id: 1
    - Contract Address: 0xc5fb36dd2fb59d3b98deff88425a3f425ee469ed
    - Name: Dejitaru Tsuka
    - Symbol: TSUKA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/20789.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 20789

---
+++ sdk/constants/token-map/1/definitions/1-0xc5fb36dd2fb59d3b98deff88425a3f425ee469ed.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc5fb36dd2fb59d3b98deff88425a3f425ee469ed",
+  chain_id: 1,
+  contract_address: "0xc5fb36dd2fb59d3b98deff88425a3f425ee469ed",
+  name: "Dejitaru Tsuka",
+  symbol: "TSUKA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/20789.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "20789",
+  type: "token",
+});
