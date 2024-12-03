c623a10aeb0e12801beb6d731595bfb24fae6f61
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:57 -0800

    feat(token-map): Update Token -- 1-0xe50365f5d679cb98a1dd62d6f6e58e59321bcddf -- LATOKEN (LA) (0xe503...cddf)
    Token Details:
    - Id: 1-0xe50365f5d679cb98a1dd62d6f6e58e59321bcddf
    - Chain Id: 1
    - Contract Address: 0xe50365f5d679cb98a1dd62d6f6e58e59321bcddf
    - Name: LATOKEN
    - Symbol: LA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2090.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2090

---
+++ sdk/constants/token-map/1/definitions/1-0xe50365f5d679cb98a1dd62d6f6e58e59321bcddf.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe50365f5d679cb98a1dd62d6f6e58e59321bcddf",
+  chain_id: 1,
+  contract_address: "0xe50365f5d679cb98a1dd62d6f6e58e59321bcddf",
+  name: "LATOKEN",
+  symbol: "LA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2090.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2090",
+  type: "token",
+});
