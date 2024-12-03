dfaff4646a8970151e3915f1b24bd896bd253a21
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:32 -0800

    feat(token-map): Update Token -- 1-0xada62f7ccd6af6cacff04accbc4f56f3d4ffd4ef -- PlayFuel (PLF) (0xada6...d4ef)
    Token Details:
    - Id: 1-0xada62f7ccd6af6cacff04accbc4f56f3d4ffd4ef
    - Chain Id: 1
    - Contract Address: 0xada62f7ccd6af6cacff04accbc4f56f3d4ffd4ef
    - Name: PlayFuel
    - Symbol: PLF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5084.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5084

---
+++ sdk/constants/token-map/1/definitions/1-0xada62f7ccd6af6cacff04accbc4f56f3d4ffd4ef.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xada62f7ccd6af6cacff04accbc4f56f3d4ffd4ef",
+  chain_id: 1,
+  contract_address: "0xada62f7ccd6af6cacff04accbc4f56f3d4ffd4ef",
+  name: "PlayFuel",
+  symbol: "PLF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5084.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5084",
+  type: "token",
+});
