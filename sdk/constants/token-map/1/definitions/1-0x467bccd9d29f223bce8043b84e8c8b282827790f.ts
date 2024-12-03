e9146ce2cf0513911211712523b409ecaff620b6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:49 -0800

    feat(token-map): Update Token -- 1-0x467bccd9d29f223bce8043b84e8c8b282827790f -- Telcoin (TEL) (0x467b...790f)
    Token Details:
    - Id: 1-0x467bccd9d29f223bce8043b84e8c8b282827790f
    - Chain Id: 1
    - Contract Address: 0x467bccd9d29f223bce8043b84e8c8b282827790f
    - Name: Telcoin
    - Symbol: TEL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2394.png
    - Decimals: 2
    - Source: coinmarketcap
    - Search Id: 2394

---
+++ sdk/constants/token-map/1/definitions/1-0x467bccd9d29f223bce8043b84e8c8b282827790f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x467bccd9d29f223bce8043b84e8c8b282827790f",
+  chain_id: 1,
+  contract_address: "0x467bccd9d29f223bce8043b84e8c8b282827790f",
+  name: "Telcoin",
+  symbol: "TEL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2394.png",
+  decimals: 2,
+  source: "coinmarketcap",
+  search_id: "2394",
+  type: "token",
+});
