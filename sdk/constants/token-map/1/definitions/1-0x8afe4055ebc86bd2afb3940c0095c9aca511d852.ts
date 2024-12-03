30a488deb280433d9d29b60b9948a0ebf15a7f49
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:55 -0800

    feat(token-map): Update Token -- 1-0x8afe4055ebc86bd2afb3940c0095c9aca511d852 -- Arbius (AIUS) (0x8afe...d852)
    Token Details:
    - Id: 1-0x8afe4055ebc86bd2afb3940c0095c9aca511d852
    - Chain Id: 1
    - Contract Address: 0x8afe4055ebc86bd2afb3940c0095c9aca511d852
    - Name: Arbius
    - Symbol: AIUS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29433.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29433

---
+++ sdk/constants/token-map/1/definitions/1-0x8afe4055ebc86bd2afb3940c0095c9aca511d852.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8afe4055ebc86bd2afb3940c0095c9aca511d852",
+  chain_id: 1,
+  contract_address: "0x8afe4055ebc86bd2afb3940c0095c9aca511d852",
+  name: "Arbius",
+  symbol: "AIUS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29433.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29433",
+  type: "token",
+});
