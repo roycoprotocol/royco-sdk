af504269ddbd1f9a4508b3eafa8ab7d992e22359
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:03 -0800

    feat(token-map): Update Token -- 1-0x80c62fe4487e1351b47ba49809ebd60ed085bf52 -- CLV (CLV) (0x80c6...bf52)
    Token Details:
    - Id: 1-0x80c62fe4487e1351b47ba49809ebd60ed085bf52
    - Chain Id: 1
    - Contract Address: 0x80c62fe4487e1351b47ba49809ebd60ed085bf52
    - Name: CLV
    - Symbol: CLV
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8384.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8384

---
+++ sdk/constants/token-map/1/definitions/1-0x80c62fe4487e1351b47ba49809ebd60ed085bf52.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x80c62fe4487e1351b47ba49809ebd60ed085bf52",
+  chain_id: 1,
+  contract_address: "0x80c62fe4487e1351b47ba49809ebd60ed085bf52",
+  name: "CLV",
+  symbol: "CLV",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8384.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8384",
+  type: "token",
+});
