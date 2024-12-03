7d3fc64dff077097267d545d431b83c9aba8c072
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:57 -0800

    feat(token-map): Update Token -- 1-0x64cdf819d3e75ac8ec217b3496d7ce167be42e80 -- VouchForMe (IPL) (0x64cd...2e80)
    Token Details:
    - Id: 1-0x64cdf819d3e75ac8ec217b3496d7ce167be42e80
    - Chain Id: 1
    - Contract Address: 0x64cdf819d3e75ac8ec217b3496d7ce167be42e80
    - Name: VouchForMe
    - Symbol: IPL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2421.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2421

---
+++ sdk/constants/token-map/1/definitions/1-0x64cdf819d3e75ac8ec217b3496d7ce167be42e80.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x64cdf819d3e75ac8ec217b3496d7ce167be42e80",
+  chain_id: 1,
+  contract_address: "0x64cdf819d3e75ac8ec217b3496d7ce167be42e80",
+  name: "VouchForMe",
+  symbol: "IPL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2421.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2421",
+  type: "token",
+});
