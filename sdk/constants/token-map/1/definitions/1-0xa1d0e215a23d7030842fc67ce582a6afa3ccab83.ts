5412724240901f8dbc69e23b6a2724e95ce51c09
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:41 -0800

    feat(token-map): Update Token -- 1-0xa1d0e215a23d7030842fc67ce582a6afa3ccab83 -- DFI.Money (YFII) (0xa1d0...ab83)
    Token Details:
    - Id: 1-0xa1d0e215a23d7030842fc67ce582a6afa3ccab83
    - Chain Id: 1
    - Contract Address: 0xa1d0e215a23d7030842fc67ce582a6afa3ccab83
    - Name: DFI.Money
    - Symbol: YFII
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5957.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5957

---
+++ sdk/constants/token-map/1/definitions/1-0xa1d0e215a23d7030842fc67ce582a6afa3ccab83.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa1d0e215a23d7030842fc67ce582a6afa3ccab83",
+  chain_id: 1,
+  contract_address: "0xa1d0e215a23d7030842fc67ce582a6afa3ccab83",
+  name: "DFI.Money",
+  symbol: "YFII",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5957.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5957",
+  type: "token",
+});
