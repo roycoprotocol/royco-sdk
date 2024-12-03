d1ccce8cb1242a21378d2b75db60d7b0cefd972d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:03:51 -0800

    feat(token-map): Update Token -- 1-0x25e1474170c4c0aa64fa98123bdc8db49d7802fa -- Bidao (BID) (0x25e1...02fa)
    Token Details:
    - Id: 1-0x25e1474170c4c0aa64fa98123bdc8db49d7802fa
    - Chain Id: 1
    - Contract Address: 0x25e1474170c4c0aa64fa98123bdc8db49d7802fa
    - Name: Bidao
    - Symbol: BID
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7200.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7200

---
+++ sdk/constants/token-map/1/definitions/1-0x25e1474170c4c0aa64fa98123bdc8db49d7802fa.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x25e1474170c4c0aa64fa98123bdc8db49d7802fa",
+  chain_id: 1,
+  contract_address: "0x25e1474170c4c0aa64fa98123bdc8db49d7802fa",
+  name: "Bidao",
+  symbol: "BID",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7200.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7200",
+  type: "token",
+});
