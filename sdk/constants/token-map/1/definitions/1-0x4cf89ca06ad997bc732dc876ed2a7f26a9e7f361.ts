fcbd2acf064d3e8ef8d15d5351b022f70949bda1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:01 -0800

    feat(token-map): Update Token -- 1-0x4cf89ca06ad997bc732dc876ed2a7f26a9e7f361 -- Mysterium (MYST) (0x4cf8...f361)
    Token Details:
    - Id: 1-0x4cf89ca06ad997bc732dc876ed2a7f26a9e7f361
    - Chain Id: 1
    - Contract Address: 0x4cf89ca06ad997bc732dc876ed2a7f26a9e7f361
    - Name: Mysterium
    - Symbol: MYST
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1721.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1721

---
+++ sdk/constants/token-map/1/definitions/1-0x4cf89ca06ad997bc732dc876ed2a7f26a9e7f361.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4cf89ca06ad997bc732dc876ed2a7f26a9e7f361",
+  chain_id: 1,
+  contract_address: "0x4cf89ca06ad997bc732dc876ed2a7f26a9e7f361",
+  name: "Mysterium",
+  symbol: "MYST",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1721.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1721",
+  type: "token",
+});
