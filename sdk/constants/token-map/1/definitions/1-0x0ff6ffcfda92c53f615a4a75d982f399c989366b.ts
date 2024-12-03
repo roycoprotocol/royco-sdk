2f396e16c64122d10547053554556984528b1019
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:00:09 -0800

    feat(token-map): Update Token -- 1-0x0ff6ffcfda92c53f615a4a75d982f399c989366b -- UniLayer (LAYER) (0x0ff6...366b)
    Token Details:
    - Id: 1-0x0ff6ffcfda92c53f615a4a75d982f399c989366b
    - Chain Id: 1
    - Contract Address: 0x0ff6ffcfda92c53f615a4a75d982f399c989366b
    - Name: UniLayer
    - Symbol: LAYER
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6638.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6638

---
+++ sdk/constants/token-map/1/definitions/1-0x0ff6ffcfda92c53f615a4a75d982f399c989366b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0ff6ffcfda92c53f615a4a75d982f399c989366b",
+  chain_id: 1,
+  contract_address: "0x0ff6ffcfda92c53f615a4a75d982f399c989366b",
+  name: "UniLayer",
+  symbol: "LAYER",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6638.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6638",
+  type: "token",
+});
