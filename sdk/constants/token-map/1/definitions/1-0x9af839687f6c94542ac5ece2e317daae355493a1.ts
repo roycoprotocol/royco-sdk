dcb7ec36ca373ffe2ac6486cdf42a975709a73fc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:29 -0800

    feat(token-map): Update Token -- 1-0x9af839687f6c94542ac5ece2e317daae355493a1 -- Hydro Protocol (HOT) (0x9af8...93a1)
    Token Details:
    - Id: 1-0x9af839687f6c94542ac5ece2e317daae355493a1
    - Chain Id: 1
    - Contract Address: 0x9af839687f6c94542ac5ece2e317daae355493a1
    - Name: Hydro Protocol
    - Symbol: HOT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2430.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2430

---
+++ sdk/constants/token-map/1/definitions/1-0x9af839687f6c94542ac5ece2e317daae355493a1.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9af839687f6c94542ac5ece2e317daae355493a1",
+  chain_id: 1,
+  contract_address: "0x9af839687f6c94542ac5ece2e317daae355493a1",
+  name: "Hydro Protocol",
+  symbol: "HOT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2430.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2430",
+  type: "token",
+});
