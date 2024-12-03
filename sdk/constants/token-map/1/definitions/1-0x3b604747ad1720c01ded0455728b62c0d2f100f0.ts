daa89f9064657cfc272e01a91ddd9da8966d3bd8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:50 -0800

    feat(token-map): Update Token -- 1-0x3b604747ad1720c01ded0455728b62c0d2f100f0 -- WAGMI Games (WAGMIGAMES) (0x3b60...00f0)
    Token Details:
    - Id: 1-0x3b604747ad1720c01ded0455728b62c0d2f100f0
    - Chain Id: 1
    - Contract Address: 0x3b604747ad1720c01ded0455728b62c0d2f100f0
    - Name: WAGMI Games
    - Symbol: WAGMIGAMES
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/20497.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 20497

---
+++ sdk/constants/token-map/1/definitions/1-0x3b604747ad1720c01ded0455728b62c0d2f100f0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3b604747ad1720c01ded0455728b62c0d2f100f0",
+  chain_id: 1,
+  contract_address: "0x3b604747ad1720c01ded0455728b62c0d2f100f0",
+  name: "WAGMI Games",
+  symbol: "WAGMIGAMES",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/20497.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "20497",
+  type: "token",
+});
