074dd3375730f237d361bb085f92345b6a2b627b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:44 -0800

    feat(token-map): Update Token -- 1-0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb -- sETH (SETH) (0x5e74...31cb)
    Token Details:
    - Id: 1-0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb
    - Chain Id: 1
    - Contract Address: 0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb
    - Name: sETH
    - Symbol: SETH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5765.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5765

---
+++ sdk/constants/token-map/1/definitions/1-0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb",
+  chain_id: 1,
+  contract_address: "0x5e74c9036fb86bd7ecdcb084a0673efc32ea31cb",
+  name: "sETH",
+  symbol: "SETH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5765.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5765",
+  type: "token",
+});
