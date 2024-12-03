9bc458f276aeb3e6f7e70ef34cfd592080b95f0d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:01 -0800

    feat(token-map): Update Token -- 1-0x07597255910a51509ca469568b048f2597e72504 -- Uptrennd (1UP) (0x0759...2504)
    Token Details:
    - Id: 1-0x07597255910a51509ca469568b048f2597e72504
    - Chain Id: 1
    - Contract Address: 0x07597255910a51509ca469568b048f2597e72504
    - Name: Uptrennd
    - Symbol: 1UP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4213.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4213

---
+++ sdk/constants/token-map/1/definitions/1-0x07597255910a51509ca469568b048f2597e72504.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x07597255910a51509ca469568b048f2597e72504",
+  chain_id: 1,
+  contract_address: "0x07597255910a51509ca469568b048f2597e72504",
+  name: "Uptrennd",
+  symbol: "1UP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4213.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4213",
+  type: "token",
+});
