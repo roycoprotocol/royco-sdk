0a6378aa6bc1c5b83278aa680f06f48e93adcf94
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:37 -0800

    feat(token-map): Update Token -- 1-0xc0ea83113038987d974fe667831a36e442e661e7 -- Libfx (LIBFX) (0xc0ea...61e7)
    Token Details:
    - Id: 1-0xc0ea83113038987d974fe667831a36e442e661e7
    - Chain Id: 1
    - Contract Address: 0xc0ea83113038987d974fe667831a36e442e661e7
    - Name: Libfx
    - Symbol: LIBFX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6714.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6714

---
+++ sdk/constants/token-map/1/definitions/1-0xc0ea83113038987d974fe667831a36e442e661e7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc0ea83113038987d974fe667831a36e442e661e7",
+  chain_id: 1,
+  contract_address: "0xc0ea83113038987d974fe667831a36e442e661e7",
+  name: "Libfx",
+  symbol: "LIBFX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6714.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6714",
+  type: "token",
+});
