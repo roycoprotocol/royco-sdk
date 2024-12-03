a6f2d4e250988b7d83782341b009fe61fd322bac
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:40:13 -0800

    feat(token-map): Update Token -- 1-0xff56cc6b1e6ded347aa0b7676c85ab0b3d08b0fa -- Orbs (ORBS) (0xff56...b0fa)
    Token Details:
    - Id: 1-0xff56cc6b1e6ded347aa0b7676c85ab0b3d08b0fa
    - Chain Id: 1
    - Contract Address: 0xff56cc6b1e6ded347aa0b7676c85ab0b3d08b0fa
    - Name: Orbs
    - Symbol: ORBS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3835.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3835

---
+++ sdk/constants/token-map/1/definitions/1-0xff56cc6b1e6ded347aa0b7676c85ab0b3d08b0fa.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xff56cc6b1e6ded347aa0b7676c85ab0b3d08b0fa",
+  chain_id: 1,
+  contract_address: "0xff56cc6b1e6ded347aa0b7676c85ab0b3d08b0fa",
+  name: "Orbs",
+  symbol: "ORBS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3835.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3835",
+  type: "token",
+});
