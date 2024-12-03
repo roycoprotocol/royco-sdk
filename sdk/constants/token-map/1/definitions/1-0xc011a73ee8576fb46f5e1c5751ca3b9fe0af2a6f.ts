587ee5176c5d7c51dd7016317f262332fac82c6f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:25 -0800

    feat(token-map): Update Token -- 1-0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f -- Synthetix (SNX) (0xc011...2a6f)
    Token Details:
    - Id: 1-0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f
    - Chain Id: 1
    - Contract Address: 0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f
    - Name: Synthetix
    - Symbol: SNX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2586.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2586

---
+++ sdk/constants/token-map/1/definitions/1-0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f",
+  chain_id: 1,
+  contract_address: "0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f",
+  name: "Synthetix",
+  symbol: "SNX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2586.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2586",
+  type: "token",
+});
