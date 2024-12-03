b6ce88d64ca229474aba82bb29ee1261e00d960a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:17:10 -0800

    feat(token-map): Update Token -- 1-0x6f40d4a6237c257fff2db00fa0510deeecd303eb -- Instadapp (INST) (0x6f40...03eb)
    Token Details:
    - Id: 1-0x6f40d4a6237c257fff2db00fa0510deeecd303eb
    - Chain Id: 1
    - Contract Address: 0x6f40d4a6237c257fff2db00fa0510deeecd303eb
    - Name: Instadapp
    - Symbol: INST
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10508.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10508

---
+++ sdk/constants/token-map/1/definitions/1-0x6f40d4a6237c257fff2db00fa0510deeecd303eb.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6f40d4a6237c257fff2db00fa0510deeecd303eb",
+  chain_id: 1,
+  contract_address: "0x6f40d4a6237c257fff2db00fa0510deeecd303eb",
+  name: "Instadapp",
+  symbol: "INST",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10508.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10508",
+  type: "token",
+});
