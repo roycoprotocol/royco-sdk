34aa5ee4b33e79c5868e7b440464af4992f03ddf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:48 -0800

    feat(token-map): Update Token -- 1-0x9d39a5de30e57443bff2a8307a4256c8797a3497 -- Ethena Staked USDe (sUSDe) (0x9d39...3497)
    Token Details:
    - Id: 1-0x9d39a5de30e57443bff2a8307a4256c8797a3497
    - Chain Id: 1
    - Contract Address: 0x9d39a5de30e57443bff2a8307a4256c8797a3497
    - Name: Ethena Staked USDe
    - Symbol: sUSDe
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29471.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29471

---
+++ sdk/constants/token-map/1/definitions/1-0x9d39a5de30e57443bff2a8307a4256c8797a3497.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9d39a5de30e57443bff2a8307a4256c8797a3497",
+  chain_id: 1,
+  contract_address: "0x9d39a5de30e57443bff2a8307a4256c8797a3497",
+  name: "Ethena Staked USDe",
+  symbol: "sUSDe",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29471.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29471",
+  type: "token",
+});
