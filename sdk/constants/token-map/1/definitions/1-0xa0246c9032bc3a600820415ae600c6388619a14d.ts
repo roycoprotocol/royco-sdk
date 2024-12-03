80c10807afa67af93ce5a6e4c583aba1f12ba8bc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:20 -0800

    feat(token-map): Update Token -- 1-0xa0246c9032bc3a600820415ae600c6388619a14d -- Harvest Finance (FARM) (0xa024...a14d)
    Token Details:
    - Id: 1-0xa0246c9032bc3a600820415ae600c6388619a14d
    - Chain Id: 1
    - Contract Address: 0xa0246c9032bc3a600820415ae600c6388619a14d
    - Name: Harvest Finance
    - Symbol: FARM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6859.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6859

---
+++ sdk/constants/token-map/1/definitions/1-0xa0246c9032bc3a600820415ae600c6388619a14d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa0246c9032bc3a600820415ae600c6388619a14d",
+  chain_id: 1,
+  contract_address: "0xa0246c9032bc3a600820415ae600c6388619a14d",
+  name: "Harvest Finance",
+  symbol: "FARM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6859.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6859",
+  type: "token",
+});
