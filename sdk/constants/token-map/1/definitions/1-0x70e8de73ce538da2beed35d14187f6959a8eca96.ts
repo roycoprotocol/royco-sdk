d4020451bfddb802b8e2434bdddfe46f0b8e6cb2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:17:22 -0800

    feat(token-map): Update Token -- 1-0x70e8de73ce538da2beed35d14187f6959a8eca96 -- XSGD (XSGD) (0x70e8...ca96)
    Token Details:
    - Id: 1-0x70e8de73ce538da2beed35d14187f6959a8eca96
    - Chain Id: 1
    - Contract Address: 0x70e8de73ce538da2beed35d14187f6959a8eca96
    - Name: XSGD
    - Symbol: XSGD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8489.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 8489

---
+++ sdk/constants/token-map/1/definitions/1-0x70e8de73ce538da2beed35d14187f6959a8eca96.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x70e8de73ce538da2beed35d14187f6959a8eca96",
+  chain_id: 1,
+  contract_address: "0x70e8de73ce538da2beed35d14187f6959a8eca96",
+  name: "XSGD",
+  symbol: "XSGD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8489.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "8489",
+  type: "token",
+});
