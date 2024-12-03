4813f6c9bde76674ca52657be06a98ed77c19722
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:13 -0800

    feat(token-map): Update Token -- 42161-0xd4d42f0b6def4ce0383636770ef773390d85c61a -- SushiSwap (SUSHI) (0xd4d4...c61a)
    Token Details:
    - Id: 42161-0xd4d42f0b6def4ce0383636770ef773390d85c61a
    - Chain Id: 42161
    - Contract Address: 0xd4d42f0b6def4ce0383636770ef773390d85c61a
    - Name: SushiSwap
    - Symbol: SUSHI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6758.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6758

---
+++ sdk/constants/token-map/42161/definitions/42161-0xd4d42f0b6def4ce0383636770ef773390d85c61a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0xd4d42f0b6def4ce0383636770ef773390d85c61a",
+  chain_id: 42161,
+  contract_address: "0xd4d42f0b6def4ce0383636770ef773390d85c61a",
+  name: "SushiSwap",
+  symbol: "SUSHI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6758.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6758",
+  type: "token",
+});
