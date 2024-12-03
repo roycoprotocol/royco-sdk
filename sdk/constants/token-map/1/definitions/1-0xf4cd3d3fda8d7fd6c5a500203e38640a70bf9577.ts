a19fd691152004cb429a3d1e2da6a1c30d7ba25e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:29 -0800

    feat(token-map): Update Token -- 1-0xf4cd3d3fda8d7fd6c5a500203e38640a70bf9577 -- YFDAI.FINANCE (YF-DAI) (0xf4cd...9577)
    Token Details:
    - Id: 1-0xf4cd3d3fda8d7fd6c5a500203e38640a70bf9577
    - Chain Id: 1
    - Contract Address: 0xf4cd3d3fda8d7fd6c5a500203e38640a70bf9577
    - Name: YFDAI.FINANCE
    - Symbol: YF-DAI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6938.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6938

---
+++ sdk/constants/token-map/1/definitions/1-0xf4cd3d3fda8d7fd6c5a500203e38640a70bf9577.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf4cd3d3fda8d7fd6c5a500203e38640a70bf9577",
+  chain_id: 1,
+  contract_address: "0xf4cd3d3fda8d7fd6c5a500203e38640a70bf9577",
+  name: "YFDAI.FINANCE",
+  symbol: "YF-DAI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6938.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6938",
+  type: "token",
+});
