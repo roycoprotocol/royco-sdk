f1afc810dbed3d98a001273f99fb9413d384039d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:41 -0800

    feat(token-map): Update Token -- 8453-0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22 -- Coinbase Wrapped Staked ETH (cbETH) (0x2ae3...ec22)
    Token Details:
    - Id: 8453-0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22
    - Chain Id: 8453
    - Contract Address: 0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22
    - Name: Coinbase Wrapped Staked ETH
    - Symbol: cbETH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/21535.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 21535

---
+++ sdk/constants/token-map/8453/definitions/8453-0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22",
+  chain_id: 8453,
+  contract_address: "0x2ae3f1ec7f1f5012cfeab0185bfc7aa3cf0dec22",
+  name: "Coinbase Wrapped Staked ETH",
+  symbol: "cbETH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/21535.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "21535",
+  type: "token",
+});
