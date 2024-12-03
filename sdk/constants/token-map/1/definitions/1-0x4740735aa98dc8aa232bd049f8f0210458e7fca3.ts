96cbd93bf17080e795cda4ac0c44f8ca05f00eea
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:56 -0800

    feat(token-map): Update Token -- 1-0x4740735aa98dc8aa232bd049f8f0210458e7fca3 -- Ridotto (RDT) (0x4740...fca3)
    Token Details:
    - Id: 1-0x4740735aa98dc8aa232bd049f8f0210458e7fca3
    - Chain Id: 1
    - Contract Address: 0x4740735aa98dc8aa232bd049f8f0210458e7fca3
    - Name: Ridotto
    - Symbol: RDT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12465.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 12465

---
+++ sdk/constants/token-map/1/definitions/1-0x4740735aa98dc8aa232bd049f8f0210458e7fca3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4740735aa98dc8aa232bd049f8f0210458e7fca3",
+  chain_id: 1,
+  contract_address: "0x4740735aa98dc8aa232bd049f8f0210458e7fca3",
+  name: "Ridotto",
+  symbol: "RDT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12465.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "12465",
+  type: "token",
+});
