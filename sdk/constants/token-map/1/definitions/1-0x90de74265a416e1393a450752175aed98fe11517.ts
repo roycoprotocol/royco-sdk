df7e4fa04e7becf0f1ecd35276bcb18024353c07
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:02 -0800

    feat(token-map): Update Token -- 1-0x90de74265a416e1393a450752175aed98fe11517 -- Unlock Protocol (UDT) (0x90de...1517)
    Token Details:
    - Id: 1-0x90de74265a416e1393a450752175aed98fe11517
    - Chain Id: 1
    - Contract Address: 0x90de74265a416e1393a450752175aed98fe11517
    - Name: Unlock Protocol
    - Symbol: UDT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9364.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9364

---
+++ sdk/constants/token-map/1/definitions/1-0x90de74265a416e1393a450752175aed98fe11517.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x90de74265a416e1393a450752175aed98fe11517",
+  chain_id: 1,
+  contract_address: "0x90de74265a416e1393a450752175aed98fe11517",
+  name: "Unlock Protocol",
+  symbol: "UDT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9364.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9364",
+  type: "token",
+});
