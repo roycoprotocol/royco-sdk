f47f604139ef32d43f47d293bec1ac6de6323afe
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:51 -0800

    feat(token-map): Update Token -- 1-0x68a47fe1cf42eba4a030a10cd4d6a1031ca3ca0a -- Tectum (TET) (0x68a4...ca0a)
    Token Details:
    - Id: 1-0x68a47fe1cf42eba4a030a10cd4d6a1031ca3ca0a
    - Chain Id: 1
    - Contract Address: 0x68a47fe1cf42eba4a030a10cd4d6a1031ca3ca0a
    - Name: Tectum
    - Symbol: TET
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/21964.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 21964

---
+++ sdk/constants/token-map/1/definitions/1-0x68a47fe1cf42eba4a030a10cd4d6a1031ca3ca0a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x68a47fe1cf42eba4a030a10cd4d6a1031ca3ca0a",
+  chain_id: 1,
+  contract_address: "0x68a47fe1cf42eba4a030a10cd4d6a1031ca3ca0a",
+  name: "Tectum",
+  symbol: "TET",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/21964.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "21964",
+  type: "token",
+});
