04eb29ed2aedd55c306bc1d89014c74058a6ad7c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:39 -0800

    feat(token-map): Update Token -- 1-0x3ab6ed69ef663bd986ee59205ccad8a20f98b4c2 -- Drep [new] (DREP) (0x3ab6...b4c2)
    Token Details:
    - Id: 1-0x3ab6ed69ef663bd986ee59205ccad8a20f98b4c2
    - Chain Id: 1
    - Contract Address: 0x3ab6ed69ef663bd986ee59205ccad8a20f98b4c2
    - Name: Drep [new]
    - Symbol: DREP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9148.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9148

---
+++ sdk/constants/token-map/1/definitions/1-0x3ab6ed69ef663bd986ee59205ccad8a20f98b4c2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3ab6ed69ef663bd986ee59205ccad8a20f98b4c2",
+  chain_id: 1,
+  contract_address: "0x3ab6ed69ef663bd986ee59205ccad8a20f98b4c2",
+  name: "Drep [new]",
+  symbol: "DREP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9148.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9148",
+  type: "token",
+});
