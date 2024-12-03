ec5f05f44fea10619018781e4f8d7c9486f5d4d3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:02 -0800

    feat(token-map): Update Token -- 1-0x60c24407d01782c2175d32fe7c8921ed732371d1 -- LemoChain (LEMO) (0x60c2...71d1)
    Token Details:
    - Id: 1-0x60c24407d01782c2175d32fe7c8921ed732371d1
    - Chain Id: 1
    - Contract Address: 0x60c24407d01782c2175d32fe7c8921ed732371d1
    - Name: LemoChain
    - Symbol: LEMO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2950.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2950

---
+++ sdk/constants/token-map/1/definitions/1-0x60c24407d01782c2175d32fe7c8921ed732371d1.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x60c24407d01782c2175d32fe7c8921ed732371d1",
+  chain_id: 1,
+  contract_address: "0x60c24407d01782c2175d32fe7c8921ed732371d1",
+  name: "LemoChain",
+  symbol: "LEMO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2950.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2950",
+  type: "token",
+});
