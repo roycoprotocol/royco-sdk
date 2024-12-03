e7a871bceca03505e502f69b4290974c376e01e5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:18 -0800

    feat(token-map): Update Token -- 1-0x745407c86df8db893011912d3ab28e68b62e49b0 -- MAHA.xyz (MAHA) (0x7454...49b0)
    Token Details:
    - Id: 1-0x745407c86df8db893011912d3ab28e68b62e49b0
    - Chain Id: 1
    - Contract Address: 0x745407c86df8db893011912d3ab28e68b62e49b0
    - Name: MAHA.xyz
    - Symbol: MAHA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8043.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8043

---
+++ sdk/constants/token-map/1/definitions/1-0x745407c86df8db893011912d3ab28e68b62e49b0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x745407c86df8db893011912d3ab28e68b62e49b0",
+  chain_id: 1,
+  contract_address: "0x745407c86df8db893011912d3ab28e68b62e49b0",
+  name: "MAHA.xyz",
+  symbol: "MAHA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8043.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8043",
+  type: "token",
+});
