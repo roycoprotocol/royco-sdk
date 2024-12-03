ae8895003cd0126e7b0b886d69564675efc39920
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:57:57 -0800

    feat(token-map): Update Token -- 1-0x06f3cdabae564b0546529b4dd8fef1bcd4235753 -- TILWIKI (TLW) (0x06f3...5753)
    Token Details:
    - Id: 1-0x06f3cdabae564b0546529b4dd8fef1bcd4235753
    - Chain Id: 1
    - Contract Address: 0x06f3cdabae564b0546529b4dd8fef1bcd4235753
    - Name: TILWIKI
    - Symbol: TLW
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5399.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 5399

---
+++ sdk/constants/token-map/1/definitions/1-0x06f3cdabae564b0546529b4dd8fef1bcd4235753.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x06f3cdabae564b0546529b4dd8fef1bcd4235753",
+  chain_id: 1,
+  contract_address: "0x06f3cdabae564b0546529b4dd8fef1bcd4235753",
+  name: "TILWIKI",
+  symbol: "TLW",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5399.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "5399",
+  type: "token",
+});
