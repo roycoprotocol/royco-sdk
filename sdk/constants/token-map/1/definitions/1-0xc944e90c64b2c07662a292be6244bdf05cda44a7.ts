b8a7441f642d0a26b019c5468de51a40e61c1eb7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:03 -0800

    feat(token-map): Update Token -- 1-0xc944e90c64b2c07662a292be6244bdf05cda44a7 -- The Graph (GRT) (0xc944...44a7)
    Token Details:
    - Id: 1-0xc944e90c64b2c07662a292be6244bdf05cda44a7
    - Chain Id: 1
    - Contract Address: 0xc944e90c64b2c07662a292be6244bdf05cda44a7
    - Name: The Graph
    - Symbol: GRT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6719.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6719

---
+++ sdk/constants/token-map/1/definitions/1-0xc944e90c64b2c07662a292be6244bdf05cda44a7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc944e90c64b2c07662a292be6244bdf05cda44a7",
+  chain_id: 1,
+  contract_address: "0xc944e90c64b2c07662a292be6244bdf05cda44a7",
+  name: "The Graph",
+  symbol: "GRT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6719.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6719",
+  type: "token",
+});
