e2f28da9a3c91788cf8723618846ac058fa20c94
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:01 -0800

    feat(token-map): Update Token -- 1-0xb753428af26e81097e7fd17f40c88aaa3e04902c -- saffron.finance (SFI) (0xb753...902c)
    Token Details:
    - Id: 1-0xb753428af26e81097e7fd17f40c88aaa3e04902c
    - Chain Id: 1
    - Contract Address: 0xb753428af26e81097e7fd17f40c88aaa3e04902c
    - Name: saffron.finance
    - Symbol: SFI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7617.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7617

---
+++ sdk/constants/token-map/1/definitions/1-0xb753428af26e81097e7fd17f40c88aaa3e04902c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb753428af26e81097e7fd17f40c88aaa3e04902c",
+  chain_id: 1,
+  contract_address: "0xb753428af26e81097e7fd17f40c88aaa3e04902c",
+  name: "saffron.finance",
+  symbol: "SFI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7617.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7617",
+  type: "token",
+});
