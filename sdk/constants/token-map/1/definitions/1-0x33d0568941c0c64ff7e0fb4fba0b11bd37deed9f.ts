fb9f88af36df4d93eb1e58147e74c730824ad9a4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:14 -0800

    feat(token-map): Update Token -- 1-0x33d0568941c0c64ff7e0fb4fba0b11bd37deed9f -- RAMP (RAMP) (0x33d0...ed9f)
    Token Details:
    - Id: 1-0x33d0568941c0c64ff7e0fb4fba0b11bd37deed9f
    - Chain Id: 1
    - Contract Address: 0x33d0568941c0c64ff7e0fb4fba0b11bd37deed9f
    - Name: RAMP
    - Symbol: RAMP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7463.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7463

---
+++ sdk/constants/token-map/1/definitions/1-0x33d0568941c0c64ff7e0fb4fba0b11bd37deed9f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x33d0568941c0c64ff7e0fb4fba0b11bd37deed9f",
+  chain_id: 1,
+  contract_address: "0x33d0568941c0c64ff7e0fb4fba0b11bd37deed9f",
+  name: "RAMP",
+  symbol: "RAMP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7463.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7463",
+  type: "token",
+});
