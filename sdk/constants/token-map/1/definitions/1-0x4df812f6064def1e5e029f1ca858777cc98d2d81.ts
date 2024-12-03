9450551fcaca7151e014b85563aa7df598949053
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:10 -0800

    feat(token-map): Update Token -- 1-0x4df812f6064def1e5e029f1ca858777cc98d2d81 -- Xaurum (XAUR) (0x4df8...2d81)
    Token Details:
    - Id: 1-0x4df812f6064def1e5e029f1ca858777cc98d2d81
    - Chain Id: 1
    - Contract Address: 0x4df812f6064def1e5e029f1ca858777cc98d2d81
    - Name: Xaurum
    - Symbol: XAUR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/895.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 895

---
+++ sdk/constants/token-map/1/definitions/1-0x4df812f6064def1e5e029f1ca858777cc98d2d81.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4df812f6064def1e5e029f1ca858777cc98d2d81",
+  chain_id: 1,
+  contract_address: "0x4df812f6064def1e5e029f1ca858777cc98d2d81",
+  name: "Xaurum",
+  symbol: "XAUR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/895.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "895",
+  type: "token",
+});
