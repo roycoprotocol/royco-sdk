3f4c78a3ad77916f47cd23b2d3a755592c184d32
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:09 -0800

    feat(token-map): Update Token -- 1-0x55296f69f40ea6d20e478533c15a6b08b654e758 -- XYO (XYO) (0x5529...e758)
    Token Details:
    - Id: 1-0x55296f69f40ea6d20e478533c15a6b08b654e758
    - Chain Id: 1
    - Contract Address: 0x55296f69f40ea6d20e478533c15a6b08b654e758
    - Name: XYO
    - Symbol: XYO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2765.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2765

---
+++ sdk/constants/token-map/1/definitions/1-0x55296f69f40ea6d20e478533c15a6b08b654e758.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x55296f69f40ea6d20e478533c15a6b08b654e758",
+  chain_id: 1,
+  contract_address: "0x55296f69f40ea6d20e478533c15a6b08b654e758",
+  name: "XYO",
+  symbol: "XYO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2765.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2765",
+  type: "token",
+});
