3c69fdc6c066cf0d154587c970e219edeb3e4d44
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:20 -0800

    feat(token-map): Update Token -- 1-0x4385328cc4d643ca98dfea734360c0f596c83449 -- tomi (TOMI) (0x4385...3449)
    Token Details:
    - Id: 1-0x4385328cc4d643ca98dfea734360c0f596c83449
    - Chain Id: 1
    - Contract Address: 0x4385328cc4d643ca98dfea734360c0f596c83449
    - Name: tomi
    - Symbol: TOMI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/23246.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 23246

---
+++ sdk/constants/token-map/1/definitions/1-0x4385328cc4d643ca98dfea734360c0f596c83449.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4385328cc4d643ca98dfea734360c0f596c83449",
+  chain_id: 1,
+  contract_address: "0x4385328cc4d643ca98dfea734360c0f596c83449",
+  name: "tomi",
+  symbol: "TOMI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/23246.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "23246",
+  type: "token",
+});
