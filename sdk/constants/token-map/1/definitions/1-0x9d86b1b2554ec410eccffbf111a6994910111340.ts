7a79de3a3083a8e983b1a9146423c6d73fa5b6a3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:55 -0800

    feat(token-map): Update Token -- 1-0x9d86b1b2554ec410eccffbf111a6994910111340 -- Open Platform (OPEN) (0x9d86...1340)
    Token Details:
    - Id: 1-0x9d86b1b2554ec410eccffbf111a6994910111340
    - Chain Id: 1
    - Contract Address: 0x9d86b1b2554ec410eccffbf111a6994910111340
    - Name: Open Platform
    - Symbol: OPEN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2762.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2762

---
+++ sdk/constants/token-map/1/definitions/1-0x9d86b1b2554ec410eccffbf111a6994910111340.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9d86b1b2554ec410eccffbf111a6994910111340",
+  chain_id: 1,
+  contract_address: "0x9d86b1b2554ec410eccffbf111a6994910111340",
+  name: "Open Platform",
+  symbol: "OPEN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2762.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2762",
+  type: "token",
+});
