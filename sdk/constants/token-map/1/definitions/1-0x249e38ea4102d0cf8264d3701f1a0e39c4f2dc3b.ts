b2fbb9823eb6d583c1f3adb0d194032045233501
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:03:33 -0800

    feat(token-map): Update Token -- 1-0x249e38ea4102d0cf8264d3701f1a0e39c4f2dc3b -- UFO Gaming (UFO) (0x249e...dc3b)
    Token Details:
    - Id: 1-0x249e38ea4102d0cf8264d3701f1a0e39c4f2dc3b
    - Chain Id: 1
    - Contract Address: 0x249e38ea4102d0cf8264d3701f1a0e39c4f2dc3b
    - Name: UFO Gaming
    - Symbol: UFO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10729.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10729

---
+++ sdk/constants/token-map/1/definitions/1-0x249e38ea4102d0cf8264d3701f1a0e39c4f2dc3b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x249e38ea4102d0cf8264d3701f1a0e39c4f2dc3b",
+  chain_id: 1,
+  contract_address: "0x249e38ea4102d0cf8264d3701f1a0e39c4f2dc3b",
+  name: "UFO Gaming",
+  symbol: "UFO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10729.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10729",
+  type: "token",
+});
