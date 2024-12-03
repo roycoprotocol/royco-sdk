79e3c8fd666d80ca5ad0e4e982f953288a2c0453
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:09 -0800

    feat(token-map): Update Token -- 1-0xf920e4f3fbef5b3ad0a25017514b769bdc4ac135 -- BABB (BAX) (0xf920...c135)
    Token Details:
    - Id: 1-0xf920e4f3fbef5b3ad0a25017514b769bdc4ac135
    - Chain Id: 1
    - Contract Address: 0xf920e4f3fbef5b3ad0a25017514b769bdc4ac135
    - Name: BABB
    - Symbol: BAX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2572.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2572

---
+++ sdk/constants/token-map/1/definitions/1-0xf920e4f3fbef5b3ad0a25017514b769bdc4ac135.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf920e4f3fbef5b3ad0a25017514b769bdc4ac135",
+  chain_id: 1,
+  contract_address: "0xf920e4f3fbef5b3ad0a25017514b769bdc4ac135",
+  name: "BABB",
+  symbol: "BAX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2572.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2572",
+  type: "token",
+});
