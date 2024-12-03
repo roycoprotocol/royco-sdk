9c10686e30cff699cb84745178b51182afb82822
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:41 -0800

    feat(token-map): Update Token -- 1-0x3adfc4999f77d04c8341bac5f3a76f58dff5b37a -- Privatix (PRIX) (0x3adf...b37a)
    Token Details:
    - Id: 1-0x3adfc4999f77d04c8341bac5f3a76f58dff5b37a
    - Chain Id: 1
    - Contract Address: 0x3adfc4999f77d04c8341bac5f3a76f58dff5b37a
    - Name: Privatix
    - Symbol: PRIX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2184.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2184

---
+++ sdk/constants/token-map/1/definitions/1-0x3adfc4999f77d04c8341bac5f3a76f58dff5b37a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3adfc4999f77d04c8341bac5f3a76f58dff5b37a",
+  chain_id: 1,
+  contract_address: "0x3adfc4999f77d04c8341bac5f3a76f58dff5b37a",
+  name: "Privatix",
+  symbol: "PRIX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2184.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2184",
+  type: "token",
+});
