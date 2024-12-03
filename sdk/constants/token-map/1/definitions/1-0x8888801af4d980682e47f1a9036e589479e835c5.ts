6dc7c0e2acebb1b1f45c695ceeddb6b5d1114e30
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:22 -0800

    feat(token-map): Update Token -- 1-0x8888801af4d980682e47f1a9036e589479e835c5 -- 88mph (MPH) (0x8888...35c5)
    Token Details:
    - Id: 1-0x8888801af4d980682e47f1a9036e589479e835c5
    - Chain Id: 1
    - Contract Address: 0x8888801af4d980682e47f1a9036e589479e835c5
    - Name: 88mph
    - Symbol: MPH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7742.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7742

---
+++ sdk/constants/token-map/1/definitions/1-0x8888801af4d980682e47f1a9036e589479e835c5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8888801af4d980682e47f1a9036e589479e835c5",
+  chain_id: 1,
+  contract_address: "0x8888801af4d980682e47f1a9036e589479e835c5",
+  name: "88mph",
+  symbol: "MPH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7742.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7742",
+  type: "token",
+});
