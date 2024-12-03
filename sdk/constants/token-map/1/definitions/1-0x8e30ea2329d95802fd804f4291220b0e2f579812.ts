98171fc634286a4321b3fbeed3dff42616083f1f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:24 -0800

    feat(token-map): Update Token -- 1-0x8e30ea2329d95802fd804f4291220b0e2f579812 -- Decentralized Vulnerability Platform (DVP) (0x8e30...9812)
    Token Details:
    - Id: 1-0x8e30ea2329d95802fd804f4291220b0e2f579812
    - Chain Id: 1
    - Contract Address: 0x8e30ea2329d95802fd804f4291220b0e2f579812
    - Name: Decentralized Vulnerability Platform
    - Symbol: DVP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4520.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4520

---
+++ sdk/constants/token-map/1/definitions/1-0x8e30ea2329d95802fd804f4291220b0e2f579812.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8e30ea2329d95802fd804f4291220b0e2f579812",
+  chain_id: 1,
+  contract_address: "0x8e30ea2329d95802fd804f4291220b0e2f579812",
+  name: "Decentralized Vulnerability Platform",
+  symbol: "DVP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4520.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4520",
+  type: "token",
+});
