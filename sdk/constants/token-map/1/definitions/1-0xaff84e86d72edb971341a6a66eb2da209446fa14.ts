12f38a1434921606359b74d269cfa2c3197eb985
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:02 -0800

    feat(token-map): Update Token -- 1-0xaff84e86d72edb971341a6a66eb2da209446fa14 -- The Currency Analytics (TCAT) (0xaff8...fa14)
    Token Details:
    - Id: 1-0xaff84e86d72edb971341a6a66eb2da209446fa14
    - Chain Id: 1
    - Contract Address: 0xaff84e86d72edb971341a6a66eb2da209446fa14
    - Name: The Currency Analytics
    - Symbol: TCAT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3730.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3730

---
+++ sdk/constants/token-map/1/definitions/1-0xaff84e86d72edb971341a6a66eb2da209446fa14.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xaff84e86d72edb971341a6a66eb2da209446fa14",
+  chain_id: 1,
+  contract_address: "0xaff84e86d72edb971341a6a66eb2da209446fa14",
+  name: "The Currency Analytics",
+  symbol: "TCAT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3730.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3730",
+  type: "token",
+});
