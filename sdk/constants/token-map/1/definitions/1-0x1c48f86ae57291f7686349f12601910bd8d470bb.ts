c43316d53a05bba5cb0a2dae0ec02690ad0f2737
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:19 -0800

    feat(token-map): Update Token -- 1-0x1c48f86ae57291f7686349f12601910bd8d470bb -- USDK (USDK) (0x1c48...70bb)
    Token Details:
    - Id: 1-0x1c48f86ae57291f7686349f12601910bd8d470bb
    - Chain Id: 1
    - Contract Address: 0x1c48f86ae57291f7686349f12601910bd8d470bb
    - Name: USDK
    - Symbol: USDK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4064.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4064

---
+++ sdk/constants/token-map/1/definitions/1-0x1c48f86ae57291f7686349f12601910bd8d470bb.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1c48f86ae57291f7686349f12601910bd8d470bb",
+  chain_id: 1,
+  contract_address: "0x1c48f86ae57291f7686349f12601910bd8d470bb",
+  name: "USDK",
+  symbol: "USDK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4064.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4064",
+  type: "token",
+});
