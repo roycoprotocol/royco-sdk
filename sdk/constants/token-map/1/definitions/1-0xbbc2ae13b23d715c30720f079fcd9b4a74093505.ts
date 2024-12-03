50017812e3f150ab1021d03b6c19a9e428a58f06
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:53 -0800

    feat(token-map): Update Token -- 1-0xbbc2ae13b23d715c30720f079fcd9b4a74093505 -- Ethernity Chain (ERN) (0xbbc2...3505)
    Token Details:
    - Id: 1-0xbbc2ae13b23d715c30720f079fcd9b4a74093505
    - Chain Id: 1
    - Contract Address: 0xbbc2ae13b23d715c30720f079fcd9b4a74093505
    - Name: Ethernity Chain
    - Symbol: ERN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8615.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8615

---
+++ sdk/constants/token-map/1/definitions/1-0xbbc2ae13b23d715c30720f079fcd9b4a74093505.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbbc2ae13b23d715c30720f079fcd9b4a74093505",
+  chain_id: 1,
+  contract_address: "0xbbc2ae13b23d715c30720f079fcd9b4a74093505",
+  name: "Ethernity Chain",
+  symbol: "ERN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8615.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8615",
+  type: "token",
+});
