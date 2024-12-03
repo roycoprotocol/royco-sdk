a554a66e8efae5998a33fa568e97ad0254047e7d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:28 -0800

    feat(token-map): Update Token -- 1-0x0954906da0bf32d5479e25f46056d22f08464cab -- Index Cooperative (INDEX) (0x0954...4cab)
    Token Details:
    - Id: 1-0x0954906da0bf32d5479e25f46056d22f08464cab
    - Chain Id: 1
    - Contract Address: 0x0954906da0bf32d5479e25f46056d22f08464cab
    - Name: Index Cooperative
    - Symbol: INDEX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7336.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7336

---
+++ sdk/constants/token-map/1/definitions/1-0x0954906da0bf32d5479e25f46056d22f08464cab.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0954906da0bf32d5479e25f46056d22f08464cab",
+  chain_id: 1,
+  contract_address: "0x0954906da0bf32d5479e25f46056d22f08464cab",
+  name: "Index Cooperative",
+  symbol: "INDEX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7336.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7336",
+  type: "token",
+});
