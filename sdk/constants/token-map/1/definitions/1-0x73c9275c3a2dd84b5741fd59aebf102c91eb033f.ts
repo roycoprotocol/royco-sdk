e57bb888a494829bfd9ddee095e8763e71f206a8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:05 -0800

    feat(token-map): Update Token -- 1-0x73c9275c3a2dd84b5741fd59aebf102c91eb033f -- Bitball Treasure (BTRS) (0x73c9...033f)
    Token Details:
    - Id: 1-0x73c9275c3a2dd84b5741fd59aebf102c91eb033f
    - Chain Id: 1
    - Contract Address: 0x73c9275c3a2dd84b5741fd59aebf102c91eb033f
    - Name: Bitball Treasure
    - Symbol: BTRS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4257.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4257

---
+++ sdk/constants/token-map/1/definitions/1-0x73c9275c3a2dd84b5741fd59aebf102c91eb033f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x73c9275c3a2dd84b5741fd59aebf102c91eb033f",
+  chain_id: 1,
+  contract_address: "0x73c9275c3a2dd84b5741fd59aebf102c91eb033f",
+  name: "Bitball Treasure",
+  symbol: "BTRS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4257.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4257",
+  type: "token",
+});
