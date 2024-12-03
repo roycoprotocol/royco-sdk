2c63b1b0d4b5139eebb0079a346d9e85f8c32749
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:46 -0800

    feat(token-map): Update Token -- 1-0x4bd70556ae3f8a6ec6c4080a0c327b24325438f3 -- HXRO (HXRO) (0x4bd7...38f3)
    Token Details:
    - Id: 1-0x4bd70556ae3f8a6ec6c4080a0c327b24325438f3
    - Chain Id: 1
    - Contract Address: 0x4bd70556ae3f8a6ec6c4080a0c327b24325438f3
    - Name: HXRO
    - Symbol: HXRO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3748.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3748

---
+++ sdk/constants/token-map/1/definitions/1-0x4bd70556ae3f8a6ec6c4080a0c327b24325438f3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4bd70556ae3f8a6ec6c4080a0c327b24325438f3",
+  chain_id: 1,
+  contract_address: "0x4bd70556ae3f8a6ec6c4080a0c327b24325438f3",
+  name: "HXRO",
+  symbol: "HXRO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3748.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3748",
+  type: "token",
+});
