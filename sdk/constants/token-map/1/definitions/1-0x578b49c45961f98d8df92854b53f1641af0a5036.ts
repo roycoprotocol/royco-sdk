86fda8c05d8e24fd6da8412046b03dc2ef4bb403
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:36 -0800

    feat(token-map): Update Token -- 1-0x578b49c45961f98d8df92854b53f1641af0a5036 -- LINKA (LINKA) (0x578b...5036)
    Token Details:
    - Id: 1-0x578b49c45961f98d8df92854b53f1641af0a5036
    - Chain Id: 1
    - Contract Address: 0x578b49c45961f98d8df92854b53f1641af0a5036
    - Name: LINKA
    - Symbol: LINKA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4850.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4850

---
+++ sdk/constants/token-map/1/definitions/1-0x578b49c45961f98d8df92854b53f1641af0a5036.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x578b49c45961f98d8df92854b53f1641af0a5036",
+  chain_id: 1,
+  contract_address: "0x578b49c45961f98d8df92854b53f1641af0a5036",
+  name: "LINKA",
+  symbol: "LINKA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4850.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4850",
+  type: "token",
+});
