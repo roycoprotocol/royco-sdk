5b8a8b21c3158aa9938430e7422678c2dec65143
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:59 -0800

    feat(token-map): Update Token -- 1-0xe530441f4f73bdb6dc2fa5af7c3fc5fd551ec838 -- GSENetwork (GSE) (0xe530...c838)
    Token Details:
    - Id: 1-0xe530441f4f73bdb6dc2fa5af7c3fc5fd551ec838
    - Chain Id: 1
    - Contract Address: 0xe530441f4f73bdb6dc2fa5af7c3fc5fd551ec838
    - Name: GSENetwork
    - Symbol: GSE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3123.png
    - Decimals: 4
    - Source: coinmarketcap
    - Search Id: 3123

---
+++ sdk/constants/token-map/1/definitions/1-0xe530441f4f73bdb6dc2fa5af7c3fc5fd551ec838.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe530441f4f73bdb6dc2fa5af7c3fc5fd551ec838",
+  chain_id: 1,
+  contract_address: "0xe530441f4f73bdb6dc2fa5af7c3fc5fd551ec838",
+  name: "GSENetwork",
+  symbol: "GSE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3123.png",
+  decimals: 4,
+  source: "coinmarketcap",
+  search_id: "3123",
+  type: "token",
+});
