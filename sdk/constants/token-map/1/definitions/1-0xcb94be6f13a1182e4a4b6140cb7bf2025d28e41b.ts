8ded3e8130791f3990a53cbf4cb461f2ebf934f7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:14 -0800

    feat(token-map): Update Token -- 1-0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b -- WeTrust (TRST) (0xcb94...e41b)
    Token Details:
    - Id: 1-0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b
    - Chain Id: 1
    - Contract Address: 0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b
    - Name: WeTrust
    - Symbol: TRST
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1638.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 1638

---
+++ sdk/constants/token-map/1/definitions/1-0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b",
+  chain_id: 1,
+  contract_address: "0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b",
+  name: "WeTrust",
+  symbol: "TRST",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1638.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "1638",
+  type: "token",
+});
