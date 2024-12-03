526ae66ed0fd38537587e0fa8448509abbc7e026
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:16 -0800

    feat(token-map): Update Token -- 1-0xdf574c24545e5ffecb9a659c229253d4111d87e1 -- HUSD (HUSD) (0xdf57...87e1)
    Token Details:
    - Id: 1-0xdf574c24545e5ffecb9a659c229253d4111d87e1
    - Chain Id: 1
    - Contract Address: 0xdf574c24545e5ffecb9a659c229253d4111d87e1
    - Name: HUSD
    - Symbol: HUSD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4779.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 4779

---
+++ sdk/constants/token-map/1/definitions/1-0xdf574c24545e5ffecb9a659c229253d4111d87e1.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdf574c24545e5ffecb9a659c229253d4111d87e1",
+  chain_id: 1,
+  contract_address: "0xdf574c24545e5ffecb9a659c229253d4111d87e1",
+  name: "HUSD",
+  symbol: "HUSD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4779.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "4779",
+  type: "token",
+});
