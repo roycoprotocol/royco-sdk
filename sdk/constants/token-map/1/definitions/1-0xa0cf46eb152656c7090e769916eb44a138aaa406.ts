caeb79702b2f819899444bf5ed5c27a6de46374a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:26 -0800

    feat(token-map): Update Token -- 1-0xa0cf46eb152656c7090e769916eb44a138aaa406 -- Spheroid Universe (SPH) (0xa0cf...a406)
    Token Details:
    - Id: 1-0xa0cf46eb152656c7090e769916eb44a138aaa406
    - Chain Id: 1
    - Contract Address: 0xa0cf46eb152656c7090e769916eb44a138aaa406
    - Name: Spheroid Universe
    - Symbol: SPH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6209.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6209

---
+++ sdk/constants/token-map/1/definitions/1-0xa0cf46eb152656c7090e769916eb44a138aaa406.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa0cf46eb152656c7090e769916eb44a138aaa406",
+  chain_id: 1,
+  contract_address: "0xa0cf46eb152656c7090e769916eb44a138aaa406",
+  name: "Spheroid Universe",
+  symbol: "SPH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6209.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6209",
+  type: "token",
+});
