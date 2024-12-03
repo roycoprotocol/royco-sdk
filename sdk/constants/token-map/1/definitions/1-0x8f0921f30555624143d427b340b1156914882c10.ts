da9f962d98f0715d737a319baeda7ff505c53c6a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:44 -0800

    feat(token-map): Update Token -- 1-0x8f0921f30555624143d427b340b1156914882c10 -- FlypMe (FYP) (0x8f09...2c10)
    Token Details:
    - Id: 1-0x8f0921f30555624143d427b340b1156914882c10
    - Chain Id: 1
    - Contract Address: 0x8f0921f30555624143d427b340b1156914882c10
    - Name: FlypMe
    - Symbol: FYP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2126.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2126

---
+++ sdk/constants/token-map/1/definitions/1-0x8f0921f30555624143d427b340b1156914882c10.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8f0921f30555624143d427b340b1156914882c10",
+  chain_id: 1,
+  contract_address: "0x8f0921f30555624143d427b340b1156914882c10",
+  name: "FlypMe",
+  symbol: "FYP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2126.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2126",
+  type: "token",
+});
