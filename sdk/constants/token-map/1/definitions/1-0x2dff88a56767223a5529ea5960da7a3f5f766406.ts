52d75e1b649653a528ba298756aebcecbfe8c3e3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:21 -0800

    feat(token-map): Update Token -- 1-0x2dff88a56767223a5529ea5960da7a3f5f766406 -- SPACE ID (ID) (0x2dff...6406)
    Token Details:
    - Id: 1-0x2dff88a56767223a5529ea5960da7a3f5f766406
    - Chain Id: 1
    - Contract Address: 0x2dff88a56767223a5529ea5960da7a3f5f766406
    - Name: SPACE ID
    - Symbol: ID
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/21846.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 21846

---
+++ sdk/constants/token-map/1/definitions/1-0x2dff88a56767223a5529ea5960da7a3f5f766406.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2dff88a56767223a5529ea5960da7a3f5f766406",
+  chain_id: 1,
+  contract_address: "0x2dff88a56767223a5529ea5960da7a3f5f766406",
+  name: "SPACE ID",
+  symbol: "ID",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/21846.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "21846",
+  type: "token",
+});
