6b173c7ed0b080dc14084fb340cda887e942bb9f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:04 -0800

    feat(token-map): Update Token -- 42161-0x3bd2dfd03bc7c3011ed7fb8c4d0949b382726cee -- Roobee (ROOBEE) (0x3bd2...6cee)
    Token Details:
    - Id: 42161-0x3bd2dfd03bc7c3011ed7fb8c4d0949b382726cee
    - Chain Id: 42161
    - Contract Address: 0x3bd2dfd03bc7c3011ed7fb8c4d0949b382726cee
    - Name: Roobee
    - Symbol: ROOBEE
    - Image: https://coin-images.coingecko.com/coins/images/8791/large/Group_11.png?1696508946
    - Decimals: 18
    - Source: coingecko
    - Search Id: roobee

---
+++ sdk/constants/token-map/42161/definitions/42161-0x3bd2dfd03bc7c3011ed7fb8c4d0949b382726cee.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x3bd2dfd03bc7c3011ed7fb8c4d0949b382726cee",
+  chain_id: 42161,
+  contract_address: "0x3bd2dfd03bc7c3011ed7fb8c4d0949b382726cee",
+  name: "Roobee",
+  symbol: "ROOBEE",
+  image: "https://coin-images.coingecko.com/coins/images/8791/large/Group_11.png?1696508946",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "roobee",
+  type: "token",
+});
