917a83f06f3fd2d1593f3b9f9eb2a447f648db4c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:17:44 -0800

    feat(token-map): Update Token -- 1-0x72b886d09c117654ab7da13a14d603001de0b777 -- XDEFI (XDEFI) (0x72b8...b777)
    Token Details:
    - Id: 1-0x72b886d09c117654ab7da13a14d603001de0b777
    - Chain Id: 1
    - Contract Address: 0x72b886d09c117654ab7da13a14d603001de0b777
    - Name: XDEFI
    - Symbol: XDEFI
    - Image: https://coin-images.coingecko.com/coins/images/19524/large/xdefi.jpg?1723436519
    - Decimals: 18
    - Source: coingecko
    - Search Id: xdefi

---
+++ sdk/constants/token-map/1/definitions/1-0x72b886d09c117654ab7da13a14d603001de0b777.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x72b886d09c117654ab7da13a14d603001de0b777",
+  chain_id: 1,
+  contract_address: "0x72b886d09c117654ab7da13a14d603001de0b777",
+  name: "XDEFI",
+  symbol: "XDEFI",
+  image: "https://coin-images.coingecko.com/coins/images/19524/large/xdefi.jpg?1723436519",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "xdefi",
+  type: "token",
+});
