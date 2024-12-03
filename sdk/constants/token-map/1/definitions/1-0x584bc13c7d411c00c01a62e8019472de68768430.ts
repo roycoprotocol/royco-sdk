91f823d97db0774d728f8b70c811192fbd8b750b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:51 -0800

    feat(token-map): Update Token -- 1-0x584bc13c7d411c00c01a62e8019472de68768430 -- Hegic (HEGIC) (0x584b...8430)
    Token Details:
    - Id: 1-0x584bc13c7d411c00c01a62e8019472de68768430
    - Chain Id: 1
    - Contract Address: 0x584bc13c7d411c00c01a62e8019472de68768430
    - Name: Hegic
    - Symbol: HEGIC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6929.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6929

---
+++ sdk/constants/token-map/1/definitions/1-0x584bc13c7d411c00c01a62e8019472de68768430.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x584bc13c7d411c00c01a62e8019472de68768430",
+  chain_id: 1,
+  contract_address: "0x584bc13c7d411c00c01a62e8019472de68768430",
+  name: "Hegic",
+  symbol: "HEGIC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6929.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6929",
+  type: "token",
+});
