036d8a30f3e12720d9340d91475ca0bffa4a7adf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:33 -0800

    feat(token-map): Update Token -- 1-0xa117000000f279d81a1d3cc75430faa017fa5a2e -- Aragon (ANT) (0xa117...5a2e)
    Token Details:
    - Id: 1-0xa117000000f279d81a1d3cc75430faa017fa5a2e
    - Chain Id: 1
    - Contract Address: 0xa117000000f279d81a1d3cc75430faa017fa5a2e
    - Name: Aragon
    - Symbol: ANT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1680.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1680

---
+++ sdk/constants/token-map/1/definitions/1-0xa117000000f279d81a1d3cc75430faa017fa5a2e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa117000000f279d81a1d3cc75430faa017fa5a2e",
+  chain_id: 1,
+  contract_address: "0xa117000000f279d81a1d3cc75430faa017fa5a2e",
+  name: "Aragon",
+  symbol: "ANT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1680.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1680",
+  type: "token",
+});
