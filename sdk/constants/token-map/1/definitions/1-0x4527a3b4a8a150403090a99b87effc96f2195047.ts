a1b1133fd725a4a82e50f21cc53193a95bf69e88
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:34 -0800

    feat(token-map): Update Token -- 1-0x4527a3b4a8a150403090a99b87effc96f2195047 -- P2P Solutions foundation (P2PS) (0x4527...5047)
    Token Details:
    - Id: 1-0x4527a3b4a8a150403090a99b87effc96f2195047
    - Chain Id: 1
    - Contract Address: 0x4527a3b4a8a150403090a99b87effc96f2195047
    - Name: P2P Solutions foundation
    - Symbol: P2PS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/13887.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 13887

---
+++ sdk/constants/token-map/1/definitions/1-0x4527a3b4a8a150403090a99b87effc96f2195047.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4527a3b4a8a150403090a99b87effc96f2195047",
+  chain_id: 1,
+  contract_address: "0x4527a3b4a8a150403090a99b87effc96f2195047",
+  name: "P2P Solutions foundation",
+  symbol: "P2PS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/13887.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "13887",
+  type: "token",
+});
