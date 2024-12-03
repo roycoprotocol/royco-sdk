4522ad6aa311487d6362b8e837ed7141742a2d95
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:36:50 -0800

    feat(token-map): Update Token -- 1-0xec213f83defb583af3a000b1c0ada660b1902a0f -- Presearch (PRE) (0xec21...2a0f)
    Token Details:
    - Id: 1-0xec213f83defb583af3a000b1c0ada660b1902a0f
    - Chain Id: 1
    - Contract Address: 0xec213f83defb583af3a000b1c0ada660b1902a0f
    - Name: Presearch
    - Symbol: PRE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2245.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2245

---
+++ sdk/constants/token-map/1/definitions/1-0xec213f83defb583af3a000b1c0ada660b1902a0f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xec213f83defb583af3a000b1c0ada660b1902a0f",
+  chain_id: 1,
+  contract_address: "0xec213f83defb583af3a000b1c0ada660b1902a0f",
+  name: "Presearch",
+  symbol: "PRE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2245.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2245",
+  type: "token",
+});
