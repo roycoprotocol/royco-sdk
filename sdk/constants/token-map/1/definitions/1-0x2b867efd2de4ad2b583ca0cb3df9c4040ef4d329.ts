61753b9a1e3ff3c17cd5d21978edaecafb3e8553
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:58 -0800

    feat(token-map): Update Token -- 1-0x2b867efd2de4ad2b583ca0cb3df9c4040ef4d329 -- Lucky Block v2 (LBLOCK) (0x2b86...d329)
    Token Details:
    - Id: 1-0x2b867efd2de4ad2b583ca0cb3df9c4040ef4d329
    - Chain Id: 1
    - Contract Address: 0x2b867efd2de4ad2b583ca0cb3df9c4040ef4d329
    - Name: Lucky Block v2
    - Symbol: LBLOCK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/21416.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 21416

---
+++ sdk/constants/token-map/1/definitions/1-0x2b867efd2de4ad2b583ca0cb3df9c4040ef4d329.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2b867efd2de4ad2b583ca0cb3df9c4040ef4d329",
+  chain_id: 1,
+  contract_address: "0x2b867efd2de4ad2b583ca0cb3df9c4040ef4d329",
+  name: "Lucky Block v2",
+  symbol: "LBLOCK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/21416.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "21416",
+  type: "token",
+});
