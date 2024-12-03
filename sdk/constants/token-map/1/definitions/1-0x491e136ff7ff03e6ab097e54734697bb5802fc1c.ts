c9892ce011154ce1ddd02162579dca16f4f898c6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:16 -0800

    feat(token-map): Update Token -- 1-0x491e136ff7ff03e6ab097e54734697bb5802fc1c -- Kattana (KTN) (0x491e...fc1c)
    Token Details:
    - Id: 1-0x491e136ff7ff03e6ab097e54734697bb5802fc1c
    - Chain Id: 1
    - Contract Address: 0x491e136ff7ff03e6ab097e54734697bb5802fc1c
    - Name: Kattana
    - Symbol: KTN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9110.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9110

---
+++ sdk/constants/token-map/1/definitions/1-0x491e136ff7ff03e6ab097e54734697bb5802fc1c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x491e136ff7ff03e6ab097e54734697bb5802fc1c",
+  chain_id: 1,
+  contract_address: "0x491e136ff7ff03e6ab097e54734697bb5802fc1c",
+  name: "Kattana",
+  symbol: "KTN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9110.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9110",
+  type: "token",
+});
