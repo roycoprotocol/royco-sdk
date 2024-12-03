38f726b6359502c8493c9a9f1417f096505306c7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:41 -0800

    feat(token-map): Update Token -- 1-0x57ab1ec28d129707052df4df418d58a2d46d5f51 -- sUSD (SUSD) (0x57ab...5f51)
    Token Details:
    - Id: 1-0x57ab1ec28d129707052df4df418d58a2d46d5f51
    - Chain Id: 1
    - Contract Address: 0x57ab1ec28d129707052df4df418d58a2d46d5f51
    - Name: sUSD
    - Symbol: SUSD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2927.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2927

---
+++ sdk/constants/token-map/1/definitions/1-0x57ab1ec28d129707052df4df418d58a2d46d5f51.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x57ab1ec28d129707052df4df418d58a2d46d5f51",
+  chain_id: 1,
+  contract_address: "0x57ab1ec28d129707052df4df418d58a2d46d5f51",
+  name: "sUSD",
+  symbol: "SUSD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2927.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2927",
+  type: "token",
+});
