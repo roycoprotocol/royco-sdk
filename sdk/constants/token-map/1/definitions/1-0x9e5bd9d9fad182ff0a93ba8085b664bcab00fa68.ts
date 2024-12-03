b3a6c6226e4876f19ead064116f8ed64d3424cff
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:06 -0800

    feat(token-map): Update Token -- 1-0x9e5bd9d9fad182ff0a93ba8085b664bcab00fa68 -- Dinger Token (DINGER) (0x9e5b...fa68)
    Token Details:
    - Id: 1-0x9e5bd9d9fad182ff0a93ba8085b664bcab00fa68
    - Chain Id: 1
    - Contract Address: 0x9e5bd9d9fad182ff0a93ba8085b664bcab00fa68
    - Name: Dinger Token
    - Symbol: DINGER
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/13352.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 13352

---
+++ sdk/constants/token-map/1/definitions/1-0x9e5bd9d9fad182ff0a93ba8085b664bcab00fa68.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9e5bd9d9fad182ff0a93ba8085b664bcab00fa68",
+  chain_id: 1,
+  contract_address: "0x9e5bd9d9fad182ff0a93ba8085b664bcab00fa68",
+  name: "Dinger Token",
+  symbol: "DINGER",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/13352.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "13352",
+  type: "token",
+});
