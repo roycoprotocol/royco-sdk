37bbc332698fe8c84cb1683fc628bdb2e2444a94
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:53 -0800

    feat(token-map): Update Token -- 1-0xd5f788ca0de8f17cbde1d1e35aa8f005a87fa00b -- Shivers (SHVR) (0xd5f7...a00b)
    Token Details:
    - Id: 1-0xd5f788ca0de8f17cbde1d1e35aa8f005a87fa00b
    - Chain Id: 1
    - Contract Address: 0xd5f788ca0de8f17cbde1d1e35aa8f005a87fa00b
    - Name: Shivers
    - Symbol: SHVR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3645.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 3645

---
+++ sdk/constants/token-map/1/definitions/1-0xd5f788ca0de8f17cbde1d1e35aa8f005a87fa00b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd5f788ca0de8f17cbde1d1e35aa8f005a87fa00b",
+  chain_id: 1,
+  contract_address: "0xd5f788ca0de8f17cbde1d1e35aa8f005a87fa00b",
+  name: "Shivers",
+  symbol: "SHVR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3645.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3645",
+  type: "token",
+});
