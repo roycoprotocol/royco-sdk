3dfb07f562d684dda4af108527d2624653cc69e1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:54 -0800

    feat(token-map): Update Token -- 1-0x1a4b46696b2bb4794eb3d4c26f1c55f9170fa4c5 -- BitDAO (BIT) (0x1a4b...a4c5)
    Token Details:
    - Id: 1-0x1a4b46696b2bb4794eb3d4c26f1c55f9170fa4c5
    - Chain Id: 1
    - Contract Address: 0x1a4b46696b2bb4794eb3d4c26f1c55f9170fa4c5
    - Name: BitDAO
    - Symbol: BIT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11221.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11221

---
+++ sdk/constants/token-map/1/definitions/1-0x1a4b46696b2bb4794eb3d4c26f1c55f9170fa4c5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1a4b46696b2bb4794eb3d4c26f1c55f9170fa4c5",
+  chain_id: 1,
+  contract_address: "0x1a4b46696b2bb4794eb3d4c26f1c55f9170fa4c5",
+  name: "BitDAO",
+  symbol: "BIT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11221.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11221",
+  type: "token",
+});
