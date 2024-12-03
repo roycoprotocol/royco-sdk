c5b3908ecff64af0309998532933636ef17b6402
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:09 -0800

    feat(token-map): Update Token -- 1-0x1b6e9c73bee68102d9dd4a2627f97bff4183ab0a -- OpenLeverage (OLE) (0x1b6e...ab0a)
    Token Details:
    - Id: 1-0x1b6e9c73bee68102d9dd4a2627f97bff4183ab0a
    - Chain Id: 1
    - Contract Address: 0x1b6e9c73bee68102d9dd4a2627f97bff4183ab0a
    - Name: OpenLeverage
    - Symbol: OLE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/20719.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 20719

---
+++ sdk/constants/token-map/1/definitions/1-0x1b6e9c73bee68102d9dd4a2627f97bff4183ab0a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1b6e9c73bee68102d9dd4a2627f97bff4183ab0a",
+  chain_id: 1,
+  contract_address: "0x1b6e9c73bee68102d9dd4a2627f97bff4183ab0a",
+  name: "OpenLeverage",
+  symbol: "OLE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/20719.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "20719",
+  type: "token",
+});
