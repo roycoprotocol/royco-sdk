f6ab060e7291985a636d3d044fccf0156057ae93
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:40 -0800

    feat(token-map): Update Token -- 1-0x0dcee5f694e492f0dd842a7fbe5bed4c6e4665a6 -- Catboy (CATBOY) (0x0dce...65a6)
    Token Details:
    - Id: 1-0x0dcee5f694e492f0dd842a7fbe5bed4c6e4665a6
    - Chain Id: 1
    - Contract Address: 0x0dcee5f694e492f0dd842a7fbe5bed4c6e4665a6
    - Name: Catboy
    - Symbol: CATBOY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28647.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 28647

---
+++ sdk/constants/token-map/1/definitions/1-0x0dcee5f694e492f0dd842a7fbe5bed4c6e4665a6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0dcee5f694e492f0dd842a7fbe5bed4c6e4665a6",
+  chain_id: 1,
+  contract_address: "0x0dcee5f694e492f0dd842a7fbe5bed4c6e4665a6",
+  name: "Catboy",
+  symbol: "CATBOY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28647.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "28647",
+  type: "token",
+});
