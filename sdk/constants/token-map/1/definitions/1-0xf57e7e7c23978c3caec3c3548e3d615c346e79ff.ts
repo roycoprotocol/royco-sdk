d10679ee1e7520862b8bbc0a0fa0bc9933eb99c2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:39 -0800

    feat(token-map): Update Token -- 1-0xf57e7e7c23978c3caec3c3548e3d615c346e79ff -- Immutable (IMX) (0xf57e...79ff)
    Token Details:
    - Id: 1-0xf57e7e7c23978c3caec3c3548e3d615c346e79ff
    - Chain Id: 1
    - Contract Address: 0xf57e7e7c23978c3caec3c3548e3d615c346e79ff
    - Name: Immutable
    - Symbol: IMX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10603.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10603

---
+++ sdk/constants/token-map/1/definitions/1-0xf57e7e7c23978c3caec3c3548e3d615c346e79ff.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf57e7e7c23978c3caec3c3548e3d615c346e79ff",
+  chain_id: 1,
+  contract_address: "0xf57e7e7c23978c3caec3c3548e3d615c346e79ff",
+  name: "Immutable",
+  symbol: "IMX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10603.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10603",
+  type: "token",
+});
