2f81c21a8c8536b2e996aa521191670d8933ac1e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:50 -0800

    feat(token-map): Update Token -- 1-0x8515cd0f00ad81996d24b9a9c35121a3b759d6cd -- Blockburn (BURN) (0x8515...d6cd)
    Token Details:
    - Id: 1-0x8515cd0f00ad81996d24b9a9c35121a3b759d6cd
    - Chain Id: 1
    - Contract Address: 0x8515cd0f00ad81996d24b9a9c35121a3b759d6cd
    - Name: Blockburn
    - Symbol: BURN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4069.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4069

---
+++ sdk/constants/token-map/1/definitions/1-0x8515cd0f00ad81996d24b9a9c35121a3b759d6cd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8515cd0f00ad81996d24b9a9c35121a3b759d6cd",
+  chain_id: 1,
+  contract_address: "0x8515cd0f00ad81996d24b9a9c35121a3b759d6cd",
+  name: "Blockburn",
+  symbol: "BURN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4069.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4069",
+  type: "token",
+});
