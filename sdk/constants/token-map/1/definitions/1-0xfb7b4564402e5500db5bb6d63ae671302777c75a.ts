16c531a4cb7db5620d938454e55e833ba8318e62
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:40 -0800

    feat(token-map): Update Token -- 1-0xfb7b4564402e5500db5bb6d63ae671302777c75a -- DEXTools (DEXT) (0xfb7b...c75a)
    Token Details:
    - Id: 1-0xfb7b4564402e5500db5bb6d63ae671302777c75a
    - Chain Id: 1
    - Contract Address: 0xfb7b4564402e5500db5bb6d63ae671302777c75a
    - Name: DEXTools
    - Symbol: DEXT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5866.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5866

---
+++ sdk/constants/token-map/1/definitions/1-0xfb7b4564402e5500db5bb6d63ae671302777c75a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfb7b4564402e5500db5bb6d63ae671302777c75a",
+  chain_id: 1,
+  contract_address: "0xfb7b4564402e5500db5bb6d63ae671302777c75a",
+  name: "DEXTools",
+  symbol: "DEXT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5866.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5866",
+  type: "token",
+});
