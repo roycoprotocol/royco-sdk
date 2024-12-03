35b2b785964c2f5ac286c291e935f320c7dd659a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:41 -0800

    feat(token-map): Update Token -- 1-0xb62d18dea74045e822352ce4b3ee77319dc5ff2f -- EventChain (EVC) (0xb62d...ff2f)
    Token Details:
    - Id: 1-0xb62d18dea74045e822352ce4b3ee77319dc5ff2f
    - Chain Id: 1
    - Contract Address: 0xb62d18dea74045e822352ce4b3ee77319dc5ff2f
    - Name: EventChain
    - Symbol: EVC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2237.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2237

---
+++ sdk/constants/token-map/1/definitions/1-0xb62d18dea74045e822352ce4b3ee77319dc5ff2f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb62d18dea74045e822352ce4b3ee77319dc5ff2f",
+  chain_id: 1,
+  contract_address: "0xb62d18dea74045e822352ce4b3ee77319dc5ff2f",
+  name: "EventChain",
+  symbol: "EVC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2237.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2237",
+  type: "token",
+});
