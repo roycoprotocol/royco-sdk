257faa016ad542e93956b45bebf27b8d912ea2c2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:52 -0800

    feat(token-map): Update Token -- 1-0x1a4743cf1af4c289351390a2b3fe7c13d2f7c235 -- Castweet (CTT) (0x1a47...c235)
    Token Details:
    - Id: 1-0x1a4743cf1af4c289351390a2b3fe7c13d2f7c235
    - Chain Id: 1
    - Contract Address: 0x1a4743cf1af4c289351390a2b3fe7c13d2f7c235
    - Name: Castweet
    - Symbol: CTT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5397.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5397

---
+++ sdk/constants/token-map/1/definitions/1-0x1a4743cf1af4c289351390a2b3fe7c13d2f7c235.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1a4743cf1af4c289351390a2b3fe7c13d2f7c235",
+  chain_id: 1,
+  contract_address: "0x1a4743cf1af4c289351390a2b3fe7c13d2f7c235",
+  name: "Castweet",
+  symbol: "CTT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5397.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5397",
+  type: "token",
+});
