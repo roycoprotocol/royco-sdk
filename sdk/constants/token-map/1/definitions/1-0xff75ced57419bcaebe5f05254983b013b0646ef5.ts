bc2ddd9fc270a5e7397d757b4403977d4bba22d8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:40:15 -0800

    feat(token-map): Update Token -- 1-0xff75ced57419bcaebe5f05254983b013b0646ef5 -- Cook Finance (COOK) (0xff75...6ef5)
    Token Details:
    - Id: 1-0xff75ced57419bcaebe5f05254983b013b0646ef5
    - Chain Id: 1
    - Contract Address: 0xff75ced57419bcaebe5f05254983b013b0646ef5
    - Name: Cook Finance
    - Symbol: COOK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8997.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8997

---
+++ sdk/constants/token-map/1/definitions/1-0xff75ced57419bcaebe5f05254983b013b0646ef5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xff75ced57419bcaebe5f05254983b013b0646ef5",
+  chain_id: 1,
+  contract_address: "0xff75ced57419bcaebe5f05254983b013b0646ef5",
+  name: "Cook Finance",
+  symbol: "COOK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8997.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8997",
+  type: "token",
+});
