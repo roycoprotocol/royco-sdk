d4b5c5340aa80f28fa03a4d2e47211daea7d14b0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:48 -0800

    feat(token-map): Update Token -- 1-0x777e2ae845272a2f540ebf6a3d03734a5a8f618e -- Ryoshis Vision (RYOSHI) (0x777e...618e)
    Token Details:
    - Id: 1-0x777e2ae845272a2f540ebf6a3d03734a5a8f618e
    - Chain Id: 1
    - Contract Address: 0x777e2ae845272a2f540ebf6a3d03734a5a8f618e
    - Name: Ryoshis Vision
    - Symbol: RYOSHI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11283.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11283

---
+++ sdk/constants/token-map/1/definitions/1-0x777e2ae845272a2f540ebf6a3d03734a5a8f618e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x777e2ae845272a2f540ebf6a3d03734a5a8f618e",
+  chain_id: 1,
+  contract_address: "0x777e2ae845272a2f540ebf6a3d03734a5a8f618e",
+  name: "Ryoshis Vision",
+  symbol: "RYOSHI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11283.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11283",
+  type: "token",
+});
