cbf1c4e90948e45d464f58b9e74a4efb362b7278
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:18 -0800

    feat(token-map): Update Token -- 1-0x089453742936dd35134383aee9d78bee63a69b01 -- Gold (GOLD) (0x0894...9b01)
    Token Details:
    - Id: 1-0x089453742936dd35134383aee9d78bee63a69b01
    - Chain Id: 1
    - Contract Address: 0x089453742936dd35134383aee9d78bee63a69b01
    - Name: Gold
    - Symbol: GOLD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28173.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 28173

---
+++ sdk/constants/token-map/1/definitions/1-0x089453742936dd35134383aee9d78bee63a69b01.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x089453742936dd35134383aee9d78bee63a69b01",
+  chain_id: 1,
+  contract_address: "0x089453742936dd35134383aee9d78bee63a69b01",
+  name: "Gold",
+  symbol: "GOLD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28173.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "28173",
+  type: "token",
+});
