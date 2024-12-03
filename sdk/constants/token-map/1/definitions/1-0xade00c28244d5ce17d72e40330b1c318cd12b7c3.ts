548d6b120ed6582ac4e9bb3da5ed5b269c3f78b7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:36 -0800

    feat(token-map): Update Token -- 1-0xade00c28244d5ce17d72e40330b1c318cd12b7c3 -- AdEx (ADX) (0xade0...b7c3)
    Token Details:
    - Id: 1-0xade00c28244d5ce17d72e40330b1c318cd12b7c3
    - Chain Id: 1
    - Contract Address: 0xade00c28244d5ce17d72e40330b1c318cd12b7c3
    - Name: AdEx
    - Symbol: ADX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1768.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1768

---
+++ sdk/constants/token-map/1/definitions/1-0xade00c28244d5ce17d72e40330b1c318cd12b7c3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xade00c28244d5ce17d72e40330b1c318cd12b7c3",
+  chain_id: 1,
+  contract_address: "0xade00c28244d5ce17d72e40330b1c318cd12b7c3",
+  name: "AdEx",
+  symbol: "ADX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1768.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1768",
+  type: "token",
+});
