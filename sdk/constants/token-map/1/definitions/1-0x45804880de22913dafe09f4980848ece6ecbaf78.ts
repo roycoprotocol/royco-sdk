4e461cd29fe8f873d8705076bc358630fec48a21
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:38 -0800

    feat(token-map): Update Token -- 1-0x45804880de22913dafe09f4980848ece6ecbaf78 -- PAX Gold (PAXG) (0x4580...af78)
    Token Details:
    - Id: 1-0x45804880de22913dafe09f4980848ece6ecbaf78
    - Chain Id: 1
    - Contract Address: 0x45804880de22913dafe09f4980848ece6ecbaf78
    - Name: PAX Gold
    - Symbol: PAXG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4705.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4705

---
+++ sdk/constants/token-map/1/definitions/1-0x45804880de22913dafe09f4980848ece6ecbaf78.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x45804880de22913dafe09f4980848ece6ecbaf78",
+  chain_id: 1,
+  contract_address: "0x45804880de22913dafe09f4980848ece6ecbaf78",
+  name: "PAX Gold",
+  symbol: "PAXG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4705.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4705",
+  type: "token",
+});
