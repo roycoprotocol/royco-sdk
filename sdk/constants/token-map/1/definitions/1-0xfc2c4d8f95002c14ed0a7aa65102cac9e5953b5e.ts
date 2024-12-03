f12336016f1492e7acc6df97152f2685d5a7bfe6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:46 -0800

    feat(token-map): Update Token -- 1-0xfc2c4d8f95002c14ed0a7aa65102cac9e5953b5e -- Rublix (RBLX) (0xfc2c...3b5e)
    Token Details:
    - Id: 1-0xfc2c4d8f95002c14ed0a7aa65102cac9e5953b5e
    - Chain Id: 1
    - Contract Address: 0xfc2c4d8f95002c14ed0a7aa65102cac9e5953b5e
    - Name: Rublix
    - Symbol: RBLX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2689.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2689

---
+++ sdk/constants/token-map/1/definitions/1-0xfc2c4d8f95002c14ed0a7aa65102cac9e5953b5e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfc2c4d8f95002c14ed0a7aa65102cac9e5953b5e",
+  chain_id: 1,
+  contract_address: "0xfc2c4d8f95002c14ed0a7aa65102cac9e5953b5e",
+  name: "Rublix",
+  symbol: "RBLX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2689.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2689",
+  type: "token",
+});
