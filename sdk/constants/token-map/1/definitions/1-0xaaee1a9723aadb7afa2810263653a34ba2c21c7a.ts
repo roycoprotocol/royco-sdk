74896beb658103a0412dd6f801ce114e22c6ea73
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:21 -0800

    feat(token-map): Update Token -- 1-0xaaee1a9723aadb7afa2810263653a34ba2c21c7a -- Mog Coin (MOG) (0xaaee...1c7a)
    Token Details:
    - Id: 1-0xaaee1a9723aadb7afa2810263653a34ba2c21c7a
    - Chain Id: 1
    - Contract Address: 0xaaee1a9723aadb7afa2810263653a34ba2c21c7a
    - Name: Mog Coin
    - Symbol: MOG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/27659.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 27659

---
+++ sdk/constants/token-map/1/definitions/1-0xaaee1a9723aadb7afa2810263653a34ba2c21c7a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xaaee1a9723aadb7afa2810263653a34ba2c21c7a",
+  chain_id: 1,
+  contract_address: "0xaaee1a9723aadb7afa2810263653a34ba2c21c7a",
+  name: "Mog Coin",
+  symbol: "MOG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/27659.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "27659",
+  type: "token",
+});
