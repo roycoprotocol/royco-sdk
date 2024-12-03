d0f00a6ddad1a7f80f1d08e2470cfd9ef479c1b1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:04 -0800

    feat(token-map): Update Token -- 1-0x26c8afbbfe1ebaca03c2bb082e69d0476bffe099 -- Cellframe (CELL) (0x26c8...e099)
    Token Details:
    - Id: 1-0x26c8afbbfe1ebaca03c2bb082e69d0476bffe099
    - Chain Id: 1
    - Contract Address: 0x26c8afbbfe1ebaca03c2bb082e69d0476bffe099
    - Name: Cellframe
    - Symbol: CELL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8992.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8992

---
+++ sdk/constants/token-map/1/definitions/1-0x26c8afbbfe1ebaca03c2bb082e69d0476bffe099.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x26c8afbbfe1ebaca03c2bb082e69d0476bffe099",
+  chain_id: 1,
+  contract_address: "0x26c8afbbfe1ebaca03c2bb082e69d0476bffe099",
+  name: "Cellframe",
+  symbol: "CELL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8992.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8992",
+  type: "token",
+});
