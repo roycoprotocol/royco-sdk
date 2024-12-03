91fea2ac50f0efb0c8b447f1cef1a9d8e00d0761
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:14 -0800

    feat(token-map): Update Token -- 1-0x38a2fdc11f526ddd5a607c1f251c065f40fbf2f7 -- PhoenixDAO (PHNX) (0x38a2...f2f7)
    Token Details:
    - Id: 1-0x38a2fdc11f526ddd5a607c1f251c065f40fbf2f7
    - Chain Id: 1
    - Contract Address: 0x38a2fdc11f526ddd5a607c1f251c065f40fbf2f7
    - Name: PhoenixDAO
    - Symbol: PHNX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5674.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5674

---
+++ sdk/constants/token-map/1/definitions/1-0x38a2fdc11f526ddd5a607c1f251c065f40fbf2f7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x38a2fdc11f526ddd5a607c1f251c065f40fbf2f7",
+  chain_id: 1,
+  contract_address: "0x38a2fdc11f526ddd5a607c1f251c065f40fbf2f7",
+  name: "PhoenixDAO",
+  symbol: "PHNX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5674.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5674",
+  type: "token",
+});
