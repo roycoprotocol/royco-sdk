7fb21e5269091172b07e84c42a23e403ea97d427
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:30 -0800

    feat(token-map): Update Token -- 1-0xdc0327d50e6c73db2f8117760592c8bbf1cdcf38 -- Stronger (STRNGR) (0xdc03...cf38)
    Token Details:
    - Id: 1-0xdc0327d50e6c73db2f8117760592c8bbf1cdcf38
    - Chain Id: 1
    - Contract Address: 0xdc0327d50e6c73db2f8117760592c8bbf1cdcf38
    - Name: Stronger
    - Symbol: STRNGR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/19423.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 19423

---
+++ sdk/constants/token-map/1/definitions/1-0xdc0327d50e6c73db2f8117760592c8bbf1cdcf38.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdc0327d50e6c73db2f8117760592c8bbf1cdcf38",
+  chain_id: 1,
+  contract_address: "0xdc0327d50e6c73db2f8117760592c8bbf1cdcf38",
+  name: "Stronger",
+  symbol: "STRNGR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/19423.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "19423",
+  type: "token",
+});
