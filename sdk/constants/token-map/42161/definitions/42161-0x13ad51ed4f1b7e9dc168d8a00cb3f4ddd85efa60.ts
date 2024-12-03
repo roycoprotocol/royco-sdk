aa375fe6392ca21c7afaf9cf77384ee758d0cd2e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:40:50 -0800

    feat(token-map): Update Token -- 42161-0x13ad51ed4f1b7e9dc168d8a00cb3f4ddd85efa60 -- Lido DAO (LDO) (0x13ad...fa60)
    Token Details:
    - Id: 42161-0x13ad51ed4f1b7e9dc168d8a00cb3f4ddd85efa60
    - Chain Id: 42161
    - Contract Address: 0x13ad51ed4f1b7e9dc168d8a00cb3f4ddd85efa60
    - Name: Lido DAO
    - Symbol: LDO
    - Image: https://coin-images.coingecko.com/coins/images/13573/large/Lido_DAO.png?1696513326
    - Decimals: 18
    - Source: coingecko
    - Search Id: lido-dao

---
+++ sdk/constants/token-map/42161/definitions/42161-0x13ad51ed4f1b7e9dc168d8a00cb3f4ddd85efa60.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x13ad51ed4f1b7e9dc168d8a00cb3f4ddd85efa60",
+  chain_id: 42161,
+  contract_address: "0x13ad51ed4f1b7e9dc168d8a00cb3f4ddd85efa60",
+  name: "Lido DAO",
+  symbol: "LDO",
+  image: "https://coin-images.coingecko.com/coins/images/13573/large/Lido_DAO.png?1696513326",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "lido-dao",
+  type: "token",
+});
