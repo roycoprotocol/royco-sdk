316e0741a043e4fd6247d5f42812643fb152d2e9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:30 -0800

    feat(token-map): Update Token -- 1-0xb4272071ecadd69d933adcd19ca99fe80664fc08 -- CryptoFranc (XCHF) (0xb427...fc08)
    Token Details:
    - Id: 1-0xb4272071ecadd69d933adcd19ca99fe80664fc08
    - Chain Id: 1
    - Contract Address: 0xb4272071ecadd69d933adcd19ca99fe80664fc08
    - Name: CryptoFranc
    - Symbol: XCHF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4075.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4075

---
+++ sdk/constants/token-map/1/definitions/1-0xb4272071ecadd69d933adcd19ca99fe80664fc08.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb4272071ecadd69d933adcd19ca99fe80664fc08",
+  chain_id: 1,
+  contract_address: "0xb4272071ecadd69d933adcd19ca99fe80664fc08",
+  name: "CryptoFranc",
+  symbol: "XCHF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4075.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4075",
+  type: "token",
+});
