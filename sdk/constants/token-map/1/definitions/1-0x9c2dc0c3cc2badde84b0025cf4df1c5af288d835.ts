ff2f556f1612ac96eaf12d6500a7548c175cfa68
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:41 -0800

    feat(token-map): Update Token -- 1-0x9c2dc0c3cc2badde84b0025cf4df1c5af288d835 -- Coreto (COR) (0x9c2d...d835)
    Token Details:
    - Id: 1-0x9c2dc0c3cc2badde84b0025cf4df1c5af288d835
    - Chain Id: 1
    - Contract Address: 0x9c2dc0c3cc2badde84b0025cf4df1c5af288d835
    - Name: Coreto
    - Symbol: COR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7398.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7398

---
+++ sdk/constants/token-map/1/definitions/1-0x9c2dc0c3cc2badde84b0025cf4df1c5af288d835.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9c2dc0c3cc2badde84b0025cf4df1c5af288d835",
+  chain_id: 1,
+  contract_address: "0x9c2dc0c3cc2badde84b0025cf4df1c5af288d835",
+  name: "Coreto",
+  symbol: "COR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7398.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7398",
+  type: "token",
+});
