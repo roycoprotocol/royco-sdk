6b2d3d022cfd3a6ece797e8809585017cf4dd18e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:31:05 -0800

    feat(token-map): Update Token -- 1-0xc40af1e4fecfa05ce6bab79dcd8b373d2e436c4e -- Hokkaidu Inu (HOKK) (0xc40a...6c4e)
    Token Details:
    - Id: 1-0xc40af1e4fecfa05ce6bab79dcd8b373d2e436c4e
    - Chain Id: 1
    - Contract Address: 0xc40af1e4fecfa05ce6bab79dcd8b373d2e436c4e
    - Name: Hokkaidu Inu
    - Symbol: HOKK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/30018.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 30018

---
+++ sdk/constants/token-map/1/definitions/1-0xc40af1e4fecfa05ce6bab79dcd8b373d2e436c4e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc40af1e4fecfa05ce6bab79dcd8b373d2e436c4e",
+  chain_id: 1,
+  contract_address: "0xc40af1e4fecfa05ce6bab79dcd8b373d2e436c4e",
+  name: "Hokkaidu Inu",
+  symbol: "HOKK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/30018.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "30018",
+  type: "token",
+});
