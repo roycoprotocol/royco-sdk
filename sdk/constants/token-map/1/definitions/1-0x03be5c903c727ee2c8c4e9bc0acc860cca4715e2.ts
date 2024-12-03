5e7b854d6a62d3e8b43dd5ac0ad5988e80dae21a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:57:24 -0800

    feat(token-map): Update Token -- 1-0x03be5c903c727ee2c8c4e9bc0acc860cca4715e2 -- Ternoa (CAPS) (0x03be...15e2)
    Token Details:
    - Id: 1-0x03be5c903c727ee2c8c4e9bc0acc860cca4715e2
    - Chain Id: 1
    - Contract Address: 0x03be5c903c727ee2c8c4e9bc0acc860cca4715e2
    - Name: Ternoa
    - Symbol: CAPS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9291.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9291

---
+++ sdk/constants/token-map/1/definitions/1-0x03be5c903c727ee2c8c4e9bc0acc860cca4715e2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x03be5c903c727ee2c8c4e9bc0acc860cca4715e2",
+  chain_id: 1,
+  contract_address: "0x03be5c903c727ee2c8c4e9bc0acc860cca4715e2",
+  name: "Ternoa",
+  symbol: "CAPS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9291.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9291",
+  type: "token",
+});
