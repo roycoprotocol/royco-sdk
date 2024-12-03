f5f04c2ffe4fbb69893cb52c5a070a270004f7af
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:27 -0800

    feat(token-map): Update Token -- 1-0x347c099f110ca6761779329d2879957b606b6ace -- Joint Ventures (JOINT) (0x347c...6ace)
    Token Details:
    - Id: 1-0x347c099f110ca6761779329d2879957b606b6ace
    - Chain Id: 1
    - Contract Address: 0x347c099f110ca6761779329d2879957b606b6ace
    - Name: Joint Ventures
    - Symbol: JOINT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2745.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2745

---
+++ sdk/constants/token-map/1/definitions/1-0x347c099f110ca6761779329d2879957b606b6ace.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x347c099f110ca6761779329d2879957b606b6ace",
+  chain_id: 1,
+  contract_address: "0x347c099f110ca6761779329d2879957b606b6ace",
+  name: "Joint Ventures",
+  symbol: "JOINT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2745.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2745",
+  type: "token",
+});
