8eb1d3a7ee84641b03722aa1164548002ff9470c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:56 -0800

    feat(token-map): Update Token -- 1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3 -- Ethena USDe (USDe) (0x4c9e...68b3)
    Token Details:
    - Id: 1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3
    - Chain Id: 1
    - Contract Address: 0x4c9edd5852cd905f086c759e8383e09bff1e68b3
    - Name: Ethena USDe
    - Symbol: USDe
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29470.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29470

---
+++ sdk/constants/token-map/1/definitions/1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3",
+  chain_id: 1,
+  contract_address: "0x4c9edd5852cd905f086c759e8383e09bff1e68b3",
+  name: "Ethena USDe",
+  symbol: "USDe",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29470.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29470",
+  type: "token",
+});
