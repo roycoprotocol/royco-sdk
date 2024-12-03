cd3c0ca623dd85a9b02942837a7074eded3b8e47
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:03 -0800

    feat(token-map): Update Token -- 1-0xb7c4a82936194fee52a4e3d4cec3415f74507532 -- iBTC (IBTC) (0xb7c4...7532)
    Token Details:
    - Id: 1-0xb7c4a82936194fee52a4e3d4cec3415f74507532
    - Chain Id: 1
    - Contract Address: 0xb7c4a82936194fee52a4e3d4cec3415f74507532
    - Name: iBTC
    - Symbol: IBTC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2131.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2131

---
+++ sdk/constants/token-map/1/definitions/1-0xb7c4a82936194fee52a4e3d4cec3415f74507532.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb7c4a82936194fee52a4e3d4cec3415f74507532",
+  chain_id: 1,
+  contract_address: "0xb7c4a82936194fee52a4e3d4cec3415f74507532",
+  name: "iBTC",
+  symbol: "IBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2131.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2131",
+  type: "token",
+});
