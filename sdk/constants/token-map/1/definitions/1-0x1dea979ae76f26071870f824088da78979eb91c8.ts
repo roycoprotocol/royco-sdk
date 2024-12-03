5c58bd66e1064e5a3902e5352a6a9962bd2e842a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:32 -0800

    feat(token-map): Update Token -- 1-0x1dea979ae76f26071870f824088da78979eb91c8 -- SPINDLE (SPD) (0x1dea...91c8)
    Token Details:
    - Id: 1-0x1dea979ae76f26071870f824088da78979eb91c8
    - Chain Id: 1
    - Contract Address: 0x1dea979ae76f26071870f824088da78979eb91c8
    - Name: SPINDLE
    - Symbol: SPD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2828.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2828

---
+++ sdk/constants/token-map/1/definitions/1-0x1dea979ae76f26071870f824088da78979eb91c8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1dea979ae76f26071870f824088da78979eb91c8",
+  chain_id: 1,
+  contract_address: "0x1dea979ae76f26071870f824088da78979eb91c8",
+  name: "SPINDLE",
+  symbol: "SPD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2828.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2828",
+  type: "token",
+});
