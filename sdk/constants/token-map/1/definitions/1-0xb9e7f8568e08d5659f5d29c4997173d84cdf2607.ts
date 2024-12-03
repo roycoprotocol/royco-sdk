4c30ed1a3fcd953f31e3ccff1f78c6b42c19c341
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:24 -0800

    feat(token-map): Update Token -- 1-0xb9e7f8568e08d5659f5d29c4997173d84cdf2607 -- Swarm City (SWT) (0xb9e7...2607)
    Token Details:
    - Id: 1-0xb9e7f8568e08d5659f5d29c4997173d84cdf2607
    - Chain Id: 1
    - Contract Address: 0xb9e7f8568e08d5659f5d29c4997173d84cdf2607
    - Name: Swarm City
    - Symbol: SWT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1562.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1562

---
+++ sdk/constants/token-map/1/definitions/1-0xb9e7f8568e08d5659f5d29c4997173d84cdf2607.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb9e7f8568e08d5659f5d29c4997173d84cdf2607",
+  chain_id: 1,
+  contract_address: "0xb9e7f8568e08d5659f5d29c4997173d84cdf2607",
+  name: "Swarm City",
+  symbol: "SWT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1562.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1562",
+  type: "token",
+});
