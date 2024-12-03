6097713c09f7cd0844a36fd01d1503dee1decd4a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:47 -0800

    feat(token-map): Update Token -- 1-0x5eaa69b29f99c84fe5de8200340b4e9b4ab38eac -- Raze Network (RAZE) (0x5eaa...8eac)
    Token Details:
    - Id: 1-0x5eaa69b29f99c84fe5de8200340b4e9b4ab38eac
    - Chain Id: 1
    - Contract Address: 0x5eaa69b29f99c84fe5de8200340b4e9b4ab38eac
    - Name: Raze Network
    - Symbol: RAZE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9173.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9173

---
+++ sdk/constants/token-map/1/definitions/1-0x5eaa69b29f99c84fe5de8200340b4e9b4ab38eac.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5eaa69b29f99c84fe5de8200340b4e9b4ab38eac",
+  chain_id: 1,
+  contract_address: "0x5eaa69b29f99c84fe5de8200340b4e9b4ab38eac",
+  name: "Raze Network",
+  symbol: "RAZE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9173.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9173",
+  type: "token",
+});
