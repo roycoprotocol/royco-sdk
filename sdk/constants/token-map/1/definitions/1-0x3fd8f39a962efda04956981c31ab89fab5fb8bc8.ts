26261e4cf3fed2015b9a6fe58954ec8d897503f6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:20 -0800

    feat(token-map): Update Token -- 1-0x3fd8f39a962efda04956981c31ab89fab5fb8bc8 -- Rotharium (RTH) (0x3fd8...8bc8)
    Token Details:
    - Id: 1-0x3fd8f39a962efda04956981c31ab89fab5fb8bc8
    - Chain Id: 1
    - Contract Address: 0x3fd8f39a962efda04956981c31ab89fab5fb8bc8
    - Name: Rotharium
    - Symbol: RTH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3279.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3279

---
+++ sdk/constants/token-map/1/definitions/1-0x3fd8f39a962efda04956981c31ab89fab5fb8bc8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3fd8f39a962efda04956981c31ab89fab5fb8bc8",
+  chain_id: 1,
+  contract_address: "0x3fd8f39a962efda04956981c31ab89fab5fb8bc8",
+  name: "Rotharium",
+  symbol: "RTH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3279.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3279",
+  type: "token",
+});
