439f01333794c2f0ee4f03247b3dbe7b52b6f52d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:36:31 -0800

    feat(token-map): Update Token -- 1-0xe9fa21e671bcfb04e6868784b89c19d5aa2424ea -- EurocoinToken (ECTE) (0xe9fa...24ea)
    Token Details:
    - Id: 1-0xe9fa21e671bcfb04e6868784b89c19d5aa2424ea
    - Chain Id: 1
    - Contract Address: 0xe9fa21e671bcfb04e6868784b89c19d5aa2424ea
    - Name: EurocoinToken
    - Symbol: ECTE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3741.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3741

---
+++ sdk/constants/token-map/1/definitions/1-0xe9fa21e671bcfb04e6868784b89c19d5aa2424ea.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe9fa21e671bcfb04e6868784b89c19d5aa2424ea",
+  chain_id: 1,
+  contract_address: "0xe9fa21e671bcfb04e6868784b89c19d5aa2424ea",
+  name: "EurocoinToken",
+  symbol: "ECTE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3741.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3741",
+  type: "token",
+});
