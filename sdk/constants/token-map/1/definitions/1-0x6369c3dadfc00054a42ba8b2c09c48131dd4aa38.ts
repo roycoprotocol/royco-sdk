a54c3b7316c2954279986d3ff5d15e807a6cbf96
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:40 -0800

    feat(token-map): Update Token -- 1-0x6369c3dadfc00054a42ba8b2c09c48131dd4aa38 -- Morpher (MPH) (0x6369...aa38)
    Token Details:
    - Id: 1-0x6369c3dadfc00054a42ba8b2c09c48131dd4aa38
    - Chain Id: 1
    - Contract Address: 0x6369c3dadfc00054a42ba8b2c09c48131dd4aa38
    - Name: Morpher
    - Symbol: MPH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7217.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7217

---
+++ sdk/constants/token-map/1/definitions/1-0x6369c3dadfc00054a42ba8b2c09c48131dd4aa38.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6369c3dadfc00054a42ba8b2c09c48131dd4aa38",
+  chain_id: 1,
+  contract_address: "0x6369c3dadfc00054a42ba8b2c09c48131dd4aa38",
+  name: "Morpher",
+  symbol: "MPH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7217.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7217",
+  type: "token",
+});
