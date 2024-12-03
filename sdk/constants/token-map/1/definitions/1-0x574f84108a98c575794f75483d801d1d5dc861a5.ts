67fffa4ed5bd9df79053abdaa9dff40fd5c2baf9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:30 -0800

    feat(token-map): Update Token -- 1-0x574f84108a98c575794f75483d801d1d5dc861a5 -- Robotina (ROX) (0x574f...61a5)
    Token Details:
    - Id: 1-0x574f84108a98c575794f75483d801d1d5dc861a5
    - Chain Id: 1
    - Contract Address: 0x574f84108a98c575794f75483d801d1d5dc861a5
    - Name: Robotina
    - Symbol: ROX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3325.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3325

---
+++ sdk/constants/token-map/1/definitions/1-0x574f84108a98c575794f75483d801d1d5dc861a5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x574f84108a98c575794f75483d801d1d5dc861a5",
+  chain_id: 1,
+  contract_address: "0x574f84108a98c575794f75483d801d1d5dc861a5",
+  name: "Robotina",
+  symbol: "ROX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3325.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3325",
+  type: "token",
+});
