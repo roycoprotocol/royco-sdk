bd0bfea03e5d143b874f6464dba7b8d46e586fc2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:36:25 -0800

    feat(token-map): Update Token -- 1-0xe95990825aab1a7f0af4cc648f76a3bcc99f25b2 -- Zenswap Network Token (ZNT) (0xe959...25b2)
    Token Details:
    - Id: 1-0xe95990825aab1a7f0af4cc648f76a3bcc99f25b2
    - Chain Id: 1
    - Contract Address: 0xe95990825aab1a7f0af4cc648f76a3bcc99f25b2
    - Name: Zenswap Network Token
    - Symbol: ZNT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3446.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3446

---
+++ sdk/constants/token-map/1/definitions/1-0xe95990825aab1a7f0af4cc648f76a3bcc99f25b2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe95990825aab1a7f0af4cc648f76a3bcc99f25b2",
+  chain_id: 1,
+  contract_address: "0xe95990825aab1a7f0af4cc648f76a3bcc99f25b2",
+  name: "Zenswap Network Token",
+  symbol: "ZNT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3446.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3446",
+  type: "token",
+});
