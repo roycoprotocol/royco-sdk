99270d460a7489eecae15efece4cf19195076c44
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:43 -0800

    feat(token-map): Update Token -- 1-0x967da4048cd07ab37855c090aaf366e4ce1b9f48 -- Ocean Protocol (OCEAN) (0x967d...9f48)
    Token Details:
    - Id: 1-0x967da4048cd07ab37855c090aaf366e4ce1b9f48
    - Chain Id: 1
    - Contract Address: 0x967da4048cd07ab37855c090aaf366e4ce1b9f48
    - Name: Ocean Protocol
    - Symbol: OCEAN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3911.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3911

---
+++ sdk/constants/token-map/1/definitions/1-0x967da4048cd07ab37855c090aaf366e4ce1b9f48.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x967da4048cd07ab37855c090aaf366e4ce1b9f48",
+  chain_id: 1,
+  contract_address: "0x967da4048cd07ab37855c090aaf366e4ce1b9f48",
+  name: "Ocean Protocol",
+  symbol: "OCEAN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3911.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3911",
+  type: "token",
+});
