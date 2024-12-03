0325f062ecab3a6c239cbbb22e571373902ec575
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:56 -0800

    feat(token-map): Update Token -- 1-0xde67d97b8770dc98c746a3fc0093c538666eb493 -- Bitrock (BROCK) (0xde67...b493)
    Token Details:
    - Id: 1-0xde67d97b8770dc98c746a3fc0093c538666eb493
    - Chain Id: 1
    - Contract Address: 0xde67d97b8770dc98c746a3fc0093c538666eb493
    - Name: Bitrock
    - Symbol: BROCK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/27606.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 27606

---
+++ sdk/constants/token-map/1/definitions/1-0xde67d97b8770dc98c746a3fc0093c538666eb493.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xde67d97b8770dc98c746a3fc0093c538666eb493",
+  chain_id: 1,
+  contract_address: "0xde67d97b8770dc98c746a3fc0093c538666eb493",
+  name: "Bitrock",
+  symbol: "BROCK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/27606.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "27606",
+  type: "token",
+});
