9478608bd91a2c2ad093d621469a8b683f03a997
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:08 -0800

    feat(token-map): Update Token -- 1-0x9ea3b5b4ec044b70375236a281986106457b20ef -- Delta (DELTA) (0x9ea3...20ef)
    Token Details:
    - Id: 1-0x9ea3b5b4ec044b70375236a281986106457b20ef
    - Chain Id: 1
    - Contract Address: 0x9ea3b5b4ec044b70375236a281986106457b20ef
    - Name: Delta
    - Symbol: DELTA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8994.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8994

---
+++ sdk/constants/token-map/1/definitions/1-0x9ea3b5b4ec044b70375236a281986106457b20ef.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9ea3b5b4ec044b70375236a281986106457b20ef",
+  chain_id: 1,
+  contract_address: "0x9ea3b5b4ec044b70375236a281986106457b20ef",
+  name: "Delta",
+  symbol: "DELTA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8994.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8994",
+  type: "token",
+});
