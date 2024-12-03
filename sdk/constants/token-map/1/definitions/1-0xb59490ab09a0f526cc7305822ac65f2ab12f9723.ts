2f7f95255bca76e2afc2c2ccf51f5a7dc1ee0671
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:37 -0800

    feat(token-map): Update Token -- 1-0xb59490ab09a0f526cc7305822ac65f2ab12f9723 -- Litentry (LIT) (0xb594...9723)
    Token Details:
    - Id: 1-0xb59490ab09a0f526cc7305822ac65f2ab12f9723
    - Chain Id: 1
    - Contract Address: 0xb59490ab09a0f526cc7305822ac65f2ab12f9723
    - Name: Litentry
    - Symbol: LIT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6833.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6833

---
+++ sdk/constants/token-map/1/definitions/1-0xb59490ab09a0f526cc7305822ac65f2ab12f9723.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb59490ab09a0f526cc7305822ac65f2ab12f9723",
+  chain_id: 1,
+  contract_address: "0xb59490ab09a0f526cc7305822ac65f2ab12f9723",
+  name: "Litentry",
+  symbol: "LIT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6833.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6833",
+  type: "token",
+});
