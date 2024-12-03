3bb93567c9d798ac3d5e69ab1894fda9783d1762
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:49 -0800

    feat(token-map): Update Token -- 1-0xe41d2489571d322189246dafa5ebde1f4699f498 -- 0x Protocol (ZRX) (0xe41d...f498)
    Token Details:
    - Id: 1-0xe41d2489571d322189246dafa5ebde1f4699f498
    - Chain Id: 1
    - Contract Address: 0xe41d2489571d322189246dafa5ebde1f4699f498
    - Name: 0x Protocol
    - Symbol: ZRX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1896.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1896

---
+++ sdk/constants/token-map/1/definitions/1-0xe41d2489571d322189246dafa5ebde1f4699f498.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe41d2489571d322189246dafa5ebde1f4699f498",
+  chain_id: 1,
+  contract_address: "0xe41d2489571d322189246dafa5ebde1f4699f498",
+  name: "0x Protocol",
+  symbol: "ZRX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1896.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1896",
+  type: "token",
+});
