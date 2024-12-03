001ff3e043cf6bfb1f92a5fc5d7524f823a68bf5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:55 -0800

    feat(token-map): Update Token -- 1-0x64bc2ca1be492be7185faa2c8835d9b824c8a194 -- Big Time (BIGTIME) (0x64bc...a194)
    Token Details:
    - Id: 1-0x64bc2ca1be492be7185faa2c8835d9b824c8a194
    - Chain Id: 1
    - Contract Address: 0x64bc2ca1be492be7185faa2c8835d9b824c8a194
    - Name: Big Time
    - Symbol: BIGTIME
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28230.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 28230

---
+++ sdk/constants/token-map/1/definitions/1-0x64bc2ca1be492be7185faa2c8835d9b824c8a194.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x64bc2ca1be492be7185faa2c8835d9b824c8a194",
+  chain_id: 1,
+  contract_address: "0x64bc2ca1be492be7185faa2c8835d9b824c8a194",
+  name: "Big Time",
+  symbol: "BIGTIME",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28230.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "28230",
+  type: "token",
+});
