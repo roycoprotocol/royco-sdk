07c6a50f2e7898e5dbd9353cc5c178917a9f0eb4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:31:35 -0800

    feat(token-map): Update Token -- 1-0xc690f7c7fcffa6a82b79fab7508c466fefdfc8c5 -- Lympo (LYM) (0xc690...c8c5)
    Token Details:
    - Id: 1-0xc690f7c7fcffa6a82b79fab7508c466fefdfc8c5
    - Chain Id: 1
    - Contract Address: 0xc690f7c7fcffa6a82b79fab7508c466fefdfc8c5
    - Name: Lympo
    - Symbol: LYM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2554.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2554

---
+++ sdk/constants/token-map/1/definitions/1-0xc690f7c7fcffa6a82b79fab7508c466fefdfc8c5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc690f7c7fcffa6a82b79fab7508c466fefdfc8c5",
+  chain_id: 1,
+  contract_address: "0xc690f7c7fcffa6a82b79fab7508c466fefdfc8c5",
+  name: "Lympo",
+  symbol: "LYM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2554.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2554",
+  type: "token",
+});
