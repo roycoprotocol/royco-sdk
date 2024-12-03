5d175376a9025559f8c9c6e7a71e5e3d7d297f44
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:02 -0800

    feat(token-map): Update Token -- 1-0x8c15ef5b4b21951d50e53e4fbda8298ffad25057 -- Function X (FX) (0x8c15...5057)
    Token Details:
    - Id: 1-0x8c15ef5b4b21951d50e53e4fbda8298ffad25057
    - Chain Id: 1
    - Contract Address: 0x8c15ef5b4b21951d50e53e4fbda8298ffad25057
    - Name: Function X
    - Symbol: FX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3884.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3884

---
+++ sdk/constants/token-map/1/definitions/1-0x8c15ef5b4b21951d50e53e4fbda8298ffad25057.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8c15ef5b4b21951d50e53e4fbda8298ffad25057",
+  chain_id: 1,
+  contract_address: "0x8c15ef5b4b21951d50e53e4fbda8298ffad25057",
+  name: "Function X",
+  symbol: "FX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3884.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3884",
+  type: "token",
+});
