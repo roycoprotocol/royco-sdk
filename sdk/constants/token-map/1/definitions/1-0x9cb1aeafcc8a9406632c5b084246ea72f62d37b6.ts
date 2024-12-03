a99f4e2b229ab34f8971d2767b5fba8a8f16de26
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:43 -0800

    feat(token-map): Update Token -- 1-0x9cb1aeafcc8a9406632c5b084246ea72f62d37b6 -- LBK (LBK) (0x9cb1...37b6)
    Token Details:
    - Id: 1-0x9cb1aeafcc8a9406632c5b084246ea72f62d37b6
    - Chain Id: 1
    - Contract Address: 0x9cb1aeafcc8a9406632c5b084246ea72f62d37b6
    - Name: LBK
    - Symbol: LBK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5445.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 5445

---
+++ sdk/constants/token-map/1/definitions/1-0x9cb1aeafcc8a9406632c5b084246ea72f62d37b6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9cb1aeafcc8a9406632c5b084246ea72f62d37b6",
+  chain_id: 1,
+  contract_address: "0x9cb1aeafcc8a9406632c5b084246ea72f62d37b6",
+  name: "LBK",
+  symbol: "LBK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5445.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "5445",
+  type: "token",
+});
