9a9e5ec95ca53794613b0843d4640b3766d8c88a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:40 -0800

    feat(token-map): Update Token -- 1-0x309627af60f0926daa6041b8279484312f2bf060 -- USD Bancor (USDB) (0x3096...f060)
    Token Details:
    - Id: 1-0x309627af60f0926daa6041b8279484312f2bf060
    - Chain Id: 1
    - Contract Address: 0x309627af60f0926daa6041b8279484312f2bf060
    - Name: USD Bancor
    - Symbol: USDB
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5219.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5219

---
+++ sdk/constants/token-map/1/definitions/1-0x309627af60f0926daa6041b8279484312f2bf060.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x309627af60f0926daa6041b8279484312f2bf060",
+  chain_id: 1,
+  contract_address: "0x309627af60f0926daa6041b8279484312f2bf060",
+  name: "USD Bancor",
+  symbol: "USDB",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5219.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5219",
+  type: "token",
+});
