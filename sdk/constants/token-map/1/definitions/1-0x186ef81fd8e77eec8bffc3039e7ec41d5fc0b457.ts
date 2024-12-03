85b9a59b40959fe5c23da8182e236b5df8cdf30f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:34 -0800

    feat(token-map): Update Token -- 1-0x186ef81fd8e77eec8bffc3039e7ec41d5fc0b457 -- Inspect (INSP) (0x186e...b457)
    Token Details:
    - Id: 1-0x186ef81fd8e77eec8bffc3039e7ec41d5fc0b457
    - Chain Id: 1
    - Contract Address: 0x186ef81fd8e77eec8bffc3039e7ec41d5fc0b457
    - Name: Inspect
    - Symbol: INSP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28610.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 28610

---
+++ sdk/constants/token-map/1/definitions/1-0x186ef81fd8e77eec8bffc3039e7ec41d5fc0b457.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x186ef81fd8e77eec8bffc3039e7ec41d5fc0b457",
+  chain_id: 1,
+  contract_address: "0x186ef81fd8e77eec8bffc3039e7ec41d5fc0b457",
+  name: "Inspect",
+  symbol: "INSP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28610.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "28610",
+  type: "token",
+});
