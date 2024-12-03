fe0d3394bba576c1dc96ce9ba3e4c1f55bf7df55
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:03:01 -0800

    feat(token-map): Update Token -- 1-0x20945ca1df56d237fd40036d47e866c7dccd2114 -- Nsure.Network (NSURE) (0x2094...2114)
    Token Details:
    - Id: 1-0x20945ca1df56d237fd40036d47e866c7dccd2114
    - Chain Id: 1
    - Contract Address: 0x20945ca1df56d237fd40036d47e866c7dccd2114
    - Name: Nsure.Network
    - Symbol: NSURE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7231.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7231

---
+++ sdk/constants/token-map/1/definitions/1-0x20945ca1df56d237fd40036d47e866c7dccd2114.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x20945ca1df56d237fd40036d47e866c7dccd2114",
+  chain_id: 1,
+  contract_address: "0x20945ca1df56d237fd40036d47e866c7dccd2114",
+  name: "Nsure.Network",
+  symbol: "NSURE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7231.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7231",
+  type: "token",
+});
