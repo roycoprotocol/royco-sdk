95ce2ac076bd7117b918857099cc7d8547908b30
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:54 -0800

    feat(token-map): Update Token -- 1-0x5faa989af96af85384b8a938c2ede4a7378d9875 -- Galxe (GAL) (0x5faa...9875)
    Token Details:
    - Id: 1-0x5faa989af96af85384b8a938c2ede4a7378d9875
    - Chain Id: 1
    - Contract Address: 0x5faa989af96af85384b8a938c2ede4a7378d9875
    - Name: Galxe
    - Symbol: GAL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11877.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11877

---
+++ sdk/constants/token-map/1/definitions/1-0x5faa989af96af85384b8a938c2ede4a7378d9875.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5faa989af96af85384b8a938c2ede4a7378d9875",
+  chain_id: 1,
+  contract_address: "0x5faa989af96af85384b8a938c2ede4a7378d9875",
+  name: "Galxe",
+  symbol: "GAL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11877.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11877",
+  type: "token",
+});
