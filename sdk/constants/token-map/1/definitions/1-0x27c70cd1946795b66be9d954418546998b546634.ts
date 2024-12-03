5e29bd5844d0e54de78ec10286439f10651064c6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:29 -0800

    feat(token-map): Update Token -- 1-0x27c70cd1946795b66be9d954418546998b546634 -- Doge Killer (LEASH) (0x27c7...6634)
    Token Details:
    - Id: 1-0x27c70cd1946795b66be9d954418546998b546634
    - Chain Id: 1
    - Contract Address: 0x27c70cd1946795b66be9d954418546998b546634
    - Name: Doge Killer
    - Symbol: LEASH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9286.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9286

---
+++ sdk/constants/token-map/1/definitions/1-0x27c70cd1946795b66be9d954418546998b546634.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x27c70cd1946795b66be9d954418546998b546634",
+  chain_id: 1,
+  contract_address: "0x27c70cd1946795b66be9d954418546998b546634",
+  name: "Doge Killer",
+  symbol: "LEASH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9286.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9286",
+  type: "token",
+});
