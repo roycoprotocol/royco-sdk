92ab084023367b5ebb0761ed2a425df5234ad6df
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:21 -0800

    feat(token-map): Update Token -- 1-0x16f812be7fff02caf662b85d5d58a5da6572d4df -- United Traders Token (UTT) (0x16f8...d4df)
    Token Details:
    - Id: 1-0x16f812be7fff02caf662b85d5d58a5da6572d4df
    - Chain Id: 1
    - Contract Address: 0x16f812be7fff02caf662b85d5d58a5da6572d4df
    - Name: United Traders Token
    - Symbol: UTT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2371.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2371

---
+++ sdk/constants/token-map/1/definitions/1-0x16f812be7fff02caf662b85d5d58a5da6572d4df.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x16f812be7fff02caf662b85d5d58a5da6572d4df",
+  chain_id: 1,
+  contract_address: "0x16f812be7fff02caf662b85d5d58a5da6572d4df",
+  name: "United Traders Token",
+  symbol: "UTT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2371.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2371",
+  type: "token",
+});
