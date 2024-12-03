2ef72c35e71be436cdb5448bae88a19d5966ca58
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:20 -0800

    feat(token-map): Update Token -- 1-0x08d32b0da63e2c3bcf8019c9c5d849d7a9d791e6 -- Dentacoin (DCN) (0x08d3...91e6)
    Token Details:
    - Id: 1-0x08d32b0da63e2c3bcf8019c9c5d849d7a9d791e6
    - Chain Id: 1
    - Contract Address: 0x08d32b0da63e2c3bcf8019c9c5d849d7a9d791e6
    - Name: Dentacoin
    - Symbol: DCN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1876.png
    - Decimals: 0
    - Source: coinmarketcap
    - Search Id: 1876

---
+++ sdk/constants/token-map/1/definitions/1-0x08d32b0da63e2c3bcf8019c9c5d849d7a9d791e6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x08d32b0da63e2c3bcf8019c9c5d849d7a9d791e6",
+  chain_id: 1,
+  contract_address: "0x08d32b0da63e2c3bcf8019c9c5d849d7a9d791e6",
+  name: "Dentacoin",
+  symbol: "DCN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1876.png",
+  decimals: 0,
+  source: "coinmarketcap",
+  search_id: "1876",
+  type: "token",
+});
