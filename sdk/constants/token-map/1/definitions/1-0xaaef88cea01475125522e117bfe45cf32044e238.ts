88c99c8edc1658d10cb445edf02ef0a0d7d8c90a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:23 -0800

    feat(token-map): Update Token -- 1-0xaaef88cea01475125522e117bfe45cf32044e238 -- GuildFi (GF) (0xaaef...e238)
    Token Details:
    - Id: 1-0xaaef88cea01475125522e117bfe45cf32044e238
    - Chain Id: 1
    - Contract Address: 0xaaef88cea01475125522e117bfe45cf32044e238
    - Name: GuildFi
    - Symbol: GF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/15585.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 15585

---
+++ sdk/constants/token-map/1/definitions/1-0xaaef88cea01475125522e117bfe45cf32044e238.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xaaef88cea01475125522e117bfe45cf32044e238",
+  chain_id: 1,
+  contract_address: "0xaaef88cea01475125522e117bfe45cf32044e238",
+  name: "GuildFi",
+  symbol: "GF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15585.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "15585",
+  type: "token",
+});
