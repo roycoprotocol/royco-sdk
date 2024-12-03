8f4c27ff25926c72fb7501d069d9bf3d8f586158
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:07 -0800

    feat(token-map): Update Token -- 1-0xbe0ed4138121ecfc5c0e56b40517da27e6c5226b -- Aethir (ATH) (0xbe0e...226b)
    Token Details:
    - Id: 1-0xbe0ed4138121ecfc5c0e56b40517da27e6c5226b
    - Chain Id: 1
    - Contract Address: 0xbe0ed4138121ecfc5c0e56b40517da27e6c5226b
    - Name: Aethir
    - Symbol: ATH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/30083.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 30083

---
+++ sdk/constants/token-map/1/definitions/1-0xbe0ed4138121ecfc5c0e56b40517da27e6c5226b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbe0ed4138121ecfc5c0e56b40517da27e6c5226b",
+  chain_id: 1,
+  contract_address: "0xbe0ed4138121ecfc5c0e56b40517da27e6c5226b",
+  name: "Aethir",
+  symbol: "ATH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/30083.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "30083",
+  type: "token",
+});
