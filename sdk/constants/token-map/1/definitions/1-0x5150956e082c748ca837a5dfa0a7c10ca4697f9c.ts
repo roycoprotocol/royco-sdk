1d5ab82f5ef8b913ac837dfc2ff420d6a2a961c1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:44 -0800

    feat(token-map): Update Token -- 1-0x5150956e082c748ca837a5dfa0a7c10ca4697f9c -- Zeedex (ZDEX) (0x5150...7f9c)
    Token Details:
    - Id: 1-0x5150956e082c748ca837a5dfa0a7c10ca4697f9c
    - Chain Id: 1
    - Contract Address: 0x5150956e082c748ca837a5dfa0a7c10ca4697f9c
    - Name: Zeedex
    - Symbol: ZDEX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6989.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6989

---
+++ sdk/constants/token-map/1/definitions/1-0x5150956e082c748ca837a5dfa0a7c10ca4697f9c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5150956e082c748ca837a5dfa0a7c10ca4697f9c",
+  chain_id: 1,
+  contract_address: "0x5150956e082c748ca837a5dfa0a7c10ca4697f9c",
+  name: "Zeedex",
+  symbol: "ZDEX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6989.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6989",
+  type: "token",
+});
