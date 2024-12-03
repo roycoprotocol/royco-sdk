d1363e5ea72c98fc90744e094d79c50d55fe7410
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:07 -0800

    feat(token-map): Update Token -- 1-0xed35af169af46a02ee13b9d79eb57d6d68c1749e -- ECOMI (OMI) (0xed35...749e)
    Token Details:
    - Id: 1-0xed35af169af46a02ee13b9d79eb57d6d68c1749e
    - Chain Id: 1
    - Contract Address: 0xed35af169af46a02ee13b9d79eb57d6d68c1749e
    - Name: ECOMI
    - Symbol: OMI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/19075.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 19075

---
+++ sdk/constants/token-map/1/definitions/1-0xed35af169af46a02ee13b9d79eb57d6d68c1749e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xed35af169af46a02ee13b9d79eb57d6d68c1749e",
+  chain_id: 1,
+  contract_address: "0xed35af169af46a02ee13b9d79eb57d6d68c1749e",
+  name: "ECOMI",
+  symbol: "OMI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/19075.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "19075",
+  type: "token",
+});
