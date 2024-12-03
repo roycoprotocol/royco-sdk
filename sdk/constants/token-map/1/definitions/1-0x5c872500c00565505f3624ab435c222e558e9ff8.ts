854121bbfa6f18c97b7db2315a3c974fab8bc1a6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:27 -0800

    feat(token-map): Update Token -- 1-0x5c872500c00565505f3624ab435c222e558e9ff8 -- CoTrader (COT) (0x5c87...9ff8)
    Token Details:
    - Id: 1-0x5c872500c00565505f3624ab435c222e558e9ff8
    - Chain Id: 1
    - Contract Address: 0x5c872500c00565505f3624ab435c222e558e9ff8
    - Name: CoTrader
    - Symbol: COT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3779.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3779

---
+++ sdk/constants/token-map/1/definitions/1-0x5c872500c00565505f3624ab435c222e558e9ff8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5c872500c00565505f3624ab435c222e558e9ff8",
+  chain_id: 1,
+  contract_address: "0x5c872500c00565505f3624ab435c222e558e9ff8",
+  name: "CoTrader",
+  symbol: "COT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3779.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3779",
+  type: "token",
+});
