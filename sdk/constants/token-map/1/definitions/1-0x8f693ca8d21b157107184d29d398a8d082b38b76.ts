a3126452f7251296818024d95a775cf07d7613a3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:48 -0800

    feat(token-map): Update Token -- 1-0x8f693ca8d21b157107184d29d398a8d082b38b76 -- Streamr (DATA) (0x8f69...8b76)
    Token Details:
    - Id: 1-0x8f693ca8d21b157107184d29d398a8d082b38b76
    - Chain Id: 1
    - Contract Address: 0x8f693ca8d21b157107184d29d398a8d082b38b76
    - Name: Streamr
    - Symbol: DATA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2143.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2143

---
+++ sdk/constants/token-map/1/definitions/1-0x8f693ca8d21b157107184d29d398a8d082b38b76.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8f693ca8d21b157107184d29d398a8d082b38b76",
+  chain_id: 1,
+  contract_address: "0x8f693ca8d21b157107184d29d398a8d082b38b76",
+  name: "Streamr",
+  symbol: "DATA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2143.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2143",
+  type: "token",
+});
