bea7e7d7348a4c336af6419403559a82a8f62131
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:22 -0800

    feat(token-map): Update Token -- 1-0xb23d80f5fefcddaa212212f028021b41ded428cf -- Echelon Prime (PRIME) (0xb23d...28cf)
    Token Details:
    - Id: 1-0xb23d80f5fefcddaa212212f028021b41ded428cf
    - Chain Id: 1
    - Contract Address: 0xb23d80f5fefcddaa212212f028021b41ded428cf
    - Name: Echelon Prime
    - Symbol: PRIME
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/23711.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 23711

---
+++ sdk/constants/token-map/1/definitions/1-0xb23d80f5fefcddaa212212f028021b41ded428cf.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb23d80f5fefcddaa212212f028021b41ded428cf",
+  chain_id: 1,
+  contract_address: "0xb23d80f5fefcddaa212212f028021b41ded428cf",
+  name: "Echelon Prime",
+  symbol: "PRIME",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/23711.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "23711",
+  type: "token",
+});
