73fe4510df3bac1bdd5af422facf32e0e7199197
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:03 -0800

    feat(token-map): Update Token -- 42161-0xaf88d065e77c8cc2239327c5edb3a432268e5831 -- USDC (USDC) (0xaf88...5831)
    Token Details:
    - Id: 42161-0xaf88d065e77c8cc2239327c5edb3a432268e5831
    - Chain Id: 42161
    - Contract Address: 0xaf88d065e77c8cc2239327c5edb3a432268e5831
    - Name: USDC
    - Symbol: USDC
    - Image: https://coin-images.coingecko.com/coins/images/6319/large/usdc.png?1696506694
    - Decimals: 6
    - Source: coingecko
    - Search Id: usd-coin

---
+++ sdk/constants/token-map/42161/definitions/42161-0xaf88d065e77c8cc2239327c5edb3a432268e5831.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+  chain_id: 42161,
+  contract_address: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+  name: "USDC",
+  symbol: "USDC",
+  image: "https://coin-images.coingecko.com/coins/images/6319/large/usdc.png?1696506694",
+  decimals: 6,
+  source: "coingecko",
+  search_id: "usd-coin",
+  type: "token",
+});
