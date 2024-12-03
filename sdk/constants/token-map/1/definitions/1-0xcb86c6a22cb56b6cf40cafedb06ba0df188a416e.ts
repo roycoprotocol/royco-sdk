e666dc7a3af36c5b9c1f4aaedd327bd4a59ea2b3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:11 -0800

    feat(token-map): Update Token -- 1-0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e -- inSure DeFi (SURE) (0xcb86...416e)
    Token Details:
    - Id: 1-0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e
    - Chain Id: 1
    - Contract Address: 0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e
    - Name: inSure DeFi
    - Symbol: SURE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5113.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5113

---
+++ sdk/constants/token-map/1/definitions/1-0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e",
+  chain_id: 1,
+  contract_address: "0xcb86c6a22cb56b6cf40cafedb06ba0df188a416e",
+  name: "inSure DeFi",
+  symbol: "SURE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5113.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5113",
+  type: "token",
+});
