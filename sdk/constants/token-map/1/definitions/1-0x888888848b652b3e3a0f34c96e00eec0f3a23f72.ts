dffe3688c672ecd309c1725a216ef4e8eba687e3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:24 -0800

    feat(token-map): Update Token -- 1-0x888888848b652b3e3a0f34c96e00eec0f3a23f72 -- Alien Worlds (TLM) (0x8888...3f72)
    Token Details:
    - Id: 1-0x888888848b652b3e3a0f34c96e00eec0f3a23f72
    - Chain Id: 1
    - Contract Address: 0x888888848b652b3e3a0f34c96e00eec0f3a23f72
    - Name: Alien Worlds
    - Symbol: TLM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9119.png
    - Decimals: 4
    - Source: coinmarketcap
    - Search Id: 9119

---
+++ sdk/constants/token-map/1/definitions/1-0x888888848b652b3e3a0f34c96e00eec0f3a23f72.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x888888848b652b3e3a0f34c96e00eec0f3a23f72",
+  chain_id: 1,
+  contract_address: "0x888888848b652b3e3a0f34c96e00eec0f3a23f72",
+  name: "Alien Worlds",
+  symbol: "TLM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9119.png",
+  decimals: 4,
+  source: "coinmarketcap",
+  search_id: "9119",
+  type: "token",
+});
