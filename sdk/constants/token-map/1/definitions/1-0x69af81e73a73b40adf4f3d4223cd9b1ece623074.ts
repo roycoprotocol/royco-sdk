ed6a51e8c28f9402fca2d7f066653473a0173e9a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:16:04 -0800

    feat(token-map): Update Token -- 1-0x69af81e73a73b40adf4f3d4223cd9b1ece623074 -- Mask Network (MASK) (0x69af...3074)
    Token Details:
    - Id: 1-0x69af81e73a73b40adf4f3d4223cd9b1ece623074
    - Chain Id: 1
    - Contract Address: 0x69af81e73a73b40adf4f3d4223cd9b1ece623074
    - Name: Mask Network
    - Symbol: MASK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8536.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8536

---
+++ sdk/constants/token-map/1/definitions/1-0x69af81e73a73b40adf4f3d4223cd9b1ece623074.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x69af81e73a73b40adf4f3d4223cd9b1ece623074",
+  chain_id: 1,
+  contract_address: "0x69af81e73a73b40adf4f3d4223cd9b1ece623074",
+  name: "Mask Network",
+  symbol: "MASK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8536.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8536",
+  type: "token",
+});
