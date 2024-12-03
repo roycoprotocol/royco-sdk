d87488de59b2008450f30c935f16dc9698b449de
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:01 -0800

    feat(token-map): Update Token -- 1-0xd2877702675e6ceb975b4a1dff9fb7baf4c91ea9 -- Wrapped LUNA Classic (WLUNC) (0xd287...1ea9)
    Token Details:
    - Id: 1-0xd2877702675e6ceb975b4a1dff9fb7baf4c91ea9
    - Chain Id: 1
    - Contract Address: 0xd2877702675e6ceb975b4a1dff9fb7baf4c91ea9
    - Name: Wrapped LUNA Classic
    - Symbol: WLUNC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11178.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11178

---
+++ sdk/constants/token-map/1/definitions/1-0xd2877702675e6ceb975b4a1dff9fb7baf4c91ea9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd2877702675e6ceb975b4a1dff9fb7baf4c91ea9",
+  chain_id: 1,
+  contract_address: "0xd2877702675e6ceb975b4a1dff9fb7baf4c91ea9",
+  name: "Wrapped LUNA Classic",
+  symbol: "WLUNC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11178.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11178",
+  type: "token",
+});
