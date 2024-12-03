4960808f5d4ac4eef171c971af7642b838b0db61
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:53 -0800

    feat(token-map): Update Token -- 1-0xde5ed76e7c05ec5e4572cfc88d1acea165109e44 -- DEUS Finance (DEUS) (0xde5e...9e44)
    Token Details:
    - Id: 1-0xde5ed76e7c05ec5e4572cfc88d1acea165109e44
    - Chain Id: 1
    - Contract Address: 0xde5ed76e7c05ec5e4572cfc88d1acea165109e44
    - Name: DEUS Finance
    - Symbol: DEUS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12972.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 12972

---
+++ sdk/constants/token-map/1/definitions/1-0xde5ed76e7c05ec5e4572cfc88d1acea165109e44.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xde5ed76e7c05ec5e4572cfc88d1acea165109e44",
+  chain_id: 1,
+  contract_address: "0xde5ed76e7c05ec5e4572cfc88d1acea165109e44",
+  name: "DEUS Finance",
+  symbol: "DEUS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12972.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "12972",
+  type: "token",
+});
