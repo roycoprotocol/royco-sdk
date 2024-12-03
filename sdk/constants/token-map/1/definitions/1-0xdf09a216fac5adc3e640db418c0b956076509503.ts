8eb68a1743411347310a85c532992bdeff846184
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:09 -0800

    feat(token-map): Update Token -- 1-0xdf09a216fac5adc3e640db418c0b956076509503 -- Poken (PKN) (0xdf09...9503)
    Token Details:
    - Id: 1-0xdf09a216fac5adc3e640db418c0b956076509503
    - Chain Id: 1
    - Contract Address: 0xdf09a216fac5adc3e640db418c0b956076509503
    - Name: Poken
    - Symbol: PKN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12692.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 12692

---
+++ sdk/constants/token-map/1/definitions/1-0xdf09a216fac5adc3e640db418c0b956076509503.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdf09a216fac5adc3e640db418c0b956076509503",
+  chain_id: 1,
+  contract_address: "0xdf09a216fac5adc3e640db418c0b956076509503",
+  name: "Poken",
+  symbol: "PKN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12692.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "12692",
+  type: "token",
+});
