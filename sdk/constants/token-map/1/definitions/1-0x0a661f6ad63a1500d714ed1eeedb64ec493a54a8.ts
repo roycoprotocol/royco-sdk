cf2c0002e5b3d1c3589e6f0da49da0118714863b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:43 -0800

    feat(token-map): Update Token -- 1-0x0a661f6ad63a1500d714ed1eeedb64ec493a54a8 -- Zedxion (USDZ) (0x0a66...54a8)
    Token Details:
    - Id: 1-0x0a661f6ad63a1500d714ed1eeedb64ec493a54a8
    - Chain Id: 1
    - Contract Address: 0x0a661f6ad63a1500d714ed1eeedb64ec493a54a8
    - Name: Zedxion
    - Symbol: USDZ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/20475.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 20475

---
+++ sdk/constants/token-map/1/definitions/1-0x0a661f6ad63a1500d714ed1eeedb64ec493a54a8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0a661f6ad63a1500d714ed1eeedb64ec493a54a8",
+  chain_id: 1,
+  contract_address: "0x0a661f6ad63a1500d714ed1eeedb64ec493a54a8",
+  name: "Zedxion",
+  symbol: "USDZ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/20475.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "20475",
+  type: "token",
+});
