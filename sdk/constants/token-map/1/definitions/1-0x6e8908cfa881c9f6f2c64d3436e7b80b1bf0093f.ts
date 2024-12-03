9189d04388fea61179915c7a56c524019269763f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:17:01 -0800

    feat(token-map): Update Token -- 1-0x6e8908cfa881c9f6f2c64d3436e7b80b1bf0093f -- Bistroo (BIST) (0x6e89...093f)
    Token Details:
    - Id: 1-0x6e8908cfa881c9f6f2c64d3436e7b80b1bf0093f
    - Chain Id: 1
    - Contract Address: 0x6e8908cfa881c9f6f2c64d3436e7b80b1bf0093f
    - Name: Bistroo
    - Symbol: BIST
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9889.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9889

---
+++ sdk/constants/token-map/1/definitions/1-0x6e8908cfa881c9f6f2c64d3436e7b80b1bf0093f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6e8908cfa881c9f6f2c64d3436e7b80b1bf0093f",
+  chain_id: 1,
+  contract_address: "0x6e8908cfa881c9f6f2c64d3436e7b80b1bf0093f",
+  name: "Bistroo",
+  symbol: "BIST",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9889.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9889",
+  type: "token",
+});
