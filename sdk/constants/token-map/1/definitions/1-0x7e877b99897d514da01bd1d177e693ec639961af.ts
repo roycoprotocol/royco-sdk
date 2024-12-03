80957786e8ce618a849240dde4e66b1c6beb98e7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:49 -0800

    feat(token-map): Update Token -- 1-0x7e877b99897d514da01bd1d177e693ec639961af -- Oggy Inu (ETH) (OGGY) (0x7e87...61af)
    Token Details:
    - Id: 1-0x7e877b99897d514da01bd1d177e693ec639961af
    - Chain Id: 1
    - Contract Address: 0x7e877b99897d514da01bd1d177e693ec639961af
    - Name: Oggy Inu (ETH)
    - Symbol: OGGY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28288.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 28288

---
+++ sdk/constants/token-map/1/definitions/1-0x7e877b99897d514da01bd1d177e693ec639961af.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7e877b99897d514da01bd1d177e693ec639961af",
+  chain_id: 1,
+  contract_address: "0x7e877b99897d514da01bd1d177e693ec639961af",
+  name: "Oggy Inu (ETH)",
+  symbol: "OGGY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28288.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "28288",
+  type: "token",
+});
