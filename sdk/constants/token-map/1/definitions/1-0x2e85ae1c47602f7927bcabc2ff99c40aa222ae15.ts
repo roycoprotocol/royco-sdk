7acbfc2c995c827e774a0a862f868bc0062c30d8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:25 -0800

    feat(token-map): Update Token -- 1-0x2e85ae1c47602f7927bcabc2ff99c40aa222ae15 -- Katana Inu (KATA) (0x2e85...ae15)
    Token Details:
    - Id: 1-0x2e85ae1c47602f7927bcabc2ff99c40aa222ae15
    - Chain Id: 1
    - Contract Address: 0x2e85ae1c47602f7927bcabc2ff99c40aa222ae15
    - Name: Katana Inu
    - Symbol: KATA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/15142.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 15142

---
+++ sdk/constants/token-map/1/definitions/1-0x2e85ae1c47602f7927bcabc2ff99c40aa222ae15.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2e85ae1c47602f7927bcabc2ff99c40aa222ae15",
+  chain_id: 1,
+  contract_address: "0x2e85ae1c47602f7927bcabc2ff99c40aa222ae15",
+  name: "Katana Inu",
+  symbol: "KATA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15142.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "15142",
+  type: "token",
+});
