db23109089625633f4ee97e3cdbe742164859394
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:58 -0800

    feat(token-map): Update Token -- 1-0x0f71b8de197a1c84d31de0f1fa7926c365f052b3 -- Arcona (ARCONA) (0x0f71...52b3)
    Token Details:
    - Id: 1-0x0f71b8de197a1c84d31de0f1fa7926c365f052b3
    - Chain Id: 1
    - Contract Address: 0x0f71b8de197a1c84d31de0f1fa7926c365f052b3
    - Name: Arcona
    - Symbol: ARCONA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6218.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6218

---
+++ sdk/constants/token-map/1/definitions/1-0x0f71b8de197a1c84d31de0f1fa7926c365f052b3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0f71b8de197a1c84d31de0f1fa7926c365f052b3",
+  chain_id: 1,
+  contract_address: "0x0f71b8de197a1c84d31de0f1fa7926c365f052b3",
+  name: "Arcona",
+  symbol: "ARCONA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6218.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6218",
+  type: "token",
+});
