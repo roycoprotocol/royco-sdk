933b1dc7b024a38e9fe0670b7ba7a028c952cf36
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:33 -0800

    feat(token-map): Update Token -- 1-0xcdf7028ceab81fa0c6971208e83fa7872994bee5 -- Threshold (T) (0xcdf7...bee5)
    Token Details:
    - Id: 1-0xcdf7028ceab81fa0c6971208e83fa7872994bee5
    - Chain Id: 1
    - Contract Address: 0xcdf7028ceab81fa0c6971208e83fa7872994bee5
    - Name: Threshold
    - Symbol: T
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/17751.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 17751

---
+++ sdk/constants/token-map/1/definitions/1-0xcdf7028ceab81fa0c6971208e83fa7872994bee5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xcdf7028ceab81fa0c6971208e83fa7872994bee5",
+  chain_id: 1,
+  contract_address: "0xcdf7028ceab81fa0c6971208e83fa7872994bee5",
+  name: "Threshold",
+  symbol: "T",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/17751.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "17751",
+  type: "token",
+});
