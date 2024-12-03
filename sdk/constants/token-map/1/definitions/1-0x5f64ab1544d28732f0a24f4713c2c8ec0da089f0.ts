fd9d8810a60d569561496cd8ed33002bacdaee78
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:49 -0800

    feat(token-map): Update Token -- 1-0x5f64ab1544d28732f0a24f4713c2c8ec0da089f0 -- Domani Protocol (DEXTF) (0x5f64...89f0)
    Token Details:
    - Id: 1-0x5f64ab1544d28732f0a24f4713c2c8ec0da089f0
    - Chain Id: 1
    - Contract Address: 0x5f64ab1544d28732f0a24f4713c2c8ec0da089f0
    - Name: Domani Protocol
    - Symbol: DEXTF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8691.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8691

---
+++ sdk/constants/token-map/1/definitions/1-0x5f64ab1544d28732f0a24f4713c2c8ec0da089f0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5f64ab1544d28732f0a24f4713c2c8ec0da089f0",
+  chain_id: 1,
+  contract_address: "0x5f64ab1544d28732f0a24f4713c2c8ec0da089f0",
+  name: "Domani Protocol",
+  symbol: "DEXTF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8691.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8691",
+  type: "token",
+});
