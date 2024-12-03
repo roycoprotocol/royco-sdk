d8a61ab975449659333116eef7fb2e9311fc7237
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:14 -0800

    feat(token-map): Update Token -- 1-0x6149c26cd2f7b5ccdb32029af817123f6e37df5b -- Launchpool (LPOOL) (0x6149...df5b)
    Token Details:
    - Id: 1-0x6149c26cd2f7b5ccdb32029af817123f6e37df5b
    - Chain Id: 1
    - Contract Address: 0x6149c26cd2f7b5ccdb32029af817123f6e37df5b
    - Name: Launchpool
    - Symbol: LPOOL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8545.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8545

---
+++ sdk/constants/token-map/1/definitions/1-0x6149c26cd2f7b5ccdb32029af817123f6e37df5b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6149c26cd2f7b5ccdb32029af817123f6e37df5b",
+  chain_id: 1,
+  contract_address: "0x6149c26cd2f7b5ccdb32029af817123f6e37df5b",
+  name: "Launchpool",
+  symbol: "LPOOL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8545.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8545",
+  type: "token",
+});
