f51bdeb4a80c3c781e68fd73218704242fd2e465
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:20 -0800

    feat(token-map): Update Token -- 1-0x7b744eea1deca2f1b7b31f15ba036fa1759452d7 -- El Hippo (HIPP) (0x7b74...52d7)
    Token Details:
    - Id: 1-0x7b744eea1deca2f1b7b31f15ba036fa1759452d7
    - Chain Id: 1
    - Contract Address: 0x7b744eea1deca2f1b7b31f15ba036fa1759452d7
    - Name: El Hippo
    - Symbol: HIPP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/27903.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 27903

---
+++ sdk/constants/token-map/1/definitions/1-0x7b744eea1deca2f1b7b31f15ba036fa1759452d7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7b744eea1deca2f1b7b31f15ba036fa1759452d7",
+  chain_id: 1,
+  contract_address: "0x7b744eea1deca2f1b7b31f15ba036fa1759452d7",
+  name: "El Hippo",
+  symbol: "HIPP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/27903.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "27903",
+  type: "token",
+});
