cef7e7ffc4da3555993af238b6e9b24913cac4bd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:44 -0800

    feat(token-map): Update Token -- 1-0x76e222b07c53d28b89b0bac18602810fc22b49a8 -- Joe coin (JOE) (0x76e2...49a8)
    Token Details:
    - Id: 1-0x76e222b07c53d28b89b0bac18602810fc22b49a8
    - Chain Id: 1
    - Contract Address: 0x76e222b07c53d28b89b0bac18602810fc22b49a8
    - Name: Joe coin
    - Symbol: JOE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28504.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 28504

---
+++ sdk/constants/token-map/1/definitions/1-0x76e222b07c53d28b89b0bac18602810fc22b49a8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x76e222b07c53d28b89b0bac18602810fc22b49a8",
+  chain_id: 1,
+  contract_address: "0x76e222b07c53d28b89b0bac18602810fc22b49a8",
+  name: "Joe coin",
+  symbol: "JOE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28504.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "28504",
+  type: "token",
+});
