77a35173a84818fab638a798b531785276a62004
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:28 -0800

    feat(token-map): Update Token -- 1-0x179e31fb25e433441a2839389a7b8ec9c4654b7b -- SynchroBitcoin (SNB) (0x179e...4b7b)
    Token Details:
    - Id: 1-0x179e31fb25e433441a2839389a7b8ec9c4654b7b
    - Chain Id: 1
    - Contract Address: 0x179e31fb25e433441a2839389a7b8ec9c4654b7b
    - Name: SynchroBitcoin
    - Symbol: SNB
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5277.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5277

---
+++ sdk/constants/token-map/1/definitions/1-0x179e31fb25e433441a2839389a7b8ec9c4654b7b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x179e31fb25e433441a2839389a7b8ec9c4654b7b",
+  chain_id: 1,
+  contract_address: "0x179e31fb25e433441a2839389a7b8ec9c4654b7b",
+  name: "SynchroBitcoin",
+  symbol: "SNB",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5277.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5277",
+  type: "token",
+});
