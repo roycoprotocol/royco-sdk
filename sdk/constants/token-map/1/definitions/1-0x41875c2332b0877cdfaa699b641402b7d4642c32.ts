0c3b505d8f4f15d7a14843b2da3d914d1c0ceb1b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:43 -0800

    feat(token-map): Update Token -- 1-0x41875c2332b0877cdfaa699b641402b7d4642c32 -- FUTURAX (FTXT) (0x4187...2c32)
    Token Details:
    - Id: 1-0x41875c2332b0877cdfaa699b641402b7d4642c32
    - Chain Id: 1
    - Contract Address: 0x41875c2332b0877cdfaa699b641402b7d4642c32
    - Name: FUTURAX
    - Symbol: FTXT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3219.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 3219

---
+++ sdk/constants/token-map/1/definitions/1-0x41875c2332b0877cdfaa699b641402b7d4642c32.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x41875c2332b0877cdfaa699b641402b7d4642c32",
+  chain_id: 1,
+  contract_address: "0x41875c2332b0877cdfaa699b641402b7d4642c32",
+  name: "FUTURAX",
+  symbol: "FTXT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3219.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3219",
+  type: "token",
+});
