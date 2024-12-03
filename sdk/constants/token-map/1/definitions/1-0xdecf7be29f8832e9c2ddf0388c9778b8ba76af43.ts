544aa7db70e06ef72ce89fb58230807767c97a75
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:03 -0800

    feat(token-map): Update Token -- 1-0xdecf7be29f8832e9c2ddf0388c9778b8ba76af43 -- BonusCloud (BXC) (0xdecf...af43)
    Token Details:
    - Id: 1-0xdecf7be29f8832e9c2ddf0388c9778b8ba76af43
    - Chain Id: 1
    - Contract Address: 0xdecf7be29f8832e9c2ddf0388c9778b8ba76af43
    - Name: BonusCloud
    - Symbol: BXC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4703.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4703

---
+++ sdk/constants/token-map/1/definitions/1-0xdecf7be29f8832e9c2ddf0388c9778b8ba76af43.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdecf7be29f8832e9c2ddf0388c9778b8ba76af43",
+  chain_id: 1,
+  contract_address: "0xdecf7be29f8832e9c2ddf0388c9778b8ba76af43",
+  name: "BonusCloud",
+  symbol: "BXC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4703.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4703",
+  type: "token",
+});
