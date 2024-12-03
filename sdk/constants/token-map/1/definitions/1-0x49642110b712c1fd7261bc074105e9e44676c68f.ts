b3302306e6511b7a47d0185e274faec4ee98bf99
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:22 -0800

    feat(token-map): Update Token -- 1-0x49642110b712c1fd7261bc074105e9e44676c68f -- DinoLFG (DINO) (0x4964...c68f)
    Token Details:
    - Id: 1-0x49642110b712c1fd7261bc074105e9e44676c68f
    - Chain Id: 1
    - Contract Address: 0x49642110b712c1fd7261bc074105e9e44676c68f
    - Name: DinoLFG
    - Symbol: DINO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/22680.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 22680

---
+++ sdk/constants/token-map/1/definitions/1-0x49642110b712c1fd7261bc074105e9e44676c68f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x49642110b712c1fd7261bc074105e9e44676c68f",
+  chain_id: 1,
+  contract_address: "0x49642110b712c1fd7261bc074105e9e44676c68f",
+  name: "DinoLFG",
+  symbol: "DINO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/22680.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "22680",
+  type: "token",
+});
