6462667b9bfa45ccb69e175fd05a82d69e4c140a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:57 -0800

    feat(token-map): Update Token -- 1-0x53884b61963351c283118a8e1fc05ba464a11959 -- MONNOS (MNS) (0x5388...1959)
    Token Details:
    - Id: 1-0x53884b61963351c283118a8e1fc05ba464a11959
    - Chain Id: 1
    - Contract Address: 0x53884b61963351c283118a8e1fc05ba464a11959
    - Name: MONNOS
    - Symbol: MNS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5702.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5702

---
+++ sdk/constants/token-map/1/definitions/1-0x53884b61963351c283118a8e1fc05ba464a11959.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x53884b61963351c283118a8e1fc05ba464a11959",
+  chain_id: 1,
+  contract_address: "0x53884b61963351c283118a8e1fc05ba464a11959",
+  name: "MONNOS",
+  symbol: "MNS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5702.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5702",
+  type: "token",
+});
