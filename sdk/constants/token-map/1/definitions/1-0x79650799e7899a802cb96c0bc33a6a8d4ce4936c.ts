7e038ce49a3c8ef36d880aad36128ca1f9fedcb5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:58 -0800

    feat(token-map): Update Token -- 1-0x79650799e7899a802cb96c0bc33a6a8d4ce4936c -- AICHAIN (AIT) (0x7965...936c)
    Token Details:
    - Id: 1-0x79650799e7899a802cb96c0bc33a6a8d4ce4936c
    - Chain Id: 1
    - Contract Address: 0x79650799e7899a802cb96c0bc33a6a8d4ce4936c
    - Name: AICHAIN
    - Symbol: AIT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2407.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2407

---
+++ sdk/constants/token-map/1/definitions/1-0x79650799e7899a802cb96c0bc33a6a8d4ce4936c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x79650799e7899a802cb96c0bc33a6a8d4ce4936c",
+  chain_id: 1,
+  contract_address: "0x79650799e7899a802cb96c0bc33a6a8d4ce4936c",
+  name: "AICHAIN",
+  symbol: "AIT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2407.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2407",
+  type: "token",
+});
