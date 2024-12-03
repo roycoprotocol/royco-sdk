addc3af0f07dfe673d4cb353c992d01a7ad6f23f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:26 -0800

    feat(token-map): Update Token -- 1-0x9469d013805bffb7d3debe5e7839237e535ec483 -- Darwinia Network (RING) (0x9469...c483)
    Token Details:
    - Id: 1-0x9469d013805bffb7d3debe5e7839237e535ec483
    - Chain Id: 1
    - Contract Address: 0x9469d013805bffb7d3debe5e7839237e535ec483
    - Name: Darwinia Network
    - Symbol: RING
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5798.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5798

---
+++ sdk/constants/token-map/1/definitions/1-0x9469d013805bffb7d3debe5e7839237e535ec483.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9469d013805bffb7d3debe5e7839237e535ec483",
+  chain_id: 1,
+  contract_address: "0x9469d013805bffb7d3debe5e7839237e535ec483",
+  name: "Darwinia Network",
+  symbol: "RING",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5798.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5798",
+  type: "token",
+});
