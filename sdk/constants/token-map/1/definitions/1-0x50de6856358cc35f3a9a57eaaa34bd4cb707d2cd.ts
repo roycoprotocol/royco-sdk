a1329a75d62d8c8ffb582af98d5b796a25b2c331
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:38 -0800

    feat(token-map): Update Token -- 1-0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd -- Razor Network (RAZOR) (0x50de...d2cd)
    Token Details:
    - Id: 1-0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd
    - Chain Id: 1
    - Contract Address: 0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd
    - Name: Razor Network
    - Symbol: RAZOR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8409.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8409

---
+++ sdk/constants/token-map/1/definitions/1-0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd",
+  chain_id: 1,
+  contract_address: "0x50de6856358cc35f3a9a57eaaa34bd4cb707d2cd",
+  name: "Razor Network",
+  symbol: "RAZOR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8409.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8409",
+  type: "token",
+});
