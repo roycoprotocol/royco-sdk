7bcfa4a3770951700010662f829160883cb35504
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:35 -0800

    feat(token-map): Update Token -- 1-0x34be5b8c30ee4fde069dc878989686abe9884470 -- SENATE (SENATE) (0x34be...4470)
    Token Details:
    - Id: 1-0x34be5b8c30ee4fde069dc878989686abe9884470
    - Chain Id: 1
    - Contract Address: 0x34be5b8c30ee4fde069dc878989686abe9884470
    - Name: SENATE
    - Symbol: SENATE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/15240.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 15240

---
+++ sdk/constants/token-map/1/definitions/1-0x34be5b8c30ee4fde069dc878989686abe9884470.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x34be5b8c30ee4fde069dc878989686abe9884470",
+  chain_id: 1,
+  contract_address: "0x34be5b8c30ee4fde069dc878989686abe9884470",
+  name: "SENATE",
+  symbol: "SENATE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15240.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "15240",
+  type: "token",
+});
