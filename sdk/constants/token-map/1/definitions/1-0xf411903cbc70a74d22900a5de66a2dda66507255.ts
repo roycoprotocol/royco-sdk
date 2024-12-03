297c0e7734751bbef95ee38fe10fa19faebcba7d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:22 -0800

    feat(token-map): Update Token -- 1-0xf411903cbc70a74d22900a5de66a2dda66507255 -- Verasity (VRA) (0xf411...7255)
    Token Details:
    - Id: 1-0xf411903cbc70a74d22900a5de66a2dda66507255
    - Chain Id: 1
    - Contract Address: 0xf411903cbc70a74d22900a5de66a2dda66507255
    - Name: Verasity
    - Symbol: VRA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3816.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3816

---
+++ sdk/constants/token-map/1/definitions/1-0xf411903cbc70a74d22900a5de66a2dda66507255.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf411903cbc70a74d22900a5de66a2dda66507255",
+  chain_id: 1,
+  contract_address: "0xf411903cbc70a74d22900a5de66a2dda66507255",
+  name: "Verasity",
+  symbol: "VRA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3816.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3816",
+  type: "token",
+});
