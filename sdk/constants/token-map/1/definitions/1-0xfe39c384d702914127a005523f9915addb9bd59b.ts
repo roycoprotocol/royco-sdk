05a588561eff572d1e9350a1cbdc3c3bdb908d58
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:40:03 -0800

    feat(token-map): Update Token -- 1-0xfe39c384d702914127a005523f9915addb9bd59b -- Hippocrat (HPO) (0xfe39...d59b)
    Token Details:
    - Id: 1-0xfe39c384d702914127a005523f9915addb9bd59b
    - Chain Id: 1
    - Contract Address: 0xfe39c384d702914127a005523f9915addb9bd59b
    - Name: Hippocrat
    - Symbol: HPO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3600.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3600

---
+++ sdk/constants/token-map/1/definitions/1-0xfe39c384d702914127a005523f9915addb9bd59b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfe39c384d702914127a005523f9915addb9bd59b",
+  chain_id: 1,
+  contract_address: "0xfe39c384d702914127a005523f9915addb9bd59b",
+  name: "Hippocrat",
+  symbol: "HPO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3600.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3600",
+  type: "token",
+});
