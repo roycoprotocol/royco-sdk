23dc65667c8a8b16fac13a16e43cc4d3846d52d9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:46 -0800

    feat(token-map): Update Token -- 1-0x51db5ad35c671a87207d88fc11d593ac0c8415bd -- Moeda Loyalty Points (MDA) (0x51db...15bd)
    Token Details:
    - Id: 1-0x51db5ad35c671a87207d88fc11d593ac0c8415bd
    - Chain Id: 1
    - Contract Address: 0x51db5ad35c671a87207d88fc11d593ac0c8415bd
    - Name: Moeda Loyalty Points
    - Symbol: MDA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1954.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1954

---
+++ sdk/constants/token-map/1/definitions/1-0x51db5ad35c671a87207d88fc11d593ac0c8415bd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x51db5ad35c671a87207d88fc11d593ac0c8415bd",
+  chain_id: 1,
+  contract_address: "0x51db5ad35c671a87207d88fc11d593ac0c8415bd",
+  name: "Moeda Loyalty Points",
+  symbol: "MDA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1954.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1954",
+  type: "token",
+});
