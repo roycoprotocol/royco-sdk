c5aa76e8b3bd9f9a6156fdf44be77fe129a96060
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:47 -0800

    feat(token-map): Update Token -- 1-0xd559f20296ff4895da39b5bd9add54b442596a61 -- FintruX Network (FTX) (0xd559...6a61)
    Token Details:
    - Id: 1-0xd559f20296ff4895da39b5bd9add54b442596a61
    - Chain Id: 1
    - Contract Address: 0xd559f20296ff4895da39b5bd9add54b442596a61
    - Name: FintruX Network
    - Symbol: FTX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2667.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2667

---
+++ sdk/constants/token-map/1/definitions/1-0xd559f20296ff4895da39b5bd9add54b442596a61.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd559f20296ff4895da39b5bd9add54b442596a61",
+  chain_id: 1,
+  contract_address: "0xd559f20296ff4895da39b5bd9add54b442596a61",
+  name: "FintruX Network",
+  symbol: "FTX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2667.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2667",
+  type: "token",
+});
