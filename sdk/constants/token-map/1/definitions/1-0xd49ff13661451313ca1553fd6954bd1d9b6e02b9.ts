bc8d96b3ec8187ce6feecd3d4cd80db344c60bd1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:34 -0800

    feat(token-map): Update Token -- 1-0xd49ff13661451313ca1553fd6954bd1d9b6e02b9 -- Electrify.Asia (ELEC) (0xd49f...02b9)
    Token Details:
    - Id: 1-0xd49ff13661451313ca1553fd6954bd1d9b6e02b9
    - Chain Id: 1
    - Contract Address: 0xd49ff13661451313ca1553fd6954bd1d9b6e02b9
    - Name: Electrify.Asia
    - Symbol: ELEC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2573.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2573

---
+++ sdk/constants/token-map/1/definitions/1-0xd49ff13661451313ca1553fd6954bd1d9b6e02b9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd49ff13661451313ca1553fd6954bd1d9b6e02b9",
+  chain_id: 1,
+  contract_address: "0xd49ff13661451313ca1553fd6954bd1d9b6e02b9",
+  name: "Electrify.Asia",
+  symbol: "ELEC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2573.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2573",
+  type: "token",
+});
