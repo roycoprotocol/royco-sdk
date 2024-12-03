0272fb87f11979372df4fff118b385525a4c32a1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:36:16 -0800

    feat(token-map): Update Token -- 1-0xe803178b48a0e560c2b19f3b3d4e504f79d229ce -- bobcoin (BOBC) (0xe803...29ce)
    Token Details:
    - Id: 1-0xe803178b48a0e560c2b19f3b3d4e504f79d229ce
    - Chain Id: 1
    - Contract Address: 0xe803178b48a0e560c2b19f3b3d4e504f79d229ce
    - Name: bobcoin
    - Symbol: BOBC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/18762.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 18762

---
+++ sdk/constants/token-map/1/definitions/1-0xe803178b48a0e560c2b19f3b3d4e504f79d229ce.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe803178b48a0e560c2b19f3b3d4e504f79d229ce",
+  chain_id: 1,
+  contract_address: "0xe803178b48a0e560c2b19f3b3d4e504f79d229ce",
+  name: "bobcoin",
+  symbol: "BOBC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/18762.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "18762",
+  type: "token",
+});
