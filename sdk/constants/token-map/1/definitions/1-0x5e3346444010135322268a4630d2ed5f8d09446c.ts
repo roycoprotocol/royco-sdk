145eb3c02eaa2b06ecb152c52fc4b5f32ad6af73
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:13:42 -0800

    feat(token-map): Update Token -- 1-0x5e3346444010135322268a4630d2ed5f8d09446c -- LockTrip (LOC) (0x5e33...446c)
    Token Details:
    - Id: 1-0x5e3346444010135322268a4630d2ed5f8d09446c
    - Chain Id: 1
    - Contract Address: 0x5e3346444010135322268a4630d2ed5f8d09446c
    - Name: LockTrip
    - Symbol: LOC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2287.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2287

---
+++ sdk/constants/token-map/1/definitions/1-0x5e3346444010135322268a4630d2ed5f8d09446c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5e3346444010135322268a4630d2ed5f8d09446c",
+  chain_id: 1,
+  contract_address: "0x5e3346444010135322268a4630d2ed5f8d09446c",
+  name: "LockTrip",
+  symbol: "LOC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2287.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2287",
+  type: "token",
+});
