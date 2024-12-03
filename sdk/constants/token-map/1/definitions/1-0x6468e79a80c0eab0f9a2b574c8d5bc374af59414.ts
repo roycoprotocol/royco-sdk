a03cde1eafcd43019af90fbac3742b8b1a004763
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:53 -0800

    feat(token-map): Update Token -- 1-0x6468e79a80c0eab0f9a2b574c8d5bc374af59414 -- e-Radix (EXRD) (0x6468...9414)
    Token Details:
    - Id: 1-0x6468e79a80c0eab0f9a2b574c8d5bc374af59414
    - Chain Id: 1
    - Contract Address: 0x6468e79a80c0eab0f9a2b574c8d5bc374af59414
    - Name: e-Radix
    - Symbol: EXRD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7692.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7692

---
+++ sdk/constants/token-map/1/definitions/1-0x6468e79a80c0eab0f9a2b574c8d5bc374af59414.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6468e79a80c0eab0f9a2b574c8d5bc374af59414",
+  chain_id: 1,
+  contract_address: "0x6468e79a80c0eab0f9a2b574c8d5bc374af59414",
+  name: "e-Radix",
+  symbol: "EXRD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7692.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7692",
+  type: "token",
+});
