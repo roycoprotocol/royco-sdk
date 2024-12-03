e8c9cda8b6383b512157736fef6187a865473ff0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:54 -0800

    feat(token-map): Update Token -- 1-0x4730fb1463a6f1f44aeb45f6c5c422427f37f4d0 -- 4P FOUR (FOUR) (0x4730...f4d0)
    Token Details:
    - Id: 1-0x4730fb1463a6f1f44aeb45f6c5c422427f37f4d0
    - Chain Id: 1
    - Contract Address: 0x4730fb1463a6f1f44aeb45f6c5c422427f37f4d0
    - Name: 4P FOUR
    - Symbol: FOUR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5409.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5409

---
+++ sdk/constants/token-map/1/definitions/1-0x4730fb1463a6f1f44aeb45f6c5c422427f37f4d0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4730fb1463a6f1f44aeb45f6c5c422427f37f4d0",
+  chain_id: 1,
+  contract_address: "0x4730fb1463a6f1f44aeb45f6c5c422427f37f4d0",
+  name: "4P FOUR",
+  symbol: "FOUR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5409.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5409",
+  type: "token",
+});
