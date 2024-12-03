741ad3b73b6760bc0212bec86d5b092e1d3f043d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:09 -0800

    feat(token-map): Update Token -- 1-0xd2b274cfbf9534f56b59ad0fb7e645e0354f4941 -- XDOGE (XDOGE) (0xd2b2...4941)
    Token Details:
    - Id: 1-0xd2b274cfbf9534f56b59ad0fb7e645e0354f4941
    - Chain Id: 1
    - Contract Address: 0xd2b274cfbf9534f56b59ad0fb7e645e0354f4941
    - Name: XDOGE
    - Symbol: XDOGE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/27691.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 27691

---
+++ sdk/constants/token-map/1/definitions/1-0xd2b274cfbf9534f56b59ad0fb7e645e0354f4941.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd2b274cfbf9534f56b59ad0fb7e645e0354f4941",
+  chain_id: 1,
+  contract_address: "0xd2b274cfbf9534f56b59ad0fb7e645e0354f4941",
+  name: "XDOGE",
+  symbol: "XDOGE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/27691.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "27691",
+  type: "token",
+});
