0e5d85dcee1ceebcbe796954f9dfc240fc26239b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:24 -0800

    feat(token-map): Update Token -- 1-0xeec2be5c91ae7f8a338e1e5f3b5de49d07afdc81 -- Dopex (DPX) (0xeec2...dc81)
    Token Details:
    - Id: 1-0xeec2be5c91ae7f8a338e1e5f3b5de49d07afdc81
    - Chain Id: 1
    - Contract Address: 0xeec2be5c91ae7f8a338e1e5f3b5de49d07afdc81
    - Name: Dopex
    - Symbol: DPX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11188.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11188

---
+++ sdk/constants/token-map/1/definitions/1-0xeec2be5c91ae7f8a338e1e5f3b5de49d07afdc81.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xeec2be5c91ae7f8a338e1e5f3b5de49d07afdc81",
+  chain_id: 1,
+  contract_address: "0xeec2be5c91ae7f8a338e1e5f3b5de49d07afdc81",
+  name: "Dopex",
+  symbol: "DPX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11188.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11188",
+  type: "token",
+});
