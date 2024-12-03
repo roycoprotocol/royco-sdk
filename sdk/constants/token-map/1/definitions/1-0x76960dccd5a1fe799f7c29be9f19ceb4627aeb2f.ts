22f2289da8e38f89ceff7cb83552470fd05b1029
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:40 -0800

    feat(token-map): Update Token -- 1-0x76960dccd5a1fe799f7c29be9f19ceb4627aeb2f -- RED (RED) (0x7696...eb2f)
    Token Details:
    - Id: 1-0x76960dccd5a1fe799f7c29be9f19ceb4627aeb2f
    - Chain Id: 1
    - Contract Address: 0x76960dccd5a1fe799f7c29be9f19ceb4627aeb2f
    - Name: RED
    - Symbol: RED
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2771.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2771

---
+++ sdk/constants/token-map/1/definitions/1-0x76960dccd5a1fe799f7c29be9f19ceb4627aeb2f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x76960dccd5a1fe799f7c29be9f19ceb4627aeb2f",
+  chain_id: 1,
+  contract_address: "0x76960dccd5a1fe799f7c29be9f19ceb4627aeb2f",
+  name: "RED",
+  symbol: "RED",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2771.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2771",
+  type: "token",
+});
