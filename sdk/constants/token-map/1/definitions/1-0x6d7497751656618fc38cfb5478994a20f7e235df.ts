89fe7ca8fd7d1cce72cca068ec0eaf0952345a66
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:16:43 -0800

    feat(token-map): Update Token -- 1-0x6d7497751656618fc38cfb5478994a20f7e235df -- SPYRO (SPYRO) (0x6d74...35df)
    Token Details:
    - Id: 1-0x6d7497751656618fc38cfb5478994a20f7e235df
    - Chain Id: 1
    - Contract Address: 0x6d7497751656618fc38cfb5478994a20f7e235df
    - Name: SPYRO
    - Symbol: SPYRO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29412.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29412

---
+++ sdk/constants/token-map/1/definitions/1-0x6d7497751656618fc38cfb5478994a20f7e235df.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6d7497751656618fc38cfb5478994a20f7e235df",
+  chain_id: 1,
+  contract_address: "0x6d7497751656618fc38cfb5478994a20f7e235df",
+  name: "SPYRO",
+  symbol: "SPYRO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29412.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29412",
+  type: "token",
+});
