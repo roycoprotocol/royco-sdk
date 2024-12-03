1e04ab9b754134a065cc40df851d57c9d24e2890
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:05 -0800

    feat(token-map): Update Token -- 1-0xca1207647ff814039530d7d35df0e1dd2e91fa84 -- dHedge DAO (DHT) (0xca12...fa84)
    Token Details:
    - Id: 1-0xca1207647ff814039530d7d35df0e1dd2e91fa84
    - Chain Id: 1
    - Contract Address: 0xca1207647ff814039530d7d35df0e1dd2e91fa84
    - Name: dHedge DAO
    - Symbol: DHT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7094.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7094

---
+++ sdk/constants/token-map/1/definitions/1-0xca1207647ff814039530d7d35df0e1dd2e91fa84.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xca1207647ff814039530d7d35df0e1dd2e91fa84",
+  chain_id: 1,
+  contract_address: "0xca1207647ff814039530d7d35df0e1dd2e91fa84",
+  name: "dHedge DAO",
+  symbol: "DHT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7094.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7094",
+  type: "token",
+});
