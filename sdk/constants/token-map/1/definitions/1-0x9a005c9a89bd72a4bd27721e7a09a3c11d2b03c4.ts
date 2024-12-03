289fc3f0a3187e148a4b295fef07dd3e8bd86175
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:18 -0800

    feat(token-map): Update Token -- 1-0x9a005c9a89bd72a4bd27721e7a09a3c11d2b03c4 -- StarterCoin (STAC) (0x9a00...03c4)
    Token Details:
    - Id: 1-0x9a005c9a89bd72a4bd27721e7a09a3c11d2b03c4
    - Chain Id: 1
    - Contract Address: 0x9a005c9a89bd72a4bd27721e7a09a3c11d2b03c4
    - Name: StarterCoin
    - Symbol: STAC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2565.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2565

---
+++ sdk/constants/token-map/1/definitions/1-0x9a005c9a89bd72a4bd27721e7a09a3c11d2b03c4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9a005c9a89bd72a4bd27721e7a09a3c11d2b03c4",
+  chain_id: 1,
+  contract_address: "0x9a005c9a89bd72a4bd27721e7a09a3c11d2b03c4",
+  name: "StarterCoin",
+  symbol: "STAC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2565.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2565",
+  type: "token",
+});
