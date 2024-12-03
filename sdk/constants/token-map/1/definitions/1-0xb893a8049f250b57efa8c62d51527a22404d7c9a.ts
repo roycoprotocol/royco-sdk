601e6c515cd5f5aa6e1b3986793e8c398d57b59e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:12 -0800

    feat(token-map): Update Token -- 1-0xb893a8049f250b57efa8c62d51527a22404d7c9a -- American Shiba (USHIBA) (0xb893...7c9a)
    Token Details:
    - Id: 1-0xb893a8049f250b57efa8c62d51527a22404d7c9a
    - Chain Id: 1
    - Contract Address: 0xb893a8049f250b57efa8c62d51527a22404d7c9a
    - Name: American Shiba
    - Symbol: USHIBA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9943.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 9943

---
+++ sdk/constants/token-map/1/definitions/1-0xb893a8049f250b57efa8c62d51527a22404d7c9a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb893a8049f250b57efa8c62d51527a22404d7c9a",
+  chain_id: 1,
+  contract_address: "0xb893a8049f250b57efa8c62d51527a22404d7c9a",
+  name: "American Shiba",
+  symbol: "USHIBA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9943.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "9943",
+  type: "token",
+});
