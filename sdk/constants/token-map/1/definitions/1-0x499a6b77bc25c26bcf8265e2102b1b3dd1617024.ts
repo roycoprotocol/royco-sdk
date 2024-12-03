2ed66fe982ab224a0b0d86c713ea43b34ba95d50
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:24 -0800

    feat(token-map): Update Token -- 1-0x499a6b77bc25c26bcf8265e2102b1b3dd1617024 -- Bitether (BTR) (0x499a...7024)
    Token Details:
    - Id: 1-0x499a6b77bc25c26bcf8265e2102b1b3dd1617024
    - Chain Id: 1
    - Contract Address: 0x499a6b77bc25c26bcf8265e2102b1b3dd1617024
    - Name: Bitether
    - Symbol: BTR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3256.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3256

---
+++ sdk/constants/token-map/1/definitions/1-0x499a6b77bc25c26bcf8265e2102b1b3dd1617024.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x499a6b77bc25c26bcf8265e2102b1b3dd1617024",
+  chain_id: 1,
+  contract_address: "0x499a6b77bc25c26bcf8265e2102b1b3dd1617024",
+  name: "Bitether",
+  symbol: "BTR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3256.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3256",
+  type: "token",
+});
