52e1af57c3081d46917214ddfca3b61ecb896dc5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:40 -0800

    feat(token-map): Update Token -- 1-0xae0585a259a3bcab258d6ee02fb583f7b33c2a12 -- TemDAO (TEM) (0xae05...2a12)
    Token Details:
    - Id: 1-0xae0585a259a3bcab258d6ee02fb583f7b33c2a12
    - Chain Id: 1
    - Contract Address: 0xae0585a259a3bcab258d6ee02fb583f7b33c2a12
    - Name: TemDAO
    - Symbol: TEM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/23082.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 23082

---
+++ sdk/constants/token-map/1/definitions/1-0xae0585a259a3bcab258d6ee02fb583f7b33c2a12.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xae0585a259a3bcab258d6ee02fb583f7b33c2a12",
+  chain_id: 1,
+  contract_address: "0xae0585a259a3bcab258d6ee02fb583f7b33c2a12",
+  name: "TemDAO",
+  symbol: "TEM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/23082.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "23082",
+  type: "token",
+});
