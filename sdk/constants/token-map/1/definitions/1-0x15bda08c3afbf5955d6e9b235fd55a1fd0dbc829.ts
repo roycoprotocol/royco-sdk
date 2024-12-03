4af5b8a76c2e6e21d9e0b646754e08b25b8ea771
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:12 -0800

    feat(token-map): Update Token -- 1-0x15bda08c3afbf5955d6e9b235fd55a1fd0dbc829 -- Alpha Coin (APC) (0x15bd...c829)
    Token Details:
    - Id: 1-0x15bda08c3afbf5955d6e9b235fd55a1fd0dbc829
    - Chain Id: 1
    - Contract Address: 0x15bda08c3afbf5955d6e9b235fd55a1fd0dbc829
    - Name: Alpha Coin
    - Symbol: APC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3512.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 3512

---
+++ sdk/constants/token-map/1/definitions/1-0x15bda08c3afbf5955d6e9b235fd55a1fd0dbc829.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x15bda08c3afbf5955d6e9b235fd55a1fd0dbc829",
+  chain_id: 1,
+  contract_address: "0x15bda08c3afbf5955d6e9b235fd55a1fd0dbc829",
+  name: "Alpha Coin",
+  symbol: "APC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3512.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "3512",
+  type: "token",
+});
