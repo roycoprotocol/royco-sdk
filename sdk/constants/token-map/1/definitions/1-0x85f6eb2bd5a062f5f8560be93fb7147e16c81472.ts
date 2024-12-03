51d67d96a4c70c0404bd58031e0b21563d23b712
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:01 -0800

    feat(token-map): Update Token -- 1-0x85f6eb2bd5a062f5f8560be93fb7147e16c81472 -- Franklin (FLY) (0x85f6...1472)
    Token Details:
    - Id: 1-0x85f6eb2bd5a062f5f8560be93fb7147e16c81472
    - Chain Id: 1
    - Contract Address: 0x85f6eb2bd5a062f5f8560be93fb7147e16c81472
    - Name: Franklin
    - Symbol: FLY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9120.png
    - Decimals: 4
    - Source: coinmarketcap
    - Search Id: 9120

---
+++ sdk/constants/token-map/1/definitions/1-0x85f6eb2bd5a062f5f8560be93fb7147e16c81472.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x85f6eb2bd5a062f5f8560be93fb7147e16c81472",
+  chain_id: 1,
+  contract_address: "0x85f6eb2bd5a062f5f8560be93fb7147e16c81472",
+  name: "Franklin",
+  symbol: "FLY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9120.png",
+  decimals: 4,
+  source: "coinmarketcap",
+  search_id: "9120",
+  type: "token",
+});
