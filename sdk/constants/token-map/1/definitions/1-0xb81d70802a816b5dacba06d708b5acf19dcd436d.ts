5ca38cb0bcfa6cbdd19025ec4abb9f0329f1774b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:07 -0800

    feat(token-map): Update Token -- 1-0xb81d70802a816b5dacba06d708b5acf19dcd436d -- Dextoken (DEXG) (0xb81d...436d)
    Token Details:
    - Id: 1-0xb81d70802a816b5dacba06d708b5acf19dcd436d
    - Chain Id: 1
    - Contract Address: 0xb81d70802a816b5dacba06d708b5acf19dcd436d
    - Name: Dextoken
    - Symbol: DEXG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6684.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6684

---
+++ sdk/constants/token-map/1/definitions/1-0xb81d70802a816b5dacba06d708b5acf19dcd436d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb81d70802a816b5dacba06d708b5acf19dcd436d",
+  chain_id: 1,
+  contract_address: "0xb81d70802a816b5dacba06d708b5acf19dcd436d",
+  name: "Dextoken",
+  symbol: "DEXG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6684.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6684",
+  type: "token",
+});
