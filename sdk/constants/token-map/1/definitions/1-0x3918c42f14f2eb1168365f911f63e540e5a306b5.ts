413552e937e996e4e8d2c896cbecea260bc57e85
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:23 -0800

    feat(token-map): Update Token -- 1-0x3918c42f14f2eb1168365f911f63e540e5a306b5 -- Neural Protocol (NRP) (0x3918...06b5)
    Token Details:
    - Id: 1-0x3918c42f14f2eb1168365f911f63e540e5a306b5
    - Chain Id: 1
    - Contract Address: 0x3918c42f14f2eb1168365f911f63e540e5a306b5
    - Name: Neural Protocol
    - Symbol: NRP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3397.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 3397

---
+++ sdk/constants/token-map/1/definitions/1-0x3918c42f14f2eb1168365f911f63e540e5a306b5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3918c42f14f2eb1168365f911f63e540e5a306b5",
+  chain_id: 1,
+  contract_address: "0x3918c42f14f2eb1168365f911f63e540e5a306b5",
+  name: "Neural Protocol",
+  symbol: "NRP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3397.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3397",
+  type: "token",
+});
