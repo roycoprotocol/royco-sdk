e04a778123a58fdd4c1b4863a3b5b5c1cf51606d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:19 -0800

    feat(token-map): Update Token -- 1-0x56015bbe3c01fe05bc30a8a9a9fd9a88917e7db3 -- Cat Token (CAT) (0x5601...7db3)
    Token Details:
    - Id: 1-0x56015bbe3c01fe05bc30a8a9a9fd9a88917e7db3
    - Chain Id: 1
    - Contract Address: 0x56015bbe3c01fe05bc30a8a9a9fd9a88917e7db3
    - Name: Cat Token
    - Symbol: CAT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6649.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6649

---
+++ sdk/constants/token-map/1/definitions/1-0x56015bbe3c01fe05bc30a8a9a9fd9a88917e7db3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x56015bbe3c01fe05bc30a8a9a9fd9a88917e7db3",
+  chain_id: 1,
+  contract_address: "0x56015bbe3c01fe05bc30a8a9a9fd9a88917e7db3",
+  name: "Cat Token",
+  symbol: "CAT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6649.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6649",
+  type: "token",
+});
