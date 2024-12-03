08062fc8cccc3eb1d248b4f64d018bd76cedb2ff
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:18 -0800

    feat(token-map): Update Token -- 1-0xd33526068d116ce69f19a9ee46f0bd304f21a51f -- Rocket Pool (RPL) (0xd335...a51f)
    Token Details:
    - Id: 1-0xd33526068d116ce69f19a9ee46f0bd304f21a51f
    - Chain Id: 1
    - Contract Address: 0xd33526068d116ce69f19a9ee46f0bd304f21a51f
    - Name: Rocket Pool
    - Symbol: RPL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2943.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2943

---
+++ sdk/constants/token-map/1/definitions/1-0xd33526068d116ce69f19a9ee46f0bd304f21a51f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd33526068d116ce69f19a9ee46f0bd304f21a51f",
+  chain_id: 1,
+  contract_address: "0xd33526068d116ce69f19a9ee46f0bd304f21a51f",
+  name: "Rocket Pool",
+  symbol: "RPL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2943.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2943",
+  type: "token",
+});
