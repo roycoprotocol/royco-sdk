0d6f4b415722797668e07405d27a46ac5a5dce05
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:44 -0800

    feat(token-map): Update Token -- 1-0x3136ef851592acf49ca4c825131e364170fa32b3 -- CoinFi (COFI) (0x3136...32b3)
    Token Details:
    - Id: 1-0x3136ef851592acf49ca4c825131e364170fa32b3
    - Chain Id: 1
    - Contract Address: 0x3136ef851592acf49ca4c825131e364170fa32b3
    - Name: CoinFi
    - Symbol: COFI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2478.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2478

---
+++ sdk/constants/token-map/1/definitions/1-0x3136ef851592acf49ca4c825131e364170fa32b3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3136ef851592acf49ca4c825131e364170fa32b3",
+  chain_id: 1,
+  contract_address: "0x3136ef851592acf49ca4c825131e364170fa32b3",
+  name: "CoinFi",
+  symbol: "COFI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2478.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2478",
+  type: "token",
+});
