d2e23ec30650bfec2630bbbc9fa345a9cba11b58
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:09 -0800

    feat(token-map): Update Token -- 1-0x87210f1d3422ba75b6c40c63c78d79324dabcd55 -- EOS TRUST (EOST) (0x8721...cd55)
    Token Details:
    - Id: 1-0x87210f1d3422ba75b6c40c63c78d79324dabcd55
    - Chain Id: 1
    - Contract Address: 0x87210f1d3422ba75b6c40c63c78d79324dabcd55
    - Name: EOS TRUST
    - Symbol: EOST
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4124.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4124

---
+++ sdk/constants/token-map/1/definitions/1-0x87210f1d3422ba75b6c40c63c78d79324dabcd55.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x87210f1d3422ba75b6c40c63c78d79324dabcd55",
+  chain_id: 1,
+  contract_address: "0x87210f1d3422ba75b6c40c63c78d79324dabcd55",
+  name: "EOS TRUST",
+  symbol: "EOST",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4124.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4124",
+  type: "token",
+});
