0710c0f092c7186c22131fc6a8db5165d3fb10d2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:04 -0800

    feat(token-map): Update Token -- 1-0xb0280743b44bf7db4b6be482b2ba7b75e5da096c -- Transcodium (TNS) (0xb028...096c)
    Token Details:
    - Id: 1-0xb0280743b44bf7db4b6be482b2ba7b75e5da096c
    - Chain Id: 1
    - Contract Address: 0xb0280743b44bf7db4b6be482b2ba7b75e5da096c
    - Name: Transcodium
    - Symbol: TNS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2704.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2704

---
+++ sdk/constants/token-map/1/definitions/1-0xb0280743b44bf7db4b6be482b2ba7b75e5da096c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb0280743b44bf7db4b6be482b2ba7b75e5da096c",
+  chain_id: 1,
+  contract_address: "0xb0280743b44bf7db4b6be482b2ba7b75e5da096c",
+  name: "Transcodium",
+  symbol: "TNS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2704.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2704",
+  type: "token",
+});
