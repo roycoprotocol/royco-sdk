5f9a134a21aac34e56126cdfc2544bba91759080
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:42 -0800

    feat(token-map): Update Token -- 1-0xa8580f3363684d76055bdc6660caefe8709744e1 -- Folder Protocol (FOL) (0xa858...44e1)
    Token Details:
    - Id: 1-0xa8580f3363684d76055bdc6660caefe8709744e1
    - Chain Id: 1
    - Contract Address: 0xa8580f3363684d76055bdc6660caefe8709744e1
    - Name: Folder Protocol
    - Symbol: FOL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7687.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7687

---
+++ sdk/constants/token-map/1/definitions/1-0xa8580f3363684d76055bdc6660caefe8709744e1.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa8580f3363684d76055bdc6660caefe8709744e1",
+  chain_id: 1,
+  contract_address: "0xa8580f3363684d76055bdc6660caefe8709744e1",
+  name: "Folder Protocol",
+  symbol: "FOL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7687.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7687",
+  type: "token",
+});
