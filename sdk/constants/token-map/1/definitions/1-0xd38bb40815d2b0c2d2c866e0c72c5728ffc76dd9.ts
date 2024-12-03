5ddf1ba0cfc69869f794dbc2fa9893b2b5656c26
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:20 -0800

    feat(token-map): Update Token -- 1-0xd38bb40815d2b0c2d2c866e0c72c5728ffc76dd9 -- Symbiosis (SIS) (0xd38b...6dd9)
    Token Details:
    - Id: 1-0xd38bb40815d2b0c2d2c866e0c72c5728ffc76dd9
    - Chain Id: 1
    - Contract Address: 0xd38bb40815d2b0c2d2c866e0c72c5728ffc76dd9
    - Name: Symbiosis
    - Symbol: SIS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/15084.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 15084

---
+++ sdk/constants/token-map/1/definitions/1-0xd38bb40815d2b0c2d2c866e0c72c5728ffc76dd9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd38bb40815d2b0c2d2c866e0c72c5728ffc76dd9",
+  chain_id: 1,
+  contract_address: "0xd38bb40815d2b0c2d2c866e0c72c5728ffc76dd9",
+  name: "Symbiosis",
+  symbol: "SIS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15084.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "15084",
+  type: "token",
+});
