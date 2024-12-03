bb0804fc24b5e98a5be485224ad89c9171bb885f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:39 -0800

    feat(token-map): Update Token -- 1-0x18aaa7115705e8be94bffebde57af9bfc265b998 -- Audius (AUDIO) (0x18aa...b998)
    Token Details:
    - Id: 1-0x18aaa7115705e8be94bffebde57af9bfc265b998
    - Chain Id: 1
    - Contract Address: 0x18aaa7115705e8be94bffebde57af9bfc265b998
    - Name: Audius
    - Symbol: AUDIO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7455.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7455

---
+++ sdk/constants/token-map/1/definitions/1-0x18aaa7115705e8be94bffebde57af9bfc265b998.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x18aaa7115705e8be94bffebde57af9bfc265b998",
+  chain_id: 1,
+  contract_address: "0x18aaa7115705e8be94bffebde57af9bfc265b998",
+  name: "Audius",
+  symbol: "AUDIO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7455.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7455",
+  type: "token",
+});
