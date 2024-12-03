995fd3705a2d1ade654c70f94d29b9b872ebcbc5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:25 -0800

    feat(token-map): Update Token -- 1-0x9ab165d795019b6d8b3e971dda91071421305e5a -- Aurora (AOA) (0x9ab1...5e5a)
    Token Details:
    - Id: 1-0x9ab165d795019b6d8b3e971dda91071421305e5a
    - Chain Id: 1
    - Contract Address: 0x9ab165d795019b6d8b3e971dda91071421305e5a
    - Name: Aurora
    - Symbol: AOA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2874.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2874

---
+++ sdk/constants/token-map/1/definitions/1-0x9ab165d795019b6d8b3e971dda91071421305e5a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9ab165d795019b6d8b3e971dda91071421305e5a",
+  chain_id: 1,
+  contract_address: "0x9ab165d795019b6d8b3e971dda91071421305e5a",
+  name: "Aurora",
+  symbol: "AOA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2874.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2874",
+  type: "token",
+});
