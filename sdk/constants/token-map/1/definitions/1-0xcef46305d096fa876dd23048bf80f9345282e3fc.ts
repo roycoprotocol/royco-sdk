51befe851e78030b26920f73d2051cbfc4e95ac9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:35 -0800

    feat(token-map): Update Token -- 1-0xcef46305d096fa876dd23048bf80f9345282e3fc -- Banque Universal (CBU) (0xcef4...e3fc)
    Token Details:
    - Id: 1-0xcef46305d096fa876dd23048bf80f9345282e3fc
    - Chain Id: 1
    - Contract Address: 0xcef46305d096fa876dd23048bf80f9345282e3fc
    - Name: Banque Universal
    - Symbol: CBU
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6377.png
    - Decimals: 0
    - Source: coinmarketcap
    - Search Id: 6377

---
+++ sdk/constants/token-map/1/definitions/1-0xcef46305d096fa876dd23048bf80f9345282e3fc.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xcef46305d096fa876dd23048bf80f9345282e3fc",
+  chain_id: 1,
+  contract_address: "0xcef46305d096fa876dd23048bf80f9345282e3fc",
+  name: "Banque Universal",
+  symbol: "CBU",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6377.png",
+  decimals: 0,
+  source: "coinmarketcap",
+  search_id: "6377",
+  type: "token",
+});
