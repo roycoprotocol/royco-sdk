98b2b2a9b6ae4c0907aec0e4724518c0648d8f3f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:53 -0800

    feat(token-map): Update Token -- 1-0x8ae4bf2c33a8e667de34b54938b0ccd03eb8cc06 -- Patientory (PTOY) (0x8ae4...cc06)
    Token Details:
    - Id: 1-0x8ae4bf2c33a8e667de34b54938b0ccd03eb8cc06
    - Chain Id: 1
    - Contract Address: 0x8ae4bf2c33a8e667de34b54938b0ccd03eb8cc06
    - Name: Patientory
    - Symbol: PTOY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1708.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 1708

---
+++ sdk/constants/token-map/1/definitions/1-0x8ae4bf2c33a8e667de34b54938b0ccd03eb8cc06.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8ae4bf2c33a8e667de34b54938b0ccd03eb8cc06",
+  chain_id: 1,
+  contract_address: "0x8ae4bf2c33a8e667de34b54938b0ccd03eb8cc06",
+  name: "Patientory",
+  symbol: "PTOY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1708.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "1708",
+  type: "token",
+});
