61cabd3c2bf9d02afe62c78627823a007da055aa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:52 -0800

    feat(token-map): Update Token -- 1-0xa2120b9e674d3fc3875f415a7df52e382f141225 -- Automata Network (ATA) (0xa212...1225)
    Token Details:
    - Id: 1-0xa2120b9e674d3fc3875f415a7df52e382f141225
    - Chain Id: 1
    - Contract Address: 0xa2120b9e674d3fc3875f415a7df52e382f141225
    - Name: Automata Network
    - Symbol: ATA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10188.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10188

---
+++ sdk/constants/token-map/1/definitions/1-0xa2120b9e674d3fc3875f415a7df52e382f141225.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa2120b9e674d3fc3875f415a7df52e382f141225",
+  chain_id: 1,
+  contract_address: "0xa2120b9e674d3fc3875f415a7df52e382f141225",
+  name: "Automata Network",
+  symbol: "ATA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10188.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10188",
+  type: "token",
+});
