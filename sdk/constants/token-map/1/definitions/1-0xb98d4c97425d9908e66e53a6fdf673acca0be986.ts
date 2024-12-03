aad78902f440b7edb9e44085f1d5d554de6012cb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:20 -0800

    feat(token-map): Update Token -- 1-0xb98d4c97425d9908e66e53a6fdf673acca0be986 -- Arcblock (ABT) (0xb98d...e986)
    Token Details:
    - Id: 1-0xb98d4c97425d9908e66e53a6fdf673acca0be986
    - Chain Id: 1
    - Contract Address: 0xb98d4c97425d9908e66e53a6fdf673acca0be986
    - Name: Arcblock
    - Symbol: ABT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2545.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2545

---
+++ sdk/constants/token-map/1/definitions/1-0xb98d4c97425d9908e66e53a6fdf673acca0be986.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb98d4c97425d9908e66e53a6fdf673acca0be986",
+  chain_id: 1,
+  contract_address: "0xb98d4c97425d9908e66e53a6fdf673acca0be986",
+  name: "Arcblock",
+  symbol: "ABT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2545.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2545",
+  type: "token",
+});
