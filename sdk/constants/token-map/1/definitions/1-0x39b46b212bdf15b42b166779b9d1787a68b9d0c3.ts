a9973f7e99f1e77469e361c964c8b4664f45788b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:28 -0800

    feat(token-map): Update Token -- 1-0x39b46b212bdf15b42b166779b9d1787a68b9d0c3 -- Dypius [New] (DYP) (0x39b4...d0c3)
    Token Details:
    - Id: 1-0x39b46b212bdf15b42b166779b9d1787a68b9d0c3
    - Chain Id: 1
    - Contract Address: 0x39b46b212bdf15b42b166779b9d1787a68b9d0c3
    - Name: Dypius [New]
    - Symbol: DYP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28543.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 28543

---
+++ sdk/constants/token-map/1/definitions/1-0x39b46b212bdf15b42b166779b9d1787a68b9d0c3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x39b46b212bdf15b42b166779b9d1787a68b9d0c3",
+  chain_id: 1,
+  contract_address: "0x39b46b212bdf15b42b166779b9d1787a68b9d0c3",
+  name: "Dypius [New]",
+  symbol: "DYP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28543.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "28543",
+  type: "token",
+});
