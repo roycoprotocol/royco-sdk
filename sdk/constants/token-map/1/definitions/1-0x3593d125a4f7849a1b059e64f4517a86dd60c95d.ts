c7364807a70f3db8211970ab419b86e002e70a78
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:43 -0800

    feat(token-map): Update Token -- 1-0x3593d125a4f7849a1b059e64f4517a86dd60c95d -- MANTRA (OM) (0x3593...c95d)
    Token Details:
    - Id: 1-0x3593d125a4f7849a1b059e64f4517a86dd60c95d
    - Chain Id: 1
    - Contract Address: 0x3593d125a4f7849a1b059e64f4517a86dd60c95d
    - Name: MANTRA
    - Symbol: OM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6536.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6536

---
+++ sdk/constants/token-map/1/definitions/1-0x3593d125a4f7849a1b059e64f4517a86dd60c95d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3593d125a4f7849a1b059e64f4517a86dd60c95d",
+  chain_id: 1,
+  contract_address: "0x3593d125a4f7849a1b059e64f4517a86dd60c95d",
+  name: "MANTRA",
+  symbol: "OM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6536.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6536",
+  type: "token",
+});
