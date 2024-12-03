215f806a4346235e67387b0d3c2cc14f79f21aef
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:29 -0800

    feat(token-map): Update Token -- 42161-0x69eb4fa4a2fbd498c257c57ea8b7655a2559a581 -- DODO (DODO) (0x69eb...a581)
    Token Details:
    - Id: 42161-0x69eb4fa4a2fbd498c257c57ea8b7655a2559a581
    - Chain Id: 42161
    - Contract Address: 0x69eb4fa4a2fbd498c257c57ea8b7655a2559a581
    - Name: DODO
    - Symbol: DODO
    - Image: https://coin-images.coingecko.com/coins/images/12651/large/dodo_logo.png?1696512458
    - Decimals: 18
    - Source: coingecko
    - Search Id: dodo

---
+++ sdk/constants/token-map/42161/definitions/42161-0x69eb4fa4a2fbd498c257c57ea8b7655a2559a581.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x69eb4fa4a2fbd498c257c57ea8b7655a2559a581",
+  chain_id: 42161,
+  contract_address: "0x69eb4fa4a2fbd498c257c57ea8b7655a2559a581",
+  name: "DODO",
+  symbol: "DODO",
+  image: "https://coin-images.coingecko.com/coins/images/12651/large/dodo_logo.png?1696512458",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "dodo",
+  type: "token",
+});
