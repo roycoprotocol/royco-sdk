9e4e6a29fdd923b583e67e23146ee082c5002f52
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:52 -0800

    feat(token-map): Update Token -- 1-0xb6ee9668771a79be7967ee29a63d4184f8097143 -- CargoX (CXO) (0xb6ee...7143)
    Token Details:
    - Id: 1-0xb6ee9668771a79be7967ee29a63d4184f8097143
    - Chain Id: 1
    - Contract Address: 0xb6ee9668771a79be7967ee29a63d4184f8097143
    - Name: CargoX
    - Symbol: CXO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2490.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2490

---
+++ sdk/constants/token-map/1/definitions/1-0xb6ee9668771a79be7967ee29a63d4184f8097143.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb6ee9668771a79be7967ee29a63d4184f8097143",
+  chain_id: 1,
+  contract_address: "0xb6ee9668771a79be7967ee29a63d4184f8097143",
+  name: "CargoX",
+  symbol: "CXO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2490.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2490",
+  type: "token",
+});
