9e7199f70c84ddf0d0f34a09f645df85ece6ebe1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:00:19 -0800

    feat(token-map): Update Token -- 1-0x111111111117dc0aa78b770fa6a738034120c302 -- 1inch Network (1INCH) (0x1111...c302)
    Token Details:
    - Id: 1-0x111111111117dc0aa78b770fa6a738034120c302
    - Chain Id: 1
    - Contract Address: 0x111111111117dc0aa78b770fa6a738034120c302
    - Name: 1inch Network
    - Symbol: 1INCH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8104.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8104

---
+++ sdk/constants/token-map/1/definitions/1-0x111111111117dc0aa78b770fa6a738034120c302.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x111111111117dc0aa78b770fa6a738034120c302",
+  chain_id: 1,
+  contract_address: "0x111111111117dc0aa78b770fa6a738034120c302",
+  name: "1inch Network",
+  symbol: "1INCH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8104.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8104",
+  type: "token",
+});
