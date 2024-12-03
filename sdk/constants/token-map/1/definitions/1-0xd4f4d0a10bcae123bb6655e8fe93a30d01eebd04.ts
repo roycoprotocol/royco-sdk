4e86f7f6ca6b473d3282ea216c6c3bb2f2d809b1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:38 -0800

    feat(token-map): Update Token -- 1-0xd4f4d0a10bcae123bb6655e8fe93a30d01eebd04 -- LinqAI (LNQ) (0xd4f4...bd04)
    Token Details:
    - Id: 1-0xd4f4d0a10bcae123bb6655e8fe93a30d01eebd04
    - Chain Id: 1
    - Contract Address: 0xd4f4d0a10bcae123bb6655e8fe93a30d01eebd04
    - Name: LinqAI
    - Symbol: LNQ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/30177.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 30177

---
+++ sdk/constants/token-map/1/definitions/1-0xd4f4d0a10bcae123bb6655e8fe93a30d01eebd04.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd4f4d0a10bcae123bb6655e8fe93a30d01eebd04",
+  chain_id: 1,
+  contract_address: "0xd4f4d0a10bcae123bb6655e8fe93a30d01eebd04",
+  name: "LinqAI",
+  symbol: "LNQ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/30177.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "30177",
+  type: "token",
+});
