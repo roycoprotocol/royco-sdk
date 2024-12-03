9abb4459b871f1f17ff858623da95d5ba4471999
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:12:45 -0800

    feat(token-map): Update Token -- 1-0x57c75eccc8557136d32619a191fbcdc88560d711 -- VeriDocGlobal (VDG) (0x57c7...d711)
    Token Details:
    - Id: 1-0x57c75eccc8557136d32619a191fbcdc88560d711
    - Chain Id: 1
    - Contract Address: 0x57c75eccc8557136d32619a191fbcdc88560d711
    - Name: VeriDocGlobal
    - Symbol: VDG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3205.png
    - Decimals: 0
    - Source: coinmarketcap
    - Search Id: 3205

---
+++ sdk/constants/token-map/1/definitions/1-0x57c75eccc8557136d32619a191fbcdc88560d711.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x57c75eccc8557136d32619a191fbcdc88560d711",
+  chain_id: 1,
+  contract_address: "0x57c75eccc8557136d32619a191fbcdc88560d711",
+  name: "VeriDocGlobal",
+  symbol: "VDG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3205.png",
+  decimals: 0,
+  source: "coinmarketcap",
+  search_id: "3205",
+  type: "token",
+});
