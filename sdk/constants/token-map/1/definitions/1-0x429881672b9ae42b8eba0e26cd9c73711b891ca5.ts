018afc4b28e46f8b257db2e6f009407002a012cd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:04 -0800

    feat(token-map): Update Token -- 1-0x429881672b9ae42b8eba0e26cd9c73711b891ca5 -- Pickle Finance (PICKLE) (0x4298...1ca5)
    Token Details:
    - Id: 1-0x429881672b9ae42b8eba0e26cd9c73711b891ca5
    - Chain Id: 1
    - Contract Address: 0x429881672b9ae42b8eba0e26cd9c73711b891ca5
    - Name: Pickle Finance
    - Symbol: PICKLE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7022.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7022

---
+++ sdk/constants/token-map/1/definitions/1-0x429881672b9ae42b8eba0e26cd9c73711b891ca5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x429881672b9ae42b8eba0e26cd9c73711b891ca5",
+  chain_id: 1,
+  contract_address: "0x429881672b9ae42b8eba0e26cd9c73711b891ca5",
+  name: "Pickle Finance",
+  symbol: "PICKLE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7022.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7022",
+  type: "token",
+});
