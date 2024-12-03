56f6624ffb426c9782decc50006d498e70d8bccb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:35 -0800

    feat(token-map): Update Token -- 1-0x9b4e2b4b13d125238aa0480dd42b4f6fc71b37cc -- MyToken (MT) (0x9b4e...37cc)
    Token Details:
    - Id: 1-0x9b4e2b4b13d125238aa0480dd42b4f6fc71b37cc
    - Chain Id: 1
    - Contract Address: 0x9b4e2b4b13d125238aa0480dd42b4f6fc71b37cc
    - Name: MyToken
    - Symbol: MT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2712.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2712

---
+++ sdk/constants/token-map/1/definitions/1-0x9b4e2b4b13d125238aa0480dd42b4f6fc71b37cc.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9b4e2b4b13d125238aa0480dd42b4f6fc71b37cc",
+  chain_id: 1,
+  contract_address: "0x9b4e2b4b13d125238aa0480dd42b4f6fc71b37cc",
+  name: "MyToken",
+  symbol: "MT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2712.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2712",
+  type: "token",
+});
