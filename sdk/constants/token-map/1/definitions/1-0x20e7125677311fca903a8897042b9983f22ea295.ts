bbeaddb061373643b9bb18846b6af98c8e0d19b6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:03:07 -0800

    feat(token-map): Update Token -- 1-0x20e7125677311fca903a8897042b9983f22ea295 -- Freeway Token (FWT) (0x20e7...a295)
    Token Details:
    - Id: 1-0x20e7125677311fca903a8897042b9983f22ea295
    - Chain Id: 1
    - Contract Address: 0x20e7125677311fca903a8897042b9983f22ea295
    - Name: Freeway Token
    - Symbol: FWT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7585.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7585

---
+++ sdk/constants/token-map/1/definitions/1-0x20e7125677311fca903a8897042b9983f22ea295.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x20e7125677311fca903a8897042b9983f22ea295",
+  chain_id: 1,
+  contract_address: "0x20e7125677311fca903a8897042b9983f22ea295",
+  name: "Freeway Token",
+  symbol: "FWT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7585.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7585",
+  type: "token",
+});
