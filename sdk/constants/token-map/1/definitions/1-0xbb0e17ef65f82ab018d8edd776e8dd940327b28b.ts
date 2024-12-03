e23d43f7adc353cde892e96f68335e0941e984e2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:47 -0800

    feat(token-map): Update Token -- 1-0xbb0e17ef65f82ab018d8edd776e8dd940327b28b -- Axie Infinity (AXS) (0xbb0e...b28b)
    Token Details:
    - Id: 1-0xbb0e17ef65f82ab018d8edd776e8dd940327b28b
    - Chain Id: 1
    - Contract Address: 0xbb0e17ef65f82ab018d8edd776e8dd940327b28b
    - Name: Axie Infinity
    - Symbol: AXS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6783.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6783

---
+++ sdk/constants/token-map/1/definitions/1-0xbb0e17ef65f82ab018d8edd776e8dd940327b28b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbb0e17ef65f82ab018d8edd776e8dd940327b28b",
+  chain_id: 1,
+  contract_address: "0xbb0e17ef65f82ab018d8edd776e8dd940327b28b",
+  name: "Axie Infinity",
+  symbol: "AXS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6783.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6783",
+  type: "token",
+});
