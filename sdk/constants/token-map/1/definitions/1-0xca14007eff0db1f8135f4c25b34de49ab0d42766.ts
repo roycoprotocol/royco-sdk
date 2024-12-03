b7e96a6975b2cb343d30e92f4eb2adc0f2917738
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:32:07 -0800

    feat(token-map): Update Token -- 1-0xca14007eff0db1f8135f4c25b34de49ab0d42766 -- Starknet (STRK) (0xca14...2766)
    Token Details:
    - Id: 1-0xca14007eff0db1f8135f4c25b34de49ab0d42766
    - Chain Id: 1
    - Contract Address: 0xca14007eff0db1f8135f4c25b34de49ab0d42766
    - Name: Starknet
    - Symbol: STRK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/22691.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 22691

---
+++ sdk/constants/token-map/1/definitions/1-0xca14007eff0db1f8135f4c25b34de49ab0d42766.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xca14007eff0db1f8135f4c25b34de49ab0d42766",
+  chain_id: 1,
+  contract_address: "0xca14007eff0db1f8135f4c25b34de49ab0d42766",
+  name: "Starknet",
+  symbol: "STRK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/22691.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "22691",
+  type: "token",
+});
