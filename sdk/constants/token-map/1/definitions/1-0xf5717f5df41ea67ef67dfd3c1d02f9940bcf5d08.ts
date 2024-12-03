7ff403b412f3854073a653515b9d468fa8ef0fb4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:37 -0800

    feat(token-map): Update Token -- 1-0xf5717f5df41ea67ef67dfd3c1d02f9940bcf5d08 -- SeChain (SNN) (0xf571...5d08)
    Token Details:
    - Id: 1-0xf5717f5df41ea67ef67dfd3c1d02f9940bcf5d08
    - Chain Id: 1
    - Contract Address: 0xf5717f5df41ea67ef67dfd3c1d02f9940bcf5d08
    - Name: SeChain
    - Symbol: SNN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6179.png
    - Decimals: 3
    - Source: coinmarketcap
    - Search Id: 6179

---
+++ sdk/constants/token-map/1/definitions/1-0xf5717f5df41ea67ef67dfd3c1d02f9940bcf5d08.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf5717f5df41ea67ef67dfd3c1d02f9940bcf5d08",
+  chain_id: 1,
+  contract_address: "0xf5717f5df41ea67ef67dfd3c1d02f9940bcf5d08",
+  name: "SeChain",
+  symbol: "SNN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6179.png",
+  decimals: 3,
+  source: "coinmarketcap",
+  search_id: "6179",
+  type: "token",
+});
