18b7d3d78613cab46adbb80cfeae7062d7902ff9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:00:28 -0800

    feat(token-map): Update Token -- 1-0x1245ef80f4d9e02ed9425375e8f649b9221b31d8 -- ArbitrageCT (ARCT) (0x1245...31d8)
    Token Details:
    - Id: 1-0x1245ef80f4d9e02ed9425375e8f649b9221b31d8
    - Chain Id: 1
    - Contract Address: 0x1245ef80f4d9e02ed9425375e8f649b9221b31d8
    - Name: ArbitrageCT
    - Symbol: ARCT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2415.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2415

---
+++ sdk/constants/token-map/1/definitions/1-0x1245ef80f4d9e02ed9425375e8f649b9221b31d8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1245ef80f4d9e02ed9425375e8f649b9221b31d8",
+  chain_id: 1,
+  contract_address: "0x1245ef80f4d9e02ed9425375e8f649b9221b31d8",
+  name: "ArbitrageCT",
+  symbol: "ARCT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2415.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2415",
+  type: "token",
+});
