ffbdf49b5821794d8a20ff254615a038b52e1e39
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:48 -0800

    feat(token-map): Update Token -- 1-0xb683d83a532e2cb7dfa5275eed3698436371cc9f -- BTU Protocol (BTU) (0xb683...cc9f)
    Token Details:
    - Id: 1-0xb683d83a532e2cb7dfa5275eed3698436371cc9f
    - Chain Id: 1
    - Contract Address: 0xb683d83a532e2cb7dfa5275eed3698436371cc9f
    - Name: BTU Protocol
    - Symbol: BTU
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3737.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3737

---
+++ sdk/constants/token-map/1/definitions/1-0xb683d83a532e2cb7dfa5275eed3698436371cc9f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb683d83a532e2cb7dfa5275eed3698436371cc9f",
+  chain_id: 1,
+  contract_address: "0xb683d83a532e2cb7dfa5275eed3698436371cc9f",
+  name: "BTU Protocol",
+  symbol: "BTU",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3737.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3737",
+  type: "token",
+});
