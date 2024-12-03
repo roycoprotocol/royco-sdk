42d6ee7c0673697136eb8a40de94ccef8a9e0aa7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:26 -0800

    feat(token-map): Update Token -- 1-0x88a9a52f944315d5b4e917b9689e65445c401e83 -- FEAR (FEAR) (0x88a9...1e83)
    Token Details:
    - Id: 1-0x88a9a52f944315d5b4e917b9689e65445c401e83
    - Chain Id: 1
    - Contract Address: 0x88a9a52f944315d5b4e917b9689e65445c401e83
    - Name: FEAR
    - Symbol: FEAR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9866.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9866

---
+++ sdk/constants/token-map/1/definitions/1-0x88a9a52f944315d5b4e917b9689e65445c401e83.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x88a9a52f944315d5b4e917b9689e65445c401e83",
+  chain_id: 1,
+  contract_address: "0x88a9a52f944315d5b4e917b9689e65445c401e83",
+  name: "FEAR",
+  symbol: "FEAR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9866.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9866",
+  type: "token",
+});
