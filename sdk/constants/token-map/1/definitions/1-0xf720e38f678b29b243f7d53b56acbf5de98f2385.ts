471370ccbfbf38cca4a3187549d831d057e3e307
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:51 -0800

    feat(token-map): Update Token -- 1-0xf720e38f678b29b243f7d53b56acbf5de98f2385 -- Upfire (UPR) (0xf720...2385)
    Token Details:
    - Id: 1-0xf720e38f678b29b243f7d53b56acbf5de98f2385
    - Chain Id: 1
    - Contract Address: 0xf720e38f678b29b243f7d53b56acbf5de98f2385
    - Name: Upfire
    - Symbol: UPR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9694.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9694

---
+++ sdk/constants/token-map/1/definitions/1-0xf720e38f678b29b243f7d53b56acbf5de98f2385.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf720e38f678b29b243f7d53b56acbf5de98f2385",
+  chain_id: 1,
+  contract_address: "0xf720e38f678b29b243f7d53b56acbf5de98f2385",
+  name: "Upfire",
+  symbol: "UPR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9694.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9694",
+  type: "token",
+});
