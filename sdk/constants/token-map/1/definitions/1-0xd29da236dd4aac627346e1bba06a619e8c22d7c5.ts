c26f8442013e577b4f5c5a535ff6b97aadc3fbe1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:07 -0800

    feat(token-map): Update Token -- 1-0xd29da236dd4aac627346e1bba06a619e8c22d7c5 -- MAGA (maga-hat.vip) (MAGA) (0xd29d...d7c5)
    Token Details:
    - Id: 1-0xd29da236dd4aac627346e1bba06a619e8c22d7c5
    - Chain Id: 1
    - Contract Address: 0xd29da236dd4aac627346e1bba06a619e8c22d7c5
    - Name: MAGA (maga-hat.vip)
    - Symbol: MAGA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/31305.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 31305

---
+++ sdk/constants/token-map/1/definitions/1-0xd29da236dd4aac627346e1bba06a619e8c22d7c5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd29da236dd4aac627346e1bba06a619e8c22d7c5",
+  chain_id: 1,
+  contract_address: "0xd29da236dd4aac627346e1bba06a619e8c22d7c5",
+  name: "MAGA (maga-hat.vip)",
+  symbol: "MAGA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/31305.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "31305",
+  type: "token",
+});
