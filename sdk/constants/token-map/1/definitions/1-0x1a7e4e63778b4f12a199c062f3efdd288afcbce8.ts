3c264a62d678dc50710c30ea42d1cc0310b8e930
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:59 -0800

    feat(token-map): Update Token -- 1-0x1a7e4e63778b4f12a199c062f3efdd288afcbce8 -- Angle Protocol (EURA) (0x1a7e...bce8)
    Token Details:
    - Id: 1-0x1a7e4e63778b4f12a199c062f3efdd288afcbce8
    - Chain Id: 1
    - Contract Address: 0x1a7e4e63778b4f12a199c062f3efdd288afcbce8
    - Name: Angle Protocol
    - Symbol: EURA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/15024.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 15024

---
+++ sdk/constants/token-map/1/definitions/1-0x1a7e4e63778b4f12a199c062f3efdd288afcbce8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1a7e4e63778b4f12a199c062f3efdd288afcbce8",
+  chain_id: 1,
+  contract_address: "0x1a7e4e63778b4f12a199c062f3efdd288afcbce8",
+  name: "Angle Protocol",
+  symbol: "EURA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15024.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "15024",
+  type: "token",
+});
