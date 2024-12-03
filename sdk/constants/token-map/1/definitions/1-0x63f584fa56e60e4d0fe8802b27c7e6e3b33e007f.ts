ee8b90591b5bdb6109e517520a9bcf0ce64c773e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:47 -0800

    feat(token-map): Update Token -- 1-0x63f584fa56e60e4d0fe8802b27c7e6e3b33e007f -- ContentBox (BOX) (0x63f5...007f)
    Token Details:
    - Id: 1-0x63f584fa56e60e4d0fe8802b27c7e6e3b33e007f
    - Chain Id: 1
    - Contract Address: 0x63f584fa56e60e4d0fe8802b27c7e6e3b33e007f
    - Name: ContentBox
    - Symbol: BOX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2945.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2945

---
+++ sdk/constants/token-map/1/definitions/1-0x63f584fa56e60e4d0fe8802b27c7e6e3b33e007f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x63f584fa56e60e4d0fe8802b27c7e6e3b33e007f",
+  chain_id: 1,
+  contract_address: "0x63f584fa56e60e4d0fe8802b27c7e6e3b33e007f",
+  name: "ContentBox",
+  symbol: "BOX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2945.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2945",
+  type: "token",
+});
