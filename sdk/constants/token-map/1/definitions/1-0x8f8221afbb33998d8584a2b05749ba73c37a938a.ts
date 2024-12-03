50c71f7446167f67586450081ca4b6358acfd324
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:50 -0800

    feat(token-map): Update Token -- 1-0x8f8221afbb33998d8584a2b05749ba73c37a938a -- Request (REQ) (0x8f82...938a)
    Token Details:
    - Id: 1-0x8f8221afbb33998d8584a2b05749ba73c37a938a
    - Chain Id: 1
    - Contract Address: 0x8f8221afbb33998d8584a2b05749ba73c37a938a
    - Name: Request
    - Symbol: REQ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2071.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2071

---
+++ sdk/constants/token-map/1/definitions/1-0x8f8221afbb33998d8584a2b05749ba73c37a938a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8f8221afbb33998d8584a2b05749ba73c37a938a",
+  chain_id: 1,
+  contract_address: "0x8f8221afbb33998d8584a2b05749ba73c37a938a",
+  name: "Request",
+  symbol: "REQ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2071.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2071",
+  type: "token",
+});
