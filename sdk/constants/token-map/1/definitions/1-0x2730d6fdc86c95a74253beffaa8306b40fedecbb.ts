4ee916ee46177cef6a6b2fdaf2ed25f35b632782
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:20 -0800

    feat(token-map): Update Token -- 1-0x2730d6fdc86c95a74253beffaa8306b40fedecbb -- UNICORN Token (UNI) (0x2730...ecbb)
    Token Details:
    - Id: 1-0x2730d6fdc86c95a74253beffaa8306b40fedecbb
    - Chain Id: 1
    - Contract Address: 0x2730d6fdc86c95a74253beffaa8306b40fedecbb
    - Name: UNICORN Token
    - Symbol: UNI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4307.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 4307

---
+++ sdk/constants/token-map/1/definitions/1-0x2730d6fdc86c95a74253beffaa8306b40fedecbb.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2730d6fdc86c95a74253beffaa8306b40fedecbb",
+  chain_id: 1,
+  contract_address: "0x2730d6fdc86c95a74253beffaa8306b40fedecbb",
+  name: "UNICORN Token",
+  symbol: "UNI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4307.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "4307",
+  type: "token",
+});
