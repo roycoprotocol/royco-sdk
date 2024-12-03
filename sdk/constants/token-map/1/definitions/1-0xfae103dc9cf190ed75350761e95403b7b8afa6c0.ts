6e659998262c760051ac27d56bd007c0ca4857b0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:33 -0800

    feat(token-map): Update Token -- 1-0xfae103dc9cf190ed75350761e95403b7b8afa6c0 -- Restaked Swell Ethereum (RSWETH) (0xfae1...a6c0)
    Token Details:
    - Id: 1-0xfae103dc9cf190ed75350761e95403b7b8afa6c0
    - Chain Id: 1
    - Contract Address: 0xfae103dc9cf190ed75350761e95403b7b8afa6c0
    - Name: Restaked Swell Ethereum
    - Symbol: RSWETH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29974.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29974

---
+++ sdk/constants/token-map/1/definitions/1-0xfae103dc9cf190ed75350761e95403b7b8afa6c0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfae103dc9cf190ed75350761e95403b7b8afa6c0",
+  chain_id: 1,
+  contract_address: "0xfae103dc9cf190ed75350761e95403b7b8afa6c0",
+  name: "Restaked Swell Ethereum",
+  symbol: "RSWETH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29974.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29974",
+  type: "token",
+});
