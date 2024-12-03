948853428c887b162c4aad53a2680849dfd2dfb8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:50 -0800

    feat(token-map): Update Token -- 1-0xb6ed7644c69416d67b522e20bc294a9a9b405b31 -- 0xBitcoin (0xBTC) (0xb6ed...5b31)
    Token Details:
    - Id: 1-0xb6ed7644c69416d67b522e20bc294a9a9b405b31
    - Chain Id: 1
    - Contract Address: 0xb6ed7644c69416d67b522e20bc294a9a9b405b31
    - Name: 0xBitcoin
    - Symbol: 0xBTC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2837.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2837

---
+++ sdk/constants/token-map/1/definitions/1-0xb6ed7644c69416d67b522e20bc294a9a9b405b31.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb6ed7644c69416d67b522e20bc294a9a9b405b31",
+  chain_id: 1,
+  contract_address: "0xb6ed7644c69416d67b522e20bc294a9a9b405b31",
+  name: "0xBitcoin",
+  symbol: "0xBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2837.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2837",
+  type: "token",
+});
