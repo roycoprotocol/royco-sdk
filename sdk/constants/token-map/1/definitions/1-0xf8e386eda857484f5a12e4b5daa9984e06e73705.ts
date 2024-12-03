bda109328bc29c13e5be412bb2f1db3cf09148da
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:05 -0800

    feat(token-map): Update Token -- 1-0xf8e386eda857484f5a12e4b5daa9984e06e73705 -- Indorse Token (IND) (0xf8e3...3705)
    Token Details:
    - Id: 1-0xf8e386eda857484f5a12e4b5daa9984e06e73705
    - Chain Id: 1
    - Contract Address: 0xf8e386eda857484f5a12e4b5daa9984e06e73705
    - Name: Indorse Token
    - Symbol: IND
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1967.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1967

---
+++ sdk/constants/token-map/1/definitions/1-0xf8e386eda857484f5a12e4b5daa9984e06e73705.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf8e386eda857484f5a12e4b5daa9984e06e73705",
+  chain_id: 1,
+  contract_address: "0xf8e386eda857484f5a12e4b5daa9984e06e73705",
+  name: "Indorse Token",
+  symbol: "IND",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1967.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1967",
+  type: "token",
+});
