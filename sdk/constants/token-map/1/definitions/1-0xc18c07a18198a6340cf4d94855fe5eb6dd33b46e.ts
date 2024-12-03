1f7dd65bf92fd3ff953dc979d451119ae088c64f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:54 -0800

    feat(token-map): Update Token -- 1-0xc18c07a18198a6340cf4d94855fe5eb6dd33b46e -- Qlindo (QLINDO) (0xc18c...b46e)
    Token Details:
    - Id: 1-0xc18c07a18198a6340cf4d94855fe5eb6dd33b46e
    - Chain Id: 1
    - Contract Address: 0xc18c07a18198a6340cf4d94855fe5eb6dd33b46e
    - Name: Qlindo
    - Symbol: QLINDO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/21315.png
    - Decimals: 0
    - Source: coinmarketcap
    - Search Id: 21315

---
+++ sdk/constants/token-map/1/definitions/1-0xc18c07a18198a6340cf4d94855fe5eb6dd33b46e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc18c07a18198a6340cf4d94855fe5eb6dd33b46e",
+  chain_id: 1,
+  contract_address: "0xc18c07a18198a6340cf4d94855fe5eb6dd33b46e",
+  name: "Qlindo",
+  symbol: "QLINDO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/21315.png",
+  decimals: 0,
+  source: "coinmarketcap",
+  search_id: "21315",
+  type: "token",
+});
