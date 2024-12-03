4e758600d03cf26eb62318bb4f12925f6f76cfdb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:22 -0800

    feat(token-map): Update Token -- 1-0x275f5ad03be0fa221b4c6649b8aee09a42d9412a -- Monavale (MONA) (0x275f...412a)
    Token Details:
    - Id: 1-0x275f5ad03be0fa221b4c6649b8aee09a42d9412a
    - Chain Id: 1
    - Contract Address: 0x275f5ad03be0fa221b4c6649b8aee09a42d9412a
    - Name: Monavale
    - Symbol: MONA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7866.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7866

---
+++ sdk/constants/token-map/1/definitions/1-0x275f5ad03be0fa221b4c6649b8aee09a42d9412a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x275f5ad03be0fa221b4c6649b8aee09a42d9412a",
+  chain_id: 1,
+  contract_address: "0x275f5ad03be0fa221b4c6649b8aee09a42d9412a",
+  name: "Monavale",
+  symbol: "MONA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7866.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7866",
+  type: "token",
+});
