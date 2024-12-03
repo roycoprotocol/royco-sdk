d35638038ee155f1d1987ae2e9b3dc83ca5ece97
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:16:39 -0800

    feat(token-map): Update Token -- 1-0x6c6ee5e31d828de241282b9606c8e98ea48526e2 -- Holo (HOT) (0x6c6e...26e2)
    Token Details:
    - Id: 1-0x6c6ee5e31d828de241282b9606c8e98ea48526e2
    - Chain Id: 1
    - Contract Address: 0x6c6ee5e31d828de241282b9606c8e98ea48526e2
    - Name: Holo
    - Symbol: HOT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2682.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2682

---
+++ sdk/constants/token-map/1/definitions/1-0x6c6ee5e31d828de241282b9606c8e98ea48526e2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6c6ee5e31d828de241282b9606c8e98ea48526e2",
+  chain_id: 1,
+  contract_address: "0x6c6ee5e31d828de241282b9606c8e98ea48526e2",
+  name: "Holo",
+  symbol: "HOT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2682.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2682",
+  type: "token",
+});
