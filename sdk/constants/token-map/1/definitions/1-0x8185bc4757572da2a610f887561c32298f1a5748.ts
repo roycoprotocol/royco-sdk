1db8d6c687c85e8188fcb6429d768e8e45d9693e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:13 -0800

    feat(token-map): Update Token -- 1-0x8185bc4757572da2a610f887561c32298f1a5748 -- Aluna.Social (ALN) (0x8185...5748)
    Token Details:
    - Id: 1-0x8185bc4757572da2a610f887561c32298f1a5748
    - Chain Id: 1
    - Contract Address: 0x8185bc4757572da2a610f887561c32298f1a5748
    - Name: Aluna.Social
    - Symbol: ALN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5544.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5544

---
+++ sdk/constants/token-map/1/definitions/1-0x8185bc4757572da2a610f887561c32298f1a5748.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8185bc4757572da2a610f887561c32298f1a5748",
+  chain_id: 1,
+  contract_address: "0x8185bc4757572da2a610f887561c32298f1a5748",
+  name: "Aluna.Social",
+  symbol: "ALN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5544.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5544",
+  type: "token",
+});
