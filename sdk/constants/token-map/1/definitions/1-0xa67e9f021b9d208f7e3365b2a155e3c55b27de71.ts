e483167a35c0b6603b3b1865a4339bc00c48905d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:28 -0800

    feat(token-map): Update Token -- 1-0xa67e9f021b9d208f7e3365b2a155e3c55b27de71 -- KleeKai (KLEE) (0xa67e...de71)
    Token Details:
    - Id: 1-0xa67e9f021b9d208f7e3365b2a155e3c55b27de71
    - Chain Id: 1
    - Contract Address: 0xa67e9f021b9d208f7e3365b2a155e3c55b27de71
    - Name: KleeKai
    - Symbol: KLEE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10262.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 10262

---
+++ sdk/constants/token-map/1/definitions/1-0xa67e9f021b9d208f7e3365b2a155e3c55b27de71.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa67e9f021b9d208f7e3365b2a155e3c55b27de71",
+  chain_id: 1,
+  contract_address: "0xa67e9f021b9d208f7e3365b2a155e3c55b27de71",
+  name: "KleeKai",
+  symbol: "KLEE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10262.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "10262",
+  type: "token",
+});
