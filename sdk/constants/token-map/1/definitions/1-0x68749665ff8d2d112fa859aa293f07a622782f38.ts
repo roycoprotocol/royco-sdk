f036c10153bd49a7a75d08f04f9cb82122a8ff4a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:42 -0800

    feat(token-map): Update Token -- 1-0x68749665ff8d2d112fa859aa293f07a622782f38 -- Tether Gold (XAUt) (0x6874...2f38)
    Token Details:
    - Id: 1-0x68749665ff8d2d112fa859aa293f07a622782f38
    - Chain Id: 1
    - Contract Address: 0x68749665ff8d2d112fa859aa293f07a622782f38
    - Name: Tether Gold
    - Symbol: XAUt
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5176.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 5176

---
+++ sdk/constants/token-map/1/definitions/1-0x68749665ff8d2d112fa859aa293f07a622782f38.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x68749665ff8d2d112fa859aa293f07a622782f38",
+  chain_id: 1,
+  contract_address: "0x68749665ff8d2d112fa859aa293f07a622782f38",
+  name: "Tether Gold",
+  symbol: "XAUt",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5176.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "5176",
+  type: "token",
+});
