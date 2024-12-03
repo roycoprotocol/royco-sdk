d038da69772c40777b7ae3c4f48ccf501d7ac006
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:14 -0800

    feat(token-map): Update Token -- 1-0x491604c0fdf08347dd1fa4ee062a822a5dd06b5d -- Cartesi (CTSI) (0x4916...6b5d)
    Token Details:
    - Id: 1-0x491604c0fdf08347dd1fa4ee062a822a5dd06b5d
    - Chain Id: 1
    - Contract Address: 0x491604c0fdf08347dd1fa4ee062a822a5dd06b5d
    - Name: Cartesi
    - Symbol: CTSI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5444.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5444

---
+++ sdk/constants/token-map/1/definitions/1-0x491604c0fdf08347dd1fa4ee062a822a5dd06b5d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x491604c0fdf08347dd1fa4ee062a822a5dd06b5d",
+  chain_id: 1,
+  contract_address: "0x491604c0fdf08347dd1fa4ee062a822a5dd06b5d",
+  name: "Cartesi",
+  symbol: "CTSI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5444.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5444",
+  type: "token",
+});
