f02e3442df8df7687bda4de320dbe9f3c9effc42
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:51 -0800

    feat(token-map): Update Token -- 1-0x41dbecc1cdc5517c6f76f6a6e836adbee2754de3 -- Medicalchain (MTN) (0x41db...4de3)
    Token Details:
    - Id: 1-0x41dbecc1cdc5517c6f76f6a6e836adbee2754de3
    - Chain Id: 1
    - Contract Address: 0x41dbecc1cdc5517c6f76f6a6e836adbee2754de3
    - Name: Medicalchain
    - Symbol: MTN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2497.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2497

---
+++ sdk/constants/token-map/1/definitions/1-0x41dbecc1cdc5517c6f76f6a6e836adbee2754de3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x41dbecc1cdc5517c6f76f6a6e836adbee2754de3",
+  chain_id: 1,
+  contract_address: "0x41dbecc1cdc5517c6f76f6a6e836adbee2754de3",
+  name: "Medicalchain",
+  symbol: "MTN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2497.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2497",
+  type: "token",
+});
