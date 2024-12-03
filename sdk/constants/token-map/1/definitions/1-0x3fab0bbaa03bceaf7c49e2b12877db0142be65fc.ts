78b649e0e46a7ab93418f831c3c4da0118901660
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:18 -0800

    feat(token-map): Update Token -- 1-0x3fab0bbaa03bceaf7c49e2b12877db0142be65fc -- Castello Coin (CAST) (0x3fab...65fc)
    Token Details:
    - Id: 1-0x3fab0bbaa03bceaf7c49e2b12877db0142be65fc
    - Chain Id: 1
    - Contract Address: 0x3fab0bbaa03bceaf7c49e2b12877db0142be65fc
    - Name: Castello Coin
    - Symbol: CAST
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/19705.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 19705

---
+++ sdk/constants/token-map/1/definitions/1-0x3fab0bbaa03bceaf7c49e2b12877db0142be65fc.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3fab0bbaa03bceaf7c49e2b12877db0142be65fc",
+  chain_id: 1,
+  contract_address: "0x3fab0bbaa03bceaf7c49e2b12877db0142be65fc",
+  name: "Castello Coin",
+  symbol: "CAST",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/19705.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "19705",
+  type: "token",
+});
