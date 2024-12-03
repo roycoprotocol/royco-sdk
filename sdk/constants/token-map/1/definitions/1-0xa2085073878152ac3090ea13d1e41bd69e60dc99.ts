705f427c231a7100892848a898946a45684cd2da
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:49 -0800

    feat(token-map): Update Token -- 1-0xa2085073878152ac3090ea13d1e41bd69e60dc99 -- EscoinToken (ELG) (0xa208...dc99)
    Token Details:
    - Id: 1-0xa2085073878152ac3090ea13d1e41bd69e60dc99
    - Chain Id: 1
    - Contract Address: 0xa2085073878152ac3090ea13d1e41bd69e60dc99
    - Name: EscoinToken
    - Symbol: ELG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10312.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10312

---
+++ sdk/constants/token-map/1/definitions/1-0xa2085073878152ac3090ea13d1e41bd69e60dc99.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa2085073878152ac3090ea13d1e41bd69e60dc99",
+  chain_id: 1,
+  contract_address: "0xa2085073878152ac3090ea13d1e41bd69e60dc99",
+  name: "EscoinToken",
+  symbol: "ELG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10312.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10312",
+  type: "token",
+});
