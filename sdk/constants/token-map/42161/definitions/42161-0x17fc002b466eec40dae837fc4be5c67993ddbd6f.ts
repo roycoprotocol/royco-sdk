d8ae50e5bfec94c92fbbc1c1ace58737127268bf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:40:53 -0800

    feat(token-map): Update Token -- 42161-0x17fc002b466eec40dae837fc4be5c67993ddbd6f -- Frax (FRAX) (0x17fc...bd6f)
    Token Details:
    - Id: 42161-0x17fc002b466eec40dae837fc4be5c67993ddbd6f
    - Chain Id: 42161
    - Contract Address: 0x17fc002b466eec40dae837fc4be5c67993ddbd6f
    - Name: Frax
    - Symbol: FRAX
    - Image: https://coin-images.coingecko.com/coins/images/13422/large/FRAX_icon.png?1696513182
    - Decimals: 18
    - Source: coingecko
    - Search Id: frax

---
+++ sdk/constants/token-map/42161/definitions/42161-0x17fc002b466eec40dae837fc4be5c67993ddbd6f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x17fc002b466eec40dae837fc4be5c67993ddbd6f",
+  chain_id: 42161,
+  contract_address: "0x17fc002b466eec40dae837fc4be5c67993ddbd6f",
+  name: "Frax",
+  symbol: "FRAX",
+  image: "https://coin-images.coingecko.com/coins/images/13422/large/FRAX_icon.png?1696513182",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "frax",
+  type: "token",
+});
