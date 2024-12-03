3045f60309ded60f92ba388665b47b066aee2d9b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:41 -0800

    feat(token-map): Update Token -- 1-0x298d492e8c1d909d3f63bc4a36c66c64acb3d695 -- PolkaBridge (PBR) (0x298d...d695)
    Token Details:
    - Id: 1-0x298d492e8c1d909d3f63bc4a36c66c64acb3d695
    - Chain Id: 1
    - Contract Address: 0x298d492e8c1d909d3f63bc4a36c66c64acb3d695
    - Name: PolkaBridge
    - Symbol: PBR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8320.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8320

---
+++ sdk/constants/token-map/1/definitions/1-0x298d492e8c1d909d3f63bc4a36c66c64acb3d695.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x298d492e8c1d909d3f63bc4a36c66c64acb3d695",
+  chain_id: 1,
+  contract_address: "0x298d492e8c1d909d3f63bc4a36c66c64acb3d695",
+  name: "PolkaBridge",
+  symbol: "PBR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8320.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8320",
+  type: "token",
+});
