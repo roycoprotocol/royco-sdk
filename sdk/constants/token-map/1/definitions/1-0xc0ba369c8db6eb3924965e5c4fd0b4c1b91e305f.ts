b33c05f398f03a378edabf3a2a89b42311631113
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:31 -0800

    feat(token-map): Update Token -- 1-0xc0ba369c8db6eb3924965e5c4fd0b4c1b91e305f -- Duck DAO (DLP Duck Token) (DUCK) (0xc0ba...305f)
    Token Details:
    - Id: 1-0xc0ba369c8db6eb3924965e5c4fd0b4c1b91e305f
    - Chain Id: 1
    - Contract Address: 0xc0ba369c8db6eb3924965e5c4fd0b4c1b91e305f
    - Name: Duck DAO (DLP Duck Token)
    - Symbol: DUCK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8063.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8063

---
+++ sdk/constants/token-map/1/definitions/1-0xc0ba369c8db6eb3924965e5c4fd0b4c1b91e305f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc0ba369c8db6eb3924965e5c4fd0b4c1b91e305f",
+  chain_id: 1,
+  contract_address: "0xc0ba369c8db6eb3924965e5c4fd0b4c1b91e305f",
+  name: "Duck DAO (DLP Duck Token)",
+  symbol: "DUCK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8063.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8063",
+  type: "token",
+});
