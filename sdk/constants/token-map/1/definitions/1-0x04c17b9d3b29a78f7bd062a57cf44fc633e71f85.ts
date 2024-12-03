1ed3ef9c4693f9ccca5216008b452371c43091d1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:57:39 -0800

    feat(token-map): Update Token -- 1-0x04c17b9d3b29a78f7bd062a57cf44fc633e71f85 -- IMPT (IMPT) (0x04c1...1f85)
    Token Details:
    - Id: 1-0x04c17b9d3b29a78f7bd062a57cf44fc633e71f85
    - Chain Id: 1
    - Contract Address: 0x04c17b9d3b29a78f7bd062a57cf44fc633e71f85
    - Name: IMPT
    - Symbol: IMPT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/22985.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 22985

---
+++ sdk/constants/token-map/1/definitions/1-0x04c17b9d3b29a78f7bd062a57cf44fc633e71f85.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x04c17b9d3b29a78f7bd062a57cf44fc633e71f85",
+  chain_id: 1,
+  contract_address: "0x04c17b9d3b29a78f7bd062a57cf44fc633e71f85",
+  name: "IMPT",
+  symbol: "IMPT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/22985.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "22985",
+  type: "token",
+});
