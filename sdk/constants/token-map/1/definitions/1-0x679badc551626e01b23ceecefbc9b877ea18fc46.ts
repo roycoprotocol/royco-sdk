9f2598bf2947e30e7f7671666bae88076c0a9a4e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:31 -0800

    feat(token-map): Update Token -- 1-0x679badc551626e01b23ceecefbc9b877ea18fc46 -- Ccore (CCO) (0x679b...fc46)
    Token Details:
    - Id: 1-0x679badc551626e01b23ceecefbc9b877ea18fc46
    - Chain Id: 1
    - Contract Address: 0x679badc551626e01b23ceecefbc9b877ea18fc46
    - Name: Ccore
    - Symbol: CCO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2241.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2241

---
+++ sdk/constants/token-map/1/definitions/1-0x679badc551626e01b23ceecefbc9b877ea18fc46.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x679badc551626e01b23ceecefbc9b877ea18fc46",
+  chain_id: 1,
+  contract_address: "0x679badc551626e01b23ceecefbc9b877ea18fc46",
+  name: "Ccore",
+  symbol: "CCO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2241.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2241",
+  type: "token",
+});
