52cfe3f4fa1fe0f13cdca893aa530c2fb837bb7c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:19 -0800

    feat(token-map): Update Token -- 1-0x3429d03c6f7521aec737a0bbf2e5ddcef2c3ae31 -- Pixels (PIXEL) (0x3429...ae31)
    Token Details:
    - Id: 1-0x3429d03c6f7521aec737a0bbf2e5ddcef2c3ae31
    - Chain Id: 1
    - Contract Address: 0x3429d03c6f7521aec737a0bbf2e5ddcef2c3ae31
    - Name: Pixels
    - Symbol: PIXEL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29335.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29335

---
+++ sdk/constants/token-map/1/definitions/1-0x3429d03c6f7521aec737a0bbf2e5ddcef2c3ae31.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3429d03c6f7521aec737a0bbf2e5ddcef2c3ae31",
+  chain_id: 1,
+  contract_address: "0x3429d03c6f7521aec737a0bbf2e5ddcef2c3ae31",
+  name: "Pixels",
+  symbol: "PIXEL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29335.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29335",
+  type: "token",
+});
