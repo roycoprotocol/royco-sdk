f22c45ecd695b3d050cd8ac56242779d8b84960e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:45 -0800

    feat(token-map): Update Token -- 1-0x4674672bcddda2ea5300f5207e1158185c944bc0 -- Gem Exchange and Trading (GXT) (0x4674...4bc0)
    Token Details:
    - Id: 1-0x4674672bcddda2ea5300f5207e1158185c944bc0
    - Chain Id: 1
    - Contract Address: 0x4674672bcddda2ea5300f5207e1158185c944bc0
    - Name: Gem Exchange and Trading
    - Symbol: GXT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7310.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7310

---
+++ sdk/constants/token-map/1/definitions/1-0x4674672bcddda2ea5300f5207e1158185c944bc0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4674672bcddda2ea5300f5207e1158185c944bc0",
+  chain_id: 1,
+  contract_address: "0x4674672bcddda2ea5300f5207e1158185c944bc0",
+  name: "Gem Exchange and Trading",
+  symbol: "GXT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7310.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7310",
+  type: "token",
+});
