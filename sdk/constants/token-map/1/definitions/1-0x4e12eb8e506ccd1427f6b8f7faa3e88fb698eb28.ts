a8179b20413fecaa9f3f55c0aa4787a3608cc6fa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:13 -0800

    feat(token-map): Update Token -- 1-0x4e12eb8e506ccd1427f6b8f7faa3e88fb698eb28 -- Jack Token (JACK) (0x4e12...eb28)
    Token Details:
    - Id: 1-0x4e12eb8e506ccd1427f6b8f7faa3e88fb698eb28
    - Chain Id: 1
    - Contract Address: 0x4e12eb8e506ccd1427f6b8f7faa3e88fb698eb28
    - Name: Jack Token
    - Symbol: JACK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5486.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5486

---
+++ sdk/constants/token-map/1/definitions/1-0x4e12eb8e506ccd1427f6b8f7faa3e88fb698eb28.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4e12eb8e506ccd1427f6b8f7faa3e88fb698eb28",
+  chain_id: 1,
+  contract_address: "0x4e12eb8e506ccd1427f6b8f7faa3e88fb698eb28",
+  name: "Jack Token",
+  symbol: "JACK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5486.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5486",
+  type: "token",
+});
