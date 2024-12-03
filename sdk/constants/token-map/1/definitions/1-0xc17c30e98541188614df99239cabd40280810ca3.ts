58d82492f69eb5ad9291c2ac1187c1dcfcb979f0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:50 -0800

    feat(token-map): Update Token -- 1-0xc17c30e98541188614df99239cabd40280810ca3 -- EverRise (RISE) (0xc17c...0ca3)
    Token Details:
    - Id: 1-0xc17c30e98541188614df99239cabd40280810ca3
    - Chain Id: 1
    - Contract Address: 0xc17c30e98541188614df99239cabd40280810ca3
    - Name: EverRise
    - Symbol: RISE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/15257.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 15257

---
+++ sdk/constants/token-map/1/definitions/1-0xc17c30e98541188614df99239cabd40280810ca3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc17c30e98541188614df99239cabd40280810ca3",
+  chain_id: 1,
+  contract_address: "0xc17c30e98541188614df99239cabd40280810ca3",
+  name: "EverRise",
+  symbol: "RISE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15257.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "15257",
+  type: "token",
+});
