89a94aa13d30cb10a8c166faf2394bcf9171dad4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:31:14 -0800

    feat(token-map): Update Token -- 1-0xc4f6e93aeddc11dc22268488465babcaf09399ac -- HI (HI) (0xc4f6...99ac)
    Token Details:
    - Id: 1-0xc4f6e93aeddc11dc22268488465babcaf09399ac
    - Chain Id: 1
    - Contract Address: 0xc4f6e93aeddc11dc22268488465babcaf09399ac
    - Name: HI
    - Symbol: HI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11240.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11240

---
+++ sdk/constants/token-map/1/definitions/1-0xc4f6e93aeddc11dc22268488465babcaf09399ac.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc4f6e93aeddc11dc22268488465babcaf09399ac",
+  chain_id: 1,
+  contract_address: "0xc4f6e93aeddc11dc22268488465babcaf09399ac",
+  name: "HI",
+  symbol: "HI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11240.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11240",
+  type: "token",
+});
