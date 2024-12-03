0450c570dcc71e746dd6e503e66d40522a4c91da
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:49 -0800

    feat(token-map): Update Token -- 1-0x968f6f898a6df937fc1859b323ac2f14643e3fed -- Numerico (NWC) (0x968f...3fed)
    Token Details:
    - Id: 1-0x968f6f898a6df937fc1859b323ac2f14643e3fed
    - Chain Id: 1
    - Contract Address: 0x968f6f898a6df937fc1859b323ac2f14643e3fed
    - Name: Numerico
    - Symbol: NWC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4890.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4890

---
+++ sdk/constants/token-map/1/definitions/1-0x968f6f898a6df937fc1859b323ac2f14643e3fed.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x968f6f898a6df937fc1859b323ac2f14643e3fed",
+  chain_id: 1,
+  contract_address: "0x968f6f898a6df937fc1859b323ac2f14643e3fed",
+  name: "Numerico",
+  symbol: "NWC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4890.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4890",
+  type: "token",
+});
