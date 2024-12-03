f6f9f70de4fa64027389cf96c5dfc64ec20610b1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:36 -0800

    feat(token-map): Update Token -- 1-0xdc9ac3c20d1ed0b540df9b1fedc10039df13f99c -- xMoney (UTK) (0xdc9a...f99c)
    Token Details:
    - Id: 1-0xdc9ac3c20d1ed0b540df9b1fedc10039df13f99c
    - Chain Id: 1
    - Contract Address: 0xdc9ac3c20d1ed0b540df9b1fedc10039df13f99c
    - Name: xMoney
    - Symbol: UTK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2320.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2320

---
+++ sdk/constants/token-map/1/definitions/1-0xdc9ac3c20d1ed0b540df9b1fedc10039df13f99c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdc9ac3c20d1ed0b540df9b1fedc10039df13f99c",
+  chain_id: 1,
+  contract_address: "0xdc9ac3c20d1ed0b540df9b1fedc10039df13f99c",
+  name: "xMoney",
+  symbol: "UTK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2320.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2320",
+  type: "token",
+});
