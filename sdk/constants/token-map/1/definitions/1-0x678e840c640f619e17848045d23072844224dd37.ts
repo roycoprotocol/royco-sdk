4242a774901551952afaa86d397aa9ed4ff3c287
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:29 -0800

    feat(token-map): Update Token -- 1-0x678e840c640f619e17848045d23072844224dd37 -- Cratos (CRTS) (0x678e...dd37)
    Token Details:
    - Id: 1-0x678e840c640f619e17848045d23072844224dd37
    - Chain Id: 1
    - Contract Address: 0x678e840c640f619e17848045d23072844224dd37
    - Name: Cratos
    - Symbol: CRTS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12064.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 12064

---
+++ sdk/constants/token-map/1/definitions/1-0x678e840c640f619e17848045d23072844224dd37.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x678e840c640f619e17848045d23072844224dd37",
+  chain_id: 1,
+  contract_address: "0x678e840c640f619e17848045d23072844224dd37",
+  name: "Cratos",
+  symbol: "CRTS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12064.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "12064",
+  type: "token",
+});
