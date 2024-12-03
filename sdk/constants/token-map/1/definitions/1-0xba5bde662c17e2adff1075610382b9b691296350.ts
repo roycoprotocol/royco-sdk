0016e4b3bc6b2c8e003c088b3815d5d5e81fb3b7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:43 -0800

    feat(token-map): Update Token -- 1-0xba5bde662c17e2adff1075610382b9b691296350 -- SuperRare (RARE) (0xba5b...6350)
    Token Details:
    - Id: 1-0xba5bde662c17e2adff1075610382b9b691296350
    - Chain Id: 1
    - Contract Address: 0xba5bde662c17e2adff1075610382b9b691296350
    - Name: SuperRare
    - Symbol: RARE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11294.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11294

---
+++ sdk/constants/token-map/1/definitions/1-0xba5bde662c17e2adff1075610382b9b691296350.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xba5bde662c17e2adff1075610382b9b691296350",
+  chain_id: 1,
+  contract_address: "0xba5bde662c17e2adff1075610382b9b691296350",
+  name: "SuperRare",
+  symbol: "RARE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11294.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11294",
+  type: "token",
+});
