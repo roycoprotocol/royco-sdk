9bd0773020a88f987a80762a2c3177e3f4d25412
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:39 -0800

    feat(token-map): Update Token -- 1-0x9bf1d7d63dd7a4ce167cf4866388226eeefa702e -- Ben (BEN) (0x9bf1...702e)
    Token Details:
    - Id: 1-0x9bf1d7d63dd7a4ce167cf4866388226eeefa702e
    - Chain Id: 1
    - Contract Address: 0x9bf1d7d63dd7a4ce167cf4866388226eeefa702e
    - Name: Ben
    - Symbol: BEN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/25047.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 25047

---
+++ sdk/constants/token-map/1/definitions/1-0x9bf1d7d63dd7a4ce167cf4866388226eeefa702e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9bf1d7d63dd7a4ce167cf4866388226eeefa702e",
+  chain_id: 1,
+  contract_address: "0x9bf1d7d63dd7a4ce167cf4866388226eeefa702e",
+  name: "Ben",
+  symbol: "BEN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/25047.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "25047",
+  type: "token",
+});
