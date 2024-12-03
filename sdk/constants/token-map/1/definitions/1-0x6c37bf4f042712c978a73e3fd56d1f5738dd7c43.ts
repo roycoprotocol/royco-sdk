2f82e5933db0c2e4b7bb79957776a8a55d182621
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:16:34 -0800

    feat(token-map): Update Token -- 1-0x6c37bf4f042712c978a73e3fd56d1f5738dd7c43 -- Elementeum (ELET) (0x6c37...7c43)
    Token Details:
    - Id: 1-0x6c37bf4f042712c978a73e3fd56d1f5738dd7c43
    - Chain Id: 1
    - Contract Address: 0x6c37bf4f042712c978a73e3fd56d1f5738dd7c43
    - Name: Elementeum
    - Symbol: ELET
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3931.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3931

---
+++ sdk/constants/token-map/1/definitions/1-0x6c37bf4f042712c978a73e3fd56d1f5738dd7c43.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6c37bf4f042712c978a73e3fd56d1f5738dd7c43",
+  chain_id: 1,
+  contract_address: "0x6c37bf4f042712c978a73e3fd56d1f5738dd7c43",
+  name: "Elementeum",
+  symbol: "ELET",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3931.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3931",
+  type: "token",
+});
