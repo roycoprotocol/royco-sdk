a4cc5e5d87a341d876954df846e9e3e555b899b1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:52 -0800

    feat(token-map): Update Token -- 8453-0x703d57164ca270b0b330a87fd159cfef1490c0a5 -- RAI Finance (SOFI) (0x703d...c0a5)
    Token Details:
    - Id: 8453-0x703d57164ca270b0b330a87fd159cfef1490c0a5
    - Chain Id: 8453
    - Contract Address: 0x703d57164ca270b0b330a87fd159cfef1490c0a5
    - Name: RAI Finance
    - Symbol: SOFI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/16552.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 16552

---
+++ sdk/constants/token-map/8453/definitions/8453-0x703d57164ca270b0b330a87fd159cfef1490c0a5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0x703d57164ca270b0b330a87fd159cfef1490c0a5",
+  chain_id: 8453,
+  contract_address: "0x703d57164ca270b0b330a87fd159cfef1490c0a5",
+  name: "RAI Finance",
+  symbol: "SOFI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/16552.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "16552",
+  type: "token",
+});
