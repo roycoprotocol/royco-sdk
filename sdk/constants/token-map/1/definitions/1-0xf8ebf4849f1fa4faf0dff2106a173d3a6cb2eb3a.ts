9fd9caba6a9bb5777f87b9c1dcac946dbac41621
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:07 -0800

    feat(token-map): Update Token -- 1-0xf8ebf4849f1fa4faf0dff2106a173d3a6cb2eb3a -- Troll (TROLL) (0xf8eb...eb3a)
    Token Details:
    - Id: 1-0xf8ebf4849f1fa4faf0dff2106a173d3a6cb2eb3a
    - Chain Id: 1
    - Contract Address: 0xf8ebf4849f1fa4faf0dff2106a173d3a6cb2eb3a
    - Name: Troll
    - Symbol: TROLL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29505.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29505

---
+++ sdk/constants/token-map/1/definitions/1-0xf8ebf4849f1fa4faf0dff2106a173d3a6cb2eb3a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf8ebf4849f1fa4faf0dff2106a173d3a6cb2eb3a",
+  chain_id: 1,
+  contract_address: "0xf8ebf4849f1fa4faf0dff2106a173d3a6cb2eb3a",
+  name: "Troll",
+  symbol: "TROLL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29505.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29505",
+  type: "token",
+});
