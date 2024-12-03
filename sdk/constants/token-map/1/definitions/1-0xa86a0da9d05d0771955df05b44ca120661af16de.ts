fd90ed15e70267daed66a78e76ae669723baee9e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:44 -0800

    feat(token-map): Update Token -- 1-0xa86a0da9d05d0771955df05b44ca120661af16de -- OTCBTC Token (OTB) (0xa86a...16de)
    Token Details:
    - Id: 1-0xa86a0da9d05d0771955df05b44ca120661af16de
    - Chain Id: 1
    - Contract Address: 0xa86a0da9d05d0771955df05b44ca120661af16de
    - Name: OTCBTC Token
    - Symbol: OTB
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2894.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2894

---
+++ sdk/constants/token-map/1/definitions/1-0xa86a0da9d05d0771955df05b44ca120661af16de.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa86a0da9d05d0771955df05b44ca120661af16de",
+  chain_id: 1,
+  contract_address: "0xa86a0da9d05d0771955df05b44ca120661af16de",
+  name: "OTCBTC Token",
+  symbol: "OTB",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2894.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2894",
+  type: "token",
+});
