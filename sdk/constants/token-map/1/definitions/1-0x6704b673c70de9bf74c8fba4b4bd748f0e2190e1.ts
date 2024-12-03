b0122e400f7a8d402dc3ee1ef62ad8a18be97db5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:19 -0800

    feat(token-map): Update Token -- 1-0x6704b673c70de9bf74c8fba4b4bd748f0e2190e1 -- Ubex (UBEX) (0x6704...90e1)
    Token Details:
    - Id: 1-0x6704b673c70de9bf74c8fba4b4bd748f0e2190e1
    - Chain Id: 1
    - Contract Address: 0x6704b673c70de9bf74c8fba4b4bd748f0e2190e1
    - Name: Ubex
    - Symbol: UBEX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3140.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3140

---
+++ sdk/constants/token-map/1/definitions/1-0x6704b673c70de9bf74c8fba4b4bd748f0e2190e1.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6704b673c70de9bf74c8fba4b4bd748f0e2190e1",
+  chain_id: 1,
+  contract_address: "0x6704b673c70de9bf74c8fba4b4bd748f0e2190e1",
+  name: "Ubex",
+  symbol: "UBEX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3140.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3140",
+  type: "token",
+});
