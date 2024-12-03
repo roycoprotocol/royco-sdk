c7e89f1fe6b9a49a9c3c7c4aed77c525510288ca
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:04 -0800

    feat(token-map): Update Token -- 1-0x986ee2b944c42d017f52af21c4c69b84dbea35d8 -- BitMart Token (BMX) (0x986e...35d8)
    Token Details:
    - Id: 1-0x986ee2b944c42d017f52af21c4c69b84dbea35d8
    - Chain Id: 1
    - Contract Address: 0x986ee2b944c42d017f52af21c4c69b84dbea35d8
    - Name: BitMart Token
    - Symbol: BMX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2933.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2933

---
+++ sdk/constants/token-map/1/definitions/1-0x986ee2b944c42d017f52af21c4c69b84dbea35d8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x986ee2b944c42d017f52af21c4c69b84dbea35d8",
+  chain_id: 1,
+  contract_address: "0x986ee2b944c42d017f52af21c4c69b84dbea35d8",
+  name: "BitMart Token",
+  symbol: "BMX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2933.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2933",
+  type: "token",
+});
