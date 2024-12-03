681d75c0b42345bd0bd447fad86bafb5b3288498
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:56 -0800

    feat(token-map): Update Token -- 1-0xfd6c31bb6f05fc8db64f4b740ab758605c271fd8 -- CONTRACOIN (CTCN) (0xfd6c...1fd8)
    Token Details:
    - Id: 1-0xfd6c31bb6f05fc8db64f4b740ab758605c271fd8
    - Chain Id: 1
    - Contract Address: 0xfd6c31bb6f05fc8db64f4b740ab758605c271fd8
    - Name: CONTRACOIN
    - Symbol: CTCN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5313.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5313

---
+++ sdk/constants/token-map/1/definitions/1-0xfd6c31bb6f05fc8db64f4b740ab758605c271fd8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfd6c31bb6f05fc8db64f4b740ab758605c271fd8",
+  chain_id: 1,
+  contract_address: "0xfd6c31bb6f05fc8db64f4b740ab758605c271fd8",
+  name: "CONTRACOIN",
+  symbol: "CTCN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5313.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5313",
+  type: "token",
+});
