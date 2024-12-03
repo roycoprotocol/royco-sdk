a292a1f4104be14095c402dd079d3166bba35876
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:00 -0800

    feat(token-map): Update Token -- 1-0xdebe620609674f21b1089042527f420372ea98a5 -- ALTAVA (TAVA) (0xdebe...98a5)
    Token Details:
    - Id: 1-0xdebe620609674f21b1089042527f420372ea98a5
    - Chain Id: 1
    - Contract Address: 0xdebe620609674f21b1089042527f420372ea98a5
    - Name: ALTAVA
    - Symbol: TAVA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/19786.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 19786

---
+++ sdk/constants/token-map/1/definitions/1-0xdebe620609674f21b1089042527f420372ea98a5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdebe620609674f21b1089042527f420372ea98a5",
+  chain_id: 1,
+  contract_address: "0xdebe620609674f21b1089042527f420372ea98a5",
+  name: "ALTAVA",
+  symbol: "TAVA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/19786.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "19786",
+  type: "token",
+});
