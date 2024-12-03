962ec8b7a52371480a024a07e59644970e540fe9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:55 -0800

    feat(token-map): Update Token -- 1-0xd6327ce1fb9d6020e8c2c0e124a1ec23dcab7536 -- CumInu (CUMINU) (0xd632...7536)
    Token Details:
    - Id: 1-0xd6327ce1fb9d6020e8c2c0e124a1ec23dcab7536
    - Chain Id: 1
    - Contract Address: 0xd6327ce1fb9d6020e8c2c0e124a1ec23dcab7536
    - Name: CumInu
    - Symbol: CUMINU
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10061.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10061

---
+++ sdk/constants/token-map/1/definitions/1-0xd6327ce1fb9d6020e8c2c0e124a1ec23dcab7536.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd6327ce1fb9d6020e8c2c0e124a1ec23dcab7536",
+  chain_id: 1,
+  contract_address: "0xd6327ce1fb9d6020e8c2c0e124a1ec23dcab7536",
+  name: "CumInu",
+  symbol: "CUMINU",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10061.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10061",
+  type: "token",
+});
