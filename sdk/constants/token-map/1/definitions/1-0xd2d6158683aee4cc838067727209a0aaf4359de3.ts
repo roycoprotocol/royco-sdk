8caa92f0ba4939f32943ded6b2654c0d0f6319aa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:14 -0800

    feat(token-map): Update Token -- 1-0xd2d6158683aee4cc838067727209a0aaf4359de3 -- Bounty0x (BNTY) (0xd2d6...9de3)
    Token Details:
    - Id: 1-0xd2d6158683aee4cc838067727209a0aaf4359de3
    - Chain Id: 1
    - Contract Address: 0xd2d6158683aee4cc838067727209a0aaf4359de3
    - Name: Bounty0x
    - Symbol: BNTY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2310.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2310

---
+++ sdk/constants/token-map/1/definitions/1-0xd2d6158683aee4cc838067727209a0aaf4359de3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd2d6158683aee4cc838067727209a0aaf4359de3",
+  chain_id: 1,
+  contract_address: "0xd2d6158683aee4cc838067727209a0aaf4359de3",
+  name: "Bounty0x",
+  symbol: "BNTY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2310.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2310",
+  type: "token",
+});
