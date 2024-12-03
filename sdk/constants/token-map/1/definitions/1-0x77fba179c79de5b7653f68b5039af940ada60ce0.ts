dfc448a16143694c63424211e134709d32412fde
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:50 -0800

    feat(token-map): Update Token -- 1-0x77fba179c79de5b7653f68b5039af940ada60ce0 -- Ampleforth Governance Token (FORTH) (0x77fb...0ce0)
    Token Details:
    - Id: 1-0x77fba179c79de5b7653f68b5039af940ada60ce0
    - Chain Id: 1
    - Contract Address: 0x77fba179c79de5b7653f68b5039af940ada60ce0
    - Name: Ampleforth Governance Token
    - Symbol: FORTH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9421.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9421

---
+++ sdk/constants/token-map/1/definitions/1-0x77fba179c79de5b7653f68b5039af940ada60ce0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x77fba179c79de5b7653f68b5039af940ada60ce0",
+  chain_id: 1,
+  contract_address: "0x77fba179c79de5b7653f68b5039af940ada60ce0",
+  name: "Ampleforth Governance Token",
+  symbol: "FORTH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9421.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9421",
+  type: "token",
+});
