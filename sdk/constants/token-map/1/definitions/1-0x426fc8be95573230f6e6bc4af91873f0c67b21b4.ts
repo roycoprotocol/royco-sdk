998981b2ea05bf050351f7ae2af9d5324a66e973
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:00 -0800

    feat(token-map): Update Token -- 1-0x426fc8be95573230f6e6bc4af91873f0c67b21b4 -- BlackPearl Token (BPLC) (0x426f...21b4)
    Token Details:
    - Id: 1-0x426fc8be95573230f6e6bc4af91873f0c67b21b4
    - Chain Id: 1
    - Contract Address: 0x426fc8be95573230f6e6bc4af91873f0c67b21b4
    - Name: BlackPearl Token
    - Symbol: BPLC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6113.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6113

---
+++ sdk/constants/token-map/1/definitions/1-0x426fc8be95573230f6e6bc4af91873f0c67b21b4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x426fc8be95573230f6e6bc4af91873f0c67b21b4",
+  chain_id: 1,
+  contract_address: "0x426fc8be95573230f6e6bc4af91873f0c67b21b4",
+  name: "BlackPearl Token",
+  symbol: "BPLC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6113.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6113",
+  type: "token",
+});
