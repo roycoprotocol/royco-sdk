e1e4325a1c260c86efa67de82bfbfc5083e1b1a8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:03 -0800

    feat(token-map): Update Token -- 1-0x4d224452801aced8b2f0aebe155379bb5d594381 -- ApeCoin (APE) (0x4d22...4381)
    Token Details:
    - Id: 1-0x4d224452801aced8b2f0aebe155379bb5d594381
    - Chain Id: 1
    - Contract Address: 0x4d224452801aced8b2f0aebe155379bb5d594381
    - Name: ApeCoin
    - Symbol: APE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/18876.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 18876

---
+++ sdk/constants/token-map/1/definitions/1-0x4d224452801aced8b2f0aebe155379bb5d594381.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4d224452801aced8b2f0aebe155379bb5d594381",
+  chain_id: 1,
+  contract_address: "0x4d224452801aced8b2f0aebe155379bb5d594381",
+  name: "ApeCoin",
+  symbol: "APE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/18876.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "18876",
+  type: "token",
+});
