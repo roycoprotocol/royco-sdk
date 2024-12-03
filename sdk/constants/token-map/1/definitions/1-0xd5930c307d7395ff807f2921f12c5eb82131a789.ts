53cb623e888ae3287c3d3dd019a77ef8f44a9b67
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:49 -0800

    feat(token-map): Update Token -- 1-0xd5930c307d7395ff807f2921f12c5eb82131a789 -- BOLT (BOLT) (0xd593...a789)
    Token Details:
    - Id: 1-0xd5930c307d7395ff807f2921f12c5eb82131a789
    - Chain Id: 1
    - Contract Address: 0xd5930c307d7395ff807f2921f12c5eb82131a789
    - Name: BOLT
    - Symbol: BOLT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3843.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3843

---
+++ sdk/constants/token-map/1/definitions/1-0xd5930c307d7395ff807f2921f12c5eb82131a789.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd5930c307d7395ff807f2921f12c5eb82131a789",
+  chain_id: 1,
+  contract_address: "0xd5930c307d7395ff807f2921f12c5eb82131a789",
+  name: "BOLT",
+  symbol: "BOLT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3843.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3843",
+  type: "token",
+});
