48e67999c12825f3ec6cbedc3e17c62b240d72c5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:03 -0800

    feat(token-map): Update Token -- 1-0xbd356a39bff2cada8e9248532dd879147221cf76 -- WOM Protocol (WOM) (0xbd35...cf76)
    Token Details:
    - Id: 1-0xbd356a39bff2cada8e9248532dd879147221cf76
    - Chain Id: 1
    - Contract Address: 0xbd356a39bff2cada8e9248532dd879147221cf76
    - Name: WOM Protocol
    - Symbol: WOM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5328.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5328

---
+++ sdk/constants/token-map/1/definitions/1-0xbd356a39bff2cada8e9248532dd879147221cf76.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbd356a39bff2cada8e9248532dd879147221cf76",
+  chain_id: 1,
+  contract_address: "0xbd356a39bff2cada8e9248532dd879147221cf76",
+  name: "WOM Protocol",
+  symbol: "WOM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5328.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5328",
+  type: "token",
+});
