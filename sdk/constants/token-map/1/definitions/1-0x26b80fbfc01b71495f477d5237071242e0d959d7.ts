61e24ee48d36ecc1add715aaa680163538c347ec
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:02 -0800

    feat(token-map): Update Token -- 1-0x26b80fbfc01b71495f477d5237071242e0d959d7 -- Oasis (ROSE) (0x26b8...59d7)
    Token Details:
    - Id: 1-0x26b80fbfc01b71495f477d5237071242e0d959d7
    - Chain Id: 1
    - Contract Address: 0x26b80fbfc01b71495f477d5237071242e0d959d7
    - Name: Oasis
    - Symbol: ROSE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7653.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7653

---
+++ sdk/constants/token-map/1/definitions/1-0x26b80fbfc01b71495f477d5237071242e0d959d7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x26b80fbfc01b71495f477d5237071242e0d959d7",
+  chain_id: 1,
+  contract_address: "0x26b80fbfc01b71495f477d5237071242e0d959d7",
+  name: "Oasis",
+  symbol: "ROSE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7653.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7653",
+  type: "token",
+});
