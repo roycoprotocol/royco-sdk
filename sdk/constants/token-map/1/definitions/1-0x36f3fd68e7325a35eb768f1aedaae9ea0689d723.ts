27487e2c9c500ca0c8325d40f141f8124f4095cc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:56 -0800

    feat(token-map): Update Token -- 1-0x36f3fd68e7325a35eb768f1aedaae9ea0689d723 -- Empty Set Dollar (ESD) (0x36f3...d723)
    Token Details:
    - Id: 1-0x36f3fd68e7325a35eb768f1aedaae9ea0689d723
    - Chain Id: 1
    - Contract Address: 0x36f3fd68e7325a35eb768f1aedaae9ea0689d723
    - Name: Empty Set Dollar
    - Symbol: ESD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7033.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7033

---
+++ sdk/constants/token-map/1/definitions/1-0x36f3fd68e7325a35eb768f1aedaae9ea0689d723.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x36f3fd68e7325a35eb768f1aedaae9ea0689d723",
+  chain_id: 1,
+  contract_address: "0x36f3fd68e7325a35eb768f1aedaae9ea0689d723",
+  name: "Empty Set Dollar",
+  symbol: "ESD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7033.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7033",
+  type: "token",
+});
