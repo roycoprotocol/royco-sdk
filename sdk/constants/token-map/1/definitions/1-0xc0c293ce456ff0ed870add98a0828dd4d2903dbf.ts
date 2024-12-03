26842e997a59c84bfee7bf19ebe42fe16c31af6c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:33 -0800

    feat(token-map): Update Token -- 1-0xc0c293ce456ff0ed870add98a0828dd4d2903dbf -- Aura Finance (AURA) (0xc0c2...3dbf)
    Token Details:
    - Id: 1-0xc0c293ce456ff0ed870add98a0828dd4d2903dbf
    - Chain Id: 1
    - Contract Address: 0xc0c293ce456ff0ed870add98a0828dd4d2903dbf
    - Name: Aura Finance
    - Symbol: AURA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/21532.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 21532

---
+++ sdk/constants/token-map/1/definitions/1-0xc0c293ce456ff0ed870add98a0828dd4d2903dbf.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc0c293ce456ff0ed870add98a0828dd4d2903dbf",
+  chain_id: 1,
+  contract_address: "0xc0c293ce456ff0ed870add98a0828dd4d2903dbf",
+  name: "Aura Finance",
+  symbol: "AURA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/21532.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "21532",
+  type: "token",
+});
