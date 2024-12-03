2e0ff8e295fae6e47a4af3491c19419c94464457
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:46 -0800

    feat(token-map): Update Token -- 1-0xae697f994fc5ebc000f8e22ebffee04612f98a0d -- LGCY Network (LGCY) (0xae69...8a0d)
    Token Details:
    - Id: 1-0xae697f994fc5ebc000f8e22ebffee04612f98a0d
    - Chain Id: 1
    - Contract Address: 0xae697f994fc5ebc000f8e22ebffee04612f98a0d
    - Name: LGCY Network
    - Symbol: LGCY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6665.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6665

---
+++ sdk/constants/token-map/1/definitions/1-0xae697f994fc5ebc000f8e22ebffee04612f98a0d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xae697f994fc5ebc000f8e22ebffee04612f98a0d",
+  chain_id: 1,
+  contract_address: "0xae697f994fc5ebc000f8e22ebffee04612f98a0d",
+  name: "LGCY Network",
+  symbol: "LGCY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6665.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6665",
+  type: "token",
+});
