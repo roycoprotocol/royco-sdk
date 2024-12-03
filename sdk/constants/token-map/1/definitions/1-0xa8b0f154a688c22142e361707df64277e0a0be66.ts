3677a57dc716217179d3ec8e3a155bcd4cc3e6f4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:46 -0800

    feat(token-map): Update Token -- 1-0xa8b0f154a688c22142e361707df64277e0a0be66 -- Rake Finance (RAK) (0xa8b0...be66)
    Token Details:
    - Id: 1-0xa8b0f154a688c22142e361707df64277e0a0be66
    - Chain Id: 1
    - Contract Address: 0xa8b0f154a688c22142e361707df64277e0a0be66
    - Name: Rake Finance
    - Symbol: RAK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7632.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7632

---
+++ sdk/constants/token-map/1/definitions/1-0xa8b0f154a688c22142e361707df64277e0a0be66.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa8b0f154a688c22142e361707df64277e0a0be66",
+  chain_id: 1,
+  contract_address: "0xa8b0f154a688c22142e361707df64277e0a0be66",
+  name: "Rake Finance",
+  symbol: "RAK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7632.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7632",
+  type: "token",
+});
