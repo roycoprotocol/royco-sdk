3a232a86241ed248847830f4ee3f45d379d0b38f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:30 -0800

    feat(token-map): Update Token -- 1-0x94a7f270cd12545a277e656266aef5e27df3eb28 -- StreamCoin (STRM) (0x94a7...eb28)
    Token Details:
    - Id: 1-0x94a7f270cd12545a277e656266aef5e27df3eb28
    - Chain Id: 1
    - Contract Address: 0x94a7f270cd12545a277e656266aef5e27df3eb28
    - Name: StreamCoin
    - Symbol: STRM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/17464.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 17464

---
+++ sdk/constants/token-map/1/definitions/1-0x94a7f270cd12545a277e656266aef5e27df3eb28.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x94a7f270cd12545a277e656266aef5e27df3eb28",
+  chain_id: 1,
+  contract_address: "0x94a7f270cd12545a277e656266aef5e27df3eb28",
+  name: "StreamCoin",
+  symbol: "STRM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/17464.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "17464",
+  type: "token",
+});
