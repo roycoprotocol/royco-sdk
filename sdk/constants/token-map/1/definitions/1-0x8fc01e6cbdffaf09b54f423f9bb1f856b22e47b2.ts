bab7ed9fd1b459addff83ffb582c03cd07e2b52e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:52 -0800

    feat(token-map): Update Token -- 1-0x8fc01e6cbdffaf09b54f423f9bb1f856b22e47b2 -- Obitan Chain (OBTC) (0x8fc0...47b2)
    Token Details:
    - Id: 1-0x8fc01e6cbdffaf09b54f423f9bb1f856b22e47b2
    - Chain Id: 1
    - Contract Address: 0x8fc01e6cbdffaf09b54f423f9bb1f856b22e47b2
    - Name: Obitan Chain
    - Symbol: OBTC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3152.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 3152

---
+++ sdk/constants/token-map/1/definitions/1-0x8fc01e6cbdffaf09b54f423f9bb1f856b22e47b2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8fc01e6cbdffaf09b54f423f9bb1f856b22e47b2",
+  chain_id: 1,
+  contract_address: "0x8fc01e6cbdffaf09b54f423f9bb1f856b22e47b2",
+  name: "Obitan Chain",
+  symbol: "OBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3152.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3152",
+  type: "token",
+});
