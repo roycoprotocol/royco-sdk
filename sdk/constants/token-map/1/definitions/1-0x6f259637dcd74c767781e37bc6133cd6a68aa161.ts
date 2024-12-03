41d5764a651cb69f17ea631292c7174ac58d7bc9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:17:07 -0800

    feat(token-map): Update Token -- 1-0x6f259637dcd74c767781e37bc6133cd6a68aa161 -- Huobi Token (HT) (0x6f25...a161)
    Token Details:
    - Id: 1-0x6f259637dcd74c767781e37bc6133cd6a68aa161
    - Chain Id: 1
    - Contract Address: 0x6f259637dcd74c767781e37bc6133cd6a68aa161
    - Name: Huobi Token
    - Symbol: HT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2502.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2502

---
+++ sdk/constants/token-map/1/definitions/1-0x6f259637dcd74c767781e37bc6133cd6a68aa161.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6f259637dcd74c767781e37bc6133cd6a68aa161",
+  chain_id: 1,
+  contract_address: "0x6f259637dcd74c767781e37bc6133cd6a68aa161",
+  name: "Huobi Token",
+  symbol: "HT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2502.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2502",
+  type: "token",
+});
