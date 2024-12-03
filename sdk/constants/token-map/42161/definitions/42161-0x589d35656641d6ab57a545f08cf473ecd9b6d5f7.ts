e1e5840f232926c05c87774387154bbf2699abbb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:23 -0800

    feat(token-map): Update Token -- 42161-0x589d35656641d6ab57a545f08cf473ecd9b6d5f7 -- GYEN (GYEN) (0x589d...d5f7)
    Token Details:
    - Id: 42161-0x589d35656641d6ab57a545f08cf473ecd9b6d5f7
    - Chain Id: 42161
    - Contract Address: 0x589d35656641d6ab57a545f08cf473ecd9b6d5f7
    - Name: GYEN
    - Symbol: GYEN
    - Image: https://coin-images.coingecko.com/coins/images/14191/large/icon_gyen_200_200.png?1696513909
    - Decimals: 6
    - Source: coingecko
    - Search Id: gyen

---
+++ sdk/constants/token-map/42161/definitions/42161-0x589d35656641d6ab57a545f08cf473ecd9b6d5f7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x589d35656641d6ab57a545f08cf473ecd9b6d5f7",
+  chain_id: 42161,
+  contract_address: "0x589d35656641d6ab57a545f08cf473ecd9b6d5f7",
+  name: "GYEN",
+  symbol: "GYEN",
+  image: "https://coin-images.coingecko.com/coins/images/14191/large/icon_gyen_200_200.png?1696513909",
+  decimals: 6,
+  source: "coingecko",
+  search_id: "gyen",
+  type: "token",
+});
