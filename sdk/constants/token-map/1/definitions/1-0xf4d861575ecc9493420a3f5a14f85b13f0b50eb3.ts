a0f04099d1fef5046fb8d636e5269756f08d4439
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:33 -0800

    feat(token-map): Update Token -- 1-0xf4d861575ecc9493420a3f5a14f85b13f0b50eb3 -- Fractal ID (FCL) (0xf4d8...0eb3)
    Token Details:
    - Id: 1-0xf4d861575ecc9493420a3f5a14f85b13f0b50eb3
    - Chain Id: 1
    - Contract Address: 0xf4d861575ecc9493420a3f5a14f85b13f0b50eb3
    - Name: Fractal ID
    - Symbol: FCL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8544.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8544

---
+++ sdk/constants/token-map/1/definitions/1-0xf4d861575ecc9493420a3f5a14f85b13f0b50eb3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf4d861575ecc9493420a3f5a14f85b13f0b50eb3",
+  chain_id: 1,
+  contract_address: "0xf4d861575ecc9493420a3f5a14f85b13f0b50eb3",
+  name: "Fractal ID",
+  symbol: "FCL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8544.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8544",
+  type: "token",
+});
