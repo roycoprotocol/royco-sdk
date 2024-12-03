1bbd0252869294affe8431094e5f7eb15a0455c6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:57 -0800

    feat(token-map): Update Token -- 1-0x9dcaf532dba85e565bf6b425f07377ec2bb6f46a -- Dragonball Z Tribute (DBZ) (0x9dca...f46a)
    Token Details:
    - Id: 1-0x9dcaf532dba85e565bf6b425f07377ec2bb6f46a
    - Chain Id: 1
    - Contract Address: 0x9dcaf532dba85e565bf6b425f07377ec2bb6f46a
    - Name: Dragonball Z Tribute
    - Symbol: DBZ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29850.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 29850

---
+++ sdk/constants/token-map/1/definitions/1-0x9dcaf532dba85e565bf6b425f07377ec2bb6f46a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9dcaf532dba85e565bf6b425f07377ec2bb6f46a",
+  chain_id: 1,
+  contract_address: "0x9dcaf532dba85e565bf6b425f07377ec2bb6f46a",
+  name: "Dragonball Z Tribute",
+  symbol: "DBZ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29850.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "29850",
+  type: "token",
+});
