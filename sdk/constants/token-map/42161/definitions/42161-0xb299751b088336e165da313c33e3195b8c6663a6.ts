d7c94fcefbdd6b341b92fdd5bb2ca51db41251dd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:05 -0800

    feat(token-map): Update Token -- 42161-0xb299751b088336e165da313c33e3195b8c6663a6 -- StarHeroes (STAR) (0xb299...63a6)
    Token Details:
    - Id: 42161-0xb299751b088336e165da313c33e3195b8c6663a6
    - Chain Id: 42161
    - Contract Address: 0xb299751b088336e165da313c33e3195b8c6663a6
    - Name: StarHeroes
    - Symbol: STAR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29727.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29727

---
+++ sdk/constants/token-map/42161/definitions/42161-0xb299751b088336e165da313c33e3195b8c6663a6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0xb299751b088336e165da313c33e3195b8c6663a6",
+  chain_id: 42161,
+  contract_address: "0xb299751b088336e165da313c33e3195b8c6663a6",
+  name: "StarHeroes",
+  symbol: "STAR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29727.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29727",
+  type: "token",
+});
