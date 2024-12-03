8db3f70fbadd147f842ca55461dcacd40fcc39c6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:48 -0800

    feat(token-map): Update Token -- 1-0x68909e586eeac8f47315e84b4c9788dd54ef65bb -- EvenCoin (EVN) (0x6890...65bb)
    Token Details:
    - Id: 1-0x68909e586eeac8f47315e84b4c9788dd54ef65bb
    - Chain Id: 1
    - Contract Address: 0x68909e586eeac8f47315e84b4c9788dd54ef65bb
    - Name: EvenCoin
    - Symbol: EVN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3261.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3261

---
+++ sdk/constants/token-map/1/definitions/1-0x68909e586eeac8f47315e84b4c9788dd54ef65bb.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x68909e586eeac8f47315e84b4c9788dd54ef65bb",
+  chain_id: 1,
+  contract_address: "0x68909e586eeac8f47315e84b4c9788dd54ef65bb",
+  name: "EvenCoin",
+  symbol: "EVN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3261.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3261",
+  type: "token",
+});
