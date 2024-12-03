2fb864249250628b3b5feb1895b03aa35adaed9d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:51 -0800

    feat(token-map): Update Token -- 1-0x7e9e431a0b8c4d532c745b1043c7fa29a48d4fba -- eosDAC (EOSDAC) (0x7e9e...4fba)
    Token Details:
    - Id: 1-0x7e9e431a0b8c4d532c745b1043c7fa29a48d4fba
    - Chain Id: 1
    - Contract Address: 0x7e9e431a0b8c4d532c745b1043c7fa29a48d4fba
    - Name: eosDAC
    - Symbol: EOSDAC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2644.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2644

---
+++ sdk/constants/token-map/1/definitions/1-0x7e9e431a0b8c4d532c745b1043c7fa29a48d4fba.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7e9e431a0b8c4d532c745b1043c7fa29a48d4fba",
+  chain_id: 1,
+  contract_address: "0x7e9e431a0b8c4d532c745b1043c7fa29a48d4fba",
+  name: "eosDAC",
+  symbol: "EOSDAC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2644.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2644",
+  type: "token",
+});
