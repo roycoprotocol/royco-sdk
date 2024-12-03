e195d742fba7830cc628ab8658809e601705c6b5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:36 -0800

    feat(token-map): Update Token -- 1-0xa823e6722006afe99e91c30ff5295052fe6b8e32 -- Neumark (NEU) (0xa823...8e32)
    Token Details:
    - Id: 1-0xa823e6722006afe99e91c30ff5295052fe6b8e32
    - Chain Id: 1
    - Contract Address: 0xa823e6722006afe99e91c30ff5295052fe6b8e32
    - Name: Neumark
    - Symbol: NEU
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2318.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2318

---
+++ sdk/constants/token-map/1/definitions/1-0xa823e6722006afe99e91c30ff5295052fe6b8e32.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa823e6722006afe99e91c30ff5295052fe6b8e32",
+  chain_id: 1,
+  contract_address: "0xa823e6722006afe99e91c30ff5295052fe6b8e32",
+  name: "Neumark",
+  symbol: "NEU",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2318.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2318",
+  type: "token",
+});
