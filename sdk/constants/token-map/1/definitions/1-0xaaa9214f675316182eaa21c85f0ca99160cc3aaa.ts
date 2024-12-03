bf450662a119c0b48cde08ef1273a58f44c98ab0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:13 -0800

    feat(token-map): Update Token -- 1-0xaaa9214f675316182eaa21c85f0ca99160cc3aaa -- QANplatform (QANX) (0xaaa9...3aaa)
    Token Details:
    - Id: 1-0xaaa9214f675316182eaa21c85f0ca99160cc3aaa
    - Chain Id: 1
    - Contract Address: 0xaaa9214f675316182eaa21c85f0ca99160cc3aaa
    - Name: QANplatform
    - Symbol: QANX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5858.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5858

---
+++ sdk/constants/token-map/1/definitions/1-0xaaa9214f675316182eaa21c85f0ca99160cc3aaa.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xaaa9214f675316182eaa21c85f0ca99160cc3aaa",
+  chain_id: 1,
+  contract_address: "0xaaa9214f675316182eaa21c85f0ca99160cc3aaa",
+  name: "QANplatform",
+  symbol: "QANX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5858.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5858",
+  type: "token",
+});
