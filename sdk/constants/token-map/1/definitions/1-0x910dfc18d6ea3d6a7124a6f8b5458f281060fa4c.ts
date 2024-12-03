e84fe86a16f7f84919e1dae69d999918e441727f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:04 -0800

    feat(token-map): Update Token -- 1-0x910dfc18d6ea3d6a7124a6f8b5458f281060fa4c -- X8X Token (X8X) (0x910d...fa4c)
    Token Details:
    - Id: 1-0x910dfc18d6ea3d6a7124a6f8b5458f281060fa4c
    - Chain Id: 1
    - Contract Address: 0x910dfc18d6ea3d6a7124a6f8b5458f281060fa4c
    - Name: X8X Token
    - Symbol: X8X
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3079.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3079

---
+++ sdk/constants/token-map/1/definitions/1-0x910dfc18d6ea3d6a7124a6f8b5458f281060fa4c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x910dfc18d6ea3d6a7124a6f8b5458f281060fa4c",
+  chain_id: 1,
+  contract_address: "0x910dfc18d6ea3d6a7124a6f8b5458f281060fa4c",
+  name: "X8X Token",
+  symbol: "X8X",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3079.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3079",
+  type: "token",
+});
