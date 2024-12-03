b11d8908fac6613a7b03f51eb9cd5ccdc2a2fb29
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:21 -0800

    feat(token-map): Update Token -- 1-0xa62894d5196bc44e4c3978400ad07e7b30352372 -- X (X) (0xa628...2372)
    Token Details:
    - Id: 1-0xa62894d5196bc44e4c3978400ad07e7b30352372
    - Chain Id: 1
    - Contract Address: 0xa62894d5196bc44e4c3978400ad07e7b30352372
    - Name: X
    - Symbol: X
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/27704.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 27704

---
+++ sdk/constants/token-map/1/definitions/1-0xa62894d5196bc44e4c3978400ad07e7b30352372.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa62894d5196bc44e4c3978400ad07e7b30352372",
+  chain_id: 1,
+  contract_address: "0xa62894d5196bc44e4c3978400ad07e7b30352372",
+  name: "X",
+  symbol: "X",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/27704.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "27704",
+  type: "token",
+});
