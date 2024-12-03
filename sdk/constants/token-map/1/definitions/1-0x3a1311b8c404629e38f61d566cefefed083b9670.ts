880fd28ed4e77cc2b81f80215cb1a87f68837f1c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:30 -0800

    feat(token-map): Update Token -- 1-0x3a1311b8c404629e38f61d566cefefed083b9670 -- Piccolo Inu (PINU) (0x3a13...9670)
    Token Details:
    - Id: 1-0x3a1311b8c404629e38f61d566cefefed083b9670
    - Chain Id: 1
    - Contract Address: 0x3a1311b8c404629e38f61d566cefefed083b9670
    - Name: Piccolo Inu
    - Symbol: PINU
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/13047.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 13047

---
+++ sdk/constants/token-map/1/definitions/1-0x3a1311b8c404629e38f61d566cefefed083b9670.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3a1311b8c404629e38f61d566cefefed083b9670",
+  chain_id: 1,
+  contract_address: "0x3a1311b8c404629e38f61d566cefefed083b9670",
+  name: "Piccolo Inu",
+  symbol: "PINU",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/13047.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "13047",
+  type: "token",
+});
