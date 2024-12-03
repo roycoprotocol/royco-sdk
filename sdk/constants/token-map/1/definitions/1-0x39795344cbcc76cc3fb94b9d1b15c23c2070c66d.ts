0a2f321eb4a84d1c456a3efa1c11a92eecd7b422
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:26 -0800

    feat(token-map): Update Token -- 1-0x39795344cbcc76cc3fb94b9d1b15c23c2070c66d -- Seigniorage Shares (SHARE) (0x3979...c66d)
    Token Details:
    - Id: 1-0x39795344cbcc76cc3fb94b9d1b15c23c2070c66d
    - Chain Id: 1
    - Contract Address: 0x39795344cbcc76cc3fb94b9d1b15c23c2070c66d
    - Name: Seigniorage Shares
    - Symbol: SHARE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6868.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 6868

---
+++ sdk/constants/token-map/1/definitions/1-0x39795344cbcc76cc3fb94b9d1b15c23c2070c66d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x39795344cbcc76cc3fb94b9d1b15c23c2070c66d",
+  chain_id: 1,
+  contract_address: "0x39795344cbcc76cc3fb94b9d1b15c23c2070c66d",
+  name: "Seigniorage Shares",
+  symbol: "SHARE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6868.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "6868",
+  type: "token",
+});
