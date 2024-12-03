87106824647864114f2dd8fed55cdc2e79492f89
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:50 -0800

    feat(token-map): Update Token -- 1-0x0ea984e789302b7b612147e4e4144e64f21425eb -- Waletoken (WTN) (0x0ea9...25eb)
    Token Details:
    - Id: 1-0x0ea984e789302b7b612147e4e4144e64f21425eb
    - Chain Id: 1
    - Contract Address: 0x0ea984e789302b7b612147e4e4144e64f21425eb
    - Name: Waletoken
    - Symbol: WTN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3484.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 3484

---
+++ sdk/constants/token-map/1/definitions/1-0x0ea984e789302b7b612147e4e4144e64f21425eb.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0ea984e789302b7b612147e4e4144e64f21425eb",
+  chain_id: 1,
+  contract_address: "0x0ea984e789302b7b612147e4e4144e64f21425eb",
+  name: "Waletoken",
+  symbol: "WTN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3484.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3484",
+  type: "token",
+});
