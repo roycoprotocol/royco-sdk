3fc1f25c6f00c2eab101c82705854b1a641cf21d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:22 -0800

    feat(token-map): Update Token -- 1-0x7ba19b7f7d106a9a1e0985397b94f38eee0b555e -- Wixlar (WIX) (0x7ba1...555e)
    Token Details:
    - Id: 1-0x7ba19b7f7d106a9a1e0985397b94f38eee0b555e
    - Chain Id: 1
    - Contract Address: 0x7ba19b7f7d106a9a1e0985397b94f38eee0b555e
    - Name: Wixlar
    - Symbol: WIX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3404.png
    - Decimals: 2
    - Source: coinmarketcap
    - Search Id: 3404

---
+++ sdk/constants/token-map/1/definitions/1-0x7ba19b7f7d106a9a1e0985397b94f38eee0b555e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7ba19b7f7d106a9a1e0985397b94f38eee0b555e",
+  chain_id: 1,
+  contract_address: "0x7ba19b7f7d106a9a1e0985397b94f38eee0b555e",
+  name: "Wixlar",
+  symbol: "WIX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3404.png",
+  decimals: 2,
+  source: "coinmarketcap",
+  search_id: "3404",
+  type: "token",
+});
