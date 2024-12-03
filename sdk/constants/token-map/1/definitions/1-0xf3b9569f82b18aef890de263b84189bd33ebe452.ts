964da37f487b013f82562d771c287520fa13b77c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:18 -0800

    feat(token-map): Update Token -- 1-0xf3b9569f82b18aef890de263b84189bd33ebe452 -- CAW(A Hunters Dream) (CAW) (0xf3b9...e452)
    Token Details:
    - Id: 1-0xf3b9569f82b18aef890de263b84189bd33ebe452
    - Chain Id: 1
    - Contract Address: 0xf3b9569f82b18aef890de263b84189bd33ebe452
    - Name: CAW(A Hunters Dream)
    - Symbol: CAW
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/19618.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 19618

---
+++ sdk/constants/token-map/1/definitions/1-0xf3b9569f82b18aef890de263b84189bd33ebe452.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf3b9569f82b18aef890de263b84189bd33ebe452",
+  chain_id: 1,
+  contract_address: "0xf3b9569f82b18aef890de263b84189bd33ebe452",
+  name: "CAW(A Hunters Dream)",
+  symbol: "CAW",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/19618.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "19618",
+  type: "token",
+});
