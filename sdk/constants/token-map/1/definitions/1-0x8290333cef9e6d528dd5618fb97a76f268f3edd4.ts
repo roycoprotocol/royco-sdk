8ae939ab4ba04ae6925c514b2db714f0cb6e3051
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:24 -0800

    feat(token-map): Update Token -- 1-0x8290333cef9e6d528dd5618fb97a76f268f3edd4 -- Ankr (ANKR) (0x8290...edd4)
    Token Details:
    - Id: 1-0x8290333cef9e6d528dd5618fb97a76f268f3edd4
    - Chain Id: 1
    - Contract Address: 0x8290333cef9e6d528dd5618fb97a76f268f3edd4
    - Name: Ankr
    - Symbol: ANKR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3783.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3783

---
+++ sdk/constants/token-map/1/definitions/1-0x8290333cef9e6d528dd5618fb97a76f268f3edd4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8290333cef9e6d528dd5618fb97a76f268f3edd4",
+  chain_id: 1,
+  contract_address: "0x8290333cef9e6d528dd5618fb97a76f268f3edd4",
+  name: "Ankr",
+  symbol: "ANKR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3783.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3783",
+  type: "token",
+});
