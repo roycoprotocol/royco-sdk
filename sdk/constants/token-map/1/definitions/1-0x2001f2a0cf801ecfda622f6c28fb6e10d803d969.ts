382df0be36d0a42ae9fde636136aa877745bddf4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:59 -0800

    feat(token-map): Update Token -- 1-0x2001f2a0cf801ecfda622f6c28fb6e10d803d969 -- CoinLoan (CLT) (0x2001...d969)
    Token Details:
    - Id: 1-0x2001f2a0cf801ecfda622f6c28fb6e10d803d969
    - Chain Id: 1
    - Contract Address: 0x2001f2a0cf801ecfda622f6c28fb6e10d803d969
    - Name: CoinLoan
    - Symbol: CLT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5401.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 5401

---
+++ sdk/constants/token-map/1/definitions/1-0x2001f2a0cf801ecfda622f6c28fb6e10d803d969.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2001f2a0cf801ecfda622f6c28fb6e10d803d969",
+  chain_id: 1,
+  contract_address: "0x2001f2a0cf801ecfda622f6c28fb6e10d803d969",
+  name: "CoinLoan",
+  symbol: "CLT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5401.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "5401",
+  type: "token",
+});
