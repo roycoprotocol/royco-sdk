dfb0281b7d871aa2a3a91b425122321f9541a611
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:33 -0800

    feat(token-map): Update Token -- 1-0xba100000625a3754423978a60c9317c58a424e3d -- Balancer (BAL) (0xba10...4e3d)
    Token Details:
    - Id: 1-0xba100000625a3754423978a60c9317c58a424e3d
    - Chain Id: 1
    - Contract Address: 0xba100000625a3754423978a60c9317c58a424e3d
    - Name: Balancer
    - Symbol: BAL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5728.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5728

---
+++ sdk/constants/token-map/1/definitions/1-0xba100000625a3754423978a60c9317c58a424e3d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xba100000625a3754423978a60c9317c58a424e3d",
+  chain_id: 1,
+  contract_address: "0xba100000625a3754423978a60c9317c58a424e3d",
+  name: "Balancer",
+  symbol: "BAL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5728.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5728",
+  type: "token",
+});
