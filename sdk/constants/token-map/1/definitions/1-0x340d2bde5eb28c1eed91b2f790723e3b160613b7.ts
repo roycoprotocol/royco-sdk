4b5eb06998c33891dd82f5e887e665cbcc7f3f02
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:17 -0800

    feat(token-map): Update Token -- 1-0x340d2bde5eb28c1eed91b2f790723e3b160613b7 -- BLOCKv (VEE) (0x340d...13b7)
    Token Details:
    - Id: 1-0x340d2bde5eb28c1eed91b2f790723e3b160613b7
    - Chain Id: 1
    - Contract Address: 0x340d2bde5eb28c1eed91b2f790723e3b160613b7
    - Name: BLOCKv
    - Symbol: VEE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2223.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2223

---
+++ sdk/constants/token-map/1/definitions/1-0x340d2bde5eb28c1eed91b2f790723e3b160613b7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x340d2bde5eb28c1eed91b2f790723e3b160613b7",
+  chain_id: 1,
+  contract_address: "0x340d2bde5eb28c1eed91b2f790723e3b160613b7",
+  name: "BLOCKv",
+  symbol: "VEE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2223.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2223",
+  type: "token",
+});
