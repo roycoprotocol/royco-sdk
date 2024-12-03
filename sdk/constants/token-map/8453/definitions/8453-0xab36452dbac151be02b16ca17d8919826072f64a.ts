873b1ea9209587ce9874e020b570448b14ecc83f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:43:04 -0800

    feat(token-map): Update Token -- 8453-0xab36452dbac151be02b16ca17d8919826072f64a -- Reserve Rights (RSR) (0xab36...f64a)
    Token Details:
    - Id: 8453-0xab36452dbac151be02b16ca17d8919826072f64a
    - Chain Id: 8453
    - Contract Address: 0xab36452dbac151be02b16ca17d8919826072f64a
    - Name: Reserve Rights
    - Symbol: RSR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3964.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3964

---
+++ sdk/constants/token-map/8453/definitions/8453-0xab36452dbac151be02b16ca17d8919826072f64a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0xab36452dbac151be02b16ca17d8919826072f64a",
+  chain_id: 8453,
+  contract_address: "0xab36452dbac151be02b16ca17d8919826072f64a",
+  name: "Reserve Rights",
+  symbol: "RSR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3964.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3964",
+  type: "token",
+});
