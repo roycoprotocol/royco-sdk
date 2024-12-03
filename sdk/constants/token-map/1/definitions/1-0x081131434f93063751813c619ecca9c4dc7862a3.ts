5b1b0bd77296eecf83ff5f18b5b663188df21639
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:14 -0800

    feat(token-map): Update Token -- 1-0x081131434f93063751813c619ecca9c4dc7862a3 -- Mines of Dalarnia (DAR) (0x0811...62a3)
    Token Details:
    - Id: 1-0x081131434f93063751813c619ecca9c4dc7862a3
    - Chain Id: 1
    - Contract Address: 0x081131434f93063751813c619ecca9c4dc7862a3
    - Name: Mines of Dalarnia
    - Symbol: DAR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11374.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 11374

---
+++ sdk/constants/token-map/1/definitions/1-0x081131434f93063751813c619ecca9c4dc7862a3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x081131434f93063751813c619ecca9c4dc7862a3",
+  chain_id: 1,
+  contract_address: "0x081131434f93063751813c619ecca9c4dc7862a3",
+  name: "Mines of Dalarnia",
+  symbol: "DAR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11374.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "11374",
+  type: "token",
+});
