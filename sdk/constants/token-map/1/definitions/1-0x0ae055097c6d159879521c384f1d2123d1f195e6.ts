2d77d996367454a43fc117ae00e5b12898575d64
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:58:50 -0800

    feat(token-map): Update Token -- 1-0x0ae055097c6d159879521c384f1d2123d1f195e6 -- STAKE (STAKE) (0x0ae0...95e6)
    Token Details:
    - Id: 1-0x0ae055097c6d159879521c384f1d2123d1f195e6
    - Chain Id: 1
    - Contract Address: 0x0ae055097c6d159879521c384f1d2123d1f195e6
    - Name: STAKE
    - Symbol: STAKE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5601.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5601

---
+++ sdk/constants/token-map/1/definitions/1-0x0ae055097c6d159879521c384f1d2123d1f195e6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0ae055097c6d159879521c384f1d2123d1f195e6",
+  chain_id: 1,
+  contract_address: "0x0ae055097c6d159879521c384f1d2123d1f195e6",
+  name: "STAKE",
+  symbol: "STAKE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5601.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5601",
+  type: "token",
+});
