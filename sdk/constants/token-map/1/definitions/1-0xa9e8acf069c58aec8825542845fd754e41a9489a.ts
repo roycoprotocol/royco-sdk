3a12723fcece481a7d6c646f5748823d8314ee22
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:01 -0800

    feat(token-map): Update Token -- 1-0xa9e8acf069c58aec8825542845fd754e41a9489a -- PepeCoin (PEPECOIN) (0xa9e8...489a)
    Token Details:
    - Id: 1-0xa9e8acf069c58aec8825542845fd754e41a9489a
    - Chain Id: 1
    - Contract Address: 0xa9e8acf069c58aec8825542845fd754e41a9489a
    - Name: PepeCoin
    - Symbol: PEPECOIN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/24835.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 24835

---
+++ sdk/constants/token-map/1/definitions/1-0xa9e8acf069c58aec8825542845fd754e41a9489a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa9e8acf069c58aec8825542845fd754e41a9489a",
+  chain_id: 1,
+  contract_address: "0xa9e8acf069c58aec8825542845fd754e41a9489a",
+  name: "PepeCoin",
+  symbol: "PEPECOIN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/24835.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "24835",
+  type: "token",
+});
