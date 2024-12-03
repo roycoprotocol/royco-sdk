b3a3a163926361a5e49586b397f162c8d6a4f579
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:17 -0800

    feat(token-map): Update Token -- 1-0xa5f2211b9b8170f694421f2046281775e8468044 -- THORSwap (THOR) (0xa5f2...8044)
    Token Details:
    - Id: 1-0xa5f2211b9b8170f694421f2046281775e8468044
    - Chain Id: 1
    - Contract Address: 0xa5f2211b9b8170f694421f2046281775e8468044
    - Name: THORSwap
    - Symbol: THOR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12942.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 12942

---
+++ sdk/constants/token-map/1/definitions/1-0xa5f2211b9b8170f694421f2046281775e8468044.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa5f2211b9b8170f694421f2046281775e8468044",
+  chain_id: 1,
+  contract_address: "0xa5f2211b9b8170f694421f2046281775e8468044",
+  name: "THORSwap",
+  symbol: "THOR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12942.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "12942",
+  type: "token",
+});
