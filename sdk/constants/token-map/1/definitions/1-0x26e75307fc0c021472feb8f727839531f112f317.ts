cf714af977191ca312aca849541d926156249d72
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:10 -0800

    feat(token-map): Update Token -- 1-0x26e75307fc0c021472feb8f727839531f112f317 -- CRYPTO20 (C20) (0x26e7...f317)
    Token Details:
    - Id: 1-0x26e75307fc0c021472feb8f727839531f112f317
    - Chain Id: 1
    - Contract Address: 0x26e75307fc0c021472feb8f727839531f112f317
    - Name: CRYPTO20
    - Symbol: C20
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2444.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2444

---
+++ sdk/constants/token-map/1/definitions/1-0x26e75307fc0c021472feb8f727839531f112f317.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x26e75307fc0c021472feb8f727839531f112f317",
+  chain_id: 1,
+  contract_address: "0x26e75307fc0c021472feb8f727839531f112f317",
+  name: "CRYPTO20",
+  symbol: "C20",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2444.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2444",
+  type: "token",
+});
