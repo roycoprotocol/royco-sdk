4bfb6d85a84b450704ebbb2b22fd7d4a1ae85ede
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:10:52 -0800

    feat(token-map): Update Token -- 1-0x4c45bbec2ff7810ef4a77ad7bd4757c446fe4155 -- Jungle Labz (JNGL) (0x4c45...4155)
    Token Details:
    - Id: 1-0x4c45bbec2ff7810ef4a77ad7bd4757c446fe4155
    - Chain Id: 1
    - Contract Address: 0x4c45bbec2ff7810ef4a77ad7bd4757c446fe4155
    - Name: Jungle Labz
    - Symbol: JNGL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28125.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 28125

---
+++ sdk/constants/token-map/1/definitions/1-0x4c45bbec2ff7810ef4a77ad7bd4757c446fe4155.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4c45bbec2ff7810ef4a77ad7bd4757c446fe4155",
+  chain_id: 1,
+  contract_address: "0x4c45bbec2ff7810ef4a77ad7bd4757c446fe4155",
+  name: "Jungle Labz",
+  symbol: "JNGL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28125.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "28125",
+  type: "token",
+});
