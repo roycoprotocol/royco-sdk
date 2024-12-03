cb0023ea5ef26ed61b11b6293a72a926d84373af
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:41 -0800

    feat(token-map): Update Token -- 1-0x358aa737e033f34df7c54306960a38d09aabd523 -- Ares Protocol (ARES) (0x358a...d523)
    Token Details:
    - Id: 1-0x358aa737e033f34df7c54306960a38d09aabd523
    - Chain Id: 1
    - Contract Address: 0x358aa737e033f34df7c54306960a38d09aabd523
    - Name: Ares Protocol
    - Symbol: ARES
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8702.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8702

---
+++ sdk/constants/token-map/1/definitions/1-0x358aa737e033f34df7c54306960a38d09aabd523.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x358aa737e033f34df7c54306960a38d09aabd523",
+  chain_id: 1,
+  contract_address: "0x358aa737e033f34df7c54306960a38d09aabd523",
+  name: "Ares Protocol",
+  symbol: "ARES",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8702.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8702",
+  type: "token",
+});
