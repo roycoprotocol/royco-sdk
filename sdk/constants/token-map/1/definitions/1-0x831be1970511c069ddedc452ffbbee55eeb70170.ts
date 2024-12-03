5623111f88b4c64b1d85a09d44961ba47ef720fc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:26 -0800

    feat(token-map): Update Token -- 1-0x831be1970511c069ddedc452ffbbee55eeb70170 -- ELF Wallet (ELF) (0x831b...0170)
    Token Details:
    - Id: 1-0x831be1970511c069ddedc452ffbbee55eeb70170
    - Chain Id: 1
    - Contract Address: 0x831be1970511c069ddedc452ffbbee55eeb70170
    - Name: ELF Wallet
    - Symbol: ELF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/22153.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 22153

---
+++ sdk/constants/token-map/1/definitions/1-0x831be1970511c069ddedc452ffbbee55eeb70170.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x831be1970511c069ddedc452ffbbee55eeb70170",
+  chain_id: 1,
+  contract_address: "0x831be1970511c069ddedc452ffbbee55eeb70170",
+  name: "ELF Wallet",
+  symbol: "ELF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/22153.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "22153",
+  type: "token",
+});
