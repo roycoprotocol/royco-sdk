6d3967ac48bab04c3484f39fbd1694e9c5df788b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:14 -0800

    feat(token-map): Update Token -- 1-0xf94b5c5651c888d928439ab6514b93944eee6f48 -- Yield App (YLD) (0xf94b...6f48)
    Token Details:
    - Id: 1-0xf94b5c5651c888d928439ab6514b93944eee6f48
    - Chain Id: 1
    - Contract Address: 0xf94b5c5651c888d928439ab6514b93944eee6f48
    - Name: Yield App
    - Symbol: YLD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8066.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8066

---
+++ sdk/constants/token-map/1/definitions/1-0xf94b5c5651c888d928439ab6514b93944eee6f48.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf94b5c5651c888d928439ab6514b93944eee6f48",
+  chain_id: 1,
+  contract_address: "0xf94b5c5651c888d928439ab6514b93944eee6f48",
+  name: "Yield App",
+  symbol: "YLD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8066.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8066",
+  type: "token",
+});
