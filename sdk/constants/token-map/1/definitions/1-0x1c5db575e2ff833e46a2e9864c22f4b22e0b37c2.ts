21ea4b618dfb26cf5e6a661e58fdd1f3cf857db8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:02:21 -0800

    feat(token-map): Update Token -- 1-0x1c5db575e2ff833e46a2e9864c22f4b22e0b37c2 -- renZEC (RENZEC) (0x1c5d...37c2)
    Token Details:
    - Id: 1-0x1c5db575e2ff833e46a2e9864c22f4b22e0b37c2
    - Chain Id: 1
    - Contract Address: 0x1c5db575e2ff833e46a2e9864c22f4b22e0b37c2
    - Name: renZEC
    - Symbol: RENZEC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8904.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 8904

---
+++ sdk/constants/token-map/1/definitions/1-0x1c5db575e2ff833e46a2e9864c22f4b22e0b37c2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1c5db575e2ff833e46a2e9864c22f4b22e0b37c2",
+  chain_id: 1,
+  contract_address: "0x1c5db575e2ff833e46a2e9864c22f4b22e0b37c2",
+  name: "renZEC",
+  symbol: "RENZEC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8904.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "8904",
+  type: "token",
+});
