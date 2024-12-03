d2ea414357c42c8706239aa3592c362a6c10ee37
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:09 -0800

    feat(token-map): Update Token -- 1-0x667088b212ce3d06a1b553a7221e1fd19000d9af -- Wings (WINGS) (0x6670...d9af)
    Token Details:
    - Id: 1-0x667088b212ce3d06a1b553a7221e1fd19000d9af
    - Chain Id: 1
    - Contract Address: 0x667088b212ce3d06a1b553a7221e1fd19000d9af
    - Name: Wings
    - Symbol: WINGS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1500.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1500

---
+++ sdk/constants/token-map/1/definitions/1-0x667088b212ce3d06a1b553a7221e1fd19000d9af.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x667088b212ce3d06a1b553a7221e1fd19000d9af",
+  chain_id: 1,
+  contract_address: "0x667088b212ce3d06a1b553a7221e1fd19000d9af",
+  name: "Wings",
+  symbol: "WINGS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1500.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1500",
+  type: "token",
+});
