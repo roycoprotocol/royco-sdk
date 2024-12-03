c8fd2d4d73e7fb12c2e5c08bf0f15438c9532042
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:35:40 -0800

    feat(token-map): Update Token -- 1-0xe28b3b32b6c345a34ff64674606124dd5aceca30 -- Injective (INJ) (0xe28b...ca30)
    Token Details:
    - Id: 1-0xe28b3b32b6c345a34ff64674606124dd5aceca30
    - Chain Id: 1
    - Contract Address: 0xe28b3b32b6c345a34ff64674606124dd5aceca30
    - Name: Injective
    - Symbol: INJ
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7226.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7226

---
+++ sdk/constants/token-map/1/definitions/1-0xe28b3b32b6c345a34ff64674606124dd5aceca30.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe28b3b32b6c345a34ff64674606124dd5aceca30",
+  chain_id: 1,
+  contract_address: "0xe28b3b32b6c345a34ff64674606124dd5aceca30",
+  name: "Injective",
+  symbol: "INJ",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7226.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7226",
+  type: "token",
+});
