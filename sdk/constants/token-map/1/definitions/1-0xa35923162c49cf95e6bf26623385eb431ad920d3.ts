9e9f336c89191e0ae1b3b6b63aec49826067d3e6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:04 -0800

    feat(token-map): Update Token -- 1-0xa35923162c49cf95e6bf26623385eb431ad920d3 -- Turbo (TURBO) (0xa359...20d3)
    Token Details:
    - Id: 1-0xa35923162c49cf95e6bf26623385eb431ad920d3
    - Chain Id: 1
    - Contract Address: 0xa35923162c49cf95e6bf26623385eb431ad920d3
    - Name: Turbo
    - Symbol: TURBO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/24911.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 24911

---
+++ sdk/constants/token-map/1/definitions/1-0xa35923162c49cf95e6bf26623385eb431ad920d3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa35923162c49cf95e6bf26623385eb431ad920d3",
+  chain_id: 1,
+  contract_address: "0xa35923162c49cf95e6bf26623385eb431ad920d3",
+  name: "Turbo",
+  symbol: "TURBO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/24911.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "24911",
+  type: "token",
+});
