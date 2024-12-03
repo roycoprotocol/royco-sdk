9fe620f4f0223beda20f94f97579065e4b13f0b0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:02 -0800

    feat(token-map): Update Token -- 1-0xf1df7305e4bab3885cab5b1e4dfc338452a67891 -- PaLM AI (PALM) (0xf1df...7891)
    Token Details:
    - Id: 1-0xf1df7305e4bab3885cab5b1e4dfc338452a67891
    - Chain Id: 1
    - Contract Address: 0xf1df7305e4bab3885cab5b1e4dfc338452a67891
    - Name: PaLM AI
    - Symbol: PALM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/28567.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 28567

---
+++ sdk/constants/token-map/1/definitions/1-0xf1df7305e4bab3885cab5b1e4dfc338452a67891.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf1df7305e4bab3885cab5b1e4dfc338452a67891",
+  chain_id: 1,
+  contract_address: "0xf1df7305e4bab3885cab5b1e4dfc338452a67891",
+  name: "PaLM AI",
+  symbol: "PALM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/28567.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "28567",
+  type: "token",
+});
