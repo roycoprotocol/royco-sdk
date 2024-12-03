c3c3e31d1d4bb42d267739d5e1c334b910ff81be
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:18:07 -0800

    feat(token-map): Update Token -- 1-0x73d7c860998ca3c01ce8c808f5577d94d545d1b4 -- IX Swap (IXS) (0x73d7...d1b4)
    Token Details:
    - Id: 1-0x73d7c860998ca3c01ce8c808f5577d94d545d1b4
    - Chain Id: 1
    - Contract Address: 0x73d7c860998ca3c01ce8c808f5577d94d545d1b4
    - Name: IX Swap
    - Symbol: IXS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11620.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 11620

---
+++ sdk/constants/token-map/1/definitions/1-0x73d7c860998ca3c01ce8c808f5577d94d545d1b4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x73d7c860998ca3c01ce8c808f5577d94d545d1b4",
+  chain_id: 1,
+  contract_address: "0x73d7c860998ca3c01ce8c808f5577d94d545d1b4",
+  name: "IX Swap",
+  symbol: "IXS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11620.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "11620",
+  type: "token",
+});
