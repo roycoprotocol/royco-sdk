cdc1b9db46f9db4b7a8a819862e4bb646b8d22cf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:28 -0800

    feat(token-map): Update Token -- 1-0x0cec1a9154ff802e7934fc916ed7ca50bde6844e -- PoolTogether (POOL) (0x0cec...844e)
    Token Details:
    - Id: 1-0x0cec1a9154ff802e7934fc916ed7ca50bde6844e
    - Chain Id: 1
    - Contract Address: 0x0cec1a9154ff802e7934fc916ed7ca50bde6844e
    - Name: PoolTogether
    - Symbol: POOL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8508.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8508

---
+++ sdk/constants/token-map/1/definitions/1-0x0cec1a9154ff802e7934fc916ed7ca50bde6844e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0cec1a9154ff802e7934fc916ed7ca50bde6844e",
+  chain_id: 1,
+  contract_address: "0x0cec1a9154ff802e7934fc916ed7ca50bde6844e",
+  name: "PoolTogether",
+  symbol: "POOL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8508.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8508",
+  type: "token",
+});
