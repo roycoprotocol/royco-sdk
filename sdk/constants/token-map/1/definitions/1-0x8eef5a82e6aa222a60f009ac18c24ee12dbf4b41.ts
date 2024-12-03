507e4579f8c08b645e0eb3652041c6d39e44e691
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:36 -0800

    feat(token-map): Update Token -- 1-0x8eef5a82e6aa222a60f009ac18c24ee12dbf4b41 -- Autobahn Network (TXL) (0x8eef...4b41)
    Token Details:
    - Id: 1-0x8eef5a82e6aa222a60f009ac18c24ee12dbf4b41
    - Chain Id: 1
    - Contract Address: 0x8eef5a82e6aa222a60f009ac18c24ee12dbf4b41
    - Name: Autobahn Network
    - Symbol: TXL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7024.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7024

---
+++ sdk/constants/token-map/1/definitions/1-0x8eef5a82e6aa222a60f009ac18c24ee12dbf4b41.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8eef5a82e6aa222a60f009ac18c24ee12dbf4b41",
+  chain_id: 1,
+  contract_address: "0x8eef5a82e6aa222a60f009ac18c24ee12dbf4b41",
+  name: "Autobahn Network",
+  symbol: "TXL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7024.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7024",
+  type: "token",
+});
