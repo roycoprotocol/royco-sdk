8e60fc1b82a1ba6bb199dfb83716f3b6946b1059
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:10 -0800

    feat(token-map): Update Token -- 1-0x9f284e1337a815fe77d2ff4ae46544645b20c5ff -- Darwinia Commitment Token (KTON) (0x9f28...c5ff)
    Token Details:
    - Id: 1-0x9f284e1337a815fe77d2ff4ae46544645b20c5ff
    - Chain Id: 1
    - Contract Address: 0x9f284e1337a815fe77d2ff4ae46544645b20c5ff
    - Name: Darwinia Commitment Token
    - Symbol: KTON
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5931.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5931

---
+++ sdk/constants/token-map/1/definitions/1-0x9f284e1337a815fe77d2ff4ae46544645b20c5ff.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9f284e1337a815fe77d2ff4ae46544645b20c5ff",
+  chain_id: 1,
+  contract_address: "0x9f284e1337a815fe77d2ff4ae46544645b20c5ff",
+  name: "Darwinia Commitment Token",
+  symbol: "KTON",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5931.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5931",
+  type: "token",
+});
