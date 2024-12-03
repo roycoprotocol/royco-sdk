c3c9424d0b672dbaffa01b4d4fef106de967b29a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:16:02 -0800

    feat(token-map): Update Token -- 1-0x6982508145454ce325ddbe47a25d4ec3d2311933 -- Pepe (PEPE) (0x6982...1933)
    Token Details:
    - Id: 1-0x6982508145454ce325ddbe47a25d4ec3d2311933
    - Chain Id: 1
    - Contract Address: 0x6982508145454ce325ddbe47a25d4ec3d2311933
    - Name: Pepe
    - Symbol: PEPE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/24478.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 24478

---
+++ sdk/constants/token-map/1/definitions/1-0x6982508145454ce325ddbe47a25d4ec3d2311933.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6982508145454ce325ddbe47a25d4ec3d2311933",
+  chain_id: 1,
+  contract_address: "0x6982508145454ce325ddbe47a25d4ec3d2311933",
+  name: "Pepe",
+  symbol: "PEPE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/24478.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "24478",
+  type: "token",
+});
