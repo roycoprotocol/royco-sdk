acf83e1a557e74138c43cacb0dac8aaf3c82be6a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:01:08 -0800

    feat(token-map): Update Token -- 1-0x159a1dfae19057de57dfffcbb3da1ae784678965 -- Reflex (RFX) (0x159a...8965)
    Token Details:
    - Id: 1-0x159a1dfae19057de57dfffcbb3da1ae784678965
    - Chain Id: 1
    - Contract Address: 0x159a1dfae19057de57dfffcbb3da1ae784678965
    - Name: Reflex
    - Symbol: RFX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7355.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 7355

---
+++ sdk/constants/token-map/1/definitions/1-0x159a1dfae19057de57dfffcbb3da1ae784678965.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x159a1dfae19057de57dfffcbb3da1ae784678965",
+  chain_id: 1,
+  contract_address: "0x159a1dfae19057de57dfffcbb3da1ae784678965",
+  name: "Reflex",
+  symbol: "RFX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7355.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "7355",
+  type: "token",
+});
