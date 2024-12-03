0569740de49033a970edcd6288225dc992b2af4b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:05 -0800

    feat(token-map): Update Token -- 1-0x990f341946a3fdb507ae7e52d17851b87168017c -- Strong (STRONG) (0x990f...017c)
    Token Details:
    - Id: 1-0x990f341946a3fdb507ae7e52d17851b87168017c
    - Chain Id: 1
    - Contract Address: 0x990f341946a3fdb507ae7e52d17851b87168017c
    - Name: Strong
    - Symbol: STRONG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6511.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6511

---
+++ sdk/constants/token-map/1/definitions/1-0x990f341946a3fdb507ae7e52d17851b87168017c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x990f341946a3fdb507ae7e52d17851b87168017c",
+  chain_id: 1,
+  contract_address: "0x990f341946a3fdb507ae7e52d17851b87168017c",
+  name: "Strong",
+  symbol: "STRONG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6511.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6511",
+  type: "token",
+});
