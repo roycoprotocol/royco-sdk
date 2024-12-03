92ffb60d19b47f2f8b210cf45850d7c845191b6a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:57:18 -0800

    feat(token-map): Update Token -- 1-0x037a54aab062628c9bbae1fdb1583c195585fe41 -- LCX (LCX) (0x037a...fe41)
    Token Details:
    - Id: 1-0x037a54aab062628c9bbae1fdb1583c195585fe41
    - Chain Id: 1
    - Contract Address: 0x037a54aab062628c9bbae1fdb1583c195585fe41
    - Name: LCX
    - Symbol: LCX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4950.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4950

---
+++ sdk/constants/token-map/1/definitions/1-0x037a54aab062628c9bbae1fdb1583c195585fe41.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x037a54aab062628c9bbae1fdb1583c195585fe41",
+  chain_id: 1,
+  contract_address: "0x037a54aab062628c9bbae1fdb1583c195585fe41",
+  name: "LCX",
+  symbol: "LCX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4950.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4950",
+  type: "token",
+});
