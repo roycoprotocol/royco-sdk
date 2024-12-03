7a3f0f4d5d299dbeab9e5487045a7f9a8bfe9f4c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:25:16 -0800

    feat(token-map): Update Token -- 1-0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08 -- Biaoqing (BIAO) (0x9fd9...de08)
    Token Details:
    - Id: 1-0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08
    - Chain Id: 1
    - Contract Address: 0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08
    - Name: Biaoqing
    - Symbol: BIAO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/31730.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 31730

---
+++ sdk/constants/token-map/1/definitions/1-0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08",
+  chain_id: 1,
+  contract_address: "0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08",
+  name: "Biaoqing",
+  symbol: "BIAO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/31730.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "31730",
+  type: "token",
+});
