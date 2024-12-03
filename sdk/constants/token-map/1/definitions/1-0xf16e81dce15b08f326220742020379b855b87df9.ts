f427b89f383e7a86bf7d03d7957f6bdeb9b061a0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:56 -0800

    feat(token-map): Update Token -- 1-0xf16e81dce15b08f326220742020379b855b87df9 -- Popsicle Finance (ICE) (0xf16e...7df9)
    Token Details:
    - Id: 1-0xf16e81dce15b08f326220742020379b855b87df9
    - Chain Id: 1
    - Contract Address: 0xf16e81dce15b08f326220742020379b855b87df9
    - Name: Popsicle Finance
    - Symbol: ICE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9073.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9073

---
+++ sdk/constants/token-map/1/definitions/1-0xf16e81dce15b08f326220742020379b855b87df9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf16e81dce15b08f326220742020379b855b87df9",
+  chain_id: 1,
+  contract_address: "0xf16e81dce15b08f326220742020379b855b87df9",
+  name: "Popsicle Finance",
+  symbol: "ICE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9073.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9073",
+  type: "token",
+});
