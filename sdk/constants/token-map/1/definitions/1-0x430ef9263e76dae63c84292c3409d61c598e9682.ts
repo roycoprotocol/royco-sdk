ce3b979d2737286c3f24877328176f8d22491cb9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:13 -0800

    feat(token-map): Update Token -- 1-0x430ef9263e76dae63c84292c3409d61c598e9682 -- Vulcan Forged (PYR) (PYR) (0x430e...9682)
    Token Details:
    - Id: 1-0x430ef9263e76dae63c84292c3409d61c598e9682
    - Chain Id: 1
    - Contract Address: 0x430ef9263e76dae63c84292c3409d61c598e9682
    - Name: Vulcan Forged (PYR)
    - Symbol: PYR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9308.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9308

---
+++ sdk/constants/token-map/1/definitions/1-0x430ef9263e76dae63c84292c3409d61c598e9682.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x430ef9263e76dae63c84292c3409d61c598e9682",
+  chain_id: 1,
+  contract_address: "0x430ef9263e76dae63c84292c3409d61c598e9682",
+  name: "Vulcan Forged (PYR)",
+  symbol: "PYR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9308.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9308",
+  type: "token",
+});
