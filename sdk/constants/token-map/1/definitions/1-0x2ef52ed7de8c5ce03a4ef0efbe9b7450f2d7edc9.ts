131f507892959209015ee00db1c6e07e336f3615
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:34 -0800

    feat(token-map): Update Token -- 1-0x2ef52ed7de8c5ce03a4ef0efbe9b7450f2d7edc9 -- Revain (REV) (0x2ef5...edc9)
    Token Details:
    - Id: 1-0x2ef52ed7de8c5ce03a4ef0efbe9b7450f2d7edc9
    - Chain Id: 1
    - Contract Address: 0x2ef52ed7de8c5ce03a4ef0efbe9b7450f2d7edc9
    - Name: Revain
    - Symbol: REV
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2135.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 2135

---
+++ sdk/constants/token-map/1/definitions/1-0x2ef52ed7de8c5ce03a4ef0efbe9b7450f2d7edc9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2ef52ed7de8c5ce03a4ef0efbe9b7450f2d7edc9",
+  chain_id: 1,
+  contract_address: "0x2ef52ed7de8c5ce03a4ef0efbe9b7450f2d7edc9",
+  name: "Revain",
+  symbol: "REV",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2135.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "2135",
+  type: "token",
+});
