303d567765949f65329bb886b67832dc7ffea3e4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:36 -0800

    feat(token-map): Update Token -- 1-0xfb130d93e49dca13264344966a611dc79a456bc5 -- DogeGF (DOGEGF) (0xfb13...6bc5)
    Token Details:
    - Id: 1-0xfb130d93e49dca13264344966a611dc79a456bc5
    - Chain Id: 1
    - Contract Address: 0xfb130d93e49dca13264344966a611dc79a456bc5
    - Name: DogeGF
    - Symbol: DOGEGF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12229.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 12229

---
+++ sdk/constants/token-map/1/definitions/1-0xfb130d93e49dca13264344966a611dc79a456bc5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfb130d93e49dca13264344966a611dc79a456bc5",
+  chain_id: 1,
+  contract_address: "0xfb130d93e49dca13264344966a611dc79a456bc5",
+  name: "DogeGF",
+  symbol: "DOGEGF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12229.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "12229",
+  type: "token",
+});
