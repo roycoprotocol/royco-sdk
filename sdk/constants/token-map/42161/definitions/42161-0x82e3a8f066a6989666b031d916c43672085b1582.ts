8334b81ec3160a00b6bcb8a1dd8ecb6192028d6b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:38 -0800

    feat(token-map): Update Token -- 42161-0x82e3a8f066a6989666b031d916c43672085b1582 -- yearn.finance (YFI) (0x82e3...1582)
    Token Details:
    - Id: 42161-0x82e3a8f066a6989666b031d916c43672085b1582
    - Chain Id: 42161
    - Contract Address: 0x82e3a8f066a6989666b031d916c43672085b1582
    - Name: yearn.finance
    - Symbol: YFI
    - Image: https://coin-images.coingecko.com/coins/images/11849/large/yearn.jpg?1696511720
    - Decimals: 18
    - Source: coingecko
    - Search Id: yearn-finance

---
+++ sdk/constants/token-map/42161/definitions/42161-0x82e3a8f066a6989666b031d916c43672085b1582.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x82e3a8f066a6989666b031d916c43672085b1582",
+  chain_id: 42161,
+  contract_address: "0x82e3a8f066a6989666b031d916c43672085b1582",
+  name: "yearn.finance",
+  symbol: "YFI",
+  image: "https://coin-images.coingecko.com/coins/images/11849/large/yearn.jpg?1696511720",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "yearn-finance",
+  type: "token",
+});
