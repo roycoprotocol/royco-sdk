6bd89ffa6a638147d507bff5eae9203f5a2a7b75
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:27:17 -0800

    feat(token-map): Update Token -- 1-0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d -- Celsius (CEL) (0xaaae...d42d)
    Token Details:
    - Id: 1-0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d
    - Chain Id: 1
    - Contract Address: 0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d
    - Name: Celsius
    - Symbol: CEL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2700.png
    - Decimals: 4
    - Source: coinmarketcap
    - Search Id: 2700

---
+++ sdk/constants/token-map/1/definitions/1-0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d",
+  chain_id: 1,
+  contract_address: "0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d",
+  name: "Celsius",
+  symbol: "CEL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2700.png",
+  decimals: 4,
+  source: "coinmarketcap",
+  search_id: "2700",
+  type: "token",
+});
