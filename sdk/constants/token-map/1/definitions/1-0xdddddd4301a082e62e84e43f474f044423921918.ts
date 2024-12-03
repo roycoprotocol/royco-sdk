351616e7effb19e8fec65745adc1770604a724c5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:45 -0800

    feat(token-map): Update Token -- 1-0xdddddd4301a082e62e84e43f474f044423921918 -- rhino.fi (DVF) (0xdddd...1918)
    Token Details:
    - Id: 1-0xdddddd4301a082e62e84e43f474f044423921918
    - Chain Id: 1
    - Contract Address: 0xdddddd4301a082e62e84e43f474f044423921918
    - Name: rhino.fi
    - Symbol: DVF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10759.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10759

---
+++ sdk/constants/token-map/1/definitions/1-0xdddddd4301a082e62e84e43f474f044423921918.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdddddd4301a082e62e84e43f474f044423921918",
+  chain_id: 1,
+  contract_address: "0xdddddd4301a082e62e84e43f474f044423921918",
+  name: "rhino.fi",
+  symbol: "DVF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10759.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10759",
+  type: "token",
+});
