f36d7b5642448af10e4c1b30bc0268804de97292
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:05 -0800

    feat(token-map): Update Token -- 1-0xd2946be786f35c3cc402c29b323647abda799071 -- VikkyToken (VIKKY) (0xd294...9071)
    Token Details:
    - Id: 1-0xd2946be786f35c3cc402c29b323647abda799071
    - Chain Id: 1
    - Contract Address: 0xd2946be786f35c3cc402c29b323647abda799071
    - Name: VikkyToken
    - Symbol: VIKKY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2965.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 2965

---
+++ sdk/constants/token-map/1/definitions/1-0xd2946be786f35c3cc402c29b323647abda799071.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd2946be786f35c3cc402c29b323647abda799071",
+  chain_id: 1,
+  contract_address: "0xd2946be786f35c3cc402c29b323647abda799071",
+  name: "VikkyToken",
+  symbol: "VIKKY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2965.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "2965",
+  type: "token",
+});
