5612753d60a5581b07bab0e1dd29ec1f487e62d6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:16 -0800

    feat(token-map): Update Token -- 1-0xb90b2a35c65dbc466b04240097ca756ad2005295 -- BOBO (BOBO) (0xb90b...5295)
    Token Details:
    - Id: 1-0xb90b2a35c65dbc466b04240097ca756ad2005295
    - Chain Id: 1
    - Contract Address: 0xb90b2a35c65dbc466b04240097ca756ad2005295
    - Name: BOBO
    - Symbol: BOBO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/25269.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 25269

---
+++ sdk/constants/token-map/1/definitions/1-0xb90b2a35c65dbc466b04240097ca756ad2005295.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb90b2a35c65dbc466b04240097ca756ad2005295",
+  chain_id: 1,
+  contract_address: "0xb90b2a35c65dbc466b04240097ca756ad2005295",
+  name: "BOBO",
+  symbol: "BOBO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/25269.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "25269",
+  type: "token",
+});
