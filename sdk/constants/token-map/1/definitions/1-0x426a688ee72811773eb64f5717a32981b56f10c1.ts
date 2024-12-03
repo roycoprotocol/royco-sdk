ca1196a7e1b280eabab2b7113e2a4d2a6b80cfb2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:58 -0800

    feat(token-map): Update Token -- 1-0x426a688ee72811773eb64f5717a32981b56f10c1 -- AMC (AMC) (0x426a...10c1)
    Token Details:
    - Id: 1-0x426a688ee72811773eb64f5717a32981b56f10c1
    - Chain Id: 1
    - Contract Address: 0x426a688ee72811773eb64f5717a32981b56f10c1
    - Name: AMC
    - Symbol: AMC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/31871.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 31871

---
+++ sdk/constants/token-map/1/definitions/1-0x426a688ee72811773eb64f5717a32981b56f10c1.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x426a688ee72811773eb64f5717a32981b56f10c1",
+  chain_id: 1,
+  contract_address: "0x426a688ee72811773eb64f5717a32981b56f10c1",
+  name: "AMC",
+  symbol: "AMC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/31871.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "31871",
+  type: "token",
+});
