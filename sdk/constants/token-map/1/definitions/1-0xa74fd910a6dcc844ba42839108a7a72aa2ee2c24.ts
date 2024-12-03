327da90bb24c1a211a17fa1ea6cd9e32346e7a0a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:34 -0800

    feat(token-map): Update Token -- 1-0xa74fd910a6dcc844ba42839108a7a72aa2ee2c24 -- NeyroAI (NAI) (0xa74f...2c24)
    Token Details:
    - Id: 1-0xa74fd910a6dcc844ba42839108a7a72aa2ee2c24
    - Chain Id: 1
    - Contract Address: 0xa74fd910a6dcc844ba42839108a7a72aa2ee2c24
    - Name: NeyroAI
    - Symbol: NAI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/32017.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 32017

---
+++ sdk/constants/token-map/1/definitions/1-0xa74fd910a6dcc844ba42839108a7a72aa2ee2c24.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa74fd910a6dcc844ba42839108a7a72aa2ee2c24",
+  chain_id: 1,
+  contract_address: "0xa74fd910a6dcc844ba42839108a7a72aa2ee2c24",
+  name: "NeyroAI",
+  symbol: "NAI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32017.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "32017",
+  type: "token",
+});
