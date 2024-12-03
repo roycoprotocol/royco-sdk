57b0ef245e819865d09795990adc29150b37a9af
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:10 -0800

    feat(token-map): Update Token -- 1-0x33abe795f9c1b6136608c36db211bd7590f5fdae -- Landwolf (firstlandwolfeth) (WOLF) (0x33ab...fdae)
    Token Details:
    - Id: 1-0x33abe795f9c1b6136608c36db211bd7590f5fdae
    - Chain Id: 1
    - Contract Address: 0x33abe795f9c1b6136608c36db211bd7590f5fdae
    - Name: Landwolf (firstlandwolfeth)
    - Symbol: WOLF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/30780.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 30780

---
+++ sdk/constants/token-map/1/definitions/1-0x33abe795f9c1b6136608c36db211bd7590f5fdae.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x33abe795f9c1b6136608c36db211bd7590f5fdae",
+  chain_id: 1,
+  contract_address: "0x33abe795f9c1b6136608c36db211bd7590f5fdae",
+  name: "Landwolf (firstlandwolfeth)",
+  symbol: "WOLF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/30780.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "30780",
+  type: "token",
+});
