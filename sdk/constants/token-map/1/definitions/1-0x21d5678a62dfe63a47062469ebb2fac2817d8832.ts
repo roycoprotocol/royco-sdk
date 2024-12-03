f83318cd3d9ae67dcf26540f2bbca80aa85b483e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:03:13 -0800

    feat(token-map): Update Token -- 1-0x21d5678a62dfe63a47062469ebb2fac2817d8832 -- YoloCash (YLC) (0x21d5...8832)
    Token Details:
    - Id: 1-0x21d5678a62dfe63a47062469ebb2fac2817d8832
    - Chain Id: 1
    - Contract Address: 0x21d5678a62dfe63a47062469ebb2fac2817d8832
    - Name: YoloCash
    - Symbol: YLC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3162.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 3162

---
+++ sdk/constants/token-map/1/definitions/1-0x21d5678a62dfe63a47062469ebb2fac2817d8832.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x21d5678a62dfe63a47062469ebb2fac2817d8832",
+  chain_id: 1,
+  contract_address: "0x21d5678a62dfe63a47062469ebb2fac2817d8832",
+  name: "YoloCash",
+  symbol: "YLC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3162.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3162",
+  type: "token",
+});
