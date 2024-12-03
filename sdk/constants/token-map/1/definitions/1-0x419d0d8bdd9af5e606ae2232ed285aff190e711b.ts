6885db26e952019d6b63a77d9cbd04fdcd05e003
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:08:47 -0800

    feat(token-map): Update Token -- 1-0x419d0d8bdd9af5e606ae2232ed285aff190e711b -- FUNToken (FUN) (0x419d...711b)
    Token Details:
    - Id: 1-0x419d0d8bdd9af5e606ae2232ed285aff190e711b
    - Chain Id: 1
    - Contract Address: 0x419d0d8bdd9af5e606ae2232ed285aff190e711b
    - Name: FUNToken
    - Symbol: FUN
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1757.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 1757

---
+++ sdk/constants/token-map/1/definitions/1-0x419d0d8bdd9af5e606ae2232ed285aff190e711b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x419d0d8bdd9af5e606ae2232ed285aff190e711b",
+  chain_id: 1,
+  contract_address: "0x419d0d8bdd9af5e606ae2232ed285aff190e711b",
+  name: "FUNToken",
+  symbol: "FUN",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1757.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "1757",
+  type: "token",
+});
