29d404ea28fba6eef75184bff6e1a05bf5c0815b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:36 -0800

    feat(token-map): Update Token -- 42161-0xff970a61a04b1ca14834a43f5de4533ebddb5cc8 -- Arbitrum Bridged USDC (Arbitrum) (USDC.E) (0xff97...5cc8)
    Token Details:
    - Id: 42161-0xff970a61a04b1ca14834a43f5de4533ebddb5cc8
    - Chain Id: 42161
    - Contract Address: 0xff970a61a04b1ca14834a43f5de4533ebddb5cc8
    - Name: Arbitrum Bridged USDC (Arbitrum)
    - Symbol: USDC.E
    - Image: https://coin-images.coingecko.com/coins/images/30691/large/usdc.png?1696529560
    - Decimals: 6
    - Source: coingecko
    - Search Id: usd-coin-ethereum-bridged

---
+++ sdk/constants/token-map/42161/definitions/42161-0xff970a61a04b1ca14834a43f5de4533ebddb5cc8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+  chain_id: 42161,
+  contract_address: "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
+  name: "Arbitrum Bridged USDC (Arbitrum)",
+  symbol: "USDC.E",
+  image: "https://coin-images.coingecko.com/coins/images/30691/large/usdc.png?1696529560",
+  decimals: 6,
+  source: "coingecko",
+  search_id: "usd-coin-ethereum-bridged",
+  type: "token",
+});
