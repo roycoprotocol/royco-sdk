8f202c0213fd348e2be2ad94eccf48dc365f048a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:26:32 -0800

    feat(token-map): Update Token -- 1-0xa6c0c097741d55ecd9a3a7def3a8253fd022ceb9 -- AVA (AVA) (0xa6c0...ceb9)
    Token Details:
    - Id: 1-0xa6c0c097741d55ecd9a3a7def3a8253fd022ceb9
    - Chain Id: 1
    - Contract Address: 0xa6c0c097741d55ecd9a3a7def3a8253fd022ceb9
    - Name: AVA
    - Symbol: AVA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2776.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2776

---
+++ sdk/constants/token-map/1/definitions/1-0xa6c0c097741d55ecd9a3a7def3a8253fd022ceb9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa6c0c097741d55ecd9a3a7def3a8253fd022ceb9",
+  chain_id: 1,
+  contract_address: "0xa6c0c097741d55ecd9a3a7def3a8253fd022ceb9",
+  name: "AVA",
+  symbol: "AVA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2776.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2776",
+  type: "token",
+});
