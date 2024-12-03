b90cb15d652adab5c2132908095a95c1758789db
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:18 -0800

    feat(token-map): Update Token -- 1-0xb97048628db6b661d4c2aa833e95dbe1a905b280 -- TenX (PAY) (0xb970...b280)
    Token Details:
    - Id: 1-0xb97048628db6b661d4c2aa833e95dbe1a905b280
    - Chain Id: 1
    - Contract Address: 0xb97048628db6b661d4c2aa833e95dbe1a905b280
    - Name: TenX
    - Symbol: PAY
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/1758.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 1758

---
+++ sdk/constants/token-map/1/definitions/1-0xb97048628db6b661d4c2aa833e95dbe1a905b280.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb97048628db6b661d4c2aa833e95dbe1a905b280",
+  chain_id: 1,
+  contract_address: "0xb97048628db6b661d4c2aa833e95dbe1a905b280",
+  name: "TenX",
+  symbol: "PAY",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1758.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1758",
+  type: "token",
+});
