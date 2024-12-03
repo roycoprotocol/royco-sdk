9c2d8890776f19f4cc21521690e5996b996b576a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:32 -0800

    feat(token-map): Update Token -- 1-0x89ab32156e46f46d02ade3fecbe5fc4243b9aaed -- pNetwork (PNT) (0x89ab...aaed)
    Token Details:
    - Id: 1-0x89ab32156e46f46d02ade3fecbe5fc4243b9aaed
    - Chain Id: 1
    - Contract Address: 0x89ab32156e46f46d02ade3fecbe5fc4243b9aaed
    - Name: pNetwork
    - Symbol: PNT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5794.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5794

---
+++ sdk/constants/token-map/1/definitions/1-0x89ab32156e46f46d02ade3fecbe5fc4243b9aaed.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x89ab32156e46f46d02ade3fecbe5fc4243b9aaed",
+  chain_id: 1,
+  contract_address: "0x89ab32156e46f46d02ade3fecbe5fc4243b9aaed",
+  name: "pNetwork",
+  symbol: "PNT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5794.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5794",
+  type: "token",
+});
