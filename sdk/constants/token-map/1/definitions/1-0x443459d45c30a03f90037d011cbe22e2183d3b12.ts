93f6e16fa7b411c9dac050b1bfe1b86cac909604
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:09:28 -0800

    feat(token-map): Update Token -- 1-0x443459d45c30a03f90037d011cbe22e2183d3b12 -- TypeAI (TYPE) (0x4434...3b12)
    Token Details:
    - Id: 1-0x443459d45c30a03f90037d011cbe22e2183d3b12
    - Chain Id: 1
    - Contract Address: 0x443459d45c30a03f90037d011cbe22e2183d3b12
    - Name: TypeAI
    - Symbol: TYPE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29106.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29106

---
+++ sdk/constants/token-map/1/definitions/1-0x443459d45c30a03f90037d011cbe22e2183d3b12.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x443459d45c30a03f90037d011cbe22e2183d3b12",
+  chain_id: 1,
+  contract_address: "0x443459d45c30a03f90037d011cbe22e2183d3b12",
+  name: "TypeAI",
+  symbol: "TYPE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29106.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29106",
+  type: "token",
+});
