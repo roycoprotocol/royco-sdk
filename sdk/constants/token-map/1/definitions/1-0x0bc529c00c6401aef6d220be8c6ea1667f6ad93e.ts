74780d7d2d2f2d1dfa14dac7a73d1b166cbfa4bf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:15 -0800

    feat(token-map): Update Token -- 1-0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e -- yearn.finance (YFI) (0x0bc5...d93e)
    Token Details:
    - Id: 1-0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e
    - Chain Id: 1
    - Contract Address: 0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e
    - Name: yearn.finance
    - Symbol: YFI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5864.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5864

---
+++ sdk/constants/token-map/1/definitions/1-0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e",
+  chain_id: 1,
+  contract_address: "0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e",
+  name: "yearn.finance",
+  symbol: "YFI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5864.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5864",
+  type: "token",
+});
