c8ce44a01a1f36d2e3c948524ac1598f871b7821
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:04:50 -0800

    feat(token-map): Update Token -- 1-0x2ab6bb8408ca3199b8fa6c92d5b455f820af03c4 -- TE-FOOD (TONE) (0x2ab6...03c4)
    Token Details:
    - Id: 1-0x2ab6bb8408ca3199b8fa6c92d5b455f820af03c4
    - Chain Id: 1
    - Contract Address: 0x2ab6bb8408ca3199b8fa6c92d5b455f820af03c4
    - Name: TE-FOOD
    - Symbol: TONE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2578.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2578

---
+++ sdk/constants/token-map/1/definitions/1-0x2ab6bb8408ca3199b8fa6c92d5b455f820af03c4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2ab6bb8408ca3199b8fa6c92d5b455f820af03c4",
+  chain_id: 1,
+  contract_address: "0x2ab6bb8408ca3199b8fa6c92d5b455f820af03c4",
+  name: "TE-FOOD",
+  symbol: "TONE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2578.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2578",
+  type: "token",
+});
