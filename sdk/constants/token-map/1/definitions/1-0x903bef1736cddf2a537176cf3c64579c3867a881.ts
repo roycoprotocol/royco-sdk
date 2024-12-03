0498b05ffc8dae22fe968a86574b1814b37e675a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:58 -0800

    feat(token-map): Update Token -- 1-0x903bef1736cddf2a537176cf3c64579c3867a881 -- Legacy ICHI (ICHI) (0x903b...a881)
    Token Details:
    - Id: 1-0x903bef1736cddf2a537176cf3c64579c3867a881
    - Chain Id: 1
    - Contract Address: 0x903bef1736cddf2a537176cf3c64579c3867a881
    - Name: Legacy ICHI
    - Symbol: ICHI
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/20631.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 20631

---
+++ sdk/constants/token-map/1/definitions/1-0x903bef1736cddf2a537176cf3c64579c3867a881.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x903bef1736cddf2a537176cf3c64579c3867a881",
+  chain_id: 1,
+  contract_address: "0x903bef1736cddf2a537176cf3c64579c3867a881",
+  name: "Legacy ICHI",
+  symbol: "ICHI",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/20631.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "20631",
+  type: "token",
+});
