1a3b7bdf63554dddd6454bc333e83d5e12d27a90
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:31 -0800

    feat(token-map): Update Token -- 1-0xfad45e47083e4607302aa43c65fb3106f1cd7607 -- Hoge Finance (HOGE) (0xfad4...7607)
    Token Details:
    - Id: 1-0xfad45e47083e4607302aa43c65fb3106f1cd7607
    - Chain Id: 1
    - Contract Address: 0xfad45e47083e4607302aa43c65fb3106f1cd7607
    - Name: Hoge Finance
    - Symbol: HOGE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8438.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 8438

---
+++ sdk/constants/token-map/1/definitions/1-0xfad45e47083e4607302aa43c65fb3106f1cd7607.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfad45e47083e4607302aa43c65fb3106f1cd7607",
+  chain_id: 1,
+  contract_address: "0xfad45e47083e4607302aa43c65fb3106f1cd7607",
+  name: "Hoge Finance",
+  symbol: "HOGE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8438.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "8438",
+  type: "token",
+});
