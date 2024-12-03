3410b0c2fb1f0085e41103c89a399db7ab03f558
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:39:03 -0800

    feat(token-map): Update Token -- 1-0xf8c3527cc04340b208c854e985240c02f7b7793f -- Frontier (FRONT) (0xf8c3...793f)
    Token Details:
    - Id: 1-0xf8c3527cc04340b208c854e985240c02f7b7793f
    - Chain Id: 1
    - Contract Address: 0xf8c3527cc04340b208c854e985240c02f7b7793f
    - Name: Frontier
    - Symbol: FRONT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5893.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5893

---
+++ sdk/constants/token-map/1/definitions/1-0xf8c3527cc04340b208c854e985240c02f7b7793f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf8c3527cc04340b208c854e985240c02f7b7793f",
+  chain_id: 1,
+  contract_address: "0xf8c3527cc04340b208c854e985240c02f7b7793f",
+  name: "Frontier",
+  symbol: "FRONT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5893.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5893",
+  type: "token",
+});
