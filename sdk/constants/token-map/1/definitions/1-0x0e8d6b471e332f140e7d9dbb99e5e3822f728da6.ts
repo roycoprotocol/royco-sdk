803eee0cb0037550a1a37c9974aab7053179f2fb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:59:48 -0800

    feat(token-map): Update Token -- 1-0x0e8d6b471e332f140e7d9dbb99e5e3822f728da6 -- Abyss (ABYSS) (0x0e8d...8da6)
    Token Details:
    - Id: 1-0x0e8d6b471e332f140e7d9dbb99e5e3822f728da6
    - Chain Id: 1
    - Contract Address: 0x0e8d6b471e332f140e7d9dbb99e5e3822f728da6
    - Name: Abyss
    - Symbol: ABYSS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2847.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2847

---
+++ sdk/constants/token-map/1/definitions/1-0x0e8d6b471e332f140e7d9dbb99e5e3822f728da6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x0e8d6b471e332f140e7d9dbb99e5e3822f728da6",
+  chain_id: 1,
+  contract_address: "0x0e8d6b471e332f140e7d9dbb99e5e3822f728da6",
+  name: "Abyss",
+  symbol: "ABYSS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2847.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2847",
+  type: "token",
+});
