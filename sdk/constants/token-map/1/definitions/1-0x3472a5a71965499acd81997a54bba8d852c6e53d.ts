d7177be4f694a6354bdbbeab965aa4f4c8582639
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:25 -0800

    feat(token-map): Update Token -- 1-0x3472a5a71965499acd81997a54bba8d852c6e53d -- Badger DAO (BADGER) (0x3472...e53d)
    Token Details:
    - Id: 1-0x3472a5a71965499acd81997a54bba8d852c6e53d
    - Chain Id: 1
    - Contract Address: 0x3472a5a71965499acd81997a54bba8d852c6e53d
    - Name: Badger DAO
    - Symbol: BADGER
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7859.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7859

---
+++ sdk/constants/token-map/1/definitions/1-0x3472a5a71965499acd81997a54bba8d852c6e53d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3472a5a71965499acd81997a54bba8d852c6e53d",
+  chain_id: 1,
+  contract_address: "0x3472a5a71965499acd81997a54bba8d852c6e53d",
+  name: "Badger DAO",
+  symbol: "BADGER",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7859.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7859",
+  type: "token",
+});
