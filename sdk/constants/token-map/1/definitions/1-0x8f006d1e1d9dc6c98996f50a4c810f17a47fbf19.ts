f4076242d6d8630bf90ff8def2d15bbf2f46f1b5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:42 -0800

    feat(token-map): Update Token -- 1-0x8f006d1e1d9dc6c98996f50a4c810f17a47fbf19 -- Pleasure Coin (NSFW) (0x8f00...bf19)
    Token Details:
    - Id: 1-0x8f006d1e1d9dc6c98996f50a4c810f17a47fbf19
    - Chain Id: 1
    - Contract Address: 0x8f006d1e1d9dc6c98996f50a4c810f17a47fbf19
    - Name: Pleasure Coin
    - Symbol: NSFW
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9840.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9840

---
+++ sdk/constants/token-map/1/definitions/1-0x8f006d1e1d9dc6c98996f50a4c810f17a47fbf19.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8f006d1e1d9dc6c98996f50a4c810f17a47fbf19",
+  chain_id: 1,
+  contract_address: "0x8f006d1e1d9dc6c98996f50a4c810f17a47fbf19",
+  name: "Pleasure Coin",
+  symbol: "NSFW",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9840.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9840",
+  type: "token",
+});
