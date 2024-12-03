e33243d03b13e72853375f2174fcd8726e8da2fd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:28:20 -0800

    feat(token-map): Update Token -- 1-0xb1f1f47061a7be15c69f378cb3f69423bd58f2f8 -- Flashstake (FLASH) (0xb1f1...f2f8)
    Token Details:
    - Id: 1-0xb1f1f47061a7be15c69f378cb3f69423bd58f2f8
    - Chain Id: 1
    - Contract Address: 0xb1f1f47061a7be15c69f378cb3f69423bd58f2f8
    - Name: Flashstake
    - Symbol: FLASH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8627.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8627

---
+++ sdk/constants/token-map/1/definitions/1-0xb1f1f47061a7be15c69f378cb3f69423bd58f2f8.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb1f1f47061a7be15c69f378cb3f69423bd58f2f8",
+  chain_id: 1,
+  contract_address: "0xb1f1f47061a7be15c69f378cb3f69423bd58f2f8",
+  name: "Flashstake",
+  symbol: "FLASH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8627.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8627",
+  type: "token",
+});
