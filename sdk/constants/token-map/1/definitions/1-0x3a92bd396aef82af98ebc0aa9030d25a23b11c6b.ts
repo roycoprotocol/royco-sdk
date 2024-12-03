7f66059b3de83df7b04c41ba43a6dfa72ceda660
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:07:36 -0800

    feat(token-map): Update Token -- 1-0x3a92bd396aef82af98ebc0aa9030d25a23b11c6b -- Tokenbox (TBX) (0x3a92...1c6b)
    Token Details:
    - Id: 1-0x3a92bd396aef82af98ebc0aa9030d25a23b11c6b
    - Chain Id: 1
    - Contract Address: 0x3a92bd396aef82af98ebc0aa9030d25a23b11c6b
    - Name: Tokenbox
    - Symbol: TBX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2452.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2452

---
+++ sdk/constants/token-map/1/definitions/1-0x3a92bd396aef82af98ebc0aa9030d25a23b11c6b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3a92bd396aef82af98ebc0aa9030d25a23b11c6b",
+  chain_id: 1,
+  contract_address: "0x3a92bd396aef82af98ebc0aa9030d25a23b11c6b",
+  name: "Tokenbox",
+  symbol: "TBX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2452.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2452",
+  type: "token",
+});
