5c56541c15a0e21fde9d1aa8ad41353b1f5c0cc3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:21 -0800

    feat(token-map): Update Token -- 1-0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0 -- Frax Share (FXS) (0x3432...64d0)
    Token Details:
    - Id: 1-0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0
    - Chain Id: 1
    - Contract Address: 0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0
    - Name: Frax Share
    - Symbol: FXS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6953.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6953

---
+++ sdk/constants/token-map/1/definitions/1-0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0",
+  chain_id: 1,
+  contract_address: "0x3432b6a60d23ca0dfca7761b7ab56459d9c964d0",
+  name: "Frax Share",
+  symbol: "FXS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6953.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6953",
+  type: "token",
+});
