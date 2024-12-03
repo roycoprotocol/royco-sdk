1a06b2d5ff7d3a70219af142b9b58cfd0224da78
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:22 -0800

    feat(token-map): Update Token -- 1-0x826e5ec70dbc5607ff9218011fbb97f9a8d97953 -- Travel Care (TRAVEL) (0x826e...7953)
    Token Details:
    - Id: 1-0x826e5ec70dbc5607ff9218011fbb97f9a8d97953
    - Chain Id: 1
    - Contract Address: 0x826e5ec70dbc5607ff9218011fbb97f9a8d97953
    - Name: Travel Care
    - Symbol: TRAVEL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/18597.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 18597

---
+++ sdk/constants/token-map/1/definitions/1-0x826e5ec70dbc5607ff9218011fbb97f9a8d97953.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x826e5ec70dbc5607ff9218011fbb97f9a8d97953",
+  chain_id: 1,
+  contract_address: "0x826e5ec70dbc5607ff9218011fbb97f9a8d97953",
+  name: "Travel Care",
+  symbol: "TRAVEL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/18597.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "18597",
+  type: "token",
+});
