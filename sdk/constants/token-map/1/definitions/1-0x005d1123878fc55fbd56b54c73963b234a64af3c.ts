fe9a1ebabd2968582ba79fe9256b60e82ed16267
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:55:46 -0800

    feat(token-map): Update Token -- 1-0x005d1123878fc55fbd56b54c73963b234a64af3c -- Kiba Inu (KIBA) (0x005d...af3c)
    Token Details:
    - Id: 1-0x005d1123878fc55fbd56b54c73963b234a64af3c
    - Chain Id: 1
    - Contract Address: 0x005d1123878fc55fbd56b54c73963b234a64af3c
    - Name: Kiba Inu
    - Symbol: KIBA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/13437.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 13437

---
+++ sdk/constants/token-map/1/definitions/1-0x005d1123878fc55fbd56b54c73963b234a64af3c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x005d1123878fc55fbd56b54c73963b234a64af3c",
+  chain_id: 1,
+  contract_address: "0x005d1123878fc55fbd56b54c73963b234a64af3c",
+  name: "Kiba Inu",
+  symbol: "KIBA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/13437.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "13437",
+  type: "token",
+});
