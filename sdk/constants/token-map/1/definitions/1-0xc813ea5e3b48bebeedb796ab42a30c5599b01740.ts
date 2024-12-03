3feaa501482fe5adc104e49aeed1d537889100b4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:31:52 -0800

    feat(token-map): Update Token -- 1-0xc813ea5e3b48bebeedb796ab42a30c5599b01740 -- Autonio (NIOX) (0xc813...1740)
    Token Details:
    - Id: 1-0xc813ea5e3b48bebeedb796ab42a30c5599b01740
    - Chain Id: 1
    - Contract Address: 0xc813ea5e3b48bebeedb796ab42a30c5599b01740
    - Name: Autonio
    - Symbol: NIOX
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2151.png
    - Decimals: 4
    - Source: coinmarketcap
    - Search Id: 2151

---
+++ sdk/constants/token-map/1/definitions/1-0xc813ea5e3b48bebeedb796ab42a30c5599b01740.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc813ea5e3b48bebeedb796ab42a30c5599b01740",
+  chain_id: 1,
+  contract_address: "0xc813ea5e3b48bebeedb796ab42a30c5599b01740",
+  name: "Autonio",
+  symbol: "NIOX",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2151.png",
+  decimals: 4,
+  source: "coinmarketcap",
+  search_id: "2151",
+  type: "token",
+});
