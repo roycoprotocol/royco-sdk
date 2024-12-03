eca140d2205463663c0f91404a3934cca7fc9f4d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:38:10 -0800

    feat(token-map): Update Token -- 1-0xf2b2f7b47715256ce4ea43363a867fdce9353e3a -- Bitgert (BRISE) (0xf2b2...3e3a)
    Token Details:
    - Id: 1-0xf2b2f7b47715256ce4ea43363a867fdce9353e3a
    - Chain Id: 1
    - Contract Address: 0xf2b2f7b47715256ce4ea43363a867fdce9353e3a
    - Name: Bitgert
    - Symbol: BRISE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/11079.png
    - Decimals: 9
    - Source: coinmarketcap
    - Search Id: 11079

---
+++ sdk/constants/token-map/1/definitions/1-0xf2b2f7b47715256ce4ea43363a867fdce9353e3a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf2b2f7b47715256ce4ea43363a867fdce9353e3a",
+  chain_id: 1,
+  contract_address: "0xf2b2f7b47715256ce4ea43363a867fdce9353e3a",
+  name: "Bitgert",
+  symbol: "BRISE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/11079.png",
+  decimals: 9,
+  source: "coinmarketcap",
+  search_id: "11079",
+  type: "token",
+});
