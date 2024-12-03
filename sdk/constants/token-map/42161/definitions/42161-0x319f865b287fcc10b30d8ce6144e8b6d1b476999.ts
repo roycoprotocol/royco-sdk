54fbe03e8b1e9c9b9ce854458931b142ca647617
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:41:02 -0800

    feat(token-map): Update Token -- 42161-0x319f865b287fcc10b30d8ce6144e8b6d1b476999 -- Cartesi (CTSI) (0x319f...6999)
    Token Details:
    - Id: 42161-0x319f865b287fcc10b30d8ce6144e8b6d1b476999
    - Chain Id: 42161
    - Contract Address: 0x319f865b287fcc10b30d8ce6144e8b6d1b476999
    - Name: Cartesi
    - Symbol: CTSI
    - Image: https://coin-images.coingecko.com/coins/images/11038/large/Cartesi_Logo.png?1696510982
    - Decimals: 18
    - Source: coingecko
    - Search Id: cartesi

---
+++ sdk/constants/token-map/42161/definitions/42161-0x319f865b287fcc10b30d8ce6144e8b6d1b476999.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x319f865b287fcc10b30d8ce6144e8b6d1b476999",
+  chain_id: 42161,
+  contract_address: "0x319f865b287fcc10b30d8ce6144e8b6d1b476999",
+  name: "Cartesi",
+  symbol: "CTSI",
+  image: "https://coin-images.coingecko.com/coins/images/11038/large/Cartesi_Logo.png?1696510982",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "cartesi",
+  type: "token",
+});
