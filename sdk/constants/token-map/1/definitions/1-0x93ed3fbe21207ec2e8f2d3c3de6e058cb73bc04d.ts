1c22a179a11d4a7bb6ff04dd40fdc0fdd5f44e60
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:23:17 -0800

    feat(token-map): Update Token -- 1-0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d -- Kleros (PNK) (0x93ed...c04d)
    Token Details:
    - Id: 1-0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d
    - Chain Id: 1
    - Contract Address: 0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d
    - Name: Kleros
    - Symbol: PNK
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3581.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3581

---
+++ sdk/constants/token-map/1/definitions/1-0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d",
+  chain_id: 1,
+  contract_address: "0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d",
+  name: "Kleros",
+  symbol: "PNK",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3581.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3581",
+  type: "token",
+});
