f21fec4d6d1c4dc9173a6b65f2522fa796354650
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:29 -0800

    feat(token-map): Update Token -- 42161-0xf97f4df75117a78c1a5a0dbb814af92458539fb4 -- Chainlink (LINK) (0xf97f...9fb4)
    Token Details:
    - Id: 42161-0xf97f4df75117a78c1a5a0dbb814af92458539fb4
    - Chain Id: 42161
    - Contract Address: 0xf97f4df75117a78c1a5a0dbb814af92458539fb4
    - Name: Chainlink
    - Symbol: LINK
    - Image: https://coin-images.coingecko.com/coins/images/877/large/chainlink-new-logo.png?1696502009
    - Decimals: 18
    - Source: coingecko
    - Search Id: chainlink

---
+++ sdk/constants/token-map/42161/definitions/42161-0xf97f4df75117a78c1a5a0dbb814af92458539fb4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
+  chain_id: 42161,
+  contract_address: "0xf97f4df75117a78c1a5a0dbb814af92458539fb4",
+  name: "Chainlink",
+  symbol: "LINK",
+  image: "https://coin-images.coingecko.com/coins/images/877/large/chainlink-new-logo.png?1696502009",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "chainlink",
+  type: "token",
+});
