953fa9bf8ce3ade570065d865b500f3dbbfa5fe6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:06:23 -0800

    feat(token-map): Update Token -- 1-0x3449fc1cd036255ba1eb19d65ff4ba2b8903a69a -- Basis Cash (BAC) (0x3449...a69a)
    Token Details:
    - Id: 1-0x3449fc1cd036255ba1eb19d65ff4ba2b8903a69a
    - Chain Id: 1
    - Contract Address: 0x3449fc1cd036255ba1eb19d65ff4ba2b8903a69a
    - Name: Basis Cash
    - Symbol: BAC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7813.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7813

---
+++ sdk/constants/token-map/1/definitions/1-0x3449fc1cd036255ba1eb19d65ff4ba2b8903a69a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3449fc1cd036255ba1eb19d65ff4ba2b8903a69a",
+  chain_id: 1,
+  contract_address: "0x3449fc1cd036255ba1eb19d65ff4ba2b8903a69a",
+  name: "Basis Cash",
+  symbol: "BAC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7813.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7813",
+  type: "token",
+});
