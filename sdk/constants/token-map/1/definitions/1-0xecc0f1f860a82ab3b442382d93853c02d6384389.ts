b7b7331355f2645132bc7ca718bd8b1956c7b51d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:36:58 -0800

    feat(token-map): Update Token -- 1-0xecc0f1f860a82ab3b442382d93853c02d6384389 -- Axis DeFi (AXIS) (0xecc0...4389)
    Token Details:
    - Id: 1-0xecc0f1f860a82ab3b442382d93853c02d6384389
    - Chain Id: 1
    - Contract Address: 0xecc0f1f860a82ab3b442382d93853c02d6384389
    - Name: Axis DeFi
    - Symbol: AXIS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6670.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6670

---
+++ sdk/constants/token-map/1/definitions/1-0xecc0f1f860a82ab3b442382d93853c02d6384389.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xecc0f1f860a82ab3b442382d93853c02d6384389",
+  chain_id: 1,
+  contract_address: "0xecc0f1f860a82ab3b442382d93853c02d6384389",
+  name: "Axis DeFi",
+  symbol: "AXIS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6670.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6670",
+  type: "token",
+});
