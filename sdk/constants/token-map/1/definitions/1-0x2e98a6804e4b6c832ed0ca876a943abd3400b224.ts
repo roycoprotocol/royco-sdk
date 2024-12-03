6b2e0a2371628a9c7dd3eb578b690753c41a0d7c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:29 -0800

    feat(token-map): Update Token -- 1-0x2e98a6804e4b6c832ed0ca876a943abd3400b224 -- Bela (BELA) (0x2e98...b224)
    Token Details:
    - Id: 1-0x2e98a6804e4b6c832ed0ca876a943abd3400b224
    - Chain Id: 1
    - Contract Address: 0x2e98a6804e4b6c832ed0ca876a943abd3400b224
    - Name: Bela
    - Symbol: BELA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/217.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 217

---
+++ sdk/constants/token-map/1/definitions/1-0x2e98a6804e4b6c832ed0ca876a943abd3400b224.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2e98a6804e4b6c832ed0ca876a943abd3400b224",
+  chain_id: 1,
+  contract_address: "0x2e98a6804e4b6c832ed0ca876a943abd3400b224",
+  name: "Bela",
+  symbol: "BELA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/217.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "217",
+  type: "token",
+});
