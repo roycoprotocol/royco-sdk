c7b7aa838b4b717d7c06bfcfb5ed718c6f20d523
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:21:57 -0800

    feat(token-map): Update Token -- 1-0x8b1f49491477e0fb46a29fef53f1ea320d13c349 -- MicroMoney (AMM) (0x8b1f...c349)
    Token Details:
    - Id: 1-0x8b1f49491477e0fb46a29fef53f1ea320d13c349
    - Chain Id: 1
    - Contract Address: 0x8b1f49491477e0fb46a29fef53f1ea320d13c349
    - Name: MicroMoney
    - Symbol: AMM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2286.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 2286

---
+++ sdk/constants/token-map/1/definitions/1-0x8b1f49491477e0fb46a29fef53f1ea320d13c349.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8b1f49491477e0fb46a29fef53f1ea320d13c349",
+  chain_id: 1,
+  contract_address: "0x8b1f49491477e0fb46a29fef53f1ea320d13c349",
+  name: "MicroMoney",
+  symbol: "AMM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2286.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "2286",
+  type: "token",
+});
