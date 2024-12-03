c2c665ed01ddfe2654b05f9c67819fa30a466454
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:15:11 -0800

    feat(token-map): Update Token -- 1-0x66761fa41377003622aee3c7675fc7b5c1c2fac5 -- Clearpool (CPOOL) (0x6676...fac5)
    Token Details:
    - Id: 1-0x66761fa41377003622aee3c7675fc7b5c1c2fac5
    - Chain Id: 1
    - Contract Address: 0x66761fa41377003622aee3c7675fc7b5c1c2fac5
    - Name: Clearpool
    - Symbol: CPOOL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/12573.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 12573

---
+++ sdk/constants/token-map/1/definitions/1-0x66761fa41377003622aee3c7675fc7b5c1c2fac5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x66761fa41377003622aee3c7675fc7b5c1c2fac5",
+  chain_id: 1,
+  contract_address: "0x66761fa41377003622aee3c7675fc7b5c1c2fac5",
+  name: "Clearpool",
+  symbol: "CPOOL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/12573.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "12573",
+  type: "token",
+});
