96a3da651971d20915626145ce1c20b1ee20ce43
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:16:10 -0800

    feat(token-map): Update Token -- 1-0x6a4c76874e686a7d080d173987a35a9c48905583 -- Luxurious Pro Network Token (LPNT) (0x6a4c...5583)
    Token Details:
    - Id: 1-0x6a4c76874e686a7d080d173987a35a9c48905583
    - Chain Id: 1
    - Contract Address: 0x6a4c76874e686a7d080d173987a35a9c48905583
    - Name: Luxurious Pro Network Token
    - Symbol: LPNT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8501.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8501

---
+++ sdk/constants/token-map/1/definitions/1-0x6a4c76874e686a7d080d173987a35a9c48905583.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6a4c76874e686a7d080d173987a35a9c48905583",
+  chain_id: 1,
+  contract_address: "0x6a4c76874e686a7d080d173987a35a9c48905583",
+  name: "Luxurious Pro Network Token",
+  symbol: "LPNT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8501.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8501",
+  type: "token",
+});
