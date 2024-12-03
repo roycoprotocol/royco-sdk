5eb0e3cc9f3de52885313db325ab7d403938b967
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:13 -0800

    feat(token-map): Update Token -- 1-0x998ffe1e43facffb941dc337dd0468d52ba5b48a -- Rupiah Token (IDRT) (0x998f...b48a)
    Token Details:
    - Id: 1-0x998ffe1e43facffb941dc337dd0468d52ba5b48a
    - Chain Id: 1
    - Contract Address: 0x998ffe1e43facffb941dc337dd0468d52ba5b48a
    - Name: Rupiah Token
    - Symbol: IDRT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4702.png
    - Decimals: 2
    - Source: coinmarketcap
    - Search Id: 4702

---
+++ sdk/constants/token-map/1/definitions/1-0x998ffe1e43facffb941dc337dd0468d52ba5b48a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x998ffe1e43facffb941dc337dd0468d52ba5b48a",
+  chain_id: 1,
+  contract_address: "0x998ffe1e43facffb941dc337dd0468d52ba5b48a",
+  name: "Rupiah Token",
+  symbol: "IDRT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4702.png",
+  decimals: 2,
+  source: "coinmarketcap",
+  search_id: "4702",
+  type: "token",
+});
