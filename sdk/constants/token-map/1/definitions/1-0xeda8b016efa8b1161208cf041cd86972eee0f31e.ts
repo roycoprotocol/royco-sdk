d11b46ac3d9758fdd3690c6a891ae931ae0c7174
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:09 -0800

    feat(token-map): Update Token -- 1-0xeda8b016efa8b1161208cf041cd86972eee0f31e -- IHT Real Estate Protocol (IHT) (0xeda8...f31e)
    Token Details:
    - Id: 1-0xeda8b016efa8b1161208cf041cd86972eee0f31e
    - Chain Id: 1
    - Contract Address: 0xeda8b016efa8b1161208cf041cd86972eee0f31e
    - Name: IHT Real Estate Protocol
    - Symbol: IHT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2552.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2552

---
+++ sdk/constants/token-map/1/definitions/1-0xeda8b016efa8b1161208cf041cd86972eee0f31e.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xeda8b016efa8b1161208cf041cd86972eee0f31e",
+  chain_id: 1,
+  contract_address: "0xeda8b016efa8b1161208cf041cd86972eee0f31e",
+  name: "IHT Real Estate Protocol",
+  symbol: "IHT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2552.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2552",
+  type: "token",
+});
