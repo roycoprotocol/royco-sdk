b8503f85c692321fb95e351866faf43a97d1aefc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:53 -0800

    feat(token-map): Update Token -- 1-0x9d71ce49ab8a0e6d2a1e7bfb89374c9392fd6804 -- NvirWorld (NVIR) (0x9d71...6804)
    Token Details:
    - Id: 1-0x9d71ce49ab8a0e6d2a1e7bfb89374c9392fd6804
    - Chain Id: 1
    - Contract Address: 0x9d71ce49ab8a0e6d2a1e7bfb89374c9392fd6804
    - Name: NvirWorld
    - Symbol: NVIR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/15924.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 15924

---
+++ sdk/constants/token-map/1/definitions/1-0x9d71ce49ab8a0e6d2a1e7bfb89374c9392fd6804.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9d71ce49ab8a0e6d2a1e7bfb89374c9392fd6804",
+  chain_id: 1,
+  contract_address: "0x9d71ce49ab8a0e6d2a1e7bfb89374c9392fd6804",
+  name: "NvirWorld",
+  symbol: "NVIR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15924.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "15924",
+  type: "token",
+});
