1b4b498522621db1170b3386d3bd6d295fe3dcf9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:05:02 -0800

    feat(token-map): Update Token -- 1-0x2ba592f78db6436527729929aaf6c908497cb200 -- Cream Finance (CREAM) (0x2ba5...b200)
    Token Details:
    - Id: 1-0x2ba592f78db6436527729929aaf6c908497cb200
    - Chain Id: 1
    - Contract Address: 0x2ba592f78db6436527729929aaf6c908497cb200
    - Name: Cream Finance
    - Symbol: CREAM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6193.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6193

---
+++ sdk/constants/token-map/1/definitions/1-0x2ba592f78db6436527729929aaf6c908497cb200.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2ba592f78db6436527729929aaf6c908497cb200",
+  chain_id: 1,
+  contract_address: "0x2ba592f78db6436527729929aaf6c908497cb200",
+  name: "Cream Finance",
+  symbol: "CREAM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6193.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6193",
+  type: "token",
+});
