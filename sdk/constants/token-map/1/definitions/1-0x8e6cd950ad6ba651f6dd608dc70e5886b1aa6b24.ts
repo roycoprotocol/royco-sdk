ca95e85ccd5533ff91aceadba0d3dbc8b5cdfe59
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:30 -0800

    feat(token-map): Update Token -- 1-0x8e6cd950ad6ba651f6dd608dc70e5886b1aa6b24 -- Starlink (STARL) (0x8e6c...6b24)
    Token Details:
    - Id: 1-0x8e6cd950ad6ba651f6dd608dc70e5886b1aa6b24
    - Chain Id: 1
    - Contract Address: 0x8e6cd950ad6ba651f6dd608dc70e5886b1aa6b24
    - Name: Starlink
    - Symbol: STARL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/10821.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 10821

---
+++ sdk/constants/token-map/1/definitions/1-0x8e6cd950ad6ba651f6dd608dc70e5886b1aa6b24.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8e6cd950ad6ba651f6dd608dc70e5886b1aa6b24",
+  chain_id: 1,
+  contract_address: "0x8e6cd950ad6ba651f6dd608dc70e5886b1aa6b24",
+  name: "Starlink",
+  symbol: "STARL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/10821.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "10821",
+  type: "token",
+});
