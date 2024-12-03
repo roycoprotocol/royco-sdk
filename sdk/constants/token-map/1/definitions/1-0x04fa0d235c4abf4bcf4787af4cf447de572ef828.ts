a693e6a6c1c8b71f29721e46d1c82f40d0ab0ef6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 10:57:41 -0800

    feat(token-map): Update Token -- 1-0x04fa0d235c4abf4bcf4787af4cf447de572ef828 -- UMA (UMA) (0x04fa...f828)
    Token Details:
    - Id: 1-0x04fa0d235c4abf4bcf4787af4cf447de572ef828
    - Chain Id: 1
    - Contract Address: 0x04fa0d235c4abf4bcf4787af4cf447de572ef828
    - Name: UMA
    - Symbol: UMA
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5617.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5617

---
+++ sdk/constants/token-map/1/definitions/1-0x04fa0d235c4abf4bcf4787af4cf447de572ef828.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x04fa0d235c4abf4bcf4787af4cf447de572ef828",
+  chain_id: 1,
+  contract_address: "0x04fa0d235c4abf4bcf4787af4cf447de572ef828",
+  name: "UMA",
+  symbol: "UMA",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5617.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5617",
+  type: "token",
+});
