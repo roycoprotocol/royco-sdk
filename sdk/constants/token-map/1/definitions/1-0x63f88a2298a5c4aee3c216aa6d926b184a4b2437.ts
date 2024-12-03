3ce57a98e0e69e4240f9019a73856988a8cc3d01
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:14:49 -0800

    feat(token-map): Update Token -- 1-0x63f88a2298a5c4aee3c216aa6d926b184a4b2437 -- GameCredits (GAME) (0x63f8...2437)
    Token Details:
    - Id: 1-0x63f88a2298a5c4aee3c216aa6d926b184a4b2437
    - Chain Id: 1
    - Contract Address: 0x63f88a2298a5c4aee3c216aa6d926b184a4b2437
    - Name: GameCredits
    - Symbol: GAME
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/576.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 576

---
+++ sdk/constants/token-map/1/definitions/1-0x63f88a2298a5c4aee3c216aa6d926b184a4b2437.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x63f88a2298a5c4aee3c216aa6d926b184a4b2437",
+  chain_id: 1,
+  contract_address: "0x63f88a2298a5c4aee3c216aa6d926b184a4b2437",
+  name: "GameCredits",
+  symbol: "GAME",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/576.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "576",
+  type: "token",
+});
