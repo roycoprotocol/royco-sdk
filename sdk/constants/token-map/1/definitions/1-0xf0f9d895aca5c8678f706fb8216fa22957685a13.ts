dee80abd012fcefa6dbeb09bd6607e142d3afa31
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:37:52 -0800

    feat(token-map): Update Token -- 1-0xf0f9d895aca5c8678f706fb8216fa22957685a13 -- Cult DAO (CULT) (0xf0f9...5a13)
    Token Details:
    - Id: 1-0xf0f9d895aca5c8678f706fb8216fa22957685a13
    - Chain Id: 1
    - Contract Address: 0xf0f9d895aca5c8678f706fb8216fa22957685a13
    - Name: Cult DAO
    - Symbol: CULT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/17742.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 17742

---
+++ sdk/constants/token-map/1/definitions/1-0xf0f9d895aca5c8678f706fb8216fa22957685a13.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf0f9d895aca5c8678f706fb8216fa22957685a13",
+  chain_id: 1,
+  contract_address: "0xf0f9d895aca5c8678f706fb8216fa22957685a13",
+  name: "Cult DAO",
+  symbol: "CULT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/17742.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "17742",
+  type: "token",
+});
