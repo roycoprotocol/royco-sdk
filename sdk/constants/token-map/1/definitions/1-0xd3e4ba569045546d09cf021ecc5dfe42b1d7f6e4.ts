de41345a51798f06904a39bbe7bdd65793f46be4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:24 -0800

    feat(token-map): Update Token -- 1-0xd3e4ba569045546d09cf021ecc5dfe42b1d7f6e4 -- Morpheus.Network (MNW) (0xd3e4...f6e4)
    Token Details:
    - Id: 1-0xd3e4ba569045546d09cf021ecc5dfe42b1d7f6e4
    - Chain Id: 1
    - Contract Address: 0xd3e4ba569045546d09cf021ecc5dfe42b1d7f6e4
    - Name: Morpheus.Network
    - Symbol: MNW
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2763.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2763

---
+++ sdk/constants/token-map/1/definitions/1-0xd3e4ba569045546d09cf021ecc5dfe42b1d7f6e4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd3e4ba569045546d09cf021ecc5dfe42b1d7f6e4",
+  chain_id: 1,
+  contract_address: "0xd3e4ba569045546d09cf021ecc5dfe42b1d7f6e4",
+  name: "Morpheus.Network",
+  symbol: "MNW",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2763.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2763",
+  type: "token",
+});
