0947d41fa8654801e2954249114ee74241519ba2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:22 -0800

    feat(token-map): Update Token -- 1-0x9aab071b4129b083b01cb5a0cb513ce7eca26fa5 -- Hunt Town (HUNT) (0x9aab...6fa5)
    Token Details:
    - Id: 1-0x9aab071b4129b083b01cb5a0cb513ce7eca26fa5
    - Chain Id: 1
    - Contract Address: 0x9aab071b4129b083b01cb5a0cb513ce7eca26fa5
    - Name: Hunt Town
    - Symbol: HUNT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/5380.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 5380

---
+++ sdk/constants/token-map/1/definitions/1-0x9aab071b4129b083b01cb5a0cb513ce7eca26fa5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9aab071b4129b083b01cb5a0cb513ce7eca26fa5",
+  chain_id: 1,
+  contract_address: "0x9aab071b4129b083b01cb5a0cb513ce7eca26fa5",
+  name: "Hunt Town",
+  symbol: "HUNT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/5380.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "5380",
+  type: "token",
+});
