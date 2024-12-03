c0c0640b9bf896d28674058af0745aae905a9526
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:24:09 -0800

    feat(token-map): Update Token -- 1-0x996229d0c6a485c7f4b52e092eaa907cb2def5c6 -- BuckHathCoin (BHIG) (0x9962...f5c6)
    Token Details:
    - Id: 1-0x996229d0c6a485c7f4b52e092eaa907cb2def5c6
    - Chain Id: 1
    - Contract Address: 0x996229d0c6a485c7f4b52e092eaa907cb2def5c6
    - Name: BuckHathCoin
    - Symbol: BHIG
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3820.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3820

---
+++ sdk/constants/token-map/1/definitions/1-0x996229d0c6a485c7f4b52e092eaa907cb2def5c6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x996229d0c6a485c7f4b52e092eaa907cb2def5c6",
+  chain_id: 1,
+  contract_address: "0x996229d0c6a485c7f4b52e092eaa907cb2def5c6",
+  name: "BuckHathCoin",
+  symbol: "BHIG",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3820.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3820",
+  type: "token",
+});
