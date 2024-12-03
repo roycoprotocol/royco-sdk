dd4828766916e630c53f31ba8de54f52124422be
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:29:14 -0800

    feat(token-map): Update Token -- 1-0xb8baa0e4287890a5f79863ab62b7f175cecbd433 -- Swerve (SWRV) (0xb8ba...d433)
    Token Details:
    - Id: 1-0xb8baa0e4287890a5f79863ab62b7f175cecbd433
    - Chain Id: 1
    - Contract Address: 0xb8baa0e4287890a5f79863ab62b7f175cecbd433
    - Name: Swerve
    - Symbol: SWRV
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/6901.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 6901

---
+++ sdk/constants/token-map/1/definitions/1-0xb8baa0e4287890a5f79863ab62b7f175cecbd433.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb8baa0e4287890a5f79863ab62b7f175cecbd433",
+  chain_id: 1,
+  contract_address: "0xb8baa0e4287890a5f79863ab62b7f175cecbd433",
+  name: "Swerve",
+  symbol: "SWRV",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/6901.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "6901",
+  type: "token",
+});
