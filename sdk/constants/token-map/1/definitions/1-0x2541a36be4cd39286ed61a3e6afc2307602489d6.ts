0d1aa5b234fe74208f1dcc9c4f12302ab5a09694
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:03:43 -0800

    feat(token-map): Update Token -- 1-0x2541a36be4cd39286ed61a3e6afc2307602489d6 -- Dogecoin20 (DOGE20) (0x2541...89d6)
    Token Details:
    - Id: 1-0x2541a36be4cd39286ed61a3e6afc2307602489d6
    - Chain Id: 1
    - Contract Address: 0x2541a36be4cd39286ed61a3e6afc2307602489d6
    - Name: Dogecoin20
    - Symbol: DOGE20
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/30941.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 30941

---
+++ sdk/constants/token-map/1/definitions/1-0x2541a36be4cd39286ed61a3e6afc2307602489d6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2541a36be4cd39286ed61a3e6afc2307602489d6",
+  chain_id: 1,
+  contract_address: "0x2541a36be4cd39286ed61a3e6afc2307602489d6",
+  name: "Dogecoin20",
+  symbol: "DOGE20",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/30941.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "30941",
+  type: "token",
+});
