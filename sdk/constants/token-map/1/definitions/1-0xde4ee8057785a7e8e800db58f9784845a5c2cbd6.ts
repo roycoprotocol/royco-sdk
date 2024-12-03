b02bb0329a8ab85678255cc36d632753f4279d73
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:51 -0800

    feat(token-map): Update Token -- 1-0xde4ee8057785a7e8e800db58f9784845a5c2cbd6 -- DeXe (DEXE) (0xde4e...cbd6)
    Token Details:
    - Id: 1-0xde4ee8057785a7e8e800db58f9784845a5c2cbd6
    - Chain Id: 1
    - Contract Address: 0xde4ee8057785a7e8e800db58f9784845a5c2cbd6
    - Name: DeXe
    - Symbol: DEXE
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7326.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7326

---
+++ sdk/constants/token-map/1/definitions/1-0xde4ee8057785a7e8e800db58f9784845a5c2cbd6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xde4ee8057785a7e8e800db58f9784845a5c2cbd6",
+  chain_id: 1,
+  contract_address: "0xde4ee8057785a7e8e800db58f9784845a5c2cbd6",
+  name: "DeXe",
+  symbol: "DEXE",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7326.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7326",
+  type: "token",
+});
