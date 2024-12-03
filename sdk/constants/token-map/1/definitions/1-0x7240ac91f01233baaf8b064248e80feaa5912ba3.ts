7c4a4b2201a9fbe84a0f2c7c86e0578267f0db9f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:17:30 -0800

    feat(token-map): Update Token -- 1-0x7240ac91f01233baaf8b064248e80feaa5912ba3 -- OctoFi (OCTO) (0x7240...2ba3)
    Token Details:
    - Id: 1-0x7240ac91f01233baaf8b064248e80feaa5912ba3
    - Chain Id: 1
    - Contract Address: 0x7240ac91f01233baaf8b064248e80feaa5912ba3
    - Name: OctoFi
    - Symbol: OCTO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7202.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7202

---
+++ sdk/constants/token-map/1/definitions/1-0x7240ac91f01233baaf8b064248e80feaa5912ba3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7240ac91f01233baaf8b064248e80feaa5912ba3",
+  chain_id: 1,
+  contract_address: "0x7240ac91f01233baaf8b064248e80feaa5912ba3",
+  name: "OctoFi",
+  symbol: "OCTO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7202.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7202",
+  type: "token",
+});
