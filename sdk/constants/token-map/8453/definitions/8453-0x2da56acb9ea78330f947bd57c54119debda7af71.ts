6a85808cdfacfc2620389db99edac476f9c2baad
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:43 -0800

    feat(token-map): Update Token -- 8453-0x2da56acb9ea78330f947bd57c54119debda7af71 -- Mog Coin (MOG) (0x2da5...af71)
    Token Details:
    - Id: 8453-0x2da56acb9ea78330f947bd57c54119debda7af71
    - Chain Id: 8453
    - Contract Address: 0x2da56acb9ea78330f947bd57c54119debda7af71
    - Name: Mog Coin
    - Symbol: MOG
    - Image: https://coin-images.coingecko.com/coins/images/31059/large/MOG_LOGO_200x200.png?1696529893
    - Decimals: 18
    - Source: coingecko
    - Search Id: mog-coin

---
+++ sdk/constants/token-map/8453/definitions/8453-0x2da56acb9ea78330f947bd57c54119debda7af71.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0x2da56acb9ea78330f947bd57c54119debda7af71",
+  chain_id: 8453,
+  contract_address: "0x2da56acb9ea78330f947bd57c54119debda7af71",
+  name: "Mog Coin",
+  symbol: "MOG",
+  image: "https://coin-images.coingecko.com/coins/images/31059/large/MOG_LOGO_200x200.png?1696529893",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "mog-coin",
+  type: "token",
+});
