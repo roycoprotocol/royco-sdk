df93dfa968c4bfdddc351357e797270894d1f2ac
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:22 -0800

    feat(token-map): Update Token -- 1-0xd39a2cecba2657e125ba6a5c98ad2f6b6d7e83fd -- LUXO (LUXO) (0xd39a...83fd)
    Token Details:
    - Id: 1-0xd39a2cecba2657e125ba6a5c98ad2f6b6d7e83fd
    - Chain Id: 1
    - Contract Address: 0xd39a2cecba2657e125ba6a5c98ad2f6b6d7e83fd
    - Name: LUXO
    - Symbol: LUXO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8443.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8443

---
+++ sdk/constants/token-map/1/definitions/1-0xd39a2cecba2657e125ba6a5c98ad2f6b6d7e83fd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd39a2cecba2657e125ba6a5c98ad2f6b6d7e83fd",
+  chain_id: 1,
+  contract_address: "0xd39a2cecba2657e125ba6a5c98ad2f6b6d7e83fd",
+  name: "LUXO",
+  symbol: "LUXO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8443.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8443",
+  type: "token",
+});
