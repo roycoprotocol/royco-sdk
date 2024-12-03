ae833ad17f05e5f6793a59915f9ebb5d4b7cd91a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:42:11 -0800

    feat(token-map): Update Token -- 42161-0xcafcd85d8ca7ad1e1c6f82f651fa15e33aefd07b -- WOO (WOO) (0xcafc...d07b)
    Token Details:
    - Id: 42161-0xcafcd85d8ca7ad1e1c6f82f651fa15e33aefd07b
    - Chain Id: 42161
    - Contract Address: 0xcafcd85d8ca7ad1e1c6f82f651fa15e33aefd07b
    - Name: WOO
    - Symbol: WOO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7501.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7501

---
+++ sdk/constants/token-map/42161/definitions/42161-0xcafcd85d8ca7ad1e1c6f82f651fa15e33aefd07b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0xcafcd85d8ca7ad1e1c6f82f651fa15e33aefd07b",
+  chain_id: 42161,
+  contract_address: "0xcafcd85d8ca7ad1e1c6f82f651fa15e33aefd07b",
+  name: "WOO",
+  symbol: "WOO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7501.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7501",
+  type: "token",
+});
