3c8e11efd1d34f9c33077691cebe275c7a3dc4b5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:34:06 -0800

    feat(token-map): Update Token -- 1-0xd82df0abd3f51425eb15ef7580fda55727875f14 -- DAV Coin (DAV) (0xd82d...5f14)
    Token Details:
    - Id: 1-0xd82df0abd3f51425eb15ef7580fda55727875f14
    - Chain Id: 1
    - Contract Address: 0xd82df0abd3f51425eb15ef7580fda55727875f14
    - Name: DAV Coin
    - Symbol: DAV
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3220.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3220

---
+++ sdk/constants/token-map/1/definitions/1-0xd82df0abd3f51425eb15ef7580fda55727875f14.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd82df0abd3f51425eb15ef7580fda55727875f14",
+  chain_id: 1,
+  contract_address: "0xd82df0abd3f51425eb15ef7580fda55727875f14",
+  name: "DAV Coin",
+  symbol: "DAV",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3220.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3220",
+  type: "token",
+});
