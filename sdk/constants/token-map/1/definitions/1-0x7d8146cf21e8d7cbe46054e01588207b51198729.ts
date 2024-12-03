f0eaccfc461456185ceecc5f7718e1e82faf429e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:19:38 -0800

    feat(token-map): Update Token -- 1-0x7d8146cf21e8d7cbe46054e01588207b51198729 -- BOB (ETH) (BOB) (0x7d81...8729)
    Token Details:
    - Id: 1-0x7d8146cf21e8d7cbe46054e01588207b51198729
    - Chain Id: 1
    - Contract Address: 0x7d8146cf21e8d7cbe46054e01588207b51198729
    - Name: BOB (ETH)
    - Symbol: BOB
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/24594.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 24594

---
+++ sdk/constants/token-map/1/definitions/1-0x7d8146cf21e8d7cbe46054e01588207b51198729.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7d8146cf21e8d7cbe46054e01588207b51198729",
+  chain_id: 1,
+  contract_address: "0x7d8146cf21e8d7cbe46054e01588207b51198729",
+  name: "BOB (ETH)",
+  symbol: "BOB",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/24594.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "24594",
+  type: "token",
+});
