ce43d81103b46416126d3167698d1d2056358118
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:16 -0800

    feat(token-map): Update Token -- 1-0xd31695a1d35e489252ce57b129fd4b1b05e6acac -- TOKPIE (TKP) (0xd316...acac)
    Token Details:
    - Id: 1-0xd31695a1d35e489252ce57b129fd4b1b05e6acac
    - Chain Id: 1
    - Contract Address: 0xd31695a1d35e489252ce57b129fd4b1b05e6acac
    - Name: TOKPIE
    - Symbol: TKP
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4116.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4116

---
+++ sdk/constants/token-map/1/definitions/1-0xd31695a1d35e489252ce57b129fd4b1b05e6acac.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd31695a1d35e489252ce57b129fd4b1b05e6acac",
+  chain_id: 1,
+  contract_address: "0xd31695a1d35e489252ce57b129fd4b1b05e6acac",
+  name: "TOKPIE",
+  symbol: "TKP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4116.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4116",
+  type: "token",
+});
