70db3397310b7d7a1039bd8b0648ab70e7eab767
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:33:36 -0800

    feat(token-map): Update Token -- 1-0xd4c435f5b09f855c3317c8524cb1f586e42795fa -- Cindicator (CND) (0xd4c4...95fa)
    Token Details:
    - Id: 1-0xd4c435f5b09f855c3317c8524cb1f586e42795fa
    - Chain Id: 1
    - Contract Address: 0xd4c435f5b09f855c3317c8524cb1f586e42795fa
    - Name: Cindicator
    - Symbol: CND
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/2043.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 2043

---
+++ sdk/constants/token-map/1/definitions/1-0xd4c435f5b09f855c3317c8524cb1f586e42795fa.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xd4c435f5b09f855c3317c8524cb1f586e42795fa",
+  chain_id: 1,
+  contract_address: "0xd4c435f5b09f855c3317c8524cb1f586e42795fa",
+  name: "Cindicator",
+  symbol: "CND",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2043.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "2043",
+  type: "token",
+});
