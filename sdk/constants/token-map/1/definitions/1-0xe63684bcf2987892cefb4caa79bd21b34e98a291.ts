9d415b0343b8042f156158ce77b32cb72b0ec798
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:36:07 -0800

    feat(token-map): Update Token -- 1-0xe63684bcf2987892cefb4caa79bd21b34e98a291 -- Chicken (KFC) (0xe636...a291)
    Token Details:
    - Id: 1-0xe63684bcf2987892cefb4caa79bd21b34e98a291
    - Chain Id: 1
    - Contract Address: 0xe63684bcf2987892cefb4caa79bd21b34e98a291
    - Name: Chicken
    - Symbol: KFC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/7169.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 7169

---
+++ sdk/constants/token-map/1/definitions/1-0xe63684bcf2987892cefb4caa79bd21b34e98a291.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xe63684bcf2987892cefb4caa79bd21b34e98a291",
+  chain_id: 1,
+  contract_address: "0xe63684bcf2987892cefb4caa79bd21b34e98a291",
+  name: "Chicken",
+  symbol: "KFC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7169.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "7169",
+  type: "token",
+});
