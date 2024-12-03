5b01670dbffd7122e9f5c4ba42228f64de884436
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:11:31 -0800

    feat(token-map): Update Token -- 1-0x4fe83213d56308330ec302a8bd641f1d0113a4cc -- NuCypher (NU) (0x4fe8...a4cc)
    Token Details:
    - Id: 1-0x4fe83213d56308330ec302a8bd641f1d0113a4cc
    - Chain Id: 1
    - Contract Address: 0x4fe83213d56308330ec302a8bd641f1d0113a4cc
    - Name: NuCypher
    - Symbol: NU
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/4761.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 4761

---
+++ sdk/constants/token-map/1/definitions/1-0x4fe83213d56308330ec302a8bd641f1d0113a4cc.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4fe83213d56308330ec302a8bd641f1d0113a4cc",
+  chain_id: 1,
+  contract_address: "0x4fe83213d56308330ec302a8bd641f1d0113a4cc",
+  name: "NuCypher",
+  symbol: "NU",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/4761.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "4761",
+  type: "token",
+});
