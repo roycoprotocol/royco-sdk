b8f21042af5246604a58229a5142168ea4cd9783
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:22:00 -0800

    feat(token-map): Update Token -- 1-0x8b39b70e39aa811b69365398e0aace9bee238aeb -- Red Kite (PKF) (0x8b39...8aeb)
    Token Details:
    - Id: 1-0x8b39b70e39aa811b69365398e0aace9bee238aeb
    - Chain Id: 1
    - Contract Address: 0x8b39b70e39aa811b69365398e0aace9bee238aeb
    - Name: Red Kite
    - Symbol: PKF
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/8617.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 8617

---
+++ sdk/constants/token-map/1/definitions/1-0x8b39b70e39aa811b69365398e0aace9bee238aeb.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8b39b70e39aa811b69365398e0aace9bee238aeb",
+  chain_id: 1,
+  contract_address: "0x8b39b70e39aa811b69365398e0aace9bee238aeb",
+  name: "Red Kite",
+  symbol: "PKF",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/8617.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "8617",
+  type: "token",
+});
