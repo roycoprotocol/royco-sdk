18d556bcf5f04a177d2045ba32df461ab4f15d97
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:03:17 -0800

    feat(token-map): Update Token -- 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599 -- Wrapped Bitcoin (WBTC) (0x2260...c599)
    Token Details:
    - Id: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Chain Id: 1
    - Contract Address: 0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Name: Wrapped Bitcoin
    - Symbol: WBTC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 3717

---
+++ sdk/constants/token-map/1/definitions/1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  chain_id: 1,
+  contract_address: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  name: "Wrapped Bitcoin",
+  symbol: "WBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3717",
+  type: "token",
+});
