411b76264e4517b7b8df630432716dac0181d66d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:30:20 -0800

    feat(token-map): Update Token -- 1-0xbf8fb919a8bbf28e590852aef2d284494ebc0657 -- ABCC Token (AT) (0xbf8f...0657)
    Token Details:
    - Id: 1-0xbf8fb919a8bbf28e590852aef2d284494ebc0657
    - Chain Id: 1
    - Contract Address: 0xbf8fb919a8bbf28e590852aef2d284494ebc0657
    - Name: ABCC Token
    - Symbol: AT
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/3238.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 3238

---
+++ sdk/constants/token-map/1/definitions/1-0xbf8fb919a8bbf28e590852aef2d284494ebc0657.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbf8fb919a8bbf28e590852aef2d284494ebc0657",
+  chain_id: 1,
+  contract_address: "0xbf8fb919a8bbf28e590852aef2d284494ebc0657",
+  name: "ABCC Token",
+  symbol: "AT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3238.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "3238",
+  type: "token",
+});
