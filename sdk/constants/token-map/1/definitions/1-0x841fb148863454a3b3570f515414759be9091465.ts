47a304f537e27c6f9d9266f3794653eaef501f8b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-03 11:20:42 -0800

    feat(token-map): Update Token -- 1-0x841fb148863454a3b3570f515414759be9091465 -- Shih Tzu (SHIH) (0x841f...1465)
    Token Details:
    - Id: 1-0x841fb148863454a3b3570f515414759be9091465
    - Chain Id: 1
    - Contract Address: 0x841fb148863454a3b3570f515414759be9091465
    - Name: Shih Tzu
    - Symbol: SHIH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/9712.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 9712

---
+++ sdk/constants/token-map/1/definitions/1-0x841fb148863454a3b3570f515414759be9091465.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x841fb148863454a3b3570f515414759be9091465",
+  chain_id: 1,
+  contract_address: "0x841fb148863454a3b3570f515414759be9091465",
+  name: "Shih Tzu",
+  symbol: "SHIH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/9712.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "9712",
+  type: "token",
+});
