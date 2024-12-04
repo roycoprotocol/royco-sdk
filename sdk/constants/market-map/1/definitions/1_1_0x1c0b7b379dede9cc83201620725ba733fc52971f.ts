37be63dfaa295bd37da831be24048d3dc4d97fa5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-04 13:27:32 -0800

    feat(market-map): Update market -- 1_1_0x1c0b7b379dede9cc83201620725ba733fc52971f -- Vault (0x1c0b...971f)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0x1c0b7b379dede9cc83201620725ba733fc52971f (Wrapped Vault Address)
    - Request Timestamp: Wed, 04 Dec 2024 21:27:31 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0x1c0b7b379dede9cc83201620725ba733fc52971f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "royco/constants";
+
+export default defineMarket({
+  id: "1_1_0x1c0b7b379dede9cc83201620725ba733fc52971f",
+  name: "Supply usdc ",
+  description: "blablabla ",
+  is_verified: false,
+});
