dfb1472f94bf4f2b8555ee87b3d694566779da4e
githubotoro <yupuday.git@gmail.com>
2024-12-04 15:53:39 -0800

    feat(sdk): automated commits

--- sdk/constants/market-map/1/definitions/1_1_0x1c0b7b379dede9cc83201620725ba733fc52971f.ts
+++ sdk/constants/market-map/1/definitions/1_1_0x1c0b7b379dede9cc83201620725ba733fc52971f.ts
@@ -1,4 +1,4 @@
-import { defineMarket } from "royco/constants";
+import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
   id: "1_1_0x1c0b7b379dede9cc83201620725ba733fc52971f",
