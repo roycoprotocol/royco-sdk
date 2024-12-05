096dfb6fceaa55338e4e31d5ec92ccc49e7c4223
githubotoro <yupuday.git@gmail.com>
2024-12-05 12:14:24 -0800

    feat(sdk): automated commits

--- sdk/constants/market-map/11155111/definitions/11155111_0_0x51fdc106de3f98aad14186309f7aafa60705efb5e3d72cf79d27111b62a1f683.ts
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x51fdc106de3f98aad14186309f7aafa60705efb5e3d72cf79d27111b62a1f683.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: "11155111_0_0x51fdc106de3f98aad14186309f7aafa60705efb5e3d72cf79d27111b62a1f683",
   name: "Sepolia Forfeitable Recipe Market 2",
-  description: `Sepolia Forfeitable Recipe Market 2`,
+  description: `Test auto update`,
   is_verified: false,
 });
