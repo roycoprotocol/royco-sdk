6ddb99609c07f218d48d5d0c3ee6a61971114be4
githubotoro <yupuday.git@gmail.com>
2024-12-04 17:48:22 -0800

    feat(sdk): automated commits

---
+++ sdk/constants/token-map/1/definitions/1-0xf3e621395fc714B90dA337AA9108771597b4E696.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf3e621395fc714B90dA337AA9108771597b4E696",
+  chain_id: 1,
+  contract_address: "0xf3e621395fc714B90dA337AA9108771597b4E696",
+  name: "Reward EUL",
+  symbol: "rEUL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/14280.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "14280",
+  type: "token",
+});
