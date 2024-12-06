bb1f00823e21f39bccdf9cf309e283b9d9fff49f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-06 14:40:22 -0800

    feat(token-map): Update Token -- 1-0x4f8e1426a9d10bddc11d26042ad270f16ccb95f2 -- YieldFi Stable Token (SUSD) (0x4f8e...95f2)
    Token Details:
    - Id: 1-0x4f8e1426a9d10bddc11d26042ad270f16ccb95f2
    - Chain Id: 1
    - Contract Address: 0x4f8e1426a9d10bddc11d26042ad270f16ccb95f2
    - Name: YieldFi Stable Token
    - Symbol: SUSD
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/34306.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 34306

---
+++ sdk/constants/token-map/1/definitions/1-0x4f8e1426a9d10bddc11d26042ad270f16ccb95f2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4f8e1426a9d10bddc11d26042ad270f16ccb95f2",
+  chain_id: 1,
+  contract_address: "0x4f8e1426a9d10bddc11d26042ad270f16ccb95f2",
+  name: "YieldFi Stable Token",
+  symbol: "SUSD",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/34306.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "34306",
+  type: "token",
+});
