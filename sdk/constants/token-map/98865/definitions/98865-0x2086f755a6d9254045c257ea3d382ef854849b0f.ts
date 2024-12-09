5007603dfe7388714e2297b997c788d23b5006db
githubotoro <yupuday.git@gmail.com>
2024-12-09 14:23:58 -0800

    feat(token-map): plume tokens

---
+++ sdk/constants/token-map/98865/definitions/98865-0x2086f755a6d9254045c257ea3d382ef854849b0f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98865-0x2086f755a6d9254045c257ea3d382ef854849b0f",
+  chain_id: 98865,
+  contract_address: "0x2086f755a6d9254045c257ea3d382ef854849b0f",
+  name: "Tether USDt",
+  symbol: "USDT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "825",
+  type: "token",
+});
---
+++ sdk/constants/token-map/98865/definitions/98865-0x28e0f0eed8d6a6a96033feee8b2d7f32eb5ccc48.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98865-0x28e0f0eed8d6a6a96033feee8b2d7f32eb5ccc48",
+  chain_id: 98865,
+  contract_address: "0x28e0f0eed8d6a6a96033feee8b2d7f32eb5ccc48",
+  name: "USDC",
+  symbol: "USDC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "3408",
+  type: "token",
+});
