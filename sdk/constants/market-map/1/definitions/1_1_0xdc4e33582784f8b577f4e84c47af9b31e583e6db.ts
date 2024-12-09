10de15a40998fbdaba95991c6b29b468e5b77548
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-09 10:49:44 -0800

    feat(market-map): Update market -- 1_1_0xdc4e33582784f8b577f4e84c47af9b31e583e6db -- Vault (0xdc4e...e6db)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0xdc4e33582784f8b577f4e84c47af9b31e583e6db (Wrapped Vault Address)
    - Request Timestamp: Mon, 09 Dec 2024 18:49:44 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0xdc4e33582784f8b577f4e84c47af9b31e583e6db.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "1_1_0xdc4e33582784f8b577f4e84c47af9b31e583e6db",
+  name: "Lend USDC on Fluid",
+  description: "USDC is supplied to Fluid lending market. Users can withdraw USDC at any time.",
+  is_verified: false,
+});
