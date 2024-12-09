3699a429abceb9b2d6ad28fd06ba3c919f1e92e6
githubotoro <yupuday.git@gmail.com>
2024-12-09 15:43:23 -0800

    feat(sdk): automated commits

---
+++ sdk/constants/token-map/21000000/definitions/21000000-0xdf0b24095e15044538866576754f3c964e902ee6.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "21000000-0xdf0b24095e15044538866576754f3c964e902ee6",
+  chain_id: 21000000,
+  contract_address: "0xdf0b24095e15044538866576754f3c964e902ee6",
+  name: "Bridged USDC (Corn)",
+  symbol: "USDC.E",
+  image:
+    "https://coin-images.coingecko.com/coins/images/30691/large/usdc.png?1696529560",
+  decimals: 6,
+  source: "coingecko",
+  search_id: "usd-coin-ethereum-bridged",
+  type: "token",
+});
