d310c0ae6a68be7f9c00c8050874347342c2e9d1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-10 14:33:54 -0800

    feat(market-map): Update market -- 11155111_1_0xbe2a22476b6e3658fddd04c20928f52929aa4019 -- Vault (0xbe2a...4019)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 1 (Vault)
    - Market ID: 0xbe2a22476b6e3658fddd04c20928f52929aa4019 (Wrapped Vault Address)
    - Request Timestamp: Tue, 10 Dec 2024 22:33:54 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_1_0xbe2a22476b6e3658fddd04c20928f52929aa4019.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "11155111_1_0xbe2a22476b6e3658fddd04c20928f52929aa4019",
+  name: "Sepolia Test Vault USDC Market",
+  description: "Sepolia Test Vault USDC Market",
+  is_verified: false,
+});
