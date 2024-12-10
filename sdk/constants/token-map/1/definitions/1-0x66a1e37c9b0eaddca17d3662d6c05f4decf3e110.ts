26e6925ace84143d58dca825adeea4c53879faea
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-10 09:59:12 -0800

    feat(token-map): Update Token -- 1-0x66a1e37c9b0eaddca17d3662d6c05f4decf3e110 -- Resolv USR (USR) (0x66a1...e110)
    Token Details:
    - Id: 1-0x66a1e37c9b0eaddca17d3662d6c05f4decf3e110
    - Chain Id: 1
    - Contract Address: 0x66a1e37c9b0eaddca17d3662d6c05f4decf3e110
    - Name: Resolv USR
    - Symbol: USR
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/32873.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 32873

---
+++ sdk/constants/token-map/1/definitions/1-0x66a1e37c9b0eaddca17d3662d6c05f4decf3e110.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x66a1e37c9b0eaddca17d3662d6c05f4decf3e110",
+  chain_id: 1,
+  contract_address: "0x66a1e37c9b0eaddca17d3662d6c05f4decf3e110",
+  name: "Resolv USR",
+  symbol: "USR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32873.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "32873",
+  type: "token",
+});
