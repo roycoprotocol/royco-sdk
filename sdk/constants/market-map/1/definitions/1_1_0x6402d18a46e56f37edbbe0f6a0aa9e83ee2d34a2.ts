fcaa07043e9fad77f90219608d841fb16e6423fb
bharatvish <bharat@waymont.co>
2024-12-11 09:40:40 -0800

    Update 1_1_0x6402d18a46e56f37edbbe0f6a0aa9e83ee2d34a2.ts

--- sdk/constants/market-map/1/definitions/1_1_0x6402d18a46e56f37edbbe0f6a0aa9e83ee2d34a2.ts
+++ sdk/constants/market-map/1/definitions/1_1_0x6402d18a46e56f37edbbe0f6a0aa9e83ee2d34a2.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: "1_1_0x6402d18a46e56f37edbbe0f6a0aa9e83ee2d34a2",
   name: "Lend WBTC on Gearbox",
   description: "When WBTC is supplied to the market, it is deposited into the Gearbox WBTC Mainnet Pool to earn yield. Users may withdraw at any time. As an additional incentive, depositors receive 2,000 Lombard LUX points per day for depositing.",
-  is_verified: false,
+  is_verified: true,
 });
