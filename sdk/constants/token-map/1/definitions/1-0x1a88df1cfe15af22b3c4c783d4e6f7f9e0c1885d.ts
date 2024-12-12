13ed0676c7882fc92d9fb867469bed4e1fe400fb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-12 15:17:04 -0800

    feat(token-map): Update Token -- 1-0x1a88df1cfe15af22b3c4c783d4e6f7f9e0c1885d -- Aave stkGHO (STKGHO) (0x1a88...885d)
    Token Details:
    - Id: 1-0x1a88df1cfe15af22b3c4c783d4e6f7f9e0c1885d
    - Chain Id: 1
    - Contract Address: 0x1a88df1cfe15af22b3c4c783d4e6f7f9e0c1885d
    - Name: Aave stkGHO
    - Symbol: STKGHO
    - Image: https://coin-images.coingecko.com/coins/images/34849/large/stkgho.png?1706260099
    - Decimals: 18
    - Source: coingecko
    - Search Id: aave-stkgho

---
+++ sdk/constants/token-map/1/definitions/1-0x1a88df1cfe15af22b3c4c783d4e6f7f9e0c1885d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1a88df1cfe15af22b3c4c783d4e6f7f9e0c1885d",
+  chain_id: 1,
+  contract_address: "0x1a88df1cfe15af22b3c4c783d4e6f7f9e0c1885d",
+  name: "Aave stkGHO",
+  symbol: "STKGHO",
+  image: "https://coin-images.coingecko.com/coins/images/34849/large/stkgho.png?1706260099",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "aave-stkgho",
+  type: "token",
+});
