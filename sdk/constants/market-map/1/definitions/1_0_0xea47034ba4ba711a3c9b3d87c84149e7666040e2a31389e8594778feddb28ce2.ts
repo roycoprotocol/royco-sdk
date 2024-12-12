d7b4d2a964a6697aa7f07dc775d6ecc43acdfd4d
GitHub Action <action@github.com>
2024-12-12 19:50:03 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/1/definitions/1_0_0xea47034ba4ba711a3c9b3d87c84149e7666040e2a31389e8594778feddb28ce2.ts
+++ sdk/constants/market-map/1/definitions/1_0_0xea47034ba4ba711a3c9b3d87c84149e7666040e2a31389e8594778feddb28ce2.ts
@@ -3,6 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: "1_0_0xea47034ba4ba711a3c9b3d87c84149e7666040e2a31389e8594778feddb28ce2",
   name: "Deposit hETH into Spectra",
-  description: "When hETH is provided it gets deposited into the Spectra LP pool for hETH.",
+  description:
+    "When hETH is provided it gets deposited into the Spectra LP pool for hETH.",
   is_verified: false,
 });
