8f0dcfec5cc30f7b700dc27879999ab29f1dbe03
githubotoro <yupuday.git@gmail.com>
2024-12-12 18:48:22 -0800

    feat(token-map): royco points are going live -- you heard it first here :)

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0xa43531c348db1bdde82d963b387d21cd344bf698.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0xa43531c348db1bdde82d963b387d21cd344bf698",
+  chain_id: 11155111,
+  contract_address: "0xa43531c348db1bdde82d963b387d21cd344bf698",
+  name: "TEST Royco Point",
+  symbol: "TST",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
