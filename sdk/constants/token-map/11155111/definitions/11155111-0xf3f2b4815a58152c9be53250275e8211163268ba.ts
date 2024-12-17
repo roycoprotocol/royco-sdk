a13915c793b6bf0242ec61f99c832ae8c396a4b0
GitHub Action <action@github.com>
2024-12-17 10:53:10 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/11155111/definitions/11155111-0xf3f2b4815a58152c9be53250275e8211163268ba.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0xf3f2b4815a58152c9be53250275e8211163268ba.ts
@@ -1,14 +1,14 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-    id: "11155111-0xf3f2b4815a58152c9be53250275e8211163268ba",
-    chain_id: 11155111,
-    contract_address: "0xf3f2b4815a58152c9be53250275e8211163268ba",
-    name: "stgUSDT",
-    symbol: "stgUSDT",
-    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png",
-    decimals: 6,
-    source: "coinmarketcap",
-    search_id: "825",
-    type: "token",
+  id: "11155111-0xf3f2b4815a58152c9be53250275e8211163268ba",
+  chain_id: 11155111,
+  contract_address: "0xf3f2b4815a58152c9be53250275e8211163268ba",
+  name: "stgUSDT",
+  symbol: "stgUSDT",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "825",
+  type: "token",
 });
