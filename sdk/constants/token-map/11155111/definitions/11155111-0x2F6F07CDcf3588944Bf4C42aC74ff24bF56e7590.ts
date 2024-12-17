727fb51030ccc398191f1eda84a588bbf5692fec
Jack Corddry <64389215+corddry@users.noreply.github.com>
2024-12-16 16:35:43 -0800

    Create 11155111-0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590.ts
    Hacky addition of Stargate Mock USDC

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590",
+  chain_id: 11155111,
+  contract_address: "0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590",
+  name: "stgUSDC",
+  symbol: "stgUSDC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "3408",
+  type: "token",
+});
