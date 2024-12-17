7e59ceed4e22dba11231515886164ea216019105
githubotoro <yupuday.git@gmail.com>
2024-12-17 17:08:07 +0530

    feat(constants): casing fix for token-map

--- sdk/constants/token-map/11155111/definitions/11155111-0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590",
+  id: "11155111-0x2f6f07cdcf3588944bf4c42ac74ff24bf56e7590",
   chain_id: 11155111,
-  contract_address: "0x2F6F07CDcf3588944Bf4C42aC74ff24bF56e7590",
+  contract_address: "0x2f6f07cdcf3588944bf4c42ac74ff24bf56e7590",
   name: "stgUSDC",
   symbol: "stgUSDC",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
--- sdk/constants/token-map/11155111/definitions/11155111-0xD34398848d35b2bD2CA373c809dC4D8E0523B00f.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0xD34398848d35b2bD2CA373c809dC4D8E0523B00f.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0xD34398848d35b2bD2CA373c809dC4D8E0523B00f",
+  id: "11155111-0xd34398848d35b2bd2ca373c809dc4d8e0523b00f",
   chain_id: 11155111,
-  contract_address: "0xD34398848d35b2bD2CA373c809dC4D8E0523B00f",
+  contract_address: "0xd34398848d35b2bd2ca373c809dc4d8e0523b00f",
   name: "UNIV2-wETH/stgUSDC",
   symbol: "wETH/stgUSDC",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png",
