6a386258979419be80466ea7e55f28239b0acc88
GitHub Action <action@github.com>
2024-12-17 14:41:45 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/11155111/definitions/11155111_0_0x7b5bf8336ed469737813a9d3d2cfd77970d9c2a033ae0c64bb94d720860e9161.ts
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x7b5bf8336ed469737813a9d3d2cfd77970d9c2a033ae0c64bb94d720860e9161.ts
@@ -3,6 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: "11155111_0_0x7b5bf8336ed469737813a9d3d2cfd77970d9c2a033ae0c64bb94d720860e9161",
   name: "Test Market - LP USDC/HONEY on Kodiak",
-  description: "When USDC is supplied to this market, it is deposited into the CCDM Deposit Locker, bridged to CArtio, split 50/50 into USDC/HONEY on CArtio and then used to LP into the USDC/HONEY Kodiak pool.",
+  description:
+    "When USDC is supplied to this market, it is deposited into the CCDM Deposit Locker, bridged to CArtio, split 50/50 into USDC/HONEY on CArtio and then used to LP into the USDC/HONEY Kodiak pool.",
   is_verified: false,
 });
