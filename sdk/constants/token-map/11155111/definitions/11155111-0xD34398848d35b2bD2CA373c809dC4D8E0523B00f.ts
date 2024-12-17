900d22a260f95ef0b0ac4c13a882d587cdbb260f
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2024-12-17 16:18:38 +0530

    tokens

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0xD34398848d35b2bD2CA373c809dC4D8E0523B00f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+    id: "11155111-0xD34398848d35b2bD2CA373c809dC4D8E0523B00f",
+    chain_id: 11155111,
+    contract_address: "0xD34398848d35b2bD2CA373c809dC4D8E0523B00f",
+    name: "UNIV2-wETH/stgUSDC",
+    symbol: "wETH/stgUSDC",
+    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png",
+    decimals: 18,
+    source: "coinmarketcap",
+    search_id: "7083",
+    type: "token",
+});
