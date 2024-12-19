b6897ea332828807934f735d5acd76460dd8d876
githubotoro <yupuday.git@gmail.com>
2024-12-19 18:19:23 +0530

    feat(provider): prod

--- sdk/provider/RoycoProvider.tsx
+++ sdk/provider/RoycoProvider.tsx
@@ -10,14 +10,6 @@ import type { ReactNode } from "react";
 
 import { RoycoContext } from "./RoycoContext";
 import type { TypedRpcApiKeys } from "@/sdk/client";
-import {
-  cookieStorage,
-  createConfig,
-  createStorage,
-  WagmiProvider,
-} from "wagmi";
-
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 interface RoycoProviderProps {
   children: ReactNode;
@@ -27,50 +19,6 @@ interface RoycoProviderProps {
   rpcApiKeys: TypedRpcApiKeys | undefined;
   defaultOptions?: DefaultOptions;
 }
-
-import {
-  mainnet,
-  sepolia,
-  arbitrum,
-  arbitrumSepolia,
-  baseSepolia,
-  base,
-} from "wagmi/chains";
-
-// Create wagmiConfig
-const chains = [
-  mainnet,
-  arbitrum,
-  base,
-  sepolia,
-  arbitrumSepolia,
-  baseSepolia,
-] as const;
-
-const projectId = "1ce2b86020b67f63d71494567a750347";
-
-export const metadata = {
-  name: "Royco",
-  description:
-    "Royco is a marketplace for exclusive incentives from the best projects.",
-  // url: "http://localhost:3000", // origin must match your domain & subdomain
-  url: "https://royco.org", // origin must match your domain & subdomain
-  icons: ["/icon.png"],
-};
-
-export const wagmiConfig = getDefaultConfig({
-  appName: "Royco",
-  projectId,
-  chains,
-  ssr: true,
-  // storage: createStorage({
-  //   storage:
-  //     typeof window !== "undefined" ? window.localStorage : cookieStorage,
-  // }),
-  batch: {
-    multicall: true,
-  },
-});
 
 const RoycoProvider = ({
   children,
@@ -98,11 +46,7 @@ const RoycoProvider = ({
 
   return (
     <RoycoContext.Provider value={value}>
-      <WagmiProvider config={wagmiConfig}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </WagmiProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </RoycoContext.Provider>
   );
 };
