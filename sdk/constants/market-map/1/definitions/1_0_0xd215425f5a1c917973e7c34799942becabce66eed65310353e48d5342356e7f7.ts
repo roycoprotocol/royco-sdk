96606a0840cbcccf420b3345478128e09f65d438
JB <jbhav24@users.noreply.github.com>
2024-12-19 13:51:43 -0800

    Update 1_0_0xd215425f5a1c917973e7c34799942becabce66eed65310353e48d5342356e7f7.ts

--- sdk/constants/market-map/1/definitions/1_0_0xd215425f5a1c917973e7c34799942becabce66eed65310353e48d5342356e7f7.ts
+++ sdk/constants/market-map/1/definitions/1_0_0xd215425f5a1c917973e7c34799942becabce66eed65310353e48d5342356e7f7.ts
@@ -1,7 +1,7 @@
 import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
-  id: "1_0_0x021b96a61753074dcb33cc139e873058ecb3b9591fe9e8a7a59a58e9aa8cfebc",
+  id: "1_0_0xd215425f5a1c917973e7c34799942becabce66eed65310353e48d5342356e7f7",
   name: "Supply sUSDe into ZAI",
   description: `Market will return ZAI to the user upon withdrawal.`,
   is_verified: true,
