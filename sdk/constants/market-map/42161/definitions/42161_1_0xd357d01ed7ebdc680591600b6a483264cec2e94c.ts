99cbc827bf93e2ac2a45cb60b301ae645aaa26ad
JB <jbhav24@users.noreply.github.com>
2024-12-19 17:38:44 -0800

    Create 42161_1_0xd357d01ed7ebdc680591600b6a483264cec2e94c.ts

---
+++ sdk/constants/market-map/42161/definitions/42161_1_0xd357d01ed7ebdc680591600b6a483264cec2e94c.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "42161_1_0xd357d01ed7ebdc680591600b6a483264cec2e94c",
+  name: "Lend USDT on Dolomite",
+  description: ``,
+  is_verified: true,
+});
