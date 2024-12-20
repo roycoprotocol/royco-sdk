316f3f9a51dbcc078c81a9a3e17f5b631a03d4d8
JB <jbhav24@users.noreply.github.com>
2024-12-19 19:33:36 -0800

    Update 42161_1_0x13c798c93e9c6293dd3c40d1f5c9fdcd4f92aa14.ts

--- sdk/constants/market-map/42161/definitions/42161_1_0x13c798c93e9c6293dd3c40d1f5c9fdcd4f92aa14.ts
+++ sdk/constants/market-map/42161/definitions/42161_1_0x13c798c93e9c6293dd3c40d1f5c9fdcd4f92aa14.ts
@@ -1,7 +1,7 @@
 import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
-  id: "42161_1_0x13c798c93E9C6293dD3c40D1f5c9fdCD4F92AA14",
+  id: "42161_1_0x13c798c93e9c6293dd3c40d1f5c9fdcd4f92aa14",
   name: "Lend USDC on Dolomite",
   description: ``,
   is_verified: true,
