f0c21b3c5166ed4774c2b0f9d75f1d39efff38c7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-20 07:24:38 +0530

    feat(market-map): Update market -- 11155111_0_0xfe62791d480b81430159f3b1ba676a05d86f6e5476fdb424f637daa32b5cadb2 -- Recipe (0xfe62...adb2)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xfe62791d480b81430159f3b1ba676a05d86f6e5476fdb424f637daa32b5cadb2 (Market Hash)
    - Request Timestamp: Fri, 20 Dec 2024 01:54:37 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xfe62791d480b81430159f3b1ba676a05d86f6e5476fdb424f637daa32b5cadb2.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "11155111_0_0xfe62791d480b81430159f3b1ba676a05d86f6e5476fdb424f637daa32b5cadb2",
+  name: "Sepolia Recipe Market",
+  description: "Sepolia Recipe Market",
+  is_verified: false,
+});
