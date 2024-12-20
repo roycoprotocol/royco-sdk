6716b090a50525e55f2954e9a739e94a2a5052b0
JB <jbhav24@users.noreply.github.com>
2024-12-19 17:35:19 -0800

    Create 42161_1_ 0x917a7d96071a219d01609508ebd62124476ff5b1cbf65b53ec8be20c481b125f.ts

---
+++ sdk/constants/market-map/42161/definitions/42161_1_ 0x917a7d96071a219d01609508ebd62124476ff5b1cbf65b53ec8be20c481b125f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "42161_1_0x13c798c93E9C6293dD3c40D1f5c9fdCD4F92AA14",
+  name: "Lend USDC on Dolomite",
+  description: ``,
+  is_verified: true,
+});
