8d76769f350de2f694868f15e13e0e3d7ff8f4d3
JB <jbhav24@users.noreply.github.com>
2024-12-19 17:39:57 -0800

    Create 42161_1_ 0xa85873e88e2235346cfe4884bdf22c01dc1840c9.ts

---
+++ sdk/constants/market-map/42161/definitions/42161_1_ 0xa85873e88e2235346cfe4884bdf22c01dc1840c9.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "42161_1_0xa85873e88e2235346cfe4884bdf22c01dc1840c9",
+  name: "Supply WBTC into Dolomite",
+  description: ``,
+  is_verified: true,
+});
