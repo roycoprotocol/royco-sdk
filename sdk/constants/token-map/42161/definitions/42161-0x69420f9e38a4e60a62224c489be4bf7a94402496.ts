5bedc4e3b3acec4c37e266921f454f3f6fc6c255
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-23 00:56:09 +0530

    feat(token-map): Update Token -- 42161-0x69420f9e38a4e60a62224c489be4bf7a94402496 -- Defi.money (MONEY) (0x6942...2496)
    Token Details:
    - Id: 42161-0x69420f9e38a4e60a62224c489be4bf7a94402496
    - Chain Id: 42161
    - Contract Address: 0x69420f9e38a4e60a62224c489be4bf7a94402496
    - Name: Defi.money
    - Symbol: MONEY
    - Image: https://coin-images.coingecko.com/coins/images/39230/large/Token_2x-1.png?1725438128
    - Decimals: 18
    - Source: coingecko
    - Search Id: defi-money

---
+++ sdk/constants/token-map/42161/definitions/42161-0x69420f9e38a4e60a62224c489be4bf7a94402496.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x69420f9e38a4e60a62224c489be4bf7a94402496",
+  chain_id: 42161,
+  contract_address: "0x69420f9e38a4e60a62224c489be4bf7a94402496",
+  name: "Defi.money",
+  symbol: "MONEY",
+  image: "https://coin-images.coingecko.com/coins/images/39230/large/Token_2x-1.png?1725438128",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "defi-money",
+  type: "token",
+});
