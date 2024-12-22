454c3091e91d7682a6b5a29d5b928c92eb2c66ed
GitHub Action <action@github.com>
2024-12-22 19:35:23 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/42161/definitions/42161-0x69420f9e38a4e60a62224c489be4bf7a94402496.ts
+++ sdk/constants/token-map/42161/definitions/42161-0x69420f9e38a4e60a62224c489be4bf7a94402496.ts
@@ -6,7 +6,8 @@ export default defineToken({
   contract_address: "0x69420f9e38a4e60a62224c489be4bf7a94402496",
   name: "Defi.money",
   symbol: "MONEY",
-  image: "https://coin-images.coingecko.com/coins/images/39230/large/Token_2x-1.png?1725438128",
+  image:
+    "https://coin-images.coingecko.com/coins/images/39230/large/Token_2x-1.png?1725438128",
   decimals: 18,
   source: "coingecko",
   search_id: "defi-money",
