ad8988e9da57833354a73ee67778ee18909169ff
JB <jbhav24@users.noreply.github.com>
2024-12-22 23:39:54 -0800

    Update and rename 8453-0x5777d92f208679db4b9778590fa3cab3ac9e2168.ts to 8453-0x950847d1dd451b67a2fc1795c0c9a53cf88e63a2.ts

--- sdk/constants/token-map/8453/definitions/8453-0x950847d1dd451b67a2fc1795c0c9a53cf88e63a2.ts
+++ sdk/constants/token-map/8453/definitions/8453-0x950847d1dd451b67a2fc1795c0c9a53cf88e63a2.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "8453-0x5777d92f208679db4b9778590fa3cab3ac9e2168",
+  id: "8453-0x950847d1dd451b67a2fc1795c0c9a53cf88e63a2",
   chain_id: 8453,
-  contract_address: "0x5777d92f208679db4b9778590fa3cab3ac9e2168",
+  contract_address: "0x950847d1dd451b67a2fc1795c0c9a53cf88e63a2",
   name: "UNIV2-DAI/USDC",
   symbol: "DAI/USDC",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png",
