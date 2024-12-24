a1ca62e9ea78820add1494bc0d9ee57862790cdb
GitHub Action <action@github.com>
2024-12-24 04:40:11 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/11155111/definitions/11155111-0x29f2d40b0605204364af54ec677bd022da425d03.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0x29f2d40b0605204364af54ec677bd022da425d03.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0x29f2D40B0605204364af54EC677bD022dA425d03",
+  id: "11155111-0x29f2d40b0605204364af54ec677bd022da425d03",
   chain_id: 11155111,
-  contract_address: "0x29f2D40B0605204364af54EC677bD022dA425d03",
+  contract_address: "0x29f2d40b0605204364af54ec677bd022da425d03",
   name: "Wrapped Bitcoin",
   symbol: "wBTC",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png",
