44f47b9f0c6f95a442be9edb98409602bb96e3e7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-24 08:41:33 +0530

    feat(market-map): Update market -- 1_0_0x96b0b1f1b039c85b4825891b25f6acb602b19d156192509b2772491e1a24e650 -- Recipe (0x96b0...e650)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x96b0b1f1b039c85b4825891b25f6acb602b19d156192509b2772491e1a24e650 (Market Hash)
    - Request Timestamp: Tue, 24 Dec 2024 03:11:32 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x96b0b1f1b039c85b4825891b25f6acb602b19d156192509b2772491e1a24e650.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "1_0_0x96b0b1f1b039c85b4825891b25f6acb602b19d156192509b2772491e1a24e650",
+  name: "Buy yUSD on YieldFi",
+  description: "yUSD is YieldFi's yield bearing stablecoin, earning 15% - 25% APY in real yield + incentives",
+  is_verified: false,
+});
