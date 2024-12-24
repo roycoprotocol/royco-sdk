90836ad8c67d90c42423fe6c270327b75043c2d0
JB <jbhav24@users.noreply.github.com>
2024-12-23 20:37:13 -0800

    Create 11155111-0x29f2D40B0605204364af54EC677bD022dA425d03.ts

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0x29f2D40B0605204364af54EC677bD022dA425d03.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0x29f2D40B0605204364af54EC677bD022dA425d03",
+  chain_id: 11155111,
+  contract_address: "0x29f2D40B0605204364af54EC677bD022dA425d03",
+  name: "Wrapped Bitcoin",
+  symbol: "wBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "3717",
+  type: "token",
+});
