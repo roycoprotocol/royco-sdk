3e422fcab1b52ac7f70cd972b60983b243c9dd0b
Naimish Lukhi <59863145+naim30@users.noreply.github.com>
2024-12-27 15:43:01 -0500

    Update 11155111-0x552bac4a13ec7c261903433f1e12e9eff8dc4adc.ts

--- sdk/constants/token-map/11155111/definitions/11155111-0x552bac4a13ec7c261903433f1e12e9eff8dc4adc.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0x552bac4a13ec7c261903433f1e12e9eff8dc4adc.ts
@@ -1,14 +1,14 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0x6a7fde508d41e65f768665fc18b9ce554dc50507",
+  id: "11155111-0x552bac4a13ec7c261903433f1e12e9eff8dc4adc",
   chain_id: 11155111,
-  contract_address: "0x6a7fde508d41e65f768665fc18b9ce554dc50507",
+  contract_address: "0x552bac4a13ec7c261903433f1e12e9eff8dc4adc",
   name: "pumpBTC.bera",
   symbol: "pumpBTC.bera",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32272.png",
   decimals: 8,
-  source: "coinmarketcap",
+  source: "external",
   search_id: "none",
   type: "token",
 });
