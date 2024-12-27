dfe6d27c0e9f9863063ff39bd512972945275920
Naimish Lukhi <59863145+naim30@users.noreply.github.com>
2024-12-27 15:45:50 -0500

    Update and rename 11155111-0xE0240d05Ae9eF703E2b71F3f4Eb326ea1888DEa3.ts to 11155111-0xe0240d05ae9ef703e2b71f3f4eb326ea1888dea3.ts

--- sdk/constants/token-map/11155111/definitions/11155111-0xe0240d05ae9ef703e2b71f3f4eb326ea1888dea3.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0xe0240d05ae9ef703e2b71f3f4eb326ea1888dea3.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0xE0240d05Ae9eF703E2b71F3f4Eb326ea1888DEa3",
+  id: "11155111-0xe0240d05ae9ef703e2b71f3f4eb326ea1888dea3",
   chain_id: 11155111,
-  contract_address: "0xE0240d05Ae9eF703E2b71F3f4Eb326ea1888DEa3",
+  contract_address: "0xe0240d05ae9ef703e2b71f3f4eb326ea1888dea3",
   name: "uniBTC",
   symbol: "uniBTC",
   image: "https://etherscan.io/token/images/bedrockunibtc_32.png",
