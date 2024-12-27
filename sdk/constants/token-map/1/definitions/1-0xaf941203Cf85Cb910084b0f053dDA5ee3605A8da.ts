eb04ac760216259811090be387e781d5bc34b31c
SG <shaileshgupta@users.noreply.github.com>
2024-12-28 03:47:36 +0800

    added YieldFi points programD

---
+++ sdk/constants/token-map/1/definitions/1-0xaf941203Cf85Cb910084b0f053dDA5ee3605A8da.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xaf941203Cf85Cb910084b0f053dDA5ee3605A8da",
+  chain_id: 1,
+  contract_address: "0xaf941203Cf85Cb910084b0f053dDA5ee3605A8da",
+  name: "YieldCrumbs",
+  symbol: "YC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3764.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
