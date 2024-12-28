565251ad38d4494a3c2cef49c6f7869948c8f733
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-28 10:11:21 +0530

    feat(market-map): Update market -- 11155111_0_0xc74bcce181b08956cd3984bcb93f6eb86c3bb7322d219131b2585f7ba862be81 -- Recipe (0xc74b...be81)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xc74bcce181b08956cd3984bcb93f6eb86c3bb7322d219131b2585f7ba862be81 (Market Hash)
    - Request Timestamp: Sat, 28 Dec 2024 04:41:21 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xc74bcce181b08956cd3984bcb93f6eb86c3bb7322d219131b2585f7ba862be81.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "11155111_0_0xc74bcce181b08956cd3984bcb93f6eb86c3bb7322d219131b2585f7ba862be81",
+  name: "Deposit USDC into Set & Forgetti (a THJ Joint)",
+  description: "When USDC is supplied to the market, it is deposited into Set & Forgetti's HENLO/USDC LP Farm in Henlo Maxi mode. Users may withdraw after 3 months.",
+  is_verified: false,
+});
