ed57d2b57f91a41a4fa226cc39a4535e3f58a843
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-28 12:19:31 +0530

    feat(market-map): Update market -- 11155111_0_0x620841ee55ae930f1471afc3feb1433a8fc68bac0a768eeb10e88837ffad7868 -- Recipe (0x6208...7868)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x620841ee55ae930f1471afc3feb1433a8fc68bac0a768eeb10e88837ffad7868 (Market Hash)
    - Request Timestamp: Sat, 28 Dec 2024 06:49:30 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x620841ee55ae930f1471afc3feb1433a8fc68bac0a768eeb10e88837ffad7868.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "11155111_0_0x620841ee55ae930f1471afc3feb1433a8fc68bac0a768eeb10e88837ffad7868",
+  name: "BOYCO — Predeposit in SatLayer uniBTC",
+  description: "Deposit uniBTC",
+  is_verified: false,
+});
