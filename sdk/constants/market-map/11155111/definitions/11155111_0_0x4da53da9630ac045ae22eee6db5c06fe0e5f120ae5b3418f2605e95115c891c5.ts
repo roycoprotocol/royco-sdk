3a8ad01083f3fb7bad9f4e5e201499cf1bc32b1b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-29 22:52:39 +0530

    feat(market-map): Update market -- 11155111_0_0x4da53da9630ac045ae22eee6db5c06fe0e5f120ae5b3418f2605e95115c891c5 -- Recipe (0x4da5...91c5)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x4da53da9630ac045ae22eee6db5c06fe0e5f120ae5b3418f2605e95115c891c5 (Market Hash)
    - Request Timestamp: Sun, 29 Dec 2024 17:22:38 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x4da53da9630ac045ae22eee6db5c06fe0e5f120ae5b3418f2605e95115c891c5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "11155111_0_0x4da53da9630ac045ae22eee6db5c06fe0e5f120ae5b3418f2605e95115c891c5",
+  name: "BOYCO — Predeposit in Dahlia BERA/USDC",
+  description: "Predeposit in Dahlia BERA/USDC lending market on Berachain.",
+  is_verified: false,
+});
