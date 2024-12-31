9e35366e0edb56a0f034cb1c40a5f381c6d48dea
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2024-12-31 17:47:00 +0530

    feat(market-map): Update market -- 42161_0_0xda2d6a0d934ca3837ef875f4fc16032b9779fae57df6d3adf24bdc9b43096f4f -- Recipe (0xda2d...6f4f)
    Market Details:
    - Chain ID: 42161
    - Market Type: 0 (Recipe)
    - Market ID: 0xda2d6a0d934ca3837ef875f4fc16032b9779fae57df6d3adf24bdc9b43096f4f (Market Hash)
    - Request Timestamp: Tue, 31 Dec 2024 12:17:00 GMT

---
+++ sdk/constants/market-map/42161/definitions/42161_0_0xda2d6a0d934ca3837ef875f4fc16032b9779fae57df6d3adf24bdc9b43096f4f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "42161_0_0xda2d6a0d934ca3837ef875f4fc16032b9779fae57df6d3adf24bdc9b43096f4f",
+  name: "Test Arbitrum Market",
+  description: "Test Arbitrum Market",
+  is_verified: false,
+});
