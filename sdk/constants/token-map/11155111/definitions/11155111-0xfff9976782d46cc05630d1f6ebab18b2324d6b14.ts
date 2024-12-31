f4b767989b83843b80c85427ea7e3b9a90038f21
GitHub Action <action@github.com>
2024-12-31 01:10:01 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/11155111/definitions/11155111-0xfff9976782d46cc05630d1f6ebab18b2324d6b14.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0xfff9976782d46cc05630d1f6ebab18b2324d6b14.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14",
+  id: "11155111-0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
   chain_id: 11155111,
-  contract_address: "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14",
+  contract_address: "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
   name: "Wrapped Ether",
   symbol: "WETH9",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png",
