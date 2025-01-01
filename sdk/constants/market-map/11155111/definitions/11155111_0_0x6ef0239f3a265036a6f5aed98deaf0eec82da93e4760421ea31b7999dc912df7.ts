60b15daabc421ffbf4794432e69e3d6cc63435a2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-01 23:06:33 +0530

    feat(market-map): Update market -- 11155111_0_0x6ef0239f3a265036a6f5aed98deaf0eec82da93e4760421ea31b7999dc912df7 -- Recipe (0x6ef0...2df7) (#104)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x6ef0239f3a265036a6f5aed98deaf0eec82da93e4760421ea31b7999dc912df7 (Market Hash)
    - Request Timestamp: Wed, 01 Jan 2025 17:36:31 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x6ef0239f3a265036a6f5aed98deaf0eec82da93e4760421ea31b7999dc912df7.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: "11155111_0_0x6ef0239f3a265036a6f5aed98deaf0eec82da93e4760421ea31b7999dc912df7",
+  name: "BOYC - Pre-Despoit in D2 Finance KODIAK++ strategy",
+  description: "Run managed strategies ontop of Kodiak, Dolomite, Ooga Booga, and infrared while incentivizing with EDA call options",
+  is_verified: false,
+});
