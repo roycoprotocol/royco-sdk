9449d2529df44b792d772f823d11002d0f813711
GitHub Action <action@github.com>
2025-01-01 17:50:02 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/11155111/definitions/11155111_0_0x463b8b0944240a3aeb108600b08d7a4357e961d0e454219a92375c5b59dbff46.ts
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x463b8b0944240a3aeb108600b08d7a4357e961d0e454219a92375c5b59dbff46.ts
@@ -3,6 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: "11155111_0_0x463b8b0944240a3aeb108600b08d7a4357e961d0e454219a92375c5b59dbff46",
   name: "BOYCO - Predeposit in D2 Finance Kodiak++ strategy",
-  description: "Run managed strategies ontop of Kodiak, Dolomite, Ooga Booga, and infrared while incentivizing with EDA call options",
+  description:
+    "Run managed strategies ontop of Kodiak, Dolomite, Ooga Booga, and infrared while incentivizing with EDA call options",
   is_verified: false,
 });
