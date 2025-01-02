b9ee8ec207b0437ebb6a9afda4800e410cf5e0b1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-03 02:06:29 +0530

    feat(market-map): Update market -- 11155111_0_0x4b8724dc73163b586d84c8fecda13266dbb6fec140b02fd70b7aae6245bb0100 -- Recipe (0x4b87...0100) (#113)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x4b8724dc73163b586d84c8fecda13266dbb6fec140b02fd70b7aae6245bb0100 (Market Hash)
    - Request Timestamp: Thu, 02 Jan 2025 20:36:27 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x4b8724dc73163b586d84c8fecda13266dbb6fec140b02fd70b7aae6245bb0100.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x4b8724dc73163b586d84c8fecda13266dbb6fec140b02fd70b7aae6245bb0100`,
+  name: `BOYCO — Predeposit in uniBTC Goldivault and LP Ownership Tokens`,
+  description: `When uniBTC is supplied to the market, it is deposited into the uniBTC Goldivault.`,
+  is_verified: false,
+});
