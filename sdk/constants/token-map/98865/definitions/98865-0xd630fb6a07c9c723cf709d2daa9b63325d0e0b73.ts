1217fc5e04e91864bd099ae2edcb42c563f6acc1
GitHub Action <action@github.com>
2025-01-02 16:18:24 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98865/definitions/98865-0xd630fb6a07c9c723cf709d2daa9b63325d0e0b73.ts
+++ sdk/constants/token-map/98865/definitions/98865-0xd630fb6a07c9c723cf709d2daa9b63325d0e0b73.ts
@@ -6,7 +6,8 @@ export default defineToken({
   contract_address: "0xd630fb6a07c9c723cf709d2daa9b63325d0e0b73",
   name: "Plume ETH",
   symbol: "pETH",
-  image: "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/67728894579669c6657e164d_pETH-token.svg",
+  image:
+    "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/67728894579669c6657e164d_pETH-token.svg",
   decimals: 18,
   source: "coinmarketcap",
   search_id: "1027",
--- sdk/constants/token-map/98865/definitions/98865-0xdddd73f5df1f0dc31373357beac77545dc5a6f3f.ts
+++ sdk/constants/token-map/98865/definitions/98865-0xdddd73f5df1f0dc31373357beac77545dc5a6f3f.ts
@@ -6,7 +6,8 @@ export default defineToken({
   contract_address: "0xdddd73f5df1f0dc31373357beac77545dc5a6f3f",
   name: "Plume USD",
   symbol: "pUSD",
-  image: "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/677284c8f1625bc0d62eaa68_pUSD-token.svg",
+  image:
+    "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/677284c8f1625bc0d62eaa68_pUSD-token.svg",
   decimals: 6,
   source: "coinmarketcap",
   search_id: "",
