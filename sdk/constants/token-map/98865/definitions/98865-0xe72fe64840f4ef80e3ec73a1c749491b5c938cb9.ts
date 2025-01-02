d48ea082c1d810c72646482ac634995eebec7194
githubotoro <yupuday.git@gmail.com>
2025-01-02 11:02:42 +0530

    feat(constants): token definition fix

--- sdk/constants/token-map/98865/definitions/98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9.ts
+++ sdk/constants/token-map/98865/definitions/98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9.ts
@@ -4,7 +4,7 @@ export default defineToken({
   id: "98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9",
   chain_id: 98865,
   contract_address: "0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9",
-  name: "Nest Treasuries Vault,
+  name: "Nest Treasuries Vault",
   symbol: "nTBILL",
   image: "",
   decimals: 6,
