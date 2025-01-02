e638af92135ae5cbe532b1f2ee43a38eabf79c0b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-02 23:43:58 +0530

    feat(market-map): Update market -- 1_1_0xdbf074ef1689a3349fd3ac2316655069c84450c2 -- Vault (0xdbf0...50c2) (#111)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0xdbf074ef1689a3349fd3ac2316655069c84450c2 (Wrapped Vault Address)
    - Request Timestamp: Thu, 02 Jan 2025 18:13:56 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0xdbf074ef1689a3349fd3ac2316655069c84450c2.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0xdbf074ef1689a3349fd3ac2316655069c84450c2`,
+  name: `Lend USDC on Euler Prime`,
+  description: `When USDC is supplied to the market, depositors earn extra rEUL rewards`,
+  is_verified: false,
+});
