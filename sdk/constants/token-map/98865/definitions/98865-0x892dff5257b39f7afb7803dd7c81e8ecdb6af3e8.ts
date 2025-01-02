d8558da997f25aa5880bfe570083aa9aa30191ba
bharatvish <bharat@waymont.co>
2025-01-02 07:56:56 -0800

    Update 98865-0x892dff5257b39f7afb7803dd7c81e8ecdb6af3e8.ts

--- sdk/constants/token-map/98865/definitions/98865-0x892dff5257b39f7afb7803dd7c81e8ecdb6af3e8.ts
+++ sdk/constants/token-map/98865/definitions/98865-0x892dff5257b39f7afb7803dd7c81e8ecdb6af3e8.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x892dff5257b39f7afb7803dd7c81e8ecdb6af3e8",
   name: "Nest Yield Vault",
   symbol: "nYIELD",
-  image: "",
+  image: "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/6732070ff8b006c89bbc698e_Logomark%20Red.svg",
   decimals: 6,
   source: "coinmarketcap",
   search_id: "",
