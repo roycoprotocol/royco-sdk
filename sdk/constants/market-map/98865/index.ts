76af3531c581bd9f8c1fd035abf7939b2e12fe40
GitHub Action <action@github.com>
2025-01-02 06:52:51 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/98865/index.ts
+++ sdk/constants/market-map/98865/index.ts
@@ -1,1 +1,6 @@
-export const MarketMap98865 = {};
+import market_98865_0_0xf1925365d20600cbcc4a8e1c3039cf68a3ae1ded9c4d95c84346b2928f20f08b from "./definitions/98865_0_0xf1925365d20600cbcc4a8e1c3039cf68a3ae1ded9c4d95c84346b2928f20f08b";
+
+export const MarketMap98865 = {
+  [market_98865_0_0xf1925365d20600cbcc4a8e1c3039cf68a3ae1ded9c4d95c84346b2928f20f08b.id]:
+    market_98865_0_0xf1925365d20600cbcc4a8e1c3039cf68a3ae1ded9c4d95c84346b2928f20f08b,
+};
