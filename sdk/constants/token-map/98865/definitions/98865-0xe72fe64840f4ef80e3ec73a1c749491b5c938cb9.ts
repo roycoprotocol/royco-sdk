026539436d8054add16e7c31e18c1627f6d163bd
bharatvish <bharat@waymont.co>
2025-01-01 16:28:13 -0800

    Create 98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9.ts

---
+++ sdk/constants/token-map/98865/definitions/98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9",
+  chain_id: 98865,
+  contract_address: "0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9",
+  name: "Nest Treasuries Vault,
+  symbol: "nTBILL",
+  image: "",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "",
+  type: "token",
+});
