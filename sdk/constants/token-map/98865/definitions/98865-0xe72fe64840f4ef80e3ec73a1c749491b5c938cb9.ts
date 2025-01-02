3c7b5458a8a7956ea20b9f1c65168d3424d74f60
bharatvish <bharat@waymont.co>
2025-01-02 07:56:35 -0800

    Update 98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9.ts

--- sdk/constants/token-map/98865/definitions/98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9.ts
+++ sdk/constants/token-map/98865/definitions/98865-0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0xe72fe64840f4ef80e3ec73a1c749491b5c938cb9",
   name: "Nest Treasuries Vault",
   symbol: "nTBILL",
-  image: "",
+  image: "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/6732070ff8b006c89bbc698e_Logomark%20Red.svg",
   decimals: 6,
   source: "coinmarketcap",
   search_id: "",
