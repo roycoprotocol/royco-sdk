e6ef60447883fcaa72d0c0b3cd6bee4d434dc92c
githubotoro <yupuday.git@gmail.com>
2025-01-02 10:59:43 +0530

    feat(constants): definitions fix

--- sdk/constants/market-map/11155111/definitions/11155111_0_0x868096ac22d1d42ced54795ebf1754400ca51b399cd1c84a46ac3be04daf22a1.ts
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x868096ac22d1d42ced54795ebf1754400ca51b399cd1c84a46ac3be04daf22a1.ts
@@ -3,8 +3,8 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: "11155111_0_0x868096ac22d1d42ced54795ebf1754400ca51b399cd1c84a46ac3be04daf22a1",
   name: "BOYCO - Predeposit in Origami bKAMI",
-  description: "USDC (HONEY) supplied to this market earns bKAMI at a $69M FDV pro-rata according to TVL (40% APR). 10% of the supply is allocated for max $69M TVL. User deposits are locked for 3 months.
+  description: `USDC (HONEY) supplied to this market earns bKAMI at a $69M FDV pro-rata according to TVL (40% APR). 10% of the supply is allocated for max $69M TVL. User deposits are locked for 3 months.
 
-bKAMI are non-transferable points that can be redeemed 1:1 for KAMI at TGE.",
+bKAMI are non-transferable points that can be redeemed 1:1 for KAMI at TGE.`,
   is_verified: false,
 });
--- sdk/constants/market-map/8453/definitions/8453_1_0x9db6dbdcf6b3f158f8b02d762f99f8ce7663e1aa.ts
+++ sdk/constants/market-map/8453/definitions/8453_1_0x9db6dbdcf6b3f158f8b02d762f99f8ce7663e1aa.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: "8453_1_0x9db6dbdcf6b3f158f8b02d762f99f8ce7663e1aa",
   name: "Supply ETH to BakerFi on Morpho",
-  description: "Supply ETH to BakerFi's Recipe "Sapphire Tartelette a la Base", a recursive staking strategy using Lido and Morpho as the underlying protocols, on Base.",
+  description: `Supply ETH to BakerFi's Recipe "Sapphire Tartelette a la Base", a recursive staking strategy using Lido and Morpho as the underlying protocols, on Base.`,
   is_verified: false,
 });
