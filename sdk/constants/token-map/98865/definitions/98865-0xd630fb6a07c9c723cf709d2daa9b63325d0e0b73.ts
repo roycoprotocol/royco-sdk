c63ae0fd6a00f5ce47ed2bfa8cebe5d33d99a4b7
bharatvish <bharat@waymont.co>
2025-01-02 07:55:22 -0800

    Update 98865-0xd630fb6a07c9c723cf709d2daa9b63325d0e0b73.ts

--- sdk/constants/token-map/98865/definitions/98865-0xd630fb6a07c9c723cf709d2daa9b63325d0e0b73.ts
+++ sdk/constants/token-map/98865/definitions/98865-0xd630fb6a07c9c723cf709d2daa9b63325d0e0b73.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0xd630fb6a07c9c723cf709d2daa9b63325d0e0b73",
   name: "Plume ETH",
   symbol: "pETH",
-  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+  image: "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/67728894579669c6657e164d_pETH-token.svg",
   decimals: 18,
   source: "coinmarketcap",
   search_id: "1027",
