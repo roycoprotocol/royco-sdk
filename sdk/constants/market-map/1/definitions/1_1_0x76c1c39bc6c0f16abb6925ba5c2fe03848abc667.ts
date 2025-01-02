dc481964438de5cf4d85165f87f20e2c645a5057
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-03 00:16:55 +0530

    feat(market-map): Update market -- 1_1_0x76c1c39bc6c0f16abb6925ba5c2fe03848abc667 -- Vault (0x76c1...c667) (#112)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0x76c1c39bc6c0f16abb6925ba5c2fe03848abc667 (Wrapped Vault Address)
    - Request Timestamp: Thu, 02 Jan 2025 18:46:53 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0x76c1c39bc6c0f16abb6925ba5c2fe03848abc667.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0x76c1c39bc6c0f16abb6925ba5c2fe03848abc667`,
+  name: `Supply WETH on Euler Prime`,
+  description: `Suppliers of WETH will be getting extra rEUL rewards.`,
+  is_verified: false,
+});
