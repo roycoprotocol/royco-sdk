02b36ed0a322ccbad20a581be0b16da121654112
bharatvish <bharat@waymont.co>
2025-01-02 07:56:14 -0800

    Update 98865-0x81537d879acc8a290a1846635a0caa908f8ca3a6.ts

--- sdk/constants/token-map/98865/definitions/98865-0x81537d879acc8a290a1846635a0caa908f8ca3a6.ts
+++ sdk/constants/token-map/98865/definitions/98865-0x81537d879acc8a290a1846635a0caa908f8ca3a6.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x81537d879acc8a290a1846635a0caa908f8ca3a6",
   name: "Nest RWA Vault Aggregate Token",
   symbol: "nRWA",
-  image: "",
+  image: "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/6732070ff8b006c89bbc698e_Logomark%20Red.svg",
   decimals: 6,
   source: "coinmarketcap",
   search_id: "",
