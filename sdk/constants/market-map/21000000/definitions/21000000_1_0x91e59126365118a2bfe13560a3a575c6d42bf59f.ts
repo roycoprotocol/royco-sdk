987334d77c058c90bbb7ab1983791f8213b0e439
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-03 18:19:29 +0530

    feat(market-map): Update market -- 21000000_1_0x91e59126365118a2bfe13560a3a575c6d42bf59f -- Vault (0x91e5...f59f)
    Market Details:
    - Chain ID: 21000000
    - Market Type: 1 (Vault)
    - Market ID: 0x91e59126365118a2bfe13560a3a575c6d42bf59f (Wrapped Vault Address)
    - Request Timestamp: Fri, 03 Jan 2025 12:49:29 GMT

---
+++ sdk/constants/market-map/21000000/definitions/21000000_1_0x91e59126365118a2bfe13560a3a575c6d42bf59f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `21000000_1_0x91e59126365118a2bfe13560a3a575c6d42bf59f`,
+  name: `Lend BTCN onto ZeroLend on Corn`,
+  description: `BTCN that is supplied into this vault earns corn points and yield from traders who will borrow the BTCN to loop into assets such as like eBTC, LBTC etc...`,
+  is_verified: false,
+});
