3e467f9cf56298b8e7c968af5c8e4562047817a5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-04 00:19:55 +0530

    feat(market-map): Update market -- 98865_1_0x8cc74dd458db4289ce03d8e4606433ab3631242d -- Vault (0x8cc7...242d)
    Market Details:
    - Chain ID: 98865
    - Market Type: 1 (Vault)
    - Market ID: 0x8cc74dd458db4289ce03d8e4606433ab3631242d (Wrapped Vault Address)
    - Request Timestamp: Fri, 03 Jan 2025 18:49:54 GMT

---
+++ sdk/constants/market-map/98865/definitions/98865_1_0x8cc74dd458db4289ce03d8e4606433ab3631242d.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98865_1_0x8cc74dd458db4289ce03d8e4606433ab3631242d`,
+  name: `Supply nRWA to Solera`,
+  description: `Supply nRWA to Solera on Plume.`,
+  is_verified: false,
+});
