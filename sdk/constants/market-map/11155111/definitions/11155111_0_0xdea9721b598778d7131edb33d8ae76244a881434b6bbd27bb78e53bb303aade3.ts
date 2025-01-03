ca33513f25edc81d0c67e477d8b22abe5b5570a2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-04 01:33:18 +0530

    feat(market-map): Update market -- 11155111_0_0xdea9721b598778d7131edb33d8ae76244a881434b6bbd27bb78e53bb303aade3 -- Recipe (0xdea9...ade3)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xdea9721b598778d7131edb33d8ae76244a881434b6bbd27bb78e53bb303aade3 (Market Hash)
    - Request Timestamp: Fri, 03 Jan 2025 20:03:17 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xdea9721b598778d7131edb33d8ae76244a881434b6bbd27bb78e53bb303aade3.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xdea9721b598778d7131edb33d8ae76244a881434b6bbd27bb78e53bb303aade3`,
+  name: `BOYCO — Predeposit in Dahlia STONE&#x2F;WETH`,
+  description: `Predeposit in Dahlia STONE&#x2F;WETH lending market on Berachain.`,
+  is_verified: false,
+});
