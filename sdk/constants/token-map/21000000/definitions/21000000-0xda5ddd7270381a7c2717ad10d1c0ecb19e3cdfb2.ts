3357303c50f6e24f2438ec0a8cd20d3ee7f1db69
bharatvish <bharat@waymont.co>
2025-01-03 10:59:29 -0800

    Update 21000000-0xda5ddd7270381a7c2717ad10d1c0ecb19e3cdfb2.ts

--- sdk/constants/token-map/21000000/definitions/21000000-0xda5ddd7270381a7c2717ad10d1c0ecb19e3cdfb2.ts
+++ sdk/constants/token-map/21000000/definitions/21000000-0xda5ddd7270381a7c2717ad10d1c0ecb19e3cdfb2.ts
@@ -10,6 +10,6 @@ export default defineToken({
     "https://imgproxy-mainnet.routescan.io/Ep-Teqz159w-GRQa32KyqR-2wDoSd6Bkr_OQYZYB8JE/pr:thumb_64/aHR0cHM6Ly9jbXMtY2RuLmF2YXNjYW4uY29tL2NtczIvY29ybl9sb2dvLmJlNzFmNWRjODJiYS5wbmc",
   decimals: 18,
   source: "",
-  search_id: ",
+  search_id: "",
   type: "token",
 });
