0bad3c225f09077927ed711772a71d946f04c092
bharatvish <bharat@waymont.co>
2025-01-03 08:59:11 -0800

    Create 21000000-0xda5dDd7270381A7C2717aD10D1c0ecB19e3CDFb2.ts

---
+++ sdk/constants/token-map/21000000/definitions/21000000-0xda5dDd7270381A7C2717aD10D1c0ecB19e3CDFb2.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "21000000-0xda5dDd7270381A7C2717aD10D1c0ecB19e3CDFb2",
+  chain_id: 21000000,
+  contract_address: "0xda5dDd7270381A7C2717aD10D1c0ecB19e3CDFb2",
+  name: "Wrapped Bitcorn",
+  symbol: "WBTCN,
+  image:
+    "https://imgproxy-mainnet.routescan.io/Ep-Teqz159w-GRQa32KyqR-2wDoSd6Bkr_OQYZYB8JE/pr:thumb_64/aHR0cHM6Ly9jbXMtY2RuLmF2YXNjYW4uY29tL2NtczIvY29ybl9sb2dvLmJlNzFmNWRjODJiYS5wbmc",
+  decimals: 18,
+  source: "",
+  search_id: ",
+  type: "token",
+});
