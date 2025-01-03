0278bcc6ba870c0a5c5936177f7f00736f71e92e
bharatvish <bharat@waymont.co>
2025-01-03 14:49:38 -0800

    Update 21000000_1_0x91e59126365118a2bfe13560a3a575c6d42bf59f.ts

--- sdk/constants/market-map/21000000/definitions/21000000_1_0x91e59126365118a2bfe13560a3a575c6d42bf59f.ts
+++ sdk/constants/market-map/21000000/definitions/21000000_1_0x91e59126365118a2bfe13560a3a575c6d42bf59f.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `21000000_1_0x91e59126365118a2bfe13560a3a575c6d42bf59f`,
   name: `Lend BTCN onto ZeroLend on Corn`,
-  description: `BTCN that is supplied into this vault earns corn points and yield from traders who will borrow the BTCN to loop into assets such as like eBTC, LBTC etc...`,
+  description: `BTCN that is supplied into this vault earns Corn Kernels and yield from traders who will borrow the BTCN to loop into assets such as like eBTC, LBTC etc...`,
   is_verified: false,
 });
