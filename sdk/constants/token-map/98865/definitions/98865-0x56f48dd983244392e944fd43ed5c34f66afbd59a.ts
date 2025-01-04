8c6a126ffc0a9fd2105d592da154e6ce57b51a09
GitHub Action <action@github.com>
2025-01-04 05:50:00 +0000

    feat(chore): update generated files

---
+++ sdk/constants/token-map/98865/definitions/98865-0x56f48dd983244392e944fd43ed5c34f66afbd59a.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98865-0x56f48dd983244392e944fd43ed5c34f66afbd59a",
+  chain_id: 98865,
+  contract_address: "0x56f48dd983244392e944fd43ed5c34f66afbd59a",
+  name: "Plume APY Boost",
+  symbol: "pAPY",
+  image:
+    "https://cdn.prod.website-files.com/670fc97cba6a0b3f2e579538/677284c8f1625bc0d62eaa68_pUSD-token.svg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
