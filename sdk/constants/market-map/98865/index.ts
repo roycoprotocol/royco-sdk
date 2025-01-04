ccf093c46be45941e9b4499f7f4b9bf21734a948
GitHub Action <action@github.com>
2025-01-04 02:59:11 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/98865/index.ts
+++ sdk/constants/market-map/98865/index.ts
@@ -1,6 +1,7 @@
 import market_98865_0_0xf1925365d20600cbcc4a8e1c3039cf68a3ae1ded9c4d95c84346b2928f20f08b from "./definitions/98865_0_0xf1925365d20600cbcc4a8e1c3039cf68a3ae1ded9c4d95c84346b2928f20f08b";
 import market_98865_1_0x5557f8bfa53b8f1e356de3dc71412c327c207ed1 from "./definitions/98865_1_0x5557f8bfa53b8f1e356de3dc71412c327c207ed1";
 import market_98865_1_0x8cc74dd458db4289ce03d8e4606433ab3631242d from "./definitions/98865_1_0x8cc74dd458db4289ce03d8e4606433ab3631242d";
+import market_98865_1_0xebedd3ec4b38c0488ccda9bd825ac174546f99a2 from "./definitions/98865_1_0xebedd3ec4b38c0488ccda9bd825ac174546f99a2";
 
 export const MarketMap98865 = {
   [market_98865_0_0xf1925365d20600cbcc4a8e1c3039cf68a3ae1ded9c4d95c84346b2928f20f08b.id]:
@@ -9,4 +10,6 @@ export const MarketMap98865 = {
     market_98865_1_0x5557f8bfa53b8f1e356de3dc71412c327c207ed1,
   [market_98865_1_0x8cc74dd458db4289ce03d8e4606433ab3631242d.id]:
     market_98865_1_0x8cc74dd458db4289ce03d8e4606433ab3631242d,
+  [market_98865_1_0xebedd3ec4b38c0488ccda9bd825ac174546f99a2.id]:
+    market_98865_1_0xebedd3ec4b38c0488ccda9bd825ac174546f99a2,
 };
