56d36b08be0b42832b542f67201768873175e7c1
bharatvish <bharat@waymont.co>
2025-01-05 11:58:55 -0800

    Update 98865-0x626613B473F7eF65747967017C11225436EFaEd7.ts

--- sdk/constants/token-map/98865/definitions/98865-0x626613B473F7eF65747967017C11225436EFaEd7.ts
+++ sdk/constants/token-map/98865/definitions/98865-0x626613B473F7eF65747967017C11225436EFaEd7.ts
@@ -1,1 +1,14 @@
-import
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98865-0x626613B473F7eF65747967017C11225436EFaEd7",
+  chain_id: 98865,
+  contract_address: "0x626613B473F7eF65747967017C11225436EFaEd7",
+  name: "Wrapped ETH",
+  symbol: "WETH",
+  image: "https://assets.plumenetwork.xyz/images/logos/nest/nYIELD-token.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "",
+  type: "token",
+});
