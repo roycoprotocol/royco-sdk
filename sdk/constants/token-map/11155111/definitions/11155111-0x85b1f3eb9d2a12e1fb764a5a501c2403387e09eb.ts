d9c5b102ad87fd8eed3ee1bc33304239675d7a22
GitHub Action <action@github.com>
2025-01-05 18:07:27 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/11155111/definitions/11155111-0x85b1f3eb9d2a12e1fb764a5a501c2403387e09eb.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0x85b1f3eb9d2a12e1fb764a5a501c2403387e09eb.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0x85b1F3eB9D2a12e1FB764a5A501c2403387E09eB",
+  id: "11155111-0x85b1f3eb9d2a12e1fb764a5a501c2403387e09eb",
   chain_id: 11155111,
-  contract_address: "0x85b1F3eB9D2a12e1FB764a5A501c2403387E09eB",
+  contract_address: "0x85b1f3eb9d2a12e1fb764a5a501c2403387e09eb",
   name: "Henlo Boyco Points",
   symbol: "HBP",
   image: "https://chainlist.org/unknown-logo.png",
