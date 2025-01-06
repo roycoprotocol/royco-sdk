02e7a7d7c585337a25e0e5e69c8f2cb22a0da8ae
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-06 23:32:17 +0530

    feat(market-map): Update market -- 11155111_0_0xc168f10a234adae731ce0d7f593ed5fe389577811ef6b024efb57ce4629fb3e4 -- Recipe (0xc168...b3e4) (#135)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xc168f10a234adae731ce0d7f593ed5fe389577811ef6b024efb57ce4629fb3e4 (Market Hash)
    - Request Timestamp: Mon, 06 Jan 2025 18:02:15 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xc168f10a234adae731ce0d7f593ed5fe389577811ef6b024efb57ce4629fb3e4.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xc168f10a234adae731ce0d7f593ed5fe389577811ef6b024efb57ce4629fb3e4`,
+  name: `BOYCO — Predeposit in Concrete WBTC-Honey`,
+  description: `When WBTC is deposited into this market the user is rewarded with Honey on Berachain.`,
+  is_verified: false,
+});
