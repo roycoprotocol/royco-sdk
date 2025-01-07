a5ba9e4338653efbef86b22978d98380e007e998
bharatvish <bharat@waymont.co>
2025-01-07 13:19:45 -0800

    Create 21000000-0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE.ts

---
+++ sdk/constants/token-map/21000000/definitions/21000000-0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "21000000-0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+  chain_id: 21000000,
+  contract_address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+  name: "Bitcorn",
+  symbol: "BTCN",
+  image:
+    "https://docs.usecorn.com/img/guides/Corn_Brand_Assets/Logos/bitcorn_logo.svg",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "token",
+});
