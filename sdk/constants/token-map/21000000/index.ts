89983e49441a18d946c60b14fca24d8464227ff2
GitHub Action <action@github.com>
2025-01-07 21:38:49 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/21000000/index.ts
+++ sdk/constants/token-map/21000000/index.ts
@@ -1,15 +1,15 @@
 import token_21000000_0x606cf606076d1a74cbea93940742b6a2cf901139 from "./definitions/21000000-0x606cf606076d1a74cbea93940742b6a2cf901139";
-import token_21000000_0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee from "./definitions/21000000-0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 import token_21000000_0xda5ddd7270381a7c2717ad10d1c0ecb19e3cdfb2 from "./definitions/21000000-0xda5ddd7270381a7c2717ad10d1c0ecb19e3cdfb2";
 import token_21000000_0xdf0b24095e15044538866576754f3c964e902ee6 from "./definitions/21000000-0xdf0b24095e15044538866576754f3c964e902ee6";
+import token_21000000_0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee from "./definitions/21000000-0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 
 export const TokenMap21000000 = {
   [token_21000000_0x606cf606076d1a74cbea93940742b6a2cf901139.id]:
     token_21000000_0x606cf606076d1a74cbea93940742b6a2cf901139,
-  [token_21000000_0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.id]:
-    token_21000000_0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee,
   [token_21000000_0xda5ddd7270381a7c2717ad10d1c0ecb19e3cdfb2.id]:
     token_21000000_0xda5ddd7270381a7c2717ad10d1c0ecb19e3cdfb2,
   [token_21000000_0xdf0b24095e15044538866576754f3c964e902ee6.id]:
     token_21000000_0xdf0b24095e15044538866576754f3c964e902ee6,
+  [token_21000000_0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.id]:
+    token_21000000_0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee,
 };
