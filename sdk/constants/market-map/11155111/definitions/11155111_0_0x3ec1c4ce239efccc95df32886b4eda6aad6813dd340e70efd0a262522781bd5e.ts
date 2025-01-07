17628c78bd538caea43892828e0b550f72fbce1d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-07 10:27:53 +0530

    feat(market-map): Update market -- 11155111_0_0x3ec1c4ce239efccc95df32886b4eda6aad6813dd340e70efd0a262522781bd5e -- Recipe (0x3ec1...bd5e)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x3ec1c4ce239efccc95df32886b4eda6aad6813dd340e70efd0a262522781bd5e (Market Hash)
    - Request Timestamp: Tue, 07 Jan 2025 04:57:53 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x3ec1c4ce239efccc95df32886b4eda6aad6813dd340e70efd0a262522781bd5e.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x3ec1c4ce239efccc95df32886b4eda6aad6813dd340e70efd0a262522781bd5e`,
+  name: `BOYCO — Predeposit in Moby stgUSDC OLP`,
+  description: `When stgUSDC is supplied to the market, it is deposited into Moby OLP Pool.`,
+  is_verified: false,
+});
