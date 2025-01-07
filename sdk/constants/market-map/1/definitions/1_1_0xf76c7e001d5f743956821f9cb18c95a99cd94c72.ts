b448e5fac1c607df178e39e019b3bb1b2a9a4407
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-07 07:27:42 +0530

    feat(market-map): Update market -- 1_1_0xf76c7e001d5f743956821f9cb18c95a99cd94c72 -- Vault (0xf76c...4c72) (#142)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0xf76c7e001d5f743956821f9cb18c95a99cd94c72 (Wrapped Vault Address)
    - Request Timestamp: Tue, 07 Jan 2025 01:57:40 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0xf76c7e001d5f743956821f9cb18c95a99cd94c72.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0xf76c7e001d5f743956821f9cb18c95a99cd94c72`,
+  name: `Bharat&#x27;s Euler Test`,
+  description: `Bharat&#x27;s Euler TestBharat&#x27;s Euler TestBharat&#x27;s Euler TestBharat&#x27;s Euler Test`,
+  is_verified: false,
+});
