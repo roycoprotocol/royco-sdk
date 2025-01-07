14bb4fa6ee94eb6d98dd156017aa952ec2a28c87
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-07 10:25:55 +0530

    feat(market-map): Update market -- 11155111_0_0x75091cb3ba55fe7af132865c9a9d97c57203c2b7c8aca6c293c4cf1a9f1c765f -- Recipe (0x7509...765f)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x75091cb3ba55fe7af132865c9a9d97c57203c2b7c8aca6c293c4cf1a9f1c765f (Market Hash)
    - Request Timestamp: Tue, 07 Jan 2025 04:55:55 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x75091cb3ba55fe7af132865c9a9d97c57203c2b7c8aca6c293c4cf1a9f1c765f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x75091cb3ba55fe7af132865c9a9d97c57203c2b7c8aca6c293c4cf1a9f1c765f`,
+  name: `BOYCO — Predeposit in Moby SPV HONEY-USDC`,
+  description: `When USDC is supplied to the market, it is deposited into Moby SPV(HONEY-USDC pool).`,
+  is_verified: false,
+});
