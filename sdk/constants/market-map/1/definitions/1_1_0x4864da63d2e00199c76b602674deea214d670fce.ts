a8971a2fe884ec0ed5206fd2d04ed9374914064f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-07 06:29:06 +0530

    feat(market-map): Update market -- 1_1_0x4864da63d2e00199c76b602674deea214d670fce -- Vault (0x4864...0fce)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0x4864da63d2e00199c76b602674deea214d670fce (Wrapped Vault Address)
    - Request Timestamp: Tue, 07 Jan 2025 00:59:06 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0x4864da63d2e00199c76b602674deea214d670fce.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0x4864da63d2e00199c76b602674deea214d670fce`,
+  name: `Test TestTest TestTest TestTest TestTest TestTest Test`,
+  description: `Test TestTest TestTest TestTest TestTest TestTest TestTest Test`,
+  is_verified: false,
+});
