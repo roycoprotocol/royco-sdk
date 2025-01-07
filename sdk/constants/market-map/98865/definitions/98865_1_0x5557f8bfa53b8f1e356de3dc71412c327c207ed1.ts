fa177df2e7348bbf6fa63438ce86574d94150e39
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2025-01-06 21:52:37 -0500

    redeploy Nest nRWA market

--- sdk/constants/market-map/98865/definitions/98865_1_0x5557f8bfa53b8f1e356de3dc71412c327c207ed1.ts
+++
@@ -1,8 +0,0 @@
-import { defineMarket } from "@/sdk/constants";
-
-export default defineMarket({
-  id: `98865_1_0x5557f8bfa53b8f1e356de3dc71412c327c207ed1`,
-  name: `Deposit pUSD into Nest`,
-  description: `When pUSD is deposited into Nest, it is deposited into RWAs that generate real-world yield. Users can withdraw at any time.`,
-  is_verified: false,
-});
