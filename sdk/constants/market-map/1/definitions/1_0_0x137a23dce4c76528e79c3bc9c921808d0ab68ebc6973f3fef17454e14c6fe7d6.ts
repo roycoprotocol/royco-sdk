887e32e19d1d3194d8b411f44f326e4f0f1feeb2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-08 03:40:15 +0530

    feat(market-map): Update market -- 1_0_0x137a23dce4c76528e79c3bc9c921808d0ab68ebc6973f3fef17454e14c6fe7d6 -- Recipe (0x137a...e7d6)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x137a23dce4c76528e79c3bc9c921808d0ab68ebc6973f3fef17454e14c6fe7d6 (Market Hash)
    - Request Timestamp: Tue, 07 Jan 2025 22:10:15 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x137a23dce4c76528e79c3bc9c921808d0ab68ebc6973f3fef17454e14c6fe7d6.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x137a23dce4c76528e79c3bc9c921808d0ab68ebc6973f3fef17454e14c6fe7d6`,
+  name: `Deposit USDC into SuperUSDC for 3 months`,
+  description: `Deposit USDC into Superform&#x27;s SuperUSDC on Ethereum for 3 months. SuperUSDC optimizes across blue-chip lending yield on Ethereum to earn the best returns.`,
+  is_verified: false,
+});
