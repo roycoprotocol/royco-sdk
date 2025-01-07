78ffe966fcb4d3bd5ba9368367febd105ab3b8e2
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2025-01-06 21:38:17 -0500

    add WETH on Plume Mainnet

--- sdk/constants/token-map/98865/definitions/98865-0x626613b473f7ef65747967017c11225436efaed7.ts
+++ sdk/constants/token-map/98865/definitions/98865-0x626613b473f7ef65747967017c11225436efaed7.ts
@@ -1,14 +1,14 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98865-0x626613b473f7ef65747967017c11225436efaed7",
+  id: "98865-0x626613B473F7eF65747967017C11225436EFaEd7",
   chain_id: 98865,
-  contract_address: "0x626613b473f7ef65747967017c11225436efaed7",
+  contract_address: "0x626613B473F7eF65747967017C11225436EFaEd7",
   name: "Wrapped ETH",
   symbol: "WETH",
-  image: "https://www.svgrepo.com/show/349356/ethereum.svg",
+  image: "https://s2.coinmarketcap.com/static/img/coins/128x128/2396.png",
   decimals: 18,
   source: "coinmarketcap",
-  search_id: "",
+  search_id: "2396",
   type: "token",
 });
