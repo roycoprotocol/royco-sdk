b9c29e77155684e1ba6b34c40b803b9829c609ee
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-08 03:45:42 +0530

    feat(market-map): Update market -- 98865_0_0x1866ba487f09ab29612567f6a8f6ede1fbfed54e9e2305275fc3202f553c2f03 -- Recipe (0x1866...2f03) (#156)
    Market Details:
    - Chain ID: 98865
    - Market Type: 0 (Recipe)
    - Market ID: 0x1866ba487f09ab29612567f6a8f6ede1fbfed54e9e2305275fc3202f553c2f03 (Market Hash)
    - Request Timestamp: Tue, 07 Jan 2025 22:15:39 GMT

---
+++ sdk/constants/market-map/98865/definitions/98865_0_0x1866ba487f09ab29612567f6a8f6ede1fbfed54e9e2305275fc3202f553c2f03.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98865_0_0x1866ba487f09ab29612567f6a8f6ede1fbfed54e9e2305275fc3202f553c2f03`,
+  name: `Lend ETH on Osprey Markets`,
+  description: `Lend pETH into the Osprey Market&#x27;s pETH pool. User&#x27;s may withdraw their lending amount anytime or choose to borrow with Osprey.`,
+  is_verified: false,
+});
