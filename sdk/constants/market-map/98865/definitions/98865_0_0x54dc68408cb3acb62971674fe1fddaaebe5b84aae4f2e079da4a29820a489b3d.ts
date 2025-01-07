b7ec85ee7b6c2a1cd3e1a70de048db7ef9ff802c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-08 04:37:52 +0530

    feat(market-map): Update market -- 98865_0_0x54dc68408cb3acb62971674fe1fddaaebe5b84aae4f2e079da4a29820a489b3d -- Recipe (0x54dc...9b3d)
    Market Details:
    - Chain ID: 98865
    - Market Type: 0 (Recipe)
    - Market ID: 0x54dc68408cb3acb62971674fe1fddaaebe5b84aae4f2e079da4a29820a489b3d (Market Hash)
    - Request Timestamp: Tue, 07 Jan 2025 23:07:51 GMT

---
+++ sdk/constants/market-map/98865/definitions/98865_0_0x54dc68408cb3acb62971674fe1fddaaebe5b84aae4f2e079da4a29820a489b3d.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98865_0_0x54dc68408cb3acb62971674fe1fddaaebe5b84aae4f2e079da4a29820a489b3d`,
+  name: `Lend pUSD on Osprey Markets`,
+  description: `Lend pUSD into the Osprey Market&#x27;s pUSD pool. User&#x27;s may withdraw their lending amount anytime or choose to borrow with Osprey.`,
+  is_verified: false,
+});
