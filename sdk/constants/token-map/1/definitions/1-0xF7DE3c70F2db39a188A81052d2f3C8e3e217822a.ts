6342c3cfc4e88f7fe2aef890d92400ee706172cf
bharatvish <bharat@waymont.co>
2025-01-08 11:50:04 -0800

    Create 1-0xF7DE3c70F2db39a188A81052d2f3C8e3e217822a.ts

---
+++ sdk/constants/token-map/1/definitions/1-0xF7DE3c70F2db39a188A81052d2f3C8e3e217822a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xF7DE3c70F2db39a188A81052d2f3C8e3e217822a",
+  chain_id: 1,
+  contract_address: "0xF7DE3c70F2db39a188A81052d2f3C8e3e217822a",
+  name: "SuperUSDC",
+  symbol: "SuperUSDC",
+  image: "https://www.superform.xyz/icon.png?a44c5d00988b5f21",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "",
+  type: "token",
+});
