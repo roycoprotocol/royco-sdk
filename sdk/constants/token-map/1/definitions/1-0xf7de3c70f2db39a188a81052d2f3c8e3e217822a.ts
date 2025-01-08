cb925e028017a9fb366030fb3f0636e684dbf45b
GitHub Action <action@github.com>
2025-01-08 20:16:13 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/1/definitions/1-0xf7de3c70f2db39a188a81052d2f3c8e3e217822a.ts
+++ sdk/constants/token-map/1/definitions/1-0xf7de3c70f2db39a188a81052d2f3c8e3e217822a.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "1-0xF7DE3c70F2db39a188A81052d2f3C8e3e217822a",
+  id: "1-0xf7de3c70f2db39a188a81052d2f3c8e3e217822a",
   chain_id: 1,
-  contract_address: "0xF7DE3c70F2db39a188A81052d2f3C8e3e217822a",
+  contract_address: "0xf7de3c70f2db39a188a81052d2f3c8e3e217822a",
   name: "SuperUSDC",
   symbol: "SuperUSDC",
   image: "https://www.superform.xyz/icon.png?a44c5d00988b5f21",
