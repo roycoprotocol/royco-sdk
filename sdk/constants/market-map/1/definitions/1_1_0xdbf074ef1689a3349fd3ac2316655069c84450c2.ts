b6ac7676a3232051a685930896a9156b74af5979
bharatvish <bharat@waymont.co>
2025-01-07 16:15:39 -0800

    Update 1_1_0xdbf074ef1689a3349fd3ac2316655069c84450c2.ts

--- sdk/constants/market-map/1/definitions/1_1_0xdbf074ef1689a3349fd3ac2316655069c84450c2.ts
+++ sdk/constants/market-map/1/definitions/1_1_0xdbf074ef1689a3349fd3ac2316655069c84450c2.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: `1_1_0xdbf074ef1689a3349fd3ac2316655069c84450c2`,
   name: `Lend USDC on Euler Prime`,
   description: `When USDC is supplied to the market, depositors earn extra rEUL rewards`,
-  is_verified: false,
+  is_verified: true,
 });
