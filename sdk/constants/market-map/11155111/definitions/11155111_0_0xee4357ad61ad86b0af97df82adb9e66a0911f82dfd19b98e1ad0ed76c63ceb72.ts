3212741b8f2362acf264132faae206ab1f421a50
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-10 04:21:31 +0530

    feat(market-map): Update market -- 11155111_0_0xee4357ad61ad86b0af97df82adb9e66a0911f82dfd19b98e1ad0ed76c63ceb72 -- Recipe (0xee43...eb72) (#171)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xee4357ad61ad86b0af97df82adb9e66a0911f82dfd19b98e1ad0ed76c63ceb72 (Market Hash)
    - Request Timestamp: Thu, 09 Jan 2025 22:51:28 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xee4357ad61ad86b0af97df82adb9e66a0911f82dfd19b98e1ad0ed76c63ceb72.ts
@@ -0,0 +1,13 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xee4357ad61ad86b0af97df82adb9e66a0911f82dfd19b98e1ad0ed76c63ceb72`,
+  name: `Marketito&quot;&lt;&gt;?&#x2F;%$#&#96;~&#x27;[]`,
+  description: `Marketito&quot;&lt;script&gt;alert(1)&lt;&#x2F;script&gt;
+${7**7}
+{{8**8}}
+{6**6}
+5**5
+&#x5C;&quot;&lt;h1&gt;Hi!&lt;&#x2F;h1&gt;`,
+  is_verified: false,
+});
