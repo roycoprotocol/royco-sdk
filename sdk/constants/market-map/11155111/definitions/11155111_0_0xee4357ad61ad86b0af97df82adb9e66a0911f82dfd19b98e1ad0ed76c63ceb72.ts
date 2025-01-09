4a657835905f0909b920d51e2f2dc23f9bd1e463
GitHub Action <action@github.com>
2025-01-09 23:15:00 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/11155111/definitions/11155111_0_0xee4357ad61ad86b0af97df82adb9e66a0911f82dfd19b98e1ad0ed76c63ceb72.ts
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xee4357ad61ad86b0af97df82adb9e66a0911f82dfd19b98e1ad0ed76c63ceb72.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `11155111_0_0xee4357ad61ad86b0af97df82adb9e66a0911f82dfd19b98e1ad0ed76c63ceb72`,
   name: `Marketito&quot;&lt;&gt;?&#x2F;%$#&#96;~&#x27;[]`,
   description: `Marketito&quot;&lt;script&gt;alert(1)&lt;&#x2F;script&gt;
-${7**7}
+${7 ** 7}
 {{8**8}}
 {6**6}
 5**5
