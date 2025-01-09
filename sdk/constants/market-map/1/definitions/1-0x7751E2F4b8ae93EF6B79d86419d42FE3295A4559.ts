afdf17017fcbed402cfdc03f2ecd727f5010d6ba
bharatvish <bharat@waymont.co>
2025-01-09 14:17:53 -0800

    Create 1-0x7751E2F4b8ae93EF6B79d86419d42FE3295A4559.ts

---
+++ sdk/constants/market-map/1/definitions/1-0x7751E2F4b8ae93EF6B79d86419d42FE3295A4559.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7751E2F4b8ae93EF6B79d86419d42FE3295A4559",
+  chain_id: 1,
+  contract_address: "0x7751E2F4b8ae93EF6B79d86419d42FE3295A4559",
+  name: "Wrapped USDL",
+  symbol: "wUSDL",
+  image: "https://etherscan.io/token/images/wusdl_32.png?=v3",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "32454",
+  type: "token",
+});
