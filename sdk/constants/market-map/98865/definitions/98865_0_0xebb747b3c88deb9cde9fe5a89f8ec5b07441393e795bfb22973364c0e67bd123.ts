3fc82044587cd55ecba8114b5ad71afab57c8b24
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-09 23:52:21 +0530

    feat(market-map): Update market -- 98865_0_0xebb747b3c88deb9cde9fe5a89f8ec5b07441393e795bfb22973364c0e67bd123 -- Recipe (0xebb7...d123)
    Market Details:
    - Chain ID: 98865
    - Market Type: 0 (Recipe)
    - Market ID: 0xebb747b3c88deb9cde9fe5a89f8ec5b07441393e795bfb22973364c0e67bd123 (Market Hash)
    - Request Timestamp: Thu, 09 Jan 2025 18:22:21 GMT

---
+++ sdk/constants/market-map/98865/definitions/98865_0_0xebb747b3c88deb9cde9fe5a89f8ec5b07441393e795bfb22973364c0e67bd123.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98865_0_0xebb747b3c88deb9cde9fe5a89f8ec5b07441393e795bfb22973364c0e67bd123`,
+  name: `Deposit pUSD in to Cultured`,
+  description: `Users receive platform fees`,
+  is_verified: false,
+});
