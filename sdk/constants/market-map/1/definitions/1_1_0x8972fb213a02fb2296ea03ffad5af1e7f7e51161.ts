024fff04a269cec5893776cc24ab85b37a756109
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-09 17:22:59 +0530

    feat(market-map): Update market -- 1_1_0x8972fb213a02fb2296ea03ffad5af1e7f7e51161 -- Vault (0x8972...1161) (#167)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0x8972fb213a02fb2296ea03ffad5af1e7f7e51161 (Wrapped Vault Address)
    - Request Timestamp: Thu, 09 Jan 2025 11:52:57 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0x8972fb213a02fb2296ea03ffad5af1e7f7e51161.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0x8972fb213a02fb2296ea03ffad5af1e7f7e51161`,
+  name: `Supply ETH to BakerFi`,
+  description: `Supply ETH to BakerFi&#x27;s Recipe &quot;Mille-feuille Classic&quot;, a recursive staking strategy using Lido and Aave as the underlying protocols.`,
+  is_verified: false,
+});
