5c97a1e6eb4278127bd9cc2db913942ad24c1f13
bharatvish <bharat@waymont.co>
2025-01-09 17:07:17 -0800

    Update 1_1_0x2407ccfff27a6c40f8c8926959ae56ca8d1c3edc.ts

--- sdk/constants/market-map/1/definitions/1_1_0x2407ccfff27a6c40f8c8926959ae56ca8d1c3edc.ts
+++ sdk/constants/market-map/1/definitions/1_1_0x2407ccfff27a6c40f8c8926959ae56ca8d1c3edc.ts
@@ -1,8 +1,8 @@
 import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
-  id: `1_1_0x2407ccfff27a6c40f8c8926959ae56ca8d1c3edc`,
-  name: `Supply wUSDL into csUSDL Vault`,
+  id: "1_1_0x2407ccfff27a6c40f8c8926959ae56ca8d1c3edc",
+  name: "Supply wUSDL into csUSDL Vault",
   description: `Supply wUSDL into csUSDL Vault`,
   is_verified: false,
 });
