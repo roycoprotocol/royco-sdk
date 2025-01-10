37c3254e0d282643626f5210b4e048e33deb7150
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-10 10:26:00 +0530

    feat(market-map): Update market -- 11155111_0_0x031ac731c8c5981d183222752c371b45bf0c2e42610d2af9d0a01f3676308145 -- Recipe (0x031a...8145) (#174)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x031ac731c8c5981d183222752c371b45bf0c2e42610d2af9d0a01f3676308145 (Market Hash)
    - Request Timestamp: Fri, 10 Jan 2025 04:55:58 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x031ac731c8c5981d183222752c371b45bf0c2e42610d2af9d0a01f3676308145.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x031ac731c8c5981d183222752c371b45bf0c2e42610d2af9d0a01f3676308145`,
+  name: `Takeover&#x2F;??`,
+  description: `Taasdasdasdsadasdasdasdasdasdasdasd`,
+  is_verified: false,
+});
