12325cc6c49d265a4eb3a34d3660c2062cc0f110
Corey <corey@dolomite.io>
2025-01-10 16:52:38 -0500

    added markets for testing

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0xcd3c96dd0a0ef94e6a389f88ce49ac0b23f0d551.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0xcd3c96dd0a0ef94e6a389f88ce49ac0b23f0d551",
+  chain_id: 11155111,
+  contract_address: "0xcd3c96dd0a0ef94e6a389f88ce49ac0b23f0d551",
+  name: "Cian Yield pumpBTC",
+  symbol: "ci-yield-pumpBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32272.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "32272",
+  type: "token",
+});
