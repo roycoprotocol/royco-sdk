331427dc034e65ca3a6315533c2027c083d69a05
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-13 02:59:43 +0530

    feat(market-map): Update market -- 11155111_0_0xc152a589c25eba66e227bf5af150dbe50e1d18df2ff92dc6ee6d5e8a9bca325c -- Recipe (0xc152...325c) (#190)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xc152a589c25eba66e227bf5af150dbe50e1d18df2ff92dc6ee6d5e8a9bca325c (Market Hash)
    - Request Timestamp: Sun, 12 Jan 2025 21:29:41 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xc152a589c25eba66e227bf5af150dbe50e1d18df2ff92dc6ee6d5e8a9bca325c.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xc152a589c25eba66e227bf5af150dbe50e1d18df2ff92dc6ee6d5e8a9bca325c`,
+  name: `Lend SBTC on Dolomite`,
+  description: `Just lend it`,
+  is_verified: false,
+});
