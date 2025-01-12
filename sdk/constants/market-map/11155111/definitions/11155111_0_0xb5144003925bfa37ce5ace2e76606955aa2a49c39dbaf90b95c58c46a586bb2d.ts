ea9c13d9952919abb46d6b8d1d92b5b9ed391dce
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-13 02:36:04 +0530

    feat(market-map): Update market -- 11155111_0_0xb5144003925bfa37ce5ace2e76606955aa2a49c39dbaf90b95c58c46a586bb2d -- Recipe (0xb514...bb2d) (#188)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xb5144003925bfa37ce5ace2e76606955aa2a49c39dbaf90b95c58c46a586bb2d (Market Hash)
    - Request Timestamp: Sun, 12 Jan 2025 21:06:02 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xb5144003925bfa37ce5ace2e76606955aa2a49c39dbaf90b95c58c46a586bb2d.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xb5144003925bfa37ce5ace2e76606955aa2a49c39dbaf90b95c58c46a586bb2d`,
+  name: `Lend yl-pumpBTC on Dolomite`,
+  description: `Just lend it`,
+  is_verified: false,
+});
