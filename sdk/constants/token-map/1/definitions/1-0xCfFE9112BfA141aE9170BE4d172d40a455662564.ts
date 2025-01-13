864b46754b009a0c171086a402d12bf4d52d0d24
frontier159 <103474701+frontier159@users.noreply.github.com>
2025-01-14 09:22:13 +1100

    Origami bKAMI points

---
+++ sdk/constants/token-map/1/definitions/1-0xCfFE9112BfA141aE9170BE4d172d40a455662564.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xCfFE9112BfA141aE9170BE4d172d40a455662564",
+  chain_id: 1,
+  contract_address: "0xCfFE9112BfA141aE9170BE4d172d40a455662564",
+  name: "Origami bKAMI",
+  symbol: "bKAMI",
+  image: "https://origami.finance/light/icons/kami.svg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
