48957a5ab84f1718a76b373c7a467d032153069c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-13 18:21:01 +0530

    feat(market-map): Update market -- 98865_0_0x897854e0338bda707d52c57d234d3ad265e7337a9d64938be266d9f0074cbb3b -- Recipe (0x8978...bb3b) (#195)
    Market Details:
    - Chain ID: 98865
    - Market Type: 0 (Recipe)
    - Market ID: 0x897854e0338bda707d52c57d234d3ad265e7337a9d64938be266d9f0074cbb3b (Market Hash)
    - Request Timestamp: Mon, 13 Jan 2025 12:50:59 GMT

---
+++ sdk/constants/market-map/98865/definitions/98865_0_0x897854e0338bda707d52c57d234d3ad265e7337a9d64938be266d9f0074cbb3b.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98865_0_0x897854e0338bda707d52c57d234d3ad265e7337a9d64938be266d9f0074cbb3b`,
+  name: `Stake on pETH`,
+  description: `When WETH is supplied to the market, it is deposited for Plume Staked Ether (pETH) for 1 month. Users may forfeit and withdraw for pETH at any time.`,
+  is_verified: false,
+});
