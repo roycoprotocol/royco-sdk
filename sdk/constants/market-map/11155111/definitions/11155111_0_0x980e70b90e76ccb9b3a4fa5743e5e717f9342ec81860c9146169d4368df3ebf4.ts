364c58e77d5f0538355a010675c5b117045d74da
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-14 12:00:08 +0530

    feat(market-map): Update market -- 11155111_0_0x980e70b90e76ccb9b3a4fa5743e5e717f9342ec81860c9146169d4368df3ebf4 -- Recipe (0x980e...ebf4) (#200)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x980e70b90e76ccb9b3a4fa5743e5e717f9342ec81860c9146169d4368df3ebf4 (Market Hash)
    - Request Timestamp: Tue, 14 Jan 2025 06:30:06 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x980e70b90e76ccb9b3a4fa5743e5e717f9342ec81860c9146169d4368df3ebf4.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x980e70b90e76ccb9b3a4fa5743e5e717f9342ec81860c9146169d4368df3ebf4`,
+  name: `Boyco Kodiak USDC-HONEY (correct one)`,
+  description: `Deposit single-sided USDC -&gt; get usdc-honey lp on bera`,
+  is_verified: false,
+});
