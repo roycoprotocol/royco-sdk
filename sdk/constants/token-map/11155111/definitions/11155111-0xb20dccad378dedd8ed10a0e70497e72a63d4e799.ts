3554a2e602c97a91a5e8969e5017c2a949d0e113
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-01-14 12:17:22 -0800

    added stgUSDC/USDT on sepolia

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0xb20dccad378dedd8ed10a0e70497e72a63d4e799.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+    id: "11155111-0xb20dccad378dedd8ed10a0e70497e72a63d4e799",
+    chain_id: 11155111,
+    contract_address: "0xb20dccad378dedd8ed10a0e70497e72a63d4e799",
+    name: "UNIV2-stgUSDC/USDT",
+    symbol: "stgUSDC/USDT",
+    image: "https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png",
+    decimals: 18,
+    source: "external",
+    search_id: "none",
+    type: "token",
+});
