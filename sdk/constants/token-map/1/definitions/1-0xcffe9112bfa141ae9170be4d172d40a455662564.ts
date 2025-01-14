bc4a6503acb5b257082a87c159c60146014dbf54
GitHub Action <action@github.com>
2025-01-14 01:07:16 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/1/definitions/1-0xcffe9112bfa141ae9170be4d172d40a455662564.ts
+++ sdk/constants/token-map/1/definitions/1-0xcffe9112bfa141ae9170be4d172d40a455662564.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "1-0xCfFE9112BfA141aE9170BE4d172d40a455662564",
+  id: "1-0xcffe9112bfa141ae9170be4d172d40a455662564",
   chain_id: 1,
-  contract_address: "0xCfFE9112BfA141aE9170BE4d172d40a455662564",
+  contract_address: "0xcffe9112bfa141ae9170be4d172d40a455662564",
   name: "Origami bKAMI",
   symbol: "bKAMI",
   image: "https://origami.finance/light/icons/kami.svg",
--- sdk/constants/token-map/1/index.ts
+++ sdk/constants/token-map/1/index.ts
@@ -761,6 +761,7 @@ import token_1_0x9f284e1337a815fe77d2ff4ae46544645b20c5ff from "./definitions/1-
 import token_1_0x9f5f3cfd7a32700c93f971637407ff17b91c7342 from "./definitions/1-0x9f5f3cfd7a32700c93f971637407ff17b91c7342";
 import token_1_0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2 from "./definitions/1-0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2";
 import token_1_0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08 from "./definitions/1-0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08";
+import token_1_0xcffe9112bfa141ae9170be4d172d40a455662564 from "./definitions/1-0xcffe9112bfa141ae9170be4d172d40a455662564";
 import token_1_0xa01199c61841fce3b3dafb83fefc1899715c8756 from "./definitions/1-0xa01199c61841fce3b3dafb83fefc1899715c8756";
 import token_1_0xa0246c9032bc3a600820415ae600c6388619a14d from "./definitions/1-0xa0246c9032bc3a600820415ae600c6388619a14d";
 import token_1_0xa0385e7283c83e2871e9af49eec0966088421ddd from "./definitions/1-0xa0385e7283c83e2871e9af49eec0966088421ddd";
@@ -2720,6 +2721,8 @@ export const TokenMap1 = {
     token_1_0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2,
   [token_1_0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08.id]:
     token_1_0x9fd9278f04f01c6a39a9d1c1cd79f7782c6ade08,
+  [token_1_0xcffe9112bfa141ae9170be4d172d40a455662564.id]:
+    token_1_0xcffe9112bfa141ae9170be4d172d40a455662564,
   [token_1_0xa01199c61841fce3b3dafb83fefc1899715c8756.id]:
     token_1_0xa01199c61841fce3b3dafb83fefc1899715c8756,
   [token_1_0xa0246c9032bc3a600820415ae600c6388619a14d.id]:
