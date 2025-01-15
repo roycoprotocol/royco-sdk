54911c8eb8cc1df67305fa9ac6963715a8aaf620
githubotoro <yupuday.git@gmail.com>
2025-01-15 11:58:52 -0800

    feat(constants):  moyco -- if you know, you know :)

--- sdk/constants/token-map/11155111/definitions/11155111-0xfff9976782d46cc05630d1f6ebab18b2324d6b14.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0xfff9976782d46cc05630d1f6ebab18b2324d6b14.ts
@@ -5,7 +5,7 @@ export default defineToken({
   chain_id: 11155111,
   contract_address: "0xfff9976782d46cc05630d1f6ebab18b2324d6b14",
   name: "Wrapped Ether",
-  symbol: "WETH9",
+  symbol: "WETH",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png",
   decimals: 18,
   source: "coinmarketcap",
