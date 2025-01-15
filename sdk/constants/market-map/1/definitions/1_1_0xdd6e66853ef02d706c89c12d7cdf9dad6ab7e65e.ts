b138c137421611ca96b9394c3c60df9fd4b379e3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-16 03:20:06 +0530

    feat(market-map): Update market -- 1_1_0xdd6e66853ef02d706c89c12d7cdf9dad6ab7e65e -- Vault (0xdd6e...e65e)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0xdd6e66853ef02d706c89c12d7cdf9dad6ab7e65e (Wrapped Vault Address)
    - Request Timestamp: Wed, 15 Jan 2025 21:50:05 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0xdd6e66853ef02d706c89c12d7cdf9dad6ab7e65e.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0xdd6e66853ef02d706c89c12d7cdf9dad6ab7e65e`,
+  name: `Supply USDC on Panoptic`,
+  description: `Panoptic is a perpetual options protocol on Ethereum, where users can actively trade options on any token available on Uniswap. Panoptic also has a money market component, where passive liquidity providers (PLPs) can lend their tokens to active traders. This campaign will incentivize the lending of USDC to active traders in the ETH-USDC-30bps market. The native yield from that lending activity is currently &gt;20% APY. In addition to this native yield, PLPs will receive pips points for their deposits. The pips points will be distributed on an ongoing basis and the campaign will remain active for at least 3 months.`,
+  is_verified: false,
+});
