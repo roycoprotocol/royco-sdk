1c92ec617bf4552721d8e5376620defb1afb1ec4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-16 12:23:20 +0530

    feat(market-map): Update market -- 11155111_0_0x15dce2565cef3fe3ed29dca20a94016d7cf6a7634f3ea8d3f49f18387dd3c976 -- Recipe (0x15dc...c976) (#212)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x15dce2565cef3fe3ed29dca20a94016d7cf6a7634f3ea8d3f49f18387dd3c976 (Market Hash)
    - Request Timestamp: Thu, 16 Jan 2025 06:53:18 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x15dce2565cef3fe3ed29dca20a94016d7cf6a7634f3ea8d3f49f18387dd3c976.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x15dce2565cef3fe3ed29dca20a94016d7cf6a7634f3ea8d3f49f18387dd3c976`,
+  name: `BOYCO — Predeposit in SatLayer pumpBTC`,
+  description: `A new market that deposits pumpBTC`,
+  is_verified: false,
+});
