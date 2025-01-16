706a4488b00762bb60f4d9117c1e826b281a4b42
bharatvish <bharat@waymont.co>
2025-01-16 17:43:16 -0600

    Update 8453_0_0x18493e771a4fd1cc17f01ec9f12cc3058bd4e30fda4affdd3e2c11ec6a557c8c.ts

--- sdk/constants/market-map/8453/definitions/8453_0_0x18493e771a4fd1cc17f01ec9f12cc3058bd4e30fda4affdd3e2c11ec6a557c8c.ts
+++ sdk/constants/market-map/8453/definitions/8453_0_0x18493e771a4fd1cc17f01ec9f12cc3058bd4e30fda4affdd3e2c11ec6a557c8c.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: `8453_0_0x18493e771a4fd1cc17f01ec9f12cc3058bd4e30fda4affdd3e2c11ec6a557c8c`,
   name: `Deposit USDC into SuperUSDC for 10 weeks`,
   description: `Deposit USDC into Superform&#x27;s SuperUSDC on Base for 3 months. SuperUSDC optimizes across blue-chip lending yield on Base to earn the best returns.`,
-  is_verified: false,
+  is_verified: true,
 });
