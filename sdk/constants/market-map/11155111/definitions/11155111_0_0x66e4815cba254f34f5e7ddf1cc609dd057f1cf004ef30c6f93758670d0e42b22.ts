c12d5d68a76ff28906a0db829ee1a017a662be79
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-16 11:44:50 +0530

    feat(market-map): Update market -- 11155111_0_0x66e4815cba254f34f5e7ddf1cc609dd057f1cf004ef30c6f93758670d0e42b22 -- Recipe (0x66e4...2b22) (#211)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x66e4815cba254f34f5e7ddf1cc609dd057f1cf004ef30c6f93758670d0e42b22 (Market Hash)
    - Request Timestamp: Thu, 16 Jan 2025 06:14:48 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x66e4815cba254f34f5e7ddf1cc609dd057f1cf004ef30c6f93758670d0e42b22.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x66e4815cba254f34f5e7ddf1cc609dd057f1cf004ef30c6f93758670d0e42b22`,
+  name: `BOYCO — Predeposit in CIAN pumpBTC`,
+  description: `BOYCO — Predeposit in CIAN pumpBTC`,
+  is_verified: false,
+});
