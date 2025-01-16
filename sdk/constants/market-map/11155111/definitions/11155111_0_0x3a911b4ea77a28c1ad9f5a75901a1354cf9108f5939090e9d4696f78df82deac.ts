4b3b1f3a382025ef1ca5a79871435a62b1b0215c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-16 12:50:09 +0530

    feat(market-map): Update market -- 11155111_0_0x3a911b4ea77a28c1ad9f5a75901a1354cf9108f5939090e9d4696f78df82deac -- Recipe (0x3a91...deac)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x3a911b4ea77a28c1ad9f5a75901a1354cf9108f5939090e9d4696f78df82deac (Market Hash)
    - Request Timestamp: Thu, 16 Jan 2025 07:20:08 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x3a911b4ea77a28c1ad9f5a75901a1354cf9108f5939090e9d4696f78df82deac.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x3a911b4ea77a28c1ad9f5a75901a1354cf9108f5939090e9d4696f78df82deac`,
+  name: `BOYCO — Predeposit in BurrBear USDC-NECT-HONEY`,
+  description: `When USDC is deposited into the market it is LPed into the USDC&#x2F;HONEY&#x2F;NECT Burr pool.`,
+  is_verified: false,
+});
