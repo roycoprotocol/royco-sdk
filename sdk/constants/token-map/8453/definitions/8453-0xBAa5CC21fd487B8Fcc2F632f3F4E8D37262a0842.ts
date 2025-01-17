5b8d480f66545edbb0938c3dde55d2dd0e23e0c0
croutypuff <141670898+croutypuff@users.noreply.github.com>
2025-01-17 11:37:12 -0800

    Create 8453-0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842.ts

---
+++ sdk/constants/token-map/8453/definitions/8453-0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842",
+  chain_id: 1,
+  contract_address: "0xBAa5CC21fd487B8Fcc2F632f3F4E8D37262a0842",
+  name: "Morpho",
+  symbol: "MORPHO",
+  image:
+    "https://assets.coingecko.com/coins/images/29837/standard/Morpho-token-icon.png?1726771230",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "morpho",
+  type: "token",
+});
