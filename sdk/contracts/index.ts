16aa048c4123b2524591302fbe78c0b029e38903
githubotoro <yupuday.git@gmail.com>
2025-01-17 15:56:19 -0800

    feat(contracts): sonic integration

--- sdk/contracts/index.ts
+++ sdk/contracts/index.ts
@@ -4,6 +4,7 @@ import { Contracts as Contracts_8453 } from "./8453";
 import { Contracts as Contracts_42161 } from "./42161";
 import { Contracts as Contracts_21000000 } from "./21000000";
 import { Contracts as Contracts_98865 } from "./98865";
+import { Contracts as Contracts_146 } from "./146";
 
 export const ContractMap = {
   11155111: Contracts_11155111,
@@ -12,6 +13,7 @@ export const ContractMap = {
   8453: Contracts_8453,
   21000000: Contracts_21000000,
   98865: Contracts_98865,
+  146: Contracts_146,
 };
 
 export * from "./utils";
