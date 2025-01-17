d6305339f2da0034a22973deb64e95ea9a07caf1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-17 15:59:58 -0800

    feat(token-map): Update Token -- 146-0x486b6fa0419b33a0c7a6e4698c231d7e2f2d5299 -- Moon Bay (MOON) (0x486b...5299)
    Token Details:
    - Id: 146-0x486b6fa0419b33a0c7a6e4698c231d7e2f2d5299
    - Chain Id: 146
    - Contract Address: 0x486b6fa0419b33a0c7a6e4698c231d7e2f2d5299
    - Name: Moon Bay
    - Symbol: MOON
    - Image: https://coin-images.coingecko.com/coins/images/53021/large/moonbay.png?1735003620
    - Decimals: 18
    - Source: coingecko
    - Search Id: moon-bay-2

---
+++ sdk/constants/token-map/146/definitions/146-0x486b6fa0419b33a0c7a6e4698c231d7e2f2d5299.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x486b6fa0419b33a0c7a6e4698c231d7e2f2d5299",
+  chain_id: 146,
+  contract_address: "0x486b6fa0419b33a0c7a6e4698c231d7e2f2d5299",
+  name: "Moon Bay",
+  symbol: "MOON",
+  image: "https://coin-images.coingecko.com/coins/images/53021/large/moonbay.png?1735003620",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "moon-bay-2",
+  type: "token",
+});
