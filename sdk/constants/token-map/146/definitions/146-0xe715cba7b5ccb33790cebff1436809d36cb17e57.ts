68e7cf1fd6dc7be073738223714b6cc1d1f4922d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-17 15:59:28 -0800

    feat(token-map): Update Token -- 146-0xe715cba7b5ccb33790cebff1436809d36cb17e57 -- EURC (EURC) (0xe715...7e57)
    Token Details:
    - Id: 146-0xe715cba7b5ccb33790cebff1436809d36cb17e57
    - Chain Id: 146
    - Contract Address: 0xe715cba7b5ccb33790cebff1436809d36cb17e57
    - Name: EURC
    - Symbol: EURC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/20641.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 20641

---
+++ sdk/constants/token-map/146/definitions/146-0xe715cba7b5ccb33790cebff1436809d36cb17e57.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xe715cba7b5ccb33790cebff1436809d36cb17e57",
+  chain_id: 146,
+  contract_address: "0xe715cba7b5ccb33790cebff1436809d36cb17e57",
+  name: "EURC",
+  symbol: "EURC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/20641.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "20641",
+  type: "token",
+});
