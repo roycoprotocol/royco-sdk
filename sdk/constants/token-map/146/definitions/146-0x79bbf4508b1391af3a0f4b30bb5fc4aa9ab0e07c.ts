745c5fcc1f25624bc637a364ad31cb8e42fa3d4d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-17 15:57:42 -0800

    feat(token-map): Update Token -- 146-0x79bbf4508b1391af3a0f4b30bb5fc4aa9ab0e07c -- Hey Anon (ANON) (0x79bb...e07c)
    Token Details:
    - Id: 146-0x79bbf4508b1391af3a0f4b30bb5fc4aa9ab0e07c
    - Chain Id: 146
    - Contract Address: 0x79bbf4508b1391af3a0f4b30bb5fc4aa9ab0e07c
    - Name: Hey Anon
    - Symbol: ANON
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/35092.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 35092

---
+++ sdk/constants/token-map/146/definitions/146-0x79bbf4508b1391af3a0f4b30bb5fc4aa9ab0e07c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x79bbf4508b1391af3a0f4b30bb5fc4aa9ab0e07c",
+  chain_id: 146,
+  contract_address: "0x79bbf4508b1391af3a0f4b30bb5fc4aa9ab0e07c",
+  name: "Hey Anon",
+  symbol: "ANON",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/35092.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "35092",
+  type: "token",
+});
