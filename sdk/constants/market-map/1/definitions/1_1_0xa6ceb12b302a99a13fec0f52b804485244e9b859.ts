3332430eadbf1d1f9159802cf0990041ea49ec36
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-17 04:49:22 -0800

    feat(market-map): Update market -- 1_1_0xa6ceb12b302a99a13fec0f52b804485244e9b859 -- Vault (0xa6ce...b859) (#222)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0xa6ceb12b302a99a13fec0f52b804485244e9b859 (Wrapped Vault Address)
    - Request Timestamp: Fri, 17 Jan 2025 12:49:19 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0xa6ceb12b302a99a13fec0f52b804485244e9b859.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0xa6ceb12b302a99a13fec0f52b804485244e9b859`,
+  name: `ETH MAX Vault - YieldNest`,
+  description: `This vault will give you MAX ETH rewards by merging DeFi&#x27;s best strategies into a single, unified, high-powered asset ynETHx with L1 settlement assurances. Start receiving continuous &amp; compounding yield with ease.`,
+  is_verified: false,
+});
