08b993c41b50e6a6cbf8264ff0eba4bd3e0a8ec7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-17 16:00:08 -0800

    feat(token-map): Update Token -- 146-0xbc0d0650412ef353d672c0bbd12efff90591b251 -- FutureStarter (FS) (0xbc0d...b251)
    Token Details:
    - Id: 146-0xbc0d0650412ef353d672c0bbd12efff90591b251
    - Chain Id: 146
    - Contract Address: 0xbc0d0650412ef353d672c0bbd12efff90591b251
    - Name: FutureStarter
    - Symbol: FS
    - Image: https://coin-images.coingecko.com/coins/images/19821/large/13237.png?1696519240
    - Decimals: 18
    - Source: coingecko
    - Search Id: fantomstarter

---
+++ sdk/constants/token-map/146/definitions/146-0xbc0d0650412ef353d672c0bbd12efff90591b251.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xbc0d0650412ef353d672c0bbd12efff90591b251",
+  chain_id: 146,
+  contract_address: "0xbc0d0650412ef353d672c0bbd12efff90591b251",
+  name: "FutureStarter",
+  symbol: "FS",
+  image: "https://coin-images.coingecko.com/coins/images/19821/large/13237.png?1696519240",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "fantomstarter",
+  type: "token",
+});
