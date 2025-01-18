327e1076fc6d01ca6de559f697d48ed3ff710868
GitHub Action <action@github.com>
2025-01-18 02:16:27 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/8453/definitions/8453-0xbaa5cc21fd487b8fcc2f632f3f4e8d37262a0842.ts
+++ sdk/constants/token-map/8453/definitions/8453-0xbaa5cc21fd487b8fcc2f632f3f4e8d37262a0842.ts
@@ -6,7 +6,8 @@ export default defineToken({
   contract_address: "0xbaa5cc21fd487b8fcc2f632f3f4e8d37262a0842",
   name: "Morpho",
   symbol: "MORPHO",
-  image: "https://coin-images.coingecko.com/coins/images/29837/large/Morpho-token-icon.png?1726771230",
+  image:
+    "https://coin-images.coingecko.com/coins/images/29837/large/Morpho-token-icon.png?1726771230",
   decimals: 18,
   source: "coingecko",
   search_id: "morpho",
