b881f06cc33861cdfa9ae0313cb998afbeba7449
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-17 16:00:55 -0800

    feat(token-map): Update Token -- 146-0x50c42deacd8fc9773493ed674b675be577f2634b -- Bridged Wrapped Ether (Sonic) (WETH) (0x50c4...634b)
    Token Details:
    - Id: 146-0x50c42deacd8fc9773493ed674b675be577f2634b
    - Chain Id: 146
    - Contract Address: 0x50c42deacd8fc9773493ed674b675be577f2634b
    - Name: Bridged Wrapped Ether (Sonic)
    - Symbol: WETH
    - Image: https://coin-images.coingecko.com/coins/images/53332/large/weth_2.jpg?1736149977
    - Decimals: 18
    - Source: coingecko
    - Search Id: bridged-wrapped-ether-sonic

---
+++ sdk/constants/token-map/146/definitions/146-0x50c42deacd8fc9773493ed674b675be577f2634b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x50c42deacd8fc9773493ed674b675be577f2634b",
+  chain_id: 146,
+  contract_address: "0x50c42deacd8fc9773493ed674b675be577f2634b",
+  name: "Bridged Wrapped Ether (Sonic)",
+  symbol: "WETH",
+  image: "https://coin-images.coingecko.com/coins/images/53332/large/weth_2.jpg?1736149977",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "bridged-wrapped-ether-sonic",
+  type: "token",
+});
