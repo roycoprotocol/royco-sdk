33e651eba37ac0364934a5c71825a9d8aa510bf2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-17 16:00:27 -0800

    feat(token-map): Update Token -- 146-0x05e31a691405d06708a355c029599c12d5da8b28 -- Fantomsonicinu (FSONIC) (0x05e3...8b28)
    Token Details:
    - Id: 146-0x05e31a691405d06708a355c029599c12d5da8b28
    - Chain Id: 146
    - Contract Address: 0x05e31a691405d06708a355c029599c12d5da8b28
    - Name: Fantomsonicinu
    - Symbol: FSONIC
    - Image: https://coin-images.coingecko.com/coins/images/53278/large/VaSB8Eil_400x400.jpg?1735953637
    - Decimals: 18
    - Source: coingecko
    - Search Id: fantomsonicinu-2

---
+++ sdk/constants/token-map/146/definitions/146-0x05e31a691405d06708a355c029599c12d5da8b28.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x05e31a691405d06708a355c029599c12d5da8b28",
+  chain_id: 146,
+  contract_address: "0x05e31a691405d06708a355c029599c12d5da8b28",
+  name: "Fantomsonicinu",
+  symbol: "FSONIC",
+  image: "https://coin-images.coingecko.com/coins/images/53278/large/VaSB8Eil_400x400.jpg?1735953637",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "fantomsonicinu-2",
+  type: "token",
+});
