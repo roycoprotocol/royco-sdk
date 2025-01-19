c08ff1ce66329f8c69ccd4b8204134246f4f7810
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-19 13:30:38 -0800

    feat(market-map): Update market -- 11155111_0_0x2782ea83adf8787682b4aba8a51225c53d6e9adceed465171343def79f5a50fb -- Recipe (0x2782...50fb) (#236)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x2782ea83adf8787682b4aba8a51225c53d6e9adceed465171343def79f5a50fb (Market Hash)
    - Request Timestamp: Sun, 19 Jan 2025 21:30:36 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x2782ea83adf8787682b4aba8a51225c53d6e9adceed465171343def79f5a50fb.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x2782ea83adf8787682b4aba8a51225c53d6e9adceed465171343def79f5a50fb`,
+  name: `BOYCO — Predeposit in Kodiak WETH-Honey`,
+  description: `BOYCO — Predeposit in Kodiak WETH-Honey`,
+  is_verified: false,
+});
