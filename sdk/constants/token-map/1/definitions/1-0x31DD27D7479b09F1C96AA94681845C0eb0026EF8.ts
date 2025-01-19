2d9b2275b217e0a22a361d6a06f089cd831aa0af
berastotle <berastotle@kodiak.finance>
2025-01-19 21:06:06 +0400

    add Kodiak Boyco Points

---
+++ sdk/constants/token-map/1/definitions/1-0x31DD27D7479b09F1C96AA94681845C0eb0026EF8.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x31DD27D7479b09F1C96AA94681845C0eb0026EF8",
+  chain_id: 1,
+  contract_address: "0x31DD27D7479b09F1C96AA94681845C0eb0026EF8",
+  name: "Kodiak Boyco Points",
+  symbol: "BODIAK",
+  image: "https://app.kodiak.finance/images/KodiakToken.svg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
+
