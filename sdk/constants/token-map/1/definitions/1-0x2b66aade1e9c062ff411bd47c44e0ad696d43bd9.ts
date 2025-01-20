bedace617596f96259f5469867145a88c96c3978
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 11:13:53 -0800

    feat(token-map): Update Token -- 1-0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9 -- sUSDa (SUSDA) (0x2b66...3bd9)
    Token Details:
    - Id: 1-0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9
    - Chain Id: 1
    - Contract Address: 0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9
    - Name: sUSDa
    - Symbol: SUSDA
    - Image: https://coin-images.coingecko.com/coins/images/51821/large/USDA.png?1732035172
    - Decimals: 18
    - Source: coingecko
    - Search Id: susda

---
+++ sdk/constants/token-map/1/definitions/1-0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9",
+  chain_id: 1,
+  contract_address: "0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9",
+  name: "sUSDa",
+  symbol: "SUSDA",
+  image: "https://coin-images.coingecko.com/coins/images/51821/large/USDA.png?1732035172",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "susda",
+  type: "token",
+});
