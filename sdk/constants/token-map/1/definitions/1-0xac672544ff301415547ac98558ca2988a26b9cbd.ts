0027f83a8f73b5e70c0488e4817199e7278293cc
shu <shu@burr>
2025-01-20 09:26:19 +0700

    🐻 BurrBear points contract

---
+++ sdk/constants/token-map/1/definitions/1-0xac672544ff301415547ac98558ca2988a26b9cbd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xac672544ff301415547ac98558ca2988a26b9cbd",
+  chain_id: 1,
+  contract_address: "0xac672544ff301415547ac98558ca2988a26b9cbd",
+  name: "BURR",
+  symbol: "BURR",
+  image: "https://app.burrbear.io/images/logos/burr_64_64.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
