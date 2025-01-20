1122c9acae3d2b907f936ab4131d3565fd7b347a
0xgeeb <0xgeeb@gmail.com>
2025-01-20 15:20:29 -0500

    add Goldilocks points contract

---
+++ sdk/constants/token-map/1/definitions/1-0x3b7795688eA8c095600BAe9D6D866D04c230Ba16.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3b7795688eA8c095600BAe9D6D866D04c230Ba16",
+  chain_id: 1,
+  contract_address: "0x3b7795688eA8c095600BAe9D6D866D04c230Ba16",
+  name: "Goldilocks PRG",
+  symbol: "PRG",
+  image: "https://www.goldilocksdao.io/images/big-porridge-logo.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
