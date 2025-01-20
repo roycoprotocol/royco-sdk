21dfc496807cd52ac8ca12b222d49739436f1dd9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 11:18:44 -0800

    feat(token-map): Update Token -- 1-0xc96de26018a54d51c097160568752c4e3bd6c364 -- Ignition (FBTC) (0xc96d...c364)
    Token Details:
    - Id: 1-0xc96de26018a54d51c097160568752c4e3bd6c364
    - Chain Id: 1
    - Contract Address: 0xc96de26018a54d51c097160568752c4e3bd6c364
    - Name: Ignition
    - Symbol: FBTC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/32306.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 32306

---
+++ sdk/constants/token-map/1/definitions/1-0xc96de26018a54d51c097160568752c4e3bd6c364.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xc96de26018a54d51c097160568752c4e3bd6c364",
+  chain_id: 1,
+  contract_address: "0xc96de26018a54d51c097160568752c4e3bd6c364",
+  name: "Ignition",
+  symbol: "FBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32306.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "32306",
+  type: "token",
+});
