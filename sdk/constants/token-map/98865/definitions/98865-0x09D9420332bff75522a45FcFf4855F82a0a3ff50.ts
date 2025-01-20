42fc4c98c539867f42e0e93dcaf24d86117e960a
bharatvish <bharat@waymont.co>
2025-01-20 08:33:08 -0800

    Create 98865-0x09D9420332bff75522a45FcFf4855F82a0a3ff50.ts

---
+++ sdk/constants/token-map/98865/definitions/98865-0x09D9420332bff75522a45FcFf4855F82a0a3ff50.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98865-0x09D9420332bff75522a45FcFf4855F82a0a3ff50",
+  chain_id: 98865,
+  contract_address: "0x09D9420332bff75522a45FcFf4855F82a0a3ff50",
+  name: "Dinero",
+  symbol: "DINERO",
+  image: "https://assets.coingecko.com/coins/images/39341/standard/dinero.jpg?1721807902",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "32492",
+  type: "token",
+});
