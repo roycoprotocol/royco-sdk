4358c30ac98cfecd80b81a19b3dccbae2e46472c
VM <arimas@foxmail.com>
2025-01-20 15:33:23 +0800

    fix: add sbtc token

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0x6bb8f9606c147f22e2a541ea93d6bad768db69a9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0x6BB8f9606c147F22e2A541eA93D6bad768DB69a9",
+  chain_id: 11155111,
+  contract_address: "0x6BB8f9606c147F22e2A541eA93D6bad768DB69a9",
+  name: "Stone Bitcoin",
+  symbol: "SBTC",
+  image: "https://icons.llamao.fi/icons/protocols/stonebtc",
+  decimals: 8,
+  source: "external",
+  search_id: "none",
+  type: "token",
+});
