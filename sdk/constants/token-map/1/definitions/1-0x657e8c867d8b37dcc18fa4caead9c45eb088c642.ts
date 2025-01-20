5d1693968c0a3aea8c217a7d510ae64d8125fe88
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 12:06:21 -0800

    feat(token-map): Update Token -- 1-0x657e8c867d8b37dcc18fa4caead9c45eb088c642 -- ether.fi Staked BTC (EBTC) (0x657e...c642)
    Token Details:
    - Id: 1-0x657e8c867d8b37dcc18fa4caead9c45eb088c642
    - Chain Id: 1
    - Contract Address: 0x657e8c867d8b37dcc18fa4caead9c45eb088c642
    - Name: ether.fi Staked BTC
    - Symbol: EBTC
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/33696.png
    - Decimals: 8
    - Source: coinmarketcap
    - Search Id: 33696

---
+++ sdk/constants/token-map/1/definitions/1-0x657e8c867d8b37dcc18fa4caead9c45eb088c642.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x657e8c867d8b37dcc18fa4caead9c45eb088c642",
+  chain_id: 1,
+  contract_address: "0x657e8c867d8b37dcc18fa4caead9c45eb088c642",
+  name: "ether.fi Staked BTC",
+  symbol: "EBTC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/33696.png",
+  decimals: 8,
+  source: "coinmarketcap",
+  search_id: "33696",
+  type: "token",
+});
