be7a88bbbefb89de8ad1173a6d1d887eb8b0e2e0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 11:10:58 -0800

    feat(token-map): Update Token -- 1-0x7122985656e38bdc0302db86685bb972b145bd3c -- StakeStone ETH (STONE) (0x7122...bd3c)
    Token Details:
    - Id: 1-0x7122985656e38bdc0302db86685bb972b145bd3c
    - Chain Id: 1
    - Contract Address: 0x7122985656e38bdc0302db86685bb972b145bd3c
    - Name: StakeStone ETH
    - Symbol: STONE
    - Image: https://coin-images.coingecko.com/coins/images/33103/large/200_200.png?1702602672
    - Decimals: 18
    - Source: coingecko
    - Search Id: stakestone-ether

---
+++ sdk/constants/token-map/1/definitions/1-0x7122985656e38bdc0302db86685bb972b145bd3c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x7122985656e38bdc0302db86685bb972b145bd3c",
+  chain_id: 1,
+  contract_address: "0x7122985656e38bdc0302db86685bb972b145bd3c",
+  name: "StakeStone ETH",
+  symbol: "STONE",
+  image: "https://coin-images.coingecko.com/coins/images/33103/large/200_200.png?1702602672",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "stakestone-ether",
+  type: "token",
+});
