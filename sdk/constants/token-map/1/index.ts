e215c2c7574fab8ab5dfcb09cf09a0a115ede2a4
0xNooodle <0xNoodle@proton.me>
2025-01-20 14:11:33 +0100

    Add Dahlia Points contract

--- sdk/constants/token-map/1/index.ts
+++ sdk/constants/token-map/1/index.ts
@@ -1195,6 +1195,7 @@ import token_1_0xff836a5821e69066c87e268bc51b849fab94240c from "./definitions/1-
 import token_1_0xff8c479134a18918059493243943150776cf8cf2 from "./definitions/1-0xff8c479134a18918059493243943150776cf8cf2";
 import token_1_0xffc63b9146967a1ba33066fb057ee3722221acf0 from "./definitions/1-0xffc63b9146967a1ba33066fb057ee3722221acf0";
 import token_1_0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265 from "./definitions/1-0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265";
+import token_1_0xbd1e5b7fa18f2679070c8ba9ab6415ef786720cc from "./definitions/1-0xbd1e5b7fa18f2679070c8ba9ab6415ef786720cc";
 
 export const TokenMap1 = {
   [token_1_0x0000000000085d4780b73119b644ae5ecd22b376.id]:
@@ -3591,4 +3592,6 @@ export const TokenMap1 = {
     token_1_0xffc63b9146967a1ba33066fb057ee3722221acf0,
   [token_1_0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265.id]:
     token_1_0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265,
+  [token_1_0xbd1e5b7fa18f2679070c8ba9ab6415ef786720cc.id]:
+  token_1_0xbd1e5b7fa18f2679070c8ba9ab6415ef786720cc,
 };
