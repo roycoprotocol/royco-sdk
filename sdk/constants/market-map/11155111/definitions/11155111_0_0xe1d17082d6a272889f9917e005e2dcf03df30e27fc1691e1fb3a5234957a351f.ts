c9db59a4f4979ee6cb1a6bf46e164f868908d4ae
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-19 23:40:08 -0800

    feat(market-map): Update market -- 11155111_0_0xe1d17082d6a272889f9917e005e2dcf03df30e27fc1691e1fb3a5234957a351f -- Recipe (0xe1d1...351f)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xe1d17082d6a272889f9917e005e2dcf03df30e27fc1691e1fb3a5234957a351f (Market Hash)
    - Request Timestamp: Mon, 20 Jan 2025 07:40:08 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xe1d17082d6a272889f9917e005e2dcf03df30e27fc1691e1fb3a5234957a351f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xe1d17082d6a272889f9917e005e2dcf03df30e27fc1691e1fb3a5234957a351f`,
+  name: `BOYCO — Predeposit in SatLayer SBTC`,
+  description: `A market that deposits SBTC.`,
+  is_verified: false,
+});
