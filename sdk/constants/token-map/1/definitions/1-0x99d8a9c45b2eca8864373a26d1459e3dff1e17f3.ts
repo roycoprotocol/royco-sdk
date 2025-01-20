e9358d3c3f3843e379de3003ab12a600726c15bb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 11:20:33 -0800

    feat(token-map): Update Token -- 1-0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3 -- Magic Internet Money (MIM) (0x99d8...17f3)
    Token Details:
    - Id: 1-0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3
    - Chain Id: 1
    - Contract Address: 0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3
    - Name: Magic Internet Money
    - Symbol: MIM
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/162.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 162

---
+++ sdk/constants/token-map/1/definitions/1-0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3",
+  chain_id: 1,
+  contract_address: "0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3",
+  name: "Magic Internet Money",
+  symbol: "MIM",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/162.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "162",
+  type: "token",
+});
