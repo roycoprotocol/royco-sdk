edd3b5ce4c0eee94b3ed362501c36eac7cd26691
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 11:11:09 -0800

    feat(token-map): Update Token -- 1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7 -- Kelp DAO Restaked ETH (RSETH) (0xa129...e5a7)
    Token Details:
    - Id: 1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7
    - Chain Id: 1
    - Contract Address: 0xa1290d69c65a6fe4df752f95823fae25cb99e5a7
    - Name: Kelp DAO Restaked ETH
    - Symbol: RSETH
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/29242.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 29242

---
+++ sdk/constants/token-map/1/definitions/1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7",
+  chain_id: 1,
+  contract_address: "0xa1290d69c65a6fe4df752f95823fae25cb99e5a7",
+  name: "Kelp DAO Restaked ETH",
+  symbol: "RSETH",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/29242.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "29242",
+  type: "token",
+});
