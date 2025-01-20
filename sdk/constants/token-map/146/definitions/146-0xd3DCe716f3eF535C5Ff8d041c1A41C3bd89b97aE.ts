33c895144c506fc1778f4dfc34ebc3933c7b5f64
bharatvish <bharat@waymont.co>
2025-01-20 11:39:02 -0800

    Create 146-0xd3DCe716f3eF535C5Ff8d041c1A41C3bd89b97aE.ts

---
+++ sdk/constants/token-map/146/definitions/146-0xd3DCe716f3eF535C5Ff8d041c1A41C3bd89b97aE.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xd3DCe716f3eF535C5Ff8d041c1A41C3bd89b97aE",
+  chain_id: 146,
+  contract_address: "0xd3DCe716f3eF535C5Ff8d041c1A41C3bd89b97aE",
+  name: "Sonic USD",
+  symbol: "scUSD",
+  image: "https://sonicscan.org/token/images/sonicusd_32.png",
+  decimals: 6,
+  source: "coingecko",
+  search_id: "rings-scusd",
+  type: "token",
+});
