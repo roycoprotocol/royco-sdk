9b6d73ff1f2d8d8010c422908bf3237773ce32df
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-01-20 12:43:26 -0800

    lorenzo mainnet tokens

---
+++ sdk/constants/token-map/1/definitions/1-0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+    id: "1-0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a",
+    chain_id: 1,
+    contract_address: "0x6A9A65B84843F5fD4aC9a0471C4fc11AFfFBce4a",
+    name: "Lorenzo Wrapped Bitcoin",
+    symbol: "enzoBTC",
+    image: "https://i.imgur.com/1oRzqer.png",
+    decimals: 8,
+    source: "coinmarketcap",
+    search_id: "1",
+    type: "token",
+});
---
+++ sdk/constants/token-map/1/definitions/1-0xf6718b2701D4a6498eF77D7c152b2137Ab28b8A3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+    id: "1-0xf6718b2701D4a6498eF77D7c152b2137Ab28b8A3",
+    chain_id: 1,
+    contract_address: "0xf6718b2701D4a6498eF77D7c152b2137Ab28b8A3",
+    name: "Lorenzo stBTC",
+    symbol: "stBTC",
+    image: "https://i.imgur.com/3p46OKE.png",
+    decimals: 18,
+    source: "coinmarketcap",
+    search_id: "1",
+    type: "token",
+});
