340f9720ec0cfc3b7502f65b360627b249f76b0d
exp_table <76456212+exp-table@users.noreply.github.com>
2025-01-20 11:40:53 +0100

    Create 1-0x325e05f22af5a3f7e2cb9b112e8f4d9b6c14b8b4.ts

---
+++ sdk/constants/token-map/1/definitions/1-0x325e05f22af5a3f7e2cb9b112e8f4d9b6c14b8b4.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x325e05f22af5a3f7e2cb9b112e8f4d9b6c14b8b4",
+  chain_id: 1,
+  contract_address: "0x325e05f22af5a3f7e2cb9b112e8f4d9b6c14b8b4",
+  name: "Henlo Boyco Points",
+  symbol: "bpHENLO",
+  image: "https://pub-2cb2198bb1d347eea80846c7d7b34411.r2.dev/LOGO_CIRCLE.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
