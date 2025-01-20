b27214374b9bd99f65f7faa7b161db711d533459
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 11:13:29 -0800

    feat(token-map): Update Token -- 1-0x8a60e489004ca22d775c5f2c657598278d17d9c2 -- USDa (USDA) (0x8a60...d9c2)
    Token Details:
    - Id: 1-0x8a60e489004ca22d775c5f2c657598278d17d9c2
    - Chain Id: 1
    - Contract Address: 0x8a60e489004ca22d775c5f2c657598278d17d9c2
    - Name: USDa
    - Symbol: USDA
    - Image: https://coin-images.coingecko.com/coins/images/51599/large/SUSDA.png?1731604761
    - Decimals: 18
    - Source: coingecko
    - Search Id: usda-2

---
+++ sdk/constants/token-map/1/definitions/1-0x8a60e489004ca22d775c5f2c657598278d17d9c2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8a60e489004ca22d775c5f2c657598278d17d9c2",
+  chain_id: 1,
+  contract_address: "0x8a60e489004ca22d775c5f2c657598278d17d9c2",
+  name: "USDa",
+  symbol: "USDA",
+  image: "https://coin-images.coingecko.com/coins/images/51599/large/SUSDA.png?1731604761",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "usda-2",
+  type: "token",
+});
