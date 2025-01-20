44d1178c73b0a22f002630d7a91e43bcdbaabb7d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-20 11:21:00 -0800

    feat(token-map): Update Token -- 1-0x35d8949372d46b7a3d5a56006ae77b215fc69bc0 -- Staked USD0 (USD0++) (0x35d8...9bc0)
    Token Details:
    - Id: 1-0x35d8949372d46b7a3d5a56006ae77b215fc69bc0
    - Chain Id: 1
    - Contract Address: 0x35d8949372d46b7a3d5a56006ae77b215fc69bc0
    - Name: Staked USD0
    - Symbol: USD0++
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/33981.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 33981

---
+++ sdk/constants/token-map/1/definitions/1-0x35d8949372d46b7a3d5a56006ae77b215fc69bc0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x35d8949372d46b7a3d5a56006ae77b215fc69bc0",
+  chain_id: 1,
+  contract_address: "0x35d8949372d46b7a3d5a56006ae77b215fc69bc0",
+  name: "Staked USD0",
+  symbol: "USD0++",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/33981.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "33981",
+  type: "token",
+});
