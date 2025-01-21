5b265f0067c29748a19b2ae588cb901937f1496d
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-01-20 17:50:27 -0800

    add rUSD on mainnet

---
+++ sdk/constants/token-map/1/definitions/1-0x09D4214C03D01F49544C0448DBE3A27f768F2b34.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+    id: "1-0x09D4214C03D01F49544C0448DBE3A27f768F2b34",
+    chain_id: 1,
+    contract_address: "0x09D4214C03D01F49544C0448DBE3A27f768F2b34",
+    name: "Reservoir Stablecoin",
+    symbol: "rUSD",
+    image: "https://i.ibb.co/YXbG3dC/rusd-bera.png",
+    decimals: 18,
+    source: "coinmarketcap",
+    search_id: "3408",
+    type: "token",
+});
