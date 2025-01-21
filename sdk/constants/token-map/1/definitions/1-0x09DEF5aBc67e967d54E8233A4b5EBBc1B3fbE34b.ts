cf1b8dcb5225aaa9da860779a973dd0336e45a56
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-01-21 09:13:22 -0800

    add waBTC on mainnet

---
+++ sdk/constants/token-map/1/definitions/1-0x09DEF5aBc67e967d54E8233A4b5EBBc1B3fbE34b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+    id: "1-0x09DEF5aBc67e967d54E8233A4b5EBBc1B3fbE34b",
+    chain_id: 1,
+    contract_address: "0x09DEF5aBc67e967d54E8233A4b5EBBc1B3fbE34b",
+    name: "Wrapped aBTC",
+    symbol: "waBTC",
+    image: "https://docs.acornnetwork.io/~gitbook/image?url=https%3A%2F%2F3030623900-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FNwiio9JesxbQZhdA7QQC%252Fuploads%252FcADEzOJEIwLcDHRU2z9A%252Fwabtc%25201%2520%281%29.png%3Falt%3Dmedia%26token%3De22c79c8-c0e2-47ec-a5b4-4eba1a646810&width=768&dpr=1&quality=100&sign=79fff55e&sv=2",
+    decimals: 18,
+    source: "coinmarketcap",
+    search_id: "3717",
+    type: "token",
+});
