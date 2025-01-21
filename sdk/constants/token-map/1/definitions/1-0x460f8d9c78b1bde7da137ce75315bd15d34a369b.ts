2ebbe14613398d3a48b5691b8b4a99eb33e28e6f
Corey <corey@dolomite.io>
2025-01-21 00:44:31 -0500

    added Dolomite veDOLO points

---
+++ sdk/constants/token-map/1/definitions/1-0x460f8d9c78b1bde7da137ce75315bd15d34a369b.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x460f8d9c78b1bde7da137ce75315bd15d34a369b",
+  chain_id: 1,
+  contract_address: "0x460f8d9c78b1bde7da137ce75315bd15d34a369b",
+  name: "Dolomite veDOLO Points",
+  symbol: "veDOLO-P",
+  image: "https://dolomite.io/static/dolomite-symbol-black-33b7f55f8e8d02a184d6fec8b8996e97.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
