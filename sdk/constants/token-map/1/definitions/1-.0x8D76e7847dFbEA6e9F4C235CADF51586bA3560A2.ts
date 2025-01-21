94e98cf56811a1f5b7022766a4e888f2e9fd420d
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-01-21 10:38:15 -0800

    add ylFBTC to mainnet tokens

---
+++ sdk/constants/token-map/1/definitions/1-.0x8D76e7847dFbEA6e9F4C235CADF51586bA3560A2.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+    id: "1-0x8D76e7847dFbEA6e9F4C235CADF51586bA3560A2",
+    chain_id: 1,
+    contract_address: "0x8D76e7847dFbEA6e9F4C235CADF51586bA3560A2",
+    name: "FBTC Yield Layer",
+    symbol: "ylFBTC",
+    image: "https://github.com/cian-ai/marketing_kit/blob/main/token/512x512/ylFBTC.png",
+    decimals: 8,
+    source: "coingecko",
+    search_id: "ignition-fbtc",
+    type: "token",
+});
