78f333153c597e7f70513186a73e89a35ebc2e18
Corey <corey@dolomite.io>
2025-01-21 12:21:10 -0500

    updated veDOLO image

--- sdk/constants/token-map/1/definitions/1-0x460f8d9c78b1bde7da137ce75315bd15d34a369b.ts
+++ sdk/constants/token-map/1/definitions/1-0x460f8d9c78b1bde7da137ce75315bd15d34a369b.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x460f8d9c78b1bde7da137ce75315bd15d34a369b",
   name: "Dolomite veDOLO Points",
   symbol: "veDOLO-P",
-  image: "https://dolomite.io/static/dolomite-symbol-black-33b7f55f8e8d02a184d6fec8b8996e97.png",
+  image: "https://dolomite.io/static/veDOLO-black-b908253bcce957330226cd87d1fe9c71.png",
   decimals: 18,
   source: "external",
   search_id: "none",
