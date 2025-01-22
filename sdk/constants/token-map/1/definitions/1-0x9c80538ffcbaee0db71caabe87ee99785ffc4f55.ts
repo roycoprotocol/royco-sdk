1dab4b401c8046af5a77609d1980835f467a3cc2
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2025-01-22 09:48:35 +0000

    sats2 rewards

---
+++ sdk/constants/token-map/1/definitions/1-0x9c80538ffcbaee0db71caabe87ee99785ffc4f55.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x9c80538ffcbaee0db71caabe87ee99785ffc4f55",
+  chain_id: 1,
+  contract_address: "0x0a2d9370cf74da3fd3df5d764e394ca8205c50b6",
+  name: "Sats2 Rewards",
+  symbol: "SATS2",
+  image: "https://satlayer.xyz/logo/satlayer.svg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
