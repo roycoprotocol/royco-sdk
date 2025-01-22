a61a184b016ecdd2f75e58708f2af7a4e9c24df2
orionStrategos <orionstrategos@proton.me>
2025-01-22 14:55:30 +0000

    Update 1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f.ts

--- sdk/constants/token-map/1/definitions/1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f.ts
+++ sdk/constants/token-map/1/definitions/1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f.ts
@@ -1,11 +1,11 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f  ",
+  id: "1-0x77d17183055303a15208c809b716dc02261129B7  ",
   chain_id: 1,
-  contract_address: "0x3b2635c5d5cc5cee62b9084636f808c67da9988f",
-  name: "Infrared Finance",
-  symbol: "IR",
+  contract_address: "0x77d17183055303a15208c809b716dc02261129B7",
+  name: "Infrared Points",
+  symbol: "IRP",
   image: "https://drive.google.com/file/d/1i_g9OhvMp0_ZIf9YWXntDCKMoKvT4yG1/view?usp=sharing",
   decimals: 18,
   source: "external",
