d437605cf085e62892749cd70759edb6d8a84926
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-22 01:46:59 -0800

    feat(market-map): Update market -- 42161_0_0x68cd2f902fc32eaf649d081513dcd4a53523d355e2e5b54ad5b0a4d5683e9982 -- Recipe (0x68cd...9982)
    Market Details:
    - Chain ID: 42161
    - Market Type: 0 (Recipe)
    - Market ID: 0x68cd2f902fc32eaf649d081513dcd4a53523d355e2e5b54ad5b0a4d5683e9982 (Market Hash)
    - Request Timestamp: Wed, 22 Jan 2025 09:46:58 GMT

---
+++ sdk/constants/market-map/42161/definitions/42161_0_0x68cd2f902fc32eaf649d081513dcd4a53523d355e2e5b54ad5b0a4d5683e9982.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `42161_0_0x68cd2f902fc32eaf649d081513dcd4a53523d355e2e5b54ad5b0a4d5683e9982`,
+  name: `Swap USDC to yUSD for 1 mo`,
+  description: `YieldFi is world&#x27;s first streamlined asset management platform offering 15% - 25% APY + incentives on stablecoins (think Blackrock with crypto&#x27;s advantages). 
+
+It creates passive best in class yields for users across Ethereum and leading L2s (Arbitrum, Base, OP etc). More details at https:&#x2F;&#x2F;yield.fi&#x2F;.`,
+  is_verified: false,
+});
