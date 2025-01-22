db96778f7358558f95f015233bfd3ae0406ee2e5
bharatvish <bharat@waymont.co>
2025-01-21 17:01:21 -0800

    Update 1_1_0xa2bb5cd636a098783f629d1f1431934a45eee7fa.ts

--- sdk/constants/market-map/1/definitions/1_1_0xa2bb5cd636a098783f629d1f1431934a45eee7fa.ts
+++ sdk/constants/market-map/1/definitions/1_1_0xa2bb5cd636a098783f629d1f1431934a45eee7fa.ts
@@ -3,7 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: "1_1_0xa2bb5cd636a098783f629d1f1431934a45eee7fa.ts",
   name: "Supply USDC into Euler Yield Market",
-  description: `Supply USDC in the USDC Vault in Euler Yield Market and get extra rEUL rewards and yield. Reward EUL converts to 1:1 with EUL over a period of 6 months. For more information, read here: https://forum.euler.finance/t/reward-eul-reul/1133. 
-`,
+  description: "Supply USDC in the USDC Vault in Euler Yield Market and get extra rEUL rewards and yield. Reward EUL converts to 1:1 with EUL over a period of 6 months. For more information, read here: https://forum.euler.finance/t/reward-eul-reul/1133." ,
   is_verified: true,
 });
