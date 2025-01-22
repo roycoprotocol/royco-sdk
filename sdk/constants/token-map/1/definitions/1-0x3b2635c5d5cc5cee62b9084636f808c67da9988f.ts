8a529c5877d031b9731df419f7c22de9a1579662
orionStrategos <orionstrategos@proton.me>
2025-01-22 12:14:57 +0000

    Create 1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f.ts

---
+++ sdk/constants/token-map/1/definitions/1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f  ",
+  chain_id: 1,
+  contract_address: "0x3b2635c5d5cc5cee62b9084636f808c67da9988f",
+  name: "Infrared Finance",
+  symbol: "IR",
+  image: "https://drive.google.com/file/d/1i_g9OhvMp0_ZIf9YWXntDCKMoKvT4yG1/view?usp=sharing",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
