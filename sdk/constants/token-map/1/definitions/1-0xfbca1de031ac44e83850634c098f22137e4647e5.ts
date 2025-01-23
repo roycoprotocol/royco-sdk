1774973c73a239e3201a4bca27ceddab23662e42
Alex Encinas <80543940+alexenc@users.noreply.github.com>
2025-01-23 15:25:06 +0100

    Create 1-0xfbca1de031ac44e83850634c098f22137e4647e5.ts

---
+++ sdk/constants/token-map/1/definitions/1-0xfbca1de031ac44e83850634c098f22137e4647e5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xfbca1de031ac44e83850634c098f22137e4647e5",
+  chain_id: 1,
+  contract_address: "0xfbca1de031ac44e83850634c098f22137e4647e5",
+  name: "Beraborrow Points Program",
+  symbol: "bPOLLEN",
+  image: "https://assets.beraborrow.com/android-chrome-512x512.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
