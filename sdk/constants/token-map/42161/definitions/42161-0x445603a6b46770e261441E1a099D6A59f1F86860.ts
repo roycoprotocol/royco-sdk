c5e673ac31947322860c3726d3e42246a2dd95b6
SG <shaileshgupta@users.noreply.github.com>
2025-01-23 14:38:52 +0530

    Create 42161-0x445603a6b46770e261441E1a099D6A59f1F86860.ts

---
+++ sdk/constants/token-map/42161/definitions/42161-0x445603a6b46770e261441E1a099D6A59f1F86860.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "42161-0x445603a6b46770e261441E1a099D6A59f1F86860",
+  chain_id: 42161,
+  contract_address: "0x445603a6b46770e261441E1a099D6A59f1F86860",
+  name: "YieldFi Points",
+  symbol: "YC",
+  image: "https://cdn.yield.fi/yfi/yieldcrumblogo.svg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
