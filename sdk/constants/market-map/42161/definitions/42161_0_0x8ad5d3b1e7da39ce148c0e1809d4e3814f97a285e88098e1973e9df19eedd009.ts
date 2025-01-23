b8557c422074c00d340af711fa5208682a3c3f71
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-23 00:00:38 -0800

    feat(market-map): Update market -- 42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009 -- Recipe (0x8ad5...d009)
    Market Details:
    - Chain ID: 42161
    - Market Type: 0 (Recipe)
    - Market ID: 0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009 (Market Hash)
    - Request Timestamp: Thu, 23 Jan 2025 08:00:37 GMT

---
+++ sdk/constants/market-map/42161/definitions/42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009`,
+  name: `Swap USDC for yUSD for 3 mo`,
+  description: `Earn 15% - 25% APY + rewards on stable coins. 
+
+More details at (https:&#x2F;&#x2F;yield.fi)`,
+  is_verified: false,
+});
