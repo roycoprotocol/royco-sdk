dba4e1a07eab87a7ef56198a7b7bf93062c1e1c0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 13:07:56 -0800

    feat(market-map): Update market -- 1_0_0xcf68c89be125a4feb11f2de983073cf792fb99674d82a87cb0ceba676f73a284 -- Recipe (0xcf68...a284) (#291)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xcf68c89be125a4feb11f2de983073cf792fb99674d82a87cb0ceba676f73a284 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 21:07:54 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xcf68c89be125a4feb11f2de983073cf792fb99674d82a87cb0ceba676f73a284.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xcf68c89be125a4feb11f2de983073cf792fb99674d82a87cb0ceba676f73a284`,
+  name: `sUSDa bridge test`,
+  description: `sUSDa bridge test`,
+  is_verified: false,
+});
