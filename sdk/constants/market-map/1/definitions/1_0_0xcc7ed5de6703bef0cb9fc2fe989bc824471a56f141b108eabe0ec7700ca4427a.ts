e081613046182f5f83f91cdf85e44ef98bf88ae5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 15:01:44 -0800

    feat(market-map): Update market -- 1_0_0xcc7ed5de6703bef0cb9fc2fe989bc824471a56f141b108eabe0ec7700ca4427a -- Recipe (0xcc7e...427a) (#297)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xcc7ed5de6703bef0cb9fc2fe989bc824471a56f141b108eabe0ec7700ca4427a (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 23:01:41 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xcc7ed5de6703bef0cb9fc2fe989bc824471a56f141b108eabe0ec7700ca4427a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xcc7ed5de6703bef0cb9fc2fe989bc824471a56f141b108eabe0ec7700ca4427a`,
+  name: `rswETH bridge test`,
+  description: `rswETH bridge test`,
+  is_verified: false,
+});
