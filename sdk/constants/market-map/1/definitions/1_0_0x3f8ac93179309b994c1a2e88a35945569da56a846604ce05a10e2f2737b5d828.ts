579bf2b03d465a37c6d22aed656ca1476e33f725
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 12:30:56 -0800

    feat(market-map): Update market -- 1_0_0x3f8ac93179309b994c1a2e88a35945569da56a846604ce05a10e2f2737b5d828 -- Recipe (0x3f8a...d828) (#288)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x3f8ac93179309b994c1a2e88a35945569da56a846604ce05a10e2f2737b5d828 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 20:30:54 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x3f8ac93179309b994c1a2e88a35945569da56a846604ce05a10e2f2737b5d828.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x3f8ac93179309b994c1a2e88a35945569da56a846604ce05a10e2f2737b5d828`,
+  name: `waBTC test bridging`,
+  description: `waBTC test bridging`,
+  is_verified: false,
+});
