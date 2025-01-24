98d9727adc8e61904f7daccc9812eed38596cc39
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 13:55:02 -0800

    feat(market-map): Update market -- 1_0_0x85e1528b3bc4fc13933683ee82d53461133ccf96ac13742e8b00de896f1a57e3 -- Recipe (0x85e1...57e3) (#294)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x85e1528b3bc4fc13933683ee82d53461133ccf96ac13742e8b00de896f1a57e3 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 21:55:00 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x85e1528b3bc4fc13933683ee82d53461133ccf96ac13742e8b00de896f1a57e3.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x85e1528b3bc4fc13933683ee82d53461133ccf96ac13742e8b00de896f1a57e3`,
+  name: `yluniBTC bridge test`,
+  description: `yluniBTC bridge test`,
+  is_verified: false,
+});
