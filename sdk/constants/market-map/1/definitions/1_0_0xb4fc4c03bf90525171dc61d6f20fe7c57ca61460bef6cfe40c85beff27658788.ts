b96f57c79ccfb609b96cfdae210a42d0ce1c9417
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 11:12:15 -0800

    feat(market-map): Update market -- 1_0_0xb4fc4c03bf90525171dc61d6f20fe7c57ca61460bef6cfe40c85beff27658788 -- Recipe (0xb4fc...8788)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xb4fc4c03bf90525171dc61d6f20fe7c57ca61460bef6cfe40c85beff27658788 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 19:12:15 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xb4fc4c03bf90525171dc61d6f20fe7c57ca61460bef6cfe40c85beff27658788.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xb4fc4c03bf90525171dc61d6f20fe7c57ca61460bef6cfe40c85beff27658788`,
+  name: `rUSD bridging test`,
+  description: `rUSD bridging test`,
+  is_verified: false,
+});
