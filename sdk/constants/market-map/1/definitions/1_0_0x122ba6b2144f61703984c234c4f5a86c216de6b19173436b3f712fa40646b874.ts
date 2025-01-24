b35cb37a0aaccda731dc470a9ff58abb880a1fab
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 12:37:44 -0800

    feat(market-map): Update market -- 1_0_0x122ba6b2144f61703984c234c4f5a86c216de6b19173436b3f712fa40646b874 -- Recipe (0x122b...b874) (#289)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x122ba6b2144f61703984c234c4f5a86c216de6b19173436b3f712fa40646b874 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 20:37:41 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x122ba6b2144f61703984c234c4f5a86c216de6b19173436b3f712fa40646b874.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x122ba6b2144f61703984c234c4f5a86c216de6b19173436b3f712fa40646b874`,
+  name: `uniBTC bridge test`,
+  description: `uniBTC bridge test`,
+  is_verified: false,
+});
