7d362728a8132de9e6f685a112556fbde1a37e3a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 14:08:54 -0800

    feat(market-map): Update market -- 1_0_0x8db4fb50a4b9573ff18c0652147969403592f4214e94c219bcfa5304a6b81b30 -- Recipe (0x8db4...1b30)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x8db4fb50a4b9573ff18c0652147969403592f4214e94c219bcfa5304a6b81b30 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 22:08:53 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x8db4fb50a4b9573ff18c0652147969403592f4214e94c219bcfa5304a6b81b30.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x8db4fb50a4b9573ff18c0652147969403592f4214e94c219bcfa5304a6b81b30`,
+  name: `MIM bridge test`,
+  description: `MIM bridge test`,
+  is_verified: false,
+});
