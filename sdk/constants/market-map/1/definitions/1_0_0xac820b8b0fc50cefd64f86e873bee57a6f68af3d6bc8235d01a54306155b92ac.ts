5a4f424ecaf193ec595c45f44e8071f15730f73e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 11:03:07 -0800

    feat(market-map): Update market -- 1_0_0xac820b8b0fc50cefd64f86e873bee57a6f68af3d6bc8235d01a54306155b92ac -- Recipe (0xac82...92ac) (#282)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xac820b8b0fc50cefd64f86e873bee57a6f68af3d6bc8235d01a54306155b92ac (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 19:03:04 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xac820b8b0fc50cefd64f86e873bee57a6f68af3d6bc8235d01a54306155b92ac.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xac820b8b0fc50cefd64f86e873bee57a6f68af3d6bc8235d01a54306155b92ac`,
+  name: `sBTC Bridge Test`,
+  description: `sBTC Bridge Test`,
+  is_verified: false,
+});
