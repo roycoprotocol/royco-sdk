ff1b17893f3d2edebf22528aea4b1b8dc846bf42
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 13:46:18 -0800

    feat(market-map): Update market -- 1_0_0xf2c982b6705e631c291a98a1931d21d9c83cec79938e079f453a1331f1d26289 -- Recipe (0xf2c9...6289) (#293)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xf2c982b6705e631c291a98a1931d21d9c83cec79938e079f453a1331f1d26289 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 21:46:16 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xf2c982b6705e631c291a98a1931d21d9c83cec79938e079f453a1331f1d26289.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xf2c982b6705e631c291a98a1931d21d9c83cec79938e079f453a1331f1d26289`,
+  name: `ylrsETH bridging test`,
+  description: `ylrsETH bridging test`,
+  is_verified: false,
+});
