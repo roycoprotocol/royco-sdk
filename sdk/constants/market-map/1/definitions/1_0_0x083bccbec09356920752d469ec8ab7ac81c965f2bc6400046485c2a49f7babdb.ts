d0260d91a5a25484808c537c782d3abe1fd13835
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 10:47:52 -0800

    feat(market-map): Update market -- 1_0_0x083bccbec09356920752d469ec8ab7ac81c965f2bc6400046485c2a49f7babdb -- Recipe (0x083b...abdb) (#281)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x083bccbec09356920752d469ec8ab7ac81c965f2bc6400046485c2a49f7babdb (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 18:47:50 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x083bccbec09356920752d469ec8ab7ac81c965f2bc6400046485c2a49f7babdb.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x083bccbec09356920752d469ec8ab7ac81c965f2bc6400046485c2a49f7babdb`,
+  name: `Stone OFT Test`,
+  description: `Test bridging STONE`,
+  is_verified: false,
+});
