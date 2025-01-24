02d427aa6653bd31cebe710f145962c48bed2bd0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 13:17:07 -0800

    feat(market-map): Update market -- 1_0_0x38c48a8354fde176cbe2b9a07d4b8321267d7d16aac5afafe209563f0b209e5c -- Recipe (0x38c4...9e5c)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x38c48a8354fde176cbe2b9a07d4b8321267d7d16aac5afafe209563f0b209e5c (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 21:17:06 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x38c48a8354fde176cbe2b9a07d4b8321267d7d16aac5afafe209563f0b209e5c.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x38c48a8354fde176cbe2b9a07d4b8321267d7d16aac5afafe209563f0b209e5c`,
+  name: `lBTC bridge test`,
+  description: `lBTC bridge test`,
+  is_verified: false,
+});
