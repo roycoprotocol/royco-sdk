2d6b997540249097ecb5a7bab15c982d07fbcf5f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-24 14:03:41 -0800

    feat(market-map): Update market -- 1_0_0xa0c53b5a7f98c8b3222dc9671553c1a3fa18188d893e57cdd7971ec803074b33 -- Recipe (0xa0c5...4b33)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xa0c53b5a7f98c8b3222dc9671553c1a3fa18188d893e57cdd7971ec803074b33 (Market Hash)
    - Request Timestamp: Fri, 24 Jan 2025 22:03:41 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xa0c53b5a7f98c8b3222dc9671553c1a3fa18188d893e57cdd7971ec803074b33.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xa0c53b5a7f98c8b3222dc9671553c1a3fa18188d893e57cdd7971ec803074b33`,
+  name: `ylstETH bridge test`,
+  description: `ylstETH bridge test`,
+  is_verified: false,
+});
