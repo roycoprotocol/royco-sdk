c180ea3923071b6c6c7e48c26a36c1bc4e460729
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:10:18 -0800

    feat(token-map): Update Token -- 1-0x004375dff511095cc5a197a54140a24efef3a416 -- Wrapped Bitcoin-USDC LP Token (WBTC-USDC) (0x0043...a416)
    Token Details:
    - Id: 1-0x004375dff511095cc5a197a54140a24efef3a416
    - Chain Id: 1
    - Contract Address: 0x004375dff511095cc5a197a54140a24efef3a416
    - Name: Wrapped Bitcoin-USDC LP Token
    - Symbol: WBTC-USDC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x004375dff511095cc5a197a54140a24efef3a416
    - Token 0: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Token 1: 1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48

---
+++ sdk/constants/token-map/1/definitions/1-0x004375dff511095cc5a197a54140a24efef3a416.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x004375dff511095cc5a197a54140a24efef3a416",
+  chain_id: 1,
+  contract_address: "0x004375dff511095cc5a197a54140a24efef3a416",
+  name: "Wrapped Bitcoin-USDC LP Token",
+  symbol: "WBTC-USDC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x004375dff511095cc5a197a54140a24efef3a416",
+  type: "lp",
+  token0: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  token1: "1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+});
