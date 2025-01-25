4ed268348f3b7dee433525badf2b5ab0726d42f1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:14:30 -0800

    feat(token-map): Update Token -- 1-0x761c035187d3670dad9cdaeca7d5591d79268a13 -- Kelp DAO Restaked ETH-CIAN yield layer rsETH LP Token (RSETH-ylrsETH) (0x761c...8a13)
    Token Details:
    - Id: 1-0x761c035187d3670dad9cdaeca7d5591d79268a13
    - Chain Id: 1
    - Contract Address: 0x761c035187d3670dad9cdaeca7d5591d79268a13
    - Name: Kelp DAO Restaked ETH-CIAN yield layer rsETH LP Token
    - Symbol: RSETH-ylrsETH
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x761c035187d3670dad9cdaeca7d5591d79268a13
    - Token 0: 1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7
    - Token 1: 1-0xd87a19ff681ae98bf10d2220d1ae3fbd374ade4e

---
+++ sdk/constants/token-map/1/definitions/1-0x761c035187d3670dad9cdaeca7d5591d79268a13.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x761c035187d3670dad9cdaeca7d5591d79268a13",
+  chain_id: 1,
+  contract_address: "0x761c035187d3670dad9cdaeca7d5591d79268a13",
+  name: "Kelp DAO Restaked ETH-CIAN yield layer rsETH LP Token",
+  symbol: "RSETH-ylrsETH",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x761c035187d3670dad9cdaeca7d5591d79268a13",
+  type: "lp",
+  token0: "1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7",
+  token1: "1-0xd87a19ff681ae98bf10d2220d1ae3fbd374ade4e",
+});
