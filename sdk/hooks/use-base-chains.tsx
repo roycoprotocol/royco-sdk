6a9260604dd451bf34148701d173018a693947ec
githubotoro <yupuday.git@gmail.com>
2025-01-25 08:43:00 -0800

    feat(constants): fixes

--- sdk/hooks/use-base-chains.tsx
+++ sdk/hooks/use-base-chains.tsx
@@ -1,7 +1,15 @@
-import { SupportedChainlist } from "@/sdk/constants";
+import {
+  BerachainMainnet,
+  BerachainTestnet,
+  SupportedChainlist,
+} from "@/sdk/constants";
 
-export const useBaseChains = ()  => {
-  const data = SupportedChainlist.sort((a, b) => a.name.localeCompare(b.name));
+export const useBaseChains = () => {
+  const hiddenChains: number[] = [BerachainTestnet.id, BerachainMainnet.id];
+
+  const data = SupportedChainlist.sort((a, b) =>
+    a.name.localeCompare(b.name),
+  ).filter((chain) => !hiddenChains.includes(chain.id));
 
   return { data };
 };
