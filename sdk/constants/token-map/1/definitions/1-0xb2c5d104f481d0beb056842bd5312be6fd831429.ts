7116d954e2c9e7bbf2043c24d0c53bd8a531b093
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:11:43 -0800

    feat(token-map): Update Token -- 1-0xb2c5d104f481d0beb056842bd5312be6fd831429 -- Reservoir Stablecoin-USDC LP Token (rUSD-USDC) (0xb2c5...1429)
    Token Details:
    - Id: 1-0xb2c5d104f481d0beb056842bd5312be6fd831429
    - Chain Id: 1
    - Contract Address: 0xb2c5d104f481d0beb056842bd5312be6fd831429
    - Name: Reservoir Stablecoin-USDC LP Token
    - Symbol: rUSD-USDC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xb2c5d104f481d0beb056842bd5312be6fd831429
    - Token 0: 1-0x09d4214c03d01f49544c0448dbe3a27f768f2b34
    - Token 1: 1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48

---
+++ sdk/constants/token-map/1/definitions/1-0xb2c5d104f481d0beb056842bd5312be6fd831429.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb2c5d104f481d0beb056842bd5312be6fd831429",
+  chain_id: 1,
+  contract_address: "0xb2c5d104f481d0beb056842bd5312be6fd831429",
+  name: "Reservoir Stablecoin-USDC LP Token",
+  symbol: "rUSD-USDC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xb2c5d104f481d0beb056842bd5312be6fd831429",
+  type: "lp",
+  token0: "1-0x09d4214c03d01f49544c0448dbe3a27f768f2b34",
+  token1: "1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+});
