e582a364bbbebfe33e027e519bece2433b1efbb7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:12:14 -0800

    feat(token-map): Update Token -- 1-0x75c240478f358f8a1a817f2a52fb84d61b9939ff -- Wrapped Bitcoin-Lorenzo stBTC LP Token (WBTC-stBTC) (0x75c2...39ff)
    Token Details:
    - Id: 1-0x75c240478f358f8a1a817f2a52fb84d61b9939ff
    - Chain Id: 1
    - Contract Address: 0x75c240478f358f8a1a817f2a52fb84d61b9939ff
    - Name: Wrapped Bitcoin-Lorenzo stBTC LP Token
    - Symbol: WBTC-stBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x75c240478f358f8a1a817f2a52fb84d61b9939ff
    - Token 0: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Token 1: 1-0xf6718b2701d4a6498ef77d7c152b2137ab28b8a3

---
+++ sdk/constants/token-map/1/definitions/1-0x75c240478f358f8a1a817f2a52fb84d61b9939ff.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x75c240478f358f8a1a817f2a52fb84d61b9939ff",
+  chain_id: 1,
+  contract_address: "0x75c240478f358f8a1a817f2a52fb84d61b9939ff",
+  name: "Wrapped Bitcoin-Lorenzo stBTC LP Token",
+  symbol: "WBTC-stBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x75c240478f358f8a1a817f2a52fb84d61b9939ff",
+  type: "lp",
+  token0: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  token1: "1-0xf6718b2701d4a6498ef77d7c152b2137ab28b8a3",
+});
