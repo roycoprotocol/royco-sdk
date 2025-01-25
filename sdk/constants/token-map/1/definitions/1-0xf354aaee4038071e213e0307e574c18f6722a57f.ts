8e15f308dd9efaeda0143ffe2634094d3da83e45
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:14:44 -0800

    feat(token-map): Update Token -- 1-0xf354aaee4038071e213e0307e574c18f6722a57f -- Staked USD0-Usual USD LP Token (USD0++-USD0) (0xf354...a57f)
    Token Details:
    - Id: 1-0xf354aaee4038071e213e0307e574c18f6722a57f
    - Chain Id: 1
    - Contract Address: 0xf354aaee4038071e213e0307e574c18f6722a57f
    - Name: Staked USD0-Usual USD LP Token
    - Symbol: USD0++-USD0
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xf354aaee4038071e213e0307e574c18f6722a57f
    - Token 0: 1-0x35d8949372d46b7a3d5a56006ae77b215fc69bc0
    - Token 1: 1-0x73a15fed60bf67631dc6cd7bc5b6e8da8190acf5

---
+++ sdk/constants/token-map/1/definitions/1-0xf354aaee4038071e213e0307e574c18f6722a57f.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xf354aaee4038071e213e0307e574c18f6722a57f",
+  chain_id: 1,
+  contract_address: "0xf354aaee4038071e213e0307e574c18f6722a57f",
+  name: "Staked USD0-Usual USD LP Token",
+  symbol: "USD0++-USD0",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xf354aaee4038071e213e0307e574c18f6722a57f",
+  type: "lp",
+  token0: "1-0x35d8949372d46b7a3d5a56006ae77b215fc69bc0",
+  token1: "1-0x73a15fed60bf67631dc6cd7bc5b6e8da8190acf5",
+});
