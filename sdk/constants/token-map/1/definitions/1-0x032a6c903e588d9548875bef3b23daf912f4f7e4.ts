4fdd660fe10bddcf6703a7a05ca54ec6e8619f3e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:12:36 -0800

    feat(token-map): Update Token -- 1-0x032a6c903e588d9548875bef3b23daf912f4f7e4 -- StakeStone Bitcoin-Wrapped Bitcoin LP Token (SBTC-WBTC) (0x032a...f7e4)
    Token Details:
    - Id: 1-0x032a6c903e588d9548875bef3b23daf912f4f7e4
    - Chain Id: 1
    - Contract Address: 0x032a6c903e588d9548875bef3b23daf912f4f7e4
    - Name: StakeStone Bitcoin-Wrapped Bitcoin LP Token
    - Symbol: SBTC-WBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x032a6c903e588d9548875bef3b23daf912f4f7e4
    - Token 0: 1-0x094c0e36210634c3cfa25dc11b96b562e0b07624
    - Token 1: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599

---
+++ sdk/constants/token-map/1/definitions/1-0x032a6c903e588d9548875bef3b23daf912f4f7e4.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x032a6c903e588d9548875bef3b23daf912f4f7e4",
+  chain_id: 1,
+  contract_address: "0x032a6c903e588d9548875bef3b23daf912f4f7e4",
+  name: "StakeStone Bitcoin-Wrapped Bitcoin LP Token",
+  symbol: "SBTC-WBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x032a6c903e588d9548875bef3b23daf912f4f7e4",
+  type: "lp",
+  token0: "1-0x094c0e36210634c3cfa25dc11b96b562e0b07624",
+  token1: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+});
