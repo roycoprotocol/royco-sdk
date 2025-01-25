38c5179b8df0af918f8b462ef6f38deba520bbb6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:12:59 -0800

    feat(token-map): Update Token -- 1-0x8cd64380b991e6d67424b9ae30fd2048947d7ffe -- SolvBTC-SolvBTC.BBN LP Token (SolvBTC-SolvBTC.BBN) (0x8cd6...7ffe)
    Token Details:
    - Id: 1-0x8cd64380b991e6d67424b9ae30fd2048947d7ffe
    - Chain Id: 1
    - Contract Address: 0x8cd64380b991e6d67424b9ae30fd2048947d7ffe
    - Name: SolvBTC-SolvBTC.BBN LP Token
    - Symbol: SolvBTC-SolvBTC.BBN
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x8cd64380b991e6d67424b9ae30fd2048947d7ffe
    - Token 0: 1-0x7a56e1c57c7475ccf742a1832b028f0456652f97
    - Token 1: 1-0xd9d920aa40f578ab794426f5c90f6c731d159def

---
+++ sdk/constants/token-map/1/definitions/1-0x8cd64380b991e6d67424b9ae30fd2048947d7ffe.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8cd64380b991e6d67424b9ae30fd2048947d7ffe",
+  chain_id: 1,
+  contract_address: "0x8cd64380b991e6d67424b9ae30fd2048947d7ffe",
+  name: "SolvBTC-SolvBTC.BBN LP Token",
+  symbol: "SolvBTC-SolvBTC.BBN",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x8cd64380b991e6d67424b9ae30fd2048947d7ffe",
+  type: "lp",
+  token0: "1-0x7a56e1c57c7475ccf742a1832b028f0456652f97",
+  token1: "1-0xd9d920aa40f578ab794426f5c90f6c731d159def",
+});
