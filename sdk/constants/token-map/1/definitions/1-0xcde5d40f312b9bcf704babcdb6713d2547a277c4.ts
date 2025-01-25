596d58ba29b19f484d09c2cf98e3c45fbd96c98e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:12:43 -0800

    feat(token-map): Update Token -- 1-0xcde5d40f312b9bcf704babcdb6713d2547a277c4 -- Universal BTC-Wrapped Bitcoin LP Token (UNIBTC-WBTC) (0xcde5...77c4)
    Token Details:
    - Id: 1-0xcde5d40f312b9bcf704babcdb6713d2547a277c4
    - Chain Id: 1
    - Contract Address: 0xcde5d40f312b9bcf704babcdb6713d2547a277c4
    - Name: Universal BTC-Wrapped Bitcoin LP Token
    - Symbol: UNIBTC-WBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xcde5d40f312b9bcf704babcdb6713d2547a277c4
    - Token 0: 1-0x004e9c3ef86bc1ca1f0bb5c7662861ee93350568
    - Token 1: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599

---
+++ sdk/constants/token-map/1/definitions/1-0xcde5d40f312b9bcf704babcdb6713d2547a277c4.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xcde5d40f312b9bcf704babcdb6713d2547a277c4",
+  chain_id: 1,
+  contract_address: "0xcde5d40f312b9bcf704babcdb6713d2547a277c4",
+  name: "Universal BTC-Wrapped Bitcoin LP Token",
+  symbol: "UNIBTC-WBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xcde5d40f312b9bcf704babcdb6713d2547a277c4",
+  type: "lp",
+  token0: "1-0x004e9c3ef86bc1ca1f0bb5c7662861ee93350568",
+  token1: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+});
