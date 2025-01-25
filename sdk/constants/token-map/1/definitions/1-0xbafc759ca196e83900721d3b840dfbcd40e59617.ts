9de8cb4cab1e75aa5b936700b51ad75f5673a8aa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:14:08 -0800

    feat(token-map): Update Token -- 1-0xbafc759ca196e83900721d3b840dfbcd40e59617 -- pumpBTC.bera-Ignition LP Token (pumpBTC.bera-FBTC) (0xbafc...9617)
    Token Details:
    - Id: 1-0xbafc759ca196e83900721d3b840dfbcd40e59617
    - Chain Id: 1
    - Contract Address: 0xbafc759ca196e83900721d3b840dfbcd40e59617
    - Name: pumpBTC.bera-Ignition LP Token
    - Symbol: pumpBTC.bera-FBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xbafc759ca196e83900721d3b840dfbcd40e59617
    - Token 0: 1-0xadc9c900b05f39f48bb6f402a1bae60929f4f9a8
    - Token 1: 1-0xc96de26018a54d51c097160568752c4e3bd6c364

---
+++ sdk/constants/token-map/1/definitions/1-0xbafc759ca196e83900721d3b840dfbcd40e59617.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbafc759ca196e83900721d3b840dfbcd40e59617",
+  chain_id: 1,
+  contract_address: "0xbafc759ca196e83900721d3b840dfbcd40e59617",
+  name: "pumpBTC.bera-Ignition LP Token",
+  symbol: "pumpBTC.bera-FBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xbafc759ca196e83900721d3b840dfbcd40e59617",
+  type: "lp",
+  token0: "1-0xadc9c900b05f39f48bb6f402a1bae60929f4f9a8",
+  token1: "1-0xc96de26018a54d51c097160568752c4e3bd6c364",
+});
