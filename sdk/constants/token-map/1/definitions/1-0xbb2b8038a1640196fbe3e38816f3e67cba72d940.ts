66a9cb9e52bdae1b64e2cf59fd4dba87b4174bec
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:10:42 -0800

    feat(token-map): Update Token -- 1-0xbb2b8038a1640196fbe3e38816f3e67cba72d940 -- Wrapped Bitcoin-WETH LP Token (WBTC-WETH) (0xbb2b...d940)
    Token Details:
    - Id: 1-0xbb2b8038a1640196fbe3e38816f3e67cba72d940
    - Chain Id: 1
    - Contract Address: 0xbb2b8038a1640196fbe3e38816f3e67cba72d940
    - Name: Wrapped Bitcoin-WETH LP Token
    - Symbol: WBTC-WETH
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xbb2b8038a1640196fbe3e38816f3e67cba72d940
    - Token 0: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Token 1: 1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2

---
+++ sdk/constants/token-map/1/definitions/1-0xbb2b8038a1640196fbe3e38816f3e67cba72d940.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbb2b8038a1640196fbe3e38816f3e67cba72d940",
+  chain_id: 1,
+  contract_address: "0xbb2b8038a1640196fbe3e38816f3e67cba72d940",
+  name: "Wrapped Bitcoin-WETH LP Token",
+  symbol: "WBTC-WETH",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xbb2b8038a1640196fbe3e38816f3e67cba72d940",
+  type: "lp",
+  token0: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  token1: "1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+});
