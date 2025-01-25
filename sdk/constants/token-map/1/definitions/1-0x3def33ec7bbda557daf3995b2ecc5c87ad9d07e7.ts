4efa77037ea3211bf7c9cf0aff33a5af7f2539aa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:11:47 -0800

    feat(token-map): Update Token -- 1-0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7 -- StakeStone ETH-WETH LP Token (STONE-WETH) (0x3def...07e7)
    Token Details:
    - Id: 1-0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7
    - Chain Id: 1
    - Contract Address: 0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7
    - Name: StakeStone ETH-WETH LP Token
    - Symbol: STONE-WETH
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7
    - Token 0: 1-0x7122985656e38bdc0302db86685bb972b145bd3c
    - Token 1: 1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2

---
+++ sdk/constants/token-map/1/definitions/1-0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7",
+  chain_id: 1,
+  contract_address: "0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7",
+  name: "StakeStone ETH-WETH LP Token",
+  symbol: "STONE-WETH",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x3def33ec7bbda557daf3995b2ecc5c87ad9d07e7",
+  type: "lp",
+  token0: "1-0x7122985656e38bdc0302db86685bb972b145bd3c",
+  token1: "1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+});
