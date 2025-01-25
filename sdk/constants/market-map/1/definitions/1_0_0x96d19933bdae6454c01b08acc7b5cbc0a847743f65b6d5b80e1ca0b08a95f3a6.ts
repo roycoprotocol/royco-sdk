b4aacf0cd2d200fac78b7d88634451f75e3f71f6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 14:06:15 -0800

    feat(market-map): Update market -- 1_0_0x96d19933bdae6454c01b08acc7b5cbc0a847743f65b6d5b80e1ca0b08a95f3a6 -- Recipe (0x96d1...f3a6)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x96d19933bdae6454c01b08acc7b5cbc0a847743f65b6d5b80e1ca0b08a95f3a6 (Market Hash)
    - Request Timestamp: Sat, 25 Jan 2025 22:06:14 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x96d19933bdae6454c01b08acc7b5cbc0a847743f65b6d5b80e1ca0b08a95f3a6.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x96d19933bdae6454c01b08acc7b5cbc0a847743f65b6d5b80e1ca0b08a95f3a6`,
+  name: `Kodiak WETH-HONEY test`,
+  description: `Kodiak WETH-HONEY test`,
+  is_verified: false,
+  category: `boyco`,
+});
