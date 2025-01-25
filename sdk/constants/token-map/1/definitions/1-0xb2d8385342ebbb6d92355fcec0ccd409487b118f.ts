8ea06eb876cd0853335258054bd04ae4e1033689
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:11:52 -0800

    feat(token-map): Update Token -- 1-0xb2d8385342ebbb6d92355fcec0ccd409487b118f -- WETH-Restaked Swell Ethereum LP Token (WETH-RSWETH) (0xb2d8...118f)
    Token Details:
    - Id: 1-0xb2d8385342ebbb6d92355fcec0ccd409487b118f
    - Chain Id: 1
    - Contract Address: 0xb2d8385342ebbb6d92355fcec0ccd409487b118f
    - Name: WETH-Restaked Swell Ethereum LP Token
    - Symbol: WETH-RSWETH
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xb2d8385342ebbb6d92355fcec0ccd409487b118f
    - Token 0: 1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
    - Token 1: 1-0xfae103dc9cf190ed75350761e95403b7b8afa6c0

---
+++ sdk/constants/token-map/1/definitions/1-0xb2d8385342ebbb6d92355fcec0ccd409487b118f.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb2d8385342ebbb6d92355fcec0ccd409487b118f",
+  chain_id: 1,
+  contract_address: "0xb2d8385342ebbb6d92355fcec0ccd409487b118f",
+  name: "WETH-Restaked Swell Ethereum LP Token",
+  symbol: "WETH-RSWETH",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xb2d8385342ebbb6d92355fcec0ccd409487b118f",
+  type: "lp",
+  token0: "1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+  token1: "1-0xfae103dc9cf190ed75350761e95403b7b8afa6c0",
+});
