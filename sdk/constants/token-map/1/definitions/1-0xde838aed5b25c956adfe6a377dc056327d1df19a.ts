037c8b81f7dadb9048d6c8215482a5f98696ae62
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:11:26 -0800

    feat(token-map): Update Token -- 1-0xde838aed5b25c956adfe6a377dc056327d1df19a -- sUSDa-USDa LP Token (SUSDA-USDA) (0xde83...f19a)
    Token Details:
    - Id: 1-0xde838aed5b25c956adfe6a377dc056327d1df19a
    - Chain Id: 1
    - Contract Address: 0xde838aed5b25c956adfe6a377dc056327d1df19a
    - Name: sUSDa-USDa LP Token
    - Symbol: SUSDA-USDA
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xde838aed5b25c956adfe6a377dc056327d1df19a
    - Token 0: 1-0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9
    - Token 1: 1-0x8a60e489004ca22d775c5f2c657598278d17d9c2

---
+++ sdk/constants/token-map/1/definitions/1-0xde838aed5b25c956adfe6a377dc056327d1df19a.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xde838aed5b25c956adfe6a377dc056327d1df19a",
+  chain_id: 1,
+  contract_address: "0xde838aed5b25c956adfe6a377dc056327d1df19a",
+  name: "sUSDa-USDa LP Token",
+  symbol: "SUSDA-USDA",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xde838aed5b25c956adfe6a377dc056327d1df19a",
+  type: "lp",
+  token0: "1-0x2b66aade1e9c062ff411bd47c44e0ad696d43bd9",
+  token1: "1-0x8a60e489004ca22d775c5f2c657598278d17d9c2",
+});
