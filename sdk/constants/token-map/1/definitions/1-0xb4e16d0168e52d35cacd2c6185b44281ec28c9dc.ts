b49b5a6e9a013d48ac8bea5ef51fdfeef05e99a1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:08:53 -0800

    feat(token-map): Update Token -- 1-0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc -- USDC-WETH LP Token (USDC-WETH) (0xb4e1...c9dc)
    Token Details:
    - Id: 1-0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
    - Chain Id: 1
    - Contract Address: 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
    - Name: USDC-WETH LP Token
    - Symbol: USDC-WETH
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
    - Token 0: 1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
    - Token 1: 1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2

---
+++ sdk/constants/token-map/1/definitions/1-0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc",
+  chain_id: 1,
+  contract_address: "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc",
+  name: "USDC-WETH LP Token",
+  symbol: "USDC-WETH",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc",
+  type: "lp",
+  token0: "1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  token1: "1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+});
