164e033d43d5c2603b67deae3131029560334bba
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:11:30 -0800

    feat(token-map): Update Token -- 1-0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2 -- Ethena USDe-USDa LP Token (USDe-USDA) (0x19db...06e2)
    Token Details:
    - Id: 1-0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2
    - Chain Id: 1
    - Contract Address: 0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2
    - Name: Ethena USDe-USDa LP Token
    - Symbol: USDe-USDA
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2
    - Token 0: 1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3
    - Token 1: 1-0x8a60e489004ca22d775c5f2c657598278d17d9c2

---
+++ sdk/constants/token-map/1/definitions/1-0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2",
+  chain_id: 1,
+  contract_address: "0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2",
+  name: "Ethena USDe-USDa LP Token",
+  symbol: "USDe-USDA",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x19dbb3cb7f6aaa65c436246fb22978b8d89206e2",
+  type: "lp",
+  token0: "1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3",
+  token1: "1-0x8a60e489004ca22d775c5f2c657598278d17d9c2",
+});
