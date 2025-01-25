066adfdbea94d1e985ecc5d4a8066f878af8cb59
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:14:00 -0800

    feat(token-map): Update Token -- 1-0xbc15ddb819ce347f8a67b5485910b8a37911f283 -- SolvBTC-Ignition LP Token (SolvBTC-FBTC) (0xbc15...f283)
    Token Details:
    - Id: 1-0xbc15ddb819ce347f8a67b5485910b8a37911f283
    - Chain Id: 1
    - Contract Address: 0xbc15ddb819ce347f8a67b5485910b8a37911f283
    - Name: SolvBTC-Ignition LP Token
    - Symbol: SolvBTC-FBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xbc15ddb819ce347f8a67b5485910b8a37911f283
    - Token 0: 1-0x7a56e1c57c7475ccf742a1832b028f0456652f97
    - Token 1: 1-0xc96de26018a54d51c097160568752c4e3bd6c364

---
+++ sdk/constants/token-map/1/definitions/1-0xbc15ddb819ce347f8a67b5485910b8a37911f283.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xbc15ddb819ce347f8a67b5485910b8a37911f283",
+  chain_id: 1,
+  contract_address: "0xbc15ddb819ce347f8a67b5485910b8a37911f283",
+  name: "SolvBTC-Ignition LP Token",
+  symbol: "SolvBTC-FBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xbc15ddb819ce347f8a67b5485910b8a37911f283",
+  type: "lp",
+  token0: "1-0x7a56e1c57c7475ccf742a1832b028f0456652f97",
+  token1: "1-0xc96de26018a54d51c097160568752c4e3bd6c364",
+});
