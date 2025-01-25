72a9357e7353f5b9ad648a621b5893911290e11a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:13:44 -0800

    feat(token-map): Update Token -- 1-0x8a05bc1665059d5e19e30a66cf077f090f88609f -- CIAN YIELD Layer fBTC-pumpBTC.bera LP Token (ylBTCLST-pumpBTC.bera) (0x8a05...609f)
    Token Details:
    - Id: 1-0x8a05bc1665059d5e19e30a66cf077f090f88609f
    - Chain Id: 1
    - Contract Address: 0x8a05bc1665059d5e19e30a66cf077f090f88609f
    - Name: CIAN YIELD Layer fBTC-pumpBTC.bera LP Token
    - Symbol: ylBTCLST-pumpBTC.bera
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x8a05bc1665059d5e19e30a66cf077f090f88609f
    - Token 0: 1-0x6c77bde03952bbcb923815d90a73a7ed7ec895d1
    - Token 1: 1-0xadc9c900b05f39f48bb6f402a1bae60929f4f9a8

---
+++ sdk/constants/token-map/1/definitions/1-0x8a05bc1665059d5e19e30a66cf077f090f88609f.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8a05bc1665059d5e19e30a66cf077f090f88609f",
+  chain_id: 1,
+  contract_address: "0x8a05bc1665059d5e19e30a66cf077f090f88609f",
+  name: "CIAN YIELD Layer fBTC-pumpBTC.bera LP Token",
+  symbol: "ylBTCLST-pumpBTC.bera",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x8a05bc1665059d5e19e30a66cf077f090f88609f",
+  type: "lp",
+  token0: "1-0x6c77bde03952bbcb923815d90a73a7ed7ec895d1",
+  token1: "1-0xadc9c900b05f39f48bb6f402a1bae60929f4f9a8",
+});
