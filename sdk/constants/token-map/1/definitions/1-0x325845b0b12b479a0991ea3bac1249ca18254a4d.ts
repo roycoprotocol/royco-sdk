4939e020483861b3fedfccab030d5dc4177af77c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:14:59 -0800

    feat(token-map): Update Token -- 1-0x325845b0b12b479a0991ea3bac1249ca18254a4d -- Universal BTC-CIAN YIELD Layer fBTC LP Token (UNIBTC-ylBTCLST) (0x3258...4a4d)
    Token Details:
    - Id: 1-0x325845b0b12b479a0991ea3bac1249ca18254a4d
    - Chain Id: 1
    - Contract Address: 0x325845b0b12b479a0991ea3bac1249ca18254a4d
    - Name: Universal BTC-CIAN YIELD Layer fBTC LP Token
    - Symbol: UNIBTC-ylBTCLST
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x325845b0b12b479a0991ea3bac1249ca18254a4d
    - Token 0: 1-0x004e9c3ef86bc1ca1f0bb5c7662861ee93350568
    - Token 1: 1-0x6c77bde03952bbcb923815d90a73a7ed7ec895d1

---
+++ sdk/constants/token-map/1/definitions/1-0x325845b0b12b479a0991ea3bac1249ca18254a4d.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x325845b0b12b479a0991ea3bac1249ca18254a4d",
+  chain_id: 1,
+  contract_address: "0x325845b0b12b479a0991ea3bac1249ca18254a4d",
+  name: "Universal BTC-CIAN YIELD Layer fBTC LP Token",
+  symbol: "UNIBTC-ylBTCLST",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x325845b0b12b479a0991ea3bac1249ca18254a4d",
+  type: "lp",
+  token0: "1-0x004e9c3ef86bc1ca1f0bb5c7662861ee93350568",
+  token1: "1-0x6c77bde03952bbcb923815d90a73a7ed7ec895d1",
+});
