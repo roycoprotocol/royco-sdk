e67348e656e4a5a7d2912d6ac2583cf9132dcb97
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:10:53 -0800

    feat(token-map): Update Token -- 1-0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90 -- Ethena USDe-USDC LP Token (USDe-USDC) (0x1e49...3a90)
    Token Details:
    - Id: 1-0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90
    - Chain Id: 1
    - Contract Address: 0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90
    - Name: Ethena USDe-USDC LP Token
    - Symbol: USDe-USDC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90
    - Token 0: 1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3
    - Token 1: 1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48

---
+++ sdk/constants/token-map/1/definitions/1-0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90",
+  chain_id: 1,
+  contract_address: "0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90",
+  name: "Ethena USDe-USDC LP Token",
+  symbol: "USDe-USDC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x1e496fae4613b4e9c4f8fc31826812cdcbd03a90",
+  type: "lp",
+  token0: "1-0x4c9edd5852cd905f086c759e8383e09bff1e68b3",
+  token1: "1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+});
