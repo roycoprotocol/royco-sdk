7677ec375e969adb58ef1cec33704239b7f443f2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 14:09:16 -0800

    feat(market-map): Update market -- 1_0_0x431f01f338ff3ec6348f14ffd5702ace4cb33c2106aca184a4cacc4ebd406b34 -- Recipe (0x431f...6b34)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x431f01f338ff3ec6348f14ffd5702ace4cb33c2106aca184a4cacc4ebd406b34 (Market Hash)
    - Request Timestamp: Sat, 25 Jan 2025 22:09:15 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x431f01f338ff3ec6348f14ffd5702ace4cb33c2106aca184a4cacc4ebd406b34.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x431f01f338ff3ec6348f14ffd5702ace4cb33c2106aca184a4cacc4ebd406b34`,
+  name: `Kodiak WETH-BERAETH test`,
+  description: `Kodiak WETH-BERAETH test`,
+  is_verified: false,
+  category: `boyco`,
+});
