7e4cd49c4ba30e25550b42f97d941b8e4a6219b4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:10:48 -0800

    feat(token-map): Update Token -- 1-0x3041cbd36888becc7bbcbc0045e3b1f144466f5f -- USDC-Tether USDt LP Token (USDC-USDT) (0x3041...6f5f)
    Token Details:
    - Id: 1-0x3041cbd36888becc7bbcbc0045e3b1f144466f5f
    - Chain Id: 1
    - Contract Address: 0x3041cbd36888becc7bbcbc0045e3b1f144466f5f
    - Name: USDC-Tether USDt LP Token
    - Symbol: USDC-USDT
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x3041cbd36888becc7bbcbc0045e3b1f144466f5f
    - Token 0: 1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
    - Token 1: 1-0xdac17f958d2ee523a2206206994597c13d831ec7

---
+++ sdk/constants/token-map/1/definitions/1-0x3041cbd36888becc7bbcbc0045e3b1f144466f5f.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x3041cbd36888becc7bbcbc0045e3b1f144466f5f",
+  chain_id: 1,
+  contract_address: "0x3041cbd36888becc7bbcbc0045e3b1f144466f5f",
+  name: "USDC-Tether USDt LP Token",
+  symbol: "USDC-USDT",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x3041cbd36888becc7bbcbc0045e3b1f144466f5f",
+  type: "lp",
+  token0: "1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  token1: "1-0xdac17f958d2ee523a2206206994597c13d831ec7",
+});
