c66c1f78736727066311b0c482246d377c582cfe
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 14:19:06 -0800

    feat(market-map): Update market -- 1_0_0x2242fddcee8fab6c3f73730c7221e324893c72d9e2cc1d811f6f07df15338a50 -- Recipe (0x2242...8a50)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x2242fddcee8fab6c3f73730c7221e324893c72d9e2cc1d811f6f07df15338a50 (Market Hash)
    - Request Timestamp: Sat, 25 Jan 2025 22:19:06 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x2242fddcee8fab6c3f73730c7221e324893c72d9e2cc1d811f6f07df15338a50.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x2242fddcee8fab6c3f73730c7221e324893c72d9e2cc1d811f6f07df15338a50`,
+  name: `Kodiak uniBTC-uniBTCOT test`,
+  description: `Kodiak uniBTC-uniBTCOT test`,
+  is_verified: false,
+  category: `boyco`,
+});
