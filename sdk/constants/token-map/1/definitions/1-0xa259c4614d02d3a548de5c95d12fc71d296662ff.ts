43f0dc1e284adf406618965884530f7f0a70a694
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:14:16 -0800

    feat(token-map): Update Token -- 1-0xa259c4614d02d3a548de5c95d12fc71d296662ff -- Wrapped Bitcoin-Ignition LP Token (WBTC-FBTC) (0xa259...62ff)
    Token Details:
    - Id: 1-0xa259c4614d02d3a548de5c95d12fc71d296662ff
    - Chain Id: 1
    - Contract Address: 0xa259c4614d02d3a548de5c95d12fc71d296662ff
    - Name: Wrapped Bitcoin-Ignition LP Token
    - Symbol: WBTC-FBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xa259c4614d02d3a548de5c95d12fc71d296662ff
    - Token 0: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Token 1: 1-0xc96de26018a54d51c097160568752c4e3bd6c364

---
+++ sdk/constants/token-map/1/definitions/1-0xa259c4614d02d3a548de5c95d12fc71d296662ff.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xa259c4614d02d3a548de5c95d12fc71d296662ff",
+  chain_id: 1,
+  contract_address: "0xa259c4614d02d3a548de5c95d12fc71d296662ff",
+  name: "Wrapped Bitcoin-Ignition LP Token",
+  symbol: "WBTC-FBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xa259c4614d02d3a548de5c95d12fc71d296662ff",
+  type: "lp",
+  token0: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  token1: "1-0xc96de26018a54d51c097160568752c4e3bd6c364",
+});
