71279edd428ab68dbbee399680d71a0f8e8cb867
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:14:24 -0800

    feat(token-map): Update Token -- 1-0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751 -- Kelp DAO Restaked ETH-WETH LP Token (RSETH-WETH) (0x94b7...f751)
    Token Details:
    - Id: 1-0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751
    - Chain Id: 1
    - Contract Address: 0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751
    - Name: Kelp DAO Restaked ETH-WETH LP Token
    - Symbol: RSETH-WETH
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751
    - Token 0: 1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7
    - Token 1: 1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2

---
+++ sdk/constants/token-map/1/definitions/1-0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751",
+  chain_id: 1,
+  contract_address: "0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751",
+  name: "Kelp DAO Restaked ETH-WETH LP Token",
+  symbol: "RSETH-WETH",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x94b78ebaf10fdb9d832860f2e2cd8ee52d39f751",
+  type: "lp",
+  token0: "1-0xa1290d69c65a6fe4df752f95823fae25cb99e5a7",
+  token1: "1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+});
