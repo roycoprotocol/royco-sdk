79ec051d82664f168df8e82b676343439c275766
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 14:11:07 -0800

    feat(market-map): Update market -- 1_0_0xbf379151853f6a5c1509ce6708861c0178ee3c17917dabf996fc47cdf87a6d4a -- Recipe (0xbf37...6d4a)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xbf379151853f6a5c1509ce6708861c0178ee3c17917dabf996fc47cdf87a6d4a (Market Hash)
    - Request Timestamp: Sat, 25 Jan 2025 22:11:07 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xbf379151853f6a5c1509ce6708861c0178ee3c17917dabf996fc47cdf87a6d4a.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xbf379151853f6a5c1509ce6708861c0178ee3c17917dabf996fc47cdf87a6d4a`,
+  name: `Kodiak NECT-HONEY test`,
+  description: `Kodiak NECT-HONEY test`,
+  is_verified: false,
+  category: `boyco`,
+});
