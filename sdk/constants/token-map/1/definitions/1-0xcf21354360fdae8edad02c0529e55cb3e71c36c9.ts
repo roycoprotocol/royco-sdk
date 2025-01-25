ebcb3b15e221c5512a31536fe2a443cea3cdaf2b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:11:03 -0800

    feat(token-map): Update Token -- 1-0xcf21354360fdae8edad02c0529e55cb3e71c36c9 -- Magic Internet Money-USDC LP Token (MIM-USDC) (0xcf21...36c9)
    Token Details:
    - Id: 1-0xcf21354360fdae8edad02c0529e55cb3e71c36c9
    - Chain Id: 1
    - Contract Address: 0xcf21354360fdae8edad02c0529e55cb3e71c36c9
    - Name: Magic Internet Money-USDC LP Token
    - Symbol: MIM-USDC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xcf21354360fdae8edad02c0529e55cb3e71c36c9
    - Token 0: 1-0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3
    - Token 1: 1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48

---
+++ sdk/constants/token-map/1/definitions/1-0xcf21354360fdae8edad02c0529e55cb3e71c36c9.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xcf21354360fdae8edad02c0529e55cb3e71c36c9",
+  chain_id: 1,
+  contract_address: "0xcf21354360fdae8edad02c0529e55cb3e71c36c9",
+  name: "Magic Internet Money-USDC LP Token",
+  symbol: "MIM-USDC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xcf21354360fdae8edad02c0529e55cb3e71c36c9",
+  type: "lp",
+  token0: "1-0x99d8a9c45b2eca8864373a26d1459e3dff1e17f3",
+  token1: "1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+});
