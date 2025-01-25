2dae178a0e926e39322f3ecc7967bb16a2e00103
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:11:58 -0800

    feat(token-map): Update Token -- 1-0xab2f42fed7f2c1aed5ca8a20139313abea74790b -- CIAN yield layer stETH-WETH LP Token (ylstETH-WETH) (0xab2f...790b)
    Token Details:
    - Id: 1-0xab2f42fed7f2c1aed5ca8a20139313abea74790b
    - Chain Id: 1
    - Contract Address: 0xab2f42fed7f2c1aed5ca8a20139313abea74790b
    - Name: CIAN yield layer stETH-WETH LP Token
    - Symbol: ylstETH-WETH
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0xab2f42fed7f2c1aed5ca8a20139313abea74790b
    - Token 0: 1-0xb13aa2d0345b0439b064f26b82d8dcf3f508775d
    - Token 1: 1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2

---
+++ sdk/constants/token-map/1/definitions/1-0xab2f42fed7f2c1aed5ca8a20139313abea74790b.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xab2f42fed7f2c1aed5ca8a20139313abea74790b",
+  chain_id: 1,
+  contract_address: "0xab2f42fed7f2c1aed5ca8a20139313abea74790b",
+  name: "CIAN yield layer stETH-WETH LP Token",
+  symbol: "ylstETH-WETH",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0xab2f42fed7f2c1aed5ca8a20139313abea74790b",
+  type: "lp",
+  token0: "1-0xb13aa2d0345b0439b064f26b82d8dcf3f508775d",
+  token1: "1-0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+});
