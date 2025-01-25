1d792609f3937489adcc2c06580fc7e07356ab05
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:12:50 -0800

    feat(token-map): Update Token -- 1-0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de -- Wrapped Bitcoin-SolvBTC LP Token (WBTC-SolvBTC) (0x16e5...b2de)
    Token Details:
    - Id: 1-0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de
    - Chain Id: 1
    - Contract Address: 0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de
    - Name: Wrapped Bitcoin-SolvBTC LP Token
    - Symbol: WBTC-SolvBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de
    - Token 0: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Token 1: 1-0x7a56e1c57c7475ccf742a1832b028f0456652f97

---
+++ sdk/constants/token-map/1/definitions/1-0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de",
+  chain_id: 1,
+  contract_address: "0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de",
+  name: "Wrapped Bitcoin-SolvBTC LP Token",
+  symbol: "WBTC-SolvBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x16e5060779b75f2f9b748d0e1c0f89a13f35b2de",
+  type: "lp",
+  token0: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  token1: "1-0x7a56e1c57c7475ccf742a1832b028f0456652f97",
+});
