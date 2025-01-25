965011c502bbbda4d0c7bb3872027dc8627e27e4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:13:51 -0800

    feat(token-map): Update Token -- 1-0x42a094364bbdca0efac8af2cf7d6b9ec885ee554 -- Wrapped aBTC-Wrapped Bitcoin LP Token (waBTC-WBTC) (0x42a0...e554)
    Token Details:
    - Id: 1-0x42a094364bbdca0efac8af2cf7d6b9ec885ee554
    - Chain Id: 1
    - Contract Address: 0x42a094364bbdca0efac8af2cf7d6b9ec885ee554
    - Name: Wrapped aBTC-Wrapped Bitcoin LP Token
    - Symbol: waBTC-WBTC
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x42a094364bbdca0efac8af2cf7d6b9ec885ee554
    - Token 0: 1-0x09def5abc67e967d54e8233a4b5ebbc1b3fbe34b
    - Token 1: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599

---
+++ sdk/constants/token-map/1/definitions/1-0x42a094364bbdca0efac8af2cf7d6b9ec885ee554.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x42a094364bbdca0efac8af2cf7d6b9ec885ee554",
+  chain_id: 1,
+  contract_address: "0x42a094364bbdca0efac8af2cf7d6b9ec885ee554",
+  name: "Wrapped aBTC-Wrapped Bitcoin LP Token",
+  symbol: "waBTC-WBTC",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x42a094364bbdca0efac8af2cf7d6b9ec885ee554",
+  type: "lp",
+  token0: "1-0x09def5abc67e967d54e8233a4b5ebbc1b3fbe34b",
+  token1: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+});
