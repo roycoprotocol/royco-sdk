2972eb31b8e623181eb8549ae2b466f350c444d6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 10:13:06 -0800

    feat(token-map): Update Token -- 1-0x8460de55c1d491fb0fde1bd3e40c1319be4dceed -- Wrapped Bitcoin-pumpBTC.bera LP Token (WBTC-pumpBTC.bera) (0x8460...ceed)
    Token Details:
    - Id: 1-0x8460de55c1d491fb0fde1bd3e40c1319be4dceed
    - Chain Id: 1
    - Contract Address: 0x8460de55c1d491fb0fde1bd3e40c1319be4dceed
    - Name: Wrapped Bitcoin-pumpBTC.bera LP Token
    - Symbol: WBTC-pumpBTC.bera
    - Image: https://chainlist.org/unknown-logo.png
    - Decimals: 18
    - Source: lp
    - Search Id: 1-0x8460de55c1d491fb0fde1bd3e40c1319be4dceed
    - Token 0: 1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
    - Token 1: 1-0xadc9c900b05f39f48bb6f402a1bae60929f4f9a8

---
+++ sdk/constants/token-map/1/definitions/1-0x8460de55c1d491fb0fde1bd3e40c1319be4dceed.ts
@@ -0,0 +1,16 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8460de55c1d491fb0fde1bd3e40c1319be4dceed",
+  chain_id: 1,
+  contract_address: "0x8460de55c1d491fb0fde1bd3e40c1319be4dceed",
+  name: "Wrapped Bitcoin-pumpBTC.bera LP Token",
+  symbol: "WBTC-pumpBTC.bera",
+  image: "https://chainlist.org/unknown-logo.png",
+  decimals: 18,
+  source: "lp",
+  search_id: "1-0x8460de55c1d491fb0fde1bd3e40c1319be4dceed",
+  type: "lp",
+  token0: "1-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  token1: "1-0xadc9c900b05f39f48bb6f402a1bae60929f4f9a8",
+});
