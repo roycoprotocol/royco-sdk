25e315697ac92286bab01367ad24d4fe4f62fd48
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 14:05:26 -0800

    feat(market-map): Update market -- 1_0_0x567d727e1596cdc3db970abe62ee3c43b05ecaa745e55dd3e818a3d7dd69e62b -- Recipe (0x567d...e62b) (#304)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x567d727e1596cdc3db970abe62ee3c43b05ecaa745e55dd3e818a3d7dd69e62b (Market Hash)
    - Request Timestamp: Sat, 25 Jan 2025 22:05:24 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x567d727e1596cdc3db970abe62ee3c43b05ecaa745e55dd3e818a3d7dd69e62b.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x567d727e1596cdc3db970abe62ee3c43b05ecaa745e55dd3e818a3d7dd69e62b`,
+  name: `Kodiak USDC-USDT test`,
+  description: `Kodiak USDC-USDT test`,
+  is_verified: false,
+  category: `boyco`,
+});
