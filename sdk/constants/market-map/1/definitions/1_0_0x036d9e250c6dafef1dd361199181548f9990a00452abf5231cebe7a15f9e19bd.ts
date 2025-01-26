a7c5a368bef2714db42f05e9ff2f59d5d38c3980
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 08:08:30 -0800

    feat(market-map): Update market -- 1_0_0x036d9e250c6dafef1dd361199181548f9990a00452abf5231cebe7a15f9e19bd -- Recipe (0x036d...19bd) (#336)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x036d9e250c6dafef1dd361199181548f9990a00452abf5231cebe7a15f9e19bd (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 16:08:28 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x036d9e250c6dafef1dd361199181548f9990a00452abf5231cebe7a15f9e19bd.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x036d9e250c6dafef1dd361199181548f9990a00452abf5231cebe7a15f9e19bd`,
+  name: `Restake pumpBTC on Satlayer`,
+  description: `Deposit pumpBTC into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
+  is_verified: false,
+  category: `boyco`,
+});
