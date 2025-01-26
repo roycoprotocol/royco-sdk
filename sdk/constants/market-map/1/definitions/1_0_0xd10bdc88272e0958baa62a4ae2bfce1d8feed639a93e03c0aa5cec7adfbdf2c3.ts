9fb08242df24b33b8ab8607653697b215d9a4c5b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:22:22 -0800

    feat(market-map): Update market -- 1_0_0xd10bdc88272e0958baa62a4ae2bfce1d8feed639a93e03c0aa5cec7adfbdf2c3 -- Recipe (0xd10b...f2c3)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xd10bdc88272e0958baa62a4ae2bfce1d8feed639a93e03c0aa5cec7adfbdf2c3 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:22:21 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xd10bdc88272e0958baa62a4ae2bfce1d8feed639a93e03c0aa5cec7adfbdf2c3.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xd10bdc88272e0958baa62a4ae2bfce1d8feed639a93e03c0aa5cec7adfbdf2c3`,
+  name: `Suppy uniBTC on Dolomite`,
+  description: `Supply uniBTC into the Dolomite money market on Berachain. This asset is collateral only.`,
+  is_verified: false,
+  category: `boyco`,
+});
