322baad963127ee692eea6a1a9d5a56552d31dfe
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 07:43:13 -0800

    feat(market-map): Update market -- 1_0_0x0964848864e96952ee2454ce58fc93b867f9b2d9a6b44216eec8b08726813d1b -- Recipe (0x0964...3d1b)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x0964848864e96952ee2454ce58fc93b867f9b2d9a6b44216eec8b08726813d1b (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 15:43:12 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x0964848864e96952ee2454ce58fc93b867f9b2d9a6b44216eec8b08726813d1b.ts
@@ -0,0 +1,11 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x0964848864e96952ee2454ce58fc93b867f9b2d9a6b44216eec8b08726813d1b`,
+  name: `D2 Finance dgnBeraland, Boyco`,
+  description: `dgnBeraland is a one-click solution for 1-3x leveraged upside exposure to BERA, while staying protected from market swings. By fully leveraging partners like Beraborrow, IVX, and OOGA Booga, the strategy delivers sophisticated leverage through options + additional yield, while D2 actively manages positions.
+
+What sets dgnBeraland apart is its options-based strategy, turning market movements into opportunities for bigger gains. Unlike traditional BERA staking or vanilla leverage, it combines smart risk management with advanced tools to optimize returns.`,
+  is_verified: false,
+  category: `boyco`,
+});
