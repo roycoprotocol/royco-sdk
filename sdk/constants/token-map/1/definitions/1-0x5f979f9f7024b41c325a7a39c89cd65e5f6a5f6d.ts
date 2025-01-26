6960d16dd12aa02f46d2843171bf73ee02b19dd6
Leo <leo.h@hotmail.de>
2025-01-26 23:39:40 +0200

    Update 1-0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d.ts
    swapped the gdrive image with a imgur image.

--- sdk/constants/token-map/1/definitions/1-0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d.ts
+++ sdk/constants/token-map/1/definitions/1-0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d",
   name: "Concrete Boyco Bags",
   symbol: "CBAGS",
-  image: "https://drive.google.com/file/d/1mr65vznWtGKgEzPIyqHAk2Jwkx9afPol/view",
+  image: "https://i.imgur.com/s1DSZXr.jpeg",
   decimals: 18,
   source: "external",
   search_id: "none",
