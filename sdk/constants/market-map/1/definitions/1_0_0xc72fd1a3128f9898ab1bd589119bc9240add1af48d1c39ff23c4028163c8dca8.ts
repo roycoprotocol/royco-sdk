74ca10767f514bfa3e0e2e07e6d822bcb917e6c3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 13:57:10 -0800

    feat(market-map): Update market -- 1_0_0xc72fd1a3128f9898ab1bd589119bc9240add1af48d1c39ff23c4028163c8dca8 -- Recipe (0xc72f...dca8) (#423)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xc72fd1a3128f9898ab1bd589119bc9240add1af48d1c39ff23c4028163c8dca8 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 21:57:08 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xc72fd1a3128f9898ab1bd589119bc9240add1af48d1c39ff23c4028163c8dca8.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xc72fd1a3128f9898ab1bd589119bc9240add1af48d1c39ff23c4028163c8dca8`,
+  name: `Concrete x Lombard - wBTC`,
+  description: `Concrete is a protocol building yield, money market, and derivatives strategies. Their Boyco Market is a vault that takes in assets to deploy on user’s behalf in the Berachain ecosystem. After mainnet, the vaults’ respective strategies will manage Lombard DeFi positions on various partner protocols that received liquidity commitments during the Boyco phase. These include providing DEX Liquidity as well as supplying into lending protocols.`,
+  is_verified: false,
+  category: `boyco`,
+});
