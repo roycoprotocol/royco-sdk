36895d85b8f336b50fd80cdb74c89baaf5547db7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 07:46:40 -0800

    feat(market-map): Update market -- 1_0_0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5 -- Recipe (0x7dad...dcd5)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 15:46:40 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5`,
+  name: `Restake solvBTC.bbn on SatLayer`,
+  description: `Deposit solvBTC.bbn into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
+  is_verified: false,
+  category: `boyco`,
+});
