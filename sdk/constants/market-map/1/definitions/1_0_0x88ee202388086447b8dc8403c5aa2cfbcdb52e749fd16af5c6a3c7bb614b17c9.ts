03a0570a80e93d98c77ac5b0b36474fc85a97b3a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 08:24:22 -0800

    feat(market-map): Update market -- 1_0_0x88ee202388086447b8dc8403c5aa2cfbcdb52e749fd16af5c6a3c7bb614b17c9 -- Recipe (0x88ee...17c9) (#341)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x88ee202388086447b8dc8403c5aa2cfbcdb52e749fd16af5c6a3c7bb614b17c9 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 16:24:20 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x88ee202388086447b8dc8403c5aa2cfbcdb52e749fd16af5c6a3c7bb614b17c9.ts
@@ -0,0 +1,11 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x88ee202388086447b8dc8403c5aa2cfbcdb52e749fd16af5c6a3c7bb614b17c9`,
+  name: `Beraborrow STONE Boyco`,
+  description: `Single sided Liquid Stability Pool:
+Deposit STONE into Boyco. STONE is bridged &amp; deposited into Beraborrow. STONE is then used to mint NECT at 300% collateral ratio, and then NECT is supplied into the Liquid Stability Pool.
+This is part of the Berachain Boyco pre-deposit campaign.`,
+  is_verified: false,
+  category: `boyco`,
+});
