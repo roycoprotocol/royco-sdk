784aa4a735436f50c309d0d2913920976889c803
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 07:43:49 -0800

    feat(market-map): Update market -- 1_0_0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769 -- Recipe (0xdd3f...3769)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 15:43:48 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769`,
+  name: `Restake uniBTC on SatLayer`,
+  description: `Deposit uniBTC into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
+  is_verified: false,
+  category: `boyco`,
+});
