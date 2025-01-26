06860a12ae1ee9a1f41d5de09f7a8a7bf7ed3f29
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:51:45 -0800

    feat(market-map): Update market -- 1_0_0x2dd74f8f8a8d7f27b2a82a6edce57b201f9b4a3c4780934caf99363115e48be6 -- Recipe (0x2dd7...8be6) (#394)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x2dd74f8f8a8d7f27b2a82a6edce57b201f9b4a3c4780934caf99363115e48be6 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:51:43 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x2dd74f8f8a8d7f27b2a82a6edce57b201f9b4a3c4780934caf99363115e48be6.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x2dd74f8f8a8d7f27b2a82a6edce57b201f9b4a3c4780934caf99363115e48be6`,
+  name: `Supply sUSDa on Dolomite`,
+  description: `Supply sUSDa into the Dolomite money market on Berachain. This asset will be collateral only.`,
+  is_verified: false,
+  category: `boyco`,
+});
