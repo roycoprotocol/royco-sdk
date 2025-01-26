120e0497f703bf193a2b3e6bb6bbba0de4094d95
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:19:00 -0800

    feat(market-map): Update market -- 1_0_0xbe5cd829fcb3cdfe8224ad72fc3379198d38da26131c5b7ab6664c8f56a9730d -- Recipe (0xbe5c...730d)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xbe5cd829fcb3cdfe8224ad72fc3379198d38da26131c5b7ab6664c8f56a9730d (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:18:59 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xbe5cd829fcb3cdfe8224ad72fc3379198d38da26131c5b7ab6664c8f56a9730d.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xbe5cd829fcb3cdfe8224ad72fc3379198d38da26131c5b7ab6664c8f56a9730d`,
+  name: `Supply NECT on Dolomite`,
+  description: `Supply NECT into the Dolomite money market on Berachain. This asset will earn lending yield.`,
+  is_verified: false,
+  category: `boyco`,
+});
