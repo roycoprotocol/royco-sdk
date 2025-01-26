d999d486ccdf01dc5d72787b9a0880b98df9a8dd
Leo <leo.h@hotmail.de>
2025-01-26 23:20:51 +0200

    Create 1-0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d.ts
    Submitting "Concrete Boyco Bags"

---
+++ sdk/constants/token-map/1/definitions/1-0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d",
+  chain_id: 1,
+  contract_address: "0x5f979f9f7024b41c325a7a39c89cd65e5f6a5f6d",
+  name: "Concrete Boyco Bags",
+  symbol: "CBAGS",
+  image: "https://drive.google.com/file/d/1mr65vznWtGKgEzPIyqHAk2Jwkx9afPol/view",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
