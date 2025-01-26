32d8c08b1ebea39c2867f1af9b1ece89a0ee05fd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 08:33:20 -0800

    feat(market-map): Update market -- 1_0_0xa6905c68ad66ea9ce966aa1662e1417df08be333ab8ec04507e0f0301d3a78e9 -- Recipe (0xa690...78e9) (#354)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xa6905c68ad66ea9ce966aa1662e1417df08be333ab8ec04507e0f0301d3a78e9 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 16:33:18 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xa6905c68ad66ea9ce966aa1662e1417df08be333ab8ec04507e0f0301d3a78e9.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xa6905c68ad66ea9ce966aa1662e1417df08be333ab8ec04507e0f0301d3a78e9`,
+  name: `Supply wBTC on Dolomite x Infrared`,
+  description: `Supply wBTC into the Dolomite money market on Berachain. This asset will earn lending yield and be eligible for potential Infrared incentives.`,
+  is_verified: false,
+  category: `boyco`,
+});
