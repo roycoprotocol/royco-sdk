02f4783fd03ca9999888809e490da99bdf9621ee
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 13:52:53 -0800

    feat(market-map): Update market -- 1_0_0xd830e95e4a9a5bddf1e9dea286445f4613ff316c07d5e9b11844e2e8e243ad95 -- Recipe (0xd830...ad95) (#421)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xd830e95e4a9a5bddf1e9dea286445f4613ff316c07d5e9b11844e2e8e243ad95 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 21:52:51 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xd830e95e4a9a5bddf1e9dea286445f4613ff316c07d5e9b11844e2e8e243ad95.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xd830e95e4a9a5bddf1e9dea286445f4613ff316c07d5e9b11844e2e8e243ad95`,
+  name: `Concrete x Ethena - sUSDe Vault`,
+  description: `Concrete is a protocol building yield, money market, and derivatives strategies. Their Boyco Market is a vault that takes in assets to deploy on user’s behalf in the Berachain ecosystem. After mainnet, the vaults’ respective strategies will manage sUSDe DeFi positions on various partner protocols that received liquidity commitments during the Boyco phase. These include providing DEX Liquidity as well as supplying into lending protocols.`,
+  is_verified: false,
+  category: `boyco`,
+});
