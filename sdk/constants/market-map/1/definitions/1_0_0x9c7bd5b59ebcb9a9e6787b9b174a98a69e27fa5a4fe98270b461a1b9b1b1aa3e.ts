5626f6ebaa2922a7cfc3e8959c734541d4322017
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 08:30:53 -0800

    feat(market-map): Update market -- 1_0_0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e -- Recipe (0x9c7b...aa3e) (#349)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 16:30:51 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e`,
+  name: `Supply USDT on Dolomite x Infrared`,
+  description: `Supply USDT into the Dolomite money market on Berachain. This asset will earn lending yield and be eligible for potential Infrared incentives.`,
+  is_verified: false,
+  category: `boyco`,
+});
