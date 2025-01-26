3bddddfbdf8fa30750bf87f39cb62a3eb2805e9e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 05:56:10 -0800

    feat(market-map): Update market -- 1_0_0x87b88dacbd04f88fe9ddfd3fe8347f3793611d001016069d467d786e4896993c -- Recipe (0x87b8...993c)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x87b88dacbd04f88fe9ddfd3fe8347f3793611d001016069d467d786e4896993c (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 13:56:09 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x87b88dacbd04f88fe9ddfd3fe8347f3793611d001016069d467d786e4896993c.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x87b88dacbd04f88fe9ddfd3fe8347f3793611d001016069d467d786e4896993c`,
+  name: `THJ&#x2F;Set and Forgetti - USDC Vault, Boyco`,
+  description: `Supply USDC into a vault for THJ&#x27;s Set and Forgetti Strategy. Assets will be split into USDC and HONEY, and LP&#x27;d onto BEX. Users forgo any BGT Rewards for this deposit.`,
+  is_verified: false,
+  category: `boyco`,
+});
