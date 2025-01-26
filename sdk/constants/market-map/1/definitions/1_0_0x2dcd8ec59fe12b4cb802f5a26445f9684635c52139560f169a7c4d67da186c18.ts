5101607d75e630e78be2252afe3905997963fd61
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 07:45:26 -0800

    feat(market-map): Update market -- 1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18 -- Recipe (0x2dcd...6c18) (#330)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 15:45:24 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18`,
+  name: `Restake waBTC on SatLayer`,
+  description: `Deposit waBTC into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
+  is_verified: false,
+  category: `boyco`,
+});
