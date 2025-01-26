b6966a10261d2c54d04f3e182fd53e82b04e79ce
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 08:03:58 -0800

    feat(market-map): Update market -- 1_0_0xde894ab596c084e65d0123ab6fa66f95b0571091cd8ec7efbeabe4942e7c40cd -- Recipe (0xde89...40cd) (#334)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xde894ab596c084e65d0123ab6fa66f95b0571091cd8ec7efbeabe4942e7c40cd (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 16:03:56 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xde894ab596c084e65d0123ab6fa66f95b0571091cd8ec7efbeabe4942e7c40cd.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xde894ab596c084e65d0123ab6fa66f95b0571091cd8ec7efbeabe4942e7c40cd`,
+  name: `Restake LBTC on SatLayer`,
+  description: `Deposit LBTC into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
+  is_verified: false,
+  category: `boyco`,
+});
