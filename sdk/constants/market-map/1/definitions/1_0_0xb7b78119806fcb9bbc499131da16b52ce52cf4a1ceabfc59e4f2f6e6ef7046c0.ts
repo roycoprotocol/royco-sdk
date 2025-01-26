bfcb39b9a954e82c82451a16e4c98daa8ec74936
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 07:52:23 -0800

    feat(market-map): Update market -- 1_0_0xb7b78119806fcb9bbc499131da16b52ce52cf4a1ceabfc59e4f2f6e6ef7046c0 -- Recipe (0xb7b7...46c0) (#333)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xb7b78119806fcb9bbc499131da16b52ce52cf4a1ceabfc59e4f2f6e6ef7046c0 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 15:52:21 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xb7b78119806fcb9bbc499131da16b52ce52cf4a1ceabfc59e4f2f6e6ef7046c0.ts
@@ -0,0 +1,11 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xb7b78119806fcb9bbc499131da16b52ce52cf4a1ceabfc59e4f2f6e6ef7046c0`,
+  name: `D2 Finance HyperBERA.USDC, Boyco`,
+  description: `HyperBERA.USDC prioritizes real yield generation with stability at its core. Building on our d2USDC strategy framework with proven track record to deliver a high yield delta neutral [+&#x2F;- 10d] strategy (historically ~20%+ APR across varying market conditions). This strategy combines funding arbitrage, yield farming, and an ensemble of strategies tailored to maximize opportunities. Unlike single-focus approaches like sUSDe, which rely primarily on funding arbitrage, HyperBERA.USDC also integrates options-driven yield extraction to consistently outperform in diverse market environments.
+
+ HyperBERA.USDC targets consistent and sustainable returns designed to exceed other stablecoin offerings. It’s not a stablecoin but rather a sophisticated ensemble of strategies, dynamically optimized to capitalize on the most attractive opportunities in the Berachain ecosystem in a low risk manner.`,
+  is_verified: false,
+  category: `boyco`,
+});
