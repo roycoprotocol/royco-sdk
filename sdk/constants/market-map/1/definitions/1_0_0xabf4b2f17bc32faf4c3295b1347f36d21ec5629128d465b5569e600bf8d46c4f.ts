57e5760a5f4e975170496d1d0b00938f225e15a6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 14:29:08 -0800

    feat(market-map): Update market -- 1_0_0xabf4b2f17bc32faf4c3295b1347f36d21ec5629128d465b5569e600bf8d46c4f -- Recipe (0xabf4...6c4f) (#432)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xabf4b2f17bc32faf4c3295b1347f36d21ec5629128d465b5569e600bf8d46c4f (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 22:29:06 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xabf4b2f17bc32faf4c3295b1347f36d21ec5629128d465b5569e600bf8d46c4f.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xabf4b2f17bc32faf4c3295b1347f36d21ec5629128d465b5569e600bf8d46c4f`,
+  name: `Veda x Ether.fi eBTC Vault - LBTC Supply`,
+  description: `After mainnet, vaults will run DeFi strategies on Berachain protocols participating in Boyco. Examples of applications include: Kodiak, Infrared, and Dolomite. eBTC supplied will be converted to eBTC on mainnet for DeFi usage.`,
+  is_verified: false,
+  category: `boyco`,
+});
