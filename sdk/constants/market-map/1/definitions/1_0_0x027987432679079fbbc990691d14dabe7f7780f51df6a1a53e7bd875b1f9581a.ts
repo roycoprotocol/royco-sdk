f1621b691061db61dfd73d8dd2a4e393f5e58268
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 07:31:17 -0800

    feat(market-map): Update market -- 1_0_0x027987432679079fbbc990691d14dabe7f7780f51df6a1a53e7bd875b1f9581a -- Recipe (0x0279...581a) (#324)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x027987432679079fbbc990691d14dabe7f7780f51df6a1a53e7bd875b1f9581a (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 15:31:15 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x027987432679079fbbc990691d14dabe7f7780f51df6a1a53e7bd875b1f9581a.ts
@@ -0,0 +1,13 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x027987432679079fbbc990691d14dabe7f7780f51df6a1a53e7bd875b1f9581a`,
+  name: `D2 Finance Kodiak++, Boyco`,
+  description: `Kodiak++ is the Berachain evolution of our flagship ETH++ strategy, fine-tuned for BGT yield maximization. 
+
+At its core, the strategy integrates looping mechanics through Dolomite and BERABorrow to dynamically optimize exposure and capital efficiency. Building on ETH++’s stellar 120% ROI track record in 2024, Kodiak++ adds a layer of advanced options-based risk management, ensuring superior returns while minimizing downside exposure. 
+
+It’s the ultimate strategy for those seeking high-yield opportunities in the Berachain ecosystem.`,
+  is_verified: false,
+  category: `boyco`,
+});
