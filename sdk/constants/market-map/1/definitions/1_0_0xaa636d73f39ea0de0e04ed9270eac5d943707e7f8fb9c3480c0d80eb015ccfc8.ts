6b92c7665758297fe56a0e7b601d12a7a75eb3fb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 10:08:43 -0800

    feat(market-map): Update market -- 1_0_0xaa636d73f39ea0de0e04ed9270eac5d943707e7f8fb9c3480c0d80eb015ccfc8 -- Recipe (0xaa63...cfc8) (#402)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xaa636d73f39ea0de0e04ed9270eac5d943707e7f8fb9c3480c0d80eb015ccfc8 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 18:08:41 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xaa636d73f39ea0de0e04ed9270eac5d943707e7f8fb9c3480c0d80eb015ccfc8.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xaa636d73f39ea0de0e04ed9270eac5d943707e7f8fb9c3480c0d80eb015ccfc8`,
+  name: `Provide pumpBTC.bera-ylpumpBTC Liquidity on Kodiak`,
+  description: `Deposit pumpBTC.bera-ylpumpBTC Uniswap V2 LP token(s) on Ethereum Mainnet and bridge these assets to Berachain. On Berachain, provide liquidity in the pumpBTC.bera-ylpumpBTC Kodiak Island, an automated liquidity management vault that tokenizes a &quot;concentrated&quot; range Uniswap V3 style liquidity position. This is part of Berachain&#x27;s Boyco pre-deposit campaign.`,
+  is_verified: false,
+  category: `boyco`,
+});
