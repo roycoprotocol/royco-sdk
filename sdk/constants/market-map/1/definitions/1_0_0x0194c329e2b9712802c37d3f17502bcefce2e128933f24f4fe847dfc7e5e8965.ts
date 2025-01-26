4ed009d6e1877a674d11bc6d32fbe1d63f831880
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:43:52 -0800

    feat(market-map): Update market -- 1_0_0x0194c329e2b9712802c37d3f17502bcefce2e128933f24f4fe847dfc7e5e8965 -- Recipe (0x0194...8965) (#389)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x0194c329e2b9712802c37d3f17502bcefce2e128933f24f4fe847dfc7e5e8965 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:43:50 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x0194c329e2b9712802c37d3f17502bcefce2e128933f24f4fe847dfc7e5e8965.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x0194c329e2b9712802c37d3f17502bcefce2e128933f24f4fe847dfc7e5e8965`,
+  name: `Supply ylBTCLST on Dolomite`,
+  description: `Supply ylBTCLST into the Dolomite money market on Berachain. This asset will be collateral only.`,
+  is_verified: false,
+  category: `boyco`,
+});
