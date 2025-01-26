b1eed3c3269f22940163cf803ea213d7c5a9aed9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 10:06:58 -0800

    feat(market-map): Update market -- 1_0_0x4dd921e829db80e73c56d888eeaf46a7934a3c4a2f7f78231dd4502f8eaa2558 -- Recipe (0x4dd9...2558)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x4dd921e829db80e73c56d888eeaf46a7934a3c4a2f7f78231dd4502f8eaa2558 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 18:06:58 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x4dd921e829db80e73c56d888eeaf46a7934a3c4a2f7f78231dd4502f8eaa2558.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x4dd921e829db80e73c56d888eeaf46a7934a3c4a2f7f78231dd4502f8eaa2558`,
+  name: `Lend wETH on Dahlia (STONE&#x2F;wETH)`,
+  description: `Earn PETALS (Dahlia Points) and BERA incentives for lending WETH on Dahlia STONE&#x2F;WETH (91% LLTV) lending market on Berachain.`,
+  is_verified: false,
+  category: `boyco`,
+});
