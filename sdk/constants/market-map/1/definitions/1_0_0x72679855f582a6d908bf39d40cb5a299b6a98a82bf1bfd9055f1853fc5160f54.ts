d7e4f6e9be049a4973a58fae30502ca2a5ae465d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 15:40:58 -0800

    feat(market-map): Update market -- 1_0_0x72679855f582a6d908bf39d40cb5a299b6a98a82bf1bfd9055f1853fc5160f54 -- Recipe (0x7267...0f54) (#437)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x72679855f582a6d908bf39d40cb5a299b6a98a82bf1bfd9055f1853fc5160f54 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 23:40:56 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x72679855f582a6d908bf39d40cb5a299b6a98a82bf1bfd9055f1853fc5160f54.ts
@@ -0,0 +1,11 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x72679855f582a6d908bf39d40cb5a299b6a98a82bf1bfd9055f1853fc5160f54`,
+  name: `Supply uniBTC into Goldilocks`,
+  description: `Goldilocks&#x27; yield splitting vaults enable users to trade the future earnings of yield bearing positions. Users can deposit assets into a yield splitting vault, and the vault will automatically use the assets to create yield bearing positions on supported platforms. Their Boyco markets will yield-split half of deposited assets, and LP the remainder against the OT (Ownership Token).
+
+Deposit uniBTC on Ethereum Mainnet and bridge the asset to Berachain. On Berachain, mint uniBTC-OT and uniBTC-YT with a portion of the uniBTC via Goldilocks, then pair the minted uniBTC-OT and remaining uniBTC to provide liquidity in the uniBTC&lt;&gt;uniBTC-OT Kodiak Island, an automated liquidity management vault that tokenizes a &quot;concentrated&quot; range Uniswap V3 style liquidity position.`,
+  is_verified: false,
+  category: `boyco`,
+});
