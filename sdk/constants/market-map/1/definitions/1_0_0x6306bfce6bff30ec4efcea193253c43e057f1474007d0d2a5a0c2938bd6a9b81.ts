d5f177d5f234df52f9a3fe5f94b1b5b73a3f7764
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:47:31 -0800

    feat(market-map): Update market -- 1_0_0x6306bfce6bff30ec4efcea193253c43e057f1474007d0d2a5a0c2938bd6a9b81 -- Recipe (0x6306...9b81)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x6306bfce6bff30ec4efcea193253c43e057f1474007d0d2a5a0c2938bd6a9b81 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:47:31 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x6306bfce6bff30ec4efcea193253c43e057f1474007d0d2a5a0c2938bd6a9b81.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x6306bfce6bff30ec4efcea193253c43e057f1474007d0d2a5a0c2938bd6a9b81`,
+  name: `Supply ylpumpBTC on Dolomite`,
+  description: `Supply ylpumpBTC into the Dolomite money market on Berachain. This asset will be collateral only.`,
+  is_verified: false,
+  category: `boyco`,
+});
