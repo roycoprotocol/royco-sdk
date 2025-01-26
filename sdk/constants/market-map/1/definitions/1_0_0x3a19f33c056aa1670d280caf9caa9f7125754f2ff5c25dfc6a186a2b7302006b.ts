d49a85d4e0856b5e66b8b0ac1c68fbb3083d8a7f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 13:53:59 -0800

    feat(market-map): Update market -- 1_0_0x3a19f33c056aa1670d280caf9caa9f7125754f2ff5c25dfc6a186a2b7302006b -- Recipe (0x3a19...006b)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x3a19f33c056aa1670d280caf9caa9f7125754f2ff5c25dfc6a186a2b7302006b (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 21:53:58 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x3a19f33c056aa1670d280caf9caa9f7125754f2ff5c25dfc6a186a2b7302006b.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x3a19f33c056aa1670d280caf9caa9f7125754f2ff5c25dfc6a186a2b7302006b`,
+  name: `Lombard x Ethena - LBTC`,
+  description: `Concrete is a protocol building yield, money market, and derivatives strategies. Their Boyco Market is a vault that takes in assets to deploy on user’s behalf in the Berachain ecosystem. After mainnet, the vaults’ respective strategies will manage Lombard DeFi positions on various partner protocols that received liquidity commitments during the Boyco phase. These include providing DEX Liquidity as well as supplying into lending protocols.`,
+  is_verified: false,
+  category: `boyco`,
+});
