3ced0c53a99539c869ae640d6e07e005120bba3c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 14:33:29 -0800

    feat(market-map): Update market -- 1_0_0xff0182973d5f1e9a64392c413caaa75f364f24632a7de0fdd1a31fe30517fdd2 -- Recipe (0xff01...fdd2)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xff0182973d5f1e9a64392c413caaa75f364f24632a7de0fdd1a31fe30517fdd2 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 22:33:28 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xff0182973d5f1e9a64392c413caaa75f364f24632a7de0fdd1a31fe30517fdd2.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xff0182973d5f1e9a64392c413caaa75f364f24632a7de0fdd1a31fe30517fdd2`,
+  name: `Veda x Ether.fi weETH Vault`,
+  description: `After mainnet, vaults will run DeFi strategies on Berachain protocols participating in Boyco. Examples of applications include: Kodiak, Infrared, and Dolomite. weETH supplied will be used for DeFi activity on Berachain Mainnet.`,
+  is_verified: false,
+  category: `boyco`,
+});
