6463a6f25f297f5b808e352915d5450f35b41681
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 08:28:03 -0800

    feat(market-map): Update market -- 1_0_0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f -- Recipe (0x9778...751f)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 16:28:02 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f`,
+  name: `Supply HONEY on Dolomite x Infrared`,
+  description: `Supply HONEY into the Dolomite money market on Berachain. This asset will earn lending yield and be eligible for potential Infrared incentives.`,
+  is_verified: false,
+  category: `boyco`,
+});
