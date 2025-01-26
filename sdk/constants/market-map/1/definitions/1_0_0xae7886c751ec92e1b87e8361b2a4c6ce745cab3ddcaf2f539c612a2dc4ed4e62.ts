198c3301c59f2656992e3e8d16671e8758bbd7a2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 17:59:29 -0800

    feat(market-map): Update market -- 1_0_0xae7886c751ec92e1b87e8361b2a4c6ce745cab3ddcaf2f539c612a2dc4ed4e62 -- Recipe (0xae78...4e62)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xae7886c751ec92e1b87e8361b2a4c6ce745cab3ddcaf2f539c612a2dc4ed4e62 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 01:59:29 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xae7886c751ec92e1b87e8361b2a4c6ce745cab3ddcaf2f539c612a2dc4ed4e62.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xae7886c751ec92e1b87e8361b2a4c6ce745cab3ddcaf2f539c612a2dc4ed4e62`,
+  name: `test market`,
+  description: `test market`,
+  is_verified: false,
+  category: `boyco`,
+});
