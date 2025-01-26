0db397a7c6d8ffeb1b53bfb5680f0584ae10d4fc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:52:39 -0800

    feat(market-map): Update market -- 1_0_0xc90525132d909f992363102ebd6298d95b1f312acdb9421fd1f7ac0c0dd78d3f -- Recipe (0xc905...8d3f)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xc90525132d909f992363102ebd6298d95b1f312acdb9421fd1f7ac0c0dd78d3f (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:52:38 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xc90525132d909f992363102ebd6298d95b1f312acdb9421fd1f7ac0c0dd78d3f.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xc90525132d909f992363102ebd6298d95b1f312acdb9421fd1f7ac0c0dd78d3f`,
+  name: `Supply rswETH on Dolomite`,
+  description: `Supply rswETH into the Dolomite money market on Berachain. This asset will be collateral only.`,
+  is_verified: false,
+  category: `boyco`,
+});
