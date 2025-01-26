9261bd4b86f9dfd4d7514f8f9261d3e0ac3f9f59
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:21:07 -0800

    feat(market-map): Update market -- 1_0_0x42a09eccabf1080c40a24522e9e8adbee5a0ad907188c9b6e50ba26ba332eac3 -- Recipe (0x42a0...eac3) (#377)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x42a09eccabf1080c40a24522e9e8adbee5a0ad907188c9b6e50ba26ba332eac3 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:21:05 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x42a09eccabf1080c40a24522e9e8adbee5a0ad907188c9b6e50ba26ba332eac3.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x42a09eccabf1080c40a24522e9e8adbee5a0ad907188c9b6e50ba26ba332eac3`,
+  name: `Supply SBTC on Dolomite`,
+  description: `Supply SBTC into the Dolomite money market on Berachain. This asset will earn lending yield.`,
+  is_verified: false,
+  category: `boyco`,
+});
