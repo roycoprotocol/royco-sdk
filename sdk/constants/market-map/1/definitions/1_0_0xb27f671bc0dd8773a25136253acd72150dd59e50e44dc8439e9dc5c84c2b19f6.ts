ef778c3195efa6ffefdf14831e5f9533fc2c19bf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:34:53 -0800

    feat(market-map): Update market -- 1_0_0xb27f671bc0dd8773a25136253acd72150dd59e50e44dc8439e9dc5c84c2b19f6 -- Recipe (0xb27f...19f6) (#385)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xb27f671bc0dd8773a25136253acd72150dd59e50e44dc8439e9dc5c84c2b19f6 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:34:51 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xb27f671bc0dd8773a25136253acd72150dd59e50e44dc8439e9dc5c84c2b19f6.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xb27f671bc0dd8773a25136253acd72150dd59e50e44dc8439e9dc5c84c2b19f6`,
+  name: `Supply STONE on Dolomite`,
+  description: `Supply STONE into the Dolomite money market on Berachain. This asset will be collateral only.`,
+  is_verified: false,
+  category: `boyco`,
+});
