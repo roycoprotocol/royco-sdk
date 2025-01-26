c7875718933de519c49ddc53e315175ba13654c0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 09:31:17 -0800

    feat(market-map): Update market -- 1_0_0x2a3a73ba927ec6bbf0e2e12e21a32e274a295389ce9d6ae2b32435d12c597c2c -- Recipe (0x2a3a...7c2c)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x2a3a73ba927ec6bbf0e2e12e21a32e274a295389ce9d6ae2b32435d12c597c2c (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 17:31:17 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x2a3a73ba927ec6bbf0e2e12e21a32e274a295389ce9d6ae2b32435d12c597c2c.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x2a3a73ba927ec6bbf0e2e12e21a32e274a295389ce9d6ae2b32435d12c597c2c`,
+  name: `Supply solvBTC.bbn on Dolomite`,
+  description: `Supply solvBTC.bbn into the Dolomite money market on Berachain. This asset will be collateral only.`,
+  is_verified: false,
+  category: `boyco`,
+});
