c256b3aa8048b2930f8cb453a108812aa1e88c8b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 08:57:07 -0800

    feat(market-map): Update market -- 1_0_0xa588ad19850cf2a111c3c727033da8e557abc94de70fce2d2b2f2f78140f15e5 -- Recipe (0xa588...15e5) (#367)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xa588ad19850cf2a111c3c727033da8e557abc94de70fce2d2b2f2f78140f15e5 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 16:57:05 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xa588ad19850cf2a111c3c727033da8e557abc94de70fce2d2b2f2f78140f15e5.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xa588ad19850cf2a111c3c727033da8e557abc94de70fce2d2b2f2f78140f15e5`,
+  name: `Supply USDe on Dolomite`,
+  description: `Supply USDe into the Dolomite money market on Berachain. This asset will earn lending yield.`,
+  is_verified: false,
+  category: `boyco`,
+});
