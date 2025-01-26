1137881106c8422fb62f7acf91ca0e4dfd5abbe4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-25 20:38:52 -0800

    feat(market-map): Update market -- 1_0_0x14028b015a5ed1e4340c16a8b6f582b49656dd20f2fc8c5af465de6a319c2504 -- Recipe (0x1402...2504) (#313)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x14028b015a5ed1e4340c16a8b6f582b49656dd20f2fc8c5af465de6a319c2504 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 04:38:50 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x14028b015a5ed1e4340c16a8b6f582b49656dd20f2fc8c5af465de6a319c2504.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x14028b015a5ed1e4340c16a8b6f582b49656dd20f2fc8c5af465de6a319c2504`,
+  name: `weETH Bridge Test`,
+  description: `weETH Bridge Test`,
+  is_verified: false,
+  category: `boyco`,
+});
