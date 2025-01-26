3c73171b550143317bb94284fe0925c726885ac3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 07:36:22 -0800

    feat(market-map): Update market -- 1_0_0xf8f745f188ddb10c16724faee95583521191c3c69e15490fa53c1136b73c17d7 -- Recipe (0xf8f7...17d7)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xf8f745f188ddb10c16724faee95583521191c3c69e15490fa53c1136b73c17d7 (Market Hash)
    - Request Timestamp: Sun, 26 Jan 2025 15:36:21 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xf8f745f188ddb10c16724faee95583521191c3c69e15490fa53c1136b73c17d7.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xf8f745f188ddb10c16724faee95583521191c3c69e15490fa53c1136b73c17d7`,
+  name: `Infrared x Kodiak USDT0-HONEY LP, Boyco`,
+  description: `Deposit USDT-USDC Uniswap V2 LP token(s) on Ethereum Mainnet and bridge the assets to Berachain. On Berachain, mint HONEY with all of the USDC, then provide liquidity in the USDT0-HONEY Kodiak Island, an automated liquidity management vault that tokenizes a &quot;concentrated&quot; range Uniswap V3 style liquidity position. Finally, stake the Island receipt token in the respective Infrared iBGT-compatible Vault.`,
+  is_verified: false,
+  category: `boyco`,
+});
