275e69ef027d3d9c0ce7cc699fe13e5f863ed0c0
JB <jbhav24@users.noreply.github.com>
2025-01-27 11:36:00 -0800

    Update 1_0_0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769.ts

--- sdk/constants/market-map/1/definitions/1_0_0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769.ts
+++ sdk/constants/market-map/1/definitions/1_0_0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769.ts
@@ -4,8 +4,9 @@ export default defineMarket({
   id: `1_0_0xdd3f0e11d59726f2e63fc1b180abc94034dd3e0f4018b51371b73348d82b3769`,
   name: `Restake uniBTC on SatLayer`,
   description: `Deposit uniBTC into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
-  is_verified: false,
+  is_verified: true,
   category: `boyco`,
+  incentive_ids: ["1-0x9c80538ffcbaee0db71caabe87ee99785ffc4f55"],
   external_incentives: [
     {
       token_id: "1-0xb000000000000000000000000000000000000bbb",
