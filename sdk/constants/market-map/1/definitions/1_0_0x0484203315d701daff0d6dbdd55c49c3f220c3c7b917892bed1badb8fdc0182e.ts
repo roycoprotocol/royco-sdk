2dbe3eea7eb264b500c98575068b2c1f759db83b
JB <jbhav24@users.noreply.github.com>
2025-01-27 12:07:05 -0800

    Update 1_0_0x0484203315d701daff0d6dbdd55c49c3f220c3c7b917892bed1badb8fdc0182e.ts

--- sdk/constants/market-map/1/definitions/1_0_0x0484203315d701daff0d6dbdd55c49c3f220c3c7b917892bed1badb8fdc0182e.ts
+++ sdk/constants/market-map/1/definitions/1_0_0x0484203315d701daff0d6dbdd55c49c3f220c3c7b917892bed1badb8fdc0182e.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `1_0_0x0484203315d701daff0d6dbdd55c49c3f220c3c7b917892bed1badb8fdc0182e`,
   name: `Veda x Ether.fi weETH Vault - wETH Supply`,
   description: `After mainnet, vaults will run DeFi strategies on Berachain protocols participating in Boyco. Examples of applications include: Kodiak, Infrared, and Dolomite. wETH supplied will be paired against weETH on mainnet for DeFi usage.`,
-  is_verified: false,
+  is_verified: true,
   category: `boyco`,
   external_incentives: [
     {
