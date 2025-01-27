2e237916e1f18711304447a0313cee10936fa8e9
GitHub Action <action@github.com>
2025-01-27 05:14:55 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/1/definitions/1-0x00000000000000000000000000000000000112.ts
+++ sdk/constants/token-map/1/definitions/1-0x00000000000000000000000000000000000112.ts
@@ -1,13 +1,12 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "1-0xB000000000000000000000000000000000000112",
+  id: "1-0xb000000000000000000000000000000000000112",
   chain_id: 1,
-  contract_address: "0xB000000000000000000000000000000000000112",
+  contract_address: "0xb000000000000000000000000000000000000112",
   name: "Plus Icon",
   symbol: "PLUS",
-  image:
-    "https://imgur.com/kJ5l3WL",
+  image: "https://imgur.com/kJ5l3WL",
   decimals: 18,
   source: "external",
   search_id: "none",
