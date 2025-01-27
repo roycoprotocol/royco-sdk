374e71be8e20968759c0a77ce650193b587d0386
JB <jbhav24@users.noreply.github.com>
2025-01-27 11:38:35 -0800

    Update 1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18.ts

--- sdk/constants/market-map/1/definitions/1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18.ts
+++ sdk/constants/market-map/1/definitions/1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18`,
   name: `Restake waBTC on SatLayer`,
   description: `Deposit waBTC into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
-  is_verified: false,
+  is_verified: true,
   incentive_ids: ["1-0x9c80538ffcbaee0db71caabe87ee99785ffc4f55"],
   category: `boyco`,
   external_incentives: [
