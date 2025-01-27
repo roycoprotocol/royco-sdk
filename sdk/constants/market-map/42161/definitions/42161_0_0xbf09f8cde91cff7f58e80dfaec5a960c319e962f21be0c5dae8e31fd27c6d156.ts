0ea9b0da57904375bcb1c959932c0cbf4979513f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-26 21:25:57 -0800

    feat(market-map): Update market -- 42161_0_0xbf09f8cde91cff7f58e80dfaec5a960c319e962f21be0c5dae8e31fd27c6d156 -- Recipe (0xbf09...d156)
    Market Details:
    - Chain ID: 42161
    - Market Type: 0 (Recipe)
    - Market ID: 0xbf09f8cde91cff7f58e80dfaec5a960c319e962f21be0c5dae8e31fd27c6d156 (Market Hash)
    - Request Timestamp: Mon, 27 Jan 2025 05:25:57 GMT

---
+++ sdk/constants/market-map/42161/definitions/42161_0_0xbf09f8cde91cff7f58e80dfaec5a960c319e962f21be0c5dae8e31fd27c6d156.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `42161_0_0xbf09f8cde91cff7f58e80dfaec5a960c319e962f21be0c5dae8e31fd27c6d156`,
+  name: `blurp`,
+  description: `this is a test blurp for blurpie`,
+  is_verified: false,
+});
