1acddf4b69df11e105f4f99435b7ed493fe1b4bc
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2025-01-27 10:20:22 -0800

    concrete verification

--- sdk/constants/market-map/1/definitions/1_0_0xece925dbccbb21333dbe99679fef655ad2dc2cb185e0963711c944e302595b28.ts
+++ sdk/constants/market-map/1/definitions/1_0_0xece925dbccbb21333dbe99679fef655ad2dc2cb185e0963711c944e302595b28.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `1_0_0xece925dbccbb21333dbe99679fef655ad2dc2cb185e0963711c944e302595b28`,
   name: `Supply WBTC to Concrete x Lombard Vault`,
   description: `When WBTC is supplied to this market, it is allocated across leading Berachain DEXs and lending applications such as Kodiak and Dolomite. The vault will also allocate to yield opportunities from leading protocols expanding to Berachain. This is part of the Berachain Boyco pre-deposit campaign.`,
-  is_verified: false,
+  is_verified: true,
   category: `boyco`,
   external_incentives: [
     {
