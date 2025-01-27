960dbc85d3b23d35c33df16e30e51401273008d4
Jack Corddry <64389215+corddry@users.noreply.github.com>
2025-01-27 12:34:04 -0800

    Update 1_0_0x7ecf55915abe3c24dc5d8365a8edabc8833f4efb8e7c027429c9528aed91ecb7.ts

--- sdk/constants/market-map/1/definitions/1_0_0x7ecf55915abe3c24dc5d8365a8edabc8833f4efb8e7c027429c9528aed91ecb7.ts
+++ sdk/constants/market-map/1/definitions/1_0_0x7ecf55915abe3c24dc5d8365a8edabc8833f4efb8e7c027429c9528aed91ecb7.ts
@@ -15,7 +15,7 @@ Earn DEX LP fees, as well as rewards from Berachain, Stakestone, Infrared and Ko
 
   external_incentives: [
     {
-      token_id: "1-0x3b2635c5d5cc5cee62b9084636f808c67da9988f",
+      token_id: "1-0x77d17183055303a15208c809b716dc02261129b7",
       label: "Infrared Points",
 
       value: async ({ roycoClient, chainClient }) => {
