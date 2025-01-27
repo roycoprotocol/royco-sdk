f3aa5db42b1d9e8579314ae41a0500e0aa768603
Jack Corddry <64389215+corddry@users.noreply.github.com>
2025-01-27 12:20:00 -0800

    Update 1_0_0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e.ts

--- sdk/constants/market-map/1/definitions/1_0_0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e.ts
+++ sdk/constants/market-map/1/definitions/1_0_0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e.ts
@@ -4,8 +4,10 @@ export default defineMarket({
   id: `1_0_0x9c7bd5b59ebcb9a9e6787b9b174a98a69e27fa5a4fe98270b461a1b9b1b1aa3e`,
   name: `Supply USDT on Dolomite x Infrared`,
   description: `Supply USDT into the Dolomite money market on Berachain. This asset will earn lending yield and be eligible for potential Infrared incentives.`,
-  is_verified: false,
+  is_verified: true,
   category: `boyco`,
+
+  incentive_ids: ["1-0x31dd27d7479b09f1c96aa94681845c0eb0026ef8"],
 
   external_incentives: [
     {
