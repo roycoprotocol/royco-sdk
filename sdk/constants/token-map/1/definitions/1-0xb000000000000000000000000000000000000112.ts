49ca4798b72b5346aff2ae3cade804bd39996364
Jack Corddry <jackcorddry@gmail.com>
2025-01-27 00:35:15 -0800

    Add Icons

---
+++ sdk/constants/token-map/1/definitions/1-0xb000000000000000000000000000000000000112.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xb000000000000000000000000000000000000112",
+  chain_id: 1,
+  contract_address: "0xb000000000000000000000000000000000000112",
+  name: "Plus Icon",
+  symbol: "PLUS",
+  image: "https://imgur.com/kJ5l3WL",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
