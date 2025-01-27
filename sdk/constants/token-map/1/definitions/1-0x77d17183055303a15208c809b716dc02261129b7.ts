06cf1ddc911a1ee5e784202884ded1776e07afb8
GitHub Action <action@github.com>
2025-01-27 21:38:59 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/1/definitions/1-0x77d17183055303a15208c809b716dc02261129b7.ts
+++ sdk/constants/token-map/1/definitions/1-0x77d17183055303a15208c809b716dc02261129b7.ts
@@ -6,8 +6,7 @@ export default defineToken({
   contract_address: "0x77d17183055303a15208c809b716dc02261129b7",
   name: "Infrared Points",
   symbol: "IRP",
-  image:
-    "https://i.imgur.com/sJptWp0.png",
+  image: "https://i.imgur.com/sJptWp0.png",
   decimals: 18,
   source: "external",
   search_id: "none",
