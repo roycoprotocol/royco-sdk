d6a1c8b937ea8c8eb74483f2d2a64b60bbb9268e
JB <jbhav24@users.noreply.github.com>
2025-01-27 11:40:05 -0800

    Update 1_0_0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5.ts

--- sdk/constants/market-map/1/definitions/1_0_0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5.ts
+++ sdk/constants/market-map/1/definitions/1_0_0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `1_0_0x7dadff589e53d9813969d0be6de99c033d140ec1d304e57a754797736656dcd5`,
   name: `Restake solvBTC.bbn on SatLayer`,
   description: `Deposit solvBTC.bbn into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
-  is_verified: false,
+  is_verified: true,
   category: `boyco`,
   incentive_ids: ["1-0x9c80538ffcbaee0db71caabe87ee99785ffc4f55"],
   external_incentives: [
