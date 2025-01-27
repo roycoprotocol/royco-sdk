22b9eee4e3854a54c03c897c9e1777283ef8b854
JB <jbhav24@users.noreply.github.com>
2025-01-27 12:10:07 -0800

    Update 1_0_0xb36f14fd392b9a1d6c3fabedb9a62a63d2067ca0ebeb63bbc2c93b11cc8eb3a2.ts

--- sdk/constants/market-map/1/definitions/1_0_0xb36f14fd392b9a1d6c3fabedb9a62a63d2067ca0ebeb63bbc2c93b11cc8eb3a2.ts
+++ sdk/constants/market-map/1/definitions/1_0_0xb36f14fd392b9a1d6c3fabedb9a62a63d2067ca0ebeb63bbc2c93b11cc8eb3a2.ts
@@ -3,9 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `1_0_0xb36f14fd392b9a1d6c3fabedb9a62a63d2067ca0ebeb63bbc2c93b11cc8eb3a2`,
   name: `Veda x Ether.fi eBTC Vault - wBTC Supply`,
-  description: `After mainnet, vaults will run DeFi strategies on Berachain protocols participating in Boyco. Examples of applications include: Kodiak, Infrared, and Dolomite. 
-
-wBTC supplied will be paired against eBTC on mainnet for DeFi usage.`,
+  description: `After mainnet, vaults will run DeFi strategies on Berachain protocols participating in Boyco. Examples of applications include: Kodiak, Infrared, and Dolomite. wBTC supplied will be paired against eBTC on mainnet for DeFi usage.`,
   is_verified: true,
   category: `boyco`,
   external_incentives: [
