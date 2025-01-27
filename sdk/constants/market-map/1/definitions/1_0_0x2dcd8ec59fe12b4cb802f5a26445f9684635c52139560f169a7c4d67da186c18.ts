a52c162042418c9525ca19734bb720931209b585
JB <jbhav24@users.noreply.github.com>
2025-01-27 11:38:10 -0800

    Update 1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18.ts

--- sdk/constants/market-map/1/definitions/1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18.ts
+++ sdk/constants/market-map/1/definitions/1_0_0x2dcd8ec59fe12b4cb802f5a26445f9684635c52139560f169a7c4d67da186c18.ts
@@ -5,5 +5,16 @@ export default defineMarket({
   name: `Restake waBTC on SatLayer`,
   description: `Deposit waBTC into SatLayer, which is restaked to bring Bitcoin shared security to applications and infrastructure on Berachain and leading ecosystems.`,
   is_verified: false,
+  incentive_ids: ["1-0x9c80538ffcbaee0db71caabe87ee99785ffc4f55"],
   category: `boyco`,
+  external_incentives: [
+    {
+      token_id: "1-0xb000000000000000000000000000000000000bbb",
+      label: "Babylon Points",
+      value: async ({ roycoClient, chainClient }) => {
+        const value = "1x";
+        return value;
+      },
+    },
+  ],
 });
