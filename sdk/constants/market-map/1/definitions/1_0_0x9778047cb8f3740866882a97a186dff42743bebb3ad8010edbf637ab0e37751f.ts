49fd4acd13d0a7bab0ae3fcda5d4d7ad820695e2
Jack Corddry <64389215+corddry@users.noreply.github.com>
2025-01-27 12:23:14 -0800

    Update 1_0_0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f.ts

--- sdk/constants/market-map/1/definitions/1_0_0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f.ts
+++ sdk/constants/market-map/1/definitions/1_0_0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f.ts
@@ -4,8 +4,11 @@ export default defineMarket({
   id: `1_0_0x9778047cb8f3740866882a97a186dff42743bebb3ad8010edbf637ab0e37751f`,
   name: `Supply HONEY on Dolomite x Infrared`,
   description: `Supply HONEY into the Dolomite money market on Berachain. This asset will earn lending yield and be eligible for potential Infrared incentives.`,
-  is_verified: false,
+  is_verified: true,
   category: `boyco`,
+
+  incentive_ids: ["1-0x460f8d9c78b1bde7da137ce75315bd15d34a369b"],
+
   external_incentives: [
     {
       token_id: "1-0x77d17183055303a15208c809b716dc02261129b7",
