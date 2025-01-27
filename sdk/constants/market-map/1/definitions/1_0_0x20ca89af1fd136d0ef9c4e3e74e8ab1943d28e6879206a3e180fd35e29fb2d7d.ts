c18c01a2c1ea71f5c75cac7d9eb933cffe7bb4c5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-27 13:54:45 -0800

    feat(market-map): Update market -- 1_0_0x20ca89af1fd136d0ef9c4e3e74e8ab1943d28e6879206a3e180fd35e29fb2d7d -- Recipe (0x20ca...2d7d) (#468)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x20ca89af1fd136d0ef9c4e3e74e8ab1943d28e6879206a3e180fd35e29fb2d7d (Market Hash)
    - Request Timestamp: Mon, 27 Jan 2025 21:54:42 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x20ca89af1fd136d0ef9c4e3e74e8ab1943d28e6879206a3e180fd35e29fb2d7d.ts
@@ -0,0 +1,9 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x20ca89af1fd136d0ef9c4e3e74e8ab1943d28e6879206a3e180fd35e29fb2d7d`,
+  name: `Lend USDC on Dahlia (WBERA&#x2F;USDC)`,
+  description: `Earn PETALS (Dahlia Points) and BERA incentives for lending USDC on Dahlia WBERA&#x2F;USDC (68% LLTV) lending market on Berachain.`,
+  is_verified: false,
+  category: `boyco`,
+});
