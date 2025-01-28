86018ed55d47bc3c28514899ddde9dee0c066505
bharatvish <bharat@waymont.co>
2025-01-28 13:16:16 -0800

    Create 1-0xba8a621b4a54e61c442f5ec623687e2a942225ef.ts

---
+++ sdk/constants/token-map/1/definitions/1-0xba8a621b4a54e61c442f5ec623687e2a942225ef.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xba8a621b4a54e61c442f5ec623687e2a942225ef",
+  chain_id: 1,
+  contract_address: "0xba8a621b4a54e61c442f5ec623687e2a942225ef",
+  name: "Sandclock",
+  symbol: "QUARTZ",
+  image: "https://assets.coingecko.com/coins/images/19368/standard/sandclock.jpg?1696518808",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "sandclock",
+  type: "token",
+});
