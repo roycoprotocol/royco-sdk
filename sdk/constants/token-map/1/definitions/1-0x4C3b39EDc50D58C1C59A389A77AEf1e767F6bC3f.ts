511fba8e077b262ac5b312e72a2a725f0b4a40d5
Saurabh <w.saurabh@protonmail.com>
2025-01-29 12:48:05 +0700

    add seeds contract to token-map

---
+++ sdk/constants/token-map/1/definitions/1-0x4C3b39EDc50D58C1C59A389A77AEf1e767F6bC3f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4C3b39EDc50D58C1C59A389A77AEf1e767F6bC3f",
+  chain_id: 1,
+  contract_address: "0x4C3b39EDc50D58C1C59A389A77AEf1e767F6bC3f",
+  name: "Seeds",
+  symbol: "Seeds",
+  image: "https://i.imgur.com/i0RF9GR.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
--- sdk/constants/token-map/1/index.ts
+++ sdk/constants/token-map/1/index.ts
@@ -1271,6 +1271,7 @@ import token_1_0xff836a5821e69066c87e268bc51b849fab94240c from "./definitions/1-
 import token_1_0xff8c479134a18918059493243943150776cf8cf2 from "./definitions/1-0xff8c479134a18918059493243943150776cf8cf2";
 import token_1_0xffc63b9146967a1ba33066fb057ee3722221acf0 from "./definitions/1-0xffc63b9146967a1ba33066fb057ee3722221acf0";
 import token_1_0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265 from "./definitions/1-0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265";
+import token_1_0x4C3b39EDc50D58C1C59A389A77AEf1e767F6bC3f from "./definitions/1-0x4C3b39EDc50D58C1C59A389A77AEf1e767F6bC3f";
 
 export const TokenMap1 = {
   [token_1_0x00000000000000000000000000000000000112.id]:
@@ -3819,4 +3820,6 @@ export const TokenMap1 = {
     token_1_0xffc63b9146967a1ba33066fb057ee3722221acf0,
   [token_1_0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265.id]:
     token_1_0xffe02ee4c69edf1b340fcad64fbd6b37a7b9e265,
+  [token_1_0x4C3b39EDc50D58C1C59A389A77AEf1e767F6bC3f.id]:
+    token_1_0x4C3b39EDc50D58C1C59A389A77AEf1e767F6bC3f,
 };
