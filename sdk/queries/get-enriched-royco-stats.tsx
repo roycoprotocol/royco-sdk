ce5cdb8b8e7a4dbb7c7376192993c09c0a0b0d47
githubotoro <yupuday.git@gmail.com>
2025-01-29 10:06:24 -0800

    feat(stats): view

--- sdk/queries/get-enriched-royco-stats.tsx
+++ sdk/queries/get-enriched-royco-stats.tsx
@@ -9,9 +9,7 @@ export const getEnrichedRoycoStatsQueryOptions = (
 ) => ({
   queryKey: ["get-enriched-royco-stats"],
   queryFn: async () => {
-    const result = await client.rpc("get_enriched_royco_stats", {
-      custom_token_data,
-    });
+    const result = await client.from("enriched_royco_stats").select("*");
 
     let total_volume = 0;
     let total_tvl = 0;
