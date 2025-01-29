259d24aabe2413a4fab9372ddab5aef64605c18e
GitHub Action <action@github.com>
2025-01-29 20:49:52 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/1/definitions/1_1_0xa2bb5cd636a098783f629d1f1431934a45eee7fa.ts
+++ sdk/constants/market-map/1/definitions/1_1_0xa2bb5cd636a098783f629d1f1431934a45eee7fa.ts
@@ -6,5 +6,4 @@ export default defineMarket({
   name: `Supply USDC into Euler Yield Market`,
   description: `Supply USDC in the USDC Vault in Euler Yield Market and get extra rEUL rewards and yield. Reward EUL converts to 1:1 with EUL over a period of 6 months. For more information, read here: https://forum.euler.finance/t/reward-eul-reul/1133.`,
   is_verified: true,
-
 });
