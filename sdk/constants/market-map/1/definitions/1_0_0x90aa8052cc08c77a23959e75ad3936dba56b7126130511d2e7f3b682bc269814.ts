f653f8fa3272f296488b37d2dfba8ae718d7d5a5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:45:43 -0800

    feat(market-map): Update market -- 1_0_0x90aa8052cc08c77a23959e75ad3936dba56b7126130511d2e7f3b682bc269814 -- Recipe (0x90aa...9814) (#523)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x90aa8052cc08c77a23959e75ad3936dba56b7126130511d2e7f3b682bc269814 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:45:41 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x90aa8052cc08c77a23959e75ad3936dba56b7126130511d2e7f3b682bc269814.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x90aa8052cc08c77a23959e75ad3936dba56b7126130511d2e7f3b682bc269814`,
+  name: `Test D2 CCDM - usdc-beraland`,
+  description: `Test D2 CCDM - usdc-beraland`,
+  is_verified: false,
+});
