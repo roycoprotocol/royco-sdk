e670a09b9bc43d573a41ea64f91218529d8382d0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:15:14 -0800

    feat(market-map): Update market -- 1_0_0x9fe71de2cbc7a18db40d01d9e83a09015236ba62a8e8fcd78839fcd53e3c7b70 -- Recipe (0x9fe7...7b70)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x9fe71de2cbc7a18db40d01d9e83a09015236ba62a8e8fcd78839fcd53e3c7b70 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:15:14 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x9fe71de2cbc7a18db40d01d9e83a09015236ba62a8e8fcd78839fcd53e3c7b70.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x9fe71de2cbc7a18db40d01d9e83a09015236ba62a8e8fcd78839fcd53e3c7b70`,
+  name: `Test SatLayer CCDM - pumpBTC.bera`,
+  description: `Test SatLayer CCDM - pumpBTC.bera`,
+  is_verified: false,
+});
