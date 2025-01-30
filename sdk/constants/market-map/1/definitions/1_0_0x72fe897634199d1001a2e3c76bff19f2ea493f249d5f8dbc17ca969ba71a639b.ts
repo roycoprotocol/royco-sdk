4e8edce1050a22005462adbde375e1b2d28ccca5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:42:35 -0800

    feat(market-map): Update market -- 1_0_0x72fe897634199d1001a2e3c76bff19f2ea493f249d5f8dbc17ca969ba71a639b -- Recipe (0x72fe...639b)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x72fe897634199d1001a2e3c76bff19f2ea493f249d5f8dbc17ca969ba71a639b (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:42:34 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x72fe897634199d1001a2e3c76bff19f2ea493f249d5f8dbc17ca969ba71a639b.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x72fe897634199d1001a2e3c76bff19f2ea493f249d5f8dbc17ca969ba71a639b`,
+  name: `Test Dahlia CCDM - stoneweth`,
+  description: `Test Dahlia CCDM - stoneweth`,
+  is_verified: false,
+});
