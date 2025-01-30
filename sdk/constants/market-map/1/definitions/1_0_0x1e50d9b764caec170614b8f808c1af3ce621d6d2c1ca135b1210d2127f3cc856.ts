c537b751c3549f69c120db0030a3d60c94d197e7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:17:15 -0800

    feat(market-map): Update market -- 1_0_0x1e50d9b764caec170614b8f808c1af3ce621d6d2c1ca135b1210d2127f3cc856 -- Recipe (0x1e50...c856) (#508)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x1e50d9b764caec170614b8f808c1af3ce621d6d2c1ca135b1210d2127f3cc856 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:17:13 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x1e50d9b764caec170614b8f808c1af3ce621d6d2c1ca135b1210d2127f3cc856.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x1e50d9b764caec170614b8f808c1af3ce621d6d2c1ca135b1210d2127f3cc856`,
+  name: `Test Origami CCDM - USDC`,
+  description: `Test Origami CCDM - USDC`,
+  is_verified: false,
+});
