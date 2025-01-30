7e062577134135b6cf8070d527217db9a6cb3ff7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:56:30 -0800

    feat(market-map): Update market -- 1_0_0xa6cde8ca73aa5309011dc3e399a59a13fd12a504329f76917646c65f6b1e0213 -- Recipe (0xa6cd...0213)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xa6cde8ca73aa5309011dc3e399a59a13fd12a504329f76917646c65f6b1e0213 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:56:30 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xa6cde8ca73aa5309011dc3e399a59a13fd12a504329f76917646c65f6b1e0213.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xa6cde8ca73aa5309011dc3e399a59a13fd12a504329f76917646c65f6b1e0213`,
+  name: `Test Beraborrow CCDM -pumpbtc`,
+  description: `Test Beraborrow CCDM -pumpbtc`,
+  is_verified: false,
+});
