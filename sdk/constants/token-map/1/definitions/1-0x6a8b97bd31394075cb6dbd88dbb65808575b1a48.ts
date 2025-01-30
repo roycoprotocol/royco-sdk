40d8ddad5a33a45b569ee3cd2a5ebe1cb05e9f3e
githubotoro <yupuday.git@gmail.com>
2025-01-29 17:08:09 -0800

    feat(constants): update

--- sdk/constants/token-map/1/definitions/1-0x6a8b97bd31394075cb6dbd88dbb65808575b1a48.ts
+++ sdk/constants/token-map/1/definitions/1-0x6a8b97bd31394075cb6dbd88dbb65808575b1a48.ts
@@ -9,6 +9,6 @@ export default defineToken({
   image: "https://d2.finance/logos/edalogo.svg",
   decimals: 18,
   source: "external",
-  search_id: "1-0x6a8B97bD31394075Cb6DBd88dBB65808575b1A48",
+  search_id: "1-0x6a8b97bd31394075cb6dbd88dbb65808575b1a48",
   type: "point",
 });
