d2d8d7461b5387fa4769a550c8779a822f5762ce
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:48:14 -0800

    feat(market-map): Update market -- 1_0_0x792bea5c2d1c00325254a0b1aeacfadf6ed010c1b00af5c3e4ef3da5d1d5665f -- Recipe (0x792b...665f) (#525)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x792bea5c2d1c00325254a0b1aeacfadf6ed010c1b00af5c3e4ef3da5d1d5665f (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:48:12 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x792bea5c2d1c00325254a0b1aeacfadf6ed010c1b00af5c3e4ef3da5d1d5665f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x792bea5c2d1c00325254a0b1aeacfadf6ed010c1b00af5c3e4ef3da5d1d5665f`,
+  name: `Test Concrete CCDM - beraETH`,
+  description: `Test Concrete CCDM - beraETH`,
+  is_verified: false,
+});
