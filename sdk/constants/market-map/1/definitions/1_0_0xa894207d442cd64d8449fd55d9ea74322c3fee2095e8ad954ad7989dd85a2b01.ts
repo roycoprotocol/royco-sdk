6bc5e94b1e074c5f1275270a29ba1542a2c064fa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:33:52 -0800

    feat(market-map): Update market -- 1_0_0xa894207d442cd64d8449fd55d9ea74322c3fee2095e8ad954ad7989dd85a2b01 -- Recipe (0xa894...2b01) (#514)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xa894207d442cd64d8449fd55d9ea74322c3fee2095e8ad954ad7989dd85a2b01 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:33:50 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xa894207d442cd64d8449fd55d9ea74322c3fee2095e8ad954ad7989dd85a2b01.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xa894207d442cd64d8449fd55d9ea74322c3fee2095e8ad954ad7989dd85a2b01`,
+  name: `Test Kodiak CCDM - honey-usdc`,
+  description: `Test Kodiak CCDM - honey-usdc`,
+  is_verified: false,
+});
