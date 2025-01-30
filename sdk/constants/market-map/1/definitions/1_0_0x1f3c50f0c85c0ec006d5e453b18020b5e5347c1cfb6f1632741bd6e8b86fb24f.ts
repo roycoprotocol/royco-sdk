df7f31416d239d3ebd52e12d680e5423a9beada2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:55:16 -0800

    feat(market-map): Update market -- 1_0_0x1f3c50f0c85c0ec006d5e453b18020b5e5347c1cfb6f1632741bd6e8b86fb24f -- Recipe (0x1f3c...b24f)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x1f3c50f0c85c0ec006d5e453b18020b5e5347c1cfb6f1632741bd6e8b86fb24f (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:55:16 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x1f3c50f0c85c0ec006d5e453b18020b5e5347c1cfb6f1632741bd6e8b86fb24f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x1f3c50f0c85c0ec006d5e453b18020b5e5347c1cfb6f1632741bd6e8b86fb24f`,
+  name: `Test Beraborrow CCDM - wbtc-weth`,
+  description: `Test Beraborrow CCDM - wbtc-weth`,
+  is_verified: false,
+});
