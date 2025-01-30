b8e0313d4145657aa8690fab73619802e12ce753
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:58:56 -0800

    feat(market-map): Update market -- 1_0_0x9566e7e23919d15cf4df9f7076aea4d50d6b943d36c9d78dd203793eb94e3a88 -- Recipe (0x9566...3a88) (#532)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x9566e7e23919d15cf4df9f7076aea4d50d6b943d36c9d78dd203793eb94e3a88 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:58:54 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x9566e7e23919d15cf4df9f7076aea4d50d6b943d36c9d78dd203793eb94e3a88.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x9566e7e23919d15cf4df9f7076aea4d50d6b943d36c9d78dd203793eb94e3a88`,
+  name: `Test Beraborrow - beraETH`,
+  description: `Test Beraborrow - beraETH`,
+  is_verified: false,
+});
