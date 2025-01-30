76c3bc2d9a0b4dadcbd61a2c0eaae3ea5b4101a6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:58:13 -0800

    feat(market-map): Update market -- 1_0_0x1151c5f4eb79bb00a37f16193ec0474382182687b9274865869844690e66100a -- Recipe (0x1151...100a)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x1151c5f4eb79bb00a37f16193ec0474382182687b9274865869844690e66100a (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:58:13 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x1151c5f4eb79bb00a37f16193ec0474382182687b9274865869844690e66100a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x1151c5f4eb79bb00a37f16193ec0474382182687b9274865869844690e66100a`,
+  name: `Test Beraborrow CCDM - nect-honey`,
+  description: `Test Beraborrow CCDM - nect-honey`,
+  is_verified: false,
+});
