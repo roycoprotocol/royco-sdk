fab469136f9699df6b0ab37cc513fe8d14cc17df
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:25:26 -0800

    feat(market-map): Update market -- 1_0_0xa540d11898d919445efa9275cb529df239fa09f9bb8b27d60c1bc2266f35da63 -- Recipe (0xa540...da63) (#510)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xa540d11898d919445efa9275cb529df239fa09f9bb8b27d60c1bc2266f35da63 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:25:24 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xa540d11898d919445efa9275cb529df239fa09f9bb8b27d60c1bc2266f35da63.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xa540d11898d919445efa9275cb529df239fa09f9bb8b27d60c1bc2266f35da63`,
+  name: `Test Kodiak CCDM - wBTC &lt;&gt; sBTC`,
+  description: `Test Kodiak CCDM - wBTC &lt;&gt; sBTC`,
+  is_verified: false,
+});
