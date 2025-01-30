5c7a440225d937d6df42248cff33ceb8a3c0458c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:54:54 -0800

    feat(market-map): Update market -- 1_0_0x354bee23352682f2193be6e62baffc36e8a443ab496542305a538d9c9de03c8c -- Recipe (0x354b...3c8c)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x354bee23352682f2193be6e62baffc36e8a443ab496542305a538d9c9de03c8c (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:54:53 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x354bee23352682f2193be6e62baffc36e8a443ab496542305a538d9c9de03c8c.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x354bee23352682f2193be6e62baffc36e8a443ab496542305a538d9c9de03c8c`,
+  name: `Test BurrBear CCDM - USDC`,
+  description: `Test BurrBear CCDM - USDC`,
+  is_verified: false,
+});
