4937920599bf15ce6767c694f97356ac48d5987f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:12:22 -0800

    feat(market-map): Update market -- 1_0_0xcd4a25be0ea0055a91b9825dc81717e68da15bc50563bc23a306694680a025d5 -- Recipe (0xcd4a...25d5)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xcd4a25be0ea0055a91b9825dc81717e68da15bc50563bc23a306694680a025d5 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:12:22 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xcd4a25be0ea0055a91b9825dc81717e68da15bc50563bc23a306694680a025d5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xcd4a25be0ea0055a91b9825dc81717e68da15bc50563bc23a306694680a025d5`,
+  name: `Test wETH CCDM - Veda`,
+  description: `Test wETH CCDM - Veda`,
+  is_verified: false,
+});
