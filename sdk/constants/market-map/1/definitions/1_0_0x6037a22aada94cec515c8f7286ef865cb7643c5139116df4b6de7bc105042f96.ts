1d56882cf65676988d9b0bbe32345e56678c952e
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:18:53 -0800

    feat(market-map): Update market -- 1_0_0x6037a22aada94cec515c8f7286ef865cb7643c5139116df4b6de7bc105042f96 -- Recipe (0x6037...2f96) (#509)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x6037a22aada94cec515c8f7286ef865cb7643c5139116df4b6de7bc105042f96 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:18:51 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x6037a22aada94cec515c8f7286ef865cb7643c5139116df4b6de7bc105042f96.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x6037a22aada94cec515c8f7286ef865cb7643c5139116df4b6de7bc105042f96`,
+  name: `Test Kodiak CCDM - wETH &lt;&gt; beraETH`,
+  description: `Test Kodiak CCDM - wETH &lt;&gt; beraETH`,
+  is_verified: false,
+});
