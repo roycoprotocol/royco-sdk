4e7128db206cf58f41f1542c3e8021dee7393e1d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:36:04 -0800

    feat(market-map): Update market -- 1_0_0xfa53e5c43a24129cede84ee971cc63b86a60efbb02049854ae7e4fdfd4d2f23a -- Recipe (0xfa53...f23a)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xfa53e5c43a24129cede84ee971cc63b86a60efbb02049854ae7e4fdfd4d2f23a (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:36:03 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xfa53e5c43a24129cede84ee971cc63b86a60efbb02049854ae7e4fdfd4d2f23a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xfa53e5c43a24129cede84ee971cc63b86a60efbb02049854ae7e4fdfd4d2f23a`,
+  name: `Test Goldilocks CCDM - rseth-rsethot`,
+  description: `Test Goldilocks CCDM - rseth-rsethot`,
+  is_verified: false,
+});
