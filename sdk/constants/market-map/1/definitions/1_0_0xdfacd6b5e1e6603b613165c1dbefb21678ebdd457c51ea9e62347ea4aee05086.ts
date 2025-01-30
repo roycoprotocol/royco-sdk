3190d094a720f0195bf710e22715b1e201c735d4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:13:46 -0800

    feat(market-map): Update market -- 1_0_0xdfacd6b5e1e6603b613165c1dbefb21678ebdd457c51ea9e62347ea4aee05086 -- Recipe (0xdfac...5086)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xdfacd6b5e1e6603b613165c1dbefb21678ebdd457c51ea9e62347ea4aee05086 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:13:45 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xdfacd6b5e1e6603b613165c1dbefb21678ebdd457c51ea9e62347ea4aee05086.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xdfacd6b5e1e6603b613165c1dbefb21678ebdd457c51ea9e62347ea4aee05086`,
+  name: `Test THJ CCDM - USDC`,
+  description: `Test THJ CCDM - USDC`,
+  is_verified: false,
+});
