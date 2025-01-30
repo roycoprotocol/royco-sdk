bc773c56deb742f76c0458a7161a6fe40313d411
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 12:38:14 -0800

    feat(market-map): Update market -- 1_0_0x6e1ba4f621a800c5866cb99d8d2cb2dcb65180c9c60bc8a569a3350e138b62a8 -- Recipe (0x6e1b...62a8)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x6e1ba4f621a800c5866cb99d8d2cb2dcb65180c9c60bc8a569a3350e138b62a8 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 20:38:13 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x6e1ba4f621a800c5866cb99d8d2cb2dcb65180c9c60bc8a569a3350e138b62a8.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x6e1ba4f621a800c5866cb99d8d2cb2dcb65180c9c60bc8a569a3350e138b62a8`,
+  name: `Test Kodiak CCDM - stone-weth`,
+  description: `Test Kodiak CCDM - stone-weth`,
+  is_verified: false,
+});
