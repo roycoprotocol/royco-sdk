05a7e0bb02215e25d091048cabef55aee09eeab1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-01-30 11:38:16 -0800

    feat(market-map): Update market -- 1_0_0xb5ad81d0cdb02b05a01ac9d29d7ec808d53def8bc41925cbe97b7d809a924475 -- Recipe (0xb5ad...4475) (#517)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xb5ad81d0cdb02b05a01ac9d29d7ec808d53def8bc41925cbe97b7d809a924475 (Market Hash)
    - Request Timestamp: Thu, 30 Jan 2025 19:38:14 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xb5ad81d0cdb02b05a01ac9d29d7ec808d53def8bc41925cbe97b7d809a924475.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xb5ad81d0cdb02b05a01ac9d29d7ec808d53def8bc41925cbe97b7d809a924475`,
+  name: `Test Dolomite CCDM - ir-dhoney`,
+  description: `Test Dolomite CCDM - ir-dhoney`,
+  is_verified: false,
+});
