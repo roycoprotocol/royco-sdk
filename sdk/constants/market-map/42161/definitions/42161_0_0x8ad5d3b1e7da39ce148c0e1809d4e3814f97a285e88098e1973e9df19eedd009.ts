8fab27e44aa0487127ff8d6584caa84f7e8d09d5
GitHub Action <action@github.com>
2025-02-03 05:49:43 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/42161/definitions/42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009.ts
+++ sdk/constants/market-map/42161/definitions/42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009.ts
@@ -1,13 +1,11 @@
 import { roycoClient } from "@/sdk/client";
 import { defineMarket } from "@/sdk/constants";
 
-const apy = async () => { 
+const apy = async () => {
   let annual_change_ratio = 0;
 
   try {
-    const res = await fetch(
-      "https://ctrl.yield.fi/t/apy",
-    );
+    const res = await fetch("https://ctrl.yield.fi/t/apy");
     const apy_data = await res.json();
     annual_change_ratio = (Number(apy_data["apy"]) ?? 0) / 100;
   } catch (error) {
@@ -15,7 +13,7 @@ const apy = async () => {
   }
 
   return annual_change_ratio;
-}
+};
 
 export default defineMarket({
   id: `42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009`,
@@ -31,11 +29,11 @@ More details at (https:&#x2F;&#x2F;yield.fi)`,
       label: "yUSD",
       annual_change_ratio: async ({ roycoClient, chainClient }) => {
         return await apy();
-      }
-    }
+      },
+    },
   ],
 
   underlying_yield: async ({ roycoClient, chainClient }) => {
     return await apy();
-  }
+  },
 });
