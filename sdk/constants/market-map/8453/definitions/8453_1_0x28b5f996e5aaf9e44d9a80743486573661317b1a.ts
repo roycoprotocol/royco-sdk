d337dc15087de1a8b003c614e0ed959a0e290551
bharatvish <bharat@waymont.co>
2025-02-05 15:07:51 -0800

    Update 8453_1_0x28b5f996e5aaf9e44d9a80743486573661317b1a.ts

--- sdk/constants/market-map/8453/definitions/8453_1_0x28b5f996e5aaf9e44d9a80743486573661317b1a.ts
+++ sdk/constants/market-map/8453/definitions/8453_1_0x28b5f996e5aaf9e44d9a80743486573661317b1a.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: `8453_1_0x28b5f996e5aaf9e44d9a80743486573661317b1a`,
   name: `Deploy ETH into superOETH`,
   description: `Deploys ETH into superOETHb to earn passive yield. Learn more at https:&#x2F;&#x2F;www.originprotocol.com&#x2F;super-oeth`,
-  is_verified: false,
+  is_verified: true,
 });
