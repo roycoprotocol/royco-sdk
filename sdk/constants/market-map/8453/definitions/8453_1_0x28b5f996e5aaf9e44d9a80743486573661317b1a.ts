b200424e83f055d12ff22a93cec3785a3f99c714
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-05 13:33:19 -0800

    feat(market-map): Update market -- 8453_1_0x28b5f996e5aaf9e44d9a80743486573661317b1a -- Vault (0x28b5...7b1a) (#549)
    Market Details:
    - Chain ID: 8453
    - Market Type: 1 (Vault)
    - Market ID: 0x28b5f996e5aaf9e44d9a80743486573661317b1a (Wrapped Vault Address)
    - Request Timestamp: Wed, 05 Feb 2025 21:33:17 GMT

---
+++ sdk/constants/market-map/8453/definitions/8453_1_0x28b5f996e5aaf9e44d9a80743486573661317b1a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `8453_1_0x28b5f996e5aaf9e44d9a80743486573661317b1a`,
+  name: `Deploy ETH into superOETH`,
+  description: `Deploys ETH into superOETHb to earn passive yield. Learn more at https:&#x2F;&#x2F;www.originprotocol.com&#x2F;super-oeth`,
+  is_verified: false,
+});
