821d0e7ff3c65865e5d3594825d4b1bdbf23a9f8
bharatvish <bharat@waymont.co>
2025-02-05 15:07:24 -0800

    Update 1_1_0x470fa30720ab37a46119c81f6e0c3cdf4c476faa.ts

--- sdk/constants/market-map/1/definitions/1_1_0x470fa30720ab37a46119c81f6e0c3cdf4c476faa.ts
+++ sdk/constants/market-map/1/definitions/1_1_0x470fa30720ab37a46119c81f6e0c3cdf4c476faa.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: `1_1_0x470fa30720ab37a46119c81f6e0c3cdf4c476faa`,
   name: `Deploy ETH into OETH`,
   description: `Deploys ETH into OETH to earn passive yield. Learn more at https:&#x2F;&#x2F;www.originprotocol.com&#x2F;oeth`,
-  is_verified: false,
+  is_verified: true,
 });
