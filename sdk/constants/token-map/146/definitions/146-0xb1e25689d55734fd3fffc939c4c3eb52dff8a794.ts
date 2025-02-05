d2b1a2b9772b2bc809724f28f0e1534c67e4956d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-05 13:07:50 -0800

    feat(token-map): Update Token -- 146-0xb1e25689d55734fd3fffc939c4c3eb52dff8a794 -- Origin Sonic (OS) (0xb1e2...a794)
    Token Details:
    - Id: 146-0xb1e25689d55734fd3fffc939c4c3eb52dff8a794
    - Chain Id: 146
    - Contract Address: 0xb1e25689d55734fd3fffc939c4c3eb52dff8a794
    - Name: Origin Sonic
    - Symbol: OS
    - Image: https://coin-images.coingecko.com/coins/images/53773/large/os-256x256.png?1737271726
    - Decimals: 18
    - Source: coingecko
    - Search Id: origin-staked-s

---
+++ sdk/constants/token-map/146/definitions/146-0xb1e25689d55734fd3fffc939c4c3eb52dff8a794.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xb1e25689d55734fd3fffc939c4c3eb52dff8a794",
+  chain_id: 146,
+  contract_address: "0xb1e25689d55734fd3fffc939c4c3eb52dff8a794",
+  name: "Origin Sonic",
+  symbol: "OS",
+  image: "https://coin-images.coingecko.com/coins/images/53773/large/os-256x256.png?1737271726",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "origin-staked-s",
+  type: "token",
+});
