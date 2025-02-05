03f084bfb5d72eb0326cc8334b6966df339aae62
bharatvish <bharat@waymont.co>
2025-02-05 10:43:29 -0800

    Update 42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009.ts

--- sdk/constants/market-map/42161/definitions/42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009.ts
+++ sdk/constants/market-map/42161/definitions/42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009.ts
@@ -16,7 +16,7 @@ const apy = async (isNative = false) => {
 
 export default defineMarket({
   id: `42161_0_0x8ad5d3b1e7da39ce148c0e1809d4e3814f97a285e88098e1973e9df19eedd009`,
-  name: `Swap USDC for yUSD for 3 mo`,
+  name: `Supply USDC into YieldFi`,
   description: `Earn 15% - 25% APY + rewards on stable coins. 
 
 More details at (https:&#x2F;&#x2F;yield.fi)`,
