de8250551e72c60c5a6234dea9c24cfe08b231fd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-05 13:23:23 -0800

    feat(market-map): Update market -- 146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca -- Vault (0x7f24...86ca)
    Market Details:
    - Chain ID: 146
    - Market Type: 1 (Vault)
    - Market ID: 0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca (Wrapped Vault Address)
    - Request Timestamp: Wed, 05 Feb 2025 21:23:23 GMT

---
+++ sdk/constants/market-map/146/definitions/146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca`,
+  name: `Deposit S to OS`,
+  description: `Deposits S to Origin Sonic (OS) to earn passive yield. Learn more at https:&#x2F;&#x2F;www.originprotocol.com&#x2F;os`,
+  is_verified: false,
+});
