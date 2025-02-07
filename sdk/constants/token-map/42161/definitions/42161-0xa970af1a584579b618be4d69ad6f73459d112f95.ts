97097c43f104d06658571d8dda123f0de681e91a
GitHub Action <action@github.com>
2025-02-07 17:50:02 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/42161/definitions/42161-0xa970af1a584579b618be4d69ad6f73459d112f95.ts
+++ sdk/constants/token-map/42161/definitions/42161-0xa970af1a584579b618be4d69ad6f73459d112f95.ts
@@ -6,7 +6,8 @@ export default defineToken({
   contract_address: "0xa970af1a584579b618be4d69ad6f73459d112f95",
   name: "sUSD",
   symbol: "SUSD",
-  image: "https://coin-images.coingecko.com/coins/images/5013/large/sUSD.png?1696505546",
+  image:
+    "https://coin-images.coingecko.com/coins/images/5013/large/sUSD.png?1696505546",
   decimals: 18,
   source: "coingecko",
   search_id: "nusd",
