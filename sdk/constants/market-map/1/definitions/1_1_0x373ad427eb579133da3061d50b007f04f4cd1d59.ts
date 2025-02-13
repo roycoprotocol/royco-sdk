5044797aa21de95c884997f412ec40d87a927e32
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-13 10:47:44 -0800

    feat(market-map): Update market -- 1_1_0x373ad427eb579133da3061d50b007f04f4cd1d59 -- Vault (0x373a...1d59) (#558)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0x373ad427eb579133da3061d50b007f04f4cd1d59 (Wrapped Vault Address)
    - Request Timestamp: Thu, 13 Feb 2025 18:47:42 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0x373ad427eb579133da3061d50b007f04f4cd1d59.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0x373ad427eb579133da3061d50b007f04f4cd1d59`,
+  name: `Deploy stablecoin into OUSD`,
+  description: `Deploys USDC, USDT, and DAI into OUSD to earn passive yield. Learn more at https:&#x2F;&#x2F;ousd.com&#x2F;`,
+  is_verified: false,
+});
