b86a67284c47f218d7f63c24d84398f11798bfd9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-19 14:37:23 -0800

    feat(market-map): Update market -- 146_1_0x4c6bac7f421fbdbb3232070517b948b8dcbdc67f -- Vault (0x4c6b...c67f) (#567)
    Market Details:
    - Chain ID: 146
    - Market Type: 1 (Vault)
    - Market ID: 0x4c6bac7f421fbdbb3232070517b948b8dcbdc67f (Wrapped Vault Address)
    - Request Timestamp: Wed, 19 Feb 2025 22:37:21 GMT

---
+++ sdk/constants/market-map/146/definitions/146_1_0x4c6bac7f421fbdbb3232070517b948b8dcbdc67f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_1_0x4c6bac7f421fbdbb3232070517b948b8dcbdc67f`,
+  name: `SONIC TEST - Deposit into Origin Protocol`,
+  description: `SONIC TEST - Deposit into Origin Protocol`,
+  is_verified: false,
+});
