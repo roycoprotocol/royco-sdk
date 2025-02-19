2738c68c91f826de924c031a77b3608f4702ca6c
bharatvish <bharat@waymont.co>
2025-02-19 15:18:03 -0800

    Create 146-0x562DDFe40FA7f965c84Fb0b42e32FA93085aF6Ba.ts

---
+++ sdk/constants/token-map/146/definitions/146-0x562DDFe40FA7f965c84Fb0b42e32FA93085aF6Ba.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x562DDFe40FA7f965c84Fb0b42e32FA93085aF6Ba",
+  chain_id: 146,
+  contract_address: "0x562DDFe40FA7f965c84Fb0b42e32FA93085aF6Ba",
+  name: "Test Points",
+  symbol: "TP",
+  image:
+    "https://coin-images.coingecko.com/coins/images/52967/large/SS_logo.png?1734881325",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "point",
+});
