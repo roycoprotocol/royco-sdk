7b248c25ffec9f92663001a28030ef2f06232066
bharatvish <bharat@waymont.co>
2025-02-20 15:00:16 -0800

    Create 146_ 0xD152f4C29fB0db011c8a5503Aee3Ce60C44F8985.ts

---
+++ sdk/constants/token-map/146/definitions/146_ 0xD152f4C29fB0db011c8a5503Aee3Ce60C44F8985.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xD152f4C29fB0db011c8a5503Aee3Ce60C44F8985",
+  chain_id: 146,
+  contract_address: "0xD152f4C29fB0db011c8a5503Aee3Ce60C44F8985",
+  name: "StableJack Points",
+  symbol: "SJP",
+  image:
+    "https://pbs.twimg.com/profile_images/1826601407648063488/zg6tr9eo_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
