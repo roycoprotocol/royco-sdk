9fbdffd85fc6d3b68c92f58ecee5f0e0b34ac5ef
bharatvish <bharat@waymont.co>
2025-02-22 10:13:42 -0800

    Create 146-0x5e75334F4270FfE07a80b28FC831BfAb2d83706e.ts

---
+++ sdk/constants/token-map/146/definitions/146-0x5e75334F4270FfE07a80b28FC831BfAb2d83706e.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x5e75334F4270FfE07a80b28FC831BfAb2d83706e",
+  chain_id: 146,
+  contract_address: "0x5e75334F4270FfE07a80b28FC831BfAb2d83706e",
+  name: "Rings Points",
+  symbol: "RP",
+  image:
+    "https://pbs.twimg.com/profile_images/1862403917717352448/vWaDsAEu_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
