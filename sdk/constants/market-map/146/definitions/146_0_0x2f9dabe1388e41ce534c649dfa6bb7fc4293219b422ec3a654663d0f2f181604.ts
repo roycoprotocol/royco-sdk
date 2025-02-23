91986a368d11474022870f04f378a511a69ab140
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-23 13:22:32 -0800

    feat(market-map): Update market -- 146_0_0x2f9dabe1388e41ce534c649dfa6bb7fc4293219b422ec3a654663d0f2f181604 -- Recipe (0x2f9d...1604)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x2f9dabe1388e41ce534c649dfa6bb7fc4293219b422ec3a654663d0f2f181604 (Market Hash)
    - Request Timestamp: Sun, 23 Feb 2025 21:22:32 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x2f9dabe1388e41ce534c649dfa6bb7fc4293219b422ec3a654663d0f2f181604.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x2f9dabe1388e41ce534c649dfa6bb7fc4293219b422ec3a654663d0f2f181604`,
+  name: `Deposit wS to buy PT-stS`,
+  description: `PT-stS enables investors to farm Sonic points, Gems, and Stable Jack Discount Tickets with leverage while maintaining long stS exposure.
+
+In exchange, PT-stS holders pay a certain fee to YT-stS holders.`,
+  is_verified: false,
+});
