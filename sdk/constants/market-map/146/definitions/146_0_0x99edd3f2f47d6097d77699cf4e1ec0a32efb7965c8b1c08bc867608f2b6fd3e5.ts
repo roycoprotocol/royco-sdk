3dae87f6fd4885cf038daf2c75f19c82bdfcc1e7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-23 00:04:56 -0800

    feat(market-map): Update market -- 146_0_0x99edd3f2f47d6097d77699cf4e1ec0a32efb7965c8b1c08bc867608f2b6fd3e5 -- Recipe (0x99ed...d3e5) (#576)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x99edd3f2f47d6097d77699cf4e1ec0a32efb7965c8b1c08bc867608f2b6fd3e5 (Market Hash)
    - Request Timestamp: Sun, 23 Feb 2025 08:04:54 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x99edd3f2f47d6097d77699cf4e1ec0a32efb7965c8b1c08bc867608f2b6fd3e5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x99edd3f2f47d6097d77699cf4e1ec0a32efb7965c8b1c08bc867608f2b6fd3e5`,
+  name: `Test Pt WOS`,
+  description: `Test Pt WOS Test Pt WOS Test Pt WOS Test Pt WOS`,
+  is_verified: false,
+});
