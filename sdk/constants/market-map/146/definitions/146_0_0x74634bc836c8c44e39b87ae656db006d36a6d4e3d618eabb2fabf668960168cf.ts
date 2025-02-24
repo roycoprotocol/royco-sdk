c1ead0e4825219b3c415c4ef0c8865d99799865b
bharatvish <bharat@waymont.co>
2025-02-23 18:34:41 -0800

    Update 146_0_0x74634bc836c8c44e39b87ae656db006d36a6d4e3d618eabb2fabf668960168cf.ts

--- sdk/constants/market-map/146/definitions/146_0_0x74634bc836c8c44e39b87ae656db006d36a6d4e3d618eabb2fabf668960168cf.ts
+++ sdk/constants/market-map/146/definitions/146_0_0x74634bc836c8c44e39b87ae656db006d36a6d4e3d618eabb2fabf668960168cf.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `146_0_0x74634bc836c8c44e39b87ae656db006d36a6d4e3d618eabb2fabf668960168cf`,
   name: `Deposit wS to buy PT-wOS`,
   description: `PT-wOS enables investors to farm Sonic points, Gems, and Stable Jack Discount Tickets with leverage while maintaining long wOS exposure. In exchange, PT-wOS holders pay a certain fee to YT-wOS holders.`,
-  is_verified: false,
+  is_verified: true,
     // @dev: Not sure what to add as token_id since discount tickets are not tokens, they are points
     external_incentives: [
       {
