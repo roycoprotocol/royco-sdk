f9bfc1eaa2e05ae528d914dee9809e044a7f15e2
bharatvish <bharat@waymont.co>
2025-02-23 22:35:35 -0800

    Create 146-0x3D10814c737B98de96f5010063ad4EB13D9c188E.ts

---
+++ sdk/constants/token-map/146/definitions/146-0x3D10814c737B98de96f5010063ad4EB13D9c188E.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x3D10814c737B98de96f5010063ad4EB13D9c188E",
+  chain_id: 146,
+  contract_address: "0x3D10814c737B98de96f5010063ad4EB13D9c188E",
+  name: "Gems Boost",
+  symbol: "GB",
+  image:
+    "https://asset-metadata-service-production.s3.amazonaws.com/asset_icons/f528ed9bd1da48a6f3373511da95961b6c57ed921edc034cc33f8be6d44da69f.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
