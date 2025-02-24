d01cdba89cbbd9bb07cc0a9a52af594405f7d331
Solomon <63205747+solomonNSI@users.noreply.github.com>
2025-02-24 03:04:00 +0100

    feat: stable jack incentives

--- sdk/constants/market-map/146/definitions/146_0_0x2e8fb605f1e044b8a6bc9f0db673b92b6815b5333508e62b85de7e2ff44199a8.ts
+++ sdk/constants/market-map/146/definitions/146_0_0x2e8fb605f1e044b8a6bc9f0db673b92b6815b5333508e62b85de7e2ff44199a8.ts
@@ -7,4 +7,135 @@ export default defineMarket({
 
 In exchange, YT-stS holders forgo their points exposure.`,
   is_verified: false,
+
+  // Basically, YT is a yield bearing token, it is ERC4626, so ig it is a native yield?
+  // @dev: We are depositing aTokens to YT-stS and this way the price is increasing
+  // @dev: These aTokens are just for internal accounting, so displaying them here might not make sense
+  // @dev: Hence, using YT as the native yield directly
+  native_yield: [
+    {
+      token_id: "146-0x0fa31f0d5a574f083e0be272a6cf807270352b3f",
+      label: 'YT-stS',
+      annual_change_ratio: async({ roycoClient, chainClient }) => {
+        let annual_change_ratio = 0;
+        
+        const treasury = '0x56B9681B78Cc43E564E14Ba215AA62F9370d3A59';
+        const yt_address = '0x0fa31f0d5a574F083E0be272a6CF807270352b3f';
+        const pt_address = '0xFCA91fEEe65DB34448A83a74f4f8970b5dddfa7c';
+        const groupId = '0x331e33bebec5fe0378b995e9ef7355a166e1dbd4902181d4e149dec9671a68ee';
+        const uApy = 4.9;
+
+        try {
+          // tvl * underlying APY / YT Market Cap * 0.8 + vt funding fee
+          const tvl_supply = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: 'function',
+                name: 'totalBaseToken',
+                inputs: [{ name: 'groupId', type: 'bytes32' }],
+                outputs: [{ name: '', type: 'uint256' }],
+                stateMutability: 'view',
+              },
+            ],
+            functionName: 'totalBaseToken',
+            args: [groupId],
+          });
+
+          const tvl_price = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: 'function',
+                name: 'currentBaseTokenPrice',
+                inputs: [{ name: 'groupId', type: 'bytes32' }],
+                outputs: [{ name: '', type: 'uint256' }],
+                stateMutability: 'view',
+              },
+            ],
+            functionName: 'currentBaseTokenPrice',
+            args: [groupId],
+          });
+
+          const tvl = Number(tvl_supply ?? 0) * Number(tvl_price ?? 0) / 10 ** 36;
+
+          const yt_supply = await chainClient.readContract({
+            address: yt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'totalSupply',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'totalSupply',
+            args: [],
+          });
+
+          const yt_price = await chainClient.readContract({
+            address: yt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'getNavPerShare',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'getNavPerShare',
+            args: [],
+          }); 
+
+          const yt_market_cap = Number(yt_supply ?? 0) * Number(yt_price ?? 0) / 10 ** 36;
+
+          const pt_supply = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'totalSupply',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'totalSupply',
+            args: [],
+          });
+
+          const pt_price = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'getNavPerShare',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'getNavPerShare',
+            args: [],
+          }); 
+
+          const pt_market_cap = Number(pt_supply ?? 0) * Number(pt_price ?? 0) / 10 ** 36;
+
+          const vt_funding_fee = 10 * pt_market_cap / yt_market_cap;
+
+          const basic_yield = uApy * tvl / yt_market_cap * 0.8;
+
+          annual_change_ratio = basic_yield + vt_funding_fee;
+          annual_change_ratio /= 100;
+
+        } catch (err) {
+
+        }
+
+        return annual_change_ratio;
+      },
+    },
+  ],
 });
--- sdk/constants/market-map/146/definitions/146_0_0x2f9dabe1388e41ce534c649dfa6bb7fc4293219b422ec3a654663d0f2f181604.ts
+++ sdk/constants/market-map/146/definitions/146_0_0x2f9dabe1388e41ce534c649dfa6bb7fc4293219b422ec3a654663d0f2f181604.ts
@@ -7,4 +7,165 @@ export default defineMarket({
 
 In exchange, PT-stS holders pay a certain fee to YT-stS holders.`,
   is_verified: false,
+  // @dev: Not sure what to add as token_id since discount tickets are not tokens, they are points
+  external_incentives: [
+    {
+      token_id: "not_sure_what_to_add_here",
+      label: 'Discount Tickets',
+      value: async({ roycoClient, chainClient }) => {
+        let value = "DT Multiplier";
+        const treasury = '0x56B9681B78Cc43E564E14Ba215AA62F9370d3A59';
+        const pt_address = '0xFCA91fEEe65DB34448A83a74f4f8970b5dddfa7c';
+        const groupId = '0x331e33bebec5fe0378b995e9ef7355a166e1dbd4902181d4e149dec9671a68ee';
+
+        try {
+          const tvl_supply = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: 'function',
+                name: 'totalBaseToken',
+                inputs: [{ name: 'groupId', type: 'bytes32' }],
+                outputs: [{ name: '', type: 'uint256' }],
+                stateMutability: 'view',
+              },
+            ],
+            functionName: 'totalBaseToken',
+            args: [groupId],
+          });
+  
+          const tvl_price = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: 'function',
+                name: 'currentBaseTokenPrice',
+                inputs: [{ name: 'groupId', type: 'bytes32' }],
+                outputs: [{ name: '', type: 'uint256' }],
+                stateMutability: 'view',
+              },
+            ],
+            functionName: 'currentBaseTokenPrice',
+            args: [groupId],
+          });
+  
+          const tvl = Number(tvl_supply ?? 0) * Number(tvl_price ?? 0) / 10 ** 36;
+  
+          const pt_supply = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'totalSupply',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'totalSupply',
+            args: [],
+          });
+  
+          const pt_price = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'getNavPerShare',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'getNavPerShare',
+            args: [],
+          }); 
+  
+          const pt_market_cap = Number(pt_supply ?? 0) * Number(pt_price ?? 0) / 10 ** 36;
+          const result = 80 * (tvl / pt_market_cap);
+          value = (result).toFixed() + "x Discount Tickets";
+        } catch (error) {}
+        return value;        
+      },
+    },
+    {
+      token_id: "not_sure_what_to_add_here",
+      label: 'Sonic Points',
+      value: async({ roycoClient, chainClient }) => {
+        let value = "Sonic Points Multiplier";
+        const treasury = '0x56B9681B78Cc43E564E14Ba215AA62F9370d3A59';
+        const pt_address = '0xFCA91fEEe65DB34448A83a74f4f8970b5dddfa7c';
+        const groupId = '0x331e33bebec5fe0378b995e9ef7355a166e1dbd4902181d4e149dec9671a68ee';
+
+        try {
+          const tvl_supply = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: 'function',
+                name: 'totalBaseToken',
+                inputs: [{ name: 'groupId', type: 'bytes32' }],
+                outputs: [{ name: '', type: 'uint256' }],
+                stateMutability: 'view',
+              },
+            ],
+            functionName: 'totalBaseToken',
+            args: [groupId],
+          });
+  
+          const tvl_price = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: 'function',
+                name: 'currentBaseTokenPrice',
+                inputs: [{ name: 'groupId', type: 'bytes32' }],
+                outputs: [{ name: '', type: 'uint256' }],
+                stateMutability: 'view',
+              },
+            ],
+            functionName: 'currentBaseTokenPrice',
+            args: [groupId],
+          });
+  
+          const tvl = Number(tvl_supply ?? 0) * Number(tvl_price ?? 0) / 10 ** 36;
+  
+          const pt_supply = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'totalSupply',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'totalSupply',
+            args: [],
+          });
+  
+          const pt_price = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: 'getNavPerShare',
+                outputs: [{ internalType: 'uint256', name: '', type: 'uint256',},],
+                stateMutability: 'view',
+                type: 'function',
+              },
+            ],
+            functionName: 'getNavPerShare',
+            args: [],
+          }); 
+  
+          const pt_market_cap = Number(pt_supply ?? 0) * Number(pt_price ?? 0) / 10 ** 36;
+          const result = 8 * (tvl / pt_market_cap);
+          value = (result).toFixed() + "x Sonic Points";
+        } catch (error) {}
+        return value;        
+      },
+    },
+  ],
 });
