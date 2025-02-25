ee0df57ad4d7f3c107bad886694815be7d9fc80d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-25 06:46:03 -0800

    feat(market-map): Update market -- 146_0_0x4ff251baadb17e754b09b625eddd4f3f79b689933c1c699d53aa4f7c89f2524c -- Recipe (0x4ff2...524c)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x4ff251baadb17e754b09b625eddd4f3f79b689933c1c699d53aa4f7c89f2524c (Market Hash)
    - Request Timestamp: Tue, 25 Feb 2025 14:46:03 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x4ff251baadb17e754b09b625eddd4f3f79b689933c1c699d53aa4f7c89f2524c.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x4ff251baadb17e754b09b625eddd4f3f79b689933c1c699d53aa4f7c89f2524c`,
+  name: `Test Beets stS - Do Not Use`,
+  description: `Test Beets stS - Do Not Use. 
+
+Deposit: Ws -&gt; S -&gt; stS`,
+  is_verified: false,
+});
