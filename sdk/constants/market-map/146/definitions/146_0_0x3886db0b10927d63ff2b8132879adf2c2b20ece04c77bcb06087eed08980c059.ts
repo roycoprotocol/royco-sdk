083e040c38a562b75de97b31dfb20b38bf9f8502
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-25 00:21:45 -0800

    feat(market-map): Update market -- 146_0_0x3886db0b10927d63ff2b8132879adf2c2b20ece04c77bcb06087eed08980c059 -- Recipe (0x3886...c059)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x3886db0b10927d63ff2b8132879adf2c2b20ece04c77bcb06087eed08980c059 (Market Hash)
    - Request Timestamp: Tue, 25 Feb 2025 08:21:45 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x3886db0b10927d63ff2b8132879adf2c2b20ece04c77bcb06087eed08980c059.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x3886db0b10927d63ff2b8132879adf2c2b20ece04c77bcb06087eed08980c059`,
+  name: `Test rings wstkscusd - Do Not Use`,
+  description: `Test rings wstkscusd - Do Not Use Test rings wstkscusd - Do Not Use`,
+  is_verified: false,
+});
