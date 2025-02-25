a235cd16d273567fd3383f90b82d7e9eced554dd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-25 09:56:34 -0800

    feat(market-map): Update market -- 146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8 -- Recipe (0xf7ba...4ca8)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8 (Market Hash)
    - Request Timestamp: Tue, 25 Feb 2025 17:56:33 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8`,
+  name: `Deposit S to earn yield with OS and Silo`,
+  description: `When wS is deposited, it is converted into OS, wrapped into wOS, and then is deposited into Silo to earn OS yield, Silo points, and any Royco incentives.`,
+  is_verified: false,
+});
