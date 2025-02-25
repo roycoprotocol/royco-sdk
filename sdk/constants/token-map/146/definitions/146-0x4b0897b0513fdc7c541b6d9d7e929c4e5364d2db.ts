b18ef88dd7a0757227efca1b871721fb71882f17
Solomon <63205747+solomonNSI@users.noreply.github.com>
2025-02-25 09:02:13 +0100

    fix: PT markets

---
+++ sdk/constants/token-map/146/definitions/146-0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db.ts
@@ -0,0 +1,16 @@
+// this is a fake token
+
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db",
+  chain_id: 146,
+  contract_address: "0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db",
+  name: "Discount Tickets",
+  symbol: "SJ DT",
+  image: "https://pbs.twimg.com/profile_images/1826601407648063488/zg6tr9eo_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "token",
+});
---
+++ sdk/constants/token-map/146/definitions/146-0xaa21e59bf97313b3b3850e9f878ffffc733a946a.ts
@@ -0,0 +1,16 @@
+// this is a fake token
+
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xaa21e59bf97313b3b3850e9f878ffffc733a946a",
+  chain_id: 146,
+  contract_address: "0xaa21e59bf97313b3b3850e9f878ffffc733a946a",
+  name: "Sonic Points",
+  symbol: "SP",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32684.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "token",
+});
