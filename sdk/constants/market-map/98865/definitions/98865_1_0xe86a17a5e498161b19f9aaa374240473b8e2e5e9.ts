7341393867113be1663a33e3f06a1edcd0d94575
bharatvish <bharat@waymont.co>
2025-02-26 15:20:57 -0800

    Update 98865_1_0xe86a17a5e498161b19f9aaa374240473b8e2e5e9.ts

--- sdk/constants/market-map/98865/definitions/98865_1_0xe86a17a5e498161b19f9aaa374240473b8e2e5e9.ts
+++ sdk/constants/market-map/98865/definitions/98865_1_0xe86a17a5e498161b19f9aaa374240473b8e2e5e9.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `98865_1_0xe86a17a5e498161b19f9aaa374240473b8e2e5e9`,
   name: `Supply pUSD into Nest RWA Vault`,
   description: `When pUSD is supplied to the market, it is deposited into the Nest RWA Vault to receive nRWA tokens. Users may withdraw at any time.`,
-  is_verified: false,
+  is_verified: true,
   underlying_yield: async ({ roycoClient, chainClient }) => {
     // Variable to store the underlying yield (we refer it as annual_change_ratio)
     let annual_change_ratio = 0;
