aad0d7808b08ca1a480daf5340931655e3636c30
bharatvish <bharat@waymont.co>
2025-02-27 10:01:51 -0800

    Create 1-0x4d56c9cba373ad39df69eb18f076b7348000ae09.ts

---
+++ sdk/constants/token-map/1/definitions/1-0x4d56c9cba373ad39df69eb18f076b7348000ae09.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4d56c9cba373ad39df69eb18f076b7348000ae09",
+  chain_id: 1,
+  contract_address: "0x4d56c9cba373ad39df69eb18f076b7348000ae09",
+  name: "dGHO",
+  symbol: "dGHO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/23508.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "23508",
+  type: "token",
+});
