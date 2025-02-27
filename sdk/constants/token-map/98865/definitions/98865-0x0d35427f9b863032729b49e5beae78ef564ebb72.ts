01aa1ffa61ad630e2d10e512491f1235b2fe313a
bharatvish <bharat@waymont.co>
2025-02-26 17:49:45 -0800

    Update 98865-0x0d35427f9b863032729b49e5beae78ef564ebb72.ts

--- sdk/constants/token-map/98865/definitions/98865-0x0d35427f9b863032729b49e5beae78ef564ebb72.ts
+++ sdk/constants/token-map/98865/definitions/98865-0x0d35427f9b863032729b49e5beae78ef564ebb72.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x0d35427f9b863032729b49e5beae78ef564ebb72",
   name: "Plume APY Boost",
   symbol: "pAPY",
-  image: "https://assets.plumenetwork.xyz/images/logos/pAPY-token.png",
+  image: "https://pbs.twimg.com/profile_images/1854933222569975808/no3lt9ZL_400x400.jpg",
   decimals: 18,
   source: "external",
   search_id: "papy-hardcoded",
