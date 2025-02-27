a087d60ec58fac916a8d8412e9d286bd20c5a969
Naimish Lukhi <lukhinaimish@gmail.com>
2025-02-27 15:36:25 -0500

    fix: update sonic market map

--- sdk/sonic/sonic-market-map.tsx
+++ sdk/sonic/sonic-market-map.tsx
@@ -40,16 +40,6 @@ export const sonicMarketMap: {
   app: SONIC_APP;
 }[] = [
   {
-    id: "146_1_0x4c6bac7f421fbdbb3232070517b948b8dcbdc67f",
-    appType: SONIC_APP_TYPE.EMERALD,
-    info: {
-      description:
-        "Avalon will distribute 13,125 Sonic Gems, pro-rata to all Avalon Points holders on Royco. Depositors will redeem Gems for $S after Sonic Season 1.",
-      url: "https://blog.soniclabs.com/sonic-points-simplified-how-to-qualify-for-200-million-s-airdrop/",
-    },
-    app: SONIC_APP.STABLE_JACK,
-  },
-  {
     id: "146_0_0x74634bc836c8c44e39b87ae656db006d36a6d4e3d618eabb2fabf668960168cf",
     appType: SONIC_APP_TYPE.SAPPHIRE,
     info: {
