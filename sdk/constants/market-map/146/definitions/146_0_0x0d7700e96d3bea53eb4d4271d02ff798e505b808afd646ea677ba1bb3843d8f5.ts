a1a4606600cd0b67638066bf9e3e3c590457e692
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-27 13:36:31 -0800

    feat(market-map): Update market -- 146_0_0x0d7700e96d3bea53eb4d4271d02ff798e505b808afd646ea677ba1bb3843d8f5 -- Recipe (0x0d77...d8f5) (#613)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x0d7700e96d3bea53eb4d4271d02ff798e505b808afd646ea677ba1bb3843d8f5 (Market Hash)
    - Request Timestamp: Thu, 27 Feb 2025 21:36:29 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x0d7700e96d3bea53eb4d4271d02ff798e505b808afd646ea677ba1bb3843d8f5.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x0d7700e96d3bea53eb4d4271d02ff798e505b808afd646ea677ba1bb3843d8f5`,
+  name: `Deposit wS to buy PT-wOS`,
+  description: `PT-wOS enables investors to farm Sonic points, Gems, and Stable Jack Discount Tickets with leverage while maintaining long wOS exposure. 
+
+In exchange, PT-wOS holders pay a certain fee to YT-wOS holders.`,
+  is_verified: false,
+});
