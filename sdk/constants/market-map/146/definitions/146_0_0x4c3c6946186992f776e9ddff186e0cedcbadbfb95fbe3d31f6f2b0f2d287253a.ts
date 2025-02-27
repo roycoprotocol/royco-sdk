1b765426f104645e6b29ad45e7c3977c3db2ab5c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-27 13:33:53 -0800

    feat(market-map): Update market -- 146_0_0x4c3c6946186992f776e9ddff186e0cedcbadbfb95fbe3d31f6f2b0f2d287253a -- Recipe (0x4c3c...253a) (#612)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x4c3c6946186992f776e9ddff186e0cedcbadbfb95fbe3d31f6f2b0f2d287253a (Market Hash)
    - Request Timestamp: Thu, 27 Feb 2025 21:33:51 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x4c3c6946186992f776e9ddff186e0cedcbadbfb95fbe3d31f6f2b0f2d287253a.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x4c3c6946186992f776e9ddff186e0cedcbadbfb95fbe3d31f6f2b0f2d287253a`,
+  name: `Deposit wS to buy YT-wOS`,
+  description: `YT-wOS enables investors to farm leveraged wOS yield while maintaining long $wOS exposure. 
+
+In exchange, YT-wOS holders forgo their points exposure.`,
+  is_verified: false,
+});
