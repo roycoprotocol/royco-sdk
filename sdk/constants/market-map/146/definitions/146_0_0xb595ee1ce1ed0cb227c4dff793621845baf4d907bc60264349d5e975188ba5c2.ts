14da736a268e709e3fb4051fb8d2feb0b2bc5c84
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-27 13:24:53 -0800

    feat(market-map): Update market -- 146_0_0xb595ee1ce1ed0cb227c4dff793621845baf4d907bc60264349d5e975188ba5c2 -- Recipe (0xb595...a5c2) (#610)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xb595ee1ce1ed0cb227c4dff793621845baf4d907bc60264349d5e975188ba5c2 (Market Hash)
    - Request Timestamp: Thu, 27 Feb 2025 21:24:51 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xb595ee1ce1ed0cb227c4dff793621845baf4d907bc60264349d5e975188ba5c2.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xb595ee1ce1ed0cb227c4dff793621845baf4d907bc60264349d5e975188ba5c2`,
+  name: `Deposit wS to buy YT-stS`,
+  description: `YT-stS enables investors to farm leveraged stS yield while maintaining long $stS exposure. 
+
+In exchange, YT-stS holders forgo their points exposure.`,
+  is_verified: false,
+});
