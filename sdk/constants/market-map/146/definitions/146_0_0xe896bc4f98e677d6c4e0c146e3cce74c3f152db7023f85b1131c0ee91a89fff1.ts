66a378f3096833c3ccffde8d8e8849bc6a2cd1b3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-27 21:53:49 -0800

    feat(market-map): Update market -- 146_0_0xe896bc4f98e677d6c4e0c146e3cce74c3f152db7023f85b1131c0ee91a89fff1 -- Recipe (0xe896...fff1)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xe896bc4f98e677d6c4e0c146e3cce74c3f152db7023f85b1131c0ee91a89fff1 (Market Hash)
    - Request Timestamp: Fri, 28 Feb 2025 05:53:48 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xe896bc4f98e677d6c4e0c146e3cce74c3f152db7023f85b1131c0ee91a89fff1.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xe896bc4f98e677d6c4e0c146e3cce74c3f152db7023f85b1131c0ee91a89fff1`,
+  name: `Deposit wS to buy PT-scUSD`,
+  description: `PT-scUSD enables investors to farm Sonic points, Gems, Rings Points, Veda Points, and Stable Jack Discount Tickets with leverage while maintaining scUSD exposure.
+
+In exchange, PT-scUSD holders pay a certain fee to YT-scUSD holders.`,
+  is_verified: false,
+});
