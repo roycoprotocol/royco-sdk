efa794ce89ec8f90fe73433ed710824c097a9468
Solomon <63205747+solomonNSI@users.noreply.github.com>
2025-02-28 09:50:19 +0100

    feat: stablejack

--- sdk/constants/market-map/146/definitions/146_0_0x30f002e2f62bc4155d273b9745b5b40863b7ce80ec9f861ba08ca3e33f9e549c.ts
+++ sdk/constants/market-map/146/definitions/146_0_0x30f002e2f62bc4155d273b9745b5b40863b7ce80ec9f861ba08ca3e33f9e549c.ts
@@ -7,4 +7,185 @@ export default defineMarket({
 
 In exchange, PT-stS holders pay a certain fee to YT-stS holders.`,
   is_verified: false,
+  external_incentives: [
+    {
+      token_id: "146-0x3d10814c737b98de96f5010063ad4eb13d9c188e",
+      label: "Gems Boost",
+      value: async ({ roycoClient, chainClient }) => {
+        return "420";
+      },
+    },
+    {
+      token_id: "146-0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db",
+      label: "Discount Tickets",
+      value: async ({ roycoClient, chainClient }) => {
+        let value = "DT Multiplier";
+        const treasury = "0x56B9681B78Cc43E564E14Ba215AA62F9370d3A59";
+        const pt_address = "0xFCA91fEEe65DB34448A83a74f4f8970b5dddfa7c";
+        const groupId =
+          "0x331e33bebec5fe0378b995e9ef7355a166e1dbd4902181d4e149dec9671a68ee";
+
+        try {
+          const tvl_supply = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: "function",
+                name: "totalBaseToken",
+                inputs: [{ name: "groupId", type: "bytes32" }],
+                outputs: [{ name: "", type: "uint256" }],
+                stateMutability: "view",
+              },
+            ],
+            functionName: "totalBaseToken",
+            args: [groupId],
+          });
+
+          const tvl_price = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: "function",
+                name: "currentBaseTokenPrice",
+                inputs: [{ name: "groupId", type: "bytes32" }],
+                outputs: [{ name: "", type: "uint256" }],
+                stateMutability: "view",
+              },
+            ],
+            functionName: "currentBaseTokenPrice",
+            args: [groupId],
+          });
+
+          const tvl =
+            (Number(tvl_supply ?? 0) * Number(tvl_price ?? 0)) / 10 ** 36;
+
+          const pt_supply = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: "totalSupply",
+                outputs: [
+                  { internalType: "uint256", name: "", type: "uint256" },
+                ],
+                stateMutability: "view",
+                type: "function",
+              },
+            ],
+            functionName: "totalSupply",
+            args: [],
+          });
+
+          const pt_price = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: "nav",
+                outputs: [
+                  { internalType: "uint256", name: "", type: "uint256" },
+                ],
+                stateMutability: "view",
+                type: "function",
+              },
+            ],
+            functionName: "nav",
+            args: [],
+          });
+
+          const pt_market_cap =
+            (Number(pt_supply ?? 0) * Number(pt_price ?? 0)) / 10 ** 36;
+          const result = 80 * (tvl / pt_market_cap);
+          value = result.toFixed() + "x Discount Tickets";
+        } catch (error) {}
+        return value;
+      },
+    },
+    {
+      token_id: "146-0xaa21e59bf97313b3b3850e9f878ffffc733a946a",
+      label: "Sonic Points",
+      value: async ({ roycoClient, chainClient }) => {
+        let value = "Sonic Points Multiplier";
+        const treasury = "0x56B9681B78Cc43E564E14Ba215AA62F9370d3A59";
+        const pt_address = "0xFCA91fEEe65DB34448A83a74f4f8970b5dddfa7c";
+        const groupId =
+          "0x331e33bebec5fe0378b995e9ef7355a166e1dbd4902181d4e149dec9671a68ee";
+
+        try {
+          const tvl_supply = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: "function",
+                name: "totalBaseToken",
+                inputs: [{ name: "groupId", type: "bytes32" }],
+                outputs: [{ name: "", type: "uint256" }],
+                stateMutability: "view",
+              },
+            ],
+            functionName: "totalBaseToken",
+            args: [groupId],
+          });
+
+          const tvl_price = await chainClient.readContract({
+            address: treasury,
+            abi: [
+              {
+                type: "function",
+                name: "currentBaseTokenPrice",
+                inputs: [{ name: "groupId", type: "bytes32" }],
+                outputs: [{ name: "", type: "uint256" }],
+                stateMutability: "view",
+              },
+            ],
+            functionName: "currentBaseTokenPrice",
+            args: [groupId],
+          });
+
+          const tvl =
+            (Number(tvl_supply ?? 0) * Number(tvl_price ?? 0)) / 10 ** 36;
+
+          const pt_supply = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: "totalSupply",
+                outputs: [
+                  { internalType: "uint256", name: "", type: "uint256" },
+                ],
+                stateMutability: "view",
+                type: "function",
+              },
+            ],
+            functionName: "totalSupply",
+            args: [],
+          });
+
+          const pt_price = await chainClient.readContract({
+            address: pt_address,
+            abi: [
+              {
+                inputs: [],
+                name: "nav",
+                outputs: [
+                  { internalType: "uint256", name: "", type: "uint256" },
+                ],
+                stateMutability: "view",
+                type: "function",
+              },
+            ],
+            functionName: "nav",
+            args: [],
+          });
+
+          const pt_market_cap =
+            (Number(pt_supply ?? 0) * Number(pt_price ?? 0)) / 10 ** 36;
+          const result = 8 * (tvl / pt_market_cap);
+          value = result.toFixed() + "x Sonic Points";
+        } catch (error) {}
+        return value;
+      },
+    },
+  ],
 });
---
+++ sdk/constants/token-map/146/definitions/146-0xbb9beb82ebf43d02d3a3f4fd431906d1e39a117d.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xbb9beb82ebf43d02d3a3f4fd431906d1e39a117d",
+  chain_id: 146,
+  contract_address: "0xbb9beb82ebf43d02d3a3f4fd431906d1e39a117d",
+  name: "Rings Points",
+  symbol: "RP",
+  image: "https://pbs.twimg.com/profile_images/1862403917717352448/vWaDsAEu_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "token",
+});
