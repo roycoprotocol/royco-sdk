7e90fa7aad4f66567e5445c9a0fecd053245d82b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-02-27 21:50:20 -0800

    feat(market-map): Update market -- 146_0_0xa617b6fa04e006f79053160f4c6d531a2285a1c7f685eea02d4b9545b4ea58a1 -- Recipe (0xa617...58a1)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xa617b6fa04e006f79053160f4c6d531a2285a1c7f685eea02d4b9545b4ea58a1 (Market Hash)
    - Request Timestamp: Fri, 28 Feb 2025 05:50:19 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xa617b6fa04e006f79053160f4c6d531a2285a1c7f685eea02d4b9545b4ea58a1.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xa617b6fa04e006f79053160f4c6d531a2285a1c7f685eea02d4b9545b4ea58a1`,
+  name: `Deposit wS to buy YT-stS`,
+  description: `YT-scUSD enables investors to farm leveraged scUSD yield.
+
+In exchange, YT-scUSD holders forgo their points exposure.`,
+  is_verified: false,
+});
