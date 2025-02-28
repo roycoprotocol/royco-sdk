7eb505a5f1446b92c6f811c25b69128735670654
GitHub Action <action@github.com>
2025-02-28 01:12:43 +0000

    feat(chore): update generated files

--- sdk/constants/market-map/146/definitions/146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8.ts
@@ -5,7 +5,7 @@ export default defineMarket({
   name: `Deposit S to earn yield with Origin and Silo`,
   description: `When wS is deposited, it is converted into OS, wrapped into wOS, and then is deposited into Silo to earn OS yield, Silo points, and any Royco incentives.`,
   is_verified: true,
- // category: "???",
+  // category: "???",
   // incentive_ids: [],
   // external_incentives: [],
 });
