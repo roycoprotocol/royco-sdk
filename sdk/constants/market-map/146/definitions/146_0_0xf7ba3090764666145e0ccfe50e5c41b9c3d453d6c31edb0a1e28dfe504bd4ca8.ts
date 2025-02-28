8d183c934d677b890a02a8214ce592c578ff2066
bharatvish <bharat@waymont.co>
2025-02-27 16:30:39 -0800

    Update 146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8.ts

--- sdk/constants/market-map/146/definitions/146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8.ts
@@ -2,9 +2,9 @@ import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
   id: `146_0_0xf7ba3090764666145e0ccfe50e5c41b9c3d453d6c31edb0a1e28dfe504bd4ca8`,
-  name: `Deposit S to earn yield with OS and Silo`,
+  name: `Deposit S to earn yield with Origin and Silo`,
   description: `When wS is deposited, it is converted into OS, wrapped into wOS, and then is deposited into Silo to earn OS yield, Silo points, and any Royco incentives.`,
-  is_verified: false,
+  is_verified: true,
  // category: "???",
   // incentive_ids: [],
   // external_incentives: [],
