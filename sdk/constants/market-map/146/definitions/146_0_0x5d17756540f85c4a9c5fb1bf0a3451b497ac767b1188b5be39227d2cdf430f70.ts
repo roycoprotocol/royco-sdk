19df00415f90497a9cd438d2d7613617f3880441
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-01 07:11:37 -0800

    feat(market-map): Update market -- 146_0_0x5d17756540f85c4a9c5fb1bf0a3451b497ac767b1188b5be39227d2cdf430f70 -- Recipe (0x5d17...0f70)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x5d17756540f85c4a9c5fb1bf0a3451b497ac767b1188b5be39227d2cdf430f70 (Market Hash)
    - Request Timestamp: Sat, 01 Mar 2025 15:11:36 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x5d17756540f85c4a9c5fb1bf0a3451b497ac767b1188b5be39227d2cdf430f70.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x5d17756540f85c4a9c5fb1bf0a3451b497ac767b1188b5be39227d2cdf430f70`,
+  name: `test 44 test`,
+  description: `test 44 test test 44 test test 44 test`,
+  is_verified: false,
+});
