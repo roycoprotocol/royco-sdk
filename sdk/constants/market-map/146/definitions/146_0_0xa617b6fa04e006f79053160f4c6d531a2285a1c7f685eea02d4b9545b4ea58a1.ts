b7186ded6c980e85edc83478223539225edf9d11
bharatvish <bharat@waymont.co>
2025-03-01 21:17:36 -0800

    Update 146_0_0xa617b6fa04e006f79053160f4c6d531a2285a1c7f685eea02d4b9545b4ea58a1.ts

--- sdk/constants/market-map/146/definitions/146_0_0xa617b6fa04e006f79053160f4c6d531a2285a1c7f685eea02d4b9545b4ea58a1.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xa617b6fa04e006f79053160f4c6d531a2285a1c7f685eea02d4b9545b4ea58a1.ts
@@ -102,13 +102,4 @@ In exchange, YT-scUSD holders forgo their points exposure.`,
       },
     },
   ],
-  external_incentives: [
-    {
-      token_id: "146-0x3d10814c737b98de96f5010063ad4eb13d9c188e",
-      label: "Gems Boost",
-      value: async ({ roycoClient, chainClient }) => {
-        return "420";
-      },
-    },
-  ],
 });
