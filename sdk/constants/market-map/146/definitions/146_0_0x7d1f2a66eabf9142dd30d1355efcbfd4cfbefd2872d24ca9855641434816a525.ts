5e6099e907a75c8497d5b06daf8ce2968d9c4520
bharatvish <bharat@waymont.co>
2025-03-02 21:49:44 -0800

    Update 146_0_0x7d1f2a66eabf9142dd30d1355efcbfd4cfbefd2872d24ca9855641434816a525.ts

--- sdk/constants/market-map/146/definitions/146_0_0x7d1f2a66eabf9142dd30d1355efcbfd4cfbefd2872d24ca9855641434816a525.ts
+++ sdk/constants/market-map/146/definitions/146_0_0x7d1f2a66eabf9142dd30d1355efcbfd4cfbefd2872d24ca9855641434816a525.ts
@@ -2,7 +2,7 @@ import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
   id: `146_0_0x7d1f2a66eabf9142dd30d1355efcbfd4cfbefd2872d24ca9855641434816a525`,
-  name: `Simple USDC Rings Vault`,
+  name: `Deposit USDC into Rings to Mint and Stake scUSD`,
   description: `When USDC is deposited in the market it is used to mint scUSD, staked and auto-compounds while collecting points. There is a 5-day redemption period for withdrawals`,
   is_verified: true,
 });
