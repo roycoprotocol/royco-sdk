c008411644f881a7db341027cdbed25f8659825a
githubotoro <yupuday.git@gmail.com>
2025-03-03 09:41:07 -0800

    feat(app): gems boost

--- sdk/constants/market-map/146/definitions/146_0_0x74634bc836c8c44e39b87ae656db006d36a6d4e3d618eabb2fabf668960168cf.ts
+++ sdk/constants/market-map/146/definitions/146_0_0x74634bc836c8c44e39b87ae656db006d36a6d4e3d618eabb2fabf668960168cf.ts
@@ -6,10 +6,6 @@ export default defineMarket({
   description: `PT-wOS enables investors to farm Sonic points, Gems, and Stable Jack Discount Tickets with leverage while maintaining long wOS exposure. In exchange, PT-wOS holders pay a certain fee to YT-wOS holders.`,
   is_verified: false,
 
-  // @dev: Not sure what to add as token_id since discount tickets are not tokens, they are points
-  // @note: You need to create a fake token with a random address and PR that into the SDK
-  // because even though it's a point, it's need to be represented somehow in the UI
-  // so that the UI can display it -- see this below for example of "Gems Boost"
   external_incentives: [
     {
       token_id: "146-0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db",
