46a22a9ad706a7f4082f00605a9a079b207b7e77
GitHub Action <action@github.com>
2025-03-04 15:39:58 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/146/definitions/146-0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38.ts
+++ sdk/constants/token-map/146/definitions/146-0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38.ts
@@ -6,8 +6,7 @@ export default defineToken({
   contract_address: "0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38",
   name: "Wrapped Sonic",
   symbol: "WS",
-  image:
-    "https://i.ibb.co/MDq6zYhQ/w-S-token-400.png",
+  image: "https://i.ibb.co/MDq6zYhQ/w-S-token-400.png",
   decimals: 18,
   source: "coingecko",
   search_id: "wrapped-sonic",
