0c9526f8afb76b9d8ec3374604ef222427c7b3b1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-04 07:21:58 -0800

    feat(market-map): Update market -- 146_0_0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6 -- Recipe (0xc2b6...8ca6) (#642)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6 (Market Hash)
    - Request Timestamp: Tue, 04 Mar 2025 15:21:56 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6`,
+  name: `Supply stS to Stability x Silo Leverage Vault`,
+  description: `stS is supplied and wS is borrowed with x17.4 leverage.`,
+  is_verified: false,
+});
