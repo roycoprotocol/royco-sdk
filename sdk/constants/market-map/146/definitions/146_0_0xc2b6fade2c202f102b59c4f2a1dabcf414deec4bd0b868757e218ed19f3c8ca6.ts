867e465221abb5d45371067c66f1f68ccec08c37
bharatvish <bharat@waymont.co>
2025-03-04 08:09:12 -0800

    Update 146_0_0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6.ts

--- sdk/constants/market-map/146/definitions/146_0_0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `146_0_0xc2b6fade2c202f102b59c4f2a1dabcf414deec4bd0b868757e218ed19f3c8ca6`,
   name: `Supply stS to Stability x Silo Leverage Vault`,
-  description: `stS is supplied and wS is borrowed with x17.4 leverage.`,
+  description: `stS is supplied and wS is borrowed with x17.4 leverage. DISCLAIMER: APR of the vault depends on Silo rates, Please DYOR beforing depositing into this high risk vault. High borrowing costs can lead to negative returns in volatile markets. `,
   is_verified: false,
 });
