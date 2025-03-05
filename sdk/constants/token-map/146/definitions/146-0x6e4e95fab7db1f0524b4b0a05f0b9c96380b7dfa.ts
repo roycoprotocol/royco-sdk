c1650bbedc1b507b52101c60a977e4d2cf76dd70
bharatvish <bharat@waymont.co>
2025-03-05 15:48:44 -0800

    Update 146-0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa.ts

--- sdk/constants/token-map/146/definitions/146-0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa.ts
+++ sdk/constants/token-map/146/definitions/146-0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa.ts
@@ -4,8 +4,8 @@ export default defineToken({
   id: "146-0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa",
   chain_id: 146,
   contract_address: "0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa",
-  name: "Pendle Market 2",
-  symbol: "PENDLE-LPT2",
+  name: "Pendle-LPT-wstkscUSD",
+  symbol: "Pendle-LPT-wstkscUSD",
   image:
     "https://pbs.twimg.com/profile_images/1621033017337843713/loDsYCJr_400x400.jpg",
   decimals: 18,
