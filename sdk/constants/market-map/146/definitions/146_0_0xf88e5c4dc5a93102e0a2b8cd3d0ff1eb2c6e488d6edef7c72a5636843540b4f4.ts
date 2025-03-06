0058d66f6a3d81ed534f84e0d291c500755e14f8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-06 10:14:08 -0800

    feat(market-map): Update market -- 146_0_0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4 -- Recipe (0xf88e...b4f4) (#656)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4 (Market Hash)
    - Request Timestamp: Thu, 06 Mar 2025 18:14:06 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4`,
+  name: `Supply wOS to Stability x Silo Leverage Vault`,
+  description: `wOS is supplied and wS is borrowed with x9.5 leverage. DISCLAIMER: APR of the vault depends on Silo rates, Please DYOR beforing`,
+  is_verified: false,
+});
