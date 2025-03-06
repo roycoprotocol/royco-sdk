358ed6e0b42c51c16aaa99da0a5467fdf5a51335
jintao <jintao@berachain.com>
2025-03-06 15:40:17 -0600

    chore: include duration

--- sdk/boyco/bera-yield/get-buckets.tsx
+++ sdk/boyco/bera-yield/get-buckets.tsx
@@ -3,6 +3,10 @@ import { getMarketAssetType, getMarketMultiplier } from "./manage-multiplier";
 import { MULTIPLIER_ASSET_TYPE } from "./manage-multiplier";
 import { sumBy } from "lodash";
 
+function getDuration(market: EnrichedMarketDataType): number {
+  return market.lockup_time ? Number(market.lockup_time) / (60 * 60 * 24) : 90;
+}
+
 export const getBuckets = (markets: EnrichedMarketDataType[]) => {
   const MajorMarkets = markets.filter(
     (market) => getMarketAssetType(market) === MULTIPLIER_ASSET_TYPE.MAJOR_ONLY,
@@ -15,13 +19,13 @@ export const getBuckets = (markets: EnrichedMarketDataType[]) => {
   const majorBucketWeight = sumBy(MajorMarkets, (market) => {
     const tvl = market.locked_quantity_usd ?? 0;
     const multiplier = +getMarketMultiplier(market);
-    return tvl * multiplier;
+    return tvl * multiplier * getDuration(market);
   });
 
   const thirdPartyBucketWeight = sumBy(ThirdPartyMarkets, (market) => {
     const tvl = market.locked_quantity_usd ?? 0;
     const multiplier = +getMarketMultiplier(market);
-    return tvl * multiplier;
+    return tvl * multiplier * getDuration(market);
   });
 
   return {
