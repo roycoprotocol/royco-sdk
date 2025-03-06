2a9b40c285f2449a9234d88c476d15f4a516da39
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-06 12:50:15 -0800

    feat(token-map): Update Token -- 8453-0x1c7a460413dd4e964f96d8dfc56e7223ce88cd85 -- Seamless Protocol (SEAM) (0x1c7a...cd85)
    Token Details:
    - Id: 8453-0x1c7a460413dd4e964f96d8dfc56e7223ce88cd85
    - Chain Id: 8453
    - Contract Address: 0x1c7a460413dd4e964f96d8dfc56e7223ce88cd85
    - Name: Seamless Protocol
    - Symbol: SEAM
    - Image: https://coin-images.coingecko.com/coins/images/33480/large/Seamless_Logo_Black_Transparent.png?1702019657
    - Decimals: 18
    - Source: coingecko
    - Search Id: seamless-protocol

---
+++ sdk/constants/token-map/8453/definitions/8453-0x1c7a460413dd4e964f96d8dfc56e7223ce88cd85.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0x1c7a460413dd4e964f96d8dfc56e7223ce88cd85",
+  chain_id: 8453,
+  contract_address: "0x1c7a460413dd4e964f96d8dfc56e7223ce88cd85",
+  name: "Seamless Protocol",
+  symbol: "SEAM",
+  image: "https://coin-images.coingecko.com/coins/images/33480/large/Seamless_Logo_Black_Transparent.png?1702019657",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "seamless-protocol",
+  type: "token",
+});
