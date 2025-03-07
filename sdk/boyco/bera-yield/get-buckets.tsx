80c9a2dccb588c52cfd549942aa2d1ba04f4c9c8
githubotoro <yupuday.git@gmail.com>
2025-03-06 18:19:12 -0800

    feat(boyco): revert change

--- sdk/boyco/bera-yield/get-buckets.tsx
+++ sdk/boyco/bera-yield/get-buckets.tsx
@@ -3,10 +3,6 @@ import { getMarketAssetType, getMarketMultiplier } from "./manage-multiplier";
 import { MULTIPLIER_ASSET_TYPE } from "./manage-multiplier";
 import { sumBy } from "lodash";
 
-function getDuration(market: EnrichedMarketDataType): number {
-  return market.lockup_time ? Number(market.lockup_time) / (60 * 60 * 24) : 90;
-}
-
 export const getBuckets = (markets: EnrichedMarketDataType[]) => {
   const MajorMarkets = markets.filter(
     (market) => getMarketAssetType(market) === MULTIPLIER_ASSET_TYPE.MAJOR_ONLY,
@@ -18,13 +14,13 @@ export const getBuckets = (markets: EnrichedMarketDataType[]) => {
   const majorBucketWeight = sumBy(MajorMarkets, (market) => {
     const tvl = market.locked_quantity_usd ?? 0;
     const multiplier = +getMarketMultiplier(market);
-    return tvl * multiplier * getDuration(market);
+    return tvl * multiplier;
   });
 
   const thirdPartyBucketWeight = sumBy(ThirdPartyMarkets, (market) => {
     const tvl = market.locked_quantity_usd ?? 0;
     const multiplier = +getMarketMultiplier(market);
-    return tvl * multiplier * getDuration(market);
+    return tvl * multiplier;
   });
 
   return {
