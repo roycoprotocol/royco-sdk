70d4df06fa01508eabef8ae413b3ffe83850d15c
bharatvish <bharat@waymont.co>
2025-03-07 11:12:15 -0800

    Create 146-0xa2161e75edf50d70544e6588788a5732a3105c00.ts

---
+++ sdk/constants/token-map/146/definitions/146-0xa2161e75edf50d70544e6588788a5732a3105c00.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xa2161e75edf50d70544e6588788a5732a3105c00",
+  chain_id: 146,
+  contract_address: "0xa2161e75edf50d70544e6588788a5732a3105c00",
+  name: "PT Wrapped stkscETH",
+  symbol: "PT wstkscETH",
+   image:
+    "https://pbs.twimg.com/profile_images/1621033017337843713/loDsYCJr_400x400.jpg",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "token",
+});
