ca780a7741e5e58abc50e544307296f9f4536a52
bharatvish <bharat@waymont.co>
2025-03-07 09:18:56 -0800

    Update 146_0_0xfc54f53282763606c7cbf28db3490b9781c9bbb3ae228bd586b97926816e0244.ts

--- sdk/constants/market-map/146/definitions/146_0_0xfc54f53282763606c7cbf28db3490b9781c9bbb3ae228bd586b97926816e0244.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xfc54f53282763606c7cbf28db3490b9781c9bbb3ae228bd586b97926816e0244.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `146_0_0xfc54f53282763606c7cbf28db3490b9781c9bbb3ae228bd586b97926816e0244`,
   name: `LP into the wstkscETH pool on Pendle`,
   description: `The Pendle wstkscETH LP token is supplied to the market as proof that you&#x27;ve provided liquidity to the Pendle wstkscETH pool. Depositors may exit at anytime, however withdrawing before the end of the 30-day period will forfeit all incentives earned to date. The maturity period for this market is May 28, 2025 and this market will stop accepting new deposits 30 days prior to this date.`,
-  is_verified: true,
+  is_verified: false,
 
   native_yield: [
     {
