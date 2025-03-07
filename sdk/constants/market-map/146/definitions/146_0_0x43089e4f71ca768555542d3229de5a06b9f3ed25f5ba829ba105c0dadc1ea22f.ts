87f77ca6385d69bc3417f9e456d4d83db6e78d17
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-07 12:05:35 -0800

    feat(market-map): Update market -- 146_0_0x43089e4f71ca768555542d3229de5a06b9f3ed25f5ba829ba105c0dadc1ea22f -- Recipe (0x4308...a22f)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x43089e4f71ca768555542d3229de5a06b9f3ed25f5ba829ba105c0dadc1ea22f (Market Hash)
    - Request Timestamp: Fri, 07 Mar 2025 20:05:34 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x43089e4f71ca768555542d3229de5a06b9f3ed25f5ba829ba105c0dadc1ea22f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x43089e4f71ca768555542d3229de5a06b9f3ed25f5ba829ba105c0dadc1ea22f`,
+  name: `Deposit PT-wstkscUSD into Royco`,
+  description: `Deposit your PT and lock for 30 days. Depositors may exit at anytime, however withdrawing before the end of the 30-day period will forfeit all incentives earned to date. The maturity period for this market is May 28, 2025 and this market will stop accepting new deposits 30 days prior to this date.`,
+  is_verified: false,
+});
