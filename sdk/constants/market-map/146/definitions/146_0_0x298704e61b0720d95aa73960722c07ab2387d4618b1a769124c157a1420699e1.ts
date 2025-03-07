67e311ddc7d109c346e523fc5aba6c95ef483f8f
bharatvish <bharat@waymont.co>
2025-03-07 12:37:49 -0800

    Update 146_0_0x298704e61b0720d95aa73960722c07ab2387d4618b1a769124c157a1420699e1.ts

--- sdk/constants/market-map/146/definitions/146_0_0x298704e61b0720d95aa73960722c07ab2387d4618b1a769124c157a1420699e1.ts
+++ sdk/constants/market-map/146/definitions/146_0_0x298704e61b0720d95aa73960722c07ab2387d4618b1a769124c157a1420699e1.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `146_0_0x298704e61b0720d95aa73960722c07ab2387d4618b1a769124c157a1420699e1`,
   name: `Deposit PT-wstkscETH into Royco`,
-  description: `Deposit your PT token and lock for 30 days. Depositors may exit at anytime, however withdrawing before the end of the 30-day period will forfeit all incentives earned to date. The maturity period for this market is May 28, 2025 and this market will stop accepting new deposits 30 days prior to this date.`,
+  description: `Deposit your PT and lock for 30 days. Depositors may exit at anytime, however withdrawing before the end of the 30-day period will forfeit all incentives earned to date. The maturity period for this market is May 28, 2025 and this market will stop accepting new deposits 30 days prior to this date.`,
   is_verified: false,
 });
