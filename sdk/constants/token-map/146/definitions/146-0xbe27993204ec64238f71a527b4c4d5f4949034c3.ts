65c0a2bb5ed5b2c2b6f66cdbf58985e31e283314
bharatvish <bharat@waymont.co>
2025-03-07 11:17:59 -0800

    Update 146-0xbe27993204ec64238f71a527b4c4d5f4949034c3.ts

--- sdk/constants/token-map/146/definitions/146-0xbe27993204ec64238f71a527b4c4d5f4949034c3.ts
+++ sdk/constants/token-map/146/definitions/146-0xbe27993204ec64238f71a527b4c4d5f4949034c3.ts
@@ -5,7 +5,7 @@ export default defineToken({
   chain_id: 146,
   contract_address: "0xbe27993204ec64238f71a527b4c4d5f4949034c3",
   name: "PT Wrapped stkscUSD",
-  symbol: "PT wstkscUSD",
+  symbol: "PT-wstkscUSD",
    image:
     "https://pbs.twimg.com/profile_images/1621033017337843713/loDsYCJr_400x400.jpg",
   decimals: 18,
