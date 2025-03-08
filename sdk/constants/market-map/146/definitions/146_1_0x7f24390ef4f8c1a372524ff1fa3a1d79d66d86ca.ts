5715a733b0d64f56724454a3462c3c8de217dd37
bharatvish <bharat@waymont.co>
2025-03-07 16:54:11 -0800

    Update 146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts

--- sdk/constants/market-map/146/definitions/146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts
+++ sdk/constants/market-map/146/definitions/146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts
@@ -3,7 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca`,
   name: `Deposit OS into Origin`,
-  description: `Deposits S to Origin Sonic (OS) to earn passive yield. Learn more at https:&#x2F;&#x2F;www.originprotocol.com&#x2F;os`,
+  description: `Deposits Origin Sonic (OS) to earn passive yield. Learn more at https:&#x2F;&#x2F;www.originprotocol.com&#x2F;os`,
   is_verified: true,
   // category: "???",
   // incentive_ids: [],
