e5286264f1b6a9e27a7f76860cb4343808b9cd57
bharatvish <bharat@waymont.co>
2025-03-07 16:44:28 -0800

    Update 146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts

--- sdk/constants/market-map/146/definitions/146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts
+++ sdk/constants/market-map/146/definitions/146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca`,
   name: `Deposit S to OS`,
   description: `Deposits S to Origin Sonic (OS) to earn passive yield. Learn more at https:&#x2F;&#x2F;www.originprotocol.com&#x2F;os`,
-  is_verified: false,
+  is_verified: true,
   // category: "???",
   // incentive_ids: [],
   // external_incentives: [],
@@ -28,4 +28,13 @@ export default defineMarket({
 
     return annual_change_ratio;
   },
+  external_incentives: [
+    {
+      token_id: "146-0xaa21e59bf97313b3b3850e9f878ffffc733a946a",
+      label: "Sonic Points",
+      value: async ({ roycoClient, chainClient }) => {
+        return "12x Sonic Points";
+      },
+    },
+  ],
 });
