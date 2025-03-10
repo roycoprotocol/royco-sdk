c6a1a46b00dbc31b2066676ee37885ca9fe782e8
bharatvish <bharat@waymont.co>
2025-03-10 11:31:51 -0700

    Update 146_0_0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4.ts

--- sdk/constants/market-map/146/definitions/146_0_0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4.ts
@@ -3,7 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `146_0_0xf88e5c4dc5a93102e0a2b8cd3d0ff1eb2c6e488d6edef7c72a5636843540b4f4`,
   name: `Supply wOS to Stability x Silo Leverage Vault`,
-  description: `wOS is supplied and wS is borrowed with x9.5 leverage. DISCLAIMER: APR of the vault depends on Silo rates, DISCLAIMER: APR of the vault depends on Silo rates, Please DYOR beforing depositing into this high risk vault. High borrowing costs can lead to negative returns in volatile markets. `,
+  description: `wOS is supplied and wS is borrowed with x9.5 leverage. DISCLAIMER: APR of the vault depends on Silo rates, Please DYOR beforing depositing into this high risk vault. High borrowing costs can lead to negative returns in volatile markets. `,
   is_verified: true,
   underlying_yield: async ({ roycoClient, chainClient }) => {
     let annual_change_ratio = 0;
