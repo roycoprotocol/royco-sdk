1bb0056cc52961c9aafb38891c8f91fa93c05c66
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-10 02:21:22 -0700

    feat(market-map): Update market -- 11155111_1_0xc64877aae6437c11c72880d0d61e9e81c2310a22 -- Vault (0xc648...0a22) (#674)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 1 (Vault)
    - Market ID: 0xc64877aae6437c11c72880d0d61e9e81c2310a22 (Wrapped Vault Address)
    - Request Timestamp: Mon, 10 Mar 2025 09:21:18 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_1_0xc64877aae6437c11c72880d0d61e9e81c2310a22.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_1_0xc64877aae6437c11c72880d0d61e9e81c2310a22`,
+  name: `Deposit TST token to CTST vault`,
+  description: `Deposit TST token to CTST vault to earn points incentive`,
+  is_verified: false,
+});
