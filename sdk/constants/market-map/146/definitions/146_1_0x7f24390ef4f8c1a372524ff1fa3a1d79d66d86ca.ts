36efd989eaa162a1a3e9c1d2debb81a1f2b012b9
bharatvish <bharat@waymont.co>
2025-03-10 10:37:26 -0700

    Update 146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts

--- sdk/constants/market-map/146/definitions/146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts
+++ sdk/constants/market-map/146/definitions/146_1_0x7f24390ef4f8c1a372524ff1fa3a1d79d66d86ca.ts
@@ -8,26 +8,6 @@ export default defineMarket({
   // category: "???",
   // incentive_ids: [],
   // external_incentives: [],
-  underlying_yield: async () => {
-    let annual_change_ratio = 0;
-
-    try {
-      const req = await fetch(
-        "https://api.originprotocol.com/api/v2/os/apr/trailing",
-      );
-
-      const data = (await req.json()) as {
-        apr: number; // 1 = 1%
-        apy: number; // 1 = 1%
-      };
-
-      annual_change_ratio = data.apr / 100; // Fix so .01 = 1%
-    } catch (err) {
-      // Omit error for clean server logs
-    }
-
-    return annual_change_ratio;
-  },
   external_incentives: [
     {
       token_id: "146-0xaa21e59bf97313b3b3850e9f878ffffc733a946a",
