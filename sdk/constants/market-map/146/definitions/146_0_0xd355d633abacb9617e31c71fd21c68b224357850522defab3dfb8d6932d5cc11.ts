9d092bc16305e57e298774d901710a90e1f03c8d
bharatvish <bharat@waymont.co>
2025-03-10 14:01:50 -0700

    Update 146_0_0xd355d633abacb9617e31c71fd21c68b224357850522defab3dfb8d6932d5cc11.ts

--- sdk/constants/market-map/146/definitions/146_0_0xd355d633abacb9617e31c71fd21c68b224357850522defab3dfb8d6932d5cc11.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xd355d633abacb9617e31c71fd21c68b224357850522defab3dfb8d6932d5cc11.ts
@@ -4,7 +4,7 @@ export default defineMarket({
   id: `146_0_0xd355d633abacb9617e31c71fd21c68b224357850522defab3dfb8d6932d5cc11`,
   name: `Stake S for stS on Beets`,
   description: `When wS is supplied to the market, it is staked for stS on Beets. Users receive stS when they exit the market. `,
-  is_verified: false,
+  is_verified: true,
 
   underlying_yield: async ({ roycoClient, chainClient }) => {
     // Add a fallback value just in case the API call fails
