e905921d5d5d0b3d430c4d7d43785561fe1b2652
bharatvish <bharat@waymont.co>
2025-03-11 12:48:44 -0700

    Create 146-0x3605509B2C8Bff9808da5dd5c81547d9EDC4Ffa2.ts

---
+++ sdk/constants/token-map/146/definitions/146-0x3605509B2C8Bff9808da5dd5c81547d9EDC4Ffa2.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x3605509B2C8Bff9808da5dd5c81547d9EDC4Ffa2",
+  chain_id: 146,
+  contract_address: "0x3605509B2C8Bff9808da5dd5c81547d9EDC4Ffa2",
+  name: "Silo_PT-wstkscUSD-29MAY2025_USDC.e",
+  symbol: "Silo_PT-wstkscUSD-29MAY2025_USDC.e",
+  image:
+    "https://pbs.twimg.com/profile_images/1611016571253624832/bz0cfjtN_400x400.png",
+  decimals: 6,
+  source: "coingecko",
+  search_id: "rings-scusd",
+  type: "token",
+});
