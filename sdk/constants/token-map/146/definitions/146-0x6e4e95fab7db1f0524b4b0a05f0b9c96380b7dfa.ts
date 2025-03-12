b3032f3597b288360f77d4db94a5f9661c986a3a
githubotoro <yupuday.git@gmail.com>
2025-03-12 16:48:56 -0700

    feat(tokens): new source added

--- sdk/constants/token-map/146/definitions/146-0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa.ts
+++ sdk/constants/token-map/146/definitions/146-0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa.ts
@@ -9,7 +9,7 @@ export default defineToken({
   image:
     "https://pbs.twimg.com/profile_images/1621033017337843713/loDsYCJr_400x400.jpg",
   decimals: 18,
-  source: "coingecko",
-  search_id: "rings-scusd",
+  source: "pendle",
+  search_id: "146-0x6e4e95fab7db1f0524b4b0a05f0b9c96380b7dfa",
   type: "token",
 });
