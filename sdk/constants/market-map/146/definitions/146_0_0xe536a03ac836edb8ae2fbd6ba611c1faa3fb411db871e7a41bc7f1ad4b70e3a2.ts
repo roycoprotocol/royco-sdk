7ccd20e55c60b023b031dd9ec3b84575a39abe60
bharatvish <bharat@waymont.co>
2025-03-13 11:26:52 -0700

    Update 146_0_0xe536a03ac836edb8ae2fbd6ba611c1faa3fb411db871e7a41bc7f1ad4b70e3a2.ts

--- sdk/constants/market-map/146/definitions/146_0_0xe536a03ac836edb8ae2fbd6ba611c1faa3fb411db871e7a41bc7f1ad4b70e3a2.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xe536a03ac836edb8ae2fbd6ba611c1faa3fb411db871e7a41bc7f1ad4b70e3a2.ts
@@ -3,8 +3,8 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `146_0_0xe536a03ac836edb8ae2fbd6ba611c1faa3fb411db871e7a41bc7f1ad4b70e3a2`,
   name: `LP into the wstkscUSD pool on Pendle`,
-  description: `The Pendle wstkscUSD LP token is supplied to the market as proof that you&#x27;ve provided liquidity to the Pendle wstkscUSD pool. Depositors may exit at anytime, however withdrawing before the end of the 30-day period will forfeit all incentives earned to date. The maturity period for this market is May 28, 2025 and this market will stop accepting new deposits 30 days prior to this date.`,
-  is_verified: true,
+  description: `Deposits in this market are not earning PENDLE rewards. Please migrate to the new Pendle Royco LP market to earn Pendle rewards. The Pendle wstkscUSD LP token is supplied to the market as proof that you&#x27;ve provided liquidity to the Pendle wstkscUSD pool. Depositors may exit at anytime, however withdrawing before the end of the 30-day period will forfeit all incentives earned to date. The maturity period for this market is May 28, 2025 and this market will stop accepting new deposits 30 days prior to this date.`,
+  is_verified: false,
 
   native_yield: [
     {
