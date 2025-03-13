ad37571e96581edf381302a90ebcd4f2169cc652
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-13 13:40:47 -0700

    feat(market-map): Update market -- 146_0_0x5c9dba1f5bd67c12e97b528ea7b43a5c48bea32fff7c8a223f2c583ade2ba0e7 -- Recipe (0x5c9d...a0e7)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x5c9dba1f5bd67c12e97b528ea7b43a5c48bea32fff7c8a223f2c583ade2ba0e7 (Market Hash)
    - Request Timestamp: Thu, 13 Mar 2025 20:40:46 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x5c9dba1f5bd67c12e97b528ea7b43a5c48bea32fff7c8a223f2c583ade2ba0e7.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x5c9dba1f5bd67c12e97b528ea7b43a5c48bea32fff7c8a223f2c583ade2ba0e7`,
+  name: `test 2 Pendle`,
+  description: `test 2 Pendletest 2 Pendletest 2 Pendle`,
+  is_verified: false,
+});
