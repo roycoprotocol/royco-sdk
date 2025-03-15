9ac19e0075357b297ae23d688930fdecf51bde71
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-14 18:09:31 -0700

    feat(market-map): Update market -- 42161_0_0xf7be9ccbe83741bc736f32233d48f395428d08ca9e1bef9921b09c1dfbd7c1ec -- Recipe (0xf7be...c1ec)
    Market Details:
    - Chain ID: 42161
    - Market Type: 0 (Recipe)
    - Market ID: 0xf7be9ccbe83741bc736f32233d48f395428d08ca9e1bef9921b09c1dfbd7c1ec (Market Hash)
    - Request Timestamp: Sat, 15 Mar 2025 01:09:31 GMT

---
+++ sdk/constants/market-map/42161/definitions/42161_0_0xf7be9ccbe83741bc736f32233d48f395428d08ca9e1bef9921b09c1dfbd7c1ec.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `42161_0_0xf7be9ccbe83741bc736f32233d48f395428d08ca9e1bef9921b09c1dfbd7c1ec`,
+  name: `Deposit WETH for 5 minutes`,
+  description: `Deposit WETH for 5 minutes`,
+  is_verified: false,
+});
