4569589b45da4d423206ff14c34950261a00331b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-14 17:56:44 -0700

    feat(market-map): Update market -- 42161_0_0x425bf45476f7964e9c3df9d4ad1709c758ec5838c0edec2d3e34a34fce8294e0 -- Recipe (0x425b...94e0)
    Market Details:
    - Chain ID: 42161
    - Market Type: 0 (Recipe)
    - Market ID: 0x425bf45476f7964e9c3df9d4ad1709c758ec5838c0edec2d3e34a34fce8294e0 (Market Hash)
    - Request Timestamp: Sat, 15 Mar 2025 00:56:44 GMT

---
+++ sdk/constants/market-map/42161/definitions/42161_0_0x425bf45476f7964e9c3df9d4ad1709c758ec5838c0edec2d3e34a34fce8294e0.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `42161_0_0x425bf45476f7964e9c3df9d4ad1709c758ec5838c0edec2d3e34a34fce8294e0`,
+  name: `Deposit USDC and lock for 20 minutes`,
+  description: `Deposit USDC and lock for 20 minutes`,
+  is_verified: false,
+});
