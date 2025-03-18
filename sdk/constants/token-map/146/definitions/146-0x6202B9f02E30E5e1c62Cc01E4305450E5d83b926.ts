edf19788ad27ea1294caa6f70682a1acc2e45cfa
bharatvish <bharat@waymont.co>
2025-03-18 16:54:20 -0700

    Create 146-0x6202B9f02E30E5e1c62Cc01E4305450E5d83b926.ts

---
+++ sdk/constants/token-map/146/definitions/146-0x6202B9f02E30E5e1c62Cc01E4305450E5d83b926.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x6202B9f02E30E5e1c62Cc01E4305450E5d83b926",
+  chain_id: 146,
+  contract_address: "0x6202B9f02E30E5e1c62Cc01E4305450E5d83b926",
+  name: "xUSD",
+  symbol: "xUSD",
+  image:
+    "https://app.streamprotocol.money/assets/stream-light-085a08b5.svg",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "token",
+});
