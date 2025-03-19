a7f97d2c1ea0a94fd08128ab75f36b92929995fd
bharatvish <bharat@waymont.co>
2025-03-19 14:46:38 -0700

    Update 146_0_0xfcd798abefe4f9784e8f7ce3019c5e567e85687235ce0ce61c27271ba97d26cd.ts

--- sdk/constants/market-map/146/definitions/146_0_0xfcd798abefe4f9784e8f7ce3019c5e567e85687235ce0ce61c27271ba97d26cd.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xfcd798abefe4f9784e8f7ce3019c5e567e85687235ce0ce61c27271ba97d26cd.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: `146_0_0xfcd798abefe4f9784e8f7ce3019c5e567e85687235ce0ce61c27271ba97d26cd`,
   name: `Deposit xUSD for 30 days`,
   description: `Stream USD deposited into Royco for 30 days for added yield!`,
-  is_verified: false,
+  is_verified: true,
 });
