c198e7dbc9804385b76a56bd8f98440ed84a27f8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-18 20:59:24 -0700

    feat(market-map): Update market -- 146_0_0xfe47f86e5193d910ab9f19486978c5ddceab4d457b6cd65acb44e604da05fd87 -- Recipe (0xfe47...fd87) (#701)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xfe47f86e5193d910ab9f19486978c5ddceab4d457b6cd65acb44e604da05fd87 (Market Hash)
    - Request Timestamp: Wed, 19 Mar 2025 03:59:22 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xfe47f86e5193d910ab9f19486978c5ddceab4d457b6cd65acb44e604da05fd87.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xfe47f86e5193d910ab9f19486978c5ddceab4d457b6cd65acb44e604da05fd87`,
+  name: `Deposit xUSD for 30 days TEST`,
+  description: `Deposit xUSD for 30 days TEST`,
+  is_verified: false,
+});
