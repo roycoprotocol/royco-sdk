cceca9f344d98feb32422441d0825702417e4420
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-20 15:26:54 -0700

    feat(market-map): Update market -- 98866_0_0x709bcc976fd6be016b2904b9331ce5f70f64ebb5b66ee65be9569b9885a4a430 -- Recipe (0x709b...a430)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x709bcc976fd6be016b2904b9331ce5f70f64ebb5b66ee65be9569b9885a4a430 (Market Hash)
    - Request Timestamp: Thu, 20 Mar 2025 22:26:54 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x709bcc976fd6be016b2904b9331ce5f70f64ebb5b66ee65be9569b9885a4a430.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x709bcc976fd6be016b2904b9331ce5f70f64ebb5b66ee65be9569b9885a4a430`,
+  name: `Uday Test Plume`,
+  description: `Uday Test Plume`,
+  is_verified: false,
+});
