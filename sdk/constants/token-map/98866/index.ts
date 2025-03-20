3c9e14eb266e3320a90981553fca36611726cba9
GitHub Action <action@github.com>
2025-03-20 19:33:16 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/index.ts
+++ sdk/constants/token-map/98866/index.ts
@@ -1,1 +1,6 @@
-export const TokenMap98866 = {};
+import token_98866_0x54fd4da2fa19cf0f63d8f93a6ea5bed3f9c042c6 from "./definitions/98866-0x54fd4da2fa19cf0f63d8f93a6ea5bed3f9c042c6";
+
+export const TokenMap98866 = {
+  [token_98866_0x54fd4da2fa19cf0f63d8f93a6ea5bed3f9c042c6.id]:
+    token_98866_0x54fd4da2fa19cf0f63d8f93a6ea5bed3f9c042c6,
+};
