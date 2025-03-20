6060aca0cbc7d3a594b8c4ea493725d69a202ce9
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-20 08:13:08 -0700

    feat(market-map): Update market -- 11155111_0_0xaa1bc1459a3e7b4e599be2f117fa065c573e1ad666d74fe994f41bc2f7d9268a -- Recipe (0xaa1b...268a)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xaa1bc1459a3e7b4e599be2f117fa065c573e1ad666d74fe994f41bc2f7d9268a (Market Hash)
    - Request Timestamp: Thu, 20 Mar 2025 15:13:07 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xaa1bc1459a3e7b4e599be2f117fa065c573e1ad666d74fe994f41bc2f7d9268a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xaa1bc1459a3e7b4e599be2f117fa065c573e1ad666d74fe994f41bc2f7d9268a`,
+  name: `Buy LSRWA with pUSD`,
+  description: `When pUSD is deposited, you will get Landshare tokens and incentives`,
+  is_verified: false,
+});
