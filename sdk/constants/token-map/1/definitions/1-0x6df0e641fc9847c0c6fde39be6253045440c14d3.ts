267bcf00717b1982ffb0edb6cabe82370a489013
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-20 12:05:11 -0700

    feat(token-map): Update Token -- 1-0x6df0e641fc9847c0c6fde39be6253045440c14d3 -- Dinero (DINERO) (0x6df0...14d3)
    Token Details:
    - Id: 1-0x6df0e641fc9847c0c6fde39be6253045440c14d3
    - Chain Id: 1
    - Contract Address: 0x6df0e641fc9847c0c6fde39be6253045440c14d3
    - Name: Dinero
    - Symbol: DINERO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/32492.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 32492

---
+++ sdk/constants/token-map/1/definitions/1-0x6df0e641fc9847c0c6fde39be6253045440c14d3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6df0e641fc9847c0c6fde39be6253045440c14d3",
+  chain_id: 1,
+  contract_address: "0x6df0e641fc9847c0c6fde39be6253045440c14d3",
+  name: "Dinero",
+  symbol: "DINERO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32492.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "32492",
+  type: "token",
+});
