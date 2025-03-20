099db7c4f4af06213e5f58df74ca48d85e71ade0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-20 11:39:38 -0700

    feat(market-map): Update market -- 11155111_0_0xe143142ecc85c74e399ca5bd4225f392e1b43f49b87d7ca6748a2f68cdbf683c -- Recipe (0xe143...683c) (#705)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xe143142ecc85c74e399ca5bd4225f392e1b43f49b87d7ca6748a2f68cdbf683c (Market Hash)
    - Request Timestamp: Thu, 20 Mar 2025 18:39:36 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xe143142ecc85c74e399ca5bd4225f392e1b43f49b87d7ca6748a2f68cdbf683c.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xe143142ecc85c74e399ca5bd4225f392e1b43f49b87d7ca6748a2f68cdbf683c`,
+  name: `Buy LSRWA with USDC`,
+  description: `When USDC is supplied, it will go to sale contract of Landshare?`,
+  is_verified: false,
+});
