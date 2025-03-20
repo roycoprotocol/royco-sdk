3241bd6597e45d9da4e9f9927627bb005f7f450f
githubotoro <yupuday.git@gmail.com>
2025-03-20 12:25:49 -0700

    feat(plume): usdc support

---
+++ sdk/constants/token-map/98866/definitions/98866-0x54fd4da2fa19cf0f63d8f93a6ea5bed3f9c042c6.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x54fd4da2fa19cf0f63d8f93a6ea5bed3f9c042c6",
+  chain_id: 98866,
+  contract_address: "0x54fd4da2fa19cf0f63d8f93a6ea5bed3f9c042c6",
+  name: "USDC",
+  symbol: "USDC",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "3408",
+  type: "token",
+});
