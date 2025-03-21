a49f415ed7e13875133d1c857cd875182b9ccbf6
bharatvish <bharat@waymont.co>
2025-03-21 11:38:30 -0700

    Create 11155111-0x35a3a7f13a715162aebbb337681c534f589b82d9.ts

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0x35a3a7f13a715162aebbb337681c534f589b82d9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0x35a3a7f13a715162aebbb337681c534f589b82d9",
+  chain_id: 11155111,
+  contract_address: "0x35a3a7f13a715162aebbb337681c534f589b82d9",
+  name: "Landshare test points",
+  symbol: "LTP",
+  image: "https://pbs.twimg.com/profile_images/1508342632409440257/X33pgoQ9_400x400.jpg",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "point",
+});
