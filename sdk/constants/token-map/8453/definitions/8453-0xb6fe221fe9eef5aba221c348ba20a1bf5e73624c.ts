6646f48c391f6ecc86af46b140c1cdb9af9ba451
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-23 17:07:31 -0700

    feat(token-map): Update Token -- 8453-0xb6fe221fe9eef5aba221c348ba20a1bf5e73624c -- Rocket Pool ETH (RETH) (0xb6fe...624c)
    Token Details:
    - Id: 8453-0xb6fe221fe9eef5aba221c348ba20a1bf5e73624c
    - Chain Id: 8453
    - Contract Address: 0xb6fe221fe9eef5aba221c348ba20a1bf5e73624c
    - Name: Rocket Pool ETH
    - Symbol: RETH
    - Image: https://coin-images.coingecko.com/coins/images/20764/large/reth.png?1696520159
    - Decimals: 18
    - Source: coingecko
    - Search Id: rocket-pool-eth

---
+++ sdk/constants/token-map/8453/definitions/8453-0xb6fe221fe9eef5aba221c348ba20a1bf5e73624c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0xb6fe221fe9eef5aba221c348ba20a1bf5e73624c",
+  chain_id: 8453,
+  contract_address: "0xb6fe221fe9eef5aba221c348ba20a1bf5e73624c",
+  name: "Rocket Pool ETH",
+  symbol: "RETH",
+  image: "https://coin-images.coingecko.com/coins/images/20764/large/reth.png?1696520159",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "rocket-pool-eth",
+  type: "token",
+});
