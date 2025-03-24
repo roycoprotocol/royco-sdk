d8e90dfe7f6da31fc1f630e9145abdbaae8d92ad
bharatvish <bharat@waymont.co>
2025-03-24 13:45:34 -0700

    Update 8453_0_0x94582ea5b2fa66674772f5f1b60ed990926dcc0b14d21e6d0931080833026884.ts

--- sdk/constants/market-map/8453/definitions/8453_0_0x94582ea5b2fa66674772f5f1b60ed990926dcc0b14d21e6d0931080833026884.ts
+++ sdk/constants/market-map/8453/definitions/8453_0_0x94582ea5b2fa66674772f5f1b60ed990926dcc0b14d21e6d0931080833026884.ts
@@ -3,7 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `8453_0_0x94582ea5b2fa66674772f5f1b60ed990926dcc0b14d21e6d0931080833026884`,
   name: `Deposit rETH to Plaza Finance`,
-  description: `Depositing wstETH to Plaza earns Plaza Points (25% APY @ $50m FDV) on top of the 3-4% inherent staking yield. Your supplied assets sit untouched in a Plaza deposit contract during the deposit period, and then convert to 70% bondETH and 30% levETH upon protocol launch (in aggregate equal exposure to ETH). Your Plaza incentives have no lockup at the generation event. More information can be found at: https://docs.plaza.finance/plaza-points-pp/season-1-guide
+  description: `Depositing rETH to Plaza earns Plaza Points (25% APY @ $50m FDV) on top of the 3-4% inherent staking yield. Your supplied assets sit untouched in a Plaza deposit contract during the deposit period, and then convert to 70% bondETH and 30% levETH upon protocol launch (in aggregate equal exposure to ETH). Your Plaza incentives have no lockup at the generation event. More information can be found at: https://docs.plaza.finance/plaza-points-pp/season-1-guide
 `,
   is_verified: false,
 });
