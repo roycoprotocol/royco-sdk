1a99b3aed48d6da073497932926d021af9ae68ac
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-24 05:51:16 -0700

    feat(market-map): Update market -- 146_0_0x22f1c99a829fd11f3a6ffdc874eb55cf881391c3035f22e47b92c18a03e32c71 -- Recipe (0x22f1...2c71)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x22f1c99a829fd11f3a6ffdc874eb55cf881391c3035f22e47b92c18a03e32c71 (Market Hash)
    - Request Timestamp: Mon, 24 Mar 2025 12:51:16 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x22f1c99a829fd11f3a6ffdc874eb55cf881391c3035f22e47b92c18a03e32c71.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x22f1c99a829fd11f3a6ffdc874eb55cf881391c3035f22e47b92c18a03e32c71`,
+  name: `Dahlia ST-S`,
+  description: `Dahlia ST-S Dahlia ST-S Dahlia ST-S Dahlia ST-S`,
+  is_verified: false,
+});
