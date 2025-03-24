08b1707812bbe223b3c05f7ed1a5cecc7d716fa7
bharatvish <bharat@waymont.co>
2025-03-24 13:56:24 -0700

    Update 8453_0_0xa8c77e0b4883c901f01d352ecdc1a5404b7ec83bcfb2a2fa3d1960a682c9d3dd.ts

--- sdk/constants/market-map/8453/definitions/8453_0_0xa8c77e0b4883c901f01d352ecdc1a5404b7ec83bcfb2a2fa3d1960a682c9d3dd.ts
+++ sdk/constants/market-map/8453/definitions/8453_0_0xa8c77e0b4883c901f01d352ecdc1a5404b7ec83bcfb2a2fa3d1960a682c9d3dd.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `8453_0_0xa8c77e0b4883c901f01d352ecdc1a5404b7ec83bcfb2a2fa3d1960a682c9d3dd`,
   name: `Deposit weETH to Plaza Finance`,
-  description: `Earn Plaza Points for depositing weETH to Plaza. After the deposit period, your weETH is converted to 70% of the value in bondETH, and 30% of the value in levETH. You may withdraw at any time but will forfeit the incentives if you withdraw before the end of the deposit period.`,
+  description: `Depositing weETH to Plaza earns Plaza Points (25% APY @ $50m FDV) on top of the 3-4% inherent staking yield. Your supplied assets sit untouched in a Plaza deposit contract during the deposit period, and then convert to 70% bondETH and 30% levETH upon protocol launch (in aggregate equal exposure to ETH). Your Plaza incentives have no lockup at the generation event. More information can be found at: https://docs.plaza.finance/plaza-points-pp/season-1-guide.`,
   is_verified: false,
 });
