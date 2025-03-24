05093421321d3bcdd25f32e4441a598e3b85c978
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-24 07:03:11 -0700

    feat(market-map): Update market -- 146_0_0x4e66b3b7e23e265e18d428a3acfe79eb782081ffb677e0fae4e1d431868d2f09 -- Recipe (0x4e66...2f09) (#735)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x4e66b3b7e23e265e18d428a3acfe79eb782081ffb677e0fae4e1d431868d2f09 (Market Hash)
    - Request Timestamp: Mon, 24 Mar 2025 14:03:08 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x4e66b3b7e23e265e18d428a3acfe79eb782081ffb677e0fae4e1d431868d2f09.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x4e66b3b7e23e265e18d428a3acfe79eb782081ffb677e0fae4e1d431868d2f09`,
+  name: `wstksceth sceth dahlia`,
+  description: `wstksceth sceth dahliawstksceth sceth dahliawstksceth sceth dahliawstksceth sceth dahlia`,
+  is_verified: false,
+});
