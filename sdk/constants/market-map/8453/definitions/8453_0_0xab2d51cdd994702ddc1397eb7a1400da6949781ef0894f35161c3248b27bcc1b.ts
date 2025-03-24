75fbe0d925599dc6a90670ce1cb4cbbca94c7b06
bharatvish <bharat@waymont.co>
2025-03-24 13:55:20 -0700

    Update 8453_0_0xab2d51cdd994702ddc1397eb7a1400da6949781ef0894f35161c3248b27bcc1b.ts

--- sdk/constants/market-map/8453/definitions/8453_0_0xab2d51cdd994702ddc1397eb7a1400da6949781ef0894f35161c3248b27bcc1b.ts
+++ sdk/constants/market-map/8453/definitions/8453_0_0xab2d51cdd994702ddc1397eb7a1400da6949781ef0894f35161c3248b27bcc1b.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `8453_0_0xab2d51cdd994702ddc1397eb7a1400da6949781ef0894f35161c3248b27bcc1b`,
   name: `Deposit wrsETH to Plaza Finance`,
-  description: `Earn Plaza Points for depositing wrsETH to Plaza. After the deposit period, your wrsETH is converted to 70% of the value in bondETH, and 30% of the value in levETH. You may withdraw at any time but will forfeit the incentives if you withdraw before the end of the deposit period.`,
+  description: `Depositing wrsETH to Plaza earns Plaza Points (25% APY @ $50m FDV) on top of the 3-4% inherent staking yield. Your supplied assets sit untouched in a Plaza deposit contract during the deposit period, and then convert to 70% bondETH and 30% levETH upon protocol launch (in aggregate equal exposure to ETH). Your Plaza incentives have no lockup at the generation event. More information can be found at: https://docs.plaza.finance/plaza-points-pp/season-1-guide.`,
   is_verified: false,
 });
