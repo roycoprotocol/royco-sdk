d562a8599ebf93378c9b20f6fd175961a7dbd902
bharatvish <bharat@waymont.co>
2025-03-24 11:38:31 -0700

    Create 8453-0xcd56a157a6e6bd1110a79c5173ce90814e7d34bc.ts

---
+++ sdk/constants/token-map/8453/definitions/8453-0xcd56a157a6e6bd1110a79c5173ce90814e7d34bc.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0xcd56a157a6e6bd1110a79c5173ce90814e7d34bc",
+  chain_id: 8453,
+  contract_address: "0xcd56a157a6e6bd1110a79c5173ce90814e7d34bc",
+  name: "Plaza Points",
+  symbol: "PP",
+  image:
+    "https://pbs.twimg.com/profile_images/1800621860184293376/fw_z8rsf_400x400.png",
+  decimals: 18,
+  source: "external",
+  search_id: "none",
+  type: "point",
+});
