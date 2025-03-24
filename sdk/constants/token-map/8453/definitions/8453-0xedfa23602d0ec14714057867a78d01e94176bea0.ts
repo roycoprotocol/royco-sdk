e5cafd46875e89d2f9f03f541dfdd36de817fb34
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-23 17:07:40 -0700

    feat(token-map): Update Token -- 8453-0xedfa23602d0ec14714057867a78d01e94176bea0 -- Wrapped rsETH (WRSETH) (0xedfa...bea0)
    Token Details:
    - Id: 8453-0xedfa23602d0ec14714057867a78d01e94176bea0
    - Chain Id: 8453
    - Contract Address: 0xedfa23602d0ec14714057867a78d01e94176bea0
    - Name: Wrapped rsETH
    - Symbol: WRSETH
    - Image: https://coin-images.coingecko.com/coins/images/37919/large/rseth.png?1715936438
    - Decimals: 18
    - Source: coingecko
    - Search Id: wrapped-rseth

---
+++ sdk/constants/token-map/8453/definitions/8453-0xedfa23602d0ec14714057867a78d01e94176bea0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0xedfa23602d0ec14714057867a78d01e94176bea0",
+  chain_id: 8453,
+  contract_address: "0xedfa23602d0ec14714057867a78d01e94176bea0",
+  name: "Wrapped rsETH",
+  symbol: "WRSETH",
+  image: "https://coin-images.coingecko.com/coins/images/37919/large/rseth.png?1715936438",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "wrapped-rseth",
+  type: "token",
+});
