918c869fff92896b9e9a3d65b8cabe4a79e5d23d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-23 17:07:20 -0700

    feat(token-map): Update Token -- 8453-0x2416092f143378750bb29b79ed961ab195cceea5 -- Renzo Restaked ETH (EZETH) (0x2416...eea5)
    Token Details:
    - Id: 8453-0x2416092f143378750bb29b79ed961ab195cceea5
    - Chain Id: 8453
    - Contract Address: 0x2416092f143378750bb29b79ed961ab195cceea5
    - Name: Renzo Restaked ETH
    - Symbol: EZETH
    - Image: https://coin-images.coingecko.com/coins/images/34753/large/Ezeth_logo_circle.png?1713496404
    - Decimals: 18
    - Source: coingecko
    - Search Id: renzo-restaked-eth

---
+++ sdk/constants/token-map/8453/definitions/8453-0x2416092f143378750bb29b79ed961ab195cceea5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0x2416092f143378750bb29b79ed961ab195cceea5",
+  chain_id: 8453,
+  contract_address: "0x2416092f143378750bb29b79ed961ab195cceea5",
+  name: "Renzo Restaked ETH",
+  symbol: "EZETH",
+  image: "https://coin-images.coingecko.com/coins/images/34753/large/Ezeth_logo_circle.png?1713496404",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "renzo-restaked-eth",
+  type: "token",
+});
