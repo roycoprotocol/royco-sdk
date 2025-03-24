d42e85581bfaaa8fc003e06a7d8669e3e946202c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-24 10:49:39 -0700

    feat(token-map): Update Token -- 1-0x6243558a24cc6116abe751f27e6d7ede50abfc76 -- Levva Protocol (LVVA) (0x6243...fc76)
    Token Details:
    - Id: 1-0x6243558a24cc6116abe751f27e6d7ede50abfc76
    - Chain Id: 1
    - Contract Address: 0x6243558a24cc6116abe751f27e6d7ede50abfc76
    - Name: Levva Protocol
    - Symbol: LVVA
    - Image: https://coin-images.coingecko.com/coins/images/54803/large/Levva_Token_Image_Nov_19_2024.png?1741853490
    - Decimals: 18
    - Source: coingecko
    - Search Id: levva-protocol

---
+++ sdk/constants/token-map/1/definitions/1-0x6243558a24cc6116abe751f27e6d7ede50abfc76.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x6243558a24cc6116abe751f27e6d7ede50abfc76",
+  chain_id: 1,
+  contract_address: "0x6243558a24cc6116abe751f27e6d7ede50abfc76",
+  name: "Levva Protocol",
+  symbol: "LVVA",
+  image: "https://coin-images.coingecko.com/coins/images/54803/large/Levva_Token_Image_Nov_19_2024.png?1741853490",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "levva-protocol",
+  type: "token",
+});
