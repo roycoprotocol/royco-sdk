daffcd2eca5bda2f6d8e7305ad997239a8157d18
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-24 17:09:57 -0700

    feat(market-map): Update market -- 8453_0_0xfc24cc4a94543caa9e24f48c5d2f09f2156e6c449ed510dac327970cb472c5a7 -- Recipe (0xfc24...c5a7) (#750)
    Market Details:
    - Chain ID: 8453
    - Market Type: 0 (Recipe)
    - Market ID: 0xfc24cc4a94543caa9e24f48c5d2f09f2156e6c449ed510dac327970cb472c5a7 (Market Hash)
    - Request Timestamp: Tue, 25 Mar 2025 00:09:55 GMT

---
+++ sdk/constants/market-map/8453/definitions/8453_0_0xfc24cc4a94543caa9e24f48c5d2f09f2156e6c449ed510dac327970cb472c5a7.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `8453_0_0xfc24cc4a94543caa9e24f48c5d2f09f2156e6c449ed510dac327970cb472c5a7`,
+  name: `Testing cbETH`,
+  description: `asdaddsfsrgvsgvsgvgvsfssasd`,
+  is_verified: false,
+});
