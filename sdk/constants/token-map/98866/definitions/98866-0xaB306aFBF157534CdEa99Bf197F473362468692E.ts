ee9707abfe8e863dbd0851540b8e7e8f151bd658
bharatvish <bharat@waymont.co>
2025-03-26 13:41:45 -0700

    Create 98866-0xaB306aFBF157534CdEa99Bf197F473362468692E.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0xaB306aFBF157534CdEa99Bf197F473362468692E.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xaB306aFBF157534CdEa99Bf197F473362468692E",
+  chain_id: 98866,
+  contract_address: "0xaB306aFBF157534CdEa99Bf197F473362468692E",
+  name: "Nucleus op-RWA",
+  symbol: "op-RWA",
+  image: "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+  decimals: 6,
+  source: "",
+  search_id: "",
+  type: "token",
+});
