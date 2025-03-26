c1afa8190bd80b7f05f15894bfbc9bcec6b6e896
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-25 21:52:55 -0700

    feat(token-map): Update Token -- 1-0xdd50c053c096cb04a3e3362e2b622529ec5f2e8a -- OpenEden TBILL Vault (TBILL) (0xdd50...2e8a)
    Token Details:
    - Id: 1-0xdd50c053c096cb04a3e3362e2b622529ec5f2e8a
    - Chain Id: 1
    - Contract Address: 0xdd50c053c096cb04a3e3362e2b622529ec5f2e8a
    - Name: OpenEden TBILL Vault
    - Symbol: TBILL
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/25158.png
    - Decimals: 6
    - Source: coinmarketcap
    - Search Id: 25158

---
+++ sdk/constants/token-map/1/definitions/1-0xdd50c053c096cb04a3e3362e2b622529ec5f2e8a.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdd50c053c096cb04a3e3362e2b622529ec5f2e8a",
+  chain_id: 1,
+  contract_address: "0xdd50c053c096cb04a3e3362e2b622529ec5f2e8a",
+  name: "OpenEden TBILL Vault",
+  symbol: "TBILL",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/25158.png",
+  decimals: 6,
+  source: "coinmarketcap",
+  search_id: "25158",
+  type: "token",
+});
