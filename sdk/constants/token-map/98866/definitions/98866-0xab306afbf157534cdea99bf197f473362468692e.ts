bb64a4dcfbbe81d38aaf9a3ea580e7853f9842c3
GitHub Action <action@github.com>
2025-03-26 20:50:05 +0000

    feat(chore): update generated files

---
+++ sdk/constants/token-map/98866/definitions/98866-0xab306afbf157534cdea99bf197f473362468692e.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xab306afbf157534cdea99bf197f473362468692e",
+  chain_id: 98866,
+  contract_address: "0xab306afbf157534cdea99bf197f473362468692e",
+  name: "Nucleus op-RWA",
+  symbol: "op-RWA",
+  image:
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+  decimals: 6,
+  source: "",
+  search_id: "",
+  type: "token",
+});
