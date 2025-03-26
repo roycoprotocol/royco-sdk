20cfd67da363358d2d4849ed3bd273c01801f107
GitHub Action <action@github.com>
2025-03-26 16:46:49 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/11155111/definitions/11155111-0x71564b27fccd03491c267f86bae2a31561975a55.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0x71564b27fccd03491c267f86bae2a31561975a55.ts
@@ -1,13 +1,12 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0x71564b27fcCd03491C267f86bAE2a31561975a55",
+  id: "11155111-0x71564b27fccd03491c267f86bae2a31561975a55",
   chain_id: 11155111,
-  contract_address: "0x71564b27fcCd03491C267f86bAE2a31561975a55",
+  contract_address: "0x71564b27fccd03491c267f86bae2a31561975a55",
   name: "TST OpenEden",
   symbol: "TST",
-  image:
-    "https://openeden.com/media-kit/logomark-dark-render.svg",
+  image: "https://openeden.com/media-kit/logomark-dark-render.svg",
   decimals: 18,
   source: "",
   search_id: "",
