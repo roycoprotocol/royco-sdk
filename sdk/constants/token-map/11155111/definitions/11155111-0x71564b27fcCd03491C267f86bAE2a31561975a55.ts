21b64d6e6df1db5e1b147df6326caadb1a1aed33
bharatvish <bharat@waymont.co>
2025-03-26 09:38:35 -0700

    Create 11155111-0x71564b27fcCd03491C267f86bAE2a31561975a55.ts

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0x71564b27fcCd03491C267f86bAE2a31561975a55.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0x71564b27fcCd03491C267f86bAE2a31561975a55",
+  chain_id: 11155111,
+  contract_address: "0x71564b27fcCd03491C267f86bAE2a31561975a55",
+  name: "TST OpenEden",
+  symbol: "TST",
+  image:
+    "https://openeden.com/media-kit/logomark-dark-render.svg",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "token",
+});
