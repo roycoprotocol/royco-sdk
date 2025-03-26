a3190851ea08575ac29d9d06dfa74b5f8cd3da5d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-26 15:00:45 -0700

    feat(market-map): Update market -- 98866_0_0x4d1dfab8800e535f8a1343c6c1af902d37b78f948287ff02c5dfa30baa4687b2 -- Recipe (0x4d1d...87b2)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x4d1dfab8800e535f8a1343c6c1af902d37b78f948287ff02c5dfa30baa4687b2 (Market Hash)
    - Request Timestamp: Wed, 26 Mar 2025 22:00:44 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x4d1dfab8800e535f8a1343c6c1af902d37b78f948287ff02c5dfa30baa4687b2.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x4d1dfab8800e535f8a1343c6c1af902d37b78f948287ff02c5dfa30baa4687b2`,
+  name: `Deposit op-RWA into Royco for 12 Weeks`,
+  description: `Deposit op-RWA into Royco for 12 Weeks to earn incentives.`,
+  is_verified: false,
+});
