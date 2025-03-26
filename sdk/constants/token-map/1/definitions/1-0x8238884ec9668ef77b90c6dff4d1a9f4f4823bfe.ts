b3c6e63ce01d93377fc06a6a6d8d1bb0cb69e851
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-25 21:53:21 -0700

    feat(token-map): Update Token -- 1-0x8238884ec9668ef77b90c6dff4d1a9f4f4823bfe -- OpenEden Open Dollar (USDO) (0x8238...3bfe)
    Token Details:
    - Id: 1-0x8238884ec9668ef77b90c6dff4d1a9f4f4823bfe
    - Chain Id: 1
    - Contract Address: 0x8238884ec9668ef77b90c6dff4d1a9f4f4823bfe
    - Name: OpenEden Open Dollar
    - Symbol: USDO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/36067.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 36067

---
+++ sdk/constants/token-map/1/definitions/1-0x8238884ec9668ef77b90c6dff4d1a9f4f4823bfe.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x8238884ec9668ef77b90c6dff4d1a9f4f4823bfe",
+  chain_id: 1,
+  contract_address: "0x8238884ec9668ef77b90c6dff4d1a9f4f4823bfe",
+  name: "OpenEden Open Dollar",
+  symbol: "USDO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/36067.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "36067",
+  type: "token",
+});
