51241d6df48391d3a926d501c1f14780c6d47307
bharatvish <bharat@waymont.co>
2025-03-27 14:31:41 -0700

    Create 146-0xB88fF15ae5f82c791e637b27337909BcF8065270.ts

---
+++ sdk/constants/token-map/146/definitions/146-0xB88fF15ae5f82c791e637b27337909BcF8065270.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0xB88fF15ae5f82c791e637b27337909BcF8065270",
+  chain_id: 146,
+  contract_address: "0xB88fF15ae5f82c791e637b27337909BcF8065270",
+  name: "xBTC",
+  symbol: "xBTC",
+  image: "https://app.streamprotocol.money/assets/usdc-a6e16350.svg",
+  decimals: 8,
+  source: "coingecko",
+  search_id: "bitcoin",
+  type: "token",
+});
