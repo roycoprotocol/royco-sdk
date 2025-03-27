209e196218956fdda412091e8e40087f9de0cbc4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-27 13:56:30 -0700

    feat(market-map): Update market -- 11155111_0_0xe3dbf381e909dea1aebf7b527d9484e1f5ba543d47fe5e2c9d00c3cb8d3109de -- Recipe (0xe3db...09de) (#766)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xe3dbf381e909dea1aebf7b527d9484e1f5ba543d47fe5e2c9d00c3cb8d3109de (Market Hash)
    - Request Timestamp: Thu, 27 Mar 2025 20:56:28 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xe3dbf381e909dea1aebf7b527d9484e1f5ba543d47fe5e2c9d00c3cb8d3109de.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xe3dbf381e909dea1aebf7b527d9484e1f5ba543d47fe5e2c9d00c3cb8d3109de`,
+  name: `Buy LSRWA with pUSD`,
+  description: `Buy LSRWA with pUSD`,
+  is_verified: false,
+});
