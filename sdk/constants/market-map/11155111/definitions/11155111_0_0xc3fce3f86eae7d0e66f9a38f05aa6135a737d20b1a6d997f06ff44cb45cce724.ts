09ccebc8c73bc9636d2b15ea1a8a8809363538c2
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-27 20:55:20 -0700

    feat(market-map): Update market -- 11155111_0_0xc3fce3f86eae7d0e66f9a38f05aa6135a737d20b1a6d997f06ff44cb45cce724 -- Recipe (0xc3fc...e724)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xc3fce3f86eae7d0e66f9a38f05aa6135a737d20b1a6d997f06ff44cb45cce724 (Market Hash)
    - Request Timestamp: Fri, 28 Mar 2025 03:55:19 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xc3fce3f86eae7d0e66f9a38f05aa6135a737d20b1a6d997f06ff44cb45cce724.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xc3fce3f86eae7d0e66f9a38f05aa6135a737d20b1a6d997f06ff44cb45cce724`,
+  name: `Deposit TST for 10 mins`,
+  description: `Deposit TST for 10 mins`,
+  is_verified: false,
+});
