da344a4b245861a47f88c8663126487ef31e42f5
Naimish Lukhi <lukhinaimish@gmail.com>
2025-03-28 14:46:38 -0400

    fix: add vault contract map

---
+++ sdk/vault/vault-contract-provider-map.tsx
@@ -0,0 +1,47 @@
+export const vaultContractProviderMap = [
+  {
+    id: "1_0x74d1fafa4e0163b2f1035f1b052137f3f9bad5cc",
+    contracts: {
+      vault: "0x74d1fafa4e0163b2f1035f1b052137f3f9bad5cc",
+      teller: "0x60ebb5d1454bb99aa35f63f609e79179b342b0b8",
+      accountant: "0x80f0b206b7e5daa1b1ba4ea1478a33241ee6bac9",
+      lens: "0x90983ebf38e981ae38f7da9e71804380e316a396",
+      boringQueue: "0x9ec26125fecde8ce5c67346db2e20c9026225440",
+    },
+    decimals: 18,
+    baseToken: {
+      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      decimals: 6,
+    },
+  },
+  {
+    id: "146_0x45088fb2ffebfdcf4dff7b7201bfa4cd2077c30e",
+    contracts: {
+      vault: "0x45088fb2ffebfdcf4dff7b7201bfa4cd2077c30e",
+      teller: "0x0f75c8176d4ebdff78d9a0c486b35d8f94b00a42",
+      accountant: "0x8301294e84ca5a2644e7f3cd47a86369f1b0416e",
+      lens: "0x90983ebf38e981ae38f7da9e71804380e316a396",
+      boringQueue: "0xd0885a285f9a00aa2d9734d2d26be1186f850e38",
+    },
+    decimals: 18,
+    baseToken: {
+      address: "0x29219dd400f2bf60e5a23d13be72b486d4038894",
+      decimals: 6,
+    },
+  },
+  {
+    id: "42161_0x3d1ad04e82e595b7295751c35203f542dc986a43",
+    contracts: {
+      vault: "0x3d1ad04e82e595b7295751c35203f542dc986a43",
+      teller: "0xf8c21f673c3dfb11daefffdeb9e1e1258351db2b",
+      accountant: "0x915565acadb68eda6de37a2423ef7cca93f6f789",
+      lens: "0x90983ebf38e981ae38f7da9e71804380e316a396",
+      boringQueue: "0xf7b0f4973b0b6a5457c4af0b29c11158af19c03f",
+    },
+    decimals: 18,
+    baseToken: {
+      address: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+      decimals: 6,
+    },
+  },
+];
