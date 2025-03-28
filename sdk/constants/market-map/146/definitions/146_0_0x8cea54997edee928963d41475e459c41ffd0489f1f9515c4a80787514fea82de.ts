3fac13230530e8169708b20b902fe9d3abd377a7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-28 10:32:18 -0700

    feat(market-map): Update market -- 146_0_0x8cea54997edee928963d41475e459c41ffd0489f1f9515c4a80787514fea82de -- Recipe (0x8cea...82de)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x8cea54997edee928963d41475e459c41ffd0489f1f9515c4a80787514fea82de (Market Hash)
    - Request Timestamp: Fri, 28 Mar 2025 17:32:17 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x8cea54997edee928963d41475e459c41ffd0489f1f9515c4a80787514fea82de.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x8cea54997edee928963d41475e459c41ffd0489f1f9515c4a80787514fea82de`,
+  name: `Deposit xBTC for 30 Days`,
+  description: `Stream BTC deposited into Royco for 30 days for added yield!`,
+  is_verified: false,
+});
