d268995659139fe07a3cb48adabdc3cf9e715313
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-28 11:43:09 -0700

    feat(market-map): Update market -- 146_0_0xb7213bb1cdb02537098e9d092e5cee938d1f19ae47494915e7b255b9114dabe5 -- Recipe (0xb721...abe5) (#773)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xb7213bb1cdb02537098e9d092e5cee938d1f19ae47494915e7b255b9114dabe5 (Market Hash)
    - Request Timestamp: Fri, 28 Mar 2025 18:43:07 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xb7213bb1cdb02537098e9d092e5cee938d1f19ae47494915e7b255b9114dabe5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xb7213bb1cdb02537098e9d092e5cee938d1f19ae47494915e7b255b9114dabe5`,
+  name: `Lend USDC on Dahlia S&#x2F;USDC (75% LLTV)`,
+  description: `Lend USDC in the Dahlia S&#x2F;USDC isolated lending market with a 75% Liquidation Loan-to-Value (LLTV). Earn native lending APY, plus a share of Dahlia Points (3x boosted) and Sonic Gems rewards!`,
+  is_verified: false,
+});
