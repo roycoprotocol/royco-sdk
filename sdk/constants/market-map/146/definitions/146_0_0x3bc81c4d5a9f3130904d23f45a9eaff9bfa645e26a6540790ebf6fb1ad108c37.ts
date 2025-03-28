7716df702ebf1a65282be439451a14b6c528d76c
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-28 11:53:44 -0700

    feat(market-map): Update market -- 146_0_0x3bc81c4d5a9f3130904d23f45a9eaff9bfa645e26a6540790ebf6fb1ad108c37 -- Recipe (0x3bc8...8c37) (#776)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x3bc81c4d5a9f3130904d23f45a9eaff9bfa645e26a6540790ebf6fb1ad108c37 (Market Hash)
    - Request Timestamp: Fri, 28 Mar 2025 18:53:42 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x3bc81c4d5a9f3130904d23f45a9eaff9bfa645e26a6540790ebf6fb1ad108c37.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x3bc81c4d5a9f3130904d23f45a9eaff9bfa645e26a6540790ebf6fb1ad108c37`,
+  name: `Deposit WETH into Rings to mint and lend scETH on Dahlia wstkscETH&#x2F;scETH (94% LLTV)`,
+  description: `Deposit WETH into Rings to mint Sonic ETH and lend in the Dahlia wstkscETH&#x2F;scETH isolated lending market with a 94% Liquidation Loan-to-Value (LLTV). Earn native lending APY, plus a share of Dahlia Points (3x boosted) and Sonic Gems rewards!`,
+  is_verified: false,
+});
