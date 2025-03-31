af9ce1f1aa756c0ef8f41a604521a2fe06b7b70f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-31 08:32:37 -0700

    feat(market-map): Update market -- 146_0_0x31bdffd0bbab9391f6c2903be86b89a7d5107e26e091ccb6c0c9239107ab02e7 -- Recipe (0x31bd...02e7)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0x31bdffd0bbab9391f6c2903be86b89a7d5107e26e091ccb6c0c9239107ab02e7 (Market Hash)
    - Request Timestamp: Mon, 31 Mar 2025 15:32:37 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0x31bdffd0bbab9391f6c2903be86b89a7d5107e26e091ccb6c0c9239107ab02e7.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0x31bdffd0bbab9391f6c2903be86b89a7d5107e26e091ccb6c0c9239107ab02e7`,
+  name: `Sonic Vault Test Market`,
+  description: `Take in USDC deposit and lock for 5 minutes`,
+  is_verified: false,
+});
