86b21bb783ff0ecf3ebb41dac2e1a6f53acda248
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-31 12:27:35 -0700

    feat(market-map): Update market -- 146_0_0xc11c90babd60a148eba7540569f05a707fcb16eb6ae10bc08d8607f2525c73a5 -- Recipe (0xc11c...73a5)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xc11c90babd60a148eba7540569f05a707fcb16eb6ae10bc08d8607f2525c73a5 (Market Hash)
    - Request Timestamp: Mon, 31 Mar 2025 19:27:34 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xc11c90babd60a148eba7540569f05a707fcb16eb6ae10bc08d8607f2525c73a5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xc11c90babd60a148eba7540569f05a707fcb16eb6ae10bc08d8607f2525c73a5`,
+  name: `Mint msUSD on Mainstreet`,
+  description: `Mint msUSD by depositing USDC.e into the MainstreetMinter. Holding msUSD will earn you rewards via rebase.`,
+  is_verified: false,
+});
