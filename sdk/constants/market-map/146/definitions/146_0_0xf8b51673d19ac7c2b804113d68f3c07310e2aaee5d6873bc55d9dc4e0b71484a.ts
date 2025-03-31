63afbbc8c64b968939102ca4aee4805e7429b1db
bharatvish <bharat@waymont.co>
2025-03-31 15:58:46 -0700

    Update 146_0_0xf8b51673d19ac7c2b804113d68f3c07310e2aaee5d6873bc55d9dc4e0b71484a.ts

--- sdk/constants/market-map/146/definitions/146_0_0xf8b51673d19ac7c2b804113d68f3c07310e2aaee5d6873bc55d9dc4e0b71484a.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xf8b51673d19ac7c2b804113d68f3c07310e2aaee5d6873bc55d9dc4e0b71484a.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: `146_0_0xf8b51673d19ac7c2b804113d68f3c07310e2aaee5d6873bc55d9dc4e0b71484a`,
   name: `Lend USDC on Dahlia wstkscUSD&#x2F;USDC (94% LLTV)`,
   description: `Lend USDC in the Dahlia wstkscUSD&#x2F;USDC isolated lending market with a 94% Liquidation Loan-to-Value (LLTV). Earn native lending APY, plus a share of Dahlia Points (3x boosted) and Sonic Gems rewards!`,
-  is_verified: false,
+  is_verified: true,
 });
