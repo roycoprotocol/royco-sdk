c8e59b9d79196cb39868dca8ac541ec773c67bc5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-03-31 10:38:35 -0700

    feat(market-map): Update market -- 11155111_0_0xb5cea6a53cba3d45d899956667483e5e593a195744880c4dcec6e048c019974e -- Recipe (0xb5ce...974e)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xb5cea6a53cba3d45d899956667483e5e593a195744880c4dcec6e048c019974e (Market Hash)
    - Request Timestamp: Mon, 31 Mar 2025 17:38:35 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xb5cea6a53cba3d45d899956667483e5e593a195744880c4dcec6e048c019974e.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xb5cea6a53cba3d45d899956667483e5e593a195744880c4dcec6e048c019974e`,
+  name: `Deposit USDC to get reward in USDT`,
+  description: `Deposit USDC to get reward in USDT`,
+  is_verified: false,
+});
