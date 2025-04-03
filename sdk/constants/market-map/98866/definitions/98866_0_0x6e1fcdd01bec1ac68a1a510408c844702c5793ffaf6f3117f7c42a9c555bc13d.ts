b0091a0ddc582c73f7ad3e678d0fec4f98bfb0cc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 10:20:50 -0700

    feat(market-map): Update market -- 98866_0_0x6e1fcdd01bec1ac68a1a510408c844702c5793ffaf6f3117f7c42a9c555bc13d -- Recipe (0x6e1f...c13d) (#800)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x6e1fcdd01bec1ac68a1a510408c844702c5793ffaf6f3117f7c42a9c555bc13d (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 17:20:48 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x6e1fcdd01bec1ac68a1a510408c844702c5793ffaf6f3117f7c42a9c555bc13d.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x6e1fcdd01bec1ac68a1a510408c844702c5793ffaf6f3117f7c42a9c555bc13d`,
+  name: `Deposit into nELIXIR on Nest`,
+  description: `When pUSD is supplied to the market, it is deposited into nELIXIR on Nest.`,
+  is_verified: false,
+});
