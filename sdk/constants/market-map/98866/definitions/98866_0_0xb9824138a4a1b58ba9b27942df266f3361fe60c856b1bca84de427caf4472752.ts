bae80ce932d13e06b6c1eb66e5cc0378811cd49f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 15:11:09 -0700

    feat(market-map): Update market -- 98866_0_0xb9824138a4a1b58ba9b27942df266f3361fe60c856b1bca84de427caf4472752 -- Recipe (0xb982...2752)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xb9824138a4a1b58ba9b27942df266f3361fe60c856b1bca84de427caf4472752 (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 22:11:09 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xb9824138a4a1b58ba9b27942df266f3361fe60c856b1bca84de427caf4472752.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xb9824138a4a1b58ba9b27942df266f3361fe60c856b1bca84de427caf4472752`,
+  name: `Supply nELIXIR on Mystic Finance`,
+  description: `When you supply nELIXIR, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 60 days after deposit.`,
+  is_verified: false,
+});
