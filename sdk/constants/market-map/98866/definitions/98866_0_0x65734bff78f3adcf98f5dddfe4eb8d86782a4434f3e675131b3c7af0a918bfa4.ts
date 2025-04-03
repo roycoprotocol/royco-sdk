dea32b2e5e0d915ee912eb3bc20f15f02410a825
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 15:22:01 -0700

    feat(market-map): Update market -- 98866_0_0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4 -- Recipe (0x6573...bfa4)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4 (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 22:22:00 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4`,
+  name: `Supply pUSD on Mystic Finance`,
+  description: `When you supply pUSD, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  is_verified: false,
+});
