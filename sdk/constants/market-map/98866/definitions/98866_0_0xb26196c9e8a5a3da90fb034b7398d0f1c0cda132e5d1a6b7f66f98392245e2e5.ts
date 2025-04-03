c626d8a3b7ae3b9580eddd9d33d1fcc17d50b040
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 15:17:29 -0700

    feat(market-map): Update market -- 98866_0_0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5 -- Recipe (0xb261...e2e5)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5 (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 22:17:29 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5`,
+  name: `Supply PLUME on Mystic Finance`,
+  description: `When you supply PLUME, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  is_verified: false,
+});
