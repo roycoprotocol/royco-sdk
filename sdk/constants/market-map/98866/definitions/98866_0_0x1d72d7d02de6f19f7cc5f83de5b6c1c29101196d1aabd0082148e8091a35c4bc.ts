0f72614f714e00eb3ea804627a1f0c15991b9063
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 14:50:24 -0700

    feat(market-map): Update market -- 98866_0_0x1d72d7d02de6f19f7cc5f83de5b6c1c29101196d1aabd0082148e8091a35c4bc -- Recipe (0x1d72...c4bc)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x1d72d7d02de6f19f7cc5f83de5b6c1c29101196d1aabd0082148e8091a35c4bc (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 21:50:24 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x1d72d7d02de6f19f7cc5f83de5b6c1c29101196d1aabd0082148e8091a35c4bc.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x1d72d7d02de6f19f7cc5f83de5b6c1c29101196d1aabd0082148e8091a35c4bc`,
+  name: `Supply nRWA on Mystic Finance`,
+  description: `When you supply nRWA, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  is_verified: false,
+});
