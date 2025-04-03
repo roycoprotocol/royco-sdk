9bbd9f2085631d6a9bbbaeb664e780739481bdfb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 05:46:34 -0700

    feat(market-map): Update market -- 98866_0_0xef6573033aed82d3de3280e5e0fb2e8e49e9ace6d4a6d1f054be8797dea1a25b -- Recipe (0xef65...a25b)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xef6573033aed82d3de3280e5e0fb2e8e49e9ace6d4a6d1f054be8797dea1a25b (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 12:46:33 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xef6573033aed82d3de3280e5e0fb2e8e49e9ace6d4a6d1f054be8797dea1a25b.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xef6573033aed82d3de3280e5e0fb2e8e49e9ace6d4a6d1f054be8797dea1a25b`,
+  name: `Borrow pUSD on Mystic Finance`,
+  description: `When you borrow pUSD, it’s automatically deposited into your wallet, earning incentives and accruing interest. You can repay your loans whenever you need.`,
+  is_verified: false,
+});
