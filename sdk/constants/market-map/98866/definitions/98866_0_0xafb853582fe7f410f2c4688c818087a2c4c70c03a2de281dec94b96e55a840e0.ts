0d0daa4fd3579ef007aba46f1c14a0fbca37fc42
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 15:03:12 -0700

    feat(market-map): Update market -- 98866_0_0xafb853582fe7f410f2c4688c818087a2c4c70c03a2de281dec94b96e55a840e0 -- Recipe (0xafb8...40e0) (#804)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xafb853582fe7f410f2c4688c818087a2c4c70c03a2de281dec94b96e55a840e0 (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 22:03:10 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xafb853582fe7f410f2c4688c818087a2c4c70c03a2de281dec94b96e55a840e0.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xafb853582fe7f410f2c4688c818087a2c4c70c03a2de281dec94b96e55a840e0`,
+  name: `Supply nTBILL on Mystic Finance`,
+  description: `When you supply nTBILL, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 60 days after deposit.`,
+  is_verified: false,
+});
