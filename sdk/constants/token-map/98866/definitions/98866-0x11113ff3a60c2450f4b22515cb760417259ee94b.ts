9e1897002b15751e11afde31093e242d13e6f5ca
GitHub Action <action@github.com>
2025-04-03 21:15:14 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/definitions/98866-0x11113ff3a60c2450f4b22515cb760417259ee94b.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x11113ff3a60c2450f4b22515cb760417259ee94b.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98866-0x11113Ff3a60C2450F4b22515cB760417259eE94B",
+  id: "98866-0x11113ff3a60c2450f4b22515cb760417259ee94b",
   chain_id: 98866,
-  contract_address: "0x11113Ff3a60C2450F4b22515cB760417259eE94B",
+  contract_address: "0x11113ff3a60c2450f4b22515cb760417259ee94b",
   name: "Nest BASIS",
   symbol: "nBASIS",
   image: "https://dev.nest.credit/images/nest-basis-vault.svg",
