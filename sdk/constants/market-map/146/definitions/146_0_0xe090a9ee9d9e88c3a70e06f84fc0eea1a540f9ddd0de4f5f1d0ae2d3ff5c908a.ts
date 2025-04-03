fdc327819dbc762396f0e42d3b830638ef366fb7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-03 13:48:05 -0700

    feat(market-map): Update market -- 146_0_0xe090a9ee9d9e88c3a70e06f84fc0eea1a540f9ddd0de4f5f1d0ae2d3ff5c908a -- Recipe (0xe090...908a)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xe090a9ee9d9e88c3a70e06f84fc0eea1a540f9ddd0de4f5f1d0ae2d3ff5c908a (Market Hash)
    - Request Timestamp: Thu, 03 Apr 2025 20:48:05 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xe090a9ee9d9e88c3a70e06f84fc0eea1a540f9ddd0de4f5f1d0ae2d3ff5c908a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xe090a9ee9d9e88c3a70e06f84fc0eea1a540f9ddd0de4f5f1d0ae2d3ff5c908a`,
+  name: `Mint and hold msUSD stablecoin`,
+  description: `When USDC.e is deposited, it is used to mint 1:1 msUSD. By holding msUSD you are earning rewards passively.`,
+  is_verified: false,
+});
