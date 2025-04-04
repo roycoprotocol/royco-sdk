6ca0ce894f5325f29e68237c011296f53b805359
GitHub Action <action@github.com>
2025-04-04 19:14:03 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/11155111/definitions/11155111-0x02ae3b18597d1e867cc31cd04c3e0932d2e50db7.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0x02ae3b18597d1e867cc31cd04c3e0932d2e50db7.ts
@@ -1,13 +1,12 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "11155111-0x02ae3B18597D1e867Cc31cD04C3E0932D2e50Db7",
+  id: "11155111-0x02ae3b18597d1e867cc31cd04c3e0932d2e50db7",
   chain_id: 11155111,
-  contract_address: "0x02ae3B18597D1e867Cc31cD04C3E0932D2e50Db7",
+  contract_address: "0x02ae3b18597d1e867cc31cd04c3e0932d2e50db7",
   name: "OpenEdenTestPoints",
   symbol: "OETP",
-  image:
-    "https://openeden.com/media-kit/logomark-dark.png",
+  image: "https://openeden.com/media-kit/logomark-dark.png",
   decimals: 18,
   source: "",
   search_id: "",
