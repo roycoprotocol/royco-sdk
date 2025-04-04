651928d7aa75e16f2621d9dff7a7c75e9c2e876f
bharatvish <bharat@waymont.co>
2025-04-04 12:06:23 -0700

    Create 11155111-0x02ae3B18597D1e867Cc31cD04C3E0932D2e50Db7.ts

---
+++ sdk/constants/token-map/11155111/definitions/11155111-0x02ae3B18597D1e867Cc31cD04C3E0932D2e50Db7.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "11155111-0x02ae3B18597D1e867Cc31cD04C3E0932D2e50Db7",
+  chain_id: 11155111,
+  contract_address: "0x02ae3B18597D1e867Cc31cD04C3E0932D2e50Db7",
+  name: "OpenEdenTestPoints",
+  symbol: "OETP",
+  image:
+    "https://openeden.com/media-kit/logomark-dark.png",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "point",
+});
