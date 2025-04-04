d371646655dd54bf4950bb50f291777b708410ae
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-04 14:48:12 -0700

    feat(market-map): Update market -- 98866_0_0xdc5e7169392a4c0c6955a4db02e90c4e197167cc6b17c5df2da9accbcaa9fa59 -- Recipe (0xdc5e...fa59) (#814)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xdc5e7169392a4c0c6955a4db02e90c4e197167cc6b17c5df2da9accbcaa9fa59 (Market Hash)
    - Request Timestamp: Fri, 04 Apr 2025 21:48:10 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xdc5e7169392a4c0c6955a4db02e90c4e197167cc6b17c5df2da9accbcaa9fa59.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xdc5e7169392a4c0c6955a4db02e90c4e197167cc6b17c5df2da9accbcaa9fa59`,
+  name: `Deposit into nBASIS on Nest`,
+  description: `When pUSD is supplied to the market, it is deposited into nBASIS on Nest.`,
+  is_verified: false,
+});
