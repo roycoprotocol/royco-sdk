a49388c10d17228ceeb16978999fc11cfccd2833
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-07 10:36:38 -0700

    feat(market-map): Update market -- 98866_0_0xe8cfa9dd3a0f06c880e10b85d7405027331e0c98dc75574769b06dcb60a8b2ad -- Recipe (0xe8cf...b2ad)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xe8cfa9dd3a0f06c880e10b85d7405027331e0c98dc75574769b06dcb60a8b2ad (Market Hash)
    - Request Timestamp: Mon, 07 Apr 2025 17:36:38 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xe8cfa9dd3a0f06c880e10b85d7405027331e0c98dc75574769b06dcb60a8b2ad.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xe8cfa9dd3a0f06c880e10b85d7405027331e0c98dc75574769b06dcb60a8b2ad`,
+  name: `Supply WPLUME on Solera`,
+  description: `Supply WPLUME on Solera`,
+  is_verified: false,
+});
