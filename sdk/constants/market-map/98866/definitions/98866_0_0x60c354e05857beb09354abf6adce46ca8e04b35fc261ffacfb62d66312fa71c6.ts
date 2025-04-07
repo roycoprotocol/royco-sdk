d383645a82c52a0db67549ec50cae7881b147010
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-07 12:02:08 -0700

    feat(market-map): Update market -- 98866_0_0x60c354e05857beb09354abf6adce46ca8e04b35fc261ffacfb62d66312fa71c6 -- Recipe (0x60c3...71c6) (#817)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x60c354e05857beb09354abf6adce46ca8e04b35fc261ffacfb62d66312fa71c6 (Market Hash)
    - Request Timestamp: Mon, 07 Apr 2025 19:02:06 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x60c354e05857beb09354abf6adce46ca8e04b35fc261ffacfb62d66312fa71c6.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x60c354e05857beb09354abf6adce46ca8e04b35fc261ffacfb62d66312fa71c6`,
+  name: `test supply pusd to solera`,
+  description: `test supply pusd to solera`,
+  is_verified: false,
+});
