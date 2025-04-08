703f333723121b53b600f7c81caf58d8a62f9625
GitHub Action <action@github.com>
2025-04-08 03:30:48 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/definitions/98866-0x39d1f90ef89c52dda276194e9a832b484ee45574.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x39d1f90ef89c52dda276194e9a832b484ee45574.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98866-0x39d1F90eF89C52dDA276194E9a832b484ee45574",
+  id: "98866-0x39d1f90ef89c52dda276194e9a832b484ee45574",
   chain_id: 98866,
-  contract_address: "0x39d1F90eF89C52dDA276194E9a832b484ee45574",
+  contract_address: "0x39d1f90ef89c52dda276194e9a832b484ee45574",
   name: "Plume ETH",
   symbol: "pETH",
   image: "https://peth.plumenetwork.xyz/pETH.svg",
