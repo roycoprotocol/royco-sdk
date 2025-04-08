6c88e15e52f249ab0c318943d507a4123b692690
githubotoro <yupuday.git@gmail.com>
2025-04-08 11:03:10 -0700

    feat(boyco): map update

--- sdk/boyco/contract-map/index.tsx
+++ sdk/boyco/contract-map/index.tsx
@@ -5,13 +5,13 @@ import { ContractAddressesBoyco } from "../contract-addresses";
 export const ContractMapBoyco = {
   [BerachainTestnet.id]: {
     DepositExecutor: {
-      address: ContractAddressesBoyco[BerachainTestnet.id].DepositExecutor,
+      address: ContractAddressesBoyco[BerachainTestnet.id]!.DepositExecutor,
       abi: ContractAbisBoyco.DepositExecutor,
     },
   },
   [BerachainMainnet.id]: {
     DepositExecutor: {
-      address: ContractAddressesBoyco[BerachainMainnet.id].DepositExecutor,
+      address: ContractAddressesBoyco[BerachainMainnet.id]!.DepositExecutor,
       abi: ContractAbisBoyco.DepositExecutor,
     },
   },
