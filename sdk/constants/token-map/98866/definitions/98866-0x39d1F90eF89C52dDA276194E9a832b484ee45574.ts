85a10a1a79d7d150a2b5300ba3ae16a8176b2932
bharatvish <bharat@waymont.co>
2025-04-07 20:19:48 -0700

    Create 98866-0x39d1F90eF89C52dDA276194E9a832b484ee45574.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x39d1F90eF89C52dDA276194E9a832b484ee45574.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x39d1F90eF89C52dDA276194E9a832b484ee45574",
+  chain_id: 98866,
+  contract_address: "0x39d1F90eF89C52dDA276194E9a832b484ee45574",
+  name: "Plume ETH",
+  symbol: "pETH",
+  image: "https://peth.plumenetwork.xyz/pETH.svg",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "ethereum",
+  type: "token",
+});
