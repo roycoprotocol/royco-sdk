e99f4cd1ebd1b5fc04b6ba51ebf99cd064645038
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-09 00:55:17 -0700

    feat(market-map): Update market -- 1_0_0x3bddcc3aaab556b8c8a1ce313fa511004a9d6977504226fbb134e2f138545159 -- Recipe (0x3bdd...5159)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x3bddcc3aaab556b8c8a1ce313fa511004a9d6977504226fbb134e2f138545159 (Market Hash)
    - Request Timestamp: Wed, 09 Apr 2025 07:55:16 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x3bddcc3aaab556b8c8a1ce313fa511004a9d6977504226fbb134e2f138545159.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x3bddcc3aaab556b8c8a1ce313fa511004a9d6977504226fbb134e2f138545159`,
+  name: `Stake weETH into weETH Vault`,
+  description: `Stake weETH and hold it approximately until 10 July 2025 (~92 days) to be eligible for staking rewards`,
+  is_verified: false,
+});
