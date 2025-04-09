3c3a8c9853444093313aeb8abde297e020bda770
GitHub Action <action@github.com>
2025-04-09 17:33:03 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/definitions/98866-0x6cbe49db39ae10503d1933de00529f210fdbe0ac.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x6cbe49db39ae10503d1933de00529f210fdbe0ac.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98866-0x6CBe49DB39AE10503d1933DE00529f210fDbE0ac",
+  id: "98866-0x6cbe49db39ae10503d1933de00529f210fdbe0ac",
   chain_id: 98866,
-  contract_address: "0x6CBe49DB39AE10503d1933DE00529f210fDbE0ac",
+  contract_address: "0x6cbe49db39ae10503d1933de00529f210fdbe0ac",
   name: "Mystic Points",
   symbol: "MP",
   image: "https://dev.nest.credit/images/nest-basis-vault.svg",
