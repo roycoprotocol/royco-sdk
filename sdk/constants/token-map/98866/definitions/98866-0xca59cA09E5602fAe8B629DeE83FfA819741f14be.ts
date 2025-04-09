ae95051d06c4fd9cce86f3ce745aab9cd6521539
bharatvish <bharat@waymont.co>
2025-04-09 13:59:54 -0700

    Create 98866-0xca59cA09E5602fAe8B629DeE83FfA819741f14be.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0xca59cA09E5602fAe8B629DeE83FfA819741f14be.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xca59cA09E5602fAe8B629DeE83FfA819741f14be",
+  chain_id: 98866,
+  contract_address: "0xca59cA09E5602fAe8B629DeE83FfA819741f14be",
+  name: "Wrapped ETH",
+  symbol: "wETH",
+  image: "https://zengo.com/wp-content/uploads/wETH_desktop.svg",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "1027",
+  type: "token",
+});
