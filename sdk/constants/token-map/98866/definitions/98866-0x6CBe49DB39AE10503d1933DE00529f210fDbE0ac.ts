6ead31de309dbf42e442807eff5c2acda9f5200a
bharatvish <bharat@waymont.co>
2025-04-09 10:26:31 -0700

    Create 98866-0x6CBe49DB39AE10503d1933DE00529f210fDbE0ac.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x6CBe49DB39AE10503d1933DE00529f210fDbE0ac.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x6CBe49DB39AE10503d1933DE00529f210fDbE0ac",
+  chain_id: 98866,
+  contract_address: "0x6CBe49DB39AE10503d1933DE00529f210fDbE0ac",
+  name: "Mystic Points",
+  symbol: "MP",
+  image: "https://dev.nest.credit/images/nest-basis-vault.svg",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "point",
+});
