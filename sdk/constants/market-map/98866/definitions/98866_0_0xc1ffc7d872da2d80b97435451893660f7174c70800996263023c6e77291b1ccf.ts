846933219e0dc74b1d26035b8b4b7388a13ce054
bharatvish <bharat@waymont.co>
2025-04-09 14:54:26 -0700

    Update 98866_0_0xc1ffc7d872da2d80b97435451893660f7174c70800996263023c6e77291b1ccf.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0xc1ffc7d872da2d80b97435451893660f7174c70800996263023c6e77291b1ccf.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0xc1ffc7d872da2d80b97435451893660f7174c70800996263023c6e77291b1ccf.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `98866_0_0xc1ffc7d872da2d80b97435451893660f7174c70800996263023c6e77291b1ccf`,
   name: `Supply wPlume on Solera`,
-  description: `Supply wPlume on Solera`,
+  description: `Supply wPlume on Solera for 90 days. Users can withdraw at anytime but have to forfeit their rewards.`,
   is_verified: false,
 });
