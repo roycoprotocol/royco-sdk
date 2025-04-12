1b476f3951facaa6151dba04d190ed56734e7c21
bharatvish <bharat@waymont.co>
2025-04-11 17:37:26 -0700

    Create 98866-0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98",
+  chain_id: 98866,
+  contract_address: "0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98",
+  name: "Nucleus op-nBASIS",
+  symbol: "op-nBASIS",
+  image:
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+  decimals: 6,
+  source: "nucleus",
+  search_id: "0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98",
+  type: "token",
+});
