06fc43b2bee3f4fc1ff168bdcb072f90c43f9180
bharatvish <bharat@waymont.co>
2025-04-11 17:39:02 -0700

    Create 98866-0x8337930b8431f10f2644ddf7189c43802f76cb88.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x8337930b8431f10f2644ddf7189c43802f76cb88.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x8337930b8431f10f2644ddf7189c43802f76cb88",
+  chain_id: 98866,
+  contract_address: "0x8337930b8431f10f2644ddf7189c43802f76cb88",
+  name: "Nucleus op-PLUME",
+  symbol: "op-PLUME",
+  image:
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+  decimals: 6,
+  source: "nucleus",
+  search_id: "0x8337930b8431f10f2644ddf7189c43802f76cb88",
+  type: "token",
+});
