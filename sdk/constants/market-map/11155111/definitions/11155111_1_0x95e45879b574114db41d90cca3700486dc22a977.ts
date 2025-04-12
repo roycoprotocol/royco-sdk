ce4d7129292e895a46cc7ccf53170ee68f4c1501
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-12 01:04:43 -0700

    feat(market-map): Update market -- 11155111_1_0x95e45879b574114db41d90cca3700486dc22a977 -- Vault (0x95e4...a977)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 1 (Vault)
    - Market ID: 0x95e45879b574114db41d90cca3700486dc22a977 (Wrapped Vault Address)
    - Request Timestamp: Sat, 12 Apr 2025 08:04:43 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_1_0x95e45879b574114db41d90cca3700486dc22a977.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_1_0x95e45879b574114db41d90cca3700486dc22a977`,
+  name: `add liq tt`,
+  description: `add liq tt`,
+  is_verified: false,
+});
