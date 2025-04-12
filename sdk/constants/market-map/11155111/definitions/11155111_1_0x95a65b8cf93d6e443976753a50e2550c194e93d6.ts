249ae989380c6dfce22ea35b1d877d75962bc6a0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-12 01:12:42 -0700

    feat(market-map): Update market -- 11155111_1_0x95a65b8cf93d6e443976753a50e2550c194e93d6 -- Vault (0x95a6...93d6) (#835)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 1 (Vault)
    - Market ID: 0x95a65b8cf93d6e443976753a50e2550c194e93d6 (Wrapped Vault Address)
    - Request Timestamp: Sat, 12 Apr 2025 08:12:41 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_1_0x95a65b8cf93d6e443976753a50e2550c194e93d6.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_1_0x95a65b8cf93d6e443976753a50e2550c194e93d6`,
+  name: `vault tt`,
+  description: `vault tt vault tt vault  dfddff`,
+  is_verified: false,
+});
