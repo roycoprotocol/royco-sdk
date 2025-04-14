b18307ce218ede48dd75774a70309b5e78ae9b5b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-14 11:50:46 -0700

    feat(market-map): Update market -- 98866_0_0x9dc945e013647ee9dcd9e297b11b1f3185fc9206e88ff92023db5ccd04bdda2c -- Recipe (0x9dc9...da2c) (#836)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x9dc945e013647ee9dcd9e297b11b1f3185fc9206e88ff92023db5ccd04bdda2c (Market Hash)
    - Request Timestamp: Mon, 14 Apr 2025 18:50:43 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x9dc945e013647ee9dcd9e297b11b1f3185fc9206e88ff92023db5ccd04bdda2c.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x9dc945e013647ee9dcd9e297b11b1f3185fc9206e88ff92023db5ccd04bdda2c`,
+  name: `Deposit op-nBASIS into Royco for 90 Days`,
+  description: `Deposit op-nBASIS into Royco for 90 Days. Depositors can withdraw at anytime but they must forfeit their incentives.`,
+  is_verified: false,
+});
