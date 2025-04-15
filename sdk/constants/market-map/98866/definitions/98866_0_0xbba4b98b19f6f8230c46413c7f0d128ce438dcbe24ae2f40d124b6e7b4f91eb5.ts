8df42f009a81129219a164dd2781651010bdb72b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-14 17:02:08 -0700

    feat(market-map): Update market -- 98866_0_0xbba4b98b19f6f8230c46413c7f0d128ce438dcbe24ae2f40d124b6e7b4f91eb5 -- Recipe (0xbba4...1eb5)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xbba4b98b19f6f8230c46413c7f0d128ce438dcbe24ae2f40d124b6e7b4f91eb5 (Market Hash)
    - Request Timestamp: Tue, 15 Apr 2025 00:02:08 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xbba4b98b19f6f8230c46413c7f0d128ce438dcbe24ae2f40d124b6e7b4f91eb5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xbba4b98b19f6f8230c46413c7f0d128ce438dcbe24ae2f40d124b6e7b4f91eb5`,
+  name: `Deposit op-nTBILL Into Royco for 90 Days`,
+  description: `Deposit op-nTBILL into Royco for 90 days to earn Plume incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
+  is_verified: false,
+});
