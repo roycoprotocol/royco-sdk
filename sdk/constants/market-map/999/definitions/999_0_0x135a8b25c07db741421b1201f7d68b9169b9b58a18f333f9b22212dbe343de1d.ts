29e776d24272b52c5e871c6e3c2b1f8a6b714c36
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-15 15:27:31 -0700

    feat(market-map): Update market -- 999_0_0x135a8b25c07db741421b1201f7d68b9169b9b58a18f333f9b22212dbe343de1d -- Recipe (0x135a...de1d)
    Market Details:
    - Chain ID: 999
    - Market Type: 0 (Recipe)
    - Market ID: 0x135a8b25c07db741421b1201f7d68b9169b9b58a18f333f9b22212dbe343de1d (Market Hash)
    - Request Timestamp: Tue, 15 Apr 2025 22:27:30 GMT

---
+++ sdk/constants/market-map/999/definitions/999_0_0x135a8b25c07db741421b1201f7d68b9169b9b58a18f333f9b22212dbe343de1d.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `999_0_0x135a8b25c07db741421b1201f7d68b9169b9b58a18f333f9b22212dbe343de1d`,
+  name: `Lend wHYPE for 30 days`,
+  description: `Lend wHYPE for 30 days`,
+  is_verified: false,
+});
