2f06da30d2c9d74ac4e861f45c3bfef3932c2dd1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-15 12:18:24 -0700

    feat(market-map): Update market -- 98866_0_0xbf398306282c870f89f4e604594355434f4518da6aaf5fa883d459fd69588b48 -- Recipe (0xbf39...8b48) (#846)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xbf398306282c870f89f4e604594355434f4518da6aaf5fa883d459fd69588b48 (Market Hash)
    - Request Timestamp: Tue, 15 Apr 2025 19:18:22 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xbf398306282c870f89f4e604594355434f4518da6aaf5fa883d459fd69588b48.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xbf398306282c870f89f4e604594355434f4518da6aaf5fa883d459fd69588b48`,
+  name: `Deposit op-PLUME Into Royco for 90 Days`,
+  description: `Deposit op-PLUME into Royco for 90 days to earn Plume incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
+  is_verified: false,
+});
