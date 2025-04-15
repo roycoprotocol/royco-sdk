8e54c3c76f62898055334192dfd6060ea60319de
bharatvish <bharat@waymont.co>
2025-04-15 15:39:21 -0700

    Create 999-0x9FDBdA0A5e284c32744D2f17Ee5c74B284993463.ts

---
+++ sdk/constants/token-map/999/definitions/999-0x9FDBdA0A5e284c32744D2f17Ee5c74B284993463.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0x9FDBdA0A5e284c32744D2f17Ee5c74B284993463",
+  chain_id: 999,
+  contract_address: "0x9FDBdA0A5e284c32744D2f17Ee5c74B284993463",
+  name: "Unit BTC",
+  symbol: "uBTC",
+  image:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "unit-bitcoin",
+  type: "token",
+});
