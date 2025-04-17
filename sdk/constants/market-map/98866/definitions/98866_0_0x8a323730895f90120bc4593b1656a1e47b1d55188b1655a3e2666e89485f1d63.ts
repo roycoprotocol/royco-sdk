d19c426be0080cfc34c038055dec77ec19e27523
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-16 21:22:05 -0700

    feat(market-map): Update market -- 98866_0_0x8a323730895f90120bc4593b1656a1e47b1d55188b1655a3e2666e89485f1d63 -- Recipe (0x8a32...1d63)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x8a323730895f90120bc4593b1656a1e47b1d55188b1655a3e2666e89485f1d63 (Market Hash)
    - Request Timestamp: Thu, 17 Apr 2025 04:22:04 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x8a323730895f90120bc4593b1656a1e47b1d55188b1655a3e2666e89485f1d63.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x8a323730895f90120bc4593b1656a1e47b1d55188b1655a3e2666e89485f1d63`,
+  name: `Leverage nTBILL on Mystic Finance`,
+  description: `When you supply nTBILL, it’s automatically leverages through Mystic Finance and Rooster Protocol and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  is_verified: false,
+});
