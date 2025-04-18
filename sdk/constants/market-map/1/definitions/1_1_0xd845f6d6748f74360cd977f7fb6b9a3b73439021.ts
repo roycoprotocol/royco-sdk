691d438c5f5e84b9d412c3a57af75f0fd70458e6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-17 23:16:34 -0700

    feat(market-map): Update market -- 1_1_0xd845f6d6748f74360cd977f7fb6b9a3b73439021 -- Vault (0xd845...9021)
    Market Details:
    - Chain ID: 1
    - Market Type: 1 (Vault)
    - Market ID: 0xd845f6d6748f74360cd977f7fb6b9a3b73439021 (Wrapped Vault Address)
    - Request Timestamp: Fri, 18 Apr 2025 06:16:33 GMT

---
+++ sdk/constants/market-map/1/definitions/1_1_0xd845f6d6748f74360cd977f7fb6b9a3b73439021.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_1_0xd845f6d6748f74360cd977f7fb6b9a3b73439021`,
+  name: `Compounding OpenDollar`,
+  description: `The OpenEden Compounding Open Dollar (“cUSDO”) is a rebasing yield-bearing stablecoin issued by OpenEden Digital (&quot;OED&quot;), a Bermuda Monetary Authority (&quot;BMA&quot;) licensed digital asset issuer. cUSDO is backed by high-quality, liquid reserves, primarily consisting of U.S. Treasury bills and reverse repurchase agreements.`,
+  is_verified: false,
+});
