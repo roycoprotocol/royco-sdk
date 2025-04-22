a20e330e29617cca135858d6ebc3d22949b9b968
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-22 10:21:11 -0700

    feat(market-map): Update market -- 98866_0_0x60b09ad85cacf66b99d8e8341715035d60517785bca4da8f69b8950ba2351a22 -- Recipe (0x60b0...1a22)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x60b09ad85cacf66b99d8e8341715035d60517785bca4da8f69b8950ba2351a22 (Market Hash)
    - Request Timestamp: Tue, 22 Apr 2025 17:21:10 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x60b09ad85cacf66b99d8e8341715035d60517785bca4da8f69b8950ba2351a22.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x60b09ad85cacf66b99d8e8341715035d60517785bca4da8f69b8950ba2351a22`,
+  name: `LP wETH into Tempest ALM for 90 days`,
+  description: `LP wETH into Tempest ALM for 90 days`,
+  is_verified: false,
+});
