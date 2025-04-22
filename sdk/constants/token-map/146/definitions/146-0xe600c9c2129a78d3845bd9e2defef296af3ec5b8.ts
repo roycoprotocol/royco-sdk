15f3433d45f6e13dd21d4f11cb82f750b9b4579e
GitHub Action <action@github.com>
2025-04-22 14:42:52 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/146/definitions/146-0xe600c9c2129a78d3845bd9e2defef296af3ec5b8.ts
+++ sdk/constants/token-map/146/definitions/146-0xe600c9c2129a78d3845bd9e2defef296af3ec5b8.ts
@@ -1,12 +1,13 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "146-0xe600c9c2129A78D3845Bd9E2DefeF296af3Ec5B8",
+  id: "146-0xe600c9c2129a78d3845bd9e2defef296af3ec5b8",
   chain_id: 146,
-  contract_address: "0xe600c9c2129A78D3845Bd9E2DefeF296af3Ec5B8",
+  contract_address: "0xe600c9c2129a78d3845bd9e2defef296af3ec5b8",
   name: "RFX Particles",
   symbol: "RFXP",
-  image: "https://pbs.twimg.com/profile_images/1752283902922874880/pAAjz_Y2_400x400.jpg",
+  image:
+    "https://pbs.twimg.com/profile_images/1752283902922874880/pAAjz_Y2_400x400.jpg",
   decimals: 18,
   source: "external",
   search_id: "1-0xe600c9c2129A78D3845Bd9E2DefeF296af3Ec5B8",
--- sdk/constants/token-map/146/index.ts
+++ sdk/constants/token-map/146/index.ts
@@ -38,9 +38,8 @@ import token_146_0xd3dce716f3ef535c5ff8d041c1a41c3bd89b97ae from "./definitions/
 import token_146_0xe16bb6061b3567ee86285ab7780187cb39acc55e from "./definitions/146-0xe16bb6061b3567ee86285ab7780187cb39acc55e";
 import token_146_0xe51ee9868c1f0d6cd968a8b8c8376dc2991bfe44 from "./definitions/146-0xe51ee9868c1f0d6cd968a8b8c8376dc2991bfe44";
 import token_146_0xe5da20f15420ad15de0fa650600afc998bbe3955 from "./definitions/146-0xe5da20f15420ad15de0fa650600afc998bbe3955";
+import token_146_0xe600c9c2129a78d3845bd9e2defef296af3ec5b8 from "./definitions/146-0xe600c9c2129a78d3845bd9e2defef296af3ec5b8";
 import token_146_0xe715cba7b5ccb33790cebff1436809d36cb17e57 from "./definitions/146-0xe715cba7b5ccb33790cebff1436809d36cb17e57";
-import token_146_0xe600c9c2129A78D3845Bd9E2DefeF296af3Ec5B8 from "./definitions/146-0xe600c9c2129A78D3845Bd9E2DefeF296af3Ec5B8";
-
 
 export const TokenMap146 = {
   [token_146_0x0000000000000000000000000000000000000000.id]:
@@ -123,8 +122,8 @@ export const TokenMap146 = {
     token_146_0xe51ee9868c1f0d6cd968a8b8c8376dc2991bfe44,
   [token_146_0xe5da20f15420ad15de0fa650600afc998bbe3955.id]:
     token_146_0xe5da20f15420ad15de0fa650600afc998bbe3955,
+  [token_146_0xe600c9c2129a78d3845bd9e2defef296af3ec5b8.id]:
+    token_146_0xe600c9c2129a78d3845bd9e2defef296af3ec5b8,
   [token_146_0xe715cba7b5ccb33790cebff1436809d36cb17e57.id]:
     token_146_0xe715cba7b5ccb33790cebff1436809d36cb17e57,
-  [token_146_0xe600c9c2129A78D3845Bd9E2DefeF296af3Ec5B8.id]:
-    token_146_0xe600c9c2129A78D3845Bd9E2DefeF296af3Ec5B8,
 };
