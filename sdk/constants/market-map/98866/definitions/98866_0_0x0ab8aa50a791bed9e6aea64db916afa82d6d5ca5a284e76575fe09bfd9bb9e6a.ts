881ed20d1d113c75d82e20540efaa33455f72c4f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-23 04:57:55 -0700

    feat(market-map): Update market -- 98866_0_0x0ab8aa50a791bed9e6aea64db916afa82d6d5ca5a284e76575fe09bfd9bb9e6a -- Recipe (0x0ab8...9e6a)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x0ab8aa50a791bed9e6aea64db916afa82d6d5ca5a284e76575fe09bfd9bb9e6a (Market Hash)
    - Request Timestamp: Wed, 23 Apr 2025 11:57:54 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x0ab8aa50a791bed9e6aea64db916afa82d6d5ca5a284e76575fe09bfd9bb9e6a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x0ab8aa50a791bed9e6aea64db916afa82d6d5ca5a284e76575fe09bfd9bb9e6a`,
+  name: `Supply pETH on Mystic Finance`,
+  description: `When you supply pETH, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  is_verified: false,
+});
