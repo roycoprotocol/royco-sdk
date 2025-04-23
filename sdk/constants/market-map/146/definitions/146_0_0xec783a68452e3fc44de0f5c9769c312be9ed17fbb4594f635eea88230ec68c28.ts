6c6681b5cb2cce26b592a974f2988f6320589b7a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-23 04:06:46 -0700

    feat(market-map): Update market -- 146_0_0xec783a68452e3fc44de0f5c9769c312be9ed17fbb4594f635eea88230ec68c28 -- Recipe (0xec78...8c28) (#870)
    Market Details:
    - Chain ID: 146
    - Market Type: 0 (Recipe)
    - Market ID: 0xec783a68452e3fc44de0f5c9769c312be9ed17fbb4594f635eea88230ec68c28 (Market Hash)
    - Request Timestamp: Wed, 23 Apr 2025 11:06:44 GMT

---
+++ sdk/constants/market-map/146/definitions/146_0_0xec783a68452e3fc44de0f5c9769c312be9ed17fbb4594f635eea88230ec68c28.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `146_0_0xec783a68452e3fc44de0f5c9769c312be9ed17fbb4594f635eea88230ec68c28`,
+  name: `Deposit scUSD into RFX Vaults`,
+  description: `Deposit scUSD into the RFX scUSD Vault to obtain RFX SLV tokens and earn fees from leverage trading. The SLV allocates liquidity to all scUSD markets on RFX, and automatically rebalances liquidity to the best performing markets with 0 impermanent loss. Traders borrow liquidity from the scUSD Vault to execute trades in exchange for fees.`,
+  is_verified: false,
+});
