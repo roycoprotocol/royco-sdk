a46b0807804f4a0ed64a607018ada87a02309424
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-23 09:47:21 -0700

    feat(market-map): Update market -- 98866_0_0x40bb8b77686fdea111f3e5b369dacd049abb8224550b453b6308b1038638e355 -- Recipe (0x40bb...e355)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x40bb8b77686fdea111f3e5b369dacd049abb8224550b453b6308b1038638e355 (Market Hash)
    - Request Timestamp: Wed, 23 Apr 2025 16:47:20 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x40bb8b77686fdea111f3e5b369dacd049abb8224550b453b6308b1038638e355.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x40bb8b77686fdea111f3e5b369dacd049abb8224550b453b6308b1038638e355`,
+  name: `LP PLUME ETH&#x2F;PLUME 90 Tempest`,
+  description: `LP PLUME ETH&#x2F;PLUME 90 Tempest`,
+  is_verified: false,
+});
