31a33df9e9460336150e5d005f044df0e5616444
GitHub Action <action@github.com>
2025-04-23 15:41:34 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/11155111/definitions/11155111-0xd295b8b73ca5277b5d4cbb91bedaca337cbd078e.ts
+++ sdk/constants/token-map/11155111/definitions/11155111-0xd295b8b73ca5277b5d4cbb91bedaca337cbd078e.ts
@@ -6,7 +6,8 @@ export default defineToken({
   contract_address: "0xd295b8b73ca5277b5d4cbb91bedaca337cbd078e",
   name: "Test Stratus Point",
   symbol: "TSTRAT",
-  image: "https://res.cloudinary.com/ds4pux6du/image/upload/v1745382106/static-assets/rpuiujhspdxihpziunic.png",
+  image:
+    "https://res.cloudinary.com/ds4pux6du/image/upload/v1745382106/static-assets/rpuiujhspdxihpziunic.png",
   decimals: 18,
   source: "external",
   search_id: "none",
