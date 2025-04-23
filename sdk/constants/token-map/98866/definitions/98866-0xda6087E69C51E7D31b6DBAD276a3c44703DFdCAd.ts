e729d34e52c0d1f3a45db4c5a62a8d99b51baf9e
bharatvish <bharat@waymont.co>
2025-04-23 10:17:18 -0700

    Create 98866-0xda6087E69C51E7D31b6DBAD276a3c44703DFdCAd.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0xda6087E69C51E7D31b6DBAD276a3c44703DFdCAd.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xda6087E69C51E7D31b6DBAD276a3c44703DFdCAd",
+  chain_id: 98866,
+  contract_address: "0xda6087E69C51E7D31b6DBAD276a3c44703DFdCAd",
+  name: "Tether",
+  symbol: "USDT",
+  image: "https://cdn.worldvectorlogo.com/logos/tether-1.svg",
+  decimals: 6,
+  source: "coingecko",
+  search_id: "tether",
+  type: "token",
+});
