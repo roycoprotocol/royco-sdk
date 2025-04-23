d45d4c618f9593e4af25b8ef072c786af39e26a0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-23 10:29:37 -0700

    feat(market-map): Update market -- 98866_0_0x2350ad77868ea5ed3cb6b3068a5891529c20ad62b1bdde0904a464163f8813f8 -- Recipe (0x2350...13f8) (#881)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x2350ad77868ea5ed3cb6b3068a5891529c20ad62b1bdde0904a464163f8813f8 (Market Hash)
    - Request Timestamp: Wed, 23 Apr 2025 17:29:35 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x2350ad77868ea5ed3cb6b3068a5891529c20ad62b1bdde0904a464163f8813f8.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x2350ad77868ea5ed3cb6b3068a5891529c20ad62b1bdde0904a464163f8813f8`,
+  name: `LP PUSD PUSD&#x2F;USDT 90 Tempest`,
+  description: `LP PUSD PUSD&#x2F;USDT 90 Tempest`,
+  is_verified: false,
+});
