b097a0b1b8ffc178045974a98a87e799239a76e6
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-23 04:52:22 -0700

    feat(market-map): Update market -- 98866_0_0xfe4f72b0ba1754133fe816f9fdc8b7b3054cdc8ce9ea387ff0a4c1ade3afc595 -- Recipe (0xfe4f...c595) (#872)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xfe4f72b0ba1754133fe816f9fdc8b7b3054cdc8ce9ea387ff0a4c1ade3afc595 (Market Hash)
    - Request Timestamp: Wed, 23 Apr 2025 11:52:20 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xfe4f72b0ba1754133fe816f9fdc8b7b3054cdc8ce9ea387ff0a4c1ade3afc595.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xfe4f72b0ba1754133fe816f9fdc8b7b3054cdc8ce9ea387ff0a4c1ade3afc595`,
+  name: `Supply WETH on Mystic Finance`,
+  description: `When you supply WETH, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  is_verified: false,
+});
