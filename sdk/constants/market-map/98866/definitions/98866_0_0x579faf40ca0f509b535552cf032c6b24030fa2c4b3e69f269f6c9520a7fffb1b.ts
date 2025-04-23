1284e4a1bd5ca1a2d10c223223aaaedc5382dfa4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-23 15:55:23 -0700

    feat(market-map): Update market -- 98866_0_0x579faf40ca0f509b535552cf032c6b24030fa2c4b3e69f269f6c9520a7fffb1b -- Recipe (0x579f...fb1b)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x579faf40ca0f509b535552cf032c6b24030fa2c4b3e69f269f6c9520a7fffb1b (Market Hash)
    - Request Timestamp: Wed, 23 Apr 2025 22:55:23 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x579faf40ca0f509b535552cf032c6b24030fa2c4b3e69f269f6c9520a7fffb1b.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x579faf40ca0f509b535552cf032c6b24030fa2c4b3e69f269f6c9520a7fffb1b`,
+  name: `Supply pUSD on Solera`,
+  description: `Supply pUSD on Solera`,
+  is_verified: false,
+});
