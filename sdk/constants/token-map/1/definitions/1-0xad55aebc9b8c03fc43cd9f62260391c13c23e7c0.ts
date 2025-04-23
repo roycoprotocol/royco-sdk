452936a7a423e4ef34ddd2d34f962bc1fff942c5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-22 21:26:20 -0700

    feat(token-map): Update Token -- 1-0xad55aebc9b8c03fc43cd9f62260391c13c23e7c0 -- OpenEden Compounding OpenDollar (CUSDO) (0xad55...e7c0)
    Token Details:
    - Id: 1-0xad55aebc9b8c03fc43cd9f62260391c13c23e7c0
    - Chain Id: 1
    - Contract Address: 0xad55aebc9b8c03fc43cd9f62260391c13c23e7c0
    - Name: OpenEden Compounding OpenDollar
    - Symbol: CUSDO
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/36068.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 36068

---
+++ sdk/constants/token-map/1/definitions/1-0xad55aebc9b8c03fc43cd9f62260391c13c23e7c0.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xad55aebc9b8c03fc43cd9f62260391c13c23e7c0",
+  chain_id: 1,
+  contract_address: "0xad55aebc9b8c03fc43cd9f62260391c13c23e7c0",
+  name: "OpenEden Compounding OpenDollar",
+  symbol: "CUSDO",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/36068.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "36068",
+  type: "token",
+});
