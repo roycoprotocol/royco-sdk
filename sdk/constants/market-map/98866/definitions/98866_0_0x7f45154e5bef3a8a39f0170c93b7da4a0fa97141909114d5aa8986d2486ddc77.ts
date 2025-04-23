272cc8e8b46eeb4a3ebe225fbb2ec918423e8ad3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-23 04:44:45 -0700

    feat(market-map): Update market -- 98866_0_0x7f45154e5bef3a8a39f0170c93b7da4a0fa97141909114d5aa8986d2486ddc77 -- Recipe (0x7f45...dc77) (#871)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x7f45154e5bef3a8a39f0170c93b7da4a0fa97141909114d5aa8986d2486ddc77 (Market Hash)
    - Request Timestamp: Wed, 23 Apr 2025 11:44:43 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x7f45154e5bef3a8a39f0170c93b7da4a0fa97141909114d5aa8986d2486ddc77.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x7f45154e5bef3a8a39f0170c93b7da4a0fa97141909114d5aa8986d2486ddc77`,
+  name: `Supply USDC on Mystic Finance`,
+  description: `When you supply USDC, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  is_verified: false,
+});
