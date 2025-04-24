d4289482d6bf424c48c73f7d2c42921320df5489
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-24 10:46:54 -0700

    feat(market-map): Update market -- 11155111_0_0x2a6887b796cd9b7fe5520f1ac0efb019fc44323ddd847f5f71e312d6fbab4ac9 -- Recipe (0x2a68...4ac9)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0x2a6887b796cd9b7fe5520f1ac0efb019fc44323ddd847f5f71e312d6fbab4ac9 (Market Hash)
    - Request Timestamp: Thu, 24 Apr 2025 17:46:54 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0x2a6887b796cd9b7fe5520f1ac0efb019fc44323ddd847f5f71e312d6fbab4ac9.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0x2a6887b796cd9b7fe5520f1ac0efb019fc44323ddd847f5f71e312d6fbab4ac9`,
+  name: `add liqudiity test`,
+  description: `add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test add liqudiity test`,
+  is_verified: false,
+});
