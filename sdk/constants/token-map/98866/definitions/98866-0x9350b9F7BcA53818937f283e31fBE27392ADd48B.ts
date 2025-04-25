39697186576abfb5827323171a0905d8912c123b
bharatvish <bharat@waymont.co>
2025-04-25 12:07:19 -0500

    Create 98866-0x9350b9F7BcA53818937f283e31fBE27392ADd48B.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x9350b9F7BcA53818937f283e31fBE27392ADd48B.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x9350b9F7BcA53818937f283e31fBE27392ADd48B",
+  chain_id: 98866,
+  contract_address: "0x9350b9F7BcA53818937f283e31fBE27392ADd48B",
+  name: "Rooster BP-nRWA-pUSD-3",
+  symbol: "RBP-nRWA-pUSD-3",
+  image: "https://pbs.twimg.com/profile_images/1870064500474347520/a8gJtOSE_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "token",
+});
