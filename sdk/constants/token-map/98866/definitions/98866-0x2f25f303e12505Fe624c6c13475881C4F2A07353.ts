dd0b39ae019c52c5ebcc182f4738892754d6984b
bharatvish <bharat@waymont.co>
2025-04-25 12:08:26 -0500

    Create 98866-0x2f25f303e12505Fe624c6c13475881C4F2A07353.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x2f25f303e12505Fe624c6c13475881C4F2A07353.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x2f25f303e12505Fe624c6c13475881C4F2A07353",
+  chain_id: 98866,
+  contract_address: "0x2f25f303e12505Fe624c6c13475881C4F2A07353",
+  name: "Rooster BP-pETH-WPLUME-2",
+  symbol: "RBP-pETH-WPLUME-2",
+  image: "https://pbs.twimg.com/profile_images/1870064500474347520/a8gJtOSE_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "token",
+});
