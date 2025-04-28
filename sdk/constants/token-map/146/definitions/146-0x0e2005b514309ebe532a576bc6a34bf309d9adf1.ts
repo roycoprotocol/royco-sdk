6249cbda94e797674477ff52dae4805403e76dde
bharatvish <bharat@waymont.co>
2025-04-28 15:55:26 -0700

    Create 146-0x0e2005b514309ebe532a576bc6a34bf309d9adf1.ts

---
+++ sdk/constants/token-map/146/definitions/146-0x0e2005b514309ebe532a576bc6a34bf309d9adf1.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "146-0x0e2005b514309ebe532a576bc6a34bf309d9adf1",
+  chain_id: 146,
+  contract_address: "0x0e2005b514309ebe532a576bc6a34bf309d9adf1",
+  name: "Triassic Stables LP Token",
+  symbol: "TSLP",
+  image:
+    "https://pbs.twimg.com/profile_images/1869124361740824576/te1GZ7nl_400x400.png",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "token",
+});
