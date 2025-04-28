99a29e27cfac319dd20517432b3930126934a95d
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-28 01:05:03 -0700

    feat(market-map): Update market -- 8453_1_0x10a1bca416313725daeaf4eb0478c0c00d34eb52 -- Vault (0x10a1...eb52) (#896)
    Market Details:
    - Chain ID: 8453
    - Market Type: 1 (Vault)
    - Market ID: 0x10a1bca416313725daeaf4eb0478c0c00d34eb52 (Wrapped Vault Address)
    - Request Timestamp: Mon, 28 Apr 2025 08:05:00 GMT

---
+++ sdk/constants/market-map/8453/definitions/8453_1_0x10a1bca416313725daeaf4eb0478c0c00d34eb52.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `8453_1_0x10a1bca416313725daeaf4eb0478c0c00d34eb52`,
+  name: `Lend ETH on ExtraFi XLend`,
+  description: `When ETH is supplied to the market, it is deposited to ExtraFi XLend&#x27;s ETH pool on Base`,
+  is_verified: false,
+});
