2d3a5422be528d751f9864b45e218499d7c5c102
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-04-28 11:42:01 -0700

    feat(market-map): Update market -- 98866_0_0x0e8ed2a7fc70f142f3fb6edcbc6b6087efd89937e04b9555016b60fc3ab732af -- Recipe (0x0e8e...32af)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x0e8ed2a7fc70f142f3fb6edcbc6b6087efd89937e04b9555016b60fc3ab732af (Market Hash)
    - Request Timestamp: Mon, 28 Apr 2025 18:42:00 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x0e8ed2a7fc70f142f3fb6edcbc6b6087efd89937e04b9555016b60fc3ab732af.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x0e8ed2a7fc70f142f3fb6edcbc6b6087efd89937e04b9555016b60fc3ab732af`,
+  name: `pETH-Plume Rooster LP YAP #2`,
+  description: `Supply a Rooster LP token from the pETH-Plume pool—this LP token is a Yield Accruing Position (YAP) in a Rooster Protocol liquidity pool. YAPs accrue Rooster emissions as users swap in and vote for the pETH-Plume pool.`,
+  is_verified: false,
+});
