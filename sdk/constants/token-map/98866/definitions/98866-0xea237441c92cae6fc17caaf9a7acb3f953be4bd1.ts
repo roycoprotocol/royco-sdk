3786bc5e5dafa804071ab25405630e884af6f0ac
GitHub Action <action@github.com>
2025-04-30 04:44:55 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/definitions/98866-0xea237441c92cae6fc17caaf9a7acb3f953be4bd1.ts
+++ sdk/constants/token-map/98866/definitions/98866-0xea237441c92cae6fc17caaf9a7acb3f953be4bd1.ts
@@ -6,7 +6,8 @@ export default defineToken({
   contract_address: "0xea237441c92cae6fc17caaf9a7acb3f953be4bd1",
   name: "Wrapped Plume",
   symbol: "WPLUME",
-  image: "https://token-media.defined.fi/98866_0xea237441c92cae6fc17caaf9a7acb3f953be4bd1_large_ea39d469-3e7d-413e-99a7-9a3eafeea22a.png",
+  image:
+    "https://token-media.defined.fi/98866_0xea237441c92cae6fc17caaf9a7acb3f953be4bd1_large_ea39d469-3e7d-413e-99a7-9a3eafeea22a.png",
   decimals: 18,
   source: "coinmarketcap",
   search_id: "35364",
