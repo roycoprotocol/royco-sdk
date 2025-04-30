7e5d011704ef8a464bec98dc1d46e4fc3fcea27d
bharatvish <bharat@waymont.co>
2025-04-30 10:04:47 -0700

    Update 1_0_0xc55f7e179abed67f34cdf01a0d313336ab5bc2dd84c834325c91c311ec079c6b.ts

--- sdk/constants/market-map/1/definitions/1_0_0xc55f7e179abed67f34cdf01a0d313336ab5bc2dd84c834325c91c311ec079c6b.ts
+++ sdk/constants/market-map/1/definitions/1_0_0xc55f7e179abed67f34cdf01a0d313336ab5bc2dd84c834325c91c311ec079c6b.ts
@@ -3,7 +3,7 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `1_0_0xc55f7e179abed67f34cdf01a0d313336ab5bc2dd84c834325c91c311ec079c6b`,
   name: `Stake weETH into weETH Vault`,
-  description: `Stake weETH and hold it approximately until 10 July 2025 (~80 days) to be eligible for staking rewards`,
+  description: `Stake weETH and hold it approximately until 10 July 2025 (~80 days) to be eligible for staking rewards. Users will be locked in the Levva Vault until the end of the term.`,
   is_verified: true,
 
   native_yield: [
