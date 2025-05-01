b6e4e0c3010046c16b9e3d4f57746dc7f0a617db
GitHub Action <action@github.com>
2025-05-01 17:15:10 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/definitions/98866-0x2fb465b77a033e62c441f1b46751c55e3ab55de3.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x2fb465b77a033e62c441f1b46751c55e3ab55de3.ts
@@ -4,7 +4,7 @@ export default defineToken({
   id: "98866-0x2fb465b77a033e62c441f1b46751c55e3ab55de3",
   chain_id: 98866,
   contract_address: "0x2fb465b77a033e62c441f1b46751c55e3ab55de3",
-  name: "Rooster YAP-pUSD-WPLUME-1",
+  name: "Rooster YAP-nRWA-pUSD-1 ",
   symbol: "YAP-nRWA-pUSD-1",
   image:
     "https://pbs.twimg.com/profile_images/1870064500474347520/a8gJtOSE_400x400.jpg",
