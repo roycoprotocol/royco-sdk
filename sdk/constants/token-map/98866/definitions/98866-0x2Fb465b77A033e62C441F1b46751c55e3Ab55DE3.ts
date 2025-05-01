f5dbc592ccba7c72d41f25655696cc0eab64d970
bharatvish <bharat@waymont.co>
2025-05-01 09:38:45 -0700

    Update and rename 98866-0x16b217cca21a043bd56b7d2495ec79ddcc5c14e2.ts to 98866-0x2Fb465b77A033e62C441F1b46751c55e3Ab55DE3.ts

--- sdk/constants/token-map/98866/definitions/98866-0x2Fb465b77A033e62C441F1b46751c55e3Ab55DE3.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x2Fb465b77A033e62C441F1b46751c55e3Ab55DE3.ts
@@ -1,11 +1,11 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98866-0x16b217cca21a043bd56b7d2495ec79ddcc5c14e2",
+  id: "98866-0x2Fb465b77A033e62C441F1b46751c55e3Ab55DE3",
   chain_id: 98866,
-  contract_address: "0x16b217cca21a043bd56b7d2495ec79ddcc5c14e2",
-  name: "Rooster BP-pUSD-WPLUME-1",
-  symbol: "RBP-pUSD-WPLUME-1",
+  contract_address: "0x2Fb465b77A033e62C441F1b46751c55e3Ab55DE3",
+  name: "Rooster YAP-pUSD-WPLUME-1",
+  symbol: "YAP-nRWA-pUSD-1",
   image:
     "https://pbs.twimg.com/profile_images/1870064500474347520/a8gJtOSE_400x400.jpg",
   decimals: 18,
