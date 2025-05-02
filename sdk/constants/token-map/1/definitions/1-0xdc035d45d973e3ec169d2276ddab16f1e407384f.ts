852f84c7a98b8b3f400668090e9f760a2d823472
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-01 17:41:24 -0700

    feat(token-map): Update Token -- 1-0xdc035d45d973e3ec169d2276ddab16f1e407384f -- USDS (USDS) (0xdc03...384f)
    Token Details:
    - Id: 1-0xdc035d45d973e3ec169d2276ddab16f1e407384f
    - Chain Id: 1
    - Contract Address: 0xdc035d45d973e3ec169d2276ddab16f1e407384f
    - Name: USDS
    - Symbol: USDS
    - Image: https://s2.coinmarketcap.com/static/img/coins/64x64/33039.png
    - Decimals: 18
    - Source: coinmarketcap
    - Search Id: 33039

---
+++ sdk/constants/token-map/1/definitions/1-0xdc035d45d973e3ec169d2276ddab16f1e407384f.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0xdc035d45d973e3ec169d2276ddab16f1e407384f",
+  chain_id: 1,
+  contract_address: "0xdc035d45d973e3ec169d2276ddab16f1e407384f",
+  name: "USDS",
+  symbol: "USDS",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/33039.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "33039",
+  type: "token",
+});
