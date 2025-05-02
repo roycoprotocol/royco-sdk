e39cf642a33d087c6facfcf8a27973205084e4be
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-01 17:41:32 -0700

    feat(token-map): Update Token -- 1-0x05be1d4c307c19450a6fd7ce7307ce72a3829a60 -- International Meme Fund (IMF) (0x05be...9a60)
    Token Details:
    - Id: 1-0x05be1d4c307c19450a6fd7ce7307ce72a3829a60
    - Chain Id: 1
    - Contract Address: 0x05be1d4c307c19450a6fd7ce7307ce72a3829a60
    - Name: International Meme Fund
    - Symbol: IMF
    - Image: https://coin-images.coingecko.com/coins/images/52031/large/imf-coingecko-200px.png?1732365603
    - Decimals: 18
    - Source: coingecko
    - Search Id: international-meme-fund

---
+++ sdk/constants/token-map/1/definitions/1-0x05be1d4c307c19450a6fd7ce7307ce72a3829a60.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x05be1d4c307c19450a6fd7ce7307ce72a3829a60",
+  chain_id: 1,
+  contract_address: "0x05be1d4c307c19450a6fd7ce7307ce72a3829a60",
+  name: "International Meme Fund",
+  symbol: "IMF",
+  image: "https://coin-images.coingecko.com/coins/images/52031/large/imf-coingecko-200px.png?1732365603",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "international-meme-fund",
+  type: "token",
+});
