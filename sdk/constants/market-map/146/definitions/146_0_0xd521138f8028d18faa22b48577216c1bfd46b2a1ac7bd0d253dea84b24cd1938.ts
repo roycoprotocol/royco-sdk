3fd0403336285cdc32abb361e33bcee4e9dbf934
bharatvish <bharat@waymont.co>
2025-05-04 16:42:09 -0700

    Update 146_0_0xd521138f8028d18faa22b48577216c1bfd46b2a1ac7bd0d253dea84b24cd1938.ts

--- sdk/constants/market-map/146/definitions/146_0_0xd521138f8028d18faa22b48577216c1bfd46b2a1ac7bd0d253dea84b24cd1938.ts
+++ sdk/constants/market-map/146/definitions/146_0_0xd521138f8028d18faa22b48577216c1bfd46b2a1ac7bd0d253dea84b24cd1938.ts
@@ -4,5 +4,5 @@ export default defineMarket({
   id: `146_0_0xd521138f8028d18faa22b48577216c1bfd46b2a1ac7bd0d253dea84b24cd1938`,
   name: `Deposit wS into RFX Vaults`,
   description: `Deposit wS into the RFX wS Vault to obtain RFX SLV tokens and earn fees from leverage trading. The SLV allocates liquidity to all wS markets on RFX, and automatically rebalances liquidity to the best performing markets with 0 impermanent loss. Traders borrow liquidity from the wS Vault to execute trades in exchange for fees.`,
-  is_verified: false,
+  is_verified: true,
 });
