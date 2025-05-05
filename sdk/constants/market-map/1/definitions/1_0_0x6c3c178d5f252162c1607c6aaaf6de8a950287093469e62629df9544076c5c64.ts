7fecd2e177ee20d20f7a593d45431a3f675cebf0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-04 17:21:59 -0700

    feat(market-map): Update market -- 1_0_0x6c3c178d5f252162c1607c6aaaf6de8a950287093469e62629df9544076c5c64 -- Recipe (0x6c3c...5c64) (#913)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0x6c3c178d5f252162c1607c6aaaf6de8a950287093469e62629df9544076c5c64 (Market Hash)
    - Request Timestamp: Mon, 05 May 2025 00:21:57 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0x6c3c178d5f252162c1607c6aaaf6de8a950287093469e62629df9544076c5c64.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0x6c3c178d5f252162c1607c6aaaf6de8a950287093469e62629df9544076c5c64`,
+  name: `Deposit USDC into Neutrl`,
+  description: `Deposit USDC into Neutrl`,
+  is_verified: false,
+});
