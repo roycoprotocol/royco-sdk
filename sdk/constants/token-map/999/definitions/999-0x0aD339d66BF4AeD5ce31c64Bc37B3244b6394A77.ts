1c45290a6a40e5ee6fe346a7273a7f3962deeb53
bharatvish <bharat@waymont.co>
2025-05-06 10:15:20 -0700

    Create 999-0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77.ts

---
+++ sdk/constants/token-map/999/definitions/999-0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77",
+  chain_id: 999,
+  contract_address: "0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77",
+  name: "USR",
+  symbol: "USR",
+  image:
+    "https://pbs.twimg.com/profile_images/1726501525843841024/1gDrgTdA_400x400.jpg",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "resolv-usr",
+  type: "token",
+});
