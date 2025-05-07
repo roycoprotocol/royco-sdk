93db8e7d85ec27aa5b3ded0bc573733ae167e2cf
bharatvish <bharat@waymont.co>
2025-05-07 10:43:01 -0700

    Update 1_0_0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a.ts

--- sdk/constants/market-map/1/definitions/1_0_0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a.ts
+++ sdk/constants/market-map/1/definitions/1_0_0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `1_0_0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a`,
   name: `Deposit USDC into Neutrl LP Program for 6 months`,
-  description: `Users deposit their USDC in the Neutrl Royco Market for six months, and will be able to claim sNUSD and NTRL tokens from the Neutrl frontend. Users cannot withdraw once they deposit. Users must KYC at legal@neutrl.xyz before depositing into this Royco market or their deposits will be refunded with no incentives.`,
+  description: `Users deposit their USDC in the Neutrl Royco Market for six months, and will be able to claim sNUSD and NTRL tokens from the Neutrl frontend. Users cannot withdraw once they deposit. $10mm of deposits into this market represent 0.21% of the NTRL token supply which will be distributed pro-rata to Royco depositors.`,
   is_verified: false,
 });
