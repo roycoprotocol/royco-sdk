3458cd7ca806f64e3609817658ab0d7abef1f262
bharatvish <bharat@waymont.co>
2025-05-07 11:15:05 -0700

    Create 1-0x4359ea85157AF7a4D4A406ea225317CB365B8582.ts

---
+++ sdk/constants/token-map/1/definitions/1-0x4359ea85157AF7a4D4A406ea225317CB365B8582.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "1-0x4359ea85157AF7a4D4A406ea225317CB365B8582",
+  chain_id: 1,
+  contract_address: "0x4359ea85157AF7a4D4A406ea225317CB365B8582",
+  name: "Neutrl Royco Points",
+  symbol: "NRP",
+  image: "https://pbs.twimg.com/profile_images/1905226923749314560/ktnoHpRe_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "point",
+});
