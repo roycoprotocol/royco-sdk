865ff15c733e0de2053fcf9ea8ab6fc4897b29d0
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-06 22:55:01 -0700

    feat(market-map): Update market -- 1_0_0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a -- Recipe (0xad06...880a) (#925)
    Market Details:
    - Chain ID: 1
    - Market Type: 0 (Recipe)
    - Market ID: 0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a (Market Hash)
    - Request Timestamp: Wed, 07 May 2025 05:54:59 GMT

---
+++ sdk/constants/market-map/1/definitions/1_0_0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `1_0_0xad0638b8e54fe13974731cbe9ecbeeb3bd67b0c66163d5995427ee3a7367880a`,
+  name: `Deposit USDC into Neutrl LP Program for 6 months`,
+  description: `Users deposit their USDC in the Neutrl Royco Market for six months, and will be able to claim sNUSD and NTRL tokens from the Neutrl frontend. Users cannot withdraw once they deposit. Users must KYC at legal@neutrl.xyz before depositing into this Royco market or their deposits will be refunded with no incentives.`,
+  is_verified: false,
+});
