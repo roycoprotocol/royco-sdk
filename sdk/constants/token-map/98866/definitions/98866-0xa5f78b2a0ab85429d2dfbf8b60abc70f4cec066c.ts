5815e5e5b310b5b768fd59d633604b27a0d19fb8
bharatvish <bharat@waymont.co>
2025-05-06 17:12:12 -0700

    Create 98866-0xa5f78b2a0ab85429d2dfbf8b60abc70f4cec066c.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0xa5f78b2a0ab85429d2dfbf8b60abc70f4cec066c.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xa5f78b2a0ab85429d2dfbf8b60abc70f4cec066c",
+  chain_id: 98866,
+  contract_address: "0xa5f78b2a0ab85429d2dfbf8b60abc70f4cec066c",
+  name: "Nest Credit Vault",
+  symbol: "nCREDIT",
+  image: "https://app.nest.credit/images/nest-credit-vault.svg",
+  decimals: 6,
+  source: "plume",
+  search_id: "nest-rwa-vault",
+  type: "token",
+});
