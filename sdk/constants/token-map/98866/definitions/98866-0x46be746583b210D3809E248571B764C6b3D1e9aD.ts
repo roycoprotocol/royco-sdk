8e49d932b329af6522ce3b1e0dab88ac257328fd
bharatvish <bharat@waymont.co>
2025-05-08 10:30:33 -0700

    Create 98866-0x46be746583b210D3809E248571B764C6b3D1e9aD.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x46be746583b210D3809E248571B764C6b3D1e9aD.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x46be746583b210D3809E248571B764C6b3D1e9aD",
+  chain_id: 98866,
+  contract_address: "0x46be746583b210D3809E248571B764C6b3D1e9aD",
+  name: "Nucleus op-nPAYFI",
+  symbol: "op-nPAYFI",
+  image:
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+  decimals: 6,
+  source: "nucleus",
+  search_id: "0x46be746583b210D3809E248571B764C6b3D1e9aD",
+  type: "token",
+});
