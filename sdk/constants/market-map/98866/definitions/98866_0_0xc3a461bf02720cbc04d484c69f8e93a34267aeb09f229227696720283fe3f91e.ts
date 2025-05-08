166ceb5ccfaf26202dac68b8da35005770b4a6cc
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-08 16:31:37 -0700

    feat(market-map): Update market -- 98866_0_0xc3a461bf02720cbc04d484c69f8e93a34267aeb09f229227696720283fe3f91e -- Recipe (0xc3a4...f91e)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xc3a461bf02720cbc04d484c69f8e93a34267aeb09f229227696720283fe3f91e (Market Hash)
    - Request Timestamp: Thu, 08 May 2025 23:31:36 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xc3a461bf02720cbc04d484c69f8e93a34267aeb09f229227696720283fe3f91e.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xc3a461bf02720cbc04d484c69f8e93a34267aeb09f229227696720283fe3f91e`,
+  name: `Deposit op-nETF into plume for 90 days`,
+  description: `Deposit op-nETF into plume for 90 days`,
+  is_verified: false,
+});
