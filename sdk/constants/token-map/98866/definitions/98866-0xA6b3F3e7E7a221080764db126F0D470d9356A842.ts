3d84146c2063a437494a12e42d128a0e350a4a8d
bharatvish <bharat@waymont.co>
2025-05-08 10:36:39 -0700

    Create 98866-0xA6b3F3e7E7a221080764db126F0D470d9356A842.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0xA6b3F3e7E7a221080764db126F0D470d9356A842.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xA6b3F3e7E7a221080764db126F0D470d9356A842",
+  chain_id: 98866,
+  contract_address: "0xA6b3F3e7E7a221080764db126F0D470d9356A842",
+  name: "Nucleus op-nETF",
+  symbol: "op-nETF",
+  image:
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+  decimals: 6,
+  source: "nucleus",
+  search_id: "0xA6b3F3e7E7a221080764db126F0D470d9356A842",
+  type: "token",
+});
