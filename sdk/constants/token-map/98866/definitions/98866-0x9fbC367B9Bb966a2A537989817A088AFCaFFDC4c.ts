1a6e3b77277259bbe98dae8fcbc8019b80607ccb
bharatvish <bharat@waymont.co>
2025-05-08 21:38:08 -0700

    Update and rename 98866-0xd3bfd6e6187444170a1674c494e55171587b5641.ts to 98866-0x9fbC367B9Bb966a2A537989817A088AFCaFFDC4c.ts

--- sdk/constants/token-map/98866/definitions/98866-0x9fbC367B9Bb966a2A537989817A088AFCaFFDC4c.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x9fbC367B9Bb966a2A537989817A088AFCaFFDC4c.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98866-0xd3bfd6e6187444170a1674c494e55171587b5641",
+  id: "98866-0x9fbC367B9Bb966a2A537989817A088AFCaFFDC4c",
   chain_id: 98866,
-  contract_address: "0xd3bfd6e6187444170a1674c494e55171587b5641",
+  contract_address: "0x9fbC367B9Bb966a2A537989817A088AFCaFFDC4c",
   name: "Nest Elixir Vault",
   symbol: "nELIXIR",
   image: "https://app.nest.credit/images/nest-elixir-vault.svg",
