39b136a86f1b128d647f3b6d4091c6b7a5b2cdaf
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-09 12:32:53 -0700

    feat(market-map): Update market -- 999_0_0x154e93f452af808d81857578e283e6349d0251760112810880fd964ef303f4ae -- Recipe (0x154e...f4ae) (#937)
    Market Details:
    - Chain ID: 999
    - Market Type: 0 (Recipe)
    - Market ID: 0x154e93f452af808d81857578e283e6349d0251760112810880fd964ef303f4ae (Market Hash)
    - Request Timestamp: Fri, 09 May 2025 19:32:51 GMT

---
+++ sdk/constants/market-map/999/definitions/999_0_0x154e93f452af808d81857578e283e6349d0251760112810880fd964ef303f4ae.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `999_0_0x154e93f452af808d81857578e283e6349d0251760112810880fd964ef303f4ae`,
+  name: `Lend HYPE on Sentiment`,
+  description: `When HYPE is supplied to this market, it will deposited into a Sentiment SuperPool on HyperEVM.`,
+  is_verified: false,
+});
