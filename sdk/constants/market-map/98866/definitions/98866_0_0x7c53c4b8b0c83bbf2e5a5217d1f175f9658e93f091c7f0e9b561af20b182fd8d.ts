9bb7925c6154eeec8c20f79ca2121b2f92c6383a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-08 18:14:24 -0700

    feat(market-map): Update market -- 98866_0_0x7c53c4b8b0c83bbf2e5a5217d1f175f9658e93f091c7f0e9b561af20b182fd8d -- Recipe (0x7c53...fd8d)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x7c53c4b8b0c83bbf2e5a5217d1f175f9658e93f091c7f0e9b561af20b182fd8d (Market Hash)
    - Request Timestamp: Fri, 09 May 2025 01:14:23 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x7c53c4b8b0c83bbf2e5a5217d1f175f9658e93f091c7f0e9b561af20b182fd8d.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x7c53c4b8b0c83bbf2e5a5217d1f175f9658e93f091c7f0e9b561af20b182fd8d`,
+  name: `Deposit nELIXIR into Royco`,
+  description: `Deposit into this market to earn nELIXIR base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
