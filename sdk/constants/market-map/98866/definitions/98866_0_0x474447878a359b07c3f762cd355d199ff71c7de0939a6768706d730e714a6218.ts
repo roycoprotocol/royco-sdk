3290c9f652971b378157419de08f0c5c3ed80bbd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-08 19:02:10 -0700

    feat(market-map): Update market -- 98866_0_0x474447878a359b07c3f762cd355d199ff71c7de0939a6768706d730e714a6218 -- Recipe (0x4744...6218) (#931)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x474447878a359b07c3f762cd355d199ff71c7de0939a6768706d730e714a6218 (Market Hash)
    - Request Timestamp: Fri, 09 May 2025 02:02:08 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x474447878a359b07c3f762cd355d199ff71c7de0939a6768706d730e714a6218.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x474447878a359b07c3f762cd355d199ff71c7de0939a6768706d730e714a6218`,
+  name: `Deposit nCredit into Royco`,
+  description: `Deposit into this market to earn nCredit base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
