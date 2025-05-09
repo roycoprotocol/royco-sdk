5fd85831cd07e44f81c476e5d45ef2be85254075
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-09 13:51:00 -0700

    feat(market-map): Update market -- 98866_0_0xc220ec824047d1438c76e06e81d38856d451f96ce1c16d97969f28e7520f4d7b -- Recipe (0xc220...4d7b)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xc220ec824047d1438c76e06e81d38856d451f96ce1c16d97969f28e7520f4d7b (Market Hash)
    - Request Timestamp: Fri, 09 May 2025 20:51:00 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xc220ec824047d1438c76e06e81d38856d451f96ce1c16d97969f28e7520f4d7b.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xc220ec824047d1438c76e06e81d38856d451f96ce1c16d97969f28e7520f4d7b`,
+  name: `LP nPAYFI-pUSD on Rooster`,
+  description: `Deposit into a Rooster YAP for the nPAYFI-pUSD and deposit the YAP token into this market for incentives.`,
+  is_verified: false,
+});
