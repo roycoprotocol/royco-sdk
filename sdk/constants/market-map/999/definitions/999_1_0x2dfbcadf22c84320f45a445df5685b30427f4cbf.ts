b87e0e4929b7d0b350811e9223b9b98275812383
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-09 10:34:23 -0700

    feat(market-map): Update market -- 999_1_0x2dfbcadf22c84320f45a445df5685b30427f4cbf -- Vault (0x2dfb...4cbf) (#935)
    Market Details:
    - Chain ID: 999
    - Market Type: 1 (Vault)
    - Market ID: 0x2dfbcadf22c84320f45a445df5685b30427f4cbf (Wrapped Vault Address)
    - Request Timestamp: Fri, 09 May 2025 17:34:20 GMT

---
+++ sdk/constants/market-map/999/definitions/999_1_0x2dfbcadf22c84320f45a445df5685b30427f4cbf.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `999_1_0x2dfbcadf22c84320f45a445df5685b30427f4cbf`,
+  name: `Lend HYPE on Sentiment`,
+  description: `When HYPE is supplied to this market, it will deposited into a Sentiment SuperPool on HyperEVM.`,
+  is_verified: false,
+});
