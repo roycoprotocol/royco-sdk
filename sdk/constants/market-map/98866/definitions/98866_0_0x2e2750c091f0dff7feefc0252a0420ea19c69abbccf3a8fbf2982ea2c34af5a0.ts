bc48e87c3996078a94058343904a4a7f713cda03
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-09 13:52:54 -0700

    feat(market-map): Update market -- 98866_0_0x2e2750c091f0dff7feefc0252a0420ea19c69abbccf3a8fbf2982ea2c34af5a0 -- Recipe (0x2e27...f5a0)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x2e2750c091f0dff7feefc0252a0420ea19c69abbccf3a8fbf2982ea2c34af5a0 (Market Hash)
    - Request Timestamp: Fri, 09 May 2025 20:52:53 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x2e2750c091f0dff7feefc0252a0420ea19c69abbccf3a8fbf2982ea2c34af5a0.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x2e2750c091f0dff7feefc0252a0420ea19c69abbccf3a8fbf2982ea2c34af5a0`,
+  name: `LP nETF-pUSD on Rooster`,
+  description: `Deposit into a Rooster YAP for the nETF-pUSD and deposit the YAP token into this market for incentives.`,
+  is_verified: false,
+});
