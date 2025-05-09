054b189b3396631c190ac749de7a10ccd8ef019f
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-09 13:55:52 -0700

    feat(market-map): Update market -- 98866_0_0xa319c980c19e1843d5e5a03c955bf6fb40e8cd1355f75841cef2f0285029a136 -- Recipe (0xa319...a136) (#941)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xa319c980c19e1843d5e5a03c955bf6fb40e8cd1355f75841cef2f0285029a136 (Market Hash)
    - Request Timestamp: Fri, 09 May 2025 20:55:51 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xa319c980c19e1843d5e5a03c955bf6fb40e8cd1355f75841cef2f0285029a136.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xa319c980c19e1843d5e5a03c955bf6fb40e8cd1355f75841cef2f0285029a136`,
+  name: `LP ETH-pUSD on Rooster`,
+  description: `Deposit into a Rooster YAP for the ETH-pUSD and deposit the YAP token into this market for incentives.`,
+  is_verified: false,
+});
