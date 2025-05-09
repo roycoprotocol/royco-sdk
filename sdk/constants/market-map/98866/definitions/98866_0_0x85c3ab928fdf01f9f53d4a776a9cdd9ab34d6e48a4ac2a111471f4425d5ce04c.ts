5cf8cb499321dc4bf1406e1f2dab4a10496e19de
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-08 20:21:14 -0700

    feat(market-map): Update market -- 98866_0_0x85c3ab928fdf01f9f53d4a776a9cdd9ab34d6e48a4ac2a111471f4425d5ce04c -- Recipe (0x85c3...e04c)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x85c3ab928fdf01f9f53d4a776a9cdd9ab34d6e48a4ac2a111471f4425d5ce04c (Market Hash)
    - Request Timestamp: Fri, 09 May 2025 03:21:14 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x85c3ab928fdf01f9f53d4a776a9cdd9ab34d6e48a4ac2a111471f4425d5ce04c.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x85c3ab928fdf01f9f53d4a776a9cdd9ab34d6e48a4ac2a111471f4425d5ce04c`,
+  name: `Deposit nINSTO into Royco`,
+  description: `Deposit into this market to earn nINSTO base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
