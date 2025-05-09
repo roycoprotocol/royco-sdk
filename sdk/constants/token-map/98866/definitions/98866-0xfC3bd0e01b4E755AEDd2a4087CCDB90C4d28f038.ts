547b125a59975f9c7318263ad24d970ccc8778d6
bharatvish <bharat@waymont.co>
2025-05-09 09:32:08 -0700

    Create 98866-0xfC3bd0e01b4E755AEDd2a4087CCDB90C4d28f038.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0xfC3bd0e01b4E755AEDd2a4087CCDB90C4d28f038.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xfC3bd0e01b4E755AEDd2a4087CCDB90C4d28f038",
+  chain_id: 98866,
+  contract_address: "0xfC3bd0e01b4E755AEDd2a4087CCDB90C4d28f038",
+  name: "Rooster YAP-nALPHA-pUSD-4 ",
+  symbol: "YAP-nALPHA-pUSD-4",
+  image:
+    "https://pbs.twimg.com/profile_images/1870064500474347520/a8gJtOSE_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "token",
+});
