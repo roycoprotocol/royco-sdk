6ebf768738d71dbdb9b9b3fac4292b6aea2c37f5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-09 20:58:08 -0700

    feat(market-map): Update market -- 999_0_0x5994273024bcdd07f91cd745f157335b5f166d87e6c28d62601a7a3346879cfc -- Recipe (0x5994...9cfc)
    Market Details:
    - Chain ID: 999
    - Market Type: 0 (Recipe)
    - Market ID: 0x5994273024bcdd07f91cd745f157335b5f166d87e6c28d62601a7a3346879cfc (Market Hash)
    - Request Timestamp: Sat, 10 May 2025 03:58:07 GMT

---
+++ sdk/constants/market-map/999/definitions/999_0_0x5994273024bcdd07f91cd745f157335b5f166d87e6c28d62601a7a3346879cfc.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `999_0_0x5994273024bcdd07f91cd745f157335b5f166d87e6c28d62601a7a3346879cfc`,
+  name: `Deposit USR to Harmonix Finance`,
+  description: `Depositing USR to earn Delta Neutral Yield on Hypercore and Harmonix Points ( $400k worth of points @40M FDV) and further point within Hyperevm ecosystem like Felix and Looped Hype, Hyperswap`,
+  is_verified: false,
+});
