131a538c2b4eba411abfa66571b4749e137e4c32
bharatvish <bharat@waymont.co>
2025-05-12 16:48:57 -0700

    Create 999-0x934a7fE85f128c6E1C8D638795D221ad0F27B401.ts

---
+++ sdk/constants/token-map/999/definitions/999-0x934a7fE85f128c6E1C8D638795D221ad0F27B401.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0x934a7fE85f128c6E1C8D638795D221ad0F27B401",
+  chain_id: 999,
+  contract_address: "0x934a7fE85f128c6E1C8D638795D221ad0F27B401",
+  name: "HypurrFi Royco Points",
+  symbol: "PRRRP",
+  image:
+    "https://pbs.twimg.com/profile_images/1882841326347005953/vewoJ4Vl_400x400.png",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "point",
+});
