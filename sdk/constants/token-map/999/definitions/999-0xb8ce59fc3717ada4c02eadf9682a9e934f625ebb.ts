0ebd523b7015952b06c2f7e62275fb6b03dfe2b8
GitHub Action <action@github.com>
2025-05-12 20:51:56 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/999/definitions/999-0xb8ce59fc3717ada4c02eadf9682a9e934f625ebb.ts
+++ sdk/constants/token-map/999/definitions/999-0xb8ce59fc3717ada4c02eadf9682a9e934f625ebb.ts
@@ -1,13 +1,12 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "999-0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+  id: "999-0xb8ce59fc3717ada4c02eadf9682a9e934f625ebb",
   chain_id: 999,
-  contract_address: "0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+  contract_address: "0xb8ce59fc3717ada4c02eadf9682a9e934f625ebb",
   name: "USDT0",
   symbol: "USDT0",
-  image:
-    "https://cdn.worldvectorlogo.com/logos/tether.svg",
+  image: "https://cdn.worldvectorlogo.com/logos/tether.svg",
   decimals: 18,
   source: "coingecko",
   search_id: "tether",
