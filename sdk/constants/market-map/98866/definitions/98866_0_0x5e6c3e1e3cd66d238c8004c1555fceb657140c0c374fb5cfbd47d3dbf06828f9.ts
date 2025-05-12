47b1e69482d0cf3b3a8112a9e9a44e34b825fec3
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-12 01:59:11 -0700

    feat(market-map): Update market -- 98866_0_0x5e6c3e1e3cd66d238c8004c1555fceb657140c0c374fb5cfbd47d3dbf06828f9 -- Recipe (0x5e6c...28f9)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x5e6c3e1e3cd66d238c8004c1555fceb657140c0c374fb5cfbd47d3dbf06828f9 (Market Hash)
    - Request Timestamp: Mon, 12 May 2025 08:59:10 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x5e6c3e1e3cd66d238c8004c1555fceb657140c0c374fb5cfbd47d3dbf06828f9.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x5e6c3e1e3cd66d238c8004c1555fceb657140c0c374fb5cfbd47d3dbf06828f9`,
+  name: `Deposit RST`,
+  description: `Deposit RST`,
+  is_verified: false,
+});
