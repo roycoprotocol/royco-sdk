f42a61539eeafd51fd0a729813f2d6b3aaa413ce
bharatvish <bharat@waymont.co>
2025-05-12 16:41:45 -0700

    Create 999-0x80518E238762B9725F0f5C0a0a8525ceE98d71d2.ts

---
+++ sdk/constants/token-map/999/definitions/999-0x80518E238762B9725F0f5C0a0a8525ceE98d71d2.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0x80518E238762B9725F0f5C0a0a8525ceE98d71d2",
+  chain_id: 999,
+  contract_address: "0x80518E238762B9725F0f5C0a0a8525ceE98d71d2",
+  name: "Sentiment Royco Points",
+  symbol: "SRP",
+  image:
+    "https://pbs.twimg.com/profile_images/1777846348735537152/7-y3mdE0_400x400.jpg01",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "point",
+});
