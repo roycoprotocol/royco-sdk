663eceb99f250824ab6e4e03bf31785482f04ba4
bharatvish <bharat@waymont.co>
2025-05-12 16:49:48 -0700

    Create 999-0xB5A8936A216a7B4cBc5550532B58E565A90893c8.ts

---
+++ sdk/constants/token-map/999/definitions/999-0xB5A8936A216a7B4cBc5550532B58E565A90893c8.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0xB5A8936A216a7B4cBc5550532B58E565A90893c8",
+  chain_id: 999,
+  contract_address: "0xB5A8936A216a7B4cBc5550532B58E565A90893c8",
+  name: "Harmonix Royco Points",
+  symbol: "HXRP",
+  image:
+    "https://pbs.twimg.com/profile_images/1795360456686837760/dAl7G6dh_400x400.png",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "point",
+});
