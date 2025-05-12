9c96ca38d81c851d0e802e9c21e28c47d8696c63
bharatvish <bharat@waymont.co>
2025-05-12 13:35:49 -0700

    Create 98866-0x11113Ff3a60C2450F4b22515cB760417259eE94B.ts.ts

---
+++ sdk/constants/token-map/98866-0x11113Ff3a60C2450F4b22515cB760417259eE94B.ts.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x11113Ff3a60C2450F4b22515cB760417259eE94B",
+  chain_id: 98866,
+  contract_address: "0x11113Ff3a60C2450F4b22515cB760417259eE94B",
+  name: "Nest Basis Vault",
+  symbol: "nBASIS",
+  image: "https://app.nest.credit/images/nest-egg-vault.svg",
+  decimals: 6,
+  source: "plume",
+  search_id: "nest-basis-vault",
+  type: "token",
+});
