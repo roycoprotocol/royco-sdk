80bb9e7b97cb627f0618c6b743a8fa9dea21341e
bharatvish <bharat@waymont.co>
2025-05-12 13:48:01 -0700

    Create 999-0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb.ts

---
+++ sdk/constants/token-map/999/definitions/999-0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+  chain_id: 999,
+  contract_address: "0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+  name: "USDT0",
+  symbol: "USDT0",
+  image:
+    "https://cdn.worldvectorlogo.com/logos/tether.svg",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "tether",
+  type: "token",
+});
