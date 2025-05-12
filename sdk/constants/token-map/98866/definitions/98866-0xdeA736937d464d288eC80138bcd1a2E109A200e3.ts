271d97c8625e777bf8780cfedb62aa1a7055cb7a
bharatvish <bharat@waymont.co>
2025-05-12 15:33:38 -0700

    Create 98866-0xdeA736937d464d288eC80138bcd1a2E109A200e3.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0xdeA736937d464d288eC80138bcd1a2E109A200e3.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0xdeA736937d464d288eC80138bcd1a2E109A200e3",
+  chain_id: 98866,
+  contract_address: "0xdeA736937d464d288eC80138bcd1a2E109A200e3",
+  name: "Nest ETF Vault",
+  symbol: "nETF",
+  image: "https://app.nest.credit/images/nest-egg-vault.svg",
+  decimals: 6,
+  source: "plume",
+  search_id: "nest-etf-vault",
+  type: "token",
+});
