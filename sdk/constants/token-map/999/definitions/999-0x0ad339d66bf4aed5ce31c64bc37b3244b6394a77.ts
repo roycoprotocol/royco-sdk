6c6fdb20b798fb132850aad1ef6b7bc4b131e6e1
GitHub Action <action@github.com>
2025-05-13 16:48:08 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/999/definitions/999-0x0ad339d66bf4aed5ce31c64bc37b3244b6394a77.ts
+++ sdk/constants/token-map/999/definitions/999-0x0ad339d66bf4aed5ce31c64bc37b3244b6394a77.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "999-0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77",
+  id: "999-0x0ad339d66bf4aed5ce31c64bc37b3244b6394a77",
   chain_id: 999,
-  contract_address: "0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77",
+  contract_address: "0x0ad339d66bf4aed5ce31c64bc37b3244b6394a77",
   name: "Resolv USR",
   symbol: "USR",
   image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32873.png",
