bd6c377b6164872d0a2763509f8cd09b962e5210
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-12 17:29:08 -0700

    feat(market-map): Update market -- 98866_0_0x499e360069408b0958f44195cd344a8b54b865529e34581a4a9ac95fc5d00805 -- Recipe (0x499e...0805) (#952)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x499e360069408b0958f44195cd344a8b54b865529e34581a4a9ac95fc5d00805 (Market Hash)
    - Request Timestamp: Tue, 13 May 2025 00:29:06 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x499e360069408b0958f44195cd344a8b54b865529e34581a4a9ac95fc5d00805.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x499e360069408b0958f44195cd344a8b54b865529e34581a4a9ac95fc5d00805`,
+  name: `Stake ETH through superOETH`,
+  description: `This vault stakes ETH through Origin Protocol&#x27;s Super OETH (superOETHp) and wraps it into a 4626 tokenized vault. Yield is distributed daily via price increase. Learn more at https:&#x2F;&#x2F;docs.originprotocol.com&#x2F;protocol&#x2F;super-oeth`,
+  is_verified: false,
+});
