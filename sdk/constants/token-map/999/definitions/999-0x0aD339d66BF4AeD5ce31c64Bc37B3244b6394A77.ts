8cbe25c153967cc557832285a41f0fd85407d3eb
bharatvish <bharat@waymont.co>
2025-05-13 09:44:19 -0700

    Create 999-0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77.ts

---
+++ sdk/constants/token-map/999/definitions/999-0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77",
+  chain_id: 999,
+  contract_address: "0x0aD339d66BF4AeD5ce31c64Bc37B3244b6394A77",
+  name: "Resolv USR",
+  symbol: "USR",
+  image: "https://s2.coinmarketcap.com/static/img/coins/64x64/32873.png",
+  decimals: 18,
+  source: "coinmarketcap",
+  search_id: "32873",
+  type: "token",
+});
