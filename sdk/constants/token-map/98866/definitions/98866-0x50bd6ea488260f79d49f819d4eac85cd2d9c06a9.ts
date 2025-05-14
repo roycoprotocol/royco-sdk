968d2b44968c8a46b22aded96d06e8f963759f0d
GitHub Action <action@github.com>
2025-05-14 17:34:30 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/definitions/98866-0x50bd6ea488260f79d49f819d4eac85cd2d9c06a9.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x50bd6ea488260f79d49f819d4eac85cd2d9c06a9.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98866-0x50BD6Ea488260f79D49f819D4EAC85Cd2D9C06a9",
+  id: "98866-0x50bd6ea488260f79d49f819d4eac85cd2d9c06a9",
   chain_id: 98866,
-  contract_address: "0x50BD6Ea488260f79D49f819D4EAC85Cd2D9C06a9",
+  contract_address: "0x50bd6ea488260f79d49f819d4eac85cd2d9c06a9",
   name: "RST0002-UAE-DUBAI-DAMAC Towers By Paramounts",
   symbol: "RST0002DB",
   image: "https://app.nest.credit/images/nest-egg-vault.svg",
