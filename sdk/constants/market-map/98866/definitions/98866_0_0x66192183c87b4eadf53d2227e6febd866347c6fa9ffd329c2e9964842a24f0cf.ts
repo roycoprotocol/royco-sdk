c4b39a64adb159b821fa50624dcb3319579ac2fe
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-14 11:28:40 -0700

    feat(market-map): Update market -- 98866_0_0x66192183c87b4eadf53d2227e6febd866347c6fa9ffd329c2e9964842a24f0cf -- Recipe (0x6619...f0cf)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x66192183c87b4eadf53d2227e6febd866347c6fa9ffd329c2e9964842a24f0cf (Market Hash)
    - Request Timestamp: Wed, 14 May 2025 18:28:40 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x66192183c87b4eadf53d2227e6febd866347c6fa9ffd329c2e9964842a24f0cf.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x66192183c87b4eadf53d2227e6febd866347c6fa9ffd329c2e9964842a24f0cf`,
+  name: `Deposit nPAYFI`,
+  description: `Deposit into this market to earn nPAYFI base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
