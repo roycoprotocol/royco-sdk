68c6ecca632c433e5e9fb223d3123da4934c330b
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-14 09:19:41 -0700

    feat(market-map): Update market -- 98866_0_0x6db65a4821684d2db2f18026eb55c1e7c84232def8eae15527a3928affa8b175 -- Recipe (0x6db6...b175)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x6db65a4821684d2db2f18026eb55c1e7c84232def8eae15527a3928affa8b175 (Market Hash)
    - Request Timestamp: Wed, 14 May 2025 16:19:41 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x6db65a4821684d2db2f18026eb55c1e7c84232def8eae15527a3928affa8b175.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x6db65a4821684d2db2f18026eb55c1e7c84232def8eae15527a3928affa8b175`,
+  name: `Deposit nETF into Royco`,
+  description: `Deposit into this market to earn nETF base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
