723c1590ec1de00823572d3ea94fb8c0338fe0f2
bharatvish <bharat@waymont.co>
2025-05-14 11:30:06 -0700

    Update 98866-0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98.ts

--- sdk/constants/token-map/98866/definitions/98866-0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98.ts
@@ -7,7 +7,7 @@ export default defineToken({
   name: "Nucleus op-nBASIS",
   symbol: "op-nBASIS",
   image:
-    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+    "https://assets.plume.org/images/logos/nest/nBASIS/nBASIS-token.svg",
   decimals: 6,
   source: "nucleus",
   search_id: "0x1caad1a4028f80e2f142dbae46e6af5bb78e7d98",
