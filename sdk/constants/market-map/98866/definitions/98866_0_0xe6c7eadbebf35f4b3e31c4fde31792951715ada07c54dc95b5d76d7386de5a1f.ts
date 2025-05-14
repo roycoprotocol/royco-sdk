3f22e9b9618e79b43170a5887a04c73ff8514e66
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-14 08:56:37 -0700

    feat(market-map): Update market -- 98866_0_0xe6c7eadbebf35f4b3e31c4fde31792951715ada07c54dc95b5d76d7386de5a1f -- Recipe (0xe6c7...5a1f) (#960)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xe6c7eadbebf35f4b3e31c4fde31792951715ada07c54dc95b5d76d7386de5a1f (Market Hash)
    - Request Timestamp: Wed, 14 May 2025 15:56:35 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xe6c7eadbebf35f4b3e31c4fde31792951715ada07c54dc95b5d76d7386de5a1f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xe6c7eadbebf35f4b3e31c4fde31792951715ada07c54dc95b5d76d7386de5a1f`,
+  name: `Deposit nALPHA into Royco`,
+  description: `Deposit into this market to earn nALPHA base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
