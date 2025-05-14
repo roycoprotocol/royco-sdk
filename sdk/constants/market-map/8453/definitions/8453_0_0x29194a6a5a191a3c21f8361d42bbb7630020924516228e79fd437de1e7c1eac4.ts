b58c4a4559c640fd99277cf7136acffa3bf02b51
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-14 00:36:48 -0700

    feat(market-map): Update market -- 8453_0_0x29194a6a5a191a3c21f8361d42bbb7630020924516228e79fd437de1e7c1eac4 -- Recipe (0x2919...eac4)
    Market Details:
    - Chain ID: 8453
    - Market Type: 0 (Recipe)
    - Market ID: 0x29194a6a5a191a3c21f8361d42bbb7630020924516228e79fd437de1e7c1eac4 (Market Hash)
    - Request Timestamp: Wed, 14 May 2025 07:36:48 GMT

---
+++ sdk/constants/market-map/8453/definitions/8453_0_0x29194a6a5a191a3c21f8361d42bbb7630020924516228e79fd437de1e7c1eac4.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `8453_0_0x29194a6a5a191a3c21f8361d42bbb7630020924516228e79fd437de1e7c1eac4`,
+  name: `Deposit usdc to get rewards`,
+  description: `Deposit usdc to get rewards`,
+  is_verified: false,
+});
