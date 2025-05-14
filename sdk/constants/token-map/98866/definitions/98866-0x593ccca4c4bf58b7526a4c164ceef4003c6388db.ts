924d87d5a134c9b19d62f1026b0d80b3f4e80139
bharatvish <bharat@waymont.co>
2025-05-14 11:25:22 -0700

    Update 98866-0x593ccca4c4bf58b7526a4c164ceef4003c6388db.ts

--- sdk/constants/token-map/98866/definitions/98866-0x593ccca4c4bf58b7526a4c164ceef4003c6388db.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x593ccca4c4bf58b7526a4c164ceef4003c6388db.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x593ccca4c4bf58b7526a4c164ceef4003c6388db",
   name: "Nest Alpha Vault",
   symbol: "nALPHA",
-  image: "https://app.nest.credit/images/nest-egg-vault.svg",
+  image: "https://assets.plume.org/images/logos/nest/nALPHA/nALPHA-token.svg",
   decimals: 6,
   source: "plume",
   search_id: "nest-alpha-vault",
