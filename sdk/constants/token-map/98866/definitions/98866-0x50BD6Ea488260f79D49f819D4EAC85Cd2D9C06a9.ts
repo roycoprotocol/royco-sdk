af4f3ca0c3f1abd65b2499e5de92e443a01ddf39
bharatvish <bharat@waymont.co>
2025-05-14 10:27:10 -0700

    Create 98866-0x50BD6Ea488260f79D49f819D4EAC85Cd2D9C06a9.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x50BD6Ea488260f79D49f819D4EAC85Cd2D9C06a9.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x50BD6Ea488260f79D49f819D4EAC85Cd2D9C06a9",
+  chain_id: 98866,
+  contract_address: "0x50BD6Ea488260f79D49f819D4EAC85Cd2D9C06a9",
+  name: "RST0002-UAE-DUBAI-DAMAC Towers By Paramounts",
+  symbol: "RST0002DB",
+  image: "https://app.nest.credit/images/nest-egg-vault.svg",
+  decimals: 18,
+  source: "",
+  search_id: "external",
+  type: "token",
+});
