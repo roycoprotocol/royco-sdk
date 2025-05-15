aca5609a1032c854ede3c4867264f94166fb2760
GitHub Action <action@github.com>
2025-05-15 18:23:45 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/8453/definitions/8453-0x99473274f8ddde2d736f548d2d6f916bf00dd9f5.ts
+++ sdk/constants/token-map/8453/definitions/8453-0x99473274f8ddde2d736f548d2d6f916bf00dd9f5.ts
@@ -1,12 +1,13 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "8453-0x99473274F8DDdE2D736F548D2D6F916BF00dD9f5",
+  id: "8453-0x99473274f8ddde2d736f548d2d6f916bf00dd9f5",
   chain_id: 8453,
-  contract_address: "0x99473274F8DDdE2D736F548D2D6F916BF00dD9f5",
+  contract_address: "0x99473274f8ddde2d736f548d2d6f916bf00dd9f5",
   name: "Volatile AMM - WETH/TMAI",
   symbol: "vAMM-WETH/TMAI",
-  image: "https://assets.coingecko.com/coins/images/37707/standard/Mascot_200x200.png?1733173189",
+  image:
+    "https://assets.coingecko.com/coins/images/37707/standard/Mascot_200x200.png?1733173189",
   decimals: 18,
   source: "",
   search_id: "",
