f5aac5c2ff75a8da8dac9d108811349de5e1a876
bharatvish <bharat@waymont.co>
2025-05-15 11:04:14 -0700

    Create 8453-0x99473274F8DDdE2D736F548D2D6F916BF00dD9f5.ts

---
+++ sdk/constants/token-map/8453/definitions/8453-0x99473274F8DDdE2D736F548D2D6F916BF00dD9f5.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "8453-0x99473274F8DDdE2D736F548D2D6F916BF00dD9f5",
+  chain_id: 8453,
+  contract_address: "0x99473274F8DDdE2D736F548D2D6F916BF00dD9f5",
+  name: "Volatile AMM - WETH/TMAI",
+  symbol: "vAMM-WETH/TMAI",
+  image: "https://assets.coingecko.com/coins/images/37707/standard/Mascot_200x200.png?1733173189",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "token",
+});
