5f10901c4e12c33f2446eef094515eb8bff1da92
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-15 05:50:53 -0700

    feat(market-map): Update market -- 98866_0_0x7626d64b7f60786b715a68bc6a0c78fddb989a883d26747112a88a70453eacba -- Recipe (0x7626...acba) (#965)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x7626d64b7f60786b715a68bc6a0c78fddb989a883d26747112a88a70453eacba (Market Hash)
    - Request Timestamp: Thu, 15 May 2025 12:50:51 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x7626d64b7f60786b715a68bc6a0c78fddb989a883d26747112a88a70453eacba.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x7626d64b7f60786b715a68bc6a0c78fddb989a883d26747112a88a70453eacba`,
+  name: `Lend pUSD on Multichainz Protocol`,
+  description: `Deposit pUSD into Multichainz pool on Plume Chain`,
+  is_verified: false,
+});
