1c0312941fb1f87333213e8e16eff5827c0ae5c5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-16 00:22:32 -0700

    feat(market-map): Update market -- 11155111_0_0xe7ce19eb3e3990861bc5187a8f26b29e54d3bf3f96924bad198c87f08d61168f -- Recipe (0xe7ce...168f) (#969)
    Market Details:
    - Chain ID: 11155111
    - Market Type: 0 (Recipe)
    - Market ID: 0xe7ce19eb3e3990861bc5187a8f26b29e54d3bf3f96924bad198c87f08d61168f (Market Hash)
    - Request Timestamp: Fri, 16 May 2025 07:22:30 GMT

---
+++ sdk/constants/market-map/11155111/definitions/11155111_0_0xe7ce19eb3e3990861bc5187a8f26b29e54d3bf3f96924bad198c87f08d61168f.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `11155111_0_0xe7ce19eb3e3990861bc5187a8f26b29e54d3bf3f96924bad198c87f08d61168f`,
+  name: `IPs create test 1`,
+  description: `IPs create test 1`,
+  is_verified: false,
+});
