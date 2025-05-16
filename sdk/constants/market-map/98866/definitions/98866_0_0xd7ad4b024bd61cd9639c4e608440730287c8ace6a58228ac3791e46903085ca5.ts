9d3063a1aa652c69ac305d3a24452fd9cfbab9f4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-15 18:59:44 -0700

    feat(market-map): Update market -- 98866_0_0xd7ad4b024bd61cd9639c4e608440730287c8ace6a58228ac3791e46903085ca5 -- Recipe (0xd7ad...5ca5)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xd7ad4b024bd61cd9639c4e608440730287c8ace6a58228ac3791e46903085ca5 (Market Hash)
    - Request Timestamp: Fri, 16 May 2025 01:59:43 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xd7ad4b024bd61cd9639c4e608440730287c8ace6a58228ac3791e46903085ca5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xd7ad4b024bd61cd9639c4e608440730287c8ace6a58228ac3791e46903085ca5`,
+  name: `CYCLEX RWA Tokenization Fund Airdrop`,
+  description: `CYCLEX, a leader in real-world asset (RWA) tokenization, launches the CYCLEX RWA Tokenization Fund Airdrop on Royco Protocol’s IAMs to drive DeFi adoption. It promotes four funds: CZTF, CATF, CMTF, and CPTF. Invest USDT (or equivalent) on Ethereum, Arbitrum, BNB Chain, BEVN, or Merlin Chain for a 22% annualized yield with a 3-month lockup. Complete lockup to earn principal, yield, and points for CYCLEx’s C*XP token airdrop; early redemption forfeits both. Tokens are distributed by points rankings. Visit https:&#x2F;&#x2F;cyclex.cc&#x2F;.`,
+  is_verified: false,
+});
