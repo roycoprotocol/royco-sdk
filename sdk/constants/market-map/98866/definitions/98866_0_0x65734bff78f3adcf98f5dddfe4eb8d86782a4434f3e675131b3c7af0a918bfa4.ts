bc01329d1350fac289b8555f5e53175514195379
bharatvish <bharat@waymont.co>
2025-05-19 16:24:59 -0700

    Update 98866_0_0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `98866_0_0x65734bff78f3adcf98f5dddfe4eb8d86782a4434f3e675131b3c7af0a918bfa4`,
   name: `Supply pUSD on Mystic Finance`,
-  description: `When you supply pUSD, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  description: `When you supply pUSD, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit. Depositors will be earning one Mystic Point per dollar supplied per day.`,
   is_verified: false,
 });
