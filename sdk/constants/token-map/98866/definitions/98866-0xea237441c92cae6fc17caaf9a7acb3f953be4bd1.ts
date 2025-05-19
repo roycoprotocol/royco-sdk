2e3fe58eafe1dc1be2612c74e0aec90595946aee
bharatvish <bharat@waymont.co>
2025-05-19 15:13:26 -0700

    Update 98866-0xea237441c92cae6fc17caaf9a7acb3f953be4bd1.ts

--- sdk/constants/token-map/98866/definitions/98866-0xea237441c92cae6fc17caaf9a7acb3f953be4bd1.ts
+++ sdk/constants/token-map/98866/definitions/98866-0xea237441c92cae6fc17caaf9a7acb3f953be4bd1.ts
@@ -7,7 +7,7 @@ export default defineToken({
   name: "Wrapped Plume",
   symbol: "WPLUME",
   image:
-    "https://token-media.defined.fi/98866_0xea237441c92cae6fc17caaf9a7acb3f953be4bd1_large_ea39d469-3e7d-413e-99a7-9a3eafeea22a.png",
+    "https://assets.plume.org/images/logos/WPLUME/WPLUME-token.svg",
   decimals: 18,
   source: "coinmarketcap",
   search_id: "35364",
