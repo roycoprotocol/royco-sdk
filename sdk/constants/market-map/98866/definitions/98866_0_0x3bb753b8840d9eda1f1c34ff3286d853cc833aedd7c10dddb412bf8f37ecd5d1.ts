5780bcc28fbbaf04ce013e55f5718a5aa7e372fa
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-19 13:16:03 -0700

    feat(market-map): Update market -- 98866_0_0x3bb753b8840d9eda1f1c34ff3286d853cc833aedd7c10dddb412bf8f37ecd5d1 -- Recipe (0x3bb7...d5d1) (#974)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x3bb753b8840d9eda1f1c34ff3286d853cc833aedd7c10dddb412bf8f37ecd5d1 (Market Hash)
    - Request Timestamp: Mon, 19 May 2025 20:16:01 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x3bb753b8840d9eda1f1c34ff3286d853cc833aedd7c10dddb412bf8f37ecd5d1.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x3bb753b8840d9eda1f1c34ff3286d853cc833aedd7c10dddb412bf8f37ecd5d1`,
+  name: `Deposit nETF into Nest`,
+  description: `Deposit into this market to earn nETF base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
