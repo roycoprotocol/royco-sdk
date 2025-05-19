e0e8e71d49f771ede8dff01ebacab451c2c96e7d
bharatvish <bharat@waymont.co>
2025-05-19 16:25:32 -0700

    Update 98866_0_0x8a0989ac3d7df5ec7259e5a0b0bfee646dc8a34b4c9de80790fa9625545bd579.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0x8a0989ac3d7df5ec7259e5a0b0bfee646dc8a34b4c9de80790fa9625545bd579.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0x8a0989ac3d7df5ec7259e5a0b0bfee646dc8a34b4c9de80790fa9625545bd579.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `98866_0_0x8a0989ac3d7df5ec7259e5a0b0bfee646dc8a34b4c9de80790fa9625545bd579`,
   name: `Supply nTBILL on Mystic Finance`,
-  description: `When you supply nTBILL, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  description: `When you supply nTBILL, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit. Depositors will be earning one Mystic Point per dollar supplied per day.`,
   is_verified: false,
 });
