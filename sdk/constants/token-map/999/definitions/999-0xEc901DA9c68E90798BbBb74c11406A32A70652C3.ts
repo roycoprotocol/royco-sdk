95fb833608a28f4af82b0d100b31142fc335d126
bharatvish <bharat@waymont.co>
2025-05-19 10:13:56 -0700

    Create 999-0xEc901DA9c68E90798BbBb74c11406A32A70652C3.ts

---
+++ sdk/constants/token-map/999/definitions/999-0xEc901DA9c68E90798BbBb74c11406A32A70652C3.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0xEc901DA9c68E90798BbBb74c11406A32A70652C3",
+  chain_id: 999,
+  contract_address: "0xEc901DA9c68E90798BbBb74c11406A32A70652C3",
+  name: "Stakestone ETH",
+  symbol: "STONE",
+  image:
+    "https://pbs.twimg.com/profile_images/1665354784000589824/2nq_Ae8e_400x400.jpg",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "ethereum",
+  type: "token",
+});
