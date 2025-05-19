9ad99610c8fe54c4429ac9b9d87352dcc2a3c44d
bharatvish <bharat@waymont.co>
2025-05-19 10:05:02 -0700

    Create 999-0xE6829d9a7eE3040e1276Fa75293Bde931859e8fA.ts

---
+++ sdk/constants/token-map/999/definitions/999-0xE6829d9a7eE3040e1276Fa75293Bde931859e8fA.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0xE6829d9a7eE3040e1276Fa75293Bde931859e8fA",
+  chain_id: 999,
+  contract_address: "0xE6829d9a7eE3040e1276Fa75293Bde931859e8fA",
+  name: "cmETH",
+  symbol: "cmETH",
+  image:
+    "https://pbs.twimg.com/profile_images/1891836818850709509/290NJBzU_400x400.jpg",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "mantle-restaked-eth",
+  type: "token",
+});
