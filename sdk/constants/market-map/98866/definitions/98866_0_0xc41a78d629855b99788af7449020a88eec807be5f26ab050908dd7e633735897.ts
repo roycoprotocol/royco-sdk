ead52941c3ba2295e88efc2ed7c9528cd797db2a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-19 13:10:25 -0700

    feat(market-map): Update market -- 98866_0_0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897 -- Recipe (0xc41a...5897)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897 (Market Hash)
    - Request Timestamp: Mon, 19 May 2025 20:10:24 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897`,
+  name: `Deposit into nALPHA on Nest`,
+  description: `Deposit into this market to earn nALPHA base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
