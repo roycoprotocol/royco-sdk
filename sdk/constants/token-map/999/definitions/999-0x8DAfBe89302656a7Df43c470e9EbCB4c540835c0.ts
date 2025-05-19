f4aa58f13c1408677f6e236609a8eb75e7842d0f
bharatvish <bharat@waymont.co>
2025-05-19 10:12:45 -0700

    Create 999-0x8DAfBe89302656a7Df43c470e9EbCB4c540835c0.ts

---
+++ sdk/constants/token-map/999/definitions/999-0x8DAfBe89302656a7Df43c470e9EbCB4c540835c0.ts
@@ -0,0 +1,15 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "999-0x8DAfBe89302656a7Df43c470e9EbCB4c540835c0",
+  chain_id: 999,
+  contract_address: "0x8DAfBe89302656a7Df43c470e9EbCB4c540835c0",
+  name: "uETH",
+  symbol: "uETH",
+  image:
+    "https://pbs.twimg.com/profile_images/1890184998445047808/qgsh4B97_400x400.jpg",
+  decimals: 18,
+  source: "coingecko",
+  search_id: "ethereum",
+  type: "token",
+});
