e6cee8cc51cddf95066a3436a718b6aa1aea5659
bharatvish <bharat@waymont.co>
2025-05-19 16:24:22 -0700

    Update 98866_0_0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `98866_0_0xb26196c9e8a5a3da90fb034b7398d0f1c0cda132e5d1a6b7f66f98392245e2e5`,
   name: `Supply PLUME on Mystic Finance`,
-  description: `When you supply PLUME, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit.`,
+  description: `When you supply PLUME, it’s automatically deposited into the Mystic Finance and earning incentives and interest. You can withdraw your assets whenever you need, but rewards are forfeited if you withdraw before 90 days after deposit. Depositors will be earning one Mystic Point per dollar supplied per day.`,
   is_verified: false,
 });
