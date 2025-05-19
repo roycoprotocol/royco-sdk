9ed7c4409516212afb5dd3924933d07d0ab06cf8
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-19 13:21:00 -0700

    feat(market-map): Update market -- 98866_0_0x4d2ae1e85a4f4c2314e23833445c0a4cb6c434a1aa382bf5939fb18140bcb709 -- Recipe (0x4d2a...b709) (#976)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x4d2ae1e85a4f4c2314e23833445c0a4cb6c434a1aa382bf5939fb18140bcb709 (Market Hash)
    - Request Timestamp: Mon, 19 May 2025 20:20:58 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x4d2ae1e85a4f4c2314e23833445c0a4cb6c434a1aa382bf5939fb18140bcb709.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x4d2ae1e85a4f4c2314e23833445c0a4cb6c434a1aa382bf5939fb18140bcb709`,
+  name: `Deposit nBASIS into Nest`,
+  description: `Deposit into this market to earn nBASIS base yields as well as PLUME incentives.`,
+  is_verified: false,
+});
