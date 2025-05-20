7e20702df38998a8c817c3c2314d4f52ed1fd202
bharatvish <bharat@waymont.co>
2025-05-20 14:52:52 -0700

    Update 98866_0_0xd7b4af5225fb14fc0f0f7e068faaa03c3d1530f695b60187f74ed7a0e259fa10.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0xd7b4af5225fb14fc0f0f7e068faaa03c3d1530f695b60187f74ed7a0e259fa10.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0xd7b4af5225fb14fc0f0f7e068faaa03c3d1530f695b60187f74ed7a0e259fa10.ts
@@ -2,7 +2,7 @@ import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
   id: `98866_0_0xd7b4af5225fb14fc0f0f7e068faaa03c3d1530f695b60187f74ed7a0e259fa10`,
-  name: `Deposit nCREDIT into Royco`,
+  name: `Deposit into nCREDIT on Nest`,
   description: `Deposit into this market to earn nCREDIT base yields as well as PLUME incentives. Learn more here: (https://app.nest.credit/nest-elixir-vault) (https://docs.plume.org/plume/tokens/plume-usdplume-token)`,
   is_verified: false,
 });
