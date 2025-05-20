f3bdbf35754d4f60ea36b9690eed72134a462560
bharatvish <bharat@waymont.co>
2025-05-20 12:15:38 -0700

    Update 98866-0x50bd6ea488260f79d49f819d4eac85cd2d9c06a9.ts

--- sdk/constants/token-map/98866/definitions/98866-0x50bd6ea488260f79d49f819d4eac85cd2d9c06a9.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x50bd6ea488260f79d49f819d4eac85cd2d9c06a9.ts
@@ -7,7 +7,7 @@ export default defineToken({
   name: "RST0002-UAE-DUBAI-DAMAC Towers By Paramounts",
   symbol: "RST0002DB",
   image:
-    "https://pbs.twimg.com/profile_images/1714842234062094336/ltqq6Fwa_400x400.jpg",
+    "https://pbs.twimg.com/profile_images/1714842234062094336/ltqq6Fwa_400x400.png",
   decimals: 18,
   source: "external",
   search_id: "none",
