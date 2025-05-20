ff8ae872a05d759d0044b21f3564dcbd80a78e68
bharatvish <bharat@waymont.co>
2025-05-20 12:14:18 -0700

    Update 98866-0x11a8d8694b656112d9a94285223772f4aad269fc.ts

--- sdk/constants/token-map/98866/definitions/98866-0x11a8d8694b656112d9a94285223772f4aad269fc.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x11a8d8694b656112d9a94285223772f4aad269fc.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x11a8d8694b656112d9a94285223772f4aad269fc",
   name: "Nest RWA",
   symbol: "nRWA",
-  image: "https://app.nest.credit/images/nest-egg-vault.svg",
+  image: "https://app.nest.credit/images/nest-egg-vault.png",
   decimals: 6,
   source: "plume",
   search_id: "nest-rwa-vault",
