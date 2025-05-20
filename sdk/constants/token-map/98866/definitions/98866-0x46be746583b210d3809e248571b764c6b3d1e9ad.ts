824781d86c593053aadb09264886c512454487aa
bharatvish <bharat@waymont.co>
2025-05-20 12:15:20 -0700

    Update 98866-0x46be746583b210d3809e248571b764c6b3d1e9ad.ts

--- sdk/constants/token-map/98866/definitions/98866-0x46be746583b210d3809e248571b764c6b3d1e9ad.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x46be746583b210d3809e248571b764c6b3d1e9ad.ts
@@ -7,7 +7,7 @@ export default defineToken({
   name: "Nucleus op-nPAYFI",
   symbol: "op-nPAYFI",
   image:
-    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.png",
   decimals: 6,
   source: "nucleus",
   search_id: "0x46be746583b210d3809e248571b764c6b3d1e9ad",
