f3d7e390847a5230d8d374007e83a9f6166e8955
bharatvish <bharat@waymont.co>
2025-05-20 12:18:20 -0700

    Update 98866-0x8337930b8431f10f2644ddf7189c43802f76cb88.ts

--- sdk/constants/token-map/98866/definitions/98866-0x8337930b8431f10f2644ddf7189c43802f76cb88.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x8337930b8431f10f2644ddf7189c43802f76cb88.ts
@@ -7,7 +7,7 @@ export default defineToken({
   name: "Nucleus op-PLUME",
   symbol: "op-PLUME",
   image:
-    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.png",
   decimals: 18,
   source: "nucleus",
   search_id: "0x8337930b8431f10f2644ddf7189c43802f76cb88",
