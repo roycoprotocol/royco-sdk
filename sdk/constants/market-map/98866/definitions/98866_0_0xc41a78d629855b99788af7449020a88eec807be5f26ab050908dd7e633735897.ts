a8622d8ad8f157a8f7a1f0cfc7c4e8ba84f95e9d
bharatvish <bharat@waymont.co>
2025-05-20 14:56:04 -0700

    Update 98866_0_0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897.ts
@@ -2,7 +2,7 @@ import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
   id: `98866_0_0xc41a78d629855b99788af7449020a88eec807be5f26ab050908dd7e633735897`,
-  name: `Deposit into nALPHA on Nest`,
-  description: `Deposit into this market to earn nALPHA base yields as well as PLUME incentives.`,
+  name: `Deposit nALPHA into Nest`,
+  description: `Deposit into this market to earn nALPHA base yields as well as PLUME incentives. Learn more here: (https://app.nest.credit/nest-alpha-vault) (https://docs.plume.org/plume/tokens/plume-usdplume-token)`,
   is_verified: false,
 });
