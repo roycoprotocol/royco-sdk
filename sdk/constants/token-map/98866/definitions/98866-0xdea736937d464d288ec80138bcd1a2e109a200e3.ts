14608162193c63707ab900c97efb7bdfa7e6ebe3
bharatvish <bharat@waymont.co>
2025-05-20 12:36:16 -0700

    Update 98866-0xdea736937d464d288ec80138bcd1a2e109a200e3.ts

--- sdk/constants/token-map/98866/definitions/98866-0xdea736937d464d288ec80138bcd1a2e109a200e3.ts
+++ sdk/constants/token-map/98866/definitions/98866-0xdea736937d464d288ec80138bcd1a2e109a200e3.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0xdea736937d464d288ec80138bcd1a2e109a200e3",
   name: "Nest ETF Vault",
   symbol: "nETF",
-  image: "https://assets.plume.org/images/logos/nest/nETF/nETF-token.svg",
+  image: "https://assets.plume.org/images/logos/nest/nETF/nETF-token.png",
   decimals: 6,
   source: "coingecko",
   search_id: "nest-etf-vault",
