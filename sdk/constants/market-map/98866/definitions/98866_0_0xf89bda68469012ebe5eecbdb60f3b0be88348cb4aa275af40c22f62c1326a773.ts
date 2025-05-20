7e869eff134f07318349ee7121d697d5f7580b4f
bharatvish <bharat@waymont.co>
2025-05-20 13:29:40 -0700

    Update 98866_0_0xf89bda68469012ebe5eecbdb60f3b0be88348cb4aa275af40c22f62c1326a773.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0xf89bda68469012ebe5eecbdb60f3b0be88348cb4aa275af40c22f62c1326a773.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0xf89bda68469012ebe5eecbdb60f3b0be88348cb4aa275af40c22f62c1326a773.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `98866_0_0xf89bda68469012ebe5eecbdb60f3b0be88348cb4aa275af40c22f62c1326a773`,
   name: `Deposit op-nALPHA Into Nucleus`,
-  description: `DDeposit op-nALPHA into Royco for 90 days to earn PLUME( hyperlink:https://docs.plume.org/plume/tokens/plume-usdplume-token) incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
+  description: `DDeposit op-nALPHA into Royco for 90 days to earn PLUME (https://docs.plume.org/plume/tokens/plume-usdplume-token) incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
   is_verified: false,
 });
