3bd3fe315f196adf51c7f6e337fdca5428d724be
bharatvish <bharat@waymont.co>
2025-05-20 12:21:28 -0700

    Update 98866-0xd5c02efe86ebbc03126281e07b4d43f0b1021065.ts

--- sdk/constants/token-map/98866/definitions/98866-0xd5c02efe86ebbc03126281e07b4d43f0b1021065.ts
+++ sdk/constants/token-map/98866/definitions/98866-0xd5c02efe86ebbc03126281e07b4d43f0b1021065.ts
@@ -7,7 +7,7 @@ export default defineToken({
   name: "Rooster YAP-USDT-pUSD-2",
   symbol: "YAP-USDT-pUSD-2",
   image:
-    "https://pbs.twimg.com/profile_images/1870064500474347520/a8gJtOSE_400x400.jpg",
+    "https://pbs.twimg.com/profile_images/1870064500474347520/a8gJtOSE_400x400.png",
   decimals: 18,
   source: "rooster",
   search_id: "0xd5c02efe86ebbc03126281e07b4d43f0b1021065",
