3522ff044a27127a8c282070a654e5b2c43c9f1c
bharatvish <bharat@waymont.co>
2025-05-20 12:14:00 -0700

    Update 98866-0x11113ff3a60c2450f4b22515cb760417259ee94b.ts

--- sdk/constants/token-map/98866/definitions/98866-0x11113ff3a60c2450f4b22515cb760417259ee94b.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x11113ff3a60c2450f4b22515cb760417259ee94b.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x11113ff3a60c2450f4b22515cb760417259ee94b",
   name: "Nest Basis Vault",
   symbol: "nBASIS",
-  image: "https://assets.plume.org/images/logos/nest/nBASIS/nBASIS-token.svg",
+  image: "https://assets.plume.org/images/logos/nest/nBASIS/nBASIS-token.png",
   decimals: 6,
   source: "coingecko",
   search_id: "nest-basis-vault",
