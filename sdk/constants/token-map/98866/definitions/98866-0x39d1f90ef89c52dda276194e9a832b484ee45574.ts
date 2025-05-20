dc601f5552f32780e922ee3c66b0c273e15c99c4
bharatvish <bharat@waymont.co>
2025-05-20 12:14:46 -0700

    Update 98866-0x39d1f90ef89c52dda276194e9a832b484ee45574.ts

--- sdk/constants/token-map/98866/definitions/98866-0x39d1f90ef89c52dda276194e9a832b484ee45574.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x39d1f90ef89c52dda276194e9a832b484ee45574.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x39d1f90ef89c52dda276194e9a832b484ee45574",
   name: "Plume ETH",
   symbol: "pETH",
-  image: "https://peth.plumenetwork.xyz/pETH.svg",
+  image: "https://peth.plumenetwork.xyz/pETH.png",
   decimals: 18,
   source: "coingecko",
   search_id: "ethereum",
