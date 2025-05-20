07b420456ae34ddb23bb055dc626494a19e97ebe
bharatvish <bharat@waymont.co>
2025-05-20 12:18:35 -0700

    Update 98866-0x9fbc367b9bb966a2a537989817a088afcaffdc4c.ts

--- sdk/constants/token-map/98866/definitions/98866-0x9fbc367b9bb966a2a537989817a088afcaffdc4c.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x9fbc367b9bb966a2a537989817a088afcaffdc4c.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0x9fbc367b9bb966a2a537989817a088afcaffdc4c",
   name: "Nest Elixir Vault",
   symbol: "nELIXIR",
-  image: "https://assets.plume.org/images/logos/nest/nELIXIR/nELIXIR-token.svg",
+  image: "https://assets.plume.org/images/logos/nest/nELIXIR/nELIXIR-token.png",
   decimals: 6,
   source: "coingecko",
   search_id: "nest-institutional-core",
