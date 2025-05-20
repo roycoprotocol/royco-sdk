0f8a2c89f753c31fc29d6a788169db099e1712de
bharatvish <bharat@waymont.co>
2025-05-20 09:17:42 -0700

    Create 98866-0x69eaA7E4A04d57E4312A4f959cCf227422EbE941.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x69eaA7E4A04d57E4312A4f959cCf227422EbE941.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x69eaA7E4A04d57E4312A4f959cCf227422EbE941",
+  chain_id: 98866,
+  contract_address: "0x69eaA7E4A04d57E4312A4f959cCf227422EbE941",
+  name: "IziSwap wPLUME/pETH",
+  symbol: "IziSwap wPLUME/pETH",
+  image: "https://pbs.twimg.com/profile_images/1509704804032937991/5qVVZwJj_400x400.jpg",
+  decimals: 18,
+  source: "external",
+  search_id: "",
+  type: "token",
+});
