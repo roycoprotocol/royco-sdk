ca04fbac24c6afa1de2c0ce6c9314e42e4ae04a5
bharatvish <bharat@waymont.co>
2025-05-20 14:26:44 -0700

    Update 98866_0_0x213e01d3919905428ff8fd846e89111d80c81d646556a24e9d96838932532f01.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0x213e01d3919905428ff8fd846e89111d80c81d646556a24e9d96838932532f01.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0x213e01d3919905428ff8fd846e89111d80c81d646556a24e9d96838932532f01.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `98866_0_0x213e01d3919905428ff8fd846e89111d80c81d646556a24e9d96838932532f01`,
   name: `Deposit into nELIXIR on Nest`,
-  description: `WDeposit into this market to earn nELIXIR base yields as well as PLUME incentives. Learn more here: (https://app.nest.credit/nest-elixir-vault) (https://docs.plume.org/plume/tokens/plume-usdplume-token) `,
+  description: `Deposit into this market to earn nELIXIR base yields as well as PLUME incentives. Learn more here: (https://app.nest.credit/nest-elixir-vault) (https://docs.plume.org/plume/tokens/plume-usdplume-token) `,
   is_verified: false,
 });
