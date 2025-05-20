667d91fbb5e7c9d59d394c48a137f845ad43cef6
bharatvish <bharat@waymont.co>
2025-05-20 13:41:31 -0700

    Update 98866_0_0x11c0d2e939d550da04ff69f76b699885b9d62d942dbf9465c6eb2d64fcac0939.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0x11c0d2e939d550da04ff69f76b699885b9d62d942dbf9465c6eb2d64fcac0939.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0x11c0d2e939d550da04ff69f76b699885b9d62d942dbf9465c6eb2d64fcac0939.ts
@@ -2,7 +2,7 @@ import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
   id: `98866_0_0x11c0d2e939d550da04ff69f76b699885b9d62d942dbf9465c6eb2d64fcac0939`,
-  name: `Deposit op-nBASIS Into Royco for 90 Days`,
-  description: `Deposit op-nBASIS into Royco for 90 days to earn Plume incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
+  name: `Deposit op-nBASIS Into Into Nucleus`,
+  description: `Deposit op-nBASIS into Royco for 90 days to earn PLUME (https://docs.plume.org/plume/tokens/plume-usdplume-token) incentives. Users may withdraw early at anytime but must forfeit their incentives. `,
   is_verified: false,
 });
