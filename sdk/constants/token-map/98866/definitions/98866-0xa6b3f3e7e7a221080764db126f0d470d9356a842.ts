c002a08d958fc10df0affc48a889913594cf9583
bharatvish <bharat@waymont.co>
2025-05-20 12:19:34 -0700

    Update 98866-0xa6b3f3e7e7a221080764db126f0d470d9356a842.ts

--- sdk/constants/token-map/98866/definitions/98866-0xa6b3f3e7e7a221080764db126f0d470d9356a842.ts
+++ sdk/constants/token-map/98866/definitions/98866-0xa6b3f3e7e7a221080764db126f0d470d9356a842.ts
@@ -7,7 +7,7 @@ export default defineToken({
   name: "Nucleus op-nETF",
   symbol: "op-nETF",
   image:
-    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.jpg",
+    "https://pbs.twimg.com/profile_images/1821310155075514368/0Kjodwv3_400x400.png",
   decimals: 6,
   source: "nucleus",
   search_id: "0xa6b3f3e7e7a221080764db126f0d470d9356a842",
