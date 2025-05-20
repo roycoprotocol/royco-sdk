4cf312f4bf49df9d28f4963ee9219cf62b571523
bharatvish <bharat@waymont.co>
2025-05-20 13:58:37 -0700

    Update 98866_0_0xde1264075f8b9ab937cd2e26278395257dfca0984803effe4d93ffb7f01bae26.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0xde1264075f8b9ab937cd2e26278395257dfca0984803effe4d93ffb7f01bae26.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0xde1264075f8b9ab937cd2e26278395257dfca0984803effe4d93ffb7f01bae26.ts
@@ -3,6 +3,6 @@ import { defineMarket } from "@/sdk/constants";
 export default defineMarket({
   id: `98866_0_0xde1264075f8b9ab937cd2e26278395257dfca0984803effe4d93ffb7f01bae26`,
   name: `Deposit op-nPAYFI Into Royco for 90 Days`,
-  description: `Deposit op-nPAYFI into Royco for 90 days to earn Plume incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
+  description: `Deposit op-nPAYFI into Royco for 90 days to earn Plume incentives. Users may withdraw early at anytime but must forfeit their incentives. Learn more about PLUME: (https://docs.plume.org/plume/tokens/plume-usdplume-token).`,
   is_verified: false,
 });
