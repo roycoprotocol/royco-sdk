b75c7a2aaea15d8c28d8258f4303c7cb4baacb36
bharatvish <bharat@waymont.co>
2025-05-21 08:38:48 -0700

    Update 98866-0xca59ca09e5602fae8b629dee83ffa819741f14be.ts

--- sdk/constants/token-map/98866/definitions/98866-0xca59ca09e5602fae8b629dee83ffa819741f14be.ts
+++ sdk/constants/token-map/98866/definitions/98866-0xca59ca09e5602fae8b629dee83ffa819741f14be.ts
@@ -6,7 +6,7 @@ export default defineToken({
   contract_address: "0xca59ca09e5602fae8b629dee83ffa819741f14be",
   name: "Wrapped ETH",
   symbol: "wETH",
-  image: "https://zengo.com/wp-content/uploads/wETH_desktop.png",
+  image: "https://usethebitcoin.com/wp-content/uploads/2021/11/wETH-.png",
   decimals: 18,
   source: "coinmarketcap",
   search_id: "1027",
