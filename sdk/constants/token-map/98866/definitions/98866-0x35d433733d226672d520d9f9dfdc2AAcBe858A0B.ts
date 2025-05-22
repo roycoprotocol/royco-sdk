fe456da03c49128901509a9b9df664f79ed2404d
bharatvish <bharat@waymont.co>
2025-05-22 09:04:28 -0700

    Create 98866-0x35d433733d226672d520d9f9dfdc2AAcBe858A0B.ts

---
+++ sdk/constants/token-map/98866/definitions/98866-0x35d433733d226672d520d9f9dfdc2AAcBe858A0B.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x35d433733d226672d520d9f9dfdc2AAcBe858A0B",
+  chain_id: 98866,
+  contract_address: "0x35d433733d226672d520d9f9dfdc2AAcBe858A0B",
+  name: "zToken Reward",
+  symbol: "nBASIS",
+  image: "https://img.cryptorank.io/coins/multichain_z1735900735195.png",
+  decimals: 18,
+  source: "",
+  search_id: "",
+  type: "token",
+});
