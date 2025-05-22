909cbf87f4993f38ad02802579c88790a3bad60b
GitHub Action <action@github.com>
2025-05-22 16:21:36 +0000

    feat(chore): update generated files

--- sdk/constants/token-map/98866/definitions/98866-0x35d433733d226672d520d9f9dfdc2aacbe858a0b.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x35d433733d226672d520d9f9dfdc2aacbe858a0b.ts
@@ -1,9 +1,9 @@
 import { defineToken } from "@/sdk/constants";
 
 export default defineToken({
-  id: "98866-0x35d433733d226672d520d9f9dfdc2AAcBe858A0B",
+  id: "98866-0x35d433733d226672d520d9f9dfdc2aacbe858a0b",
   chain_id: 98866,
-  contract_address: "0x35d433733d226672d520d9f9dfdc2AAcBe858A0B",
+  contract_address: "0x35d433733d226672d520d9f9dfdc2aacbe858a0b",
   name: "zToken Reward",
   symbol: "nBASIS",
   image: "https://img.cryptorank.io/coins/multichain_z1735900735195.png",
