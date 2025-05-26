487f98e0b8e4cb425fb3884843239244b5968a20
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-26 02:47:51 -0700

    feat(market-map): Update market -- 98866_0_0x6a9d07fe95736dd37d9ed2e1d0678c899447f79dd7982997a9b129e0212b2766 -- Recipe (0x6a9d...2766) (#988)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x6a9d07fe95736dd37d9ed2e1d0678c899447f79dd7982997a9b129e0212b2766 (Market Hash)
    - Request Timestamp: Mon, 26 May 2025 09:47:48 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x6a9d07fe95736dd37d9ed2e1d0678c899447f79dd7982997a9b129e0212b2766.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x6a9d07fe95736dd37d9ed2e1d0678c899447f79dd7982997a9b129e0212b2766`,
+  name: `Supply Plume on Mystic&#x27;s MEV Vault`,
+  description: `Supply Plume on Mystic&#x27;s MEV Capital Plume Vault and earn guaranteed Mystic and Plume incentives.
+
+Depositors will earn one Mystic Point per Plume supplied per day.`,
+  is_verified: false,
+});
