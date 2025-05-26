a72c09d04b86ae2c176b8f74c5eb83c3ec3596d4
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-26 02:33:56 -0700

    feat(market-map): Update market -- 98866_0_0x2f0cc7b9660aec4fdb980fcde5990f271b5d5f3ce9ace5c1a6cfbf9b381c1682 -- Recipe (0x2f0c...1682) (#986)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x2f0cc7b9660aec4fdb980fcde5990f271b5d5f3ce9ace5c1a6cfbf9b381c1682 (Market Hash)
    - Request Timestamp: Mon, 26 May 2025 09:33:54 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x2f0cc7b9660aec4fdb980fcde5990f271b5d5f3ce9ace5c1a6cfbf9b381c1682.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x2f0cc7b9660aec4fdb980fcde5990f271b5d5f3ce9ace5c1a6cfbf9b381c1682`,
+  name: `Supply pUSD on Mystic&#x27;s MEV Vault`,
+  description: `Supply pUSD on Mystic&#x27;s MEV Capital pUSD Vault and earn guaranteed Mystic and Plume incentives.
+
+Depositors will earn one Mystic Point per pUSD supplied per day.`,
+  is_verified: false,
+});
