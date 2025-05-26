6ec2be24bebe763b01efbf2d4fa1ad675f53d1a1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-26 02:42:37 -0700

    feat(market-map): Update market -- 98866_0_0xb3f456fbb6061788958ea052b811f2c778bb2ba683af45db9e22f764a928748d -- Recipe (0xb3f4...748d) (#987)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xb3f456fbb6061788958ea052b811f2c778bb2ba683af45db9e22f764a928748d (Market Hash)
    - Request Timestamp: Mon, 26 May 2025 09:42:34 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xb3f456fbb6061788958ea052b811f2c778bb2ba683af45db9e22f764a928748d.ts
@@ -0,0 +1,10 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xb3f456fbb6061788958ea052b811f2c778bb2ba683af45db9e22f764a928748d`,
+  name: `Supply wETH on Mystic&#x27;s MEV Vault`,
+  description: `Supply wETH on Mystic&#x27;s MEV Capital wETH Vault and earn guaranteed Mystic and Plume incentives.
+
+Depositors will earn one Mystic Point for every dollar of wETH supplied per day.`,
+  is_verified: false,
+});
