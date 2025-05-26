90b4c813bfd55fa50c899fd554ef13c9c92e8dad
bharatvish <bharat@waymont.co>
2025-05-26 11:06:05 -0700

    Create 98866-0x4b7e8efddc168a568c25120aa69301130a698c07.ts

---
+++ sdk/constants/market-map/98866/definitions/98866-0x4b7e8efddc168a568c25120aa69301130a698c07.ts
@@ -0,0 +1,14 @@
+import { defineToken } from "@/sdk/constants";
+
+export default defineToken({
+  id: "98866-0x4b7e8efddc168a568c25120aa69301130a698c07",
+  chain_id: 98866,
+  contract_address: "0x4b7e8efddc168a568c25120aa69301130a698c07",
+  name: "Rooster YAP-nBASIS-pUSD-10 ",
+  symbol: "YAP-nBASIS-pUSD-10",
+  image: "https://i.ibb.co/gLH8H50M/Frame-1000004596.png",
+  decimals: 18,
+  source: "rooster",
+  search_id: "0x4b7e8efddc168a568c25120aa69301130a698c07",
+  type: "token",
+});
