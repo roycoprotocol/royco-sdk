1aa57849890946660ad6358dd95affb08beba044
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 12:55:30 -0700

    feat(market-map): Update market -- 98866_0_0x2f6cad82d875fea7c1167dbd0a0c8f31f46e23812e3074f9c42dd41261b416e6 -- Recipe (0x2f6c...16e6)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x2f6cad82d875fea7c1167dbd0a0c8f31f46e23812e3074f9c42dd41261b416e6 (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 19:55:29 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x2f6cad82d875fea7c1167dbd0a0c8f31f46e23812e3074f9c42dd41261b416e6.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x2f6cad82d875fea7c1167dbd0a0c8f31f46e23812e3074f9c42dd41261b416e6`,
+  name: `Lend nETF on Multichainz Protocol`,
+  description: `When nETF is supplied in the market, it is deposited into the Multichainz nETF pool live on Plume Mainnet`,
+  is_verified: false,
+});
