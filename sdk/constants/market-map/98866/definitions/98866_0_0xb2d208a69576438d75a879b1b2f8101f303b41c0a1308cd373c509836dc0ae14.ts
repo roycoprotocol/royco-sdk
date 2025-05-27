387bc40a2fd0dcf67782fb84b6fcf8265e6519db
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 12:22:40 -0700

    feat(market-map): Update market -- 98866_0_0xb2d208a69576438d75a879b1b2f8101f303b41c0a1308cd373c509836dc0ae14 -- Recipe (0xb2d2...ae14)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xb2d208a69576438d75a879b1b2f8101f303b41c0a1308cd373c509836dc0ae14 (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 19:22:40 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xb2d208a69576438d75a879b1b2f8101f303b41c0a1308cd373c509836dc0ae14.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xb2d208a69576438d75a879b1b2f8101f303b41c0a1308cd373c509836dc0ae14`,
+  name: `Lend nBASIS on Multichainz Protocol`,
+  description: `When nBASIS is supplied in the market, it is deposited into the Multichainz nBASIS pool live on Plume Mainnet`,
+  is_verified: false,
+});
