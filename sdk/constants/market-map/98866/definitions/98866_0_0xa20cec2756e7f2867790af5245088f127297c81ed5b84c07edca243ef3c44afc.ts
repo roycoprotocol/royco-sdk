c0b5175b6743225d1836a66d504a006b24c811dd
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 07:24:58 -0700

    feat(market-map): Update market -- 98866_0_0xa20cec2756e7f2867790af5245088f127297c81ed5b84c07edca243ef3c44afc -- Recipe (0xa20c...4afc)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xa20cec2756e7f2867790af5245088f127297c81ed5b84c07edca243ef3c44afc (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 14:24:57 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xa20cec2756e7f2867790af5245088f127297c81ed5b84c07edca243ef3c44afc.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xa20cec2756e7f2867790af5245088f127297c81ed5b84c07edca243ef3c44afc`,
+  name: `Lend USDT on Multichainz Protocol`,
+  description: `When USDT is supplied to the market, it is deposited to the Multichainz USDT pool on Plume Mainnet`,
+  is_verified: false,
+});
