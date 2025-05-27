3fab42f8262bd3768e1737e521c7091a35995893
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 07:55:32 -0700

    feat(market-map): Update market -- 98866_0_0x99437c449bc5e81a78fa8cf7aa82b9cc5c72aeb20c59d1e4d9d487f9c0a5530b -- Recipe (0x9943...530b)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x99437c449bc5e81a78fa8cf7aa82b9cc5c72aeb20c59d1e4d9d487f9c0a5530b (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 14:55:31 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x99437c449bc5e81a78fa8cf7aa82b9cc5c72aeb20c59d1e4d9d487f9c0a5530b.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x99437c449bc5e81a78fa8cf7aa82b9cc5c72aeb20c59d1e4d9d487f9c0a5530b`,
+  name: `Lend WETH on Multichainz Protocol`,
+  description: `When WETH is supplied in the market, it is deposited into the Multichainz WETH pool deployed on Plume Chain`,
+  is_verified: false,
+});
