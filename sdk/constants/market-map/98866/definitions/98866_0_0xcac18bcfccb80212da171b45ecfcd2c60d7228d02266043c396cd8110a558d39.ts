e519642c1cd6925ccf8d4a6bcdc2d023de6e4eba
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 04:19:23 -0700

    feat(market-map): Update market -- 98866_0_0xcac18bcfccb80212da171b45ecfcd2c60d7228d02266043c396cd8110a558d39 -- Recipe (0xcac1...8d39) (#992)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xcac18bcfccb80212da171b45ecfcd2c60d7228d02266043c396cd8110a558d39 (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 11:19:21 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xcac18bcfccb80212da171b45ecfcd2c60d7228d02266043c396cd8110a558d39.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xcac18bcfccb80212da171b45ecfcd2c60d7228d02266043c396cd8110a558d39`,
+  name: `Lend pUSD on Multichainz Protocol`,
+  description: `If you supply and borrow pUSD on Royco, it gets deposited into the Multichainz pUSD pool deployed on Plume Mainnet`,
+  is_verified: false,
+});
