a2aefe2e0b51df406b3d572752326c6779e30bc6
bharatvish <bharat@waymont.co>
2025-05-27 09:50:33 -0700

    Update 98866_0_0x3c711b4305c02ed5a355039962172e0101e51ce33c6cd761276e682d82aaa6b3.ts

--- sdk/constants/market-map/98866/definitions/98866_0_0x3c711b4305c02ed5a355039962172e0101e51ce33c6cd761276e682d82aaa6b3.ts
+++ sdk/constants/market-map/98866/definitions/98866_0_0x3c711b4305c02ed5a355039962172e0101e51ce33c6cd761276e682d82aaa6b3.ts
@@ -2,7 +2,7 @@ import { defineMarket } from "@/sdk/constants";
 
 export default defineMarket({
   id: `98866_0_0x3c711b4305c02ed5a355039962172e0101e51ce33c6cd761276e682d82aaa6b3`,
-  name: `Deposit iZiSwap wPLUME&#x2F;pETH on iZUMi`,
-  description: `Deposit iZiSwap wPLUME&#x2F;pETH on iZUMi for 90 days to earn PLUME incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
+  name: `Deposit iZiSwap wETH/PLUME on iZUMi`,
+  description: `Deposit iZiSwap wETH/PLUME on iZUMi for 90 days to earn PLUME incentives. Users may withdraw early at anytime but must forfeit their incentives.`,
   is_verified: false,
 });
