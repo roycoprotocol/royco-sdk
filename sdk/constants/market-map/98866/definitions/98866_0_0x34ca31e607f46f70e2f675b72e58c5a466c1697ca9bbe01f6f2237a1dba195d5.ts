19a48e3df0c685289249013741edf70eac2c8af1
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 12:06:09 -0700

    feat(market-map): Update market -- 98866_0_0x34ca31e607f46f70e2f675b72e58c5a466c1697ca9bbe01f6f2237a1dba195d5 -- Recipe (0x34ca...95d5) (#998)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x34ca31e607f46f70e2f675b72e58c5a466c1697ca9bbe01f6f2237a1dba195d5 (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 19:06:07 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x34ca31e607f46f70e2f675b72e58c5a466c1697ca9bbe01f6f2237a1dba195d5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x34ca31e607f46f70e2f675b72e58c5a466c1697ca9bbe01f6f2237a1dba195d5`,
+  name: `Lend nALPHA on Multichainz Protocol`,
+  description: `When nALPHA is supplied in the market, it is deposited into the Multichainz nALPHA pool live on Plume Mainnet`,
+  is_verified: false,
+});
