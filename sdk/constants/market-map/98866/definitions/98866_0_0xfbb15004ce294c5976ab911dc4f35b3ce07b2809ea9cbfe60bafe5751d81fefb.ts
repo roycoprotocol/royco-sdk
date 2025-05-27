27876a0e242838024ab9280fded4f4a3b6bfa0a5
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-26 20:50:15 -0700

    feat(market-map): Update market -- 98866_0_0xfbb15004ce294c5976ab911dc4f35b3ce07b2809ea9cbfe60bafe5751d81fefb -- Recipe (0xfbb1...fefb)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xfbb15004ce294c5976ab911dc4f35b3ce07b2809ea9cbfe60bafe5751d81fefb (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 03:50:15 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xfbb15004ce294c5976ab911dc4f35b3ce07b2809ea9cbfe60bafe5751d81fefb.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xfbb15004ce294c5976ab911dc4f35b3ce07b2809ea9cbfe60bafe5751d81fefb`,
+  name: `Deposit into a Rooster YAP for the nBASIS-pUSD Pool`,
+  description: `Deposit into a Rooster YAP for the nBASIS-pUSD Pool, then deposit the YAP token into this market for PLUME incentives.`,
+  is_verified: false,
+});
