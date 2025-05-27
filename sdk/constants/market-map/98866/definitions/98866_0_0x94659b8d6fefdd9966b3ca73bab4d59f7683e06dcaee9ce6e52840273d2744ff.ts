33730cd8e398fcf7a45eb9772ff4fe3375ba20d7
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 12:46:19 -0700

    feat(market-map): Update market -- 98866_0_0x94659b8d6fefdd9966b3ca73bab4d59f7683e06dcaee9ce6e52840273d2744ff -- Recipe (0x9465...44ff) (#1000)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x94659b8d6fefdd9966b3ca73bab4d59f7683e06dcaee9ce6e52840273d2744ff (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 19:46:17 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x94659b8d6fefdd9966b3ca73bab4d59f7683e06dcaee9ce6e52840273d2744ff.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x94659b8d6fefdd9966b3ca73bab4d59f7683e06dcaee9ce6e52840273d2744ff`,
+  name: `Lend nELIXIR on Multichainz Protocol`,
+  description: `When nELIXIR is supplied in the market, it is deposited into the Multichainz nELIXIR pool live on Plume Mainnet`,
+  is_verified: false,
+});
