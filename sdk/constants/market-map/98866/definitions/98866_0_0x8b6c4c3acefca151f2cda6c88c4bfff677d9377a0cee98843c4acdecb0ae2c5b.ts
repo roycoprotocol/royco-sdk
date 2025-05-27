3cdc5aea76b3907efee018af98ce1849d64d6d2a
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 07:36:53 -0700

    feat(market-map): Update market -- 98866_0_0x8b6c4c3acefca151f2cda6c88c4bfff677d9377a0cee98843c4acdecb0ae2c5b -- Recipe (0x8b6c...2c5b)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0x8b6c4c3acefca151f2cda6c88c4bfff677d9377a0cee98843c4acdecb0ae2c5b (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 14:36:53 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0x8b6c4c3acefca151f2cda6c88c4bfff677d9377a0cee98843c4acdecb0ae2c5b.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0x8b6c4c3acefca151f2cda6c88c4bfff677d9377a0cee98843c4acdecb0ae2c5b`,
+  name: `Lend pETH on Multichainz Protocol`,
+  description: `When pETH is supplied in the market, it is deposited into the Multichainz pETH pool on Plume Mainnet`,
+  is_verified: false,
+});
