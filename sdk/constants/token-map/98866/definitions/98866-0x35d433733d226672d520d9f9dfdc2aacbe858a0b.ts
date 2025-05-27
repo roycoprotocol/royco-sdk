c7ee8cd91c6e9b711a33b4ed73c296ff2b1ddf8b
bharatvish <bharat@waymont.co>
2025-05-27 10:18:50 -0700

    Update 98866-0x35d433733d226672d520d9f9dfdc2aacbe858a0b.ts

--- sdk/constants/token-map/98866/definitions/98866-0x35d433733d226672d520d9f9dfdc2aacbe858a0b.ts
+++ sdk/constants/token-map/98866/definitions/98866-0x35d433733d226672d520d9f9dfdc2aacbe858a0b.ts
@@ -5,7 +5,7 @@ export default defineToken({
   chain_id: 98866,
   contract_address: "0x35d433733d226672d520d9f9dfdc2aacbe858a0b",
   name: "zToken Reward",
-  symbol: "nBASIS",
+  symbol: "zToken",
   image: "https://img.cryptorank.io/coins/multichain_z1735900735195.png",
   decimals: 18,
   source: "",
