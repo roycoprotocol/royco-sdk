8d5b4f3bcf26e1eeea9b08eda800c7170022ebcb
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 11:56:38 -0700

    feat(market-map): Update market -- 98866_0_0xea3dcf2cd381b888ba6b0aadfc31ce5946402661c287dd4909ada41b9b250ac5 -- Recipe (0xea3d...0ac5) (#997)
    Market Details:
    - Chain ID: 98866
    - Market Type: 0 (Recipe)
    - Market ID: 0xea3dcf2cd381b888ba6b0aadfc31ce5946402661c287dd4909ada41b9b250ac5 (Market Hash)
    - Request Timestamp: Tue, 27 May 2025 18:56:36 GMT

---
+++ sdk/constants/market-map/98866/definitions/98866_0_0xea3dcf2cd381b888ba6b0aadfc31ce5946402661c287dd4909ada41b9b250ac5.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `98866_0_0xea3dcf2cd381b888ba6b0aadfc31ce5946402661c287dd4909ada41b9b250ac5`,
+  name: `Lend nTBILL on Multichainz Protocol`,
+  description: `When nTBILL is supplied in the market, it is deposited into the Multichainz nTBILL pool live on Plume Mainnet`,
+  is_verified: false,
+});
