42b23133fa6caefa4ac201163ded8c0028a3a557
Uday Khokhariya <101034951+githubotoro@users.noreply.github.com>
2025-05-27 15:17:31 -0700

    feat(market-map): Update market -- 8453_1_0x6eaf2a89e4930a8124d9424625c8dfb74dd23dcb -- Vault (0x6eaf...3dcb) (#1003)
    Market Details:
    - Chain ID: 8453
    - Market Type: 1 (Vault)
    - Market ID: 0x6eaf2a89e4930a8124d9424625c8dfb74dd23dcb (Wrapped Vault Address)
    - Request Timestamp: Tue, 27 May 2025 22:17:29 GMT

---
+++ sdk/constants/market-map/8453/definitions/8453_1_0x6eaf2a89e4930a8124d9424625c8dfb74dd23dcb.ts
@@ -0,0 +1,8 @@
+import { defineMarket } from "@/sdk/constants";
+
+export default defineMarket({
+  id: `8453_1_0x6eaf2a89e4930a8124d9424625c8dfb74dd23dcb`,
+  name: `Lend USDC on SuperFunds`,
+  description: `When you deposit USDC, SuperFund seamlessly routes your funds to the most profitable markets available, ensuring you earn the best yield at all times. The vault continuously monitors the market, making real-time adjustments to ensure your funds are always placed in the highest-performing blue-chip protocols, including Aave, Morpho, Euler, and Fluid.`,
+  is_verified: false,
+});
